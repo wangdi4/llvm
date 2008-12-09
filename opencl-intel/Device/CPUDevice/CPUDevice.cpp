ef77bb084c9d400086854c888cbd177c2bcab746
Fiksman, Evgeny <evgeny.fiksman@intel.com>
2008-12-09 17:19:11 +0000

    1. Moved test project to root. (DevPartner doesn't work in other case) 2. Added dependencies between projects 3. Change output directory in CPUDevice project to be routed to bin directory 4. Removed DllMain() function from CPUDevice.cpp, it presents in another file. Otherwise solution doesn't pass compilation.

--- opencl-intel/Device/CPUDevice/CPUDevice.cpp
+++ opencl-intel/Device/CPUDevice/CPUDevice.cpp
@@ -8,13 +8,6 @@
 #pragma managed(push, off)
 #endif
 
-BOOL APIENTRY DllMain( HMODULE hModule,
-                       DWORD  ul_reason_for_call,
-                       LPVOID lpReserved
-					 )
-{
-    return TRUE;
-}
 
 #ifdef _MANAGED
 #pragma managed(pop)
