5db6d1c149e6a8930812ca01dd58fe55877b2ee9
Fiksman, Evgeny <evgeny.fiksman@intel.com>
2008-12-15 08:31:58 +0000

    1. Updating device_api.h with logger API 2. Export clDevInitDevice() from CPU device. 3. Fixing compilation & linking issues    a. All project are statically linked with C runtime    b. removing 64bit comp. check

--- opencl-intel/Device/CPUDevice/CPUDevice.cpp
+++ opencl-intel/Device/CPUDevice/CPUDevice.cpp
@@ -13,3 +13,10 @@
 #pragma managed(pop)
 #endif
 
+cl_dev_err_code clDevCreateDevice(	cl_uint	dev_id,
+									cl_dev_entry_points	*pDevEntry,
+									cl_dev_call_backs	*pDevCallBacks
+									)
+{
+	return CL_DEV_INVALID_OPERATION;
+}
