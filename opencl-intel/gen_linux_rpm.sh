4ad56563e8eaac6c0b4552bcafb5c74aec2400de
Kosnitsky, Simeon <imeon.kosnitsky@intel.com>
2011-03-10 12:32:09 +0000

    QABS: Added script for rpm generations.

--- opencl-intel/gen_linux_rpm.sh
+++ opencl-intel/gen_linux_rpm.sh
@@ -1,33 +1,40 @@
 #!/bin/sh
 
-@echo off
-
 cd `dirname $0`
 export CUR_DIR=`pwd`
 
-perl -v | grep "5.10">nul
-if [ $? -neq 0 ]
+perl -v | grep "5.10">/dev/null
+if [ $? != 0 ]
 	then
-		echo ActivePerl v5.10.1 is not currently installed and it's required for msi's generations. Please install it from "\\ger\ec\proj\ha\ptl\MobileTV\Installations\ActivePerl" or from "\\nntavc101xwb1.ccr.corp.intel.com\AVC.QA_OpenCL_resources\OpenCL Tools\ActivePerl". 
+		echo "ActivePerl v5.10.* is not currently installed and it's required for rpms generations. Please install it from yast."
 		exit 1
 fi
 
-cd $CUR_DIR%/../BuildSystem
-if [ $? -neq 0 ]
+cd $CUR_DIR/../BuildSystem
+
+perl -c run.pl
+
+if [ $? != 0]
 	then
-		echo Directory "$CUR_DIR/../BuildSystem" doesn't exist, please sync it from svn.
+		echo "Please install XML Simple v2.18 from '\\ger\ec\proj\ha\ptl\MobileTV\Installations\XML-Simple-2.18' or from '\\nntavc101xwb1.ccr.corp.intel.com\AVC.QA_OpenCL_resources\OpenCL Tools\Linux\XML-Simple-2.18'"
 		exit 1
 fi
 
-if [ "$1" -eq "debug" ]
+if [ $? != 0 ]
 	then
-		call ./build.sh -bt opencl11_create_rpm -p -napz -c -glp binaries_target[Debug]
+		echo "Directory '$CUR_DIR/../BuildSystem' doesn't exist, please sync it from svn."
+		exit 1
+fi
+
+if [ "$1" == "debug" ]
+	then
+		./build.sh -bt opencl11_create_rpm -p -napz -c -glp binaries_target[Debug]
 	else
-		call ./build.sh -bt opencl11_create_rpm -p -napz -c
+		./build.sh -bt opencl11_create_rpm -p -napz -c
 fi
 
-if [ $? -neq 0 ]
+if [ $? != 0 ]
 	then
-		echo Please see more details in "$CUR_DIR/../BuildSystem/logs/Summary.log".
+		echo "Please see more details in '$CUR_DIR/../BuildSystem/logs/Summary.log'."
 		exit 1
 fi
