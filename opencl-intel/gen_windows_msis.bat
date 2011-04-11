abbb072f783424cbd2eea20ded2aef8315bb24f7
Kosnitsky, Simeon <imeon.kosnitsky@intel.com>
2011-04-11 12:29:39 +0000

    QABS: Updated msi gen script (removed pause)

--- opencl-intel/gen_windows_msis.bat
+++ opencl-intel/gen_windows_msis.bat
@@ -12,25 +12,20 @@ if "%1" EQU "debug" (
 call build.bat -bt opencl11_create_msis -p -napz -c -glp binaries_target[Debug]
 ) else call build.bat -bt opencl11_create_msis -p -napz -c
 if %ERRORLEVEL% NEQ 0 goto BUILDFAIL
-pause
 exit /B %ERRORLEVEL%
 
 :PERLNE
 echo ActivePerl v5.10.1 is not currently installed and it's required for msi's generations. Please install it from "\\ger\ec\proj\ha\ptl\MobileTV\Installations\ActivePerl" or from "\\nntavc101xwb1.ccr.corp.intel.com\AVC.QA_OpenCL_resources\OpenCL Tools\ActivePerl".
-pause
 exit /B 1
 
 :WIXNE
 echo Directory "%ProgramFiles%\Windows Installer XML\bin" doesn't exist. Please install WIX v2.0 to it. Which can be obtained from "\\ger\ec\proj\ha\ptl\MobileTV\Installations\WIX Tools\Windows Installer XML" or from "\\nntavc101xwb1.ccr.corp.intel.com\AVC.QA_OpenCL_resources\OpenCL Tools\WIX Tools\Windows Installer XML".
-pause
 exit /B 1
 
 :BSNE
 echo Directory "%CUR_DIR%..\BuildSystem" doesn't exist, please sync it from svn.
-pause
 exit /B 1
 
 :BUILDFAIL
 echo Please see more details in "%CUR_DIR%..\BuildSystem\logs\Summary.log".
-pause
 exit /B 1
