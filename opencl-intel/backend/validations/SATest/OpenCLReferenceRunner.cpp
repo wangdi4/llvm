10f797b8866a0c4b3332dc54be883c1a951abab3
Maslov, Oleg <oleg.maslov@intel.com>
2012-01-31 12:30:42 +0000

    Fix for CSSD100012442 Stencil2d reference don't match the CG output Fixed OCL reference in SATest. It failed on SHOC workload Stencil2D. SATest were executed in validation with single workgroup (-single_wg). Reason of failure is overwriting variable with Global Work group sizes in single_wg mode. As solution I Separated global workgroup loop threshold into separate variable
    CR: Mohammed, AlexeyB, http://cvcc-ubu-01.iil.intel.com/cr/r/534/diff/
    Pre-commit passed

--- opencl-intel/backend/validations/SATest/OpenCLReferenceRunner.cpp
+++ opencl-intel/backend/validations/SATest/OpenCLReferenceRunner.cpp
@@ -836,6 +836,9 @@ void OpenCLReferenceRunner::RunKernel( IRunResult * runResult,
     std::vector<uint64_t> globalWGSizes(MAX_WORK_DIM, 1);
     ConvertSizeTtoUint64T(pKernelConfig->GetGlobalWorkSize(), globalWGSizes, workDim);
 
+    // init global workgroup sizes for usage in execution loop
+    std::vector<uint64_t> loopGlobalWGSizes(globalWGSizes);
+
     // init local workgroup sizes with ones
     std::vector<uint64_t> localWGSizes(MAX_WORK_DIM, 1);
     // convert size_t to uint64_t
@@ -852,7 +855,7 @@ void OpenCLReferenceRunner::RunKernel( IRunResult * runResult,
         if(localWGSizes[i] == 0)
         {   // set local Work size
             localWGSizes[i] = std::min<uint64_t>(
-                globalWGSizes[i],
+                loopGlobalWGSizes[i],
                 uint64_t(runConfig->GetValue<uint32_t>(RC_COMMON_DEFAULT_LOCAL_WG_SIZE, 0)));
         }
     }
@@ -861,9 +864,9 @@ void OpenCLReferenceRunner::RunKernel( IRunResult * runResult,
     // reference runs only single work group
     if(runConfig->GetValue<bool>(RC_COMMON_RUN_SINGLE_WG, false))
     {
-        globalWGSizes[0] = localWGSizes[0];
-        globalWGSizes[1] = localWGSizes[1];
-        globalWGSizes[2] = localWGSizes[2];
+        loopGlobalWGSizes[0] = localWGSizes[0];
+        loopGlobalWGSizes[1] = localWGSizes[1];
+        loopGlobalWGSizes[2] = localWGSizes[2];
     }
 
     // work item storage
@@ -930,11 +933,11 @@ void OpenCLReferenceRunner::RunKernel( IRunResult * runResult,
 
     // global work item execution loop
     // global offset is not added (included) to loop variables
-    for(uint64_t tid_z=0;tid_z<globalWGSizes[2];tid_z+=localWGSizes[2])
+    for(uint64_t tid_z=0;tid_z<loopGlobalWGSizes[2];tid_z+=localWGSizes[2])
     {
-        for(uint64_t tid_y=0;tid_y<globalWGSizes[1];tid_y+=localWGSizes[1])
+        for(uint64_t tid_y=0;tid_y<loopGlobalWGSizes[1];tid_y+=localWGSizes[1])
         {
-            for(uint64_t tid_x=0;tid_x<globalWGSizes[0];tid_x+=localWGSizes[0])
+            for(uint64_t tid_x=0;tid_x<loopGlobalWGSizes[0];tid_x+=localWGSizes[0])
             {
                 // Run static constructors.
                 FOR3_LOCALWG {
