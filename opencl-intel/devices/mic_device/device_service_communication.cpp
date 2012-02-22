07584710816880849cb78cf075ff71897f9e8541
Deri, Adir <adir.deri@intel.com>
2012-02-22 11:49:34 +0000

    Change back the coi process create request to use reserved memory due to coi defect. (Also add the sleep(2) because of it)

--- opencl-intel/devices/mic_device/device_service_communication.cpp
+++ opencl-intel/devices/mic_device/device_service_communication.cpp
@@ -56,6 +56,8 @@ cl_dev_err_code DeviceServiceCommunication::deviceSeviceCommunicationFactory(uns
                                                                              MICDeviceConfig *config,
                                                                              DeviceServiceCommunication** ppDeviceServiceCom)
 {
+	// TODO - Remove the next sleep command when the COI issue HSD 4115835 will be solve.
+	sleep(2);
     // find the first unused device index
     DeviceServiceCommunication* tDeviceServiceComm = new DeviceServiceCommunication(uiMicId, config);
 	if (NULL == tDeviceServiceComm)
@@ -263,7 +265,7 @@ void* DeviceServiceCommunication::initEntryPoint(void* arg)
 										 0, NULL,													// argc, argv
 										 false, NULL,												// duplicate env, additional env vars
 										 MIC_DEV_IO_PROXY_TO_HOST, NULL,							// I/O proxy required + host root
-                                         0,															// reserve buffer space
+                                         MIC_AVAILABLE_PROCESS_MEMORY(pDevServiceComm->m_uiMicId),	// reserve buffer space
 										 nativeDirName,												// a path to locate dynamic libraries dependencies for the sink application
 										 &pDevServiceComm->m_process);
 		assert(result == COI_SUCCESS && "COIProcessCreateFromFile failed");
