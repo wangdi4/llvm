2f22e8e830fe26dd4a07dbb6439a16c049cae86c
nrotem <nrotem>
2012-03-22 21:02:50 +0000

    Increase the unroll threshold in the standard passes (O3) from 128 to 1024.
    This may (or may not) improve the performance of intel_median which may (or may not) have a regression in its scalar version. 
    
    This is a speculative commit since we don't have a way to measure the workloads locally.

--- opencl-intel/backend/ocl_cpu_backend/Optimizer.cpp
+++ opencl-intel/backend/ocl_cpu_backend/Optimizer.cpp
@@ -32,8 +32,10 @@ File Name:  Optimizer.cpp
 #include "llvm/Transforms/IPO.h"
 #include "llvm/Transforms/Scalar.h"
 #include "llvm/Analysis/Passes.h"
+#include "llvm/Support/raw_ostream.h"
 #include "llvm/Transforms/Utils/UnifyFunctionExitNodes.h"
 #include "llvm/Analysis/Verifier.h"
+#include "llvm/Assembly/PrintModulePass.h"
 #include "PrintIRPass.h"
 
 extern "C" llvm::Pass *createVectorizerPass(const llvm::Module *runtimeModule,
@@ -130,7 +132,7 @@ void getKernelInfoMap(llvm::ModulePass *pKUPath, std::map<const llvm::Function*,
     PM->add(createIndVarSimplifyPass());        // Canonicalize indvars
     PM->add(createLoopDeletionPass());          // Delete dead loops
     if (UnrollLoops) {
-      PM->add(createLoopUnrollPass());          // Unroll small loops
+      PM->add(createLoopUnrollPass(1024, 0, 0));          // Unroll small loops
       PM->add(createScalarReplAggregatesPass(256));  // Break up aggregate allocas
     }
     PM->add(createInstructionCombiningPass());  // Clean up after the unroller
