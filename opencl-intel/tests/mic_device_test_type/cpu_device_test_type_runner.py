bf58f8a555da42683aaaef1ec08b043f4de9001c
Deri, Adir <adir.deri@intel.com>
2012-03-28 14:53:33 +0000

    Add mic device test type

---
+++ opencl-intel/tests/mic_device_test_type/cpu_device_test_type_runner.py
@@ -0,0 +1,29 @@
+import sys
+import os
+from cmk_test_type_runner import *
+
+def main():
+    osType = sys.platform
+    #add current working directory to PATH / LD_LIBRARY_PATH in order to find libs
+    if osType == 'win32':
+        oldPath = os.getenv('PATH')
+        os.putenv('PATH', os.getcwd() + ";" + oldPath)
+    else:
+        oldPath = os.getenv('LD_LIBRARY_PATH')
+        if oldPath == None:
+            os.putenv('LD_LIBRARY_PATH', os.getcwd())
+        else:
+            os.putenv('LD_LIBRARY_PATH', os.getcwd() + ":" + oldPath)
+    prefixPath = 'validation/cpu_device_test_type'
+    os.chdir(prefixPath)
+    prefixPath = '.'
+    script = prefixPath + '/cmk_cpu_device_test_type.py'
+    if osType == 'win32':
+        cfgPath = prefixPath + '/cpu_device_test/cfg.xml'
+    else:
+        cfgPath = prefixPath + '/cpu_device_test/cfg_linux.xml'
+    outDir = prefixPath + '/result'
+    
+    run_test_type(prefixPath, script, cfgPath, outDir)
+    
+main()
