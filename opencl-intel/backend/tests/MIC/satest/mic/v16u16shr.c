6e9e0d9053847160331ddd40ea9f46f964dec6c5
Bader, Aleksey A <aleksey.a.bader@intel.com>
2012-04-24 10:06:45 +0000

    <LOG_BODY> - Fixed explicit_s2v_char, explicit_s2v_uchar, explicit_s2v_short and explicit_s2v_ushort subtests of test basic. <RELATED_CQ> - None. <CODE_REVIEW> - http://cvcc-ubu-01.iil.intel.com/cr/r/797/. <PERF_IMPLICATIONS> - None. <TEST> - Added 2 codegen tests and 4 'satests'. <CHECKED> - pre-commit: http://cvcc-fc14-01.iil.intel.com:8080/job/PCompiler_aabader/19/.

---
+++ opencl-intel/backend/tests/MIC/satest/mic/v16u16shr.c
@@ -0,0 +1,11 @@
+// RUN: pwd
+// RUN: clang -cc1 -cl-kernel-arg-info -emit-llvm-bc -x cl -I ../../../../../../../src/backend/clang_headers/ \
+// RUN: -include opencl_.h -o %s.bc %s
+// RUN: python ../../bin/SATest.py -config=%s.cfg.xml -tsize=1
+
+__kernel void test(__global ushort16 * in)
+{
+    int id = get_global_id(0);
+    in[id] = in[id] >> in[id];
+}
+
