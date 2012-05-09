cf0e5d06a84c2b5ddadc11966b7e2ef9f0a9d698
Shemer, Anat <anat.shemer@intel.com>
2012-05-09 11:52:55 +0000

    added script that gives dynamic instruction count and cycle count of SHOC workloads jitted code.

---
+++ opencl-intel/tests/shoc1.1.1/inst_count.pl
@@ -0,0 +1,58 @@
+#/usr/bin/perl
+
+use File::Temp qw/ tempfile tempdir /;
+
+@bmlst = ("Triad", "SGEMM", "Stencil2D", "FFT", "Spmv", "Sort", "MD", "Scan", "MaxFlops", "Reduction", "S3D");
+#@bmlst = ("Sort");
+
+@isVect = ("True", "False");
+@codeType = ("Vect", "Scalar");
+
+$tempdir = tempdir("/tmp/shocCIXXXX");
+
+foreach $prog (@bmlst) {
+  $progpath = "bin/Serial/OpenCL/" . $prog;
+  for ($i = 0; $i < 2; $i++) {
+    print "Running $prog $codeType[$i]\n";
+    $ENV{CL_CONFIG_USE_VECTORIZER} = $isVect[$i];
+    $resfile = $tempdir . "/res" . $codeType[$i] . "_" . $prog;
+    system ("/opt/intel/sep3.9/bin/sep -start -mic -um -d 0 -out $resfile &");
+    system ("$progpath");
+    system ("/opt/intel/sep3.9/bin/sep -stop -mic");
+    $resfile .= ".tb6";
+    while (! -r $resfile) {};
+    $result = `/opt/intel/sep3.9/bin/sfdump5 $resfile -modules`;
+    while ($result) {
+      ($line, $result) = split /\n/, $result, 2;
+      if ($line =~ /\+Other64\+/) {
+        ($line, $result) = split /\n/, $result, 2;
+        ($samplesI) = $line =~ / (\d+) /;
+        ($samplesC) = $result =~ / (\d+) /;
+        $icounts{$prog}{$codeType[$i]}{C} = $samplesC;
+        $icounts{$prog}{$codeType[$i]}{I} = $samplesI;
+        print "$prog $codeType[$i] I $samplesI C $samplesC\n";
+        last;
+      }
+    }
+  }
+  print "SHOC results are under $tempdir\n";
+}
+
+print "\n\nInstruction and Cycle count (samples) for workloads\n";
+print "Workload,Vectorized I,Scalar I,Vectorized C,Scalar C\n";
+
+foreach $prog (@bmlst) {
+  print "$prog";
+  $samples = $icounts{$prog}{$codeType[0]}{I};
+  print ",$samples";
+  $samples = $icounts{$prog}{$codeType[1]}{I};
+  print ",$samples";
+  $samples = $icounts{$prog}{$codeType[0]}{C};
+  print ",$samples";
+  $samples = $icounts{$prog}{$codeType[1]}{C};
+  print ",$samples";
+  print "\n";
+}
+
+print "To get actual event number the sample count should be multiplied by 2000000\n";
+
