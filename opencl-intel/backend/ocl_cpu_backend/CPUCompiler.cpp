6d464c9d77972214ce88d1020fc85b7b0cfa322c
Demikhovsky, Elena <elena.demikhovsky@intel.com>
2012-07-10 08:05:57 +0000

    Enabled FMA for HSW.

--- opencl-intel/backend/ocl_cpu_backend/CPUCompiler.cpp
+++ opencl-intel/backend/ocl_cpu_backend/CPUCompiler.cpp
@@ -292,7 +292,7 @@ void CPUCompiler::SelectCpu( const std::string& cpuName, const std::string& cpuF
     if (!DisableAVX && (selectedCpuId == Intel::CPU_HASWELL)) {
       m_forcedCpuFeatures.push_back("+avx2");
       m_forcedCpuFeatures.push_back("+f16c");
-      //m_forcedCpuFeatures.push_back("+fma3");
+      m_forcedCpuFeatures.push_back("+fma3");
     }
 
     unsigned int selectedCpuFeatures = Utils::SelectCpuFeatures( selectedCpuId, m_forcedCpuFeatures );
