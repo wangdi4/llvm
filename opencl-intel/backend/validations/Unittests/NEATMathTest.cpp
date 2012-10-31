3a1ff8fcf7048855102588544c18ab7a2968c4ad
Veselov, Yuri <yuri.veselov@intel.com>
2012-10-31 13:53:51 +0000

    Log message: <LOG_BODY> - function half_exp10 enabled in ValidationTests (used to be disable due to CSSD100014655) fix was committed at revision 50052 <CODE_REVIEW> - None <PERF_IMPLICATIONS> - None. <TEST> - locally launched ValidationTests

--- opencl-intel/backend/validations/Unittests/NEATMathTest.cpp
+++ opencl-intel/backend/validations/Unittests/NEATMathTest.cpp
@@ -1183,8 +1183,7 @@ TYPED_TEST(NEATExpTestRun, DISABLED_native_exp10)
     expTest.TestExp(NEATFunc, NEATFuncVec, RefFunc, float(NEATALU::NATIVE_EXP10_ERROR));
 }
 
-// disable until bug CSSD100014655 will be fixed
-TYPED_TEST(NEATExpTestRun, DISABLED_half_exp10)
+TYPED_TEST(NEATExpTestRun, half_exp10)
 {
     RefALU::SetFTZmode(TypeParam::mode); // we use ValueTypeContainer type here, T::mode is FTZ mode, can be true or false
     typedef typename TypeParam::Type TypeP;
