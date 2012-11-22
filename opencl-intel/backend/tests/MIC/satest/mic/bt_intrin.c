0dd7faee5d4ae7fbe4b61c463e01d3049c654ce3
Mishne, Alon <alon.mishne@intel.com>
2012-11-22 16:32:11 +0000

    Changes in MIC codegen:
    * Changing the order of parameters used with an arithmetic / logical operand for a comparison (in the generation of setcc and cmov).
    
    * Changing the treatment of BT node to use an appropriate cc string for PCG, instead of emulating the instruction's behavior.
    
    * Removed PerformSETCCCombine function in Y86ISelLowering which (apparently) needlessly expanded a bit into a byte after one kind of setcc, and has had a negative impact on the new use of BT instructions.
    
    These changes allow Hydro 2D HMPP to pass and potentially increase the performance of code which is folded into BT.

---
+++ opencl-intel/backend/tests/MIC/satest/mic/bt_intrin.c
@@ -0,0 +1,6 @@
+__kernel void bt_intrin(__global int* shift, __global int* mask, __global int* out) {
+  int id = get_global_id(0);
+  int num = 1 << shift[id];
+  int result = num & mask[id];
+  out[id] = result == 0;
+}
