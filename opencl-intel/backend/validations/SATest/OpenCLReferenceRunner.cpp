7795377d6208146d7031a6576583f010f3519ea9
dlachino <dlachino>
2012-11-29 15:14:47 +0000

    <DESCRIPTION> bug fix for CSSD100014954(Reference crashed on SHOC/FFT workload.) <CODE REVIEW> http://cvcc-ubu-01.iil.intel.com/cr/r/1729/ <TESTS> precommit http://cvcc-fc14-01.iil.intel.com:8080/job/PersonalCompilerReport/2086/

--- opencl-intel/backend/validations/SATest/OpenCLReferenceRunner.cpp
+++ opencl-intel/backend/validations/SATest/OpenCLReferenceRunner.cpp
@@ -788,9 +788,7 @@ void OpenCLReferenceRunner::RunKernel( IRunResult * runResult,
         runConfig->GetValue<uint64_t>(RC_COMMON_RANDOM_DG_SEED, 0));
 
     // memory for storing kernel data marked with local addr space
-    // Warning!: method delete [] is called on scratchMem
-    // be careful that pointers in scratchMem are allocated by operator new[]
-    std::vector<int8_t*> localsMemVec;
+    NEATPlugIn::GlobalAddressMapTy NEATlocalMap;
 
     std::vector<GenericValue> ArgVals;
     std::string kernelName = pKernelConfig->GetKernelName();
@@ -892,7 +890,7 @@ void OpenCLReferenceRunner::RunKernel( IRunResult * runResult,
         if(m_bUseNEAT)
         {
             // create NEAT plug in
-            pNEAT = new NEATPlugIn(m_bUseFmaNEAT);
+            pNEAT = new NEATPlugIn(m_bUseFmaNEAT, NEATlocalMap);
             InterpreterPluggable *pInterp = static_cast<InterpreterPluggable*>(pExecEngine);
             pInterp->addPlugIn(*pNEAT);
             // set arguments for NEAT
@@ -950,41 +948,26 @@ void OpenCLReferenceRunner::RunKernel( IRunResult * runResult,
                     }
                 }
 
-                std::vector<void*> localNEATMemList;
                 // handle local variables for NEAT
                 if(m_bUseNEAT)
                 {
-                    FOR3_LOCALWG
+                    NEATPlugIn *pNeat = localNEATs[0];
+
+                    for(uint32_t i=0, cntNeat=0;i<localList.size();++i)
                     {
-                        NEATPlugIn *pNeat = localNEATs[idx];
+                        const GlobalVariable * GV = localList[i];
+                        const Type *GlobalType =
+                            GV->getType()->getElementType();
 
-                        for(uint32_t i=0, cntNeat=0;i<localList.size();++i)
-                        {
-                            const GlobalVariable * GV = localList[i];
-                            const Type *GlobalType =
-                                GV->getType()->getElementType();
+                        // skip unsupported variables
+                        if(NEATTargetData::IsNEATSupported(GlobalType) == false)
+                            continue;
 
-                            // skip unsupported variables
-                            if(NEATTargetData::IsNEATSupported(GlobalType)
-                                                            == false)
-                                continue;
-
-                            DEBUG(dbgs() << "About to add NEAT supported "
-                                "__local variable:\n " << *GV << "\n");
-
-                            if(idx == 0)
-                            {   // zero WI allocate space
-                                void *globalMem =
-                                    pNeat->getOrEmitGlobalVariable(GV);
-                                localNEATMemList.push_back(globalMem);
-                                localsMemVec.push_back((int8_t*) globalMem);
-                            }
-                            else
-                            {   // next WIs add mapping
-                                pNeat->updateGlobalMapping(localList[i], localNEATMemList[cntNeat]);
-                            }
-                            cntNeat++;
-                        }
+                        DEBUG(dbgs() << "About to add NEAT supported "
+                            "__local variable:\n " << *GV << "\n");
+                        // zero WI allocate space
+                        pNeat->getOrEmitGlobalVariable(GV);
+                        cntNeat++;
                     }
                 } // if(m_bUseNEAT)
 
@@ -1043,8 +1026,11 @@ void OpenCLReferenceRunner::RunKernel( IRunResult * runResult,
         delete localEngines[i];
     }
 
-    for(std::size_t i=0; i<localsMemVec.size(); ++i)
-        delete [] localsMemVec[i];
+    for(NEATPlugIn::GlobalAddressMapTy::iterator I = NEATlocalMap.begin(), E=NEATlocalMap.end(); 
+        I!=E; ++I)
+    {
+        delete [] (int8_t*)(*I).second;
+    }
 
     // release lock
     InterpreterLock->release();
