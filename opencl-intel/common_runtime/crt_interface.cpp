84eabc7d8ee64ef9ba1d8073e12427854ed9f291
Jiossy, Rami <rami.jiossy@intel.com>
2012-12-10 11:00:30 +0000

    Fixes bug HSD 4730601. CL_INVALID_KERNEL_DEFINITION, fix point 1 of the following:
    Current clCreateKernel implementation in CRT creates two cl_kernel objects (one for each device) in order to create one CRT cl_kernel object returned to application.
    1.	Additional code required to pass negative test mentioned in HSD (good enough solution -  “number of arguments” checking):
      a.	Query for number of kernel arguments for each device (call to clGetKernelInfo with CL_KERNEL_NUM_ARGS parameter for each created cl_kernel object)
      b.	Comparison of returned values
    2.	Additional checks explicitly mentioned in OpenCL spec (“the argument types”):
      a.	Verification of values returned by call to clGetKernelArgInfo with CL_KERNEL_ARG_TYPE_NAME for each kernel argument (data available for program built with the -cl-kernel-arg-info option)
    3.	Additional checks for ideal solution (“function definition for __kernel function given by kernel_name”)
      a.	Verification of remaining data provided by clGetKernelInfo - CL_KERNEL_ATTRIBUTES
      b.	Verification of remaining data provided by clGetKernelArgInfo for each kernel argument (for program built with the -cl-kernel-arg-info option) - CL_KERNEL_ARG_ADDRESS_QUALIFIER, CL_KERNEL_ARG_ACCESS_QUALIFIER, CL_KERNEL_ARG_TYPE_QUALIFIER
    
    
    
    review board: http://cvcc-ubu-01.iil.intel.com/cr/r/1813/
    code collaborator: https://codecollaborator.intel.com/ui#file:review=14735/version=412932
    
    
    Tests: Pre-Commit
    Reviewer: Singer, Doron

--- opencl-intel/common_runtime/crt_interface.cpp
+++ opencl-intel/common_runtime/crt_interface.cpp
@@ -3683,6 +3683,8 @@ cl_kernel CL_API_CALL clCreateKernel( cl_program       program ,
     }
     CrtContext* ctx = pgm->m_contextCRT;
 
+    cl_uint num_kernel_args = 0;
+
     for( cl_uint i=0; i < pgm->m_buildContexts.size(); i++)
     {
         cl_context ctxObj = pgm->m_buildContexts[i];
@@ -3692,6 +3694,23 @@ cl_kernel CL_API_CALL clCreateKernel( cl_program       program ,
         if (CL_SUCCESS == errCode)
         {
             crtKernel->m_ContextToKernel[ctxObj] = knlObj;
+            
+            // As for now, we only support #difference number of arguments detection for the sake of 
+            // supporting CL_INVALID_KERNEL_DEFINITION
+            if( i == 0 )
+            {
+                knlObj->dispatch->clGetKernelInfo(knlObj, CL_KERNEL_NUM_ARGS, sizeof( cl_uint ), &num_kernel_args, NULL );
+            }
+            else
+            {
+                cl_uint o_num_kernel_args = 0;
+                knlObj->dispatch->clGetKernelInfo(knlObj, CL_KERNEL_NUM_ARGS, sizeof( cl_uint ), &o_num_kernel_args, NULL );
+                if( num_kernel_args != o_num_kernel_args )
+                {
+                    errCode = CL_INVALID_KERNEL_DEFINITION;
+                    goto FINISH;
+                }
+            }
         }
     }
 
