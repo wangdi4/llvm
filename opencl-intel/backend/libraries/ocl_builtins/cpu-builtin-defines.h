d15fcef711e8d37f676e7af6db10a9286fe691d2
Aboud, Amjad <amjad.aboud@intel.com>
2012-12-20 16:14:40 +0000

    Added address space constant to global const variables defined in built-ins. This is meed to be aligned with OpenCL spec and pass compilation on Apple's clang.

--- opencl-intel/backend/libraries/ocl_builtins/cpu-builtin-defines.h
+++ opencl-intel/backend/libraries/ocl_builtins/cpu-builtin-defines.h
@@ -10,15 +10,15 @@ extern "C" {
 #endif
 
 // integers masks
-const char char_MSB_mask =       0x80;
-const int  int_MSB_mask  = 0x80000000;
-const char LSB_mask      =          1;
+const constant char char_MSB_mask =       0x80;
+const constant int  int_MSB_mask  = 0x80000000;
+const constant char LSB_mask      =          1;
 
-const long long_even_mask = 0x00000000FFFFFFFF;
+const constant long long_even_mask = 0x00000000FFFFFFFF;
 
 // "magic numbers" for popcount parallel algorithm
-const int  magic_num_S[] = {1, 2, 4, 8, 16, 32};
-const long magic_num_B[] = {0x5555555555555555, 0x3333333333333333, 0x0F0F0F0F0F0F0F0F,
+const constant int  magic_num_S[] = {1, 2, 4, 8, 16, 32};
+const constant long magic_num_B[] = {0x5555555555555555, 0x3333333333333333, 0x0F0F0F0F0F0F0F0F,
                             0x00FF00FF00FF00FF, 0x0000FFFF0000FFFF, 0x00000000FFFFFFFF};
 
 #ifdef __cplusplus
