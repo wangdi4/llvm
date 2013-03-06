0935c89294685cf7e184b039a3b67e7e14da86b4
Meraey, Nael <nael.meraey@intel.com>
2013-03-06 11:15:11 +0000

    Miscellaneous CPU built-ins in tbl-gen: Fixed some opened issues in CR.
    CQ:
    ---
    CSSD100015286
    
    Reviewers:
    ----------
    Marina, Asaf, Oleg, Aleksey Bader
    
    ReviewBoard:
    ------------
    http://cvcc-ubu-01.iil.intel.com/cr/r/1833/
    
    Testing:
    --------
    PreCommit:
    http://cvcc-fc14-01.iil.intel.com:8080/job/PCompiler_nmeraey/79/

--- opencl-intel/backend/libraries/ocl_builtins/cpu-builtin-defines.h
+++ opencl-intel/backend/libraries/ocl_builtins/cpu-builtin-defines.h
@@ -42,36 +42,6 @@ const constant uchar16 _shuffle_epi32_amask = (uchar16)(0, 1, 2, 3, 0, 1, 2, 3, 
 const constant uchar16 _shuffle_epi64_smask = (uchar16)(0, 0, 0, 0, 0, 0, 0, 0, 8, 8, 8, 8, 8, 8, 8, 8);
 const constant uchar16 _shuffle_epi64_amask = (uchar16)(0, 1, 2, 3, 4, 5, 6, 7, 0, 1, 2, 3, 4, 5, 6, 7);
 
-
-uchar16 _MM_SHUFFLE_EPI8(uchar16 x, uchar16 mask)
-{
-   return as_uchar16( _mm_shuffle_epi8((__m128i)x, (__m128i)mask) );
-}
-
-ushort8 _MM_SHUFFLE_EPI16(ushort8 x, ushort8 mask)
-{
-    mask = as_ushort8( _MM_SHUFFLE_EPI8(as_uchar16(mask), _shuffle_epi16_smask) );
-    mask = as_ushort8( _mm_slli_epi16((__m128i)mask, 1) );
-    mask = as_ushort8( _mm_adds_epu8((__m128i)mask, (__m128i)_shuffle_epi16_amask) );
-    return as_ushort8( _MM_SHUFFLE_EPI8(as_uchar16(x), as_uchar16(mask)) );
-}
-
-uint4 _MM_SHUFFLE_EPI32(uint4 x, uint4 mask)
-{
-    mask = as_uint4( _MM_SHUFFLE_EPI8(as_uchar16(mask), _shuffle_epi32_smask) );
-    mask = as_uint4( _mm_slli_epi16((__m128i)mask, 2) );
-    mask = as_uint4( _mm_adds_epu8((__m128i)mask, (__m128i)_shuffle_epi32_amask) );
-    return as_uint4( _MM_SHUFFLE_EPI8(as_uchar16(x), as_uchar16(mask)) );
-}
-
-ulong2 _MM_SHUFFLE_EPI64(ulong2 x, ulong2 mask)
-{
-    mask = as_ulong2( _MM_SHUFFLE_EPI8(as_uchar16(mask), _shuffle_epi64_smask) );
-    mask = as_ulong2( _mm_slli_epi16((__m128i)mask, 3) );
-    mask = as_ulong2( _mm_adds_epu8((__m128i)mask, (__m128i)_shuffle_epi64_amask) );
-    return as_ulong2( _MM_SHUFFLE_EPI8(as_uchar16(x), as_uchar16(mask)) );
-}
-
 // conversion sat
 const constant float as_float_min_char    =  -128.0f;
 const constant float as_float_max_char    =   127.0f;
