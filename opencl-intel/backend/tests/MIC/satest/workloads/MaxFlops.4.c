d03b25eef739d1b51fe0e2fda1c6c91b18cf0bb8
Rackover, Zvi <zvi.rackover@intel.com>
2013-05-23 14:22:24 +0000

    Rolling back commit 68479 since it adds files with same name on Windows which breaks the ccheckout

---
+++ opencl-intel/backend/tests/MIC/satest/workloads/MaxFlops.4.c
@@ -0,0 +1,38 @@
+__kernel void Add8(__global float *data, int nIters) {
+  int gid = get_global_id(0), globalSize = get_global_size(0);
+  float s = data[gid];
+  float8 s0 = s + (float8)(0,0.1,0.2,0.3,0.4,0.5,0.6,0.7);
+  for (int j=0 ; j<nIters ; ++j) {
+ s0=10.f-s0;
+ s0=10.f-s0;
+ s0=10.f-s0;
+ s0=10.f-s0;
+ s0=10.f-s0;
+ s0=10.f-s0;
+ s0=10.f-s0;
+ s0=10.f-s0;
+ s0=10.f-s0;
+ s0=10.f-s0;
+ s0=10.f-s0;
+ s0=10.f-s0;
+ s0=10.f-s0;
+ s0=10.f-s0;
+ s0=10.f-s0;
+ s0=10.f-s0;
+ s0=10.f-s0;
+ s0=10.f-s0;
+ s0=10.f-s0;
+ s0=10.f-s0;
+ s0=10.f-s0;
+ s0=10.f-s0;
+ s0=10.f-s0;
+ s0=10.f-s0;
+ s0=10.f-s0;
+ s0=10.f-s0;
+ s0=10.f-s0;
+ s0=10.f-s0;
+ s0=10.f-s0;
+ s0=10.f-s0;
+  }
+   data[gid] = s0.s0+s0.s1+s0.s2+s0.s3+s0.s4+s0.s5+s0.s6+s0.s7;
+}
