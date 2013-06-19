6c922ecb2d2d2a949dd07b4959924813ea8b2252
Rackover, Zvi <zvi.rackover@intel.com>
2013-06-19 05:43:43 +0000

    Replace shell script with Makefile which allows concurrent executions of SATest

--- opencl-intel/backend/tests/MIC/satest/workloads/run_all.sh
+++
@@ -1,7 +0,0 @@
-# setenv LD_LIBRARY_PATH .../trunk/install/RH64/Release/bin/
-# setenv PATH ${LD_LIBRARY_PATH}:${PATH}
-setenv DUMPIR 1
-setenv DUMPASM 1
-setenv OUTDIR ./trunk-new-kernels
-mkdir $OUTDIR
-find . -maxdepth 1 -name '*.xml' -exec satest_wrap.sh {} $OUTDIR \; >& ${OUTDIR}/run.log
--- opencl-intel/backend/tests/MIC/satest/workloads/satest_wrap.sh
+++ opencl-intel/backend/tests/MIC/satest/workloads/satest_wrap.sh
@@ -1,4 +1,8 @@
 echo Command: SATest -BUILD -cpuarch knc -config $1
-SATest -BUILD -cpuarch knc -config $1
+export TMP=`mktemp -d`
+cp *.h ${TMP}/ && cd ${TMP} && SATest -BUILD -cpuarch knc -config $1
 mv SATest.s $2/`basename $1`.s
 mv SATest.ll $2/`basename $1`.ll
+rm *
+cd -
+rmdir ${TMP}
