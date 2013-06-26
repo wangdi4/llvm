43a67e7531a41e5184ac4f8f6242e0885264b8d2
Meraey, Nael <nael.meraey@intel.com>
2013-06-26 08:04:08 +0000

    [CommonRuntime]: Fixed images failures in shared context on Android.
    PreCommit:
    http://cvcc-fc14-01.iil.intel.com:8080/job/PRuntime_nmeraey/35/

--- opencl-intel/common_runtime/export/crt_dispatch_table.h
+++ opencl-intel/common_runtime/export/crt_dispatch_table.h
@@ -43,10 +43,10 @@ namespace CRT_ICD_DISPATCH
         cl_uint *                   /* num_devices */ );
 
 #else   //Linux/Android
-    typedef void *INTELpfn_clCreateFromDX9MediaSurfaceINTEL();
-    typedef void *INTELpfn_clEnqueueAcquireDX9ObjectsINTEL();
-    typedef void *INTELpfn_clEnqueueReleaseDX9ObjectsINTEL();
-    typedef void *INTELpfn_clGetDeviceIDsFromDX9INTEL();
+    typedef void *INTELpfn_clCreateFromDX9MediaSurfaceINTEL;
+    typedef void *INTELpfn_clEnqueueAcquireDX9ObjectsINTEL;
+    typedef void *INTELpfn_clEnqueueReleaseDX9ObjectsINTEL;
+    typedef void *INTELpfn_clGetDeviceIDsFromDX9INTEL;
 #endif
 
     typedef CL_API_ENTRY cl_int (CL_API_CALL *INTELpfn_clGetImageParamsINTEL)(
