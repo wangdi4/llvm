aed88f1edf2d6cbf28c34e58b0544c771064c188
Veselov, Yuri <yuri.veselov@intel.com>
2013-07-31 11:23:19 +0000

    Log message: <LOG_BODY> - fix for CSSD100014964 (SATest will generate a segmentation fault if the kernel name is incorrect in the config file) <CODE_REVIEW> - http://cvcc-ubu-01.iil.intel.com/cr/r/2793/ <PERF_IMPLICATIONS> - None. <TEST> - pre-commit http://cvcc-fc14-01.iil.intel.com:8080/job/PersonalCompilerReport/546/

--- opencl-intel/backend/validations/SATest/OpenCLCPUBackendRunner.cpp
+++ opencl-intel/backend/validations/SATest/OpenCLCPUBackendRunner.cpp
@@ -428,7 +428,19 @@ void OpenCLCPUBackendRunner::ExecuteKernel(IBufferContainerList& input,
     // Get kernel to run
     std::string kernelName = pKernelConfig->GetKernelName();
     const ICLDevBackendKernel_* pKernel = NULL;
-    pProgram->GetKernelByName(kernelName.c_str(), &pKernel);
+
+    cl_dev_err_code errCode = pProgram->GetKernelByName(kernelName.c_str(), &pKernel);
+
+    // currently pProgram->GetKernelByName returns CL_DEV_INVALID_KERNEL_NAME or CL_DEV_SUCCESS only
+    switch(errCode) {
+    case CL_DEV_SUCCESS:
+        break;
+    case CL_DEV_INVALID_KERNEL_NAME:
+        throw Exception::TestRunnerException(std::string("kernel name ") + kernelName + std::string(" was not found in the source code\n"));
+        break;
+    default:
+        throw Exception::TestRunnerException("Unexpected error code from ICLDevBackendProgram_::GetKernelByName method");
+    }
 
     // Get kernel arguments
     int kernelNumArgs = pKernel->GetKernelParamsCount();
