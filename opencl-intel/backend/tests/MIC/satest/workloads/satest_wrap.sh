09e9ddbd919ce62728340874c4c4ad9f553c6613
Rackover, Zvi <zvi.rackover@intel.com>
2013-08-19 14:03:36 +0000

    Added missing header file for Hydro

--- opencl-intel/backend/tests/MIC/satest/workloads/satest_wrap.sh
+++ opencl-intel/backend/tests/MIC/satest/workloads/satest_wrap.sh
@@ -1,6 +1,6 @@
 echo Command: SATest -BUILD -cpuarch knc -config $1
 export TMP=`mktemp -d`
-#cp *.h ${TMP}/
+cp *.h ${TMP}/
 cd ${TMP} && SATest -BUILD -cpuarch knc -config $1
 mv SATest.s $2/`basename $1`.s
 mv SATest.ll $2/`basename $1`.ll
