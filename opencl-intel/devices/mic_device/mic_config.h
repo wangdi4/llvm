5632c650d2c669958fe29ebe6bb1920ab5c8ece8
Kaptsenel, Dmitry <dmitry.kaptsenel@intel.com>
2013-11-25 10:26:11 +0000

    Merge commit 80542 from 2013_xe_r3: Force 2M pages for all buffers above 1K

--- opencl-intel/devices/mic_device/mic_config.h
+++ opencl-intel/devices/mic_device/mic_config.h
@@ -40,31 +40,31 @@ using namespace Intel::OpenCL::Utils;
 
 
 // device setup
-#define    CL_CONFIG_MIC_DEVICE_STOP_AT_LOAD				    "CL_CONFIG_MIC_DEVICE_STOP_AT_LOAD"           // bool
-#define    CL_CONFIG_MIC_DEVICE_USE_AFFINITY				    "CL_CONFIG_MIC_DEVICE_USE_AFFINITY"           // bool
+#define    CL_CONFIG_MIC_DEVICE_STOP_AT_LOAD            "CL_CONFIG_MIC_DEVICE_STOP_AT_LOAD"           // bool
+#define    CL_CONFIG_MIC_DEVICE_USE_AFFINITY            "CL_CONFIG_MIC_DEVICE_USE_AFFINITY"           // bool
 #define    CL_CONFIG_MIC_DEVICE_THREADS_PER_CORE        "CL_CONFIG_MIC_DEVICE_THREADS_PER_CORE"       // unsigned int
-#define    CL_CONFIG_MIC_DEVICE_NUM_CORES					      "CL_CONFIG_MIC_DEVICE_NUM_CORES"	            // unsigned int
-#define    CL_CONFIG_MIC_DEVICE_IGNORE_CORE_0				    "CL_CONFIG_MIC_DEVICE_IGNORE_CORE_0"          // bool
-#define    CL_CONFIG_MIC_DEVICE_IGNORE_LAST_CORE		    "CL_CONFIG_MIC_DEVICE_IGNORE_LAST_CORE"       // bool
-#define    CL_CONFIG_MIC_DEVICE_2MB_BUF_MINSIZE_KB	    "CL_CONFIG_MIC_DEVICE_2MB_BUF_MINSIZE_KB"     // size_t in KB
-#define    CL_CONFIG_MIC_DEVICE_TBB_GRAIN_SIZE				  "CL_CONFIG_MIC_DEVICE_TBB_GRAIN_SIZE"         // unsigned int
-#define    CL_CONFIG_MIC_DEVICE_TBB_SCHEDULER				    "CL_CONFIG_MIC_DEVICE_TBB_SCHEDULER"          // string
+#define    CL_CONFIG_MIC_DEVICE_NUM_CORES               "CL_CONFIG_MIC_DEVICE_NUM_CORES"              // unsigned int
+#define    CL_CONFIG_MIC_DEVICE_IGNORE_CORE_0           "CL_CONFIG_MIC_DEVICE_IGNORE_CORE_0"          // bool
+#define    CL_CONFIG_MIC_DEVICE_IGNORE_LAST_CORE        "CL_CONFIG_MIC_DEVICE_IGNORE_LAST_CORE"       // bool
+#define    CL_CONFIG_MIC_DEVICE_2MB_BUF_MINSIZE_KB      "CL_CONFIG_MIC_DEVICE_2MB_BUF_MINSIZE_KB"     // size_t in KB
+#define    CL_CONFIG_MIC_DEVICE_TBB_GRAIN_SIZE          "CL_CONFIG_MIC_DEVICE_TBB_GRAIN_SIZE"         // unsigned int
+#define    CL_CONFIG_MIC_DEVICE_TBB_SCHEDULER           "CL_CONFIG_MIC_DEVICE_TBB_SCHEDULER"          // string
 #define    CL_CONFIG_MIC_DEVICE_TBB_BLOCK_OPTIMIZATION  "CL_CONFIG_MIC_DEVICE_TBB_BLOCK_OPTIMIZATION" // string
-#define    CL_CONFIG_MIC_DEVICE_TBB_TRAP_WORKERS	      "CL_CONFIG_MIC_DEVICE_TBB_TRAP_WORKERS"       // bool
-#define    CL_CONFIG_MIC_DEVICE_LAZY_TRANSFER				    "CL_CONFIG_MIC_DEVICE_LAZY_TRANSFER"          // unsigned int
-#define    CL_CONFIG_MIC_DEVICE_SAFE_KERNEL_EXECUTION		"CL_CONFIG_MIC_DEVICE_SAFE_KERNEL_EXECUTION"  // bool
+#define    CL_CONFIG_MIC_DEVICE_TBB_TRAP_WORKERS        "CL_CONFIG_MIC_DEVICE_TBB_TRAP_WORKERS"       // bool
+#define    CL_CONFIG_MIC_DEVICE_LAZY_TRANSFER           "CL_CONFIG_MIC_DEVICE_LAZY_TRANSFER"          // unsigned int
+#define    CL_CONFIG_MIC_DEVICE_SAFE_KERNEL_EXECUTION   "CL_CONFIG_MIC_DEVICE_SAFE_KERNEL_EXECUTION"  // bool
 #ifdef __MIC_DA_OMP__
-	#define    CL_CONFIG_MIC_DEVICE_OMP_SCHEDULE  "CL_CONFIG_MIC_DEVICE_OMP_SCHED" // string
-	#define    CL_CONFIG_MIC_DEVICE_OMP_KMP_AFFINITY  "CL_CONFIG_MIC_DEVICE_OMP_KMP_AFFINITY" // string
-	#define    CL_CONFIG_MIC_DEVICE_OMP_KMP_BLOCKTIME  "CL_CONFIG_MIC_DEVICE_OMP_KMP_BLOCKTIME" // string
+    #define    CL_CONFIG_MIC_DEVICE_OMP_SCHEDULE        "CL_CONFIG_MIC_DEVICE_OMP_SCHED"              // string
+    #define    CL_CONFIG_MIC_DEVICE_OMP_KMP_AFFINITY    "CL_CONFIG_MIC_DEVICE_OMP_KMP_AFFINITY"       // string
+    #define    CL_CONFIG_MIC_DEVICE_OMP_KMP_BLOCKTIME   "CL_CONFIG_MIC_DEVICE_OMP_KMP_BLOCKTIME"      // string
 #endif
-#define    CL_CONFIG_MIC_DEVICE_PARALLEL_FILL_BUFFER_FROM_SIZE_BYTE    "CL_CONFIG_MIC_DEVICE_PARALLEL_FILL_BUFFER_FROM_SIZE_BYTE"  // int
-#define    CL_CONFIG_MIC_DEVICE_PARALLEL_FILL_MAX_TASK_PER_WORKER    "CL_CONFIG_MIC_DEVICE_PARALLEL_FILL_MAX_TASK_PER_WORKER"  // int
-#define    CL_CONFIG_MIC_DEVICE_PARALLEL_FILL_MAX_WORKERS    "CL_CONFIG_MIC_DEVICE_PARALLEL_FILL_MAX_WORKERS"  // int
+#define    CL_CONFIG_MIC_DEVICE_PARALLEL_FILL_BUFFER_FROM_SIZE_BYTE     "CL_CONFIG_MIC_DEVICE_PARALLEL_FILL_BUFFER_FROM_SIZE_BYTE"  // int
+#define    CL_CONFIG_MIC_DEVICE_PARALLEL_FILL_MAX_TASK_PER_WORKER       "CL_CONFIG_MIC_DEVICE_PARALLEL_FILL_MAX_TASK_PER_WORKER"    // int
+#define    CL_CONFIG_MIC_DEVICE_PARALLEL_FILL_MAX_WORKERS               "CL_CONFIG_MIC_DEVICE_PARALLEL_FILL_MAX_WORKERS"            // int
 
-#define    CL_CONFIG_MIC_DEVICE_PRINT_CONFIG             "CL_CONFIG_MIC_DEVICE_PRINT_CONFIG"          // bool
+#define    CL_CONFIG_MIC_DEVICE_PRINT_CONFIG                            "CL_CONFIG_MIC_DEVICE_PRINT_CONFIG"                         // bool
 
-#define    OFFLOAD_DEVICES									             "OFFLOAD_DEVICES"							              // string
+#define    OFFLOAD_DEVICES                                              "OFFLOAD_DEVICES"                                           // string
 
 namespace Intel { namespace OpenCL { namespace MICDevice {
 class MICDeviceConfig
@@ -77,40 +77,40 @@ public:
     cl_err_code    Initialize(string file_name);
     void           Release();
 
-		bool           Device_PrintConfig() const { return m_pConfigFile->Read<bool>(CL_CONFIG_MIC_DEVICE_PRINT_CONFIG, false); }
+    bool           Device_PrintConfig() const { return m_pConfigFile->Read<bool>(CL_CONFIG_MIC_DEVICE_PRINT_CONFIG, false); }
 
 
     bool           UseITT() const { return m_pConfigFile->Read<bool>(CL_CONFIG_USE_ITT_API, false); }
     bool           UseVectorizer() const { return m_pConfigFile->Read<bool>(CL_CONFIG_USE_VECTORIZER, true ); }
     bool           UseVTune()      const { return m_pConfigFile->Read<bool>(CL_CONFIG_USE_VTUNE,      false); }
 
-		// Device performance setup
-		bool           Device_StopAtLoad()      const { return m_pConfigFile->Read<bool>(CL_CONFIG_MIC_DEVICE_STOP_AT_LOAD, false); }
-		bool           Device_UseAffinity()     const { return m_pConfigFile->Read<bool>(CL_CONFIG_MIC_DEVICE_USE_AFFINITY, true); }
-		unsigned int   Device_ThreadsPerCore()  const { return m_pConfigFile->Read<unsigned int>(CL_CONFIG_MIC_DEVICE_THREADS_PER_CORE, 0); }
-		unsigned int   Device_NumCores()        const { return m_pConfigFile->Read<unsigned int>(CL_CONFIG_MIC_DEVICE_NUM_CORES, 0); }
-		bool           Device_IgnoreCore0()     const { return m_pConfigFile->Read<bool>(CL_CONFIG_MIC_DEVICE_IGNORE_CORE_0, false); }
-		bool           Device_IgnoreLastCore()  const { return m_pConfigFile->Read<bool>(CL_CONFIG_MIC_DEVICE_IGNORE_LAST_CORE, true); }
-		size_t         Device_2MB_BufferMinSizeInKB() const { return m_pConfigFile->Read<size_t>(CL_CONFIG_MIC_DEVICE_2MB_BUF_MINSIZE_KB, 512); }
-		unsigned int   Device_TbbGrainSize()    const { return m_pConfigFile->Read<unsigned int>(CL_CONFIG_MIC_DEVICE_TBB_GRAIN_SIZE, 1); }
-		string         Device_TbbScheduler()    const { return m_pConfigFile->Read<string>(CL_CONFIG_MIC_DEVICE_TBB_SCHEDULER, "auto"); }
-		string         Device_TbbBlockOptimization() const { return m_pConfigFile->Read<string>(CL_CONFIG_MIC_DEVICE_TBB_BLOCK_OPTIMIZATION, "rows"); }
-		bool           Device_TbbTrapWorkers()  const { return m_pConfigFile->Read<bool>(CL_CONFIG_MIC_DEVICE_TBB_TRAP_WORKERS, false); }
-		bool           Device_LazyTransfer()    const { return m_pConfigFile->Read<bool>(CL_CONFIG_MIC_DEVICE_LAZY_TRANSFER, false); }
-		// Device safe mode setup
-		bool           Device_safeKernelExecution() const { return m_pConfigFile->Read<bool>(CL_CONFIG_MIC_DEVICE_SAFE_KERNEL_EXECUTION, true); }
+    // Device performance setup
+    bool           Device_StopAtLoad()      const { return m_pConfigFile->Read<bool>(CL_CONFIG_MIC_DEVICE_STOP_AT_LOAD, false); }
+    bool           Device_UseAffinity()     const { return m_pConfigFile->Read<bool>(CL_CONFIG_MIC_DEVICE_USE_AFFINITY, true); }
+    unsigned int   Device_ThreadsPerCore()  const { return m_pConfigFile->Read<unsigned int>(CL_CONFIG_MIC_DEVICE_THREADS_PER_CORE, 0); }
+    unsigned int   Device_NumCores()        const { return m_pConfigFile->Read<unsigned int>(CL_CONFIG_MIC_DEVICE_NUM_CORES, 0); }
+    bool           Device_IgnoreCore0()     const { return m_pConfigFile->Read<bool>(CL_CONFIG_MIC_DEVICE_IGNORE_CORE_0, false); }
+    bool           Device_IgnoreLastCore()  const { return m_pConfigFile->Read<bool>(CL_CONFIG_MIC_DEVICE_IGNORE_LAST_CORE, true); }
+    size_t         Device_2MB_BufferMinSizeInKB() const { return m_pConfigFile->Read<size_t>(CL_CONFIG_MIC_DEVICE_2MB_BUF_MINSIZE_KB, 1); }
+    unsigned int   Device_TbbGrainSize()    const { return m_pConfigFile->Read<unsigned int>(CL_CONFIG_MIC_DEVICE_TBB_GRAIN_SIZE, 1); }
+    string         Device_TbbScheduler()    const { return m_pConfigFile->Read<string>(CL_CONFIG_MIC_DEVICE_TBB_SCHEDULER, "auto"); }
+    string         Device_TbbBlockOptimization() const { return m_pConfigFile->Read<string>(CL_CONFIG_MIC_DEVICE_TBB_BLOCK_OPTIMIZATION, "rows"); }
+    bool           Device_TbbTrapWorkers()  const { return m_pConfigFile->Read<bool>(CL_CONFIG_MIC_DEVICE_TBB_TRAP_WORKERS, false); }
+    bool           Device_LazyTransfer()    const { return m_pConfigFile->Read<bool>(CL_CONFIG_MIC_DEVICE_LAZY_TRANSFER, false); }
+    // Device safe mode setup
+    bool           Device_safeKernelExecution() const { return m_pConfigFile->Read<bool>(CL_CONFIG_MIC_DEVICE_SAFE_KERNEL_EXECUTION, true); }
 
 #ifdef __MIC_DA_OMP__
-		string         Device_OmpSchedule() const { return m_pConfigFile->Read<string>(CL_CONFIG_MIC_DEVICE_OMP_SCHEDULE, "static"); }
-		string         Device_OmpKmpAffinity() const { return m_pConfigFile->Read<string>(CL_CONFIG_MIC_DEVICE_OMP_KMP_AFFINITY, "granularity=fine,balanced"); }
-		string         Device_OmpKmpBlockTime() const { return m_pConfigFile->Read<string>(CL_CONFIG_MIC_DEVICE_OMP_KMP_BLOCKTIME, "infinite"); }
+    string         Device_OmpSchedule() const { return m_pConfigFile->Read<string>(CL_CONFIG_MIC_DEVICE_OMP_SCHEDULE, "static"); }
+    string         Device_OmpKmpAffinity() const { return m_pConfigFile->Read<string>(CL_CONFIG_MIC_DEVICE_OMP_KMP_AFFINITY, "granularity=fine,balanced"); }
+    string         Device_OmpKmpBlockTime() const { return m_pConfigFile->Read<string>(CL_CONFIG_MIC_DEVICE_OMP_KMP_BLOCKTIME, "infinite"); }
 #endif
 
-		int   Device_ParallelFillBufferFromSize() const { return m_pConfigFile->Read<unsigned int>(CL_CONFIG_MIC_DEVICE_PARALLEL_FILL_BUFFER_FROM_SIZE_BYTE, 32768); }
-		int   Device_ParallelFillMaxTaskPerWorker() const { return m_pConfigFile->Read<unsigned int>(CL_CONFIG_MIC_DEVICE_PARALLEL_FILL_MAX_TASK_PER_WORKER, -1); }
-		int   Device_ParallelFillMaxWorkers() const { return m_pConfigFile->Read<unsigned int>(CL_CONFIG_MIC_DEVICE_PARALLEL_FILL_MAX_WORKERS, -1); }
+    int            Device_ParallelFillBufferFromSize() const { return m_pConfigFile->Read<unsigned int>(CL_CONFIG_MIC_DEVICE_PARALLEL_FILL_BUFFER_FROM_SIZE_BYTE, 32768); }
+    int            Device_ParallelFillMaxTaskPerWorker() const { return m_pConfigFile->Read<unsigned int>(CL_CONFIG_MIC_DEVICE_PARALLEL_FILL_MAX_TASK_PER_WORKER, -1); }
+    int            Device_ParallelFillMaxWorkers() const { return m_pConfigFile->Read<unsigned int>(CL_CONFIG_MIC_DEVICE_PARALLEL_FILL_MAX_WORKERS, -1); }
 
-		string		     Device_offloadDevices() const { return m_pConfigFile->Read<string>(OFFLOAD_DEVICES, ""); }
+    string         Device_offloadDevices() const { return m_pConfigFile->Read<string>(OFFLOAD_DEVICES, ""); }
 
 private:
 
@@ -155,16 +155,16 @@ private:
         std::cout << std::endl;
 
 #ifdef __MIC_DA_OMP__
-		MICDeviceConfigPrintKey( CL_CONFIG_MIC_DEVICE_OMP_SCHEDULE, Device_OmpSchedule, "Define the OpenMP scheduler to use, the default is \"static\"");
-		MICDeviceConfigPrintKey( CL_CONFIG_MIC_DEVICE_OMP_KMP_AFFINITY, Device_OmpKmpAffinity, "Define the OpenMP affinity type, the default is \"granularity=fine,balanced\"");
-		MICDeviceConfigPrintKey( CL_CONFIG_MIC_DEVICE_OMP_KMP_BLOCKTIME, Device_OmpKmpBlockTime, "Define the OpenMP blocktime of the threads, the default is \"infinite\"");
-		
-		std::cout << std::endl;
+        MICDeviceConfigPrintKey( CL_CONFIG_MIC_DEVICE_OMP_SCHEDULE, Device_OmpSchedule, "Define the OpenMP scheduler to use, the default is \"static\"");
+        MICDeviceConfigPrintKey( CL_CONFIG_MIC_DEVICE_OMP_KMP_AFFINITY, Device_OmpKmpAffinity, "Define the OpenMP affinity type, the default is \"granularity=fine,balanced\"");
+        MICDeviceConfigPrintKey( CL_CONFIG_MIC_DEVICE_OMP_KMP_BLOCKTIME, Device_OmpKmpBlockTime, "Define the OpenMP blocktime of the threads, the default is \"infinite\"");
+        
+        std::cout << std::endl;
 #endif
 
-		MICDeviceConfigPrintKey( CL_CONFIG_MIC_DEVICE_PARALLEL_FILL_BUFFER_FROM_SIZE_BYTE, Device_ParallelFillBufferFromSize, "Minimum size of buffer in kilobytes to use parallel fill for clEnqueueFillBuffer"  );
-		MICDeviceConfigPrintKey( CL_CONFIG_MIC_DEVICE_PARALLEL_FILL_MAX_TASK_PER_WORKER, Device_ParallelFillMaxTaskPerWorker, "Max tasks per worker when calling to clEnqueueFillBuffer"  );
-		MICDeviceConfigPrintKey( CL_CONFIG_MIC_DEVICE_PARALLEL_FILL_MAX_WORKERS, Device_ParallelFillMaxWorkers, "Max workers to use when calling to clEnqueueFillBuffer"  );
+        MICDeviceConfigPrintKey( CL_CONFIG_MIC_DEVICE_PARALLEL_FILL_BUFFER_FROM_SIZE_BYTE, Device_ParallelFillBufferFromSize, "Minimum size of buffer in kilobytes to use parallel fill for clEnqueueFillBuffer"  );
+        MICDeviceConfigPrintKey( CL_CONFIG_MIC_DEVICE_PARALLEL_FILL_MAX_TASK_PER_WORKER, Device_ParallelFillMaxTaskPerWorker, "Max tasks per worker when calling to clEnqueueFillBuffer"  );
+        MICDeviceConfigPrintKey( CL_CONFIG_MIC_DEVICE_PARALLEL_FILL_MAX_WORKERS, Device_ParallelFillMaxWorkers, "Max workers to use when calling to clEnqueueFillBuffer"  );
 
         std::cout << "--------------------------------------------------------" << std::endl << std::endl;
     }
