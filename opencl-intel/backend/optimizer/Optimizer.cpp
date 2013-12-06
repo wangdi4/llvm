8d6f38b7f8ee89fb35440b0902da3609a2c7cf87
Bader, Aleksey A <aleksey.a.bader@intel.com>
2013-12-06 11:08:42 +0000

    Merge rev. 80200 from R3 branch.
    "
    Message:
    Added smartGVN pass to configure LLVM's global value numbering optimization (GVN-PRE) so as not to move loads out of loops that already suffer from high register pressure.
    
    The issue with GEMM performance was exposed by optimization at rev. 79101.
    Problem description:
    With 79101 we allocate dynamic local memory (__local arguments to kernel) on the JIT's stack. This enables alias analysis and eventually GVN-PRE to hoist some of load instructions from the inner loop of GEMM workload.
    Unfortunately, LLVM generates a lot of spills in that case, which kill the performance of GEMM.
    
    Proposed solution:
    GVN pass has a parameter that disables this optimization for load instructions.
    We add new pass that analyses the function's code and looks for the cases similar to GEMM. Once it finds it - we disable GVN-PRE for loads, otherwise run regular GVN pass.
    "
    
    CQ: CSSD100017465
    Pre-Commit: http://cvcc-fc14-01.iil.intel.com:8080/job/PersonalCompilerReport/1396
    Performance impact: SHOC/GEMM 2x improvement is expected.

--- opencl-intel/backend/optimizer/Optimizer.cpp
+++ opencl-intel/backend/optimizer/Optimizer.cpp
@@ -79,6 +79,7 @@ llvm::ModulePass *createPrintIRPass(int option, int optionLocation, std::string 
 llvm::ModulePass* createDebugInfoPass();
 llvm::ModulePass *createReduceAlignmentPass();
 llvm::ModulePass* createProfilingInfoPass();
+llvm::Pass *createSmartGVNPass();
 #endif
 llvm::ModulePass *createResolveWICallPass();
 llvm::ModulePass *createDetectFuncPtrCalls();
@@ -501,8 +502,8 @@ static void populatePassesPostFailCheck(llvm::PassManagerBase &PM,
     PM.add(llvm::createCFGSimplificationPass());    // Merge & remove BBs
     PM.add(llvm::createInstructionCombiningPass()); // Cleanup for scalarrepl.
 #ifdef __APPLE__
-    //Due to none default ABI, some built-ins are creating an allaca in middle of function.
-    //Need to run scalar aggregation to get red of these alloca (after built-in import).
+    //Due to none default ABI, some built-ins are creating an alloca in middle of function.
+    //Need to run scalar aggregation to get rid of these alloca (after built-in import).
     //mem2reg pass is not enough! as it only handles alloca in first basic block.
     PM.add(llvm::createScalarReplAggregatesPass());
 #else
@@ -522,7 +523,7 @@ static void populatePassesPostFailCheck(llvm::PassManagerBase &PM,
     PM.add(llvm::createInstructionCombiningPass());       // Instruction combining
     PM.add(llvm::createDeadStoreEliminationPass());       // Eliminated dead stores
     PM.add(llvm::createEarlyCSEPass());
-    PM.add(llvm::createGVNPass());
+    PM.add(createSmartGVNPass());
 
 #ifdef _DEBUG
     PM.add(llvm::createVerifierPass());
