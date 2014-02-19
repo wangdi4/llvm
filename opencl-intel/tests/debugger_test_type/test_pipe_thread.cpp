5e5a056b453a39ef7db9e24717c449d193f6ae80
Ofir Cohen <ofir.o.cohen@intel.com>
2014-02-19 14:56:54 +0200

    master: API Debugger - added support for named pipe
    Changes
    =======
    - ApiDebugger:
    (o) Added support for named pipe while maintaining backward compatibility with old co-rt
        (passing enabled mode & port number via registry)
    (o) Refactoring & cleanup
    
    - ToolsUtilsCSharp: Added common tools utils for C#
    - CommonRTHelper: helps API Debugger to determine if co-rt supports named-pipe
    - OCLDebugEngine: now using the DebuggerPipeWriter class, and disposing the named pipe server
      at the end of the debug session.
    
    - DebuggerPipeWriter: changed implementation to use ANSI WinApi calls
    
    Reviewer
    ========
    Oded Perez
    
    Pre-commits
    ===========
    Build, Install
    http://ocl-jenkins:8080/view/users/view/ocohen11/job/PBuild_ocohen11/19/
    http://ocl-jenkins:8080/view/users/view/ocohen11/job/PToolsInstall_ocohen11/16/

--- opencl-intel/tests/debugger_test_type/test_pipe_thread.cpp
+++ opencl-intel/tests/debugger_test_type/test_pipe_thread.cpp
@@ -50,17 +50,14 @@ RETURN_TYPE_ENTRY_POINT NamedPipeThread::Run()
 
     // Create the data string
     string portString(portNumber);
-    wstring wPortString(portString.begin(), portString.end());
-    wstring dataString = L"1;" + wPortString;
+    string dataString = "1;" + portString;
 
     // This call blocks until a client process reads all the data
-    const wchar_t *data = dataString.c_str();
-
     DWORD numBytesWritten = 0;
     result = WriteFile(
         pipe,                           // handle to our outbound pipe
-        data,                           // data to send
-        wcslen(data) * sizeof(wchar_t), // length of data to send (bytes)
+        dataString.c_str(),             // data to send
+        dataString.length(),            // length of data to send (bytes)
         &numBytesWritten,               // will store actual amount of data sent
         NULL                            // not using overlapped IO
     );
