e2ef1d60930d33694b604677e2653153ca0e1dab
Alon Mishne <alon.mishne@intel.com>
2014-04-06 17:35:27 +0300

    Doubling test_incorrect_command_flow timeout durations
    This is a continuation of the previous commit:
    8762892872295c3564d6aa8d2822e9054530d146
    in an attempt and stabilize this test.
    
    Precommit:
    http://compute-ci-jenkins.iil.intel.com:8080/job/PCompilerReport/355/
    Ticket: CSSD100018524
    Review Board: http://cvcc-ubu-01.iil.intel.com/cr/r/4329/

--- opencl-intel/tests/debugger_test_type/testcases/test_incorrect_command_flow.py
+++ opencl-intel/tests/debugger_test_type/testcases/test_incorrect_command_flow.py
@@ -12,10 +12,10 @@ class TestIncorrectCommandFlow(DebuggerTestCase):
         self.client.execute_debuggee(
             hostprog_name='ndrange_inout',
             cl_name=self.CLNAME)
-        self.client.connect_to_server(timeout=20)
+        self.client.connect_to_server(timeout=40)
         bp = (self.CLNAME, self.AFTER_LOOP_ROW)
         try:
-            self.client.debug_run([bp], timeout=15)
+            self.client.debug_run([bp], timeout=30)
         except Exception as e:
             if self.use_gdb:
                 # GDB error
