9cbe766e5dc18d1ebae8afcf4864d484aab61602
Adir Deri <adir.deri@intel.com>
2014-07-06 13:24:56 +0300

    Fix bug in notification port when 32k or more events are sending to COIEventWait API. it cause crash or deadlock in OpenMM testOpenCLRpmd test.

--- opencl-intel/devices/mic_device/notification_port.h
+++ opencl-intel/devices/mic_device/notification_port.h
@@ -45,106 +45,108 @@ class NotificationPort : private OclThread, public ReferenceCountedObject
 {
 public:
 
-	PREPARE_SHARED_PTR(NotificationPort)
+    PREPARE_SHARED_PTR(NotificationPort)
 
-	// CallBack interface
-	class CallBack
-	{
-	public:
-		virtual void fireCallBack(void* arg) = 0;
-	};
+    // CallBack interface
+    class CallBack
+    {
+    public:
+        virtual void fireCallBack(void* arg) = 0;
+    };
 
-	enum ERROR_CODE{
-		SUCCESS=0,
-		ALREADY_INITIALIZE_FAILURE=1,
-		MEM_OBJECT_ALLOCATION_FAILURE=2,
-		CREATE_BARRIER_FAILURE=3,
-		CREATE_WORKER_THREAD_FAILURE=4,
-		NOT_INITIASLIZE_FAILURE=5
-	};
+    enum ERROR_CODE{
+        SUCCESS=0,
+        ALREADY_INITIALIZE_FAILURE=1,
+        MEM_OBJECT_ALLOCATION_FAILURE=2,
+        CREATE_BARRIER_FAILURE=3,
+        CREATE_WORKER_THREAD_FAILURE=4,
+        NOT_INITIASLIZE_FAILURE=5
+    };
 
-	virtual ~NotificationPort();
+    virtual ~NotificationPort();
 	
-	/* Factory for notification port.
-	   Create new NotificationPort object and call to initialize.
-	   maxBarriers - initial size of allocate barriers for the notification port.
-	   In case of success - the notification port initialization completes and return pointer to NotificationPort object.
-	   In case of failure - return NULL.
-	   Do NOT delete this object it will delete itself when the worker thread will finish its work. */
-	static SharedPtr<NotificationPort> notificationPortFactory(uint16_t maxBarriers, const ocl_gpa_data* pGPAData);
+    /* Factory for notification port.
+       Create new NotificationPort object and call to initialize.
+       maxBarriers - initial size of allocate barriers for the notification port.
+       In case of success - the notification port initialization completes and return pointer to NotificationPort object.
+       In case of failure - return NULL.
+       Do NOT delete this object it will delete itself when the worker thread will finish its work. */
+    static SharedPtr<NotificationPort> notificationPortFactory(uint16_t maxBarriers, const ocl_gpa_data* pGPAData);
 
-	ERROR_CODE addBarrier(const COIEVENT& barrier, NotificationPort::CallBack* callBack, void* arg);
+    ERROR_CODE addBarrier(const COIEVENT& barrier, NotificationPort::CallBack* callBack, void* arg);
 
-	ERROR_CODE release();
+    ERROR_CODE release();
 
     static void waitForAllNotificationPortThreads();
 
 private:
 
-	enum AVAILABLE_OPERATIONS{
-		RELEASE=0,
-		ADD,
-		AVAILABLE_OPERATIONS_LEN
-	};
+    enum AVAILABLE_OPERATIONS{
+        RELEASE=0,
+        ADD,
+        AVAILABLE_OPERATIONS_LEN
+    };
 	
-	enum WORKER_STATE {
-	    CREATED=0,
-		BEGINNING,
-		RUNNING,
-		FINISHED
-	};
+    enum WORKER_STATE {
+        CREATED=0,
+        BEGINNING,
+        RUNNING,
+        FINISHED
+    };
 
-	struct notificationPackage
-	{
-		NotificationPort::CallBack* callBack;
-		void* arg;
-		uint64_t age;
+    struct notificationPackage
+    {
+        NotificationPort::CallBack* callBack;
+        void* arg;
+        uint64_t age;
 
-		static bool compare(const notificationPackage& first, const notificationPackage& second)
-		{
-			return (first.age < second.age);
-		}
-	};
+        static bool compare(const notificationPackage& first, const notificationPackage& second)
+        {
+            return (first.age < second.age);
+        }
+    };
 
-	typedef pair<COIEVENT, notificationPackage> EventNotificationPackagePair;
+    typedef pair<COIEVENT, notificationPackage> EventNotificationPackagePair;
 
-	bool m_operationMask[AVAILABLE_OPERATIONS_LEN];
+    bool m_operationMask[AVAILABLE_OPERATIONS_LEN];
 
-	COIEVENT* m_barriers;
-	notificationPackage* m_notificationsPackages;
+    COIEVENT* m_barriers;
+    notificationPackage* m_notificationsPackages;
 
-	vector<EventNotificationPackagePair> m_pendingNotificationArr;
+    vector<EventNotificationPackagePair> m_pendingNotificationArr;
 
-	OclMutex m_mutex;
+    OclMutex m_mutex;
 
-	OclCondition m_clientCond;
+    OclCondition m_clientCond;
 
-	uint16_t m_maxBarriers;
-	uint16_t m_waitingSize;
+    uint16_t m_maxBarriers;
+    uint16_t m_waitingSize;
 
-	// Need it in order to enforce right order of callbacks
-	uint64_t m_lastCallBackAge;
+    // Need it in order to enforce right order of callbacks
+    uint64_t m_lastCallBackAge;
 	
-	volatile WORKER_STATE m_workerState;
+    volatile WORKER_STATE m_workerState;
 
-	const ocl_gpa_data* m_pGPAData;
+    const ocl_gpa_data* m_pGPAData;
 
-	RETURN_TYPE_ENTRY_POINT Run();
+    RETURN_TYPE_ENTRY_POINT Run();
 
     /* Private constructor in order to avoid construction by the client. */
-	NotificationPort(const ocl_gpa_data* pGPAData);
+    NotificationPort(const ocl_gpa_data* pGPAData);
 
-	/* Init the data structures.
-	Initialize and start the responsible thread.
-	Call this function only once after constructor.
-	Do not call addBarrier() or quit() before init() function completed. */
-	int initialize(uint16_t maxBarriers);
+    /* Init the data structures.
+    Initialize and start the responsible thread.
+    Call this function only once after constructor.
+    Do not call addBarrier() or quit() before init() function completed. */
+    int initialize(uint16_t maxBarriers);
 
-	void releaseResources();
+    void releaseResources();
 
-	void getFiredCallBacks(unsigned int numSignaled, unsigned int* signaledIndices, notificationPackage* callBacksRet, bool* workerThreadSignaled);
+    void getFiredCallBacks(unsigned int numSignaled, unsigned int* signaledIndices, notificationPackage* callBacksRet, bool* workerThreadSignaled);
 
-	void resizeBuffers(notificationPackage** fireCallBacksArr, unsigned int** firedIndicesArr, size_t minimumResize);
+    void resizeBuffers(notificationPackage** fireCallBacksArr, unsigned int** firedIndicesArr, size_t minimumResize);
+
+    void addPendingEvents();
 
     class StaticInitializer;
     static StaticInitializer init_statics;
