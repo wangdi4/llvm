fc9cfba2c1538b5ff1f0bf4db97fb59504748c06
abataev <abataev>
2015-07-22 11:03:16 +0000

    Fix for CQ#372133: overloaded operator's parameter Old/split clang commit ID: 0ddf6d3cb034cbcda4d2bfe1d60ba4315fee53d3

--- clang/lib/Sema/SemaDeclCXX.cpp
+++ clang/lib/Sema/SemaDeclCXX.cpp
@@ -11675,6 +11675,12 @@ bool Sema::CheckOverloadedOperatorDeclaration(FunctionDecl *FnDecl) {
       }
     }
 
+#ifdef INTEL_CUSTOMIZATION
+    // Fix for CQ#372133: overloaded operator's parameter.
+    if (!ClassOrEnumParam && getLangOpts().IntelCompat &&
+        FnDecl->isTemplateInstantiation())
+      return true;
+#endif // INTEL_CUSTOMIZATION
     if (!ClassOrEnumParam)
       return Diag(FnDecl->getLocation(),
                   diag::err_operator_overload_needs_class_or_enum)
