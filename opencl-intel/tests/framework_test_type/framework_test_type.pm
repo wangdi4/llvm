a131ba1009f7c86bc7b2f885856beeaa1c762b24
vromanov <vlad.romanov@intel.com>
2015-09-25 16:23:34 +0300

    Run CL2.1 related tests from framework_test_type with env var ForceOCLCPUVersion=2.1

--- opencl-intel/tests/framework_test_type/framework_test_type.pm
+++ opencl-intel/tests/framework_test_type/framework_test_type.pm
@@ -1,4 +1,7 @@
 package framework_test_type;
 use base 'Test::Unit::XCIGTestCase';
-use Test::Unit::XCIGTestCase include => [ 'framework_test_type' ];
+use Test::Unit::XCIGTestCase include => [ 'framework_test_type' ], exclude => ['framework_test_type -*CL21*'];
+$ENV{"ForceOCLCPUVersion"}="2.1";
+use Test::Unit::XCIGTestCase include => [ 'framework_test_type *CL21*'];
+$ENV{"ForceOCLCPUVersion"}="";
 1;
