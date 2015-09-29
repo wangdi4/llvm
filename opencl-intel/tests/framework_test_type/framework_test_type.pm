2b7601929660610af3406c2f2a30b7b5de58c11f
vromanov <vlad.romanov@intel.com>
2015-09-29 09:56:10 +0300

    Revert "Run CL2.1 related tests from framework_test_type with env var ForceOCLCPUVersion=2.1"
    This reverts commit a131ba1009f7c86bc7b2f885856beeaa1c762b24.

--- opencl-intel/tests/framework_test_type/framework_test_type.pm
+++ opencl-intel/tests/framework_test_type/framework_test_type.pm
@@ -1,7 +1,4 @@
 package framework_test_type;
 use base 'Test::Unit::XCIGTestCase';
-use Test::Unit::XCIGTestCase include => [ 'framework_test_type' ], exclude => ['framework_test_type -*CL21*'];
-$ENV{"ForceOCLCPUVersion"}="2.1";
-use Test::Unit::XCIGTestCase include => [ 'framework_test_type *CL21*'];
-$ENV{"ForceOCLCPUVersion"}="";
+use Test::Unit::XCIGTestCase include => [ 'framework_test_type' ];
 1;
