e3e3a0779a7ca53abe7985cd583f04a7372c8a92
Chawla, Pankaj <pankaj.chawla@intel.com>
2015-10-13 17:48:45 +0000

    Summary: bug fix for DDRef's getDestType() Reviewed by: Perez, Ruben M; Sharma, Kamal G Rurl: https://ir-codecollab.intel.com/ui#review:id=29604 Old/split llvm commit ID: 3e6a7b5357bda40dfcb4f17c56ed12446cab478b

--- llvm/lib/IR/Intel_LoopIR/DDRef.cpp
+++ llvm/lib/IR/Intel_LoopIR/DDRef.cpp
@@ -100,9 +100,25 @@ Type *DDRef::getTypeImpl(bool IsSrc) const {
         }
       }
 
-      // The highest dimension can come from "*" instead of "[]".
-      // For example- GEP i32* A, 0 can be mapped as A[0].
-      assert((I == NumDim) && "Malformed DDRef!");
+      // 'I' can be less than NumDim for destination type for cases like this-
+      // %arrayidx = getelementptr [10 x float], [10 x float]* %p, i64 0, i64 %k
+      // %190 = bitcast float* %arrayidx to i32*
+      // store i32 %189, i32* %190
+      //
+      // The DDRef looks like this in HIR-
+      // *(i32*)(%ex1)[0][i1]
+      //
+      // The base canon expr is stored like this-
+      // bitcast.[1001 x float]*.i32*(%ex1)
+      // The represented cast is imprecise because the actual casting occurs
+      // from float* to i32*. The stored information is enough to generate the
+      // correct code, though. This setup needs to be rethought if the
+      // transformations want to access three different types involved here-
+      // [1001 x float]*, float* and i32*. We are currently not storing the
+      // intermediate float* type but it can be computed on the fly.
+      // 
+      // TODO: Rethink the setup, if required.
+      assert((!IsSrc || (I == NumDim)) && "Malformed DDRef!");
 
       // For DDRefs representing addresses, we need to return a pointer to
       // RetTy.
@@ -135,7 +151,8 @@ bool DDRef::isSelfBlob() const {
     return Ref->isScalarRef() && Ref->getSingleCanonExpr()->isSelfBlob();
   } else if (auto Ref = dyn_cast<BlobDDRef>(this)) {
     (void)Ref;
-    assert(Ref->getCanonExpr()->isSelfBlob() && "Blob DDRef is not a self blob!");
+    assert(Ref->getCanonExpr()->isSelfBlob() &&
+           "Blob DDRef is not a self blob!");
     return true;
   }
   llvm_unreachable("Unknown DDRef kind!");
