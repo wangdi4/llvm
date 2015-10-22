bdc8e2741d1773c40d772e2bc7b3b4e23059ab79
amakaro1 <amakaro1>
2015-10-22 11:18:07 +0000

    Fix for CQ#377372 - allow redefinition of inline extern functions in C++. Old/split clang commit ID: 005e41e6990ba33ee5e17c68fed796391d52ec26

--- clang/lib/Sema/SemaDecl.cpp
+++ clang/lib/Sema/SemaDecl.cpp
@@ -2622,7 +2622,10 @@ getNoteDiagForInvalidRedeclaration(const T *Old, const T *New) {
 static bool canRedefineFunction(const FunctionDecl *FD,
                                 const LangOptions& LangOpts) {
   return ((FD->hasAttr<GNUInlineAttr>() || LangOpts.GNUInline) &&
-          !LangOpts.CPlusPlus &&
+#if INTEL_CUSTOMIZATION
+          // CQ#377372 - allow to redefine functions in C++.
+          (!LangOpts.CPlusPlus || LangOpts.IntelCompat) &&
+#endif // INTEL_CUSTOMIZATION
           FD->isInlineSpecified() &&
           FD->getStorageClass() == SC_Extern);
 }
@@ -10788,8 +10791,17 @@ Sema::CheckForFunctionRedefinition(FunctionDecl *FD,
     if (!FD->isDefined(Definition))
       return;
 
-  if (canRedefineFunction(Definition, getLangOpts()))
+#if INTEL_CUSTOMIZATION
+  if (canRedefineFunction(Definition, getLangOpts())) {
+    if (getLangOpts().CPlusPlus) {
+      // CQ#377372 - show warning that we use Intel extension.
+      Diag(FD->getLocation(), diag::ext_intel_redefinition_extern_inline)
+          << FD->getDeclName();
+      Diag(Definition->getLocation(), diag::note_previous_definition);
+    }
     return;
+  }
+#endif // INTEL_CUSTOMIZATION
 
   // If we don't have a visible definition of the function, and it's inline or
   // a template, skip the new definition.
