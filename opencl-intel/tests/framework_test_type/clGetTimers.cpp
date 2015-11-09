9ff9492d1c2b9c3685f021c2ed4d2f2c57fbc555
vromanov <vlad.romanov@intel.com>
2015-11-09 14:40:58 +0300

    [CL2.1][Timers] Remade test for timers feature and added new test for CL_PLATFORM_HOST_TIMER_RESOLUTION query.

--- opencl-intel/tests/framework_test_type/clGetTimers.cpp
+++ opencl-intel/tests/framework_test_type/clGetTimers.cpp
@@ -1,82 +1,79 @@
-#include <iostream>
-#include "CL/cl.h"
-#include "test_utils.h"
-#include "CL/cl_platform.h"
+#include "CL21.h"
 
-extern cl_device_type gDeviceType;
-
-bool Timers()
+void CL21::GetPlatformInfo_DEVICE_TIMER_RESOLUTION() const
 {
-    std::cout << "=============================================================" << std::endl;
-    std::cout << "Timers" << std::endl;
-    std::cout << "=============================================================" << std::endl;
-
     cl_int iRet = CL_SUCCESS;
 
-    //Get platform.
-    cl_platform_id platform = 0;
-    iRet = clGetPlatformIDs(1, &platform, NULL);
-    CheckException(L"clGetPlatformIDs", CL_SUCCESS, iRet);
+    cl_ulong timer_res = 0;
+    size_t ret_size = 0;
+    iRet = clGetPlatformInfo(m_platform,
+                             CL_PLATFORM_HOST_TIMER_RESOLUTION,
+                             sizeof(timer_res),
+                             &timer_res,
+                             &ret_size);
+    ASSERT_EQ(CL_SUCCESS, iRet)
+        << " clGetPlatformInfo(CL_PLATFORM_HOST_TIMER_RESOLUTION query) failed. ";
+    ASSERT_EQ(sizeof(cl_ulong), ret_size);
+}
 
-    //Get device.
-    cl_device_id device = NULL;
-    iRet = clGetDeviceIDs(platform, gDeviceType, 1, &device, NULL);
-    CheckException(L"clGetDeviceIDs", CL_SUCCESS, iRet);
+void CL21::GetHostTimer_Negative() const
+{
+    cl_int iRet = CL_SUCCESS;
 
-    //Get host time.
     cl_ulong host_timer = 0;
-    {
-        //Wrong prameters for call.
-        iRet = clGetHostTimer(cl_device_id(-1), &host_timer);
-        CheckException(L"clGetHostTimer with invalid device", CL_INVALID_DEVICE, iRet);
 
-        iRet = clGetHostTimer(device, NULL);
-        CheckException(L"clGetHostTimer with invalid ptr to host_timer", CL_INVALID_VALUE, iRet);
+    iRet = clGetHostTimer(cl_device_id(-1), &host_timer);
+    ASSERT_EQ(CL_INVALID_DEVICE, iRet)
+        << " clGetHostTimer with invalid device failed. ";
 
-        host_timer = 0;
-        //Proper prameters for call.
-        iRet = clGetHostTimer(device, &host_timer);
-        CheckException(L"clGetHostTimer", CL_SUCCESS, iRet);
-    }
-    if(!host_timer)
-    {
-        std::cout << "Looks like clGetHostTimer has not set host time." << std::endl;
-        return false;
-    }
+    iRet = clGetHostTimer(m_device, NULL);
+    ASSERT_EQ(CL_INVALID_VALUE, iRet)
+        << " clGetHostTimer with invalid ptr to host_timer failed. ";
+}
+
+void CL21::GetDeviceAndHostTimer_Negative() const
+{
+    cl_int iRet = CL_SUCCESS;
 
     cl_ulong device_timer = 0;
-    {
-        //Wrong prameters for call.
-        iRet = clGetDeviceAndHostTimer(cl_device_id(-1), &device_timer, &host_timer);
-        CheckException(L"clGetDeviceAndHostTimer with invalid device", CL_INVALID_DEVICE, iRet);
+    cl_ulong host_timer   = 0;
 
-        iRet = clGetDeviceAndHostTimer(device, NULL, &host_timer);
-        CheckException(L"clGetDeviceAndHostTimer with invalid ptr to device_timer", CL_INVALID_VALUE, iRet);
+    //Wrong prameters for call.
+    iRet = clGetDeviceAndHostTimer(cl_device_id(-1), &device_timer, &host_timer);
+    ASSERT_EQ(CL_INVALID_DEVICE, iRet)
+        << " clGetDeviceAndHostTimer with invalid device failed. ";
 
-        iRet = clGetDeviceAndHostTimer(device, &device_timer, NULL);
-        CheckException(L"clGetDeviceAndHostTimer with invalid ptr to host_timer", CL_INVALID_VALUE, iRet);
+    iRet = clGetDeviceAndHostTimer(m_device, NULL, &host_timer);
+    ASSERT_EQ(CL_INVALID_VALUE, iRet)
+        << " clGetDeviceAndHostTimer with invalid ptr to device_timer failed. ";
 
-        device_timer = 0;
-        host_timer = 0;
-        //Proper prameters for call.
-        iRet = clGetDeviceAndHostTimer(device, &device_timer, &host_timer);
-        CheckException(L"clGetDeviceAndHostTimer", CL_SUCCESS, iRet);
-    }
-    if(!host_timer)
-    {
-        std::cout << "Looks like clGetDeviceAndHostTimer has not set host time." << std::endl;
-        return false;
-    }
-    if(!device_timer)
-    {
-        std::cout << "Looks like clGetDeviceAndHostTimer has not set device time." << std::endl;
-        return false;
-    }
-    if((host_timer/10000) != (device_timer/10000))
-    {
-        std::cout << "Looks like clGetDeviceAndHostTimer returns async timestamps. Very slow system?" << std::endl;
-        return false;
-    }
+    iRet = clGetDeviceAndHostTimer(m_device, &device_timer, NULL);
+    ASSERT_EQ(CL_INVALID_VALUE, iRet)
+        << " clGetDeviceAndHostTimer with invalid ptr to host_timer. ";
+}
 
-    return true;
+void CL21::GetHostTimer() const
+{
+    cl_int iRet = CL_SUCCESS;
+
+    cl_ulong host_timer = 0;
+    iRet = clGetHostTimer(m_device, &host_timer);
+    ASSERT_EQ(CL_SUCCESS, iRet) << " clGetHostTimer failed. ";
+    ASSERT_NE((cl_ulong)0, host_timer)
+        << "Looks like clGetHostTimer has not set host time.";
+}
+
+void CL21::GetDeviceAndHostTimer() const
+{
+    cl_int iRet = CL_SUCCESS;
+
+    cl_ulong device_timer = 0;
+    cl_ulong host_timer   = 0;
+
+    iRet = clGetDeviceAndHostTimer(m_device, &device_timer, &host_timer);
+    ASSERT_EQ(CL_SUCCESS, iRet) << " clGetDeviceAndHostTimer failed. ";
+    ASSERT_NE((cl_ulong)0, host_timer)
+        << "Looks like clGetHostTimer has not set host time.";
+    ASSERT_NE((cl_ulong)0, device_timer)
+        << "Looks like clGetHostTimer has not set host time.";
 }
