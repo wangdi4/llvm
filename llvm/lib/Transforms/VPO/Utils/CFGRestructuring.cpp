1fa2dc07a18e47858b1f587c0d3fe74ed1b11a35
Zhang, Yuanrui <yuanrui.zhang@intel.com>
2015-11-11 21:57:52 +0000

    Extend CFGRestructuring to handle general directives that need to be in a standalone basic block. Reviewers: Xinmin Tian and Ernesto Su Old/split llvm commit ID: bf8bb5667ef3307785477c1a6c9b6822f2c7adac

--- llvm/lib/Transforms/VPO/Utils/CFGRestructuring.cpp
+++ llvm/lib/Transforms/VPO/Utils/CFGRestructuring.cpp
@@ -34,7 +34,7 @@ using namespace llvm::vpo;
 // This function puts "directive_begin" and "directive_end" into standalone
 // basic blocks. This is required by WRegion Construction.
 //
-// Each "directive_begin" or "directive_end" has a "directive_qual_end"
+// Each "directive_begin" or "directive_end" has a "directive_qual_list_end"
 // associated to indicate the end of this directive. Here are some examples,
 // where the strings are represented by metadata in the real code:
 //
@@ -42,14 +42,14 @@ using namespace llvm::vpo;
 //
 // call void @llvm.intel.directive("DIR_BEGIN")
 // ...
-// call void @llvm.intel.directive.qual("DIR_QUAL_END")
+// call void @llvm.intel.directive("DIR_QUAL_LIST_END")
 //
 //
 // Example 2:
 //
 // call void @llvm.intel.directive("DIR_END")
 // ...
-// call void @llvm.intel.directive.qual("DIR_QUAL_END")
+// call void @llvm.intel.directive("DIR_QUAL_LIST_END")
 //
 void VPOUtils::CFGRestructuring(
   Function &F,
@@ -61,32 +61,24 @@ void VPOUtils::CFGRestructuring(
   DEBUG(dbgs() << "VPO CFG Restructuring \n");
 
   // Find all the intrinsic instructions, including directive_begin,
-  // directive_end, directive_qual_end, and bookkeep them in
+  // directive_end, directive_qual_list_end, and bookkeep them in
   // InstructionsToSplit.
   std::set<Instruction* > InstructionsToSplit;
   InstructionsToSplit.clear();
   for (Function::iterator B = F.begin(), BE = F.end(); B != BE; ++B)
     for (BasicBlock::iterator I = B->begin(), IE = B->end(); I != IE; ++I)
       if (IntrinsicInst* Inst = dyn_cast<IntrinsicInst>(&*I)) 
-        // To-do:
-        //
-        // Use the following code to find directive_qual_end once Matt
-        // modifies getDirectiveMetadataString to support intel_directive_qual
-        //
-        // if (Inst->getIntrinsicID() == 
-        //     Intrinsic::intel_directive_qual) {
-        //
         if (Inst->getIntrinsicID() == Intrinsic::intel_directive) {
           StringRef DirString = getDirectiveMetadataString(Inst);
-          if ((DirString == "dir.simd") || (DirString == "dir.simd.end") ||
-              (DirString == "dir.qual.end")) {
+          if (isBeginOrEndDirective(DirString) ||
+              isListEndDirective(DirString)) {
             InstructionsToSplit.insert(I);
           }
         }
 
   // Now, go through InstructionsToSplit and do the splitting around
   // directive_begin or directive_end, or the next instruction right after
-  // directive_qual_end.
+  // directive_qual_list_end.
 
   unsigned counter = 0;
 
@@ -98,12 +90,13 @@ void VPOUtils::CFGRestructuring(
 
     // If directive_begin or directive_end is the first instruction of a basic
     // block, we can just skip splitting around it. Note that,
-    // directive_qual_end will never be the first instruction of a basic block,
-    // since it is always paired with directive_begin or directive_end.
+    // directive_qual_list_end will never be the first instruction of a basic
+    // block, since it is always paired with directive_begin or directive_end.
     if (I != BB->begin()) {
       Instruction* SplitPoint = I;
-      StringRef DirString = getDirectiveMetadataString(dyn_cast<IntrinsicInst>(I));
-      if (DirString == "dir.qual.end") {
+      StringRef DirString = 
+          getDirectiveMetadataString(dyn_cast<IntrinsicInst>(I));
+      if (isListEndDirective(DirString)) {
         BasicBlock::iterator inst = I;
         SplitPoint = ++inst;
       }
