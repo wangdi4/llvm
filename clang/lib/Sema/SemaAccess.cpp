74c26313daf92174bc8f8f3826307150f5016052
Chupina, Olga <olga.chupina@intel.com>
2015-12-21 10:34:07 +0000

    [[msticlel4-47933]] Fix for CQ374722: change error to the warning when private constructor is called
    For compatibility reasons allow to call private constructor from template or
    template instantiation in intel compatibility mode. In MS compatibility mode
    allow to call private constructor at any point.
    
    R=Denis Zobnin, Alexey Bataev
    BUG=CQ#374722
    TEST=check-clang, alloy, no new fails
    PERF=low
    
    
    Old/split clang commit ID: 5bc327734325204ceb9de7e7e2d204760e4be1f8

--- clang/lib/Sema/SemaAccess.cpp
+++ clang/lib/Sema/SemaAccess.cpp
@@ -1640,6 +1640,17 @@ Sema::AccessResult Sema::CheckConstructorAccess(SourceLocation UseLoc,
   PartialDiagnostic PD(PDiag());
   switch (Entity.getKind()) {
   default:
+#if INTEL_CUSTOMIZATION
+    // Fix for CQ374722: for compatibility reasons allow to call private
+    // constructor from template or template instantiation in intel
+    // compatibility mode. In MS compatibility mode allow to call private
+    // constructor at any point.
+    if (getLangOpts().IntelCompat &&
+        (getLangOpts().IntelMSCompat || CurContext->isDependentContext() ||
+         !ActiveTemplateInstantiations.empty()))
+      PD = PDiag(diag::warn_access_ctor);
+    else
+#endif // INTEL_CUSTOMIZATION
     PD = PDiag(IsCopyBindingRefToTemp
                  ? diag::ext_rvalue_to_reference_access_ctor
                  : diag::err_access_ctor);
