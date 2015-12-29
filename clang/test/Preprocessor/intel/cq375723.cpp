b2ac2b8a505e065c6e5dde237a61728e14f0e6c6
amakaro1 <amakaro1>
2015-12-29 11:28:12 +0000

    Fix for ill-formed #[line_number] pragma (turns error into warning).
    R=Alexey Bataev
    BUG=375723
    TEST=check-clang, alloy, no new fails
    PERF=low
    Old/split clang commit ID: 0b63de55e269bf753d8cfa05e23f2215cddf9e3d

--- clang/test/Preprocessor/intel/cq375723.cpp
+++ clang/test/Preprocessor/intel/cq375723.cpp
@@ -4,3 +4,7 @@
 #line 12 incorrect filename // expected-warning{{expected a file name}}
 #line 13 incorrect_filename.cpp // expected-warning{{expected a file name}}
 #line 228 "correct_filename.cpp" // no warning
+
+#20 incorrect // expected-warning{{expected a file name}}
+
+# 19 "/export/users/aeloviko/ws/boost/boost_1_50_0/libs/chrono/test/../../../boost/mpl/lambda_fwd.hpp" 2
