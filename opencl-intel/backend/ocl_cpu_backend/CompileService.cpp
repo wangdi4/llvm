3b8b19e8b5ec848e9bf83f7d3cba7b5ac99ee54b
Tyurin Evgeniy <evgeniy.tyurin@intel.com>
2016-01-25 19:29:39 +0300

    Remove traces of SPIR-V consumption from Backend.

--- opencl-intel/backend/ocl_cpu_backend/CompileService.cpp
+++ opencl-intel/backend/ocl_cpu_backend/CompileService.cpp
@@ -83,10 +83,6 @@ cl_dev_err_code CompileService::CreateProgram( const void* pBinary,
             spProgram->SetBitCodeContainer(new BitCodeContainer(pBinaryData, uiBinaryDataSize, "main"));
             GetProgramBuilder()->ParseProgram(spProgram.get());
         }
-        else if ( _CL_SPIRV_MAGIC_NUMBER_ == ((unsigned int*)pBinary)[0] )
-        {
-            printf("Create BE program with IL is not implemented! FILE: %s, LINE: %d", __FILE__, __LINE__);
-        }
         else
         {
             throw Exceptions::DeviceBackendExceptionBase("Unknown binary type", CL_DEV_INVALID_BINARY);
