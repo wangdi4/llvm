511992d471a254251481128e72fc560643cfbd3c
yongzhi.zhang <yongzhiz@hdci2248.hd.intel.com>
2016-03-24 12:05:19 -0400

    fix shift amount type Old/split llvm commit ID: d46594b1101557872fcb3cd485da60bf28b67e1f

--- llvm/lib/Target/LPU/LPUISelLowering.h
+++ llvm/lib/Target/LPU/LPUISelLowering.h
@@ -43,7 +43,9 @@ namespace llvm {
     // given the EVT of that operand. Since the LPU supports
     // all types save i1, that is what we return.
     // (Copied from FFWD)
-    virtual MVT getScalarShiftAmountTy(EVT LHSTy) const override {
+    MVT getScalarShiftAmountTy(EVT LHSTy) const override {
+      return MVT::i8;
+      /*
       switch(LHSTy.getSimpleVT().SimpleTy) {
         case MVT::i1:
           return MVT::i8;
@@ -52,6 +54,7 @@ namespace llvm {
         default:
           llvm_unreachable("Unknown shift type");
       }
+      */
     }
 
     /// LowerOperation - Provide custom lowering hooks for some operations.
