9d5b1286c89d6076f724c00a124194d209de0401
vromanov <vlad.romanov@intel.com>
2016-04-25 08:18:28 +0300

    Revert "Revert "Finalize tbb at exit.""
    This reverts commit 54d9f99b35f4648196164d76e7334e17cbcbbf56.

--- opencl-intel/utils/task_executor/tbb_executor.cpp
+++ opencl-intel/utils/task_executor/tbb_executor.cpp
@@ -218,9 +218,7 @@ TBBTaskExecutor::TBBTaskExecutor() :
 
 TBBTaskExecutor::~TBBTaskExecutor()
 {
-    /* We don't delete m_pGlobalArenaHandler because of all kind of TBB issues in the shutdown sequence, but since this destructor is called when the whole library goes down and m_pGlobalArenaHandler
-       is a singleton, it doesn't really matter. */
-    // TBB seem to have a bug in ~task_scheduler_init(), so we work around it by not deleting m_pScheduler (TBB bug #1955)
+    Finalize();
 }
 
 int TBBTaskExecutor::Init(FrameworkUserLogger* pUserLogger, unsigned int uiNumOfThreads, ocl_gpa_data * pGPAData)
