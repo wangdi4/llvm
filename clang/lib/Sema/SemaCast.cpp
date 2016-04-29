790766596c159843221c97c005774af1d9f0f7f9
dpolukhi <dpolukhi>
2016-04-29 05:00:53 +0000

    Allow downcast from private base class in MSVC mode
    BUG=409860
    R=Olga Chupina
    TEST=xmain_checkin
    
    Old/split clang commit ID: b4e7dac9b3eaa59ccfe69f74239b4de3087c6fe5

--- clang/lib/Sema/SemaCast.cpp
+++ clang/lib/Sema/SemaCast.cpp
@@ -1381,10 +1381,15 @@ TryStaticDowncast(Sema &Self, CanQualType SrcType, CanQualType DestType,
   }
 
   if (!CStyle) {
-    switch (Self.CheckBaseClassAccess(OpRange.getBegin(),
-                                      SrcType, DestType,
-                                      Paths.front(),
-                                diag::err_downcast_from_inaccessible_base)) {
+#if INTEL_CUSTOMIZATION
+    // CQ#409860 report warning instead of error in compatibility mode.
+    unsigned Diag =
+        (Self.getLangOpts().IntelCompat && Self.getLangOpts().IntelMSCompat)
+            ? diag::ext_ms_downcast_from_inaccessible_base
+            : diag::err_downcast_from_inaccessible_base;
+    switch (Self.CheckBaseClassAccess(OpRange.getBegin(), SrcType, DestType,
+                                      Paths.front(), Diag)) {
+#endif
     case Sema::AR_accessible:
     case Sema::AR_delayed:     // be optimistic
     case Sema::AR_dependent:   // be optimistic
