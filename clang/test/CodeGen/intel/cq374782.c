ca68dbef2829d67f02b3525a25141c3bf7984ac9
Bokhanko, Andrey S <andrey.s.bokhanko@intel.com>
2016-05-30 13:48:19 +0000

    [[msticlel4-755]] Enabling of several _Interlocked... builtins on Linux
    R=Dmitry Polukhin
    BUG=374782
    
    Old/split clang commit ID: da9189909652552731ed0a38d0701e20b7e19803

---
+++ clang/test/CodeGen/intel/cq374782.c
@@ -0,0 +1,20 @@
+// RUN: %clang_cc1 -triple x86_64-unknown-unknown -fintel-compatibility %s -emit-llvm -o - | FileCheck %s
+
+void foo(void * volatile *vpvp,
+         long volatile *lvp,
+         long *lp,
+         void *vp,
+         void volatile *vvp,
+         long l) {
+  _InterlockedCompareExchange(lvp, l, l);
+// CHECK: {{%.* = cmpxchg volatile i64\* %.*, i64 %.*, i64 %.* seq_cst seq_cst}}
+  _InterlockedCompareExchangePointer(vpvp, vp, vp);
+// CHECK: {{%.* = cmpxchg volatile i64\* %.*, i64 %.*, i64 %.* seq_cst seq_cst}}
+  _InterlockedExchangeAdd(lvp, l);
+// CHECK: {{%.* = atomicrmw volatile add i64\* %.*, i64 %.* seq_cst}}
+  _InterlockedExchange(lvp, l);
+// CHECK: {{%.* = atomicrmw xchg i64\* %.*, i64 %.* seq_cst}}
+  _InterlockedExchangePointer(vpvp, vp);
+// CHECK: {{%.* = atomicrmw xchg i64\* %.*, i64 %.* seq_cst}}
+}
+
