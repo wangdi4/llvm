6331ef98238703480f6fcf2e3346f77b4b9b93e2
Bokhanko, Andrey S <andrey.s.bokhanko@intel.com>
2016-06-15 11:23:00 +0000

    [[msticlel4-53507]] Fixing cq410807.cpp test to make in pass with prod compiler
    R=Dmitry Polukhin
    BUG=CQ#411782
    
    Old/split clang commit ID: fef8d25dbc47fa5d7b5b4b5706acba001174063d

--- clang/test/CodeGenCXX/intel/cq410807.cpp
+++ clang/test/CodeGenCXX/intel/cq410807.cpp
@@ -14,11 +14,11 @@ public:
 
   void g() {
     B b = {f};
-// CHECK: call void @llvm.memcpy.p0i8.p0i8.i64(i8* %0, i8* bitcast (%"struct.A::B"* @_ZZN1A1gEvE1b to i8*), i64 16, i32 8, i1 false)
+// CHECK: call void @llvm.memcpy.p0i8.p0i8.i64(i8* {{%.*}}, i8* bitcast (%"struct.A::B"* @_ZZN1A1gEvE1b to i8*), i64 16, i32 8, i1 false)
     B b2 = {&A::f};
-// CHECK: void @llvm.memcpy.p0i8.p0i8.i64(i8* %1, i8* bitcast (%"struct.A::B"* @_ZZN1A1gEvE2b2 to i8*), i64 16, i32 8, i1 false)
+// CHECK: call void @llvm.memcpy.p0i8.p0i8.i64(i8* {{%.*}}, i8* bitcast (%"struct.A::B"* @_ZZN1A1gEvE2b2 to i8*), i64 16, i32 8, i1 false)
     B b3 = {(TAmtd)f};
-// CHECK: call void @llvm.memcpy.p0i8.p0i8.i64(i8* %2, i8* bitcast (%"struct.A::B"* @_ZZN1A1gEvE2b3 to i8*), i64 16, i32 8, i1 false)
+// CHECK: call void @llvm.memcpy.p0i8.p0i8.i64(i8* {{%.*}}, i8* bitcast (%"struct.A::B"* @_ZZN1A1gEvE2b3 to i8*), i64 16, i32 8, i1 false)
   }
 };
 
