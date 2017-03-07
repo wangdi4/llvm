652a23b6179b0f4ff92c94d3de0a19a67ca43169
Barry Tannenbaum <barry.m.tannenbaum@intel.com>
2017-03-07 11:41:16 -0500

    Revert ""clang-format" FPGA-Advisor and BasicBlockDumper. NFC."
    This reverts commit f28a295e8ebb441520bbcc954be696cbeb89d201.
    Old/split llvm commit ID: d1a7e710b496f14bb540723d5172f73f197b2a63

--- llvm/lib/Transforms/BasicBlockDumper/BasicBlockCompiler.cpp
+++ llvm/lib/Transforms/BasicBlockDumper/BasicBlockCompiler.cpp
@@ -1,80 +1,82 @@
 #include "BasicBlockCompiler.h"
 
-static cl::opt<unsigned>
-    CompilerVerbose("compiler-verbose",
-                    cl::desc("Dumps verbose information on compiler interface"),
-                    cl::Hidden, cl::init(1));
+static cl::opt<unsigned> CompilerVerbose("compiler-verbose", cl::desc("Dumps verbose information on compiler interface"),
+		cl::Hidden, cl::init(1));
 
-std::map<BasicBlock *, BlockMetadata> *metaMap = NULL;
+
+std::map<BasicBlock*, BlockMetadata> *metaMap = NULL;
 
 void compileBlock(BasicBlock *BB) {
 
-  std::stringstream filename;
+    std::stringstream filename ;
 
-  filename.str("");
-  filename << "basic_block_" << BB->getName().str() << "_new.ll";
+    filename.str("");
+    filename << "basic_block_" << BB->getName().str() << "_new.ll";
+    
+    dumpBasicBlock(filename.str(), BB);
 
-  dumpBasicBlock(filename.str(), BB);
+    // invoke compiler. 
+    // spawn a process
+    //spawn a process                                                             
+    int compiler_to_advisor[2];
+                                                           
+    if (pipe(compiler_to_advisor) < 0)
+    {
+        exit(1);
+    }
 
-  // invoke compiler.
-  // spawn a process
-  // spawn a process
-  int compiler_to_advisor[2];
+    int pid_compiler;
+    // fork off a process which will eventually invoke the target compiler. 
+    pid_compiler = fork();
+    if (pid_compiler == 0)
+    {
+      std::string baseLL(filename.str());
+      filename << ".err";
+      FILE* errfd = fopen(filename.str().c_str(), "w");
 
-  if (pipe(compiler_to_advisor) < 0) {
-    exit(1);
-  }
+      // child                                                                
+      close(compiler_to_advisor[0]);
 
-  int pid_compiler;
-  // fork off a process which will eventually invoke the target compiler.
-  pid_compiler = fork();
-  if (pid_compiler == 0) {
-    std::string baseLL(filename.str());
-    filename << ".err";
-    FILE *errfd = fopen(filename.str().c_str(), "w");
+      dup2(compiler_to_advisor[1], fileno(stdout));  // dump output to log             
+      dup2(fileno(errfd), fileno(stderr));  // dump output to log             
 
-    // child
-    close(compiler_to_advisor[0]);
+      // make incoming fd non-blocking                                        
 
-    dup2(compiler_to_advisor[1], fileno(stdout)); // dump output to log
-    dup2(fileno(errfd), fileno(stderr));          // dump output to log
+      fprintf(errfd, "launching basic block compiler %s\n", baseLL.c_str());
+      fflush(errfd);
+      // Each jtag channel will need some special sauce from                  
+      // a particular vendor...                                               
+      execlp("analyze-basic-block", "analyze-basic-block", baseLL.c_str(), NULL);
+    }
 
-    // make incoming fd non-blocking
+    // parent                                                                     
+    close(compiler_to_advisor[1]);
+    
+    BlockMetadata metadata;
+    // put pipe in a buffer 
+    FILE *advisor = fdopen(compiler_to_advisor[0],"r");
 
-    fprintf(errfd, "launching basic block compiler %s\n", baseLL.c_str());
-    fflush(errfd);
-    // Each jtag channel will need some special sauce from
-    // a particular vendor...
-    execlp("analyze-basic-block", "analyze-basic-block", baseLL.c_str(), NULL);
-  }
+    fscanf(advisor, "%d,%d,%d", &metadata.area, &metadata.latency, &metadata.ii);
 
-  // parent
-  close(compiler_to_advisor[1]);
+    // use at least one area and latency so as not to screw up the tools.
+    if (metadata.area == 0) {
+      metadata.area = 1;
+    }
 
-  BlockMetadata metadata;
-  // put pipe in a buffer
-  FILE *advisor = fdopen(compiler_to_advisor[0], "r");
+    if (metadata.latency == 0) {
+      metadata.latency = 1;
+    }
+ 
+    //if (CompilerVerbose) {
+      fprintf(stderr, "Block %s area: %d latency: %d ii: %d\n", BB->getName().str().c_str(), metadata.area, metadata.latency, metadata.ii);
+      //}
 
-  fscanf(advisor, "%d,%d,%d", &metadata.area, &metadata.latency, &metadata.ii);
+    // now, wait for the child to emit something. 
 
-  // use at least one area and latency so as not to screw up the tools.
-  if (metadata.area == 0) {
-    metadata.area = 1;
-  }
+    metaMap->insert(std::pair<BasicBlock*, BlockMetadata>(BB, metadata));
 
-  if (metadata.latency == 0) {
-    metadata.latency = 1;
-  }
-
-  // if (CompilerVerbose) {
-  fprintf(stderr, "Block %s area: %d latency: %d ii: %d\n",
-          BB->getName().str().c_str(), metadata.area, metadata.latency,
-          metadata.ii);
-  //}
-
-  // now, wait for the child to emit something.
-
-  metaMap->insert(std::pair<BasicBlock *, BlockMetadata>(BB, metadata));
-
-  fclose(advisor);
+    fclose(advisor);
 }
+
+
+
