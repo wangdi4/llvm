200b231499b755e063a078fb825755424ec9179a
Barry Tannenbaum <barry.m.tannenbaum@intel.com>
2017-04-04 13:54:51 -0400

    Convert to use in CSA build - reference CSA_TOP Old/split llvm commit ID: 2937b779fb4d03809e1ad400a781497519b490d0

--- llvm/lib/Transforms/FPGA-Advisor/linkfpgaroi.sh
+++ llvm/lib/Transforms/FPGA-Advisor/linkfpgaroi.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
-CSALLVMSRC=/nfs/site/home/hgpatil/links/ThreadPoints/DesignForward2/CSA-LPU/Leap-FPGA-Advisor/llvm-3.6.1.src/
-CSALLVMBUILD=/nfs/site/home/hgpatil/links/ThreadPoints/DesignForward2/CSA-LPU/Leap-FPGA-Advisor/llvm-build/
+
 BOOST_ROOT=/usr/intel/pkgs/boost/1.59.0/
 TBBLIBDIR=/nfs/hd/proj/asim/x86_64_linux26/bld/tbb/tbb41_20121003oss/build/linux_intel64_gcc_cc4.4.2_libc2.4_kernel2.6.16.60_release
 
@@ -38,12 +37,12 @@ fi
 roifile=$2
 export LD_LIBRARY_PATH=$TBBLIBDIR:$BOOST_ROOT/lib/
 
-$CSALLVMBUILD/bin/opt -load=$CSALLVMBUILD/lib/LLVMBasicBlockNumberer.so -bbnumber -bbn:o $prefix.bbnumber.txt < $prefix.bc > $prefix.bbn.bc
+csa-opt -load=$CSA_TOP/tools/install/lib/LLVMBasicBlockNumberer.so -bbnumber -bbn:o $prefix.bbnumber.txt < $prefix.bc > $prefix.bbn.bc
 
-$CSALLVMBUILD/bin/opt -load=$CSALLVMBUILD/lib/LLVMFPGA-Advisor.so -fpga-advisor-instrument < $prefix.bbn.bc > $prefix.bbn.fpgaadv.bc
+csa-opt -load=$CSA_TOP/tools/install/lib/LLVMFPGA-Advisor.so -fpga-advisor-instrument < $prefix.bbn.bc > $prefix.bbn.fpgaadv.bc
 
-$CSALLVMBUILD/bin/opt -load=$CSALLVMBUILD/lib/LLVMROIController.so -roi -roi:i $roifile < $prefix.bbn.fpgaadv.bc > $prefix.bbn.fpgaadv.roi.bc
+csa-opt -load=$CSA_TOP/tools/install/lib/LLVMROIController.so -roi -roi:i $roifile < $prefix.bbn.fpgaadv.bc > $prefix.bbn.fpgaadv.roi.bc
 
-clang++ $prefix.bbn.fpgaadv.roi.bc $CSALLVMSRC/examples/FPGA-Advisor/common/rdtsc.ll $CSALLVMSRC/lib/Transforms/LLVMROIController/ROIanalysis.bc $CSALLVMSRC/lib/Transforms/FPGA-Advisor/ROIhandler.bc -o $prefix.roicontroller.fpgaadvisor -L$BOOST_ROOT/lib -lboost_system -lboost_serialization
+clang++ $prefix.bbn.fpgaadv.roi.bc $$CSA_TOP/tools/src/llvm/examples/FPGA-Advisor/common/rdtsc.ll $$CSA_TOP/tools/src/llvm/lib/Transforms/LLVMROIController/ROIanalysis.bc $$CSA_TOP/tools/src/llvm/lib/Transforms/FPGA-Advisor/ROIhandler.bc -o $prefix.roicontroller.fpgaadvisor -L$BOOST_ROOT/lib -lboost_system -lboost_serialization
 
 echo "$prefix.roicontroller.fpgaadvisor  generated!"
