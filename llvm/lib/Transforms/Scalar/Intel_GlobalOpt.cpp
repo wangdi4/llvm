79824c052e622e86dc766fafb0e34488cd7d1c0b
Topper, Craig <craig.topper@intel.com>
2017-05-08 19:34:25 +0000

    Summary: Fix copyright header on Intel_GlobalOpt.cpp Reviewed by: Kaylor, Andrew Rurl: https://ir-codecollab.intel.com/ui#review:id=68517 Old/split llvm commit ID: 3e650b364ed8ee158106fc2f065037bfae845805

--- llvm/lib/Transforms/Scalar/Intel_GlobalOpt.cpp
+++ llvm/lib/Transforms/Scalar/Intel_GlobalOpt.cpp
@@ -1,10 +1,11 @@
-//===- IntelGlobalOpt.cpp - Optimize Global Variables
-//--------------------------===//
+//===- IntelGlobalOpt.cpp - Optimize Global Variables----------------------===//
 //
-//                     The LLVM Compiler Infrastructure
+// Copyright (C) 2016-2017 Intel Corporation. All rights reserved.
 //
-// This file is distributed under the University of Illinois Open Source
-// License. See LICENSE.TXT for details.
+// The information and source code contained herein is the exclusive
+// property of Intel Corporation and may not be disclosed, examined
+// or reproduced in whole or in part without explicit written authorization
+// from the company.
 //
 //===----------------------------------------------------------------------===//
 //
