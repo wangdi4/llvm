84f6e5d93a2464a6a5c8ad58669aa2d540824822
Alexey Bader <alexey.bader@intel.com>
2017-06-09 16:47:48 +0300

    Update year in copyright header. NFC.

--- opencl-intel/backend/ocl_cpu_backend/ProgramBuilder.h
+++ opencl-intel/backend/ocl_cpu_backend/ProgramBuilder.h
@@ -1,6 +1,6 @@
 /*****************************************************************************\
 
-Copyright (c) Intel Corporation (2010-2012).
+Copyright (c) Intel Corporation (2010-2017).
 
     INTEL MAKES NO WARRANTY OF ANY KIND REGARDING THE CODE.  THIS CODE IS
     LICENSED ON AN "AS IS" BASIS AND INTEL WILL NOT PROVIDE ANY SUPPORT,
