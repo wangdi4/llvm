0167f7a2dc70e3047539746f6d9ff3d3d98b98a9
Tim Creech <timothy.m.creech@intel.com>
2017-08-08 22:44:38 -0400

    CSA: add a special case to RedundantMovElim to avoid defeating MemopOrdering's final SXU MOV0 Old/split llvm commit ID: dad8cd47a36aadae4a5bd383499beb5bd3a55da4

--- llvm/lib/Target/CSA/CSARedundantMovElim.cpp
+++ llvm/lib/Target/CSA/CSARedundantMovElim.cpp
@@ -418,6 +418,7 @@ bool CSARedundantMovElim::isRedundantMov(const MachineInstr& MI) const {
   if (!MI.getFlag(MachineInstr::NonSequential))
     return false;
 
+
   // (b)
   assert(MI.getNumOperands() == 2);
   const MachineOperand* dest = &MI.getOperand(0);
@@ -436,6 +437,17 @@ bool CSARedundantMovElim::isRedundantMov(const MachineInstr& MI) const {
 
   unsigned src_reg = src->getReg();
   unsigned dest_reg = dest->getReg();
+
+  const TargetRegisterClass* src_RC= TII->lookupLICRegClass(src_reg);
+  const TargetRegisterClass* dest_RC = TII->lookupLICRegClass(dest_reg);
+
+  if ((!src_RC) || (!dest_RC)) {
+    // Error looking up register classes for input/output LICs.
+    return false;
+  }
+  if (dest_RC == &CSA::RI1RegClass)
+    return false;
+
 
   // (c)
   //
@@ -468,14 +480,6 @@ bool CSARedundantMovElim::isRedundantMov(const MachineInstr& MI) const {
   }
 
   // (e)
-  const TargetRegisterClass* src_RC= TII->lookupLICRegClass(src_reg);
-  const TargetRegisterClass* dest_RC = TII->lookupLICRegClass(dest_reg);
-
-  if ((!src_RC) || (!dest_RC)) {
-    // Error looking up register classes for input/output LICs.
-    return false;
-  }
-
   int src_bitwidth = this->TRI->getRegSizeInBits(*src_RC);
   int mov_bitwidth = TII->getMOVBitwidth(MI.getOpcode());
   int dest_bitwidth = this->TRI->getRegSizeInBits(*dest_RC);
