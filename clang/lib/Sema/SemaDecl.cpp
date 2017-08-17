763234ee1b3bfb5ba2a2e992330a682f9249de91
Erich Keane <erich.keane@intel.com>
2017-08-17 14:30:57 -0700

    Fix SemaDecl.cpp conflict/build failure
    Change-Id: If8d0bd3702c5864203352f42b44f7ca974204632
    Old/split clang commit ID: c3911bd0cb16ac87b72d6495fbbb2c0228aee0d9

--- clang/lib/Sema/SemaDecl.cpp
+++ clang/lib/Sema/SemaDecl.cpp
@@ -12641,7 +12641,6 @@ Decl *Sema::ActOnFinishFunctionBody(Decl *dcl, Stmt *Body,
   sema::AnalysisBasedWarnings::Policy WP = AnalysisWarnings.getDefaultPolicy();
   sema::AnalysisBasedWarnings::Policy *ActivePolicy = nullptr;
 
-<<<<<<< HEAD
 #ifdef INTEL_SPECIFIC_IL0_BACKEND
   if (Body && !CommonFunctionOptions.empty()) {
     SmallVector<Stmt *, 4> Stmts;
@@ -12673,10 +12672,7 @@ Decl *Sema::ActOnFinishFunctionBody(Decl *dcl, Stmt *Body,
     CommonFunctionOptions.erase("CHECK_STACK");
   }
 #endif  // INTEL_SPECIFIC_IL0_BACKEND
-  if (getLangOpts().CoroutinesTS && getCurFunction()->CoroutinePromise)
-=======
   if (getLangOpts().CoroutinesTS && getCurFunction()->isCoroutine())
->>>>>>> 28be04dd8e51399b1955d413777003f80e81136f
     CheckCompletedCoroutineBody(FD, Body);
 
   if (FD) {
