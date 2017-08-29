4f3a961ba36cc06d0938172d394cd19bfd8f3217
Erich Keane <erich.keane@intel.com>
2017-08-29 10:33:40 -0700

    Fix conflicts for dcdc5a7
    Change-Id: Iae2dc96139807f9c9d2c6c5a5a1277c80ff46cdd
    Old/split clang commit ID: dcf77c61aada363c8cb63f07d64c200a765d37e0

--- clang/test/CodeGen/pgo-sample-thinlto-summary.c
+++ clang/test/CodeGen/pgo-sample-thinlto-summary.c
@@ -1,12 +1,7 @@
-<<<<<<< HEAD
 // INTEL -- xmain inlining logic prefers cloning foo() to inlining, so this test
 //          fails.  To work around that, disable xmain-specific inlining logic.
-// RUN: %clang_cc1 -O2 -fprofile-sample-use=%S/Inputs/pgo-sample-thinlto-summary.prof %s -emit-llvm -mllvm -inline-for-xmain=0 -o - 2>&1 | FileCheck %s -check-prefix=O2
+// RUN: %clang_cc1 -O2 -fprofile-sample-use=%S/Inputs/pgo-sample-thinlto-summary.prof %s -emit-llvm -mllvm -inline-for-xmain=0 -o - 2>&1 | FileCheck %s -check-prefix=SAMPLEPGO
 // RUN: %clang_cc1 -O2 -fprofile-sample-use=%S/Inputs/pgo-sample-thinlto-summary.prof %s -emit-llvm -mllvm -inline-for-xmain=0 -flto=thin -o - 2>&1 | FileCheck %s -check-prefix=THINLTO
-=======
-// RUN: %clang_cc1 -O2 -fprofile-sample-use=%S/Inputs/pgo-sample-thinlto-summary.prof %s -emit-llvm -o - 2>&1 | FileCheck %s -check-prefix=SAMPLEPGO
-// RUN: %clang_cc1 -O2 -fprofile-sample-use=%S/Inputs/pgo-sample-thinlto-summary.prof %s -emit-llvm -flto=thin -o - 2>&1 | FileCheck %s -check-prefix=THINLTO
->>>>>>> dcdc5a7d3cbec7c4e0aaccf9e2adfa9c4ae66b4a
 // Checks if hot call is inlined by normal compile, but not inlined by
 // thinlto compile.
 
