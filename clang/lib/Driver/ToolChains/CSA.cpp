db36db352222255bfd528097ce0f0ce933c4f501
Ravi Narayanaswamy <ravi.narayanaswamy@intel.com>
2017-08-31 14:11:29 -0700

    Merge of csa/clang dated aug 31 2017
    Change-Id: I0e4582114127b0299ceb97b2bc1146084b48a792
    Old/split clang commit ID: 0d4f83c166079cff5f1dd34163d2d92c456eda2f

---
+++ clang/lib/Driver/ToolChains/CSA.cpp
@@ -0,0 +1,116 @@
+//===--- CSA.cpp - CSA ToolChain Implementations ----*- C++ -*-===//
+//
+//                     The LLVM Compiler Infrastructure
+//
+// This file is distributed under the University of Illinois Open Source
+// License. See LICENSE.TXT for details.
+//
+//===----------------------------------------------------------------------===//
+
+#include "CSA.h"
+#include "CommonArgs.h"
+#include "clang/Driver/Compilation.h"
+#include "clang/Driver/Driver.h"
+#include "clang/Driver/Options.h"
+#include "llvm/Option/ArgList.h"
+
+using namespace clang::driver;
+using namespace clang::driver::tools;
+using namespace clang::driver::toolchains;
+using namespace clang;
+using namespace llvm::opt;
+
+void CSAToolChain::addClangTargetOptions(const ArgList &DriverArgs,
+    ArgStringList &CC1Args, Action::OffloadKind DeviceOffloadKind) const {
+
+  // Linux default options to pass to cc1
+  Linux::addClangTargetOptions(DriverArgs, CC1Args, DeviceOffloadKind);
+
+  // Examine the effective (last/rightmost) optimization option given.
+  if (Arg *A = DriverArgs.getLastArg(options::OPT_O_Group)) {
+    // If -O0 is specified, disable dataflow conversion and memop ordering.
+    if (A->getOption().matches(options::OPT_O0)) {
+      CC1Args.push_back("-mllvm");
+      CC1Args.push_back("-csa-cvt-cf-df-pass=0");
+      CC1Args.push_back("-mllvm");
+      CC1Args.push_back("-csa-order-memops=0");
+    }
+  } else {
+    // If no -O option was given, then the usual default is like -O0. Instead,
+    // the CSA toolchain enables optimization by default. This is unlike other
+    // Targets.
+    CC1Args.push_back("-O");
+  }
+
+  // Since we're using the CSAToolChain, we know the target is CSA.
+  // Check for the -fopenmp-targets=... list to see if we're compiling an
+  // offloaded statement.
+  Arg *targets = DriverArgs.getLastArg(options::OPT_fopenmp_targets_EQ);
+  if (nullptr == targets) {
+
+    // Include some inlined libm implementations. (This may be temporary until a
+    // real math library is in place.) Note that we only want to incur this
+    // header once, so it doesn't get run again for the -fopenmp-targets=csa
+    // invocation of cc1.
+    CC1Args.push_back("-include");
+    CC1Args.push_back("__clang_csa_math.h");
+
+    return;
+  }
+
+  // Check the list of arguments for "csa"
+  for (unsigned i = 0; i < targets->getNumValues(); i++) {
+
+    // If we found an offload for the CSA. Add -mllvm -csa-wrap-asm to the
+    // options so any CSA assembly code will be "wrapped" as strings and
+    // won't cause errors when the x86 assembler sees them.
+    const char *tgt = targets->getValue(i);
+    if (0 == strcmp(tgt, "csa")) {
+      CC1Args.push_back("-mllvm");
+      CC1Args.push_back("-csa-wrap-asm");
+      return;
+    }
+  }
+}
+
+// This is currently only used when doing offload from x86. This link phase is
+// preparing a blob for the upcoming x86 link, which includes the blob as
+// specified by the Clang-generated offload linker script.
+//
+// Essentially, this just calls "ld [-static|-shared] -o OUTPUT INPUTS...",
+// where INPUTS are x86 object files containing the IR to be offloaded to CSA.
+// Note that the tools::gnutools::Linker nearly does what we want, except it
+// also wants to link in startup files and possibly other things that we don't
+// need.
+void CSA::Linker::ConstructJob(Compilation &C, const JobAction &JA,
+                                 const InputInfo &Output,
+                                 const InputInfoList &Inputs,
+                                 const ArgList &Args,
+                                 const char *LinkingOutput) const {
+  const auto &TC =
+      static_cast<const toolchains::CSAToolChain &>(getToolChain());
+  assert(TC.getTriple().getArch() == llvm::Triple::csa && "Wrong platform");
+
+  ArgStringList CmdArgs;
+  if (Args.hasArg(options::OPT_static))
+    CmdArgs.push_back("-static");
+  else if (Args.hasArg(options::OPT_shared))
+    CmdArgs.push_back("-shared");
+
+  CmdArgs.push_back(Args.MakeArgString("-o"));
+  CmdArgs.push_back(Args.MakeArgString(Output.getFilename()));
+
+  // This is a simple version of AddLinkerInputs. It simply includes appends
+  // all file inputs.
+  for (const auto &II : Inputs)
+    if (II.isFilename())
+      CmdArgs.push_back(II.getFilename());
+
+  const char *Exec = Args.MakeArgString(TC.GetLinkerPath());
+  C.addCommand(llvm::make_unique<Command>(JA, *this, Exec, CmdArgs, Inputs));
+}
+
+Tool *CSAToolChain::buildLinker() const {
+  return new tools::CSA::Linker(*this);
+}
+
---
+++ clang/lib/Driver/ToolChains/CSA.h
@@ -0,0 +1,64 @@
+//===--- CSA.h - CSA ToolChain Implementations --------------*- C++ -*-===//
+//
+//                     The LLVM Compiler Infrastructure
+//
+// This file is distributed under the University of Illinois Open Source
+// License. See LICENSE.TXT for details.
+//
+//===----------------------------------------------------------------------===//
+
+#ifndef LLVM_CLANG_LIB_DRIVER_TOOLCHAINS_CSA_H
+#define LLVM_CLANG_LIB_DRIVER_TOOLCHAINS_CSA_H
+
+#include "Linux.h"
+#include "clang/Driver/ToolChain.h"
+
+namespace clang {
+namespace driver {
+namespace tools {
+namespace CSA {
+
+class LLVM_LIBRARY_VISIBILITY Linker : public GnuTool {
+ public:
+   Linker(const ToolChain &TC)
+       : GnuTool("csa::Linker", "csa-ld", TC) {}
+
+   bool hasIntegratedCPP() const override { return false; }
+
+   void ConstructJob(Compilation &C, const JobAction &JA,
+                     const InputInfo &Output, const InputInfoList &Inputs,
+                     const llvm::opt::ArgList &TCArgs,
+                     const char *LinkingOutput) const override;
+};
+} // end namespace CSA
+} // end namespace tools
+
+namespace toolchains {
+
+// Tweaks to the toolchain for CSA
+class LLVM_LIBRARY_VISIBILITY CSAToolChain : public Linux {
+public:
+  CSAToolChain(const Driver &D, const llvm::Triple &Triple,
+               const llvm::opt::ArgList &Args) :
+    Linux(D, Triple, Args) {}
+
+  // CSA defaults to -fno-math-errno. This means that the math functions
+  // will have the "readnone" attribute indicating that they do not change
+  // any global state (like errno)
+  bool IsMathErrnoDefault() const override { return false; }
+
+  // If this is an OpenMP offload build, add -mllvm -csa-wrap-asm to the
+  // options
+  void addClangTargetOptions(const llvm::opt::ArgList &DriverArgs,
+                             llvm::opt::ArgStringList &CC1Args,
+                             Action::OffloadKind DeviceOffloadKind) const override;
+protected:
+  Tool *buildLinker() const override;
+
+};
+
+} // end namespace toolchains
+} // end namespace driver
+} // end namespace clang
+
+#endif // LLVM_CLANG_LIB_DRIVER_TOOLCHAINS_CSA_H
