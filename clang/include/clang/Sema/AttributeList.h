16bc58b1eb14803624a16e7e70e4267a74797104
Erich Keane <erich.keane@intel.com>
2017-09-15 08:53:54 -0700

    Fix merge conflict with 6688aa21fb
    Change-Id: Ife46bc41b318aa602fd29e26b698db0d7088a7f2
    Old/split clang commit ID: 2b1de976292b619c940fef512c563363ddec175d

--- clang/include/clang/Sema/AttributeList.h
+++ clang/include/clang/Sema/AttributeList.h
@@ -106,24 +106,15 @@ public:
     AS_Microsoft,
     /// __ptr16, alignas(...), etc.
     AS_Keyword,
-<<<<<<< HEAD
-#if INTEL_CUSTOMIZATION
-    /// #pragma ...
-    AS_Pragma,
-    /// Context-sensitive version of a keyword attribute.
-    AS_ContextSensitiveKeyword,
-#endif // INTEL_CUSTOMIZATION
-#if INTEL_SPECIFIC_CILKPLUS
-    AS_CilkKeyword
-#endif // INTEL_SPECIFIC_CILKPLUS
-=======
     /// #pragma ...
     AS_Pragma,
     // Note TableGen depends on the order above.  Do not add or change the order
     // without adding related code to TableGen/ClangAttrEmitter.cpp.
     /// Context-sensitive version of a keyword attribute.
     AS_ContextSensitiveKeyword,
->>>>>>> 6688aa21fbb5aa2bd258563e969a440fda6a1528
+#if INTEL_SPECIFIC_CILKPLUS
+    AS_CilkKeyword
+#endif // INTEL_SPECIFIC_CILKPLUS
   };
 
 private:
