3021fc6d050072cb2a03ab3dfaacd9fdb5ebb3b5
Alexey Bader <alexey.bader@intel.com>
2017-11-14 11:21:09 +0300

    [NFCI] Remove old dead scripts from cmake_utils.
    Change-Id: I90a9da894ee45fd14cbc09060da16881b421aa3b

--- opencl-intel/cmake_utils/vc_2_intel_cl_fix.bat
+++
@@ -1,4 +0,0 @@
-rem @echo off
-rem Add prefix 'i' to the command name cl->icl
-i%*
-exit /b %ERRORLEVEL
--- opencl-intel/cmake_utils/vc_2_intel_link_fix.bat
+++
@@ -1,4 +0,0 @@
-rem @echo off
-rem Add prefix 'xi' to the command name lib->xilib  link->xilink
-xi%*
-exit /b %ERRORLEVEL
