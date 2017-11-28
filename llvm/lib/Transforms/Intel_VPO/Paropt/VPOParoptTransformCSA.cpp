f7a47e7283d0034a15744e852b08a27b39903b90
Sergey Dmitriev <serguei.n.dmitriev@intel.com>
2017-11-28 10:32:00 -0800

    CSA - add spmdization region for omp parallel for with num_threads clause
    Change-Id: Ibc34ab55c9d5c4c9710ef9e61bf8434a3a546664
    Old/split llvm commit ID: 31691c3ee2b957cd62637b8bafe3c39684b4b466

--- llvm/lib/Transforms/Intel_VPO/Paropt/VPOParoptTransformCSA.cpp
+++ llvm/lib/Transforms/Intel_VPO/Paropt/VPOParoptTransformCSA.cpp
@@ -24,9 +24,9 @@ using namespace llvm::vpo;
 // Insert a pair of CSA parallel region enter and exit intrinsic calls around
 // the parallel work region body as follows
 //
-//   %region_id = call i32 @llvm.csa.parallel.region.entry(i32 2002);
+//   %region = call i32 @llvm.csa.parallel.region.entry(i32 2002);
 //   <parallel construct body>
-//   call void @llvm.csa.parallel.region.exit(i32 %region_id);
+//   call void @llvm.csa.parallel.region.exit(i32 %region);
 //
 // These calls mark the beginning and end of the parallel region for back-end.
 //
@@ -48,7 +48,7 @@ Value* VPOParoptTransform::genCSAParallelRegion(WRegionNode *W) {
   // loop intrinsics lowering pass which uses IDs starting from 1000).
   IRBuilder<> Builder(W->getEntryBBlock()->getTerminator());
   auto *UniqueID = Builder.getInt32(2000u + W->getNumber());
-  auto *RegionID = Builder.CreateCall(RegionEntry, { UniqueID }, {});
+  auto *RegionID = Builder.CreateCall(RegionEntry, { UniqueID }, "region");
 
   // And an "exit" call into the work region's exit bblock.
   Builder.SetInsertPoint(&*W->getExitBBlock()->getFirstInsertionPt());
@@ -61,13 +61,18 @@ Value* VPOParoptTransform::genCSAParallelRegion(WRegionNode *W) {
 // which annotate loop as parallel for the back-end passes.
 // After transformation the annotated loop looks as follows
 //
-//   %region_id = call i32 @llvm.csa.parallel.region.entry(i32 2002);
+//   [%spmd = call i32 @llvm.csa.spmdization.entry(<num_threads>);]*
+//   %region = call i32 @llvm.csa.parallel.region.entry(i32 2002);
 //   for (...) {
-//     %section_id = call i32 @llvm.csa.parallel.section.entry(i32 %region_id);
+//     %section = call i32 @llvm.csa.parallel.section.entry(i32 %region);
 //     <loop body>
-//     call void @llvm.csa.parallel.section.exit(i32 %section_id);
+//     call void @llvm.csa.parallel.section.exit(i32 %section);
 //   }
-//   call void @llvm.csa.parallel.region.exit(i32 %region_id);
+//   call void @llvm.csa.parallel.region.exit(i32 %region);
+//   [call void @llvm.csa.spmdization.exit(i32 %spmd);]*
+//
+// *Optional spmdization entry/exit calls are inserted only if parallel for
+// has num_threads clause.
 //
 bool VPOParoptTransform::genCSAParallelLoop(WRegionNode *W) {
   assert(isTargetCSA() && "unexpected target");
@@ -76,6 +81,24 @@ bool VPOParoptTransform::genCSAParallelLoop(WRegionNode *W) {
 
   auto *Loop = W->getLoop();
   assert(Loop->isLoopSimplifyForm());
+
+  // Annotating loop with spmdization entry/exit intrinsic calls if parallel
+  // region has num_threads clause.
+  if (auto *NumThreads = W->getNumThreads()) {
+    assert(dyn_cast<ConstantInt>(NumThreads));
+
+    auto *Entry = Intrinsic::getDeclaration(Module,
+      Intrinsic::csa_spmdization_entry);
+
+    auto *Exit = Intrinsic::getDeclaration(Module,
+      Intrinsic::csa_spmdization_exit);
+
+    IRBuilder<> Builder(W->getEntryBBlock()->getTerminator());
+    auto *SpmdID = Builder.CreateCall(Entry, { NumThreads }, "spmd");
+
+    Builder.SetInsertPoint(&*W->getExitBBlock()->getFirstInsertionPt());
+    Builder.CreateCall(Exit, { SpmdID }, {});
+  }
 
   // Insert parallel region entry/exit calls
   auto *RegionID = genCSAParallelRegion(W);
@@ -89,7 +112,7 @@ bool VPOParoptTransform::genCSAParallelLoop(WRegionNode *W) {
 
   // Insert section entry call into the beginning of the loop header.
   IRBuilder<> Builder(&*Loop->getHeader()->getFirstInsertionPt());
-  auto *SectionID = Builder.CreateCall(SectionEntry, { RegionID }, {});
+  auto *SectionID = Builder.CreateCall(SectionEntry, { RegionID }, "section");
 
   // Section exit call should be inserted right before the induction
   // variable increment in the loop latch.
