0baf44de496b0ace0cb9fdc02f5e6b5e07b29618
Tyurin Evgeniy <evgeniy.tyurin@intel.com>
2017-12-12 17:07:08 +0300

    [NFC] Remove redundant headers in CPUCompiler.cpp
    Change-Id: Iff375a4f70f2491f9b87c0f93f4551a1a0e224d0

--- opencl-intel/backend/ocl_cpu_backend/CPUCompiler.cpp
+++ opencl-intel/backend/ocl_cpu_backend/CPUCompiler.cpp
@@ -20,7 +20,6 @@ File Name:  CPUCompiler.cpp
 #include "BuiltinModules.h"
 #include "BuiltinModuleManager.h"
 #include "CPUCompiler.h"
-#include "CPUDetect.h"
 #include "ObjectCodeCache.h"
 #include "CompilationUtils.h"
 #include "cl_types.h"
@@ -33,30 +32,13 @@ File Name:  CPUCompiler.cpp
 #include "llvm/ExecutionEngine/JITEventListener.h"
 #include "llvm/ExecutionEngine/MCJIT.h"
 #include "llvm/ExecutionEngine/SectionMemoryManager.h"
-#include "llvm/IR/Argument.h"
-#include "llvm/IR/BasicBlock.h"
 #include "llvm/IR/DataLayout.h"
-#include "llvm/IR/DerivedTypes.h"
 #include "llvm/IR/Function.h"
-#include "llvm/IR/Instruction.h"
-#include "llvm/IR/Instructions.h"
-#include "llvm/IR/LLVMContext.h"
-#include "llvm/IR/Module.h"
-#include "llvm/IR/Type.h"
 #include "llvm/IR/LegacyPassManager.h"
-#include "llvm/Support/Casting.h"
+#include "llvm/IR/Module.h"
 #include "llvm/Support/CommandLine.h"
-#include "llvm/Support/FormattedStream.h"
-#include "llvm/Support/ManagedStatic.h"
 #include "llvm/Support/MemoryBuffer.h"
-#include "llvm/Support/TargetRegistry.h"
-#include "llvm/Support/TargetSelect.h"
-#include "llvm/Support/raw_ostream.h"
 
-#include "llvm/Transforms/Utils/UnifyFunctionExitNodes.h"
-
-#include <iosfwd>
-#include <sstream>
 #include <string>
 #include <vector>
 
