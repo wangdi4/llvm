68d8b345c245035750ad6eb8316c624966d4b9db
Ernesto Su <ernesto.su@intel.com>
2017-12-21 18:34:38 -0800

    CMPLRS-47177: Clang sets the "may-have-openmp-directive" attribute
    Change-Id: Ia5ac85db6d272ece386191a1ce1f2fe01b49c9ef
    Old/split clang commit ID: dc9c5e1d9f0add9779e64982e48af6786f0c0b3e

---
+++ clang/test/OpenMP/intel/may_have_openmp_dir.cpp
@@ -0,0 +1,41 @@
+// RUN: %clang_cc1 -emit-llvm -o - %s -fopenmp -fintel-compatibility -fintel-openmp-region -triple x86_64-unknown-linux-gnu | FileCheck %s
+
+void bar();
+// foo1,foo2,foo3 should have the same attributes
+//CHECK: define void @_Z4foo1v() #[[FOO1:[0-9]+]]
+void foo1() {
+  // CHECK: DIR.OMP.PARALLEL
+  #pragma omp parallel
+  {
+    bar();
+  }
+}
+
+//CHECK: define void @_Z4foo2v() #[[FOO1]]
+void foo2() {
+  // CHECK: DIR.OMP.PARALLEL
+  #pragma omp parallel for
+  for (int i=0; i<10; ++i) {
+    bar();
+  }
+}
+
+//CHECK: define void @_Z4foo3v() #[[FOO1]]
+void foo3() {
+  // CHECK: DIR.OMP.PARALLEL
+  #pragma omp parallel for
+  for (int i=0; i<10; ++i) {
+    bar();
+  }
+  #pragma omp parallel
+  {
+    bar();
+  }
+  foo2();
+}
+
+//CHECK: define void @_Z4foo4v() #[[FOO4:[0-9]+]]
+void foo4() {}
+
+//CHECK: attributes #[[FOO1]] = {{.*}}"may-have-openmp-directive"="true"
+//CHECK-NOT: attributes #[[FOO4]] = {{.*}}"may-have-openmp-directive"="true"
