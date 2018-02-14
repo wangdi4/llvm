b145c966db14a4dae525e8194201f357ce4e109b
Michael P Rice <michael.p.rice@intel.com>
2018-02-13 16:49:40 -0800

    Add QUAL.OMP.NORMALIZED.IV for normalized iteration variable on OpenMP loops.
    Change-Id: I988e2d14a18de6f8b3d837a237499a5d7eeb5c1f
    Old/split clang commit ID: b447353fbd3e5ed4852f6b98114b229d03011a66

--- clang/lib/CodeGen/intel/CGIntelStmtOpenMP.cpp
+++ clang/lib/CodeGen/intel/CGIntelStmtOpenMP.cpp
@@ -399,9 +399,16 @@ namespace CGIntelOpenMP {
       Dirs.push_back(&Directives[0]);
       return;
     }
-    for (auto &D : Directives)
+    for (auto &D : Directives) {
+      if (CurrentClauseKind == OMPC_unknown && isOpenMPLoopDirective(D.DKind)) {
+        // This is the normalized iteration variable.  Just place it on the
+        // first loop directive and return.
+        Dirs.push_back(&D);
+        return;
+      }
       if (isAllowedClauseForDirective(D.DKind, CurrentClauseKind))
         Dirs.push_back(&D);
+    }
   }
 
   void OpenMPCodeOutliner::startDirectiveIntrinsicSet(StringRef Begin,
@@ -447,29 +454,41 @@ namespace CGIntelOpenMP {
     emitClause(llvm::Intrinsic::intel_directive_qual_opndlist);
   }
 
-  void OpenMPCodeOutliner::emitImplicit(Expr *E, OpenMPClauseKind K) {
+  void OpenMPCodeOutliner::emitImplicit(Expr *E, ImplicitClauseKind K) {
     switch (K) {
-    case OMPC_private:
-      addArg("QUAL.OMP.PRIVATE"); break;
-    case OMPC_firstprivate:
-      addArg("QUAL.OMP.FIRSTPRIVATE"); break;
-    case OMPC_shared:
-      addArg("QUAL.OMP.SHARED"); break;
-    case OMPC_map:
-      addArg("QUAL.OMP.MAP.TOFROM"); break;
+    case ICK_private:
+      CurrentClauseKind = OMPC_private;
+      addArg("QUAL.OMP.PRIVATE");
+      break;
+    case ICK_firstprivate:
+      CurrentClauseKind = OMPC_firstprivate;
+      addArg("QUAL.OMP.FIRSTPRIVATE");
+      break;
+    case ICK_shared:
+      CurrentClauseKind = OMPC_shared;
+      addArg("QUAL.OMP.SHARED");
+      break;
+    case ICK_map_tofrom:
+      CurrentClauseKind = OMPC_map;
+      addArg("QUAL.OMP.MAP.TOFROM");
+      break;
+    case ICK_normalized_iv:
+      CurrentClauseKind = OMPC_unknown;
+      addArg("QUAL.OMP.NORMALIZED.IV");
+      break;
     default:
       llvm_unreachable("Clause not allowed");
     }
-    CurrentClauseKind = K;
     addArg(E);
     emitListClause();
     CurrentClauseKind = OMPC_unknown;
   }
 
-  void OpenMPCodeOutliner::emitImplicit(const VarDecl *VD, OpenMPClauseKind K) {
-    // OMPC_unknown is used when we do not want a variable to appear in any
+  void OpenMPCodeOutliner::emitImplicit(const VarDecl *VD,
+                                        ImplicitClauseKind K) {
+    // ICK_unknown is used when we do not want a variable to appear in any
     // clause list, so just return when we see it.
-    if (K == OMPC_unknown)
+    if (K == ICK_unknown)
       return;
 
     // We don't want this DeclRefExpr to generate entries in the Def/Ref lists,
@@ -510,16 +529,16 @@ namespace CGIntelOpenMP {
       }
       if (VarDefs.find(VD) != VarDefs.end()) {
         // Defined in the region: private
-        emitImplicit(VD, OMPC_private);
+        emitImplicit(VD, ICK_private);
       } else if (DKind == OMPD_target) {
         if (!VD->getType()->isScalarType() ||
             Directive.hasClausesOfKind<OMPDefaultmapClause>())
-          emitImplicit(VD, OMPC_map);
+          emitImplicit(VD, ICK_map_tofrom);
         else
-          emitImplicit(VD, OMPC_firstprivate);
+          emitImplicit(VD, ICK_firstprivate);
       } else if (DKind != OMPD_simd && DKind != OMPD_for) {
         // Referenced but not defined in the region: shared
-        emitImplicit(VD, OMPC_shared);
+        emitImplicit(VD, ICK_shared);
       }
     }
   }
@@ -1211,22 +1230,22 @@ namespace CGIntelOpenMP {
       for (auto *E : LoopDir->counters()) {
         auto *PVD = cast<VarDecl>(cast<DeclRefExpr>(E)->getDecl());
         if (isOpenMPSimdDirective(DKind))
-          ImplicitMap.insert(std::make_pair(PVD, OMPC_unknown));
+          ImplicitMap.insert(std::make_pair(PVD, ICK_unknown));
         else
-          ImplicitMap.insert(std::make_pair(PVD, OMPC_private));
+          ImplicitMap.insert(std::make_pair(PVD, ICK_private));
       }
       auto IVExpr = cast<DeclRefExpr>(LoopDir->getIterationVariable());
       auto IVDecl = cast<VarDecl>(IVExpr->getDecl());
-      ImplicitMap.insert(std::make_pair(IVDecl, OMPC_unknown));
+      ImplicitMap.insert(std::make_pair(IVDecl, ICK_normalized_iv));
       if (isOpenMPWorksharingDirective(DKind) ||
           isOpenMPTaskLoopDirective(DKind) ||
           isOpenMPDistributeDirective(DKind)) {
         auto LBExpr = cast<DeclRefExpr>(LoopDir->getLowerBoundVariable());
         auto LBDecl = cast<VarDecl>(LBExpr->getDecl());
-        ImplicitMap.insert(std::make_pair(LBDecl, OMPC_firstprivate));
+        ImplicitMap.insert(std::make_pair(LBDecl, ICK_firstprivate));
         auto UBExpr = cast<DeclRefExpr>(LoopDir->getUpperBoundVariable());
         auto UBDecl = cast<VarDecl>(UBExpr->getDecl());
-        ImplicitMap.insert(std::make_pair(UBDecl, OMPC_firstprivate));
+        ImplicitMap.insert(std::make_pair(UBDecl, ICK_firstprivate));
       }
     }
   }
