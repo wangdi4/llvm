bd1f56b5ae6a4f136044748fccc3e2250a79c81c
Andrew Savonichev <andrew.savonichev@intel.com>
2018-06-06 06:47:17 +0300

    [FPGA] Define __fpga_reg as an alias to __builtin_fpga_reg
    PSG requested that __builtin_fpga_reg should retain its old name for
    compatibility purposes.
    
    Change-Id: Iefa7e6a7be8438476ba6797d60dec255d729a680
    Reviewed-on: https://git-amr-2.devtools.intel.com/gerrit/132329
    Reviewed-by: Savonichev, Andrew <andrew.savonichev@intel.com>
    Tested-by: Savonichev, Andrew <andrew.savonichev@intel.com>
    Old/split clang commit ID: 49f0757f015ce7038b7ea9a7b302127f17e6255c

--- clang/lib/Basic/Targets/X86.cpp
+++ clang/lib/Basic/Targets/X86.cpp
@@ -1274,6 +1274,11 @@ void X86TargetInfo::getTargetDefines(const LangOptions &Opts,
 
   if (HasFloat128)
     Builder.defineMacro("__SIZEOF_FLOAT128__", "16");
+
+#if INTEL_CUSTOMIZATION
+  if (getTriple().getEnvironment() == llvm::Triple::IntelFPGA)
+    Builder.defineMacro("__fpga_reg", "__builtin_fpga_reg");
+#endif // INTEL_CUSTOMIZATION
 }
 
 bool X86TargetInfo::isValidFeatureName(StringRef Name) const {
