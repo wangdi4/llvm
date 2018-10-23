5a046a8007c47cb7a77a224b53e097abf0f2f68b
Dmitry Sidorov <dmitry.sidorov@intel.com>
2018-10-23 13:26:55 +0300

    [OpenCL][FPGA] Define INTELFPGA_CL macro
    INTELFPGA_CL was defined for SPIR and X86 targets.
    
    Change-Id: I0b505229e368577986ff02f554a0bc2b007896a0
    Reviewed-on: https://git-amr-2.devtools.intel.com/gerrit/155199
    Reviewed-by: Savonichev, Andrew <andrew.savonichev@intel.com>
    Tested-by: xmain_checkin <lab_iclt@iclgerrit.sc.intel.com>
    Reviewed-by: Maslov, Sergey V <sergey.v.maslov@intel.com>
    Old/split clang commit ID: 032a0aeb843fa479d1d0ccc1eded2d0aec61427b

--- clang/lib/Basic/Targets/X86.cpp
+++ clang/lib/Basic/Targets/X86.cpp
@@ -1281,8 +1281,10 @@ void X86TargetInfo::getTargetDefines(const LangOptions &Opts,
     Builder.defineMacro("__SIZEOF_FLOAT128__", "16");
 
 #if INTEL_CUSTOMIZATION
-  if (getTriple().getEnvironment() == llvm::Triple::IntelFPGA)
+  if (getTriple().getEnvironment() == llvm::Triple::IntelFPGA) {
     Builder.defineMacro("__fpga_reg", "__builtin_fpga_reg");
+    Builder.defineMacro("INTELFPGA_CL", "191");
+  }
 #endif // INTEL_CUSTOMIZATION
 }
 
