7024ec7f8f3cf3830ea7a4a3d08be8f927454298
Chang-Sun Lin Jr <chang-sun.lin.jr@intel.com>
2018-12-11 00:32:06 -0800

    7618: Recognize loops with single results which are only used to test against zero.
    Change-Id: I10910b9a96d8cbd921974e7810462b97f4a158c0
    Reviewed-on: https://git-amr-2.devtools.intel.com/gerrit/164574
    Reviewed-by: Kaylor, Andrew <andrew.kaylor@intel.com>
    Tested-by: xmain_checkin <lab_iclt@iclgerrit.sc.intel.com>
    Tested-by: zperf_checkin_xmain <lab_iclz@iclgerrit.sc.intel.com>
    Reviewed-by: Maslov, Sergey V <sergey.v.maslov@intel.com>
    Old/split llvm commit ID: 622772899454c46ce8d09942f82c612cebf21d6b

--- llvm/lib/Transforms/Scalar/LoopDeletion.cpp
+++ llvm/lib/Transforms/Scalar/LoopDeletion.cpp
@@ -36,6 +36,202 @@ enum class LoopDeletionResult {
   Deleted,
 };
 
+#if INTEL_CUSTOMIZATION
+// Analyze a loop for this pattern:
+//
+// while.header:
+//   %src = phi (%ptr , %initial)
+//   %ptr = getelementptr inbounds Ty, Ty* %src, CONST
+//   ...
+//   br %condition, %while.header, %loop.exit
+// loop.exit:
+//   PN = phi %ptr
+//
+// "ptr" is a simple pointer-type induction variable that is incremented by
+// a constant amount on every loop iteration, and initialized outside the loop.
+//
+// "PN" is the phi at the loop exit for the loop result. It must have only one
+// incoming value, which implies that the loop body dominates the exit, and
+// the loop has  executed at least one time.
+// "inc" is set to the integer increment value from the GEP.
+// The function returns the initial value "%initial".
+// If the loop does not fit this pattern, nullptr is returned.
+static Value *getInitialValueAndInc(Loop *L, PHINode *PN, uint64_t &inc,
+                                    const DataLayout &DL) {
+  PHINode *HeaderPhi = nullptr;
+  Instruction *UpdateInst = nullptr;
+  int64_t Increment = 0;
+
+  // exit phi must have 1 arg
+  if (PN->getNumIncomingValues() > 1)
+    return nullptr;
+  Value *ValInLoop = PN->getIncomingValue(0);
+
+  // Since this is pointer type, we're looking for a GEP increment.
+  if (auto *G = dyn_cast<GetElementPtrInst>(ValInLoop)) {
+    if (G->getNumIndices() != 1 || !G->hasAllConstantIndices())
+      return nullptr;
+    if (ConstantInt *IdxCI = dyn_cast<ConstantInt>(G->getOperand(1))) {
+      int64_t IdxVal = IdxCI->getSExtValue();
+      // Increment = sizeof(Ty) * index
+      Increment = DL.getTypeAllocSize(G->getResultElementType()) * IdxVal;
+      if (Increment <= 0)
+        return nullptr;
+      HeaderPhi = dyn_cast<PHINode>(G->getPointerOperand());
+      UpdateInst = G;
+    }
+  }
+
+  if (UpdateInst == nullptr)
+    return nullptr;
+
+  // No other updates to the induction pointer are allowed.
+  // There should be a phi at the top of the loop with the GEP and the initial
+  // value.
+  if (!HeaderPhi || HeaderPhi->getParent() != L->getHeader() ||
+      HeaderPhi->getNumIncomingValues() != 2)
+    return nullptr;
+
+  Value *InitVal = nullptr;
+  if (HeaderPhi->getIncomingValue(0) == UpdateInst)
+    InitVal = HeaderPhi->getIncomingValue(1);
+  else if (HeaderPhi->getIncomingValue(1) == UpdateInst)
+    InitVal = HeaderPhi->getIncomingValue(0);
+  else
+    return nullptr;
+
+  // Make sure the initial value is really outside the loop.
+  if (!L->isLoopInvariant(InitVal))
+    return nullptr;
+
+  inc = Increment;
+  return InitVal;
+}
+
+// Remove any bitcasts and ptrtoint instructions defining V, returning the
+// original source (or V itself).
+static Value *stripCast(Value *V) {
+  if (!V)
+    return nullptr;
+  // backwards walk of sequence: [bitcast], [bitcast],...[ptrtoint]
+  if (auto *ConvInst = dyn_cast<PtrToIntInst>(V))
+    V = ConvInst->getOperand(0);
+  return V->stripPointerCasts();
+}
+
+// Used by the array "len" optimization below.
+// Given a Value "len" that is the result of a computation with minimum
+// value "MinVal", we may be able to prove that "len" can be replaced
+// with the constant "MinVal".
+// One case is if "len" is ultimately used in a zero-compare
+// instruction, and no other instructions along the way, can make "len" zero.
+//
+// "I" is the use of "len", where we start walking the uses.
+// "MinVal" is the constant minimum value of "len".
+// "depth" is a recursion limiter (capped at 2 levels).
+//
+// Example:
+//  %sub.ptr.sub.i = sub i64 %sub.ptr.lhs.cast.i, %sub.ptr.rhs.cast.i
+//  %9 = lshr exact i64 %sub.ptr.sub.i, 1
+//  %conv2.i = trunc i64 %9 to i32
+//  %retval.0.i98 = phi i32 [ %conv2.i, %while.end.i ], [ 0, %lor.lhs.false.i ],
+//                            [ 0, %if.end ]
+//  %13 = or i32 %retval.0.i111, %retval.0.i98
+//  %14 = icmp eq i32 %13, 0
+//  %tobool5 = icmp ne i32 %retval.0.i98, 0
+//
+// If we can prove that the subtraction must be at least 2,
+// we can replace the subtraction with the constant 2, without changing
+// the result of the two compares.
+static bool AllUsesCmpZero(Instruction *I, uint64_t MinVal,
+                           const DataLayout &DL, uint32_t depth) {
+  bool found = false;
+  if (!I)
+    return false;
+
+  // Walk the uses of I until we hit a compare.
+  // We use a while-loop to process a chain of single-uses.
+  // Then we handle multiple-uses with recursion.
+  while (MinVal > 0) {
+    CmpInst *Cmp = dyn_cast<CmpInst>(I);
+    if (Cmp) {
+      // Found a compare. Must be eq 0 or ne 0.
+      if ((Cmp->getPredicate() != ICmpInst::ICMP_EQ) &&
+          (Cmp->getPredicate() != ICmpInst::ICMP_NE))
+        break;
+      ConstantInt *CmpVal = dyn_cast<ConstantInt>(Cmp->getOperand(1));
+      if (!CmpVal || !CmpVal->isZero())
+        break;
+      found = true;
+      break;
+    }
+
+    // Process other instructions that we can prove won't make MinVal zero.
+    ConstantInt *ShVal = nullptr;
+    uint64_t TruncBits = 0;
+    unsigned Opcode = I->getOpcode();
+    switch (Opcode) {
+    case Instruction::LShr:
+      if ((ShVal = dyn_cast<ConstantInt>(I->getOperand(1))))
+        MinVal = MinVal >> ShVal->getZExtValue();
+      else
+        MinVal = 0;
+      break;
+    case Instruction::AShr:
+      if ((ShVal = dyn_cast<ConstantInt>(I->getOperand(1))))
+        MinVal = (int64_t)MinVal >> ShVal->getZExtValue();
+      else
+        MinVal = 0;
+      break;
+    case Instruction::Shl:
+      if ((ShVal = dyn_cast<ConstantInt>(I->getOperand(1))) &&
+          I->hasNoUnsignedWrap())
+        MinVal = MinVal << ShVal->getZExtValue();
+      else
+        MinVal = 0;
+      break;
+    case Instruction::Trunc:
+      TruncBits = DL.getTypeStoreSize(I->getType());
+      MinVal = MinVal & ((1ULL << TruncBits) - 1);
+      break;
+    case Instruction::Or:
+      // OR will not change nonzero to zero.
+      break;
+    case Instruction::PHI:
+      // Tracing PHI in forward direction; no-op.
+      break;
+    default:
+      // Anything else, we bail out.
+      MinVal = 0;
+      break;
+    }
+    if (!I->hasOneUse())
+      break; // multiple uses are OK. Recursive loop is below.
+
+    // One use, look at the next inst in the chain.
+    I = I->user_back();
+  }
+
+  // Handle instructions with multiple uses by searching each use recursively.
+  // Each use must end in a zero-compare with the same allowable instructions.
+  if (!found && MinVal > 0 && !I->hasOneUse() && depth < 2) {
+    // depth check limits searching to 4 paths: more hits are very unlikely
+    for (auto UIter = I->use_begin(), EIter = I->use_end();
+         UIter != EIter;) { // inc is in loop
+      Use &U = *UIter++;
+      Instruction *User = dyn_cast<Instruction>(U.getUser());
+      if (User == nullptr) {
+        break;
+      }
+      found = AllUsesCmpZero(User, MinVal, DL, depth + 1);
+      if (!found)
+        break;
+    }
+  }
+  return found;
+}
+#endif // INTEL_CUSTOMIZATION
+
 /// Determines if a loop is dead.
 ///
 /// This assumes that we've already checked for unique exit and exiting blocks,
@@ -43,6 +239,9 @@ enum class LoopDeletionResult {
 static bool isLoopDead(Loop *L, ScalarEvolution &SE,
                        SmallVectorImpl<BasicBlock *> &ExitingBlocks,
                        BasicBlock *ExitBlock, bool &Changed,
+#if INTEL_CUSTOMIZATION
+                       bool &NotInfinite,
+#endif // INTEL_CUSTOMIZATION
                        BasicBlock *Preheader) {
   // Make sure that all PHI entries coming from the loop are loop invariant.
   // Because the code is in LCSSA form, any values used outside of the loop
@@ -68,6 +267,122 @@ static bool isLoopDead(Loop *L, ScalarEvolution &SE,
 
     if (Instruction *I = dyn_cast<Instruction>(incoming))
       if (!L->makeLoopInvariant(I, Changed, Preheader->getTerminator())) {
+
+#if INTEL_CUSTOMIZATION
+        // p = pstart;
+        // {
+        //   p++; // actually p += sizeof(*p) in bytes
+        // } while (*p)
+        // pp = PHI(p) // 1 arg only
+        // len = pp-pstart;
+        // if (len != 0)
+        //
+        // If "p" and "len" are only used to make the computation above, the
+        // loop is dead. "len" is guaranteed to be nonzero, as pointer
+        // wraparound is undefined.
+        // We can replace p with undef, and len with sizeof(*p), without
+        // changing the code behavior. The loop deleter will then remove the
+        // loop, if the loop has no side effects, all other results are
+        // invariant, etc. (the usual criteria)
+        AllEntriesInvariant = false;
+        if (!I->getType()->isPointerTy())
+          // cannot be generalized to integers without knowing if the loop
+          // will terminate.
+          break;
+
+        uint64_t MinVal = 0;
+        assert(P.getModule() != nullptr && "Null module found!");
+        const DataLayout &DL = P.getModule()->getDataLayout();
+
+        // First, analyze the loop and get the initial value "pstart" and
+        // the real pointer increment value.
+        Value *Initial = getInitialValueAndInc(L, &P, MinVal, DL);
+        if (Initial == nullptr || MinVal == 0)
+          break;
+
+        // Find the instruction defining "len" above, by starting at the phi
+        // at the loop exit, and walking its uses.
+        // getInitialValueandInc() checks that the phi has only 1 incoming
+        // x value.
+        // This is important, as the "len" computation must be dominated
+        // by the loop body so that we are guaranteed the loop has executed
+        // at least once.
+        Instruction *SubResult = nullptr;
+        Instruction *CurrInst = &P;
+        while (CurrInst->hasOneUse()) {
+          // pattern may be:
+          //   %P = phi [x]
+          //   %1 = ptrtoint %P
+          //   %2 = ptrtoint %Initial
+          //   %len = sub %1 %2
+          // Only ptrtoint and bitcast are allowed.
+          Instruction *User = CurrInst->user_back();
+          if (User->getOpcode() == Instruction::Sub) {
+            // "len" must be as above: operand 0 is directly derived from
+            // the phi, and operand 1 must be the initial value of "p".
+            if (User->getOperand(0) != CurrInst)
+              break;
+            // The other sub operand may have some ptrtoints that we need
+            // to get through.
+            if (stripCast(User->getOperand(1)) == Initial)
+              SubResult = User;
+            CurrInst = User;
+            break;
+          }
+          if (!isa<BitCastInst>(User) && !isa<PtrToIntInst>(User))
+            break;
+          CurrInst = User;
+        }
+
+        if (SubResult == nullptr)
+          break;
+
+        LLVM_DEBUG(
+            dbgs()
+                << "Loop result is always positive, looking for zero test.\n";
+            SubResult->print(dbgs()); dbgs() << "\n");
+
+        if (!CurrInst->hasOneUse())
+          break;
+
+        assert(CurrInst == SubResult && "Incorrect subtract pattern match");
+
+        // CurrInst is now the subtraction resulting in "len".
+        // We need to prove that "len" is only used to test zero/nonzero.
+        // Some other operands may be applied to "len" before the compare.
+        // As long as these operations do not change the zero/nonzero property
+        // of "len", they can be ignored. More detail in AllUsesCmpZero().
+        CurrInst = CurrInst->user_back();
+        bool AllCmp = AllUsesCmpZero(CurrInst, MinVal, DL, 0);
+
+        if (AllCmp) {
+          // Replace "len" with the loop increment value (the minimum
+          // difference if the loop only iterated one time). The subtraction
+          // is now dead.
+          LLVM_DEBUG(dbgs() << "Loop result only used in zero-test, can "
+                            << "replace with constant " << MinVal << "\n");
+
+          Value *MinValIR =
+              ConstantInt::get(SubResult->getType(), MinVal, false);
+          SubResult->replaceAllUsesWith(MinValIR);
+
+          // I is the result incoming from the loop, which is the phi
+          // argument "p" in the example. Replace it with a constant, to
+          // force "p" to be dead. This allows the loop to be deleted if
+          // suitable.
+          I->replaceUsesOutsideBlock(
+              UndefValue::get(cast<PointerType>(I->getType())), I->getParent());
+          // Pointer increments can't roll over, tell the caller.
+          NotInfinite = true;
+          // This doesn't mean "all" entries; we will still continue to
+          // check the other
+          // live-out values of the loop and reset this flag if needed.
+          AllEntriesInvariant = true;
+
+          continue; // keep checking other live values
+        }
+#endif // INTEL_CUSTOMIZATION
+
         AllEntriesInvariant = false;
         break;
       }
@@ -184,7 +499,11 @@ static LoopDeletionResult deleteLoopIfDead(Loop *L, DominatorTree &DT,
   }
   // Finally, we have to check that the loop really is dead.
   bool Changed = false;
-  if (!isLoopDead(L, SE, ExitingBlocks, ExitBlock, Changed, Preheader)) {
+#if INTEL_CUSTOMIZATION
+  bool NotInfinite = false;
+  if (!isLoopDead(L, SE, ExitingBlocks, ExitBlock, Changed, NotInfinite,
+                  Preheader)) {
+#endif // INTEL_CUSTOMIZATION
     LLVM_DEBUG(dbgs() << "Loop is not invariant, cannot delete.\n");
     return Changed ? LoopDeletionResult::Modified
                    : LoopDeletionResult::Unmodified;
@@ -193,7 +512,9 @@ static LoopDeletionResult deleteLoopIfDead(Loop *L, DominatorTree &DT,
   // Don't remove loops for which we can't solve the trip count.
   // They could be infinite, in which case we'd be changing program behavior.
   const SCEV *S = SE.getMaxBackedgeTakenCount(L);
-  if (isa<SCEVCouldNotCompute>(S)) {
+#if INTEL_CUSTOMIZATION
+  if (isa<SCEVCouldNotCompute>(S) && !NotInfinite) {
+#endif // INTEL_CUSTOMIZATION
     LLVM_DEBUG(dbgs() << "Could not compute SCEV MaxBackedgeTakenCount.\n");
     return Changed ? LoopDeletionResult::Modified
                    : LoopDeletionResult::Unmodified;
---
+++ llvm/test/Transforms/LoopDeletion/intel-loop-delete.ll
@@ -0,0 +1,87 @@
+; RUN: opt < %s -loop-deletion -S | FileCheck %s
+
+; CHECK-NOT: while.body.test1
+; CHECK: sub.ptr.div = lshr {{.*}} i64 2
+; CHECK-NOT: while.body.test2
+; CHECK: sub.ptr2.div = lshr {{.*}} i64 2
+
+; The only live value out of the while.body.test1 loop is "%p.0". Some arithmetic is applied
+; and then it is used to compute
+; the icmp eq 0 instruction.
+; We can prove that %p.0 must always be > 0 exiting the loop, and the specific value does not
+; matter, even through the following computations.
+; The loop can be deleted and replaced with a constant result for %p.0.
+
+define zeroext i1 @_Z3fooPs(i16* %orig_p) local_unnamed_addr {
+entry:
+  br label %while.body.test1
+
+while.body.test1:
+  %orig_p.pn = phi i16* [ %orig_p, %entry ], [ %p.0, %while.body.test1 ]
+  %p.0 = getelementptr inbounds i16, i16* %orig_p.pn, i64 1
+  %0 = load i16, i16* %p.0, align 2
+  %tobool = icmp eq i16 %0, 0
+  br i1 %tobool, label %while.end, label %while.body.test1
+
+while.end:
+  %sub.ptr.lhs.cast = ptrtoint i16* %p.0 to i64 ; %p.0 must be at least %orig_p+2
+  %sub.ptr.rhs.cast = ptrtoint i16* %orig_p to i64
+  %sub.ptr.sub = sub i64 %sub.ptr.lhs.cast, %sub.ptr.rhs.cast ; result must be > 2
+  %sub.ptr.div = lshr exact i64 %sub.ptr.sub, 1
+  %conv = trunc i64 %sub.ptr.div to i32
+  %cmp = icmp eq i32 %conv, 0                 ; this eq is always false
+  br i1 %cmp, label %if.end, label %if.then
+
+if.then:                                          ; preds = %while.end
+  br label %cleanup
+
+if.end:                                           ; preds = %while.end
+  br label %cleanup
+
+cleanup:                                          ; preds = %if.end, %if.then
+  %retval.0 = phi i1 [ true, %if.then ], [ false, %if.end ]
+  ret i1 %retval.0
+}
+
+
+; More complicated test, with some phi nodes and multiple uses of the loop result.
+; All the paths out of the loop must have an icmp eq|ne 0.
+
+define zeroext i1 @_Z3foo2Ps(i16* %orig_p) local_unnamed_addr {
+entry:
+  br label %while.body.test2
+
+while.body.test2:
+  %orig_p.pn = phi i16* [ %orig_p, %entry ], [ %p.0, %while.body.test2 ]
+  %p.0 = getelementptr inbounds i16, i16* %orig_p.pn, i64 1
+  %0 = load i16, i16* %p.0, align 2
+  %tobool = icmp eq i16 %0, 0
+  br i1 %tobool, label %while.end, label %while.body.test2
+
+while.end:
+  %join = phi i16* [ %p.0, %while.body.test2 ]
+  %sub.ptr.lhs.cast = ptrtoint i16* %join to i64
+  %sub.ptr.rhs.cast = ptrtoint i16* %orig_p to i64
+  %sub.ptr.sub = sub i64 %sub.ptr.lhs.cast, %sub.ptr.rhs.cast
+  %sub.ptr2.div = lshr exact i64 %sub.ptr.sub, 1
+  %conv = trunc i64 %sub.ptr2.div to i32
+  br label %next.block
+
+next.block:
+  %join2 = phi i32 [ %conv, %while.end ]
+  %orr = or i32 %join2, 4
+  %cmp = icmp eq i32 %orr, 0
+  br i1 %cmp, label %if.end, label %if.then
+
+if.then:
+  %cmp2 = icmp ne i32 %join2, 0
+  br i1 %cmp2, label %if.end, label %cleanup
+
+if.end:
+  br label %cleanup
+
+cleanup:
+  %retval.0 = phi i1 [ true, %if.then ], [ false, %if.end ]
+  ret i1 %retval.0
+}
+
