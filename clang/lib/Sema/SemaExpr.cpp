7644fa2fd7862a92c5bc3bbf9574563c4482680d
Alexey Sachkov <alexey.sachkov@intel.com>
2018-12-12 13:42:09 +0300

    Revert "Disabled diagnostic for taking address of a pipe."
    This reverts commit f2d5ed4ed8bebca68ea0ab06a4f7e2c33fd6a838.
    
    OpenCL CPU built-ins were refactored and this hack is not needed anymore
    
    Change-Id: Ia0f4d6c3dac4aa062f013b21226041f93bd3a40b
    Reviewed-on: https://git-amr-2.devtools.intel.com/gerrit/164021
    Tested-by: xmain_checkin <lab_iclt@iclgerrit.sc.intel.com>
    Reviewed-by: Sidorov, Dmitry <dmitry.sidorov@intel.com>
    Reviewed-by: Savonichev, Andrew <andrew.savonichev@intel.com>
    Reviewed-by: Maslov, Sergey V <sergey.v.maslov@intel.com>
    Old/split clang commit ID: fe7afc1eb8adccdbe499a4b53334704ed44709e7

--- clang/lib/Sema/SemaExpr.cpp
+++ clang/lib/Sema/SemaExpr.cpp
@@ -13257,12 +13257,11 @@ ExprResult Sema::CreateBuiltinUnaryOp(SourceLocation OpLoc,
     QualType Ty = InputExpr->getType();
     // The only legal unary operation for atomics is '&'.
     if ((Opc != UO_AddrOf && Ty->isAtomicType()) ||
-#if INTEL_CUSTOMIZATION
-    // OpenCL special types - image, sampler and blocks are to be used
-    // only with a builtin functions and therefore should be disallowed here.
-        (Ty->isImageType() || Ty->isSamplerT() || Ty->isBlockPointerType() ||
-         Ty->isChannelType())) {
-#endif // INTEL_CUSTOMIZATION
+        // OpenCL special types - image, sampler, pipe, and blocks are to be
+        // used only with a builtin functions and therefore should be disallowed
+        // here.
+        (Ty->isImageType() || Ty->isSamplerT() || Ty->isPipeType() ||
+         Ty->isBlockPointerType() || Ty->isChannelType())) { // INTEL
       return ExprError(Diag(OpLoc, diag::err_typecheck_unary_expr)
                        << InputExpr->getType()
                        << Input.get()->getSourceRange());
--- clang/test/SemaOpenCL/invalid-pipes-cl2.0.cl
+++ clang/test/SemaOpenCL/invalid-pipes-cl2.0.cl
@@ -22,10 +22,8 @@ void test4() {
 void test5(pipe int p) {
   p+p; // expected-error{{invalid operands to binary expression ('read_only pipe int' and 'read_only pipe int')}}
   p=p; // expected-error{{invalid operands to binary expression ('read_only pipe int' and 'read_only pipe int')}}
-// if INTEL_CUSTOMIZATION
-  (void)&p;
-  (void)*p; // expected-error{{indirection requires pointer operand ('read_only pipe int' invalid)}}
-// endif INTEL_CUSTOMIZATION
+  &p; // expected-error{{invalid argument type 'read_only pipe int' to unary expression}}
+  *p; // expected-error{{invalid argument type 'read_only pipe int' to unary expression}}
 }
 
 typedef pipe int pipe_int_t;
