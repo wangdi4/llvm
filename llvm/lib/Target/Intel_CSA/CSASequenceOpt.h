c93b72db245fba124ff9fcda80c93b557451568f
Vyacheslav Zakharin <vyacheslav.p.zakharin@intel.com>
2019-01-24 18:06:41 -0800

    [NFC] Copyright dates update for changes from https://git-amr-2.devtools.intel.com/gerrit/#/c/167512/-1..7
    Change-Id: I49985845583559b039f5a32151c753e5a77d8d46
    Reviewed-on: https://git-amr-2.devtools.intel.com/gerrit/169608
    Tested-by: xmain_checkin <lab_iclt@iclgerrit.sc.intel.com>
    Reviewed-by: Kaylor, Andrew <andrew.kaylor@intel.com>
    Reviewed-by: Guggilla, Satish <satish.guggilla@intel.com>
    Old/split llvm commit ID: 343d6a99911367a994e0b89f0e9bda543c2640ae

--- llvm/lib/Target/Intel_CSA/CSASequenceOpt.h
+++ llvm/lib/Target/Intel_CSA/CSASequenceOpt.h
@@ -1,6 +1,6 @@
 //===-- CSASequenceOpt.h - CSA structures and methods for sequence opt.----===//
 //
-// Copyright (C) 2017-2018 Intel Corporation. All rights reserved.
+// Copyright (C) 2017-2019 Intel Corporation. All rights reserved.
 //
 // The information and source code contained herein is the exclusive
 // property of Intel Corporation and may not be disclosed, examined
