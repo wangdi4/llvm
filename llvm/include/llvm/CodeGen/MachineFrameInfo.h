0a5d29b7e263370704ae607f29528d4789f699f6
Andy Kaylor <andrew.kaylor@intel.com>
2019-02-08 11:56:38 -0800

    [WinEH] Allocate a unqiue stack slot for xmm regs in funclets
    Currently funclets reuse the same stack slots that are used in the parent
    function for saving callee-saved xmm registers. If the parent function modifies
    a callee-saved xmm register before an excpetion is thrown, the catch handler
    will overwrite the original saved value.
    
    This patch allocates a second stack slot to be used in the EH funclets for
    saving these same registers. Long term, it would be better to determine actual
    CSR use by the funclets and only allocate the extra space when needed.
    
    This patch was submitted for community review:
    
    https://reviews.llvm.org/D57970
    
    A problem was pointed out there that the automatic stack unwinding
    when an exception is thrown from inside a handler would result in the
    xmm CSRs being restored from the wrong location because the .seh_savexmm
    directive that we are producing is relative to the parent frame rather
    than the funclet frame. As I commented there, I believe that this is
    only going to be a problem in a very few cases and in all cases my
    proposed patch is better than the existing behavior.
    
    Change-Id: I5efe3f0f2b16214cade33778820e9e8218caae67
    Reviewed-on: https://git-amr-2.devtools.intel.com/gerrit/171980
    Tested-by: zperf_checkin_xmain <lab_iclz@iclgerrit.sc.intel.com>
    Tested-by: xmain_checkin <lab_iclt@iclgerrit.sc.intel.com>
    Reviewed-by: Pengfei Wang <pengfei.wang@intel.com>
    Reviewed-by: Topper, Craig <craig.topper@intel.com>
    Reviewed-by: Zakharin, Vyacheslav P <vyacheslav.p.zakharin@intel.com>
    Old/split llvm commit ID: 17904fd9ce210956439104d789e1010b37d1070d

--- llvm/include/llvm/CodeGen/MachineFrameInfo.h
+++ llvm/include/llvm/CodeGen/MachineFrameInfo.h
@@ -36,6 +36,15 @@ class CalleeSavedInfo {
     int FrameIdx;
     unsigned DstReg;
   };
+
+#if INTEL_CUSTOMIZATION
+  // CMPLRLLVM - 7519: These changes are based on a preliminary workaround.
+  // When a long term fix is checked into llorg, it will take precedence.
+  // Windows exception handling requires callee saved registers to be saved
+  // for EH funclets separately from the main function.
+  int FuncletFrameIdx;
+#endif // INTEL_CUSTOMIZATION
+
   /// Flag indicating whether the register is actually restored in the epilog.
   /// In most cases, if a register is saved, it is also restored. There are
   /// some situations, though, when this is not the case. For example, the
@@ -55,7 +64,12 @@ class CalleeSavedInfo {
 
 public:
   explicit CalleeSavedInfo(unsigned R, int FI = 0)
-  : Reg(R), FrameIdx(FI), Restored(true), SpilledToReg(false) {}
+#if INTEL_CUSTOMIZATION
+    // CMPLRLLVM - 7519: These changes are based on a preliminary workaround.
+    // When a long term fix is checked into llorg, it will take precedence.
+    : Reg(R), FrameIdx(FI), FuncletFrameIdx(0), Restored(true),
+        SpilledToReg(false) {}
+#endif // INTEL_CUSTOMIZATION
 
   // Accessors.
   unsigned getReg()                        const { return Reg; }
@@ -69,6 +83,14 @@ public:
     DstReg = SpillReg;
     SpilledToReg = true;
   }
+
+#if INTEL_CUSTOMIZATION
+  // CMPLRLLVM - 7519: These changes are based on a preliminary workaround.
+  // When a long term fix is checked into llorg, it will take precedence.
+  int getFuncletFrameIdx() const { return FuncletFrameIdx; }
+  void setFuncletFrameIdx(int FI) { FuncletFrameIdx = FI; }
+#endif // INTEL_CUSTOMIZATION
+
   bool isRestored()                        const { return Restored; }
   void setRestored(bool R)                       { Restored = R; }
   bool isSpilledToReg()                    const { return SpilledToReg; }
--- llvm/lib/Target/X86/X86FrameLowering.cpp
+++ llvm/lib/Target/X86/X86FrameLowering.cpp
@@ -2016,6 +2016,38 @@ bool X86FrameLowering::assignCalleeSavedSpillSlots(
     MFI.ensureMaxAlignment(Align);
   }
 
+#if INTEL_CUSTOMIZATION
+  // CMPLRLLVM - 7519: These changes are based on a preliminary workaround.
+  // When a long term fix is checked into llorg, it will take precedence.
+  // If this function has funclets, create extra slots for non-GPRs in the
+  // funclets.
+  if (MF.hasEHFunclets()) {
+    for (unsigned i = CSI.size(); i != 0; --i) {
+      unsigned Reg = CSI[i - 1].getReg();
+      if (X86::GR64RegClass.contains(Reg) || X86::GR32RegClass.contains(Reg))
+        continue;
+
+      // FIXME: I'm not sure all of this is necessary, but it will work.
+
+      // If this is k-register make sure we lookup via the largest legal type.
+      MVT VT = MVT::Other;
+      if (X86::VK16RegClass.contains(Reg))
+        VT = STI.hasBWI() ? MVT::v64i1 : MVT::v16i1;
+
+      const TargetRegisterClass *RC = TRI->getMinimalPhysRegClass(Reg, VT);
+      unsigned Size = TRI->getSpillSize(*RC);
+      unsigned Align = TRI->getSpillAlignment(*RC);
+      // ensure alignment
+      SpillSlotOffset -= std::abs(SpillSlotOffset) % Align;
+      // spill into slot
+      SpillSlotOffset -= Size;
+      int SlotIndex = MFI.CreateFixedSpillStackObject(Size, SpillSlotOffset);
+      CSI[i - 1].setFuncletFrameIdx(SlotIndex);
+      MFI.ensureMaxAlignment(Align);
+    }
+  }
+#endif // INTEL_CUSTOMIZATION
+
   return true;
 }
 
@@ -2081,8 +2113,13 @@ bool X86FrameLowering::spillCalleeSavedRegisters(
     MBB.addLiveIn(Reg);
     const TargetRegisterClass *RC = TRI->getMinimalPhysRegClass(Reg, VT);
 
-    TII.storeRegToStackSlot(MBB, MI, Reg, true, CSI[i - 1].getFrameIdx(), RC,
-                            TRI);
+#if INTEL_CUSTOMIZATION
+    // CMPLRLLVM - 7519: These changes are based on a preliminary workaround.
+    // When a long term fix is checked into llorg, it will take precedence.
+    int FrameIdx = (MBB.isEHFuncletEntry() ? CSI[i - 1].getFuncletFrameIdx()
+                                           : CSI[i - 1].getFrameIdx());
+    TII.storeRegToStackSlot(MBB, MI, Reg, true, FrameIdx, RC, TRI);
+#endif // INTEL_CUSTOMIZATION
     --MI;
     MI->setFlag(MachineInstr::FrameSetup);
     ++MI;
@@ -2127,8 +2164,13 @@ bool X86FrameLowering::restoreCalleeSavedRegisters(MachineBasicBlock &MBB,
                                           const TargetRegisterInfo *TRI) const {
   if (CSI.empty())
     return false;
-
-  if (MI != MBB.end() && isFuncletReturnInstr(*MI) && STI.isOSWindows()) {
+#if INTEL_CUSTOMIZATION
+  // CMPLRLLVM - 7519: These changes are based on a preliminary workaround.
+  // When a long term fix is checked into llorg, it will take precedence.
+  bool IsEHFuncletReturn =
+      (MI != MBB.end() && isFuncletReturnInstr(*MI) && STI.isOSWindows());
+  if (IsEHFuncletReturn) {
+#endif // INTEL_CUSTOMIZATION
     // Don't restore CSRs in 32-bit EH funclets. Matches
     // spillCalleeSavedRegisters.
     if (STI.is32Bit())
@@ -2159,7 +2201,13 @@ bool X86FrameLowering::restoreCalleeSavedRegisters(MachineBasicBlock &MBB,
       VT = STI.hasBWI() ? MVT::v64i1 : MVT::v16i1;
 
     const TargetRegisterClass *RC = TRI->getMinimalPhysRegClass(Reg, VT);
-    TII.loadRegFromStackSlot(MBB, MI, Reg, CSI[i].getFrameIdx(), RC, TRI);
+#if INTEL_CUSTOMIZATION
+    // CMPLRLLVM - 7519: These changes are based on a preliminary workaround.
+    // When a long term fix is checked into llorg, it will take precedence.
+    int FrameIdx = (IsEHFuncletReturn ? CSI[i].getFuncletFrameIdx()
+                                      : CSI[i].getFrameIdx());
+    TII.loadRegFromStackSlot(MBB, MI, Reg, FrameIdx, RC, TRI);
+#endif // INTEL_CUSTOMIZATION
   }
 
   // POP GPRs.
