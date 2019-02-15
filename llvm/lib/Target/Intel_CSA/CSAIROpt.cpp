f4f4ae3eebdf76c9ebad465375c1c5b6f0ff7104
Joshua Cranmer <joshua.cranmer@intel.com>
2019-02-15 15:48:43 -0500

    Prevent CSAIROpt from crashing when -disable-lsr is passed to the compiler.
    Change-Id: I7c898d845bd8fd6315141b750f823ef1822d9667
    Reviewed-on: https://git-amr-2.devtools.intel.com/gerrit/173283
    Tested-by: alloy <lab_icla@iclgerrit.sc.intel.com>
    Reviewed-by: Sudarsanam, Arvind <arvind.sudarsanam@intel.com>
    Old/split llvm commit ID: 9af332f89c8f878ea9acda7ddfeb40276ac84570

--- llvm/lib/Target/Intel_CSA/CSAIROpt.cpp
+++ llvm/lib/Target/Intel_CSA/CSAIROpt.cpp
@@ -1,6 +1,6 @@
 //===- CSAIRReductionOpt.cpp - IR level optimization pass --------===//
 //
-// Copyright (C) 2017-2018 Intel Corporation. All rights reserved.
+// Copyright (C) 2017-2019 Intel Corporation. All rights reserved.
 //
 // The information and source code contained herein is the exclusive
 // property of Intel Corporation and may not be disclosed, examined
@@ -93,6 +93,9 @@ INITIALIZE_PASS_END(CSAIRReductionOpt, DEBUG_TYPE, "IR Reduction optimization",
 
 bool CSAIRReductionOpt::runOnLoop(Loop *L, LPPassManager &) {
   if (DisableIRReductionOpt)
+    return false;
+  // This causes isReductionPHI to crash if there is no loop preheader.
+  if (!L->getLoopPreheader())
     return false;
   bool changed         = false;
   LLVMContext &context = L->getHeader()->getContext();
