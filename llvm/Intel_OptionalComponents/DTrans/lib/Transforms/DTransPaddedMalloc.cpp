150df602fe5c8ebb1d548dc27596a3ba26a99557
Axel Y Rivera <axel.y.rivera@intel.com>
2019-02-15 10:39:23 -0800

    [NFC] Assert for klocwork in Padded Malloc
    Change-Id: I93d4816a7e1ac3cb8c7deb0ea98175a6b3bb6a95
    Reviewed-on: https://git-amr-2.devtools.intel.com/gerrit/173249
    Reviewed-by: Cox, Robert <robert.cox@intel.com>
    Tested-by: zperf_checkin_xmain <lab_iclz@iclgerrit.sc.intel.com>
    Tested-by: xmain_checkin <lab_iclt@iclgerrit.sc.intel.com>
    Reviewed-by: Zhao, Chuck <chuck.zhao@intel.com>
    Reviewed-by: Guggilla, Satish <satish.guggilla@intel.com>
    Old/split llvm commit ID: 96a409da85576c104197be4516e5f96505d47f77

--- llvm/Intel_OptionalComponents/DTrans/lib/Transforms/DTransPaddedMalloc.cpp
+++ llvm/Intel_OptionalComponents/DTrans/lib/Transforms/DTransPaddedMalloc.cpp
@@ -1,6 +1,6 @@
 //===------- DtransPaddedMalloc.cpp - DTrans Padded Malloc -*------===//
 //
-// Copyright (C) 2018 Intel Corporation. All rights reserved.
+// Copyright (C) 2018-2019 Intel Corporation. All rights reserved.
 //
 // The information and source code contained herein is the exclusive property
 // of Intel Corporation and may not be disclosed, examined or reproduced in
@@ -798,6 +798,8 @@ void dtrans::PaddedMallocGlobals::buildInterfaceFunction(Module &M) {
 
   GlobalVariable *GlobalCounter = M.getGlobalVariable(
       "__Intel_PaddedMallocCounter", true /*AllowInternal*/);
+
+  assert(GlobalCounter && "dtrans-paddedmalloc: Global counter not generated");
 
   // Create the function first
   FunctionType *funcType = FunctionType::get(Builder.getInt1Ty(), false);
