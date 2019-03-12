d0e9b73d3a75606b02e5407c87fb44798c646cd0
Hansang Bae <hansang.bae@intel.com>
2019-03-12 09:23:47 -0500

    [OMPTarget] Improve OpenCL platform/device probing
    This change gives higher priority to GPU when searching for OpenCL devices.
    Devices from a platform having a GPU device are listed before other devices,
    and mapping from numeric device ID to OCL platform ID is also stored.
    
    Change-Id: I0af4a8c5f55a99086cee6826f7298679fad58acf
    Reviewed-on: https://git-amr-2.devtools.intel.com/gerrit/177430
    Reviewed-by: Bae, Hansang <hansang.bae@intel.com>
    Tested-by: Bae, Hansang <hansang.bae@intel.com>
    Reviewed-by: Zakharin, Vyacheslav P <vyacheslav.p.zakharin@intel.com>
    Old/split openmp commit ID: 136f86614cbcae87513cb6157daf19e0aebaabf4

--- openmp/libomptarget/plugins/opencl/src/rtl.cpp
+++ openmp/libomptarget/plugins/opencl/src/rtl.cpp
@@ -139,9 +139,6 @@ static const char *getCLErrorName(int error) {
 #define INVOKE_CL_RET_FAIL(fn, ...) INVOKE_CL_RET(OFFLOAD_FAIL, fn, __VA_ARGS__)
 #define INVOKE_CL_RET_NULL(fn, ...) INVOKE_CL_RET(NULL, fn, __VA_ARGS__)
 
-// TODO: The current implementation only supports one device. It will be
-// extended in the future.
-#define NUMBER_OF_DEVICES 1
 #define OFFLOADSECTIONNAME ".omp_offloading.entries"
 
 //#pragma OPENCL EXTENSION cl_khr_spir : enable
@@ -222,8 +219,8 @@ class RTLDeviceInfoTy {
 
 public:
   cl_uint numDevices;
-  cl_platform_id platformId;
   // per device information
+  std::vector<cl_platform_id> platformIDs;
   std::vector<cl_device_id> deviceIDs;
   std::vector<int32_t> maxWorkGroups;
   std::vector<int32_t> maxWorkGroupSize;
@@ -234,12 +231,17 @@ public:
   int64_t flag;
   const int64_t DEVICE_LIMIT_NUM_WORK_GROUPS = 0x1;
 
-  RTLDeviceInfoTy() {
+  RTLDeviceInfoTy() : numDevices(0), flag(0) {
 #ifdef OMPTARGET_DEBUG
     if (char *envStr = getenv("LIBOMPTARGET_DEBUG")) {
       DebugLevel = std::stoi(envStr);
     }
 #endif // OMPTARGET_DEBUG
+    // set misc. flags
+    const char *env = std::getenv("SIMT");
+    if (!env || std::string(env) != "on") {
+      flag |= DEVICE_LIMIT_NUM_WORK_GROUPS;
+    }
 
     DP("Start initializing OpenCL\n");
     // get available platforms
@@ -248,57 +250,85 @@ public:
     std::vector<cl_platform_id> platformIds(platformIdCount);
     clGetPlatformIDs(platformIdCount, platformIds.data(), nullptr);
 
-    // check version and devices
+    std::vector<cl_device_id> deviceIDTail;
+    std::vector<cl_platform_id> platformIDTail;
+
+    // OpenCL device IDs are stored in a list so that
+    // 1. All device IDs from a single platfrom are stored consecutively.
+    // 2. Device IDs from a platform having at least one GPU device appears
+    //    before any device IDs from a platform having no GPU devices.
     for (cl_platform_id id : platformIds) {
       char buffer[128];
       clGetPlatformInfo(id, CL_PLATFORM_VERSION, 128, buffer, NULL);
       if (strncmp("OpenCL 2", buffer, 8)) {
         continue;
       }
-      DP("Platform version is %s\n", buffer);
 
-      clGetDeviceIDs(id, CL_DEVICE_TYPE_ALL, 0, nullptr, &numDevices);
-      deviceIDs.resize(numDevices);
-      clGetDeviceIDs(id, CL_DEVICE_TYPE_ALL, numDevices, deviceIDs.data(),
+      cl_uint numGPU = 0, numACC = 0, numCPU = 0;
+      char *envStr = getenv("LIBOMPTARGET_DEVICETYPE");
+      if (!envStr || strncmp(envStr, "GPU", 3) == 0)
+        clGetDeviceIDs(id, CL_DEVICE_TYPE_GPU, 0, nullptr, &numGPU);
+      if (!envStr || strncmp(envStr, "ACCELERATOR", 11) == 0)
+        clGetDeviceIDs(id, CL_DEVICE_TYPE_ACCELERATOR, 0, nullptr, &numACC);
+      if (!envStr || strncmp(envStr, "CPU", 3) == 0)
+        clGetDeviceIDs(id, CL_DEVICE_TYPE_CPU, 0, nullptr, &numCPU);
+      cl_uint numCurrDevices = numGPU + numACC + numCPU;
+      if (numCurrDevices == 0)
+        continue;
+
+      DP("Platform %s has %d GPUs, %d ACCELERATORS, %d CPUs\n", buffer, numGPU,
+         numACC, numCPU);
+      std::vector<cl_device_id> currDeviceIDs(numCurrDevices);
+      std::vector<cl_platform_id> currPlatformIDs(numCurrDevices, id);
+      clGetDeviceIDs(id, CL_DEVICE_TYPE_GPU, numGPU, &currDeviceIDs[0],
                      nullptr);
-      DP("Found %d OpenCL devices\n", numDevices);
+      clGetDeviceIDs(id, CL_DEVICE_TYPE_ACCELERATOR, numACC,
+                     &currDeviceIDs[numGPU], nullptr);
+      clGetDeviceIDs(id, CL_DEVICE_TYPE_CPU, numCPU,
+                     &currDeviceIDs[numGPU + numACC], nullptr);
 
-      maxWorkGroups.resize(numDevices);
-      maxWorkGroupSize.resize(numDevices);
-      CTX.resize(numDevices);
-      Queues.resize(numDevices);
-      FuncGblEntries.resize(numDevices);
-      platformId = id;
-
-      // get device specific information
-      for (unsigned i = 0; i < numDevices; i++) {
-        cl_device_id deviceId = deviceIDs[i];
-        clGetDeviceInfo(deviceId, CL_DEVICE_NAME, 128, buffer, nullptr);
-        DP("Device %d: %s\n", i, buffer);
-        clGetDeviceInfo(deviceId, CL_DEVICE_MAX_COMPUTE_UNITS, 4,
-                        &maxWorkGroups[i], nullptr);
-        DP("Maximum number of work groups (compute units) is %d\n",
-           maxWorkGroups[i]);
-        clGetDeviceInfo(deviceId, CL_DEVICE_MAX_WORK_GROUP_SIZE, sizeof(size_t),
-                        &maxWorkGroupSize[i], nullptr);
-        DP("Maximum work group size is %d\n", maxWorkGroupSize[i]);
-#ifdef OMPTARGET_DEBUG
-        cl_uint addressmode;
-        clGetDeviceInfo(deviceId, CL_DEVICE_ADDRESS_BITS, 4, &addressmode,
-                        nullptr);
-        DP("Addressing mode is %d bit\n", addressmode);
-#endif
+      std::vector<cl_device_id> *dID = &deviceIDs;
+      std::vector<cl_platform_id> *pID = &platformIDs;
+      if (numGPU == 0) {
+        dID = &deviceIDTail;
+        pID = &platformIDTail;
       }
-      // set misc. flags
-      flag = 0LL;
-      const char *env = std::getenv("SIMT");
-      if (!env || std::string(env) != "on") {
-        flag |= DEVICE_LIMIT_NUM_WORK_GROUPS;
-      }
-      // return;
+      dID->insert(dID->end(), currDeviceIDs.begin(), currDeviceIDs.end());
+      pID->insert(pID->end(), currPlatformIDs.begin(), currPlatformIDs.end());
+      numDevices += numCurrDevices;
     }
-    // numDevices = 0;
-    // DP("No OpenCL devices found.\n");
+    deviceIDs.insert(deviceIDs.end(), deviceIDTail.begin(), deviceIDTail.end());
+    platformIDs.insert(platformIDs.end(), platformIDTail.begin(),
+                       platformIDTail.end());
+
+    maxWorkGroups.resize(numDevices);
+    maxWorkGroupSize.resize(numDevices);
+    CTX.resize(numDevices);
+    Queues.resize(numDevices);
+    FuncGblEntries.resize(numDevices);
+
+    // get device specific information
+    for (unsigned i = 0; i < numDevices; i++) {
+      char buffer[128];
+      cl_device_id deviceId = deviceIDs[i];
+      clGetDeviceInfo(deviceId, CL_DEVICE_NAME, 128, buffer, nullptr);
+      DP("Device %d: %s\n", i, buffer);
+      clGetDeviceInfo(deviceId, CL_DEVICE_MAX_COMPUTE_UNITS, 4,
+                      &maxWorkGroups[i], nullptr);
+      DP("Maximum number of work groups (compute units) is %d\n",
+         maxWorkGroups[i]);
+      clGetDeviceInfo(deviceId, CL_DEVICE_MAX_WORK_GROUP_SIZE, sizeof(size_t),
+                      &maxWorkGroupSize[i], nullptr);
+      DP("Maximum work group size is %d\n", maxWorkGroupSize[i]);
+#ifdef OMPTARGET_DEBUG
+      cl_uint addressmode;
+      clGetDeviceInfo(deviceId, CL_DEVICE_ADDRESS_BITS, 4, &addressmode,
+                      nullptr);
+      DP("Addressing mode is %d bit\n", addressmode);
+#endif
+    }
+    if (numDevices == 0)
+      DP("WARNING: No OpenCL devices found.\n");
   }
 };
 
@@ -323,10 +353,13 @@ int32_t __tgt_rtl_init_device(int32_t device_id) {
 
   cl_int status;
   DP("Initialize OpenCL device\n");
+  assert(device_id >= 0 && (cl_uint)device_id < DeviceInfo.numDevices &&
+         "bad device id");
 
   // create context
   cl_context_properties props[] = {
-      CL_CONTEXT_PLATFORM, (cl_context_properties)DeviceInfo.platformId, 0};
+      CL_CONTEXT_PLATFORM,
+      (cl_context_properties)DeviceInfo.platformIDs[device_id], 0};
   DeviceInfo.CTX[device_id] = clCreateContext(
       props, 1, &DeviceInfo.deviceIDs[device_id], nullptr, nullptr, &status);
   if (status != CL_SUCCESS) {
@@ -356,8 +389,6 @@ __tgt_target_table *__tgt_rtl_load_binary(int32_t device_id,
 
   DP("Device %d: load binary from " DPxMOD " image\n", device_id,
      DPxPTR(image->ImageStart));
-
-  assert(device_id >= 0 && device_id < NUMBER_OF_DEVICES && "bad device id");
 
   size_t ImageSize = (size_t)image->ImageEnd - (size_t)image->ImageStart;
   size_t NumEntries = (size_t)(image->EntriesEnd - image->EntriesBegin);
