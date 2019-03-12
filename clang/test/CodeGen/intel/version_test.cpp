543992d51fa0c416c5d68708e25995b8c93b5b2b
Michael P Rice <michael.p.rice@intel.com>
2019-03-12 09:44:22 -0700

    CMPLRLLVM-8597 - add "clang based " in front of the DWARF producer string.
      GDB checks the producer string when making prologue calculations and
      gets them wrong when it doesn't begin with "clang ".
    
    Change-Id: I407517a4e0cba5f0b2777077e17010e9a5683e3f
    Reviewed-on: https://git-amr-2.devtools.intel.com/gerrit/177458
    Reviewed-by: Wyma, Brock <brock.wyma@intel.com>
    Tested-by: xmain_checkin <lab_iclt@iclgerrit.sc.intel.com>
    Old/split clang commit ID: 3d3710b472d0d14e6f3c9e6afe167bc7ad5b0fa5

--- clang/test/CodeGen/intel/version_test.cpp
+++ clang/test/CodeGen/intel/version_test.cpp
@@ -1,5 +1,6 @@
 // RUN: %clang_cc1 -triple=x86_64-unknown-linux-gnu -O0 -emit-llvm %s -o - \
-// RUN:   | FileCheck %s
+// RUN:  -debug-info-kind=limited -dwarf-version=4 \
+// RUN:  | FileCheck %s
 
 // CHECK: constant {{.*}} c"{{[1-9][0-9]*[\.][0-9]+[\.][0-9]+}}
 // CHECK-SAME: (icx {{[0-9\.]+}})
@@ -12,4 +13,5 @@ int foo()
 }
 
 // CHECK: !llvm.ident = !{[[I:![0-9]+]]}
+// CHECK: !DICompileUnit{{.*}}producer: "clang based
 // CHECK: [[I]] = {{.*}}icx
