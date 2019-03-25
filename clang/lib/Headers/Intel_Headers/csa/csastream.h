fbbe362f4cb51ba7e36209fe6d8ec7b1a7404bfa
Dounia Khaldi <dounia.khaldi@intel.com>
2019-03-25 17:04:16 -0400

    [CSA Streams] - Add the rotate method for CSA streams               - Remove ifdef __CSA__ as it is needed in the enduser code that is using the class and not in the header
    Change-Id: I739dd4e5ca3b122ee6c2fb02fa4065158e643b34
    Reviewed-on: https://git-amr-2.devtools.intel.com/gerrit/179855
    Reviewed-by: Khaldi, Dounia <dounia.khaldi@intel.com>
    Tested-by: Khaldi, Dounia <dounia.khaldi@intel.com>
    Old/split clang commit ID: a55e83749a61ff6a138ee7608987114c62244626

--- clang/lib/Headers/Intel_Headers/csa/csastream.h
+++ clang/lib/Headers/Intel_Headers/csa/csastream.h
@@ -32,23 +32,53 @@ namespace csa
     int identifier;
   public:
     __attribute__((always_inline))
-#ifdef __CSA__
     stream() : identifier(__builtin_csa_lic_init(sizeof(T), depth, pref_depth))
     {}
-#endif
     __attribute__((always_inline))
-  T read() {
-#ifdef __CSA__
+    T read() {
       return __builtin_csa_lic_read((T)0, identifier);
-#else
-      return 0;
-#endif
     }
     __attribute__((always_inline))
     void write(T val) {
-#ifdef __CSA__
       __builtin_csa_lic_write(identifier, val);
-#endif
+    }
+    // rotate is used to bypass the single write site restriction
+    // to the same stream in the following scenario:
+    // if input stream in has {012} values,
+    // this function will return this sequence:{012012012}
+    // if repetition rate R is 3
+    // An example is below where one loop does the memory load
+    // the second loop does the rotation and use the returned values (val)
+    // as follows:
+    // Original code (C, C++, Fortran)
+    // for (int i = 0; i < M; ++i)
+    //   in.write(A[i]);
+    // for (int r = 0; r < R; ++r) {
+    //   for (int i = 0; i < M; ++i) {
+    //     int val;
+    //     if (r != 0) val = back.read();
+    //     else val = in.read();
+    //     use_the_values(val);
+    //     if (r != R-1) back.write(val);
+    //   }
+    // }
+    // Using rotate method:
+    // #include “csastream.h”
+    // for (int i=0; i<M; i++)
+    //   in.write(A[i]);
+    // for (int r = 0; r < R; ++r)
+    //   for (int i=0; i<M; i++) {
+    //     val = in.rotate(r, 0, R, 1);
+    //     use_the_values(val);
+    //   }
+    __attribute__((always_inline))
+    T rotate(int range, int low, int up, int step) {
+      csa::stream<T> back;
+      T val;
+      if(range == low) val = this->read();
+      if(range != low) val = back.read();
+      if(range != up-step) back.write(val);
+      return val;
     }
   };
 }
