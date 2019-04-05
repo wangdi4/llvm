fb3c7e9b7382aa62427d98f49d21a121b2d2a045
Christopher M Chrulski <christopher.m.chrulski@intel.com>
2019-04-04 17:41:35 -0700

    [DTrans] [CMPLRLLVM-1164] Basic candidate selection for Transpose transformation
    This looks for global arrays that meet the basic criteria of
    being multi-dimensional arrays, which have the same length in
    all dimensions, to select the initial candidates to examine
    for the Transpose transformation.
    
    Change-Id: I61cc58fa6400fd5ee636a0d72028f7c578bbb724
    Reviewed-on: https://git-amr-2.devtools.intel.com/gerrit/181886
    Reviewed-by: Cox, Robert <robert.cox@intel.com>
    Tested-by: zperf_checkin_xmain <lab_iclz@iclgerrit.sc.intel.com>
    Tested-by: xmain_checkin <lab_iclt@iclgerrit.sc.intel.com>
    Reviewed-by: Guggilla, Satish <satish.guggilla@intel.com>
    Old/split llvm commit ID: 1b76bfe64b46a0e1ec0b7e81497c2c61097473d9

--- llvm/Intel_OptionalComponents/DTrans/lib/Transforms/Transpose.cpp
+++ llvm/Intel_OptionalComponents/DTrans/lib/Transforms/Transpose.cpp
@@ -22,8 +22,116 @@ using namespace llvm;
 
 #define DEBUG_TYPE "dtrans-transpose"
 
+#if !defined(NDEBUG) || defined(LLVM_ENABLE_DUMP)
+// Print the list of candidates identified and their analysis result.
+static cl::opt<bool> PrintCandidates("dtrans-transpose-print-candidates",
+                                     cl::ReallyHidden);
+#endif // !defined(NDEBUG) || defined(LLVM_ENABLE_DUMP)
+
 namespace {
 
+// Maximum rank for a Fortran array.
+const uint32_t FortranMaxRank = 9;
+
+// This is the class that manages the analysis and transformation
+// of the stride information for a candidate variable.
+class TransposeCandidate {
+public:
+  TransposeCandidate(GlobalVariable *GV, uint32_t ArrayRank,
+                     uint64_t ArrayLength, uint64_t ElementSize)
+      : GV(GV), ArrayRank(ArrayRank), ArrayLength(ArrayLength),
+        ElementSize(ElementSize), IsValid(false) {
+    assert(ArrayRank > 0 && ArrayRank <= FortranMaxRank && "Invalid Rank");
+    uint64_t Stride = ElementSize;
+    for (uint32_t RankNum = 0; RankNum < ArrayRank; ++RankNum) {
+      Strides.push_back(Stride);
+      Stride *= ArrayLength;
+    }
+  }
+
+  // This function analyzes a candidate to check whether all uses of the
+  // variable are supported for the transformation.
+  bool analyze() {
+    // TODO: Add the safety and profitability analysis for the candidate
+    return false;
+  }
+
+  // Transform the strides in the subscript calls and dope vector creation, if
+  // the candidate is valid for being transposed.
+  bool transform() {
+    if (!IsValid)
+      return false;
+
+    transposeStrides();
+    return true;
+  }
+
+#if !defined(NDEBUG) || defined(LLVM_ENABLE_DUMP)
+  void dump() { print(dbgs()); }
+
+  void print(raw_ostream &OS) {
+    OS << "Transpose candidate: " << GV->getName() << "\n";
+    OS << "Type         : " << *GV->getType() << "\n";
+    OS << "Rank         : " << ArrayRank << "\n";
+    OS << "Length       : " << ArrayLength << "\n";
+    OS << "Element size : " << ElementSize << "\n";
+    OS << "Strides      :";
+    for (uint32_t RankNum = 0; RankNum < ArrayRank; ++RankNum)
+      OS << " " << Strides[RankNum];
+    OS << "\n";
+
+    OS << "Transposition:";
+    if (!Transposition.empty())
+      for (uint32_t RankNum = 0; RankNum < ArrayRank; ++RankNum)
+        OS << " " << Transposition[RankNum];
+    OS << "\n";
+    OS << "IsValid      : " << (IsValid ? "true" : "false") << "\n";
+    OS << "--------------\n";
+  }
+#endif // !defined(NDEBUG) || defined(LLVM_ENABLE_DUMP)
+
+private:
+  // The global variable that is a possible candidate
+  GlobalVariable *GV;
+
+  // Number of dimensions (Fortran Rank) for the array
+  uint32_t ArrayRank;
+
+  // Number of elements in each dimension of the array. (Candidates must have
+  // the same length in all dimensions)
+  uint64_t ArrayLength;
+
+  // Size of one element in the array, in bytes.
+  uint64_t ElementSize;
+
+  // This vector stores the stride values used when operating on the complete
+  // array. For this optimization, we do not support cases where a sub-object is
+  // passed to a function as a portion of the array.
+  SmallVector<uint64_t, FortranMaxRank> Strides;
+
+  // This vector stores the transpose index that will be used to access the
+  // stride for a particular rank. For example, the regular layout of an array
+  // that accesses 'block[i][j][k]', uses 'i' for the Rank 2 element, 'j' for
+  // the Rank 1 element, and 'k' for the Rank 0 element, which would be
+  // represented as accessing elements 0, 1, and 2 from the 'Strides' array.
+  // Transposing the strides for the i and k elements would correspond to this
+  // index lookup array being {2, 1, 0}
+  SmallVector<uint32_t, FortranMaxRank> Transposition;
+
+  // Indicates whether the analysis determined the candidate is safe to
+  // transpose.
+  bool IsValid;
+
+  // This function will swap the strides used for indexing into the array. These
+  // need to be changed for subscript operators that directly index into the
+  // global variable, and for the setup of the dope vectors used when passing
+  // the global variable to another function.
+  void transposeStrides() {
+    // TODO: transformation of uses goes here.
+  }
+};
+
+//
 // The array stride transpose optimization for Fortran.
 //
 // This optimization swaps the stride values used for multi-dimensional Fortran
@@ -35,9 +143,8 @@ namespace {
 // to block(i,j) being computed as:
 //     &block + j * 3 * sizeof(integer) + i * sizeof(integer)
 //
-// For a loop iterating along 'j', this may enable downstream optimizations if
-// the strides are transposed so that iterations along 'j' will be a unit
-// stride.
+// For a loop iterating along 'j', transposing the strides may enable downstream
+// optimizations so that iterations along 'j' will be a unit stride.
 //
 // This class will heuristically estimate the benefit and swap the stride values
 // when beneficial.
@@ -45,10 +152,81 @@ class TransposeImpl {
 public:
   TransposeImpl(std::function<LoopInfo &(Function &)> &GetLI) : GetLI(GetLI) {}
 
-  bool run(Module &M) { return false; }
+  bool run(Module &M) {
+    IdentifyCandidates(M);
+
+    bool ValidCandidate = false;
+    for (auto &Cand : Candidates) {
+      ValidCandidate |= Cand.analyze();
+
+#if !defined(NDEBUG) || defined(LLVM_ENABLE_DUMP)
+      if (PrintCandidates)
+        Cand.dump();
+#endif // !defined(NDEBUG) || defined(LLVM_ENABLE_DUMP)
+    }
+
+    bool Changed = false;
+    if (ValidCandidate)
+      for (auto &Cand : Candidates)
+        Changed |= Cand.transform();
+
+    return Changed;
+  }
 
 private:
   std::function<LoopInfo &(Function &)> &GetLI;
+
+  // Global variable candidates for the transformation.
+  SmallVector<TransposeCandidate, 8> Candidates;
+
+  // Identify potential candidates for the transpose optimization.
+  //
+  // The initial set of candidates meet the following criteria:
+  // - Global Variable with internal linkage
+  // - Multi-dimensional array of integer type
+  // - The array lengths in all dimensions are equal
+  // - Variable uses zero initializer
+  void IdentifyCandidates(Module &M) {
+    const DataLayout &DL = M.getDataLayout();
+
+    for (auto &GV : M.globals()) {
+      if (!GV.hasInitializer() || !GV.getInitializer()->isZeroValue())
+        continue;
+
+      // All uses of the variable need to be analyzed, therefore we need
+      // internal linkage.
+      if (!GV.hasInternalLinkage())
+        continue;
+
+      // All global variables are pointers
+      llvm::Type *Ty = GV.getType()->getPointerElementType();
+      auto *ArrType = dyn_cast<llvm::ArrayType>(Ty);
+      if (!ArrType)
+        continue;
+
+      uint32_t Dimensions = 1;
+      bool AllSame = true;
+      uint64_t Length = ArrType->getArrayNumElements();
+      llvm::Type *ElemType = ArrType->getArrayElementType();
+      while (ElemType->isArrayTy()) {
+        auto *InnerArrType = cast<llvm::ArrayType>(ElemType);
+        if (InnerArrType->getArrayNumElements() != Length) {
+          AllSame = false;
+          break;
+        }
+        Dimensions++;
+        ElemType = InnerArrType->getArrayElementType();
+      }
+
+      if (AllSame && Dimensions > 1 && Dimensions <= FortranMaxRank &&
+          ElemType->isIntegerTy()) {
+        LLVM_DEBUG(dbgs() << "Adding candidate: " << GV << "\n");
+        uint64_t ElemSize = DL.getTypeStoreSize(ElemType);
+        TransposeCandidate Candidate(&GV, Dimensions, Length, ElemSize);
+        Candidates.push_back(Candidate);
+      }
+    }
+  }
 };
 
 // Legacy pass manager wrapper for invoking the Transpose pass.
