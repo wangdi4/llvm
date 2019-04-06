43d7b9d93c97056bce9858cdd67309e1db651d76
Prasad V Battini <prasad.v.battini@intel.com>
2019-04-06 00:35:40 -0400

    CMPLRLLVM-8888: Andersen shouldn't ignore non-pointer store instructions.
    Change-Id: I175ca1e5ac39ba8dc4b37053b27bba67b7205717
    Reviewed-on: https://git-amr-2.devtools.intel.com/gerrit/182203
    Reviewed-by: Chrulski, Christopher M <christopher.m.chrulski@intel.com>
    Reviewed-by: Zhao, Chuck <chuck.zhao@intel.com>
    Tested-by: Battini, Prasad V <prasad.v.battini@intel.com>
    Reviewed-by: Guggilla, Satish <satish.guggilla@intel.com>
    Old/split llvm commit ID: c91d2b5861c675ed5f61005fdf0b55ff80ab43fb

--- llvm/lib/Analysis/Intel_Andersens.cpp
+++ llvm/lib/Analysis/Intel_Andersens.cpp
@@ -1820,6 +1820,21 @@ void AndersensAAResult::visitStoreInst(StoreInst &SI) {
                        getNode(SI.getOperand(0)));
     }
   }
+  // CMPLRLLVM-8888: Analysis shouldn't ignore non-pointer store
+  // instructions. For now, conservatively assume it is storing
+  // unknown pointer.
+  //  Ex:
+  //    %0 = bitcast i8* %call to %struct.S*
+  //    %int = ptrtoint %struct.S* %0 to i64
+  //    %ptr1 = bitcast %struct.S** %q to i64*
+  //    store i64 %int, i64* %ptr1, align 8
+  //
+  // TODO: Analysis needs to be improved to compute more accurate
+  // points-to info by analyzing the non-pointer value that is being
+  // stored.
+  if (!isPointsToType(SI.getOperand(0)->getType()))
+    CreateConstraint(Constraint::Store, getNode(SI.getOperand(1)),
+                     UniversalSet);
 }
 
 void AndersensAAResult::visitGetElementPtrInst(GetElementPtrInst &GEP) {
