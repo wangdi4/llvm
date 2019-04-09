17da3558deb1363b7f0d7c62f28ab12be2363690
Christopher M Chrulski <christopher.m.chrulski@intel.com>
2019-04-09 16:40:39 -0700

    [DTrans] [CMPLRLLVM-1164] First part of safety analysis for Transpose candidates
    This change starts implementing the safety analysis on the direct uses of
    the global variable candidate for the transpose, and checks that the
    subscript intrinsic calls have the expected values.
    
    Change-Id: I74c34b961003d1592ba2865ead74083aa8c48776
    Reviewed-on: https://git-amr-2.devtools.intel.com/gerrit/182564
    Reviewed-by: Cox, Robert <robert.cox@intel.com>
    Reviewed-by: Battini, Prasad V <prasad.v.battini@intel.com>
    Tested-by: zperf_checkin_xmain <lab_iclz@iclgerrit.sc.intel.com>
    Tested-by: xmain_checkin <lab_iclt@iclgerrit.sc.intel.com>
    Reviewed-by: Guggilla, Satish <satish.guggilla@intel.com>
    Old/split llvm commit ID: 129f8b2ef24bf290dbc4667773fd00bf75639c8a

--- llvm/Intel_OptionalComponents/DTrans/lib/Transforms/Transpose.cpp
+++ llvm/Intel_OptionalComponents/DTrans/lib/Transforms/Transpose.cpp
@@ -22,6 +22,9 @@ using namespace llvm;
 
 #define DEBUG_TYPE "dtrans-transpose"
 
+// Trace messages regarding the analysis of the candidate variables.
+#define DEBUG_ANALYSIS "dtrans-transpose-analysis"
+
 #if !defined(NDEBUG) || defined(LLVM_ENABLE_DUMP)
 // Print the list of candidates identified and their analysis result.
 static cl::opt<bool> PrintCandidates("dtrans-transpose-print-candidates",
@@ -30,6 +33,11 @@ static cl::opt<bool> PrintCandidates("dtrans-transpose-print-candidates",
 
 namespace {
 
+// Helper routine to check if a CallInst is to llvm.intel.subscript.
+bool isSubscriptIntrinsicCall(const CallInst &CI) {
+  const Function *F = CI.getCalledFunction();
+  return F && F->getIntrinsicID() == Intrinsic::intel_subscript;
+}
 // Maximum rank for a Fortran array.
 const uint32_t FortranMaxRank = 9;
 
@@ -51,8 +59,218 @@ public:
 
   // This function analyzes a candidate to check whether all uses of the
   // variable are supported for the transformation.
+  //
+  // The only valid uses for the global variable itself are:
+  // - Base pointer argument in outermost call of a llvm.intel.subscript
+  //   intrinsic call chain.
+  // - Storing the array's address into a dope vector that represents the
+  //   entire array object using the default values for the lower bound/
+  //   extent/stride.
+  // - The dope vector object may be passed to a function that takes an
+  //   assumed shape array. The called function will be checked that there
+  //   are only reads of the dope vector structure elements, or the transfer
+  //   of the dope vector pointer to an uplevel variable.
+  // - The uplevel variable can be passed to a function, and again all uses
+  //   of the dope vector fields will be checked to verify that only reads
+  //   are done on the dope vector elements.
+  //
   bool analyze() {
-    // TODO: Add the safety and profitability analysis for the candidate
+    DEBUG_WITH_TYPE(DEBUG_ANALYSIS,
+                    dbgs() << "Analyzing variable: " << *GV << "\n");
+
+    // Check all the direct uses of the global. This loop will also collect
+    // the functions that take a dope vector which need to be checked.
+    IsValid = true;
+    for (auto *U : GV->users()) {
+      DEBUG_WITH_TYPE(DEBUG_ANALYSIS,
+                      dbgs() << "Checking global var use: " << *U << "\n");
+
+      // Uses of the global should be in the form of a GEP operator which should
+      // only be getting the base address of the array. For example:
+      //   i32* getelementptr ([9 x [9 x i32]],
+      //                       [9 x [9 x i32]]* @var1, i64 0, i64 0, i64 0)
+
+      auto *GepOp = dyn_cast<GEPOperator>(U);
+      if (!GepOp) {
+        DEBUG_WITH_TYPE(DEBUG_ANALYSIS,
+                        dbgs() << "  Invalid: Unsupported instruction: " << *U
+                               << "\n");
+        IsValid = false;
+        break;
+      }
+
+      if (!GepOp->hasAllZeroIndices()) {
+        DEBUG_WITH_TYPE(
+            DEBUG_ANALYSIS,
+            dbgs() << "  Invalid: Global variable GEP not getting base "
+                      "pointer address\n");
+        IsValid = false;
+        break;
+      }
+
+      // Now check the users of the pointer address for safety
+      for (auto *GepOpUser : GepOp->users()) {
+        DEBUG_WITH_TYPE(DEBUG_ANALYSIS, {
+          dbgs() << "  Checking global var address use: " << *GepOpUser << "\n";
+          if (auto *I = dyn_cast<Instruction>(GepOpUser))
+            dbgs() << "  in function: "
+                   << I->getParent()->getParent()->getName() << "\n";
+        });
+
+        if (auto *CI = dyn_cast<CallInst>(GepOpUser)) {
+          // Check that the call is to llvm.intel.subscript.
+          //
+          // This could be extended in the future to allow the address to be
+          // passed without a dope vector, but that is not needed for the case
+          // of interest, at the moment.
+          if (!isSubscriptIntrinsicCall(*CI)) {
+            DEBUG_WITH_TYPE(
+                DEBUG_ANALYSIS,
+                dbgs() << "  Invalid: Call with pointer address may only be "
+                          "subscript intrinsic call\n");
+
+            IsValid = false;
+            break;
+          }
+
+          // The global variable should only be accessed with a subscript call
+          // that uses the rank of the variable, and the array should only be
+          // using default values for the lower bound and stride, rather than a
+          // user defined value for the lower bound. It should not be required
+          // for the transform, but it avoids cases such as:
+          //     integer :: my_array(2:10, 9, 11:19)
+          if (!isValidUseOfSubscriptForGlobal(*CI, *GepOp)) {
+            DEBUG_WITH_TYPE(
+                DEBUG_ANALYSIS,
+                dbgs() << "  Invalid: Subscript call values not supported\n");
+
+            IsValid = false;
+            break;
+          }
+
+          // Save the subscript call because we will need this for computing
+          // profitability and transforming the arguments later.
+          SubscriptCalls.insert(CI);
+        } else if (auto *SI = dyn_cast<StoreInst>(GepOpUser)) {
+          // The only case the address of the variable may be saved is into a
+          // dope vector, check that case here.
+          if (!isValidStoreForGlobal(*SI, GepOp)) {
+            DEBUG_WITH_TYPE(
+                DEBUG_ANALYSIS,
+                dbgs()
+                    << "  Invalid: Store of pointer address not supported\n");
+
+            IsValid = false;
+            break;
+          }
+        } else {
+          // Other uses are not allowed.
+          DEBUG_WITH_TYPE(DEBUG_ANALYSIS,
+                          dbgs() << "Unsupported use of global: " << *GepOpUser
+                                 << "\n");
+          IsValid = false;
+          break;
+        }
+      }
+    }
+
+    // TODO: Analysis of the dope vectors passed to functions will go here.
+
+    if (!IsValid)
+      SubscriptCalls.clear();
+
+    return IsValid;
+  }
+
+  // Check that \p CI is a supported subscript call on the global array base
+  // address \p BasePtr. For a global variable, we expect the subscript call to
+  // contain the constant values for the lower bound and stride that represent
+  // the full array, and a lower bound index of 1.
+  bool isValidUseOfSubscriptForGlobal(const CallInst &CI,
+                                      const Value &BasePtr) {
+
+    // Helper that checks constants for one subscript call, and recurse if
+    // there are more ranks to check.
+    std::function<bool(const CallInst &, const Value &, uint32_t)>
+
+        IsValidUseForRank = [this, &IsValidUseForRank](const CallInst &Call,
+                                                       const Value &Ptr,
+                                                       uint32_t Rank) -> bool {
+      if (!isValidUseOfSubscriptCall(Call, Ptr, Rank, 1, Strides[Rank]))
+        return false;
+
+      // Verify the subscript result is only fed to another subscript call. In
+      // the future this could be extended to support PHI nodes/select
+      // instructions, but for now that is not needed.
+      if (Rank > 0) {
+        for (const auto *U : Call.users()) {
+          const auto *CI = dyn_cast<CallInst>(U);
+          if (!CI)
+            return false;
+
+          if (!IsValidUseForRank(*CI, Call, Rank - 1))
+            return false;
+        }
+      }
+      return true;
+    };
+
+    // Check the use of this subscript call, and all the subscript calls the
+    // result is fed to. Note, subscript call rank parameter value starts at 0,
+    // not 1.
+    return IsValidUseForRank(CI, BasePtr, ArrayRank - 1);
+  }
+
+  // Check for arguments of a subscript intrinsic call for the expected values.
+  // The intrinsic call is declared as:
+  //    declare <ty>* @llvm.intel.subscript...(i8 <rank>, <ty> <lb>,
+  //                                           <ty> <stride>, <ty>* <base>,
+  //                                           <ty> <index>)
+  //
+  // Return 'true' if call has the expected values for the Base, Rank,
+  // LowerBound and Stride parameters.
+  //
+  bool isValidUseOfSubscriptCall(const CallInst &CI, const Value &Base,
+                                 uint32_t Rank, uint64_t LowerBound,
+                                 uint64_t Stride) {
+    const unsigned RankOpNum = 0;
+    const unsigned LBOpNum = 1;
+    const unsigned StrideOpNum = 2;
+    const unsigned PtrOpNum = 3;
+
+    DEBUG_WITH_TYPE(DEBUG_ANALYSIS, {
+      dbgs().indent((ArrayRank - Rank) * 2 + 4);
+      dbgs() << "Checking call: " << CI << "\n";
+    });
+
+    if (!isSubscriptIntrinsicCall(CI))
+      return false;
+
+    if (CI.getArgOperand(PtrOpNum) != &Base)
+      return false;
+
+    auto RankVal = dyn_cast<ConstantInt>(CI.getArgOperand(RankOpNum));
+    if (!RankVal || RankVal->getLimitedValue() != Rank)
+      return false;
+
+    auto LBVal = dyn_cast<ConstantInt>(CI.getArgOperand(LBOpNum));
+    if (!LBVal || LBVal->getLimitedValue() != LowerBound)
+      return false;
+
+    auto StrideVal = dyn_cast<ConstantInt>(CI.getArgOperand(StrideOpNum));
+    if (!StrideVal || StrideVal->getLimitedValue() != Stride)
+      return false;
+
+    return true;
+  }
+
+  // The only supported use of storing the address of the array's base pointer
+  // into another memory location is when the address is being stored into a
+  // dope vector, and the dope vector is describing the entire array (Lower
+  // Bound = 1, Extent = array length, and Stride is each element for each array
+  // dimension).
+  bool isValidStoreForGlobal(const StoreInst &SI, const Value *BasePtr) {
+    // TODO: Implement checks for storing into a dope vector
     return false;
   }
 
@@ -118,6 +336,13 @@ private:
   // index lookup array being {2, 1, 0}
   SmallVector<uint32_t, FortranMaxRank> Transposition;
 
+  // Set of calls to the subscript intrinsic that directly access the array
+  // address. These have the highest 'rank' value for the subscript calls. The
+  // result of this instruction is fed to the subscript call of the next lower
+  // rank, so we only need to store the initial call to get to all the others
+  // for computing profitability and transposing the stride values.
+  SmallPtrSet<CallInst *, 16> SubscriptCalls;
+
   // Indicates whether the analysis determined the candidate is safe to
   // transpose.
   bool IsValid;
@@ -158,6 +383,8 @@ public:
     bool ValidCandidate = false;
     for (auto &Cand : Candidates) {
       ValidCandidate |= Cand.analyze();
+
+      // TODO: Analyze the candidate for profitability
 
 #if !defined(NDEBUG) || defined(LLVM_ENABLE_DUMP)
       if (PrintCandidates)
