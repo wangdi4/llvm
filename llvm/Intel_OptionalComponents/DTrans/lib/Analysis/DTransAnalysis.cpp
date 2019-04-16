4a5ddc88916965106712d2c2829a2e14a17fa037
Prasad V Battini <prasad.v.battini@intel.com>
2019-04-16 14:35:05 -0400

    CMPLRLLVM-8956: Handle vector types in bitcast instructions conservatively in DTrans.
    Change-Id: Id0ef1a4acb1fa34356529068b0f6a3c8e454aa73
    Reviewed-on: https://git-amr-2.devtools.intel.com/gerrit/183642
    Reviewed-by: Chrulski, Christopher M <christopher.m.chrulski@intel.com>
    Reviewed-by: Chupina, Olga <olga.chupina@intel.com>
    Tested-by: Battini, Prasad V <prasad.v.battini@intel.com>
    Reviewed-by: Guggilla, Satish <satish.guggilla@intel.com>
    Old/split llvm commit ID: 03fe8397d5e2feb20e6799b87cfc0c33a2754012

--- llvm/Intel_OptionalComponents/DTrans/lib/Analysis/DTransAnalysis.cpp
+++ llvm/Intel_OptionalComponents/DTrans/lib/Analysis/DTransAnalysis.cpp
@@ -5292,6 +5292,20 @@ public:
   }
 
   void visitBitCastOperator(BitCastOperator *I) {
+
+    // Returns true if Ty is related to vector type.
+    auto IsRelatedToVectorType = [](llvm::Type *Ty) {
+      llvm::Type *BaseTy = Ty;
+
+      // For pointers, see what they point to.
+      while (BaseTy->isPointerTy())
+        BaseTy = cast<PointerType>(BaseTy)->getElementType();
+
+      if (BaseTy->isVectorTy())
+        return true;
+      return false;
+    };
+
     llvm::Type *DestTy = I->getDestTy();
     llvm::Value *SrcVal = I->getOperand(0);
 
@@ -5338,6 +5352,16 @@ public:
       setValueTypeInfoSafetyData(SrcVal, dtrans::BadCasting);
       return;
     }
+    // CMPLRLLVM-8956: Handle vector types conservatively for now.
+    // TODO: Analysis and transformations need to be improved for
+    // vector instructions and types.
+    if (IsRelatedToVectorType(DestTy)) {
+      LLVM_DEBUG(dbgs() << "dtrans-safety: Bad casting -- "
+                        << "pointer cast to vector type:\n"
+                        << "  " << *I << "\n");
+      setValueTypeInfoSafetyData(SrcVal, dtrans::BadCasting);
+      return;
+    }
 
     // Get the dominant aggregate type to which the source operand points.
     // Usually the value will only alias to one aggregate type. However, if
@@ -5354,6 +5378,16 @@ public:
       // In the case of multiple nested element zero types, there may be
       // more than one safe element pointee.
       for (auto &PointeePair : ElementPointees) {
+        // CMPLRLLVM-8956: Handle vector types conservatively for now.
+        // TODO: Analysis and transformations need to be improved for
+        // vector instructions and types.
+        if (IsRelatedToVectorType(PointeePair.first)) {
+          LLVM_DEBUG(dbgs() << "dtrans-safety: Bad casting -- "
+                            << "vector pointer cast to type of interest:\n"
+                            << "  " << *I << "\n");
+          setValueTypeInfoSafetyData(I, dtrans::BadCasting);
+          return;
+        }
         dtrans::TypeInfo *ParentTI =
             DTInfo.getOrCreateTypeInfo(PointeePair.first);
         llvm::Type *ElemTy;
