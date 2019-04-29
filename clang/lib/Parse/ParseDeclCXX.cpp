b88842017a037b311fe124c056baacbbd0756510
soumi.manna <smanna@scels80.sc.intel.com>
2019-04-29 02:51:51 -0700

    Remove Intel Customization for CQ#374121: Xmain cannot find suitable existing template declaration.
    This reverts commit 032913c16179f9484e82a07382951d8d9e1c7925
    
    Change-Id: Iddc1755a976bb1a76684f0cef9b4e1d4a2058580
    Reviewed-on: https://git-amr-2.devtools.intel.com/gerrit/185613
    Tested-by: alloy <lab_icla@iclgerrit.sc.intel.com>
    Reviewed-by: Rice, Michael P <michael.p.rice@intel.com>
    Reviewed-by: Guggilla, Satish <satish.guggilla@intel.com>
    Old/split clang commit ID: ec3eb8f431c1c75074853455549857552cfde7f9

--- clang/lib/Parse/ParseDeclCXX.cpp
+++ clang/lib/Parse/ParseDeclCXX.cpp
@@ -2891,11 +2891,6 @@ Parser::ParseCXXClassMemberDeclaration(AccessSpecifier AS,
         ParseCXXNonStaticMemberInitializer(ThisDecl);
     } else if (HasStaticInitializer) {
       // Normal initializer.
-#if INTEL_CUSTOMIZATION
-      // Fix for CQ374121: Xmain cannot find suitable existing template
-      // declaration.
-      Sema::ParsingFieldDeclsRAII GuardRAII(Actions, ThisDecl);
-#endif // INTEL_CUSTOMIZATION
       ExprResult Init = ParseCXXMemberInitializer(
           ThisDecl, DeclaratorInfo.isDeclarationOfFunction(), EqualLoc);
 
