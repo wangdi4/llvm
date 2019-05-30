257a091a8c36b8cd17494bf6d4e63af4530c5e7e
Robert Cox <robert.cox@intel.com>
2019-05-30 09:16:38 -0700

    CMPLRLLVM-9339: This fixes two issues with the "classic" inlining report:   (1) Inlining report should mark function as current after       InlineReportFunction for it is created.   (2) Clone of recursively inlined call site should be created from scratch,       not cloned from the original with stale information. There is still an issue with DELETE appearing on some messages in the metadata inlining report.  Olga will investigate.
    Change-Id: Ic30cca541fc26d7ab3a3e0765399a9e7462fd418
    Reviewed-on: https://git-amr-2.devtools.intel.com/gerrit/190682
    Reviewed-by: Rivera, Axel Y <axel.y.rivera@intel.com>
    Tested-by: zperf_checkin_xmain <lab_iclz@iclgerrit.sc.intel.com>
    Reviewed-by: Chupina, Olga <olga.chupina@intel.com>
    Reviewed-by: Prokofiev, Pavel G <pavel.g.prokofiev@intel.com>
    Old/split llvm commit ID: 3939b316c86c760dfcd092ced4c2fe63ad6fff2d

--- llvm/lib/Transforms/IPO/Intel_InlineReport.cpp
+++ llvm/lib/Transforms/IPO/Intel_InlineReport.cpp
@@ -60,7 +60,8 @@ InlineReportCallSite::cloneBase(const ValueToValueMapTy &IIMap,
     return nullptr;
   // If the OldCall was the ActiveInlineInstruction, we placed a nullptr
   // into the IIMap for it.
-  if (OldCall == ActiveInlineInstruction)
+  bool IsRecursiveCopy = OldCall == ActiveInlineInstruction;
+  if (IsRecursiveCopy)
     OldCall = nullptr;
   ValueToValueMapTy::const_iterator VMI = IIMap.find(OldCall);
   if (VMI == IIMap.end())
@@ -68,7 +69,16 @@ InlineReportCallSite::cloneBase(const ValueToValueMapTy &IIMap,
   if (VMI->second == nullptr)
     return nullptr;
   Instruction *NI = cast<Instruction>(VMI->second);
-  InlineReportCallSite *IRCSk = copyBase(*this, NI);
+  InlineReportCallSite *IRCSk = nullptr;
+  if (IsRecursiveCopy) {
+    // Start with a clean copy, as this is a newly created callsite produced
+    // by recursive inlining.
+    IRCSk = new InlineReportCallSite(this->IRCallee, false, NinlrNoReason,
+      this->M, nullptr, NI);
+    IRCSk->Line = this->Line;
+    IRCSk->Col = this->Col;
+  } else
+    IRCSk = copyBase(*this, NI);
   return IRCSk;
 }
 
@@ -307,7 +317,7 @@ void InlineReport::beginSCC(LazyCallGraph &CG, LazyCallGraph::SCC &SCC) {
 void InlineReport::beginFunction(Function *F) {
   if (!F || F->isDeclaration())
     return;
-  addFunction(F, M);
+  InlineReportFunction *IRF = addFunction(F, M);
   for (BasicBlock &BB : *F) {
     for (Instruction &I : BB) {
       CallSite CS(cast<Value>(&I));
@@ -330,6 +340,7 @@ void InlineReport::beginFunction(Function *F) {
         }
     }
   }
+  IRF->setCurrent(true);
 }
 
 void InlineReport::endSCC(void) {
