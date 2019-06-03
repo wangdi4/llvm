2d43774648e0be01bb105dda75873d69021f18a0
Zahira Ammarguellat <zahira.ammarguellat@intel.com>
2019-06-03 13:31:00 -0400

    Remove Intel customization CQ409175: allow class name specifiers of forward declarations.
    Change-Id: I06db7a151f92934b74d9803d58c04eebc769343e
    Reviewed-on: https://git-amr-2.devtools.intel.com/gerrit/191172
    Reviewed-by: Ammarguellat, Zahira <zahira.ammarguellat@intel.com>
    Tested-by: Ammarguellat, Zahira <zahira.ammarguellat@intel.com>
    Old/split clang commit ID: 467904ed819cf84950b6b5388e522dbd5dff4767

--- clang/lib/Sema/SemaDecl.cpp
+++ clang/lib/Sema/SemaDecl.cpp
@@ -4532,13 +4532,6 @@ Sema::ParsedFreeStandingDeclSpec(Scope *S, AccessSpecifier AS, DeclSpec &DS,
     // obvious intent of DR1819.
     //
     // Per C++ [dcl.enum]p1, an opaque-enum-declaration can't either.
-#if INTEL_CUSTOMIZATION
-    // CQ#409175: Replace error with warning
-    if (getLangOpts().IntelCompat)
-      Diag(SS.getBeginLoc(), diag::warn_standalone_class_nested_name_specifier)
-           << GetDiagnosticTypeSpecifierID(DS.getTypeSpecType()) << SS.getRange();
-    else
-#endif // INTEL_CUSTOMIZATION
     Diag(SS.getBeginLoc(), diag::err_standalone_class_nested_name_specifier)
         << GetDiagnosticTypeSpecifierID(DS.getTypeSpecType()) << SS.getRange();
     return nullptr;
--- clang/test/SemaCXX/intel/cq409175.cpp
+++
@@ -1,8 +0,0 @@
-// RUN: %clang_cc1 -fsyntax-only -verify -fintel-compatibility %s
-
-namespace NS {
-  struct S {int x,y;};
-}
-
-struct NS::S; // expected-warning {{forward declaration of struct cannot have a nested name specifier}}
-NS::S s = {1,2}; 
