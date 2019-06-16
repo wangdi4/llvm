afa2e48ed40449ea330a25dde530fcda14917040
soumi.manna <smanna@scels80.sc.intel.com>
2019-06-16 09:11:44 -0700

    Remove Intel Customization for CQ375353: Allow casting of const char[] to void* in intel ms compat mode.
    This patch reverts commit 8244e97460c7a85ea5289efb2f9636932644505c
    
    Change-Id: I9fd42fd1a966d019ae554698609ac2a8fd2ac97b
    Reviewed-on: https://git-amr-2.devtools.intel.com/gerrit/193655
    Tested-by: zperf_checkin_xmain <lab_iclz@iclgerrit.sc.intel.com>
    Tested-by: xmain_checkin <lab_iclt@iclgerrit.sc.intel.com>
    Reviewed-by: Keane, Erich <erich.keane@intel.com>
    Reviewed-by: Prokofiev, Pavel G <pavel.g.prokofiev@intel.com>
    Old/split clang commit ID: d39da32792280a16414e0be3f32483b8bdf3a481

--- clang/lib/Sema/SemaExprCXX.cpp
+++ clang/lib/Sema/SemaExprCXX.cpp
@@ -3720,23 +3720,9 @@ Sema::IsStringLiteralToNonConstPointerConversion(Expr *From, QualType ToType) {
               // We don't allow UTF literals to be implicitly converted
               break;
             case StringLiteral::Ascii:
-#if INTEL_CUSTOMIZATION
-              // Fix for CQ375353: Allow casting of const char[] to void* in
-              // intel ms compat mode.
-              if (getLangOpts().IntelCompat && getLangOpts().IntelMSCompat &&
-                  ToPointeeType->getKind() == BuiltinType::Void)
-                return true;
-#endif // INTEL_CUSTOMIZATION
               return (ToPointeeType->getKind() == BuiltinType::Char_U ||
                       ToPointeeType->getKind() == BuiltinType::Char_S);
             case StringLiteral::Wide:
-#if INTEL_CUSTOMIZATION
-              // Fix for CQ375353: Allow casting of const char[] to void* in
-              // intel ms compat mode.
-              if (getLangOpts().IntelCompat && getLangOpts().IntelMSCompat &&
-                  ToPointeeType->getKind() == BuiltinType::Void)
-                return true;
-#endif // INTEL_CUSTOMIZATION
               return Context.typesAreCompatible(Context.getWideCharType(),
                                                 QualType(ToPointeeType, 0));
           }
