1bc12ceb3fd2cfd903c5d53587075a5955e49c2b
Viktoria Maksimova <viktoria.maksimova@intel.com>
2019-06-21 12:43:27 +0300

    Fixed and enabled back CreateProgramWithIL* tests
    Change-Id: I07f3405ecc7e4d165826cced1143823a0d256d6d
    Reviewed-on: https://git-amr-2.devtools.intel.com/gerrit/194766
    Reviewed-by: Maksimova, Viktoria <viktoria.maksimova@intel.com>
    Tested-by: Maksimova, Viktoria <viktoria.maksimova@intel.com>

--- opencl-intel/tests/test_utils/export/common_utils.h
+++ opencl-intel/tests/test_utils/export/common_utils.h
@@ -1,13 +1,14 @@
 //==--- common_utils.h - Common utility functions for unittests -*- C++ -*--==//
 //
-// Copyright (C) 2017 Intel Corporation. All rights reserved.
+// Copyright (C) 2017-2019 Intel Corporation. All rights reserved.
 //
 // The information and source code contained herein is the exclusive property
 // of Intel Corporation and may not be disclosed, examined or reproduced in
 // whole or in part without explicit written authorization from the company.
 //
 // ===--------------------------------------------------------------------=== //
-
+#ifndef __COMMON_UTILS_H__
+#define __COMMON_UTILS_H__
 #include <string>
 #include <cstdio>
 
@@ -25,3 +26,5 @@ std::string get_exe_dir(unsigned int pid = 0);
 #define SETENV(NAME,VALUE)      (setenv(NAME,VALUE,1) == 0)
 #define UNSETENV(NAME)          (unsetenv(NAME) == 0)
 #endif
+
+#endif /*__COMMON_UTILS_H__*/
