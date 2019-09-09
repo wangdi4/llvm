2153a961b599aab38d696c1932edaaba3d5e7cec
Xiang1 Zhang <xiang1.zhang@intel.com>
2019-09-08 22:52:39 -0700

    Adjust CET test after 9c37130 [ELF][X86] Allow PT_LOAD to have overlapping p_offset ranges on EM_386
    Change-Id: I52af06455eab14489dc0267fbdb977ff39d050c7
    Reviewed-on: https://git-amr-2.devtools.intel.com/gerrit/208606
    Reviewed-by: Rivera, Axel Y <axel.y.rivera@intel.com>
    Reviewed-by: Bakhvalov, Denis <denis.bakhvalov@intel.com>
    Tested-by: Bakhvalov, Denis <denis.bakhvalov@intel.com>
    Reviewed-on: https://git-amr-2.devtools.intel.com/gerrit/208973

--- lld/test/ELF/i386-cet.s
+++ lld/test/ELF/i386-cet.s
@@ -36,31 +36,28 @@
 # RUN: llvm-objdump -s -d %t | FileCheck -check-prefix=DISASM %s
 
 # DISASM:      Disassembly of section .text:
-# DISASM:      0040124c func1:
-# DISASM-NEXT:   40124c: e8 2f 00 00 00                calll   47 <func2+0x401280>
-# DISASM-NEXT:   401251: c3                            retl
+# DISASM:      {{.*}} func1:
+# DISASM-NEXT: {{.*}}              calll {{.*}}
+# DISASM-NEXT: {{.*}}              retl
 
 # DISASM:      Disassembly of section .plt:
-# DISASM:      00401260 .plt:
-# DISASM-NEXT:   401260: ff 35 fc 32 40 00             pushl   4207356
-# DISASM-NEXT:   401266: ff 25 00 33 40 00             jmpl    *4207360
-# DISASM-NEXT:   40126c: 90                            nop
-# DISASM-NEXT:   40126d: 90                            nop
-# DISASM-NEXT:   40126e: 90                            nop
-# DISASM-NEXT:   40126f: 90                            nop
-# DISASM-NEXT:   401270: f3 0f 1e fb                   endbr32
-# DISASM-NEXT:   401274: 68 00 00 00 00                pushl   $0
-# DISASM-NEXT:   401279: e9 e2 ff ff ff                jmp     -30 <.plt>
-# DISASM-NEXT:   40127e: 66 90                         nop
+# DISASM:      {{.*}}.plt:
+# DISASM-NEXT: {{.*}} pushl {{.*}}
+# DISASM-NEXT: {{.*}} jmpl  {{.*}}
+# DISASM-NEXT: {{.*}} nop
+# DISASM-NEXT: {{.*}} nop
+# DISASM-NEXT: {{.*}} nop
+# DISASM-NEXT: {{.*}} nop
+# DISASM-NEXT: {{.*}} endbr32
+# DISASM-NEXT: {{.*}} pushl   $0
+# DISASM-NEXT: {{.*}} jmp     -30 <.plt>
+# DISASM-NEXT: {{.*}} nop
 
 # DISASM:      Disassembly of section .plt.sec:
-# DISASM:      00401280 .plt.sec:
-# DISASM-NEXT:   401280: f3 0f 1e fb                   endbr32
-# DISASM-NEXT:   401284: ff 25 04 33 40 00             jmpl    *4207364
-# DISASM-NEXT:   40128a: 66 0f 1f 44 00 00             nopw    (%eax,%eax)
-
-# DISASM:      Contents of section .got.plt:
-# DISASM-NEXT:   4032f8 90224000 00000000 00000000 70124000  ."@.........p.@.
+# DISASM:      {{.*}}.plt.sec:
+# DISASM-NEXT: {{.*}} endbr32
+# DISASM-NEXT: {{.*}} jmpl  {{.*}}
+# DISASM-NEXT: {{.*}} nopw  {{.*}}
 # end INTEL_CUSTOMIZATION
 
 .section ".note.gnu.property", "a"
