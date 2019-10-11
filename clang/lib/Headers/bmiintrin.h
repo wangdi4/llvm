77f5e13a54815fe2fb04adf74cb3d636fb5edf76
Craig Topper <craig.topper@intel.com>
2019-10-11 16:55:08 -0700

    Resolve conflicts after 282eff38477ebf665f88f52411edd591067af883 [X86] Always define the tzcnt intrinsics even when _MSC_VER is defined.
    Change-Id: Icad0055473c859e16052dbbbd5ea5125beafa569
    Reviewed-on: https://git-amr-2.devtools.intel.com/gerrit/214541
    Reviewed-by: Topper, Craig <craig.topper@intel.com>
    Tested-by: Topper, Craig <craig.topper@intel.com>

--- clang/lib/Headers/bmiintrin.h
+++ clang/lib/Headers/bmiintrin.h
@@ -111,7 +111,7 @@ _mm_tzcnt_64(unsigned long long __X)
 
 #undef __RELAXED_FN_ATTRS
 
-#if !defined(_MSC_VER) || __has_feature(modules) || defined(__BMI__)
+#if !defined(_MSC_VER) || __has_feature(modules) || defined(__BMI__) || defined(__M_INTRINSIC_PROMOTE__)
 
 /* Define the default attributes for the functions in this file. */
 #define __DEFAULT_FN_ATTRS __attribute__((__always_inline__, __nodebug__, __target__("bmi")))
