a51f280ccbbf993e44fe38288058c1cb2f0b50d8
Sergey Dmitriev <serguei.n.dmitriev@intel.com>
2019-11-05 09:10:50 -0800

    Resolve conflicts after 0016c1f4004 (tag: sycl-web/master-latest-good) [JumpThreading] Factor out common code to update the SSA form (NFC)
    Change-Id: Ied047dcd26f6bd849c4672be18ee50237c3e729b
    Reviewed-on: https://git-amr-2.devtools.intel.com/gerrit/218574
    Reviewed-by: Dmitriev, Serguei N <serguei.n.dmitriev@intel.com>
    Tested-by: Dmitriev, Serguei N <serguei.n.dmitriev@intel.com>

--- llvm/include/llvm/Transforms/Scalar/JumpThreading.h
+++ llvm/include/llvm/Transforms/Scalar/JumpThreading.h
@@ -164,14 +164,10 @@ public:
 
   void FindLoopHeaders(Function &F);
   bool ProcessBlock(BasicBlock *BB);
-<<<<<<< HEAD
-  bool ThreadEdge(const jumpthreading::ThreadRegionInfo &RegionInfo,  // INTEL
-                  const SmallVectorImpl<BasicBlock*> &PredBBs,        // INTEL
-=======
   void UpdateSSA(BasicBlock *BB, BasicBlock *NewBB,
                  DenseMap<Instruction *, Value *> &ValueMapping);
-  bool ThreadEdge(BasicBlock *BB, const SmallVectorImpl<BasicBlock *> &PredBBs,
->>>>>>> 0016c1f40043e67068230208fd8f34657ad44b99
+  bool ThreadEdge(const jumpthreading::ThreadRegionInfo &RegionInfo,  // INTEL
+                  const SmallVectorImpl<BasicBlock*> &PredBBs,        // INTEL
                   BasicBlock *SuccBB);
   bool DuplicateCondBranchOnPHIIntoPred(
       BasicBlock *BB, const SmallVectorImpl<BasicBlock *> &PredBBs);
