ec212ad50e7fdae3923d3197c81d1fdcffc95dce
soumi.manna <smanna@scels80.sc.intel.com>
2019-11-08 09:56:46 -0800

    Remove Intel Customization for CQ#366309 and CQ#380872
    CQ#366309 - compilation of arrays of structures that are not yet defined
    This patch reverts commit c91d1a4a7d83363db92fe8ae77f0ca9e015f1808
    Original bug tracker: https://jira.devtools.intel.com/browse/CMPLRLLVM-3869
    
    CQ#380872: Arrays with incomplete (unknown) size
    This patch reverts commit 2394f55cf26d24f3585bd59c8f0e1a3cc1325e57
    Original bug tracker: https://jira.devtools.intel.com/browse/CMPLRLLVM-5525
    
    Change-Id: Idcb1b5af2a0a79882a84534d3a1a5ad91ea41b85
    Reviewed-on: https://git-amr-2.devtools.intel.com/gerrit/219284
    Reviewed-by: Manna, Soumi <soumi.manna@intel.com>
    Tested-by: Manna, Soumi <soumi.manna@intel.com>

--- clang/test/CodeGen/intel/cq366309.c
+++
@@ -1,9 +0,0 @@
-// RUN: %clang_cc1 -fintel-compatibility -triple x86_64-unknown-linux-gnu -O0 -emit-llvm %s -o - | FileCheck %s
-
-// CHECK: @{{.+}} = common global [3 x %struct.A]*
-struct A (*x)[3];
-
-struct A {
-  int a;
-  int b;
-};
