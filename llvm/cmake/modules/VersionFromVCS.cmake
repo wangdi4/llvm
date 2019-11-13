0ca1952a39d960ca56fe64fc22aae42cd6bc084a
Tianqing Wang <tianqing.wang@intel.com>
2019-11-13 21:20:32 +0800

    Resolve conflicts after caad2170aed [cmake] Remove SVN support from VersionFromVCS.cmake.
    Change-Id: I2602239f02233474b2d11a082ef9ee59e2044650
    Reviewed-on: https://git-amr-2.devtools.intel.com/gerrit/219943
    Reviewed-by: Tianqing Wang <tianqing.wang@intel.com>
    Tested-by: Tianqing Wang <tianqing.wang@intel.com>

--- llvm/cmake/modules/VersionFromVCS.cmake
+++ llvm/cmake/modules/VersionFromVCS.cmake
@@ -3,34 +3,7 @@
 # existence of certain subdirectories under SOURCE_DIR (if provided as an
 # extra argument, otherwise uses CMAKE_CURRENT_SOURCE_DIR).
 
-<<<<<<< HEAD
-function(get_source_info_svn path revision repository)
-  # If svn is a bat file, find_program(Subversion) doesn't find it.
-  # Explicitly search for that here; Subversion_SVN_EXECUTABLE will override
-  # the find_program call in FindSubversion.cmake.
-  find_program(Subversion_SVN_EXECUTABLE NAMES svn svn.bat)
-  find_package(Subversion)
-
-  # Subversion module does not work with symlinks, see PR8437.
-  get_filename_component(realpath ${path} REALPATH)
-  if(Subversion_FOUND)
-    subversion_wc_info(${realpath} Project)
-    if(Project_WC_REVISION)
-      set(${revision} ${Project_WC_REVISION} PARENT_SCOPE)
-    endif()
-    # INTEL_CUSTOMIZATION
-    # -- Don't expose our repository location --
-    # if(Project_WC_URL)
-    #   set(${repository} ${Project_WC_URL} PARENT_SCOPE)
-    # endif()
-    # end INTEL_CUSTOMIZATION
-  endif()
-endfunction()
-
-function(get_source_info_git path revision repository)
-=======
 function(get_source_info path revision repository)
->>>>>>> caad2170aed76d1df8b4305b1b7d81c4943626db
   find_package(Git)
   if(GIT_FOUND)
     execute_process(COMMAND ${GIT_EXECUTABLE} rev-parse --git-dir
@@ -41,58 +14,6 @@ function(get_source_info path revision repository)
     if(git_result EQUAL 0)
       string(STRIP "${git_output}" git_output)
       get_filename_component(git_dir ${git_output} ABSOLUTE BASE_DIR ${path})
-<<<<<<< HEAD
-      if(EXISTS "${git_dir}/svn/refs")
-        execute_process(COMMAND ${GIT_EXECUTABLE} svn info
-          WORKING_DIRECTORY ${path}
-          RESULT_VARIABLE git_result
-          OUTPUT_VARIABLE git_output)
-        if(git_result EQUAL 0)
-          string(REGEX REPLACE "^(.*\n)?Revision: ([^\n]+).*"
-            "\\2" git_svn_rev "${git_output}")
-          set(${revision} ${git_svn_rev} PARENT_SCOPE)
-          # INTEL_CUSTOMIZATION
-          # -- Don't expose our repository location --
-          # string(REGEX REPLACE "^(.*\n)?URL: ([^\n]+).*"
-          #   "\\2" git_url "${git_output}")
-          # set(${repository} ${git_url} PARENT_SCOPE)
-          # end INTEL_CUSTOMIZATION
-        endif()
-      else()
-        execute_process(COMMAND ${GIT_EXECUTABLE} rev-parse HEAD
-          WORKING_DIRECTORY ${path}
-          RESULT_VARIABLE git_result
-          OUTPUT_VARIABLE git_output)
-        if(git_result EQUAL 0)
-          string(STRIP "${git_output}" git_output)
-          set(${revision} ${git_output} PARENT_SCOPE)
-        endif()
-        execute_process(COMMAND ${GIT_EXECUTABLE} rev-parse --abbrev-ref --symbolic-full-name @{upstream}
-          WORKING_DIRECTORY ${path}
-          RESULT_VARIABLE git_result
-          OUTPUT_VARIABLE git_output
-          ERROR_QUIET)
-        if(git_result EQUAL 0)
-          string(REPLACE "/" ";" branch ${git_output})
-          list(GET branch 0 remote)
-        else()
-          set(remote "origin")
-        endif()
-        # INTEL_CUSTOMIZATION
-        # -- Don't expose our repository location --
-        # execute_process(COMMAND ${GIT_EXECUTABLE} remote get-url ${remote}
-        #   WORKING_DIRECTORY ${path}
-        #   RESULT_VARIABLE git_result
-        #   OUTPUT_VARIABLE git_output
-        #   ERROR_QUIET)
-        # if(git_result EQUAL 0)
-        #   string(STRIP "${git_output}" git_output)
-        #   set(${repository} ${git_output} PARENT_SCOPE)
-        # else()
-        #   set(${repository} ${path} PARENT_SCOPE)
-        # endif()
-        # end INTEL_CUSTOMIZATION
-=======
       execute_process(COMMAND ${GIT_EXECUTABLE} rev-parse HEAD
         WORKING_DIRECTORY ${path}
         RESULT_VARIABLE git_result
@@ -112,18 +33,20 @@ function(get_source_info path revision repository)
       else()
         set(remote "origin")
       endif()
-      execute_process(COMMAND ${GIT_EXECUTABLE} remote get-url ${remote}
-        WORKING_DIRECTORY ${path}
-        RESULT_VARIABLE git_result
-        OUTPUT_VARIABLE git_output
-        ERROR_QUIET)
-      if(git_result EQUAL 0)
-        string(STRIP "${git_output}" git_output)
-        set(${repository} ${git_output} PARENT_SCOPE)
-      else()
-        set(${repository} ${path} PARENT_SCOPE)
->>>>>>> caad2170aed76d1df8b4305b1b7d81c4943626db
-      endif()
+      # INTEL_CUSTOMIZATION
+      # -- Don't expose our repository location --
+      # execute_process(COMMAND ${GIT_EXECUTABLE} remote get-url ${remote}
+      #   WORKING_DIRECTORY ${path}
+      #   RESULT_VARIABLE git_result
+      #   OUTPUT_VARIABLE git_output
+      #   ERROR_QUIET)
+      # if(git_result EQUAL 0)
+      #   string(STRIP "${git_output}" git_output)
+      #   set(${repository} ${git_output} PARENT_SCOPE)
+      # else()
+      #   set(${repository} ${path} PARENT_SCOPE)
+      # endif()
+      # end INTEL_CUSTOMIZATION
     endif()
   endif()
 endfunction()
