e441cecb0ffa247a134a197a18e600c5ac65094c
Tianqing Wang <tianqing.wang@intel.com>
2019-11-14 15:08:20 +0800

    Resolve conflicts after 637a178b7a0 [SYCL][PI] Fix unittests for missing PI platforms.
    Change-Id: Ieaf3f0655f8cf1a28a9412d7548488617ea18f81
    Reviewed-on: https://git-amr-2.devtools.intel.com/gerrit/220104
    Tested-by: Tianqing Wang <tianqing.wang@intel.com>
    Reviewed-by: Dmitriev, Serguei N <serguei.n.dmitriev@intel.com>
    Tested-by: Dmitriev, Serguei N <serguei.n.dmitriev@intel.com>

--- sycl/unittests/pi/PlatformTest.cpp
+++ sycl/unittests/pi/PlatformTest.cpp
@@ -81,34 +81,12 @@ TEST_F(PlatformTest, piPlatformGetInfo) {
         << ".\n";
   };
 
-<<<<<<< HEAD
-  pi_uint32 platformCount = 0;
-  PI_CALL(RT::piPlatformsGet(0, 0, &platformCount));
-#if INTEL_CUSTOMIZATION
-  if (platformCount < 1u)
-    return;
-#endif // INTEL_CUSTOMIZATION
-  std::vector<pi_platform> platforms(platformCount);
-  PI_CALL(RT::piPlatformsGet(platformCount, platforms.data(), nullptr));
-
-  auto out_string_buffer = std::unique_ptr<char[]>(new char[out_string_size]);
-  auto out_string = out_string_buffer.get();
-
-  for (auto i = 0u; i < platformCount; ++i) {
-    const auto &platform = platforms[i];
-    get_info_test(out_string, platform, PI_PLATFORM_INFO_NAME);
-    get_info_test(out_string, platform, PI_PLATFORM_INFO_VENDOR);
-    get_info_test(out_string, platform, PI_PLATFORM_INFO_PROFILE);
-    get_info_test(out_string, platform, PI_PLATFORM_INFO_VERSION);
-    get_info_test(out_string, platform, PI_PLATFORM_INFO_EXTENSIONS);
-=======
   for (const auto &platform : _platforms) {
     get_info_test(platform, PI_PLATFORM_INFO_NAME);
     get_info_test(platform, PI_PLATFORM_INFO_VENDOR);
     get_info_test(platform, PI_PLATFORM_INFO_PROFILE);
     get_info_test(platform, PI_PLATFORM_INFO_VERSION);
     get_info_test(platform, PI_PLATFORM_INFO_EXTENSIONS);
->>>>>>> 637a178b7a0fe9ef59665cd330ebfa2e8a97de09
   }
 }
 } // namespace
