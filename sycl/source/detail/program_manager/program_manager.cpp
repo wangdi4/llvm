b9c917b40ab4e7e79e24e424ba1f411214019bd2
Sergey Dmitriev <serguei.n.dmitriev@intel.com>
2019-11-15 14:45:54 -0800

    Resolve conflicts after d6023e8bbed [SYCL] Re-route PI_CALL macro to PI_TRACE (#798)
    Change-Id: I1827261fd64d7b51df41385fa61cc9b15d4babcf
    Reviewed-on: https://git-amr-2.devtools.intel.com/gerrit/220487
    Reviewed-by: Dmitriev, Serguei N <serguei.n.dmitriev@intel.com>
    Tested-by: Dmitriev, Serguei N <serguei.n.dmitriev@intel.com>

--- sycl/source/detail/program_manager/program_manager.cpp
+++ sycl/source/detail/program_manager/program_manager.cpp
@@ -38,34 +38,18 @@ ProgramManager &ProgramManager::getInstance() {
 }
 
 static RT::PiDevice getFirstDevice(RT::PiContext Context) {
-<<<<<<< HEAD
-  pi_uint32 NumDevices = 0;
-  PI_CALL(RT::piContextGetInfo(Context, PI_CONTEXT_INFO_NUM_DEVICES,
-                               sizeof(NumDevices), &NumDevices,
-                               /*param_value_size_ret=*/nullptr));
-=======
   cl_uint NumDevices = 0;
   PI_CALL(RT::piContextGetInfo, Context, PI_CONTEXT_INFO_NUM_DEVICES,
           sizeof(NumDevices), &NumDevices,
           /*param_value_size_ret=*/nullptr);
->>>>>>> d6023e8bbed2591a5987c8ea78b28e0fedf92525
   assert(NumDevices > 0 && "Context without devices?");
 
   vector_class<RT::PiDevice> Devices(NumDevices);
   size_t ParamValueSize = 0;
-<<<<<<< HEAD
-  PI_CALL(RT::piContextGetInfo(Context, PI_CONTEXT_INFO_DEVICES,
-                               sizeof(RT::PiDevice) * NumDevices, &Devices[0],
-                               &ParamValueSize));
-  assert(ParamValueSize == sizeof(RT::PiDevice) * NumDevices &&
-         "Number of PI_CONTEXT_INFO_DEVICES should match "
-         "PI_CONTEXT_INFO_NUM_DEVICES.");
-=======
   PI_CALL(RT::piContextGetInfo, Context, PI_CONTEXT_INFO_DEVICES,
           sizeof(cl_device_id) * NumDevices, &Devices[0], &ParamValueSize);
   assert(ParamValueSize == sizeof(cl_device_id) * NumDevices &&
          "Number of CL_CONTEXT_DEVICES should match CL_CONTEXT_NUM_DEVICES.");
->>>>>>> d6023e8bbed2591a5987c8ea78b28e0fedf92525
   return Devices[0];
 }
 
