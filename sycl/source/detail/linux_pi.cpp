b48c7cbaff1a6cbfd97ac8ca806feca30b410f24
Sergey V Maslov <sergey.v.maslov@intel.com>
2019-11-26 15:53:25 -0800

    [SYCL]: L0 plugin sync with SYCL RT
    Change-Id: Ifdcd501961fa9a00b5476c4c41a95e96425fd757
    Signed-off-by: Sergey V Maslov <sergey.v.maslov@intel.com>
    Reviewed-on: https://git-amr-2.devtools.intel.com/gerrit/222133
    Reviewed-by: Begam, Rehana <rehana.begam@intel.com>
    Reviewed-by: Narayanaswamy, Ravi <ravi.narayanaswamy@intel.com>

--- sycl/source/detail/linux_pi.cpp
+++ sycl/source/detail/linux_pi.cpp
@@ -1,10 +1,18 @@
 #include <dlfcn.h>
 #include <string>
+#include <iostream>
 
 void *loadOsLibrary(const std::string &PluginPath) {
   // TODO: Check if the option RTLD_NOW is correct. Explore using
   // RTLD_DEEPBIND option when there are multiple plugins.
-  return dlopen(PluginPath.c_str(), RTLD_NOW);
+#if INTEL_CUSTOMIZATION
+  void *so = dlopen(PluginPath.c_str(), RTLD_NOW);
+  if (!so) {
+    std::cerr << "dlopen(" << PluginPath << ") failed with <" <<
+      dlerror() << ">" << std::endl;
+  }
+  return so;
+#endif // INTEL_CUSTOMIZATION
 }
 
 void *getOsLibraryFuncAddress(void *Library, const std::string &FunctionName) {
