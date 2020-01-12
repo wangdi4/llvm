d826b00054cf2ca9b9ddb2a2a6b6e61de76c7af2
Wenju He <wenju.he@intel.com>
2020-01-12 18:25:40 +0800

    [UnitTest] Add test of extension names
    Check extension names queried from clGetDeviceInfo are the same as
    reference extension names.
    
    Change-Id: Ifdf1736f1f72dc6b9a29a4153ccd5d9d689f49e1
    Reviewed-on: https://git-amr-2.devtools.intel.com/gerrit/227967
    Tested-by: xmain_checkin <lab_iclt@iclgerrit.sc.intel.com>
    Reviewed-by: Dai, Chunyang <chunyang.dai@intel.com>
    Reviewed-by: Romanov, Vlad <vlad.romanov@intel.com>

--- opencl-intel/tests/framework_test_type/checkExtensions.cpp
+++ opencl-intel/tests/framework_test_type/checkExtensions.cpp
@@ -1,133 +1,110 @@
-#include "CL.h"
-#include <string>
+// Copyright (c) 2020 Intel Corporation
+// All rights reserved.
+//
+// WARRANTY DISCLAIMER
+//
+// THESE MATERIALS ARE PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+// "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+// LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+// A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL INTEL OR ITS
+// CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+// EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+// PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+// PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY
+// OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY OR TORT (INCLUDING
+// NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THESE
+// MATERIALS, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+//
+// Intel Corporation is the author of the Materials, and requests that all
+// problem reports or change requests be submitted to it directly
 
-class Extensions
-{
-    std::string m_ext;
+#include "TestsHelpClasses.h"
+#include <algorithm>
+#include <iterator>
+#include <sstream>
 
-    bool IsExtSupported(const std::string& ext)
-    {
-        return m_ext.find(ext) != std::string::npos;
-    }
-public:
-    Extensions(const std::string& ext):
-        m_ext(ext)
-    {}
+extern cl_device_type gDeviceType;
 
-    bool IsSpirSupported()
-    {
-        return IsExtSupported("cl_khr_spir");
-    }
+class CheckExtensions : public ::testing::Test {
+protected:
+  virtual void SetUp() {
+    cl_int err = clGetPlatformIDs(1, &m_platform, NULL);
+    ASSERT_OCL_SUCCESS(err, "clGetPlatformIDs");
 
-    bool IsICDSupported()
-    {
-        return IsExtSupported("cl_khr_icd");
-    }
+    err = clGetDeviceIDs(m_platform, gDeviceType, 1, &m_device, NULL);
+    ASSERT_OCL_SUCCESS(err, "clGetDeviceIDs");
+  }
 
-    bool IsDX9Supported()
-    {
-        return IsExtSupported("cl_khr_dx9_media_sharing");
-    }
-
-    bool IsDX9Supported_INTEL()
-    {
-        return IsExtSupported("cl_intel_dx9_media_sharing");
-    }
-
-    bool IsDX11Supported()
-    {
-        return IsExtSupported("cl_khr_d3d11_sharing");
-    }
-
-    bool IsGLSupported()
-    {
-        return IsExtSupported("cl_khr_gl_sharing");
-    }
-
-    bool IsLocalThreadSupported_INTEL()
-    {
-        return IsExtSupported("cl_intel_exec_by_local_thread");
-    }
-
-    bool IsVecLenHintSupported()
-    {
-        return IsExtSupported("cl_intel_vec_len_hint");
-    }
-
-    bool IsDevicePartitionByNamesSupported_INTEL()
-    {
-        return IsExtSupported("cl_intel_device_partition_by_names");
-    }
+protected:
+  cl_platform_id m_platform;
+  cl_device_id m_device;
 };
 
-void CL::CheckExtensions()
-{
-#if defined (_WIN32)
-    std::string os("windows.");
-#else
-    std::string os("linux.");
+TEST_F(CheckExtensions, CpuDevice) {
+  // Query list of extension names from clGetPlatformInfo/clGetDeviceInfo
+  size_t retSize;
+  cl_int err = clGetPlatformInfo(m_platform, CL_PLATFORM_EXTENSIONS, 0, nullptr,
+                                 &retSize);
+  ASSERT_OCL_SUCCESS(err, "clGetPlatformInfo");
+
+  std::string extensionsPlatform(retSize, '\0');
+  err = clGetPlatformInfo(m_platform, CL_PLATFORM_EXTENSIONS,
+                          extensionsPlatform.size(), &extensionsPlatform[0],
+                          nullptr);
+  ASSERT_OCL_SUCCESS(err, "clGetPlatformInfo");
+  err = clGetDeviceInfo(m_device, CL_DEVICE_EXTENSIONS, 0, nullptr, &retSize);
+  ASSERT_OCL_SUCCESS(err, "clGetPlatformInfo");
+
+  std::string extensions(retSize, '\0');
+  err = clGetDeviceInfo(m_device, CL_DEVICE_EXTENSIONS, extensions.size(),
+                        &extensions[0], nullptr);
+  ASSERT_OCL_SUCCESS(err, "clGetPlatformInfo");
+
+  ASSERT_TRUE(extensionsPlatform == extensions)
+      << " Expected that platform and device extensions are equal!";
+
+  // Reference CPU extensions. Update this list if supported extension names
+  // changes.
+  std::set<std::string> extRefCPU = {"cl_khr_icd",
+                                     "cl_khr_global_int32_base_atomics",
+                                     "cl_khr_global_int32_extended_atomics",
+                                     "cl_khr_local_int32_base_atomics",
+                                     "cl_khr_local_int32_extended_atomics",
+                                     "cl_khr_int64_base_atomics",
+                                     "cl_khr_int64_extended_atomics",
+                                     "cl_khr_byte_addressable_store",
+                                     "cl_khr_depth_images",
+                                     "cl_khr_3d_image_writes",
+                                     "cl_khr_il_program",
+                                     "cl_intel_unified_shared_memory_preview",
+                                     "cl_intel_exec_by_local_thread",
+                                     "cl_intel_vec_len_hint",
+#ifndef _WIN32
+                                     "cl_intel_device_partition_by_names",
 #endif
+                                     "cl_khr_spir",
+                                     "cl_khr_fp64",
+                                     "cl_khr_image2d_from_buffer"};
 
-    cl_int iRet = CL_SUCCESS;
+  // Remove trailing '\0'
+  extensions.erase(std::find(extensions.begin(), extensions.end(), '\0'),
+                   extensions.end());
+  // Check each queried extension is present in reference extensions
+  std::istringstream extss(extensions);
+  for (auto i = std::istream_iterator<std::string>(extss),
+            e = std::istream_iterator<std::string>();
+       i != e; ++i) {
+    ASSERT_EQ(extRefCPU.count(*i), 1)
+        << ("Expect " + (*i) + " exists once in reference extensions");
+    extRefCPU.erase(*i);
+  }
 
-    size_t ret_size = 0;
-    iRet = clGetPlatformInfo(m_platform,
-                             CL_PLATFORM_EXTENSIONS,
-                             /*param_value_size*/0,
-                             /*param_value*/nullptr,
-                             &ret_size);
-    ASSERT_EQ(CL_SUCCESS, iRet) << " clGetPlatformInfo failed. ";
-
-    std::string ext_string(ret_size, '\0');
-
-    iRet = clGetPlatformInfo(m_platform,
-                             CL_PLATFORM_EXTENSIONS,
-                             ext_string.size(),
-                             &ext_string[0],
-                             &ret_size);
-    ASSERT_EQ(CL_SUCCESS, iRet) << " clGetPlatformInfo failed. ";
-
-    iRet = clGetDeviceInfo(m_device,
-                           CL_DEVICE_EXTENSIONS,
-                           /*param_value_size*/0,
-                           /*param_value*/nullptr,
-                           &ret_size);
-    ASSERT_EQ(CL_SUCCESS, iRet) << " clGetPlatformInfo failed. ";
-
-    std::string device_ext_string(ret_size, '\0');
-
-    iRet = clGetDeviceInfo(m_device,
-                           CL_DEVICE_EXTENSIONS,
-                           device_ext_string.size(),
-                           &device_ext_string[0],
-                           &ret_size);
-    ASSERT_EQ(CL_SUCCESS, iRet) << " clGetPlatformInfo failed. ";
-
-    ASSERT_TRUE(ext_string == device_ext_string)
-        << " Expected that platform and device extensions are equal!";
-
-    Extensions extensions(ext_string);
-
-    ASSERT_TRUE(extensions.IsSpirSupported())
-        << " Expected that cl_khr_spir is not supported on " << os;
-
-    ASSERT_TRUE(extensions.IsICDSupported())
-        << " Expected that cl_khr_icd  is not supported on " << os;
-
-    ASSERT_TRUE(extensions.IsVecLenHintSupported())
-        << " Expected that cl_intel_vec_len_hint  is supported on " << os;
-
-#if defined (_WIN32)
-    ASSERT_TRUE(extensions.IsLocalThreadSupported_INTEL())
-        << " Expected that cl_intel_exec_by_local_thread is supported on "
-        << os;
-#else //LINUX
-    ASSERT_TRUE(extensions.IsLocalThreadSupported_INTEL())
-        << " Expected that cl_intel_exec_by_local_thread is supported on "
-        << os;
-
-    ASSERT_TRUE(extensions.IsDevicePartitionByNamesSupported_INTEL())
-        << " Expected that cl_intel_device_partition_by_names is supported"
-           " on " << os;
-#endif
+  // Check extRefCPU is empty.
+  if (!extRefCPU.empty()) {
+    std::ostringstream ss;
+    std::copy(extRefCPU.begin(), extRefCPU.end(),
+              std::ostream_iterator<std::string>(ss, ","));
+    FAIL() << ("Reference extensions " + ss.str() +
+               " are not in extensions queried from clGetDeviceInfo");
+  }
 }
