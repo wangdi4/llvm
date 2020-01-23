785c99c6a9b28b7386c8c9f3b3fec10e4ac2c88c
Sergey Dmitriev <serguei.n.dmitriev@intel.com>
2020-01-23 02:35:14 -0800

    Resolve conflicts after e57a9ab [Concepts] Placeholder constraints and abbreviated templates
    Change-Id: I88ba25acd5e5fa3026a67a56331cb0c9db1c9495
    Reviewed-on: https://git-amr-2.devtools.intel.com/gerrit/229588
    Reviewed-by: Dmitriev, Serguei N <serguei.n.dmitriev@intel.com>
    Tested-by: Dmitriev, Serguei N <serguei.n.dmitriev@intel.com>

--- clang/include/clang/Sema/DeclSpec.h
+++ clang/include/clang/Sema/DeclSpec.h
@@ -441,15 +441,11 @@ public:
         TypeSpecComplex(TSC_unspecified), TypeSpecSign(TSS_unspecified),
         TypeSpecType(TST_unspecified), TypeAltiVecVector(false),
         TypeAltiVecPixel(false), TypeAltiVecBool(false), TypeSpecOwned(false),
-<<<<<<< HEAD
 #if INTEL_CUSTOMIZATION
         TypeSpecChannel(false),
 #endif // INTEL_CUSTOMIZATION
-        TypeSpecPipe(false), TypeSpecSat(false), TypeQualifiers(TQ_unspecified),
-=======
         TypeSpecPipe(false), TypeSpecSat(false), ConstrainedAuto(false),
         TypeQualifiers(TQ_unspecified),
->>>>>>> e57a9abc4b01fa69fe81ace8df70517983b6cbac
         FS_inline_specified(false), FS_forceinline_specified(false),
         FS_virtual_specified(false), FS_noreturn_specified(false),
         Friend_specified(false), ConstexprSpecifier(CSK_unspecified),
