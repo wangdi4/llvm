f7e16e5b9b3a77b35548d0da39ee6883be54ee06
Sergey Dmitriev <serguei.n.dmitriev@intel.com>
2020-01-24 07:24:39 -0800

    Resolve conflicts after c467faf [WinEH] Ignore lifetime.end PHI nodes in empty cleanuppads
    Change-Id: Ib966679fffb4108c4df26dde8cc82d1c1287d40c
    Reviewed-on: https://git-amr-2.devtools.intel.com/gerrit/229852
    Tested-by: Dmitriev, Serguei N <serguei.n.dmitriev@intel.com>
    Reviewed-by: Dmitriev, Serguei N <serguei.n.dmitriev@intel.com>

--- llvm/lib/Transforms/Utils/SimplifyCFG.cpp
+++ llvm/lib/Transforms/Utils/SimplifyCFG.cpp
@@ -4205,23 +4205,11 @@ static bool removeEmptyCleanup(CleanupReturnInst *RI) {
       // The iterator must be incremented here because the instructions are
       // being moved to another block.
       PHINode *PN = cast<PHINode>(I++);
-<<<<<<< HEAD
-#if INTEL_CUSTOMIZATION
-      if (PN->use_empty() ||
-          std::all_of(PN->user_begin(), PN->user_end(),
-                      [&BB](const Value *V) {
-                        if (auto *I = dyn_cast<Instruction>(V))
-                          return I->getParent() == BB;
-                        return false;
-                      }))
-=======
       if (PN->use_empty() || !PN->isUsedOutsideOfBlock(BB))
->>>>>>> c467faf23c7abda60cfd5486a39ffadd6f546d5c
         // If the PHI node has no uses or all of its uses are in this basic
         // block (meaning they are debug or lifetime intrinsics), just leave
         // it.  It will be erased when we erase BB below.
         continue;
-#endif // INTEL_CUSTOMIZATION
 
       // Otherwise, sink this PHI node into UnwindDest.
       // Any predecessors to UnwindDest which are not already represented
--- llvm/test/Transforms/SimplifyCFG/empty-cleanuppad.ll
+++ llvm/test/Transforms/SimplifyCFG/empty-cleanuppad.ll
@@ -457,10 +457,6 @@ cleanupret2:
   cleanupret from %cp unwind to caller
 }
 
-<<<<<<< HEAD
-; INTEL_CUSTOMIZATION
-=======
->>>>>>> c467faf23c7abda60cfd5486a39ffadd6f546d5c
 ; CHECK-LABEL: define void @f11(
 ;   This case tests the handling of an empty cleanup pad that
 ;   contains a lifetime_end intrinsic and does not dominate its
@@ -471,11 +467,6 @@ cleanupret2:
 ; CHECK:   invoke void @g()
 ; CHECK: invoke.cont2:
 ; CHECK:   invoke void @g()
-<<<<<<< HEAD
-; FIXME -- As a temporary workaround, we're not deleting the cleanup pad.
-;          Long term, we should delete the cleanup pad and sink the intrinsic.
-=======
->>>>>>> c467faf23c7abda60cfd5486a39ffadd6f546d5c
 ; CHECK-NOT: ehcleanup:
 ; CHECK-NOT:   phi
 ; CHECK-NOT:   cleanuppad
@@ -514,10 +505,6 @@ catch:                                            ; preds = %catch.dispatch
 return:                                           ; preds = %invoke.cont, %catch.cont
   ret void
 }
-<<<<<<< HEAD
-; end INTEL_CUSTOMIZATION
-=======
->>>>>>> c467faf23c7abda60cfd5486a39ffadd6f546d5c
 
 %struct.S = type { i8 }
 %struct.S2 = type { i8 }
