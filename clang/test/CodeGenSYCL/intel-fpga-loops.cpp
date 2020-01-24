b37634001d26ddced64af14d59e3c7bf05b2673d
Sergey Dmitriev <serguei.n.dmitriev@intel.com>
2020-01-23 20:37:13 -0800

    Resolve conflicts after a91b0f5 [SYCL][Test] Improve intel-fpga-loops test (#1034)
    Change-Id: I5d15ede9bfe9b02e318677260390cdf09ffee9f3
    Reviewed-on: https://git-amr-2.devtools.intel.com/gerrit/229771
    Reviewed-by: Dmitriev, Serguei N <serguei.n.dmitriev@intel.com>
    Tested-by: Dmitriev, Serguei N <serguei.n.dmitriev@intel.com>

--- clang/test/CodeGenSYCL/intel-fpga-loops.cpp
+++ clang/test/CodeGenSYCL/intel-fpga-loops.cpp
@@ -1,16 +1,13 @@
 // RUN: %clang_cc1 -triple spir64-unknown-unknown-sycldevice -disable-llvm-passes -fsycl-is-device -emit-llvm %s -o - | FileCheck %s
 
-<<<<<<< HEAD
 // INTEL_CUSTOMIZATION
 // CHECK: br label %for.cond, !llvm.loop ![[MD_X:[0-9]+]]
 // CHECK: br label %for.cond, !llvm.loop ![[MD_Y:[0-9]+]]
 // end INTEL_CUSTOMIZATION
-// CHECK: br label %for.cond, !llvm.loop ![[MD_A:[0-9]+]]
-// CHECK: br label %for.cond, !llvm.loop ![[MD_B:[0-9]+]]
-// CHECK: br label %for.cond, !llvm.loop ![[MD_C:[0-9]+]]
-// CHECK: br label %for.cond2, !llvm.loop ![[MD_D:[0-9]+]]
-// CHECK: br label %for.cond,  !llvm.loop ![[MD_E:[0-9]+]]
-// CHECK: br label %for.cond2, !llvm.loop ![[MD_F:[0-9]+]]
+// CHECK: br label %for.cond,  !llvm.loop ![[MD_II:[0-9]+]]
+// CHECK: br label %for.cond2, !llvm.loop ![[MD_II_2:[0-9]+]]
+// CHECK: br label %for.cond,  !llvm.loop ![[MD_MC:[0-9]+]]
+// CHECK: br label %for.cond2, !llvm.loop ![[MD_MC_2:[0-9]+]]
 
 // INTEL_CUSTOMIZATION
 // CHECK: ![[MD_X]] = distinct !{![[MD_X]], ![[MD_ivdep_X:[0-9]+]]}
@@ -32,33 +29,6 @@ void car() {
 }
 // end INTEL_CUSTOMIZATION
 
-// CHECK: ![[MD_A]] = distinct !{![[MD_A]], ![[MD_ii:[0-9]+]]}
-// CHECK-NEXT: ![[MD_ii]] = !{!"llvm.loop.ii.count", i32 2}
-void goo() {
-  int a[10];
-  [[intelfpga::ii(2)]]
-  for (int i = 0; i != 10; ++i)
-    a[i] = 0;
-}
-
-// CHECK: ![[MD_B]] = distinct !{![[MD_B]], ![[MD_max_concurrency:[0-9]+]]}
-// CHECK-NEXT: ![[MD_max_concurrency]] = !{!"llvm.loop.max_concurrency.count", i32 2}
-void zoo() {
-  int a[10];
-  [[intelfpga::max_concurrency(2)]]
-  for (int i = 0; i != 10; ++i)
-    a[i] = 0;
-}
-
-// CHECK: ![[MD_C]] = distinct !{![[MD_C]], ![[MD_ii_2:[0-9]+]]}
-// CHECK-NEXT: ![[MD_ii_2]] = !{!"llvm.loop.ii.count", i32 4}
-=======
-// CHECK: br label %for.cond,  !llvm.loop ![[MD_II:[0-9]+]]
-// CHECK: br label %for.cond2, !llvm.loop ![[MD_II_2:[0-9]+]]
-// CHECK: br label %for.cond,  !llvm.loop ![[MD_MC:[0-9]+]]
-// CHECK: br label %for.cond2, !llvm.loop ![[MD_MC_2:[0-9]+]]
-
->>>>>>> a91b0f5061247abb7e4b90eccda208de29f7000d
 template <int A>
 void ii() {
   int a[10];
@@ -96,19 +66,12 @@ __attribute__((sycl_kernel)) void kernel_single_task(Func kernelFunc) {
 
 int main() {
   kernel_single_task<class kernel_function>([]() {
-<<<<<<< HEAD
 // INTEL_CUSTOMIZATION
     bar();
     car();
 // end INTEL_CUSTOMIZATION
-    goo();
-    zoo();
-    boo<4>();
-    foo<0>();
-=======
     ii<4>();
     max_concurrency<0>();
->>>>>>> a91b0f5061247abb7e4b90eccda208de29f7000d
   });
   return 0;
 }
