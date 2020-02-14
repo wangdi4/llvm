69a749e556ad9158d0036e6d35145b931c51c906
Xiang1 Zhang <xiang1.zhang@intel.com>
2020-02-14 13:50:54 +0800

    // INTEL_FEATURE_ISA_AMX_FP16 Update TDPFP16PS encoding // end INTEL_FEATURE_ISA_AMX_FP16
    Change-Id: I1e4063be8bd79e572e2cd3b77f160819b67b1b85
    Reviewed-on: https://git-amr-2.devtools.intel.com/gerrit/233234
    Tested-by: xmain_checkin_sanity <lab_icls@iclgerrit.sc.intel.com>
    Reviewed-by: Luo, Yuanke <yuanke.luo@intel.com>
    Tested-by: xmain_checkin <lab_iclt@iclgerrit.sc.intel.com>
    Reviewed-by: Topper, Craig <craig.topper@intel.com>

--- llvm/test/MC/X86/Intel_AMX_FP16/x86-64AmxFP16-intel.s
+++ llvm/test/MC/X86/Intel_AMX_FP16/x86-64AmxFP16-intel.s
@@ -2,5 +2,5 @@
 // RUN: llvm-mc -triple x86_64-unknown-unknown -x86-asm-syntax=intel -output-asm-variant=1 --show-encoding %s | FileCheck %s
 
 // CHECK:      tdpfp16ps       tmm3, tmm4, tmm5
-// CHECK: encoding: [0xc4,0xe2,0x53,0x5c,0xdc]
+// CHECK: encoding: [0xc4,0xe2,0x52,0x5d,0xdc]
                tdpfp16ps       tmm3, tmm4, tmm5
