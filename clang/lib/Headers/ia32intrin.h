d10eef19cc612f6a96b7ffbf4b791433545cdf2f
Craig Topper <craig.topper@intel.com>
2020-02-21 14:15:51 -0800

    Remove copyright header from ia32intrin.h
    The date is wrong but probably shouldn't be there at all.
    
    This file is mostly community code. The Intel specific changes are
    adding an include of immintrin.h and removing the error that says
     you can't include the file by itself. There are also a couple
     changes to allow some 'q' intrinsics in 32-bit mode.
    
    Change-Id: I76a741e50e12daf58d8513a92c128ccbb24dda58
    Reviewed-on: https://git-amr-2.devtools.intel.com/gerrit/234463
    Tested-by: xmain_checkin <lab_iclt@iclgerrit.sc.intel.com>
    Reviewed-by: Kaylor, Andrew <andrew.kaylor@intel.com>
    Reviewed-by: Topper, Craig <craig.topper@intel.com>

--- clang/lib/Headers/ia32intrin.h
+++ clang/lib/Headers/ia32intrin.h
@@ -1,11 +1,3 @@
-//
-// Copyright (C) 2015 Intel Corporation.  All rights reserved.
-//
-// The information and source code contained herein is the exclusive
-// property of Intel Corporation and may not be disclosed, examined
-// or reproduced in whole or in part without explicit written authorization
-// from the company.
-//
 /* ===-------- ia32intrin.h ---------------------------------------------------===
  *
  * Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
