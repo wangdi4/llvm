c674da497a04d1cdf09b2ffbde2914a11b5881e1
Alexey Sachkov <alexey.sachkov@intel.com>
2020-03-24 12:25:03 +0300

    Conflict resolution for a9bcb4f8b137bf5bdf06947a8520e8f1eca1a138 Merge from 'sycl' to 'sycl-web' (#1)
    Change-Id: I6a249f9ddf9986857061915052453ede0eb773e7
    Reviewed-on: https://git-amr-2.devtools.intel.com/gerrit/239501
    Reviewed-by: Sachkov, Alexey <alexey.sachkov@intel.com>
    Tested-by: Sachkov, Alexey <alexey.sachkov@intel.com>

--- sycl/test/basic_tests/image_constructors.cpp
+++ sycl/test/basic_tests/image_constructors.cpp
@@ -1,13 +1,9 @@
-<<<<<<< HEAD
 // INTEL_CUSTOMIZATION
 // This test fires assertion: CORC-5473.
 // TODO: remove the requirement once issue fixed.
 // REQUIRES: !asserts
 // end INTEL_CUSTOMIZATION
-// RUN: %clangxx %s -o %t1.out -lsycl
-=======
 // RUN: %clangxx %s -o %t1.out -lsycl -I %sycl_include
->>>>>>> a9bcb4f8b137bf5bdf06947a8520e8f1eca1a138
 // RUN: env SYCL_DEVICE_TYPE=HOST %t1.out
 // RUN: %clangxx -fsycl -fsycl-targets=%sycl_triple %s -o %t2.out
 // RUN: env SYCL_DEVICE_TYPE=HOST %t2.out
