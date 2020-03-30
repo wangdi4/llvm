461cd3df5829eb067f3720828e7e3b154a6c728b
Michael D Toguchi <michael.d.toguchi@intel.com>
2020-03-30 13:25:04 -0700

    CMPLRLLVM-18662 - disable CUDA install check for DPC++
    DPC++ does not support Cuda, so when we know we are enabling that mode, we
    disable the check.  The install check emits a diagnostic when the version
    is newer than known supported, and this is confusing to end users.
    
    Change-Id: Ieffe4e7f4ba301b5b49cc3a53492cf7f6235035e
    Reviewed-on: https://git-amr-2.devtools.intel.com/gerrit/240554
    Reviewed-by: Toguchi, Michael D <michael.d.toguchi@intel.com>
    Tested-by: Toguchi, Michael D <michael.d.toguchi@intel.com>
    Reviewed-by: Su, Ernesto <ernesto.su@intel.com>

--- clang/lib/Driver/ToolChains/Cuda.cpp
+++ clang/lib/Driver/ToolChains/Cuda.cpp
@@ -57,6 +57,11 @@ CudaInstallationDetector::CudaInstallationDetector(
     const Driver &D, const llvm::Triple &HostTriple,
     const llvm::opt::ArgList &Args)
     : D(D) {
+#if INTEL_CUSTOMIZATION
+  // DPC++ does not support Cuda, do not do the installation check.
+  if (Args.hasArg(options::OPT__dpcpp))
+    return;
+#endif // INTEL_CUSTOMIZATION
   struct Candidate {
     std::string Path;
     bool StrictChecking;
