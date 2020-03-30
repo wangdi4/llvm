8169aaf7a6307dd6c68104903d43ad26cc5ea454
Alexey Sachkov <alexey.sachkov@intel.com>
2020-03-30 13:29:23 +0300

    Enable spec_const_redefine.cpp test on CPU
    This test was XFAIL-ed in open-source, because not all underlying
    runtimes are up-to-date enough to support spec constants. However, this
    is not quite true for xmain: I'm not sure about GPU and ACC devices, but
    CPU definitely supports the feature
    
    Change-Id: Id9dda6dfca163d1a25bc3a40f71ee4a35aa6aae9
    Reviewed-on: https://git-amr-2.devtools.intel.com/gerrit/240466
    Reviewed-by: Sachkov, Alexey <alexey.sachkov@intel.com>
    Tested-by: Sachkov, Alexey <alexey.sachkov@intel.com>

--- sycl/test/spec_const/spec_const_redefine.cpp
+++ sycl/test/spec_const/spec_const_redefine.cpp
@@ -1,11 +1,8 @@
 // RUN: %clangxx -fsycl %s -o %t.out
 // RUN: env SYCL_DEVICE_TYPE=HOST %t.out
 // RUN: %CPU_RUN_PLACEHOLDER %t.out
-// RUN: %GPU_RUN_PLACEHOLDER %t.out
-// RUN: %ACC_RUN_PLACEHOLDER %t.out
-// TODO the test currently fails as program recompilation based on spec
-//      constants set change is not complete yet.
-// XFAIL: *
+// RUNx: %GPU_RUN_PLACEHOLDER %t.out // INTEL
+// RUNx: %ACC_RUN_PLACEHOLDER %t.out // INTEL
 //
 //==----------- spec_const_redefine.cpp ------------------------------------==//
 //
