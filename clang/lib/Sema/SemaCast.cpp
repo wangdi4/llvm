ed00847bd0512cd69387ce641a4ae0e2e923bbd6
Abhinav Gaba <abhinav.gaba@intel.com>
2020-04-01 20:58:36 -0700

    Resolve conflicts after 856bdd01fd65002d8d88b491bbff53648b6002c1 [Sema][SVE] Allow casting SVE types to themselves in C
    Change-Id: Ib1917164b3c7718251db55d3e5f7c3107f5e7f50
    Reviewed-on: https://git-amr-2.devtools.intel.com/gerrit/241185
    Reviewed-by: Gaba, Abhinav <abhinav.gaba@intel.com>
    Tested-by: Gaba, Abhinav <abhinav.gaba@intel.com>

--- clang/lib/Sema/SemaCast.cpp
+++ clang/lib/Sema/SemaCast.cpp
@@ -2659,12 +2659,6 @@ void CastOperation::CheckCStyleCast() {
     return;
   }
 
-<<<<<<< HEAD
-#if INTEL_CUSTOMIZATION
-  if (!DestType->isScalarType() && !DestType->isVectorType() &&
-      !DestType->isArbPrecIntType()) {
-#endif // INTEL_CUSTOMIZATION
-=======
   // Allow casting a sizeless built-in type to itself.
   if (DestType->isSizelessBuiltinType() &&
       Self.Context.hasSameUnqualifiedType(DestType, SrcType)) {
@@ -2672,8 +2666,10 @@ void CastOperation::CheckCStyleCast() {
     return;
   }
 
-  if (!DestType->isScalarType() && !DestType->isVectorType()) {
->>>>>>> 856bdd01fd65002d8d88b491bbff53648b6002c1
+#if INTEL_CUSTOMIZATION
+  if (!DestType->isScalarType() && !DestType->isVectorType() &&
+      !DestType->isArbPrecIntType()) {
+#endif // INTEL_CUSTOMIZATION
     const RecordType *DestRecordTy = DestType->getAs<RecordType>();
 
     if (DestRecordTy && Self.Context.hasSameUnqualifiedType(DestType, SrcType)){
