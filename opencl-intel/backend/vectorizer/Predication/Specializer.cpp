eefef686da512ac21d738dcb4343fa2c3b2a3ee4
Abhinav Gaba <abhinav.gaba@intel.com>
2020-04-17 12:14:00 -0700

    Fix build after debb066 Merge from 'master' to 'sycl-web' (#1) Replaced uses of CallSite with CallBase as it's being removed.
    Change-Id: I520f6e0dbbc58a4ba17622d5caecc9946a196e52
    Reviewed-on: https://git-amr-2.devtools.intel.com/gerrit/244221
    Reviewed-by: Gaba, Abhinav <abhinav.gaba@intel.com>
    Tested-by: Gaba, Abhinav <abhinav.gaba@intel.com>

--- opencl-intel/backend/vectorizer/Predication/Specializer.cpp
+++ opencl-intel/backend/vectorizer/Predication/Specializer.cpp
@@ -42,8 +42,8 @@ EnableSpecialization("specialize", cl::init(true), cl::Hidden,
 /// callIsSmall - If a call is likely to lower to a single target instruction,
 /// or is otherwise deemed small return true.
 /// TODO: Perhaps calls like memcpy, strcpy, etc?
-static bool estimateCallIsSmall(ImmutableCallSite CS) {
-  if (isa<IntrinsicInst>(CS.getInstruction()))
+static bool estimateCallIsSmall(const CallBase &CS) {
+  if (isa<IntrinsicInst>(&CS))
     return true;
 
   const Function *F = CS.getCalledFunction();
@@ -128,11 +128,11 @@ static unsigned estimateNumInsts(const BasicBlock *BB) {
 
     // Special handling for calls.
     if (isa<CallInst>(I) || isa<InvokeInst>(I)) {
-      ImmutableCallSite CS(cast<Instruction>(I));
+      auto *CB = cast<CallBase>(I);
 
-      if (!estimateCallIsSmall(CS)) {
+      if (!estimateCallIsSmall(*CB)) {
         // Each argument to a call takes on average one instruction to set up.
-        NumInsts += CS.arg_size();
+        NumInsts += CB->arg_size();
       }
     }
 
