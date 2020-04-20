bd9092a505afada2602ab348454db32720548f80
Wei Wu <wei.wu@intel.com>
2020-04-20 11:07:28 +0800

    [subgroups] Support cl_khr_subgroup_non_uniform_arithmetic
    Change-Id: I4627698acd81098888d250954d31390c13b1464e
    Reviewed-on: https://git-amr-2.devtools.intel.com/gerrit/244372
    Tested-by: xmain_checkin <lab_iclt@iclgerrit.sc.intel.com>
    Reviewed-by: Wenju He <wenju.he@intel.com>
    Reviewed-by: Dai, Chunyang <chunyang.dai@intel.com>

--- opencl-intel/devices/cpu_device/cpu_config.cpp
+++ opencl-intel/devices/cpu_device/cpu_config.cpp
@@ -206,6 +206,7 @@ const char* CPUDeviceConfig::GetExtensions() const
         // the conformance test fully (meaning that masked kernel is integrated).
         if (GetUseNativeSubgroups()) {
             m_extensions += OCL_EXT_KHR_SUBGROUP_SHUFFLE " ";
+            m_extensions += OCL_EXT_KHR_SUBGROUP_NON_UNIFORM_ARITHMETIC " ";
             m_extensions += OCL_EXT_INTEL_SUBGROUPS " ";
             m_extensions += OCL_EXT_INTEL_SUBGROUPS_CHAR " ";
             m_extensions += OCL_EXT_INTEL_SUBGROUPS_SHORT " ";
