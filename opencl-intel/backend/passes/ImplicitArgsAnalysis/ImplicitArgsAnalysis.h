wangdi4/llvm

commit e381129a0a2aef28e3b4c3cc6727b16bd761a2f9

[NFC] Update API calls related to IRBuilder
To get the alignment of Instructions inserted, some APIs of IRBuilder
assume that current BasicBlock has been added to a Module, which leads
to SegFault in PrepareKernelArgs pass and ImplicitArgsAnalysis pass.

Fix this by calling Builder.Create* APIs with alignment argument or
calling Builder.Insert directly.

Change-Id: I5d595912645bf23ebbd2d8516790ce95feea54fb
Reviewed-on: https://git-amr-2.devtools.intel.com/gerrit/244405
Tested-by: Sachkov, Alexey <alexey.sachkov@intel.com>
Reviewed-by: Sachkov, Alexey <alexey.sachkov@intel.com>

opencl-intel/backend/passes/ImplicitArgsAnalysis/ImplicitArgsAnalysis.h
@@ -141,11 +141,12 @@ public:
     params.push_back(ConstantInt::get(Type::getInt32Ty(C), 0));
     params.push_back(ConstantInt::get(Type::getInt32Ty(C), RecordID));
     params.push_back(Dimension);
-    Value *pAddr = Builder.CreateGEP(WorkInfo, ArrayRef<Value *>(params));
+    auto *pAddr = cast<GEPOperator>(
+        Builder.CreateGEP(WorkInfo, ArrayRef<Value *>(params)));
     std::string Name(NDInfo::getRecordName(RecordID));
     AppendWithDimension(Name, Dimension);
-    //return Builder.CreateAlignedLoad(pAddr, 1, Name);
-    return Builder.CreateLoad(pAddr, Name);
+    return Builder.Insert(new LoadInst(pAddr->getResultElementType(), pAddr),
+                          Name);
   }
   Value *GenerateGetFromWorkInfo(unsigned RecordID, Value *WorkInfo,
                                     IRBuilder<> &Builder) {
@@ -154,10 +155,11 @@ public:
     Type *Int32Ty = Type::getInt32Ty(C);
     params.push_back(ConstantInt::get(Int32Ty, 0));
     params.push_back(ConstantInt::get(Int32Ty, RecordID));
-    Value *pAddr = Builder.CreateGEP(WorkInfo, ArrayRef<Value *>(params));
+    auto *pAddr = cast<GEPOperator>(
+        Builder.CreateGEP(WorkInfo, ArrayRef<Value *>(params)));
     std::string Name(NDInfo::getRecordName(RecordID));
-    //Value *V = Builder.CreateAlignedLoad(pAddr);
-    Value *V = Builder.CreateLoad(pAddr);
+    Value *V = Builder.Insert(
+        new LoadInst(pAddr->getResultElementType(), pAddr), Name);
     if (V->getType() != Int32Ty && RecordID == NDInfo::WORK_DIM)
       V = Builder.CreateTrunc(V, Int32Ty);
     V->setName(Name);
@@ -230,10 +232,12 @@ private:
     params.push_back(ConstantInt::get(Type::getInt32Ty(C), NDInfo::LOCAL_SIZE));
     params.push_back(LocalSizeIdx);
     params.push_back(Dimension);
-    Value *pAddr = Builder.CreateGEP(WorkInfo, ArrayRef<Value *>(params));
+    auto *pAddr = cast<GEPOperator>(
+        Builder.CreateGEP(WorkInfo, ArrayRef<Value *>(params)));
     std::string Name(NDInfo::getRecordName(NDInfo::LOCAL_SIZE));
     AppendWithDimension(Name, Dimension);
-    return Builder.CreateLoad(pAddr, Name);
+    return Builder.Insert(new LoadInst(pAddr->getResultElementType(), pAddr),
+                          Name);
   }
 
 public:
@@ -245,10 +249,11 @@ public:
   }
   Value *GenerateGetGroupID(Value *GroupID, Value *Dimension,
                                   IRBuilder<> &Builder) {
-    Value *pIdAddr = Builder.CreateGEP(GroupID, Dimension);
+    auto *pIdAddr = cast<GEPOperator>(Builder.CreateGEP(GroupID, Dimension));
     std::string Name("GroupID_");
     AppendWithDimension(Name, Dimension);
-    return Builder.CreateLoad(pIdAddr, Name);
+    return Builder.Insert(
+        new LoadInst(pIdAddr->getResultElementType(), pIdAddr), Name);
   }
 
   Value *GenerateGetBaseGlobalID(Value *BaseGlobalID, Value *Dimension,
@@ -270,8 +275,9 @@ public:
       std::vector<Value *> Indices;
       Indices.push_back(ConstantInt::get(IntegerType::get(C, 32), 0));
       Indices.push_back(Dimension);
-      Value *GEP = Builder.CreateGEP(A, Indices);
-      return Builder.CreateLoad(GEP, Name);
+      auto *GEP = cast<GEPOperator>(Builder.CreateGEP(A, Indices));
+      return Builder.Insert(new LoadInst(GEP->getResultElementType(), GEP),
+                            Name);
     }
   }
   static char ID; // Pass identification, replacement for typeid

opencl-intel/backend/passes/PrepareKernelArgs/PrepareKernelArgs.cpp
@@ -146,12 +146,12 @@ namespace intel{
       } else if (arg.type == CL_KRNL_ARG_PTR_LOCAL) {
         // The argument is actually the size of the buffer
         Value *pPointerCast = builder.CreatePointerCast(pGEP, PointerType::get(m_SizetTy, 0));
-        LoadInst *BufferSize = builder.CreateLoad(pPointerCast);
+        LoadInst *BufferSize = builder.CreateAlignedLoad(
+            m_SizetTy, pPointerCast,
+            MaybeAlign(m_DL->getABITypeAlign(m_SizetTy)), false);
         // TODO: when buffer size is 0, we might want to set dummy address for debugging!
         const auto AllocaAddrSpace = m_DL->getAllocaAddrSpace();
-        // We can't use overload without explicit alloca addrspace because the BB does
-        // not have a parent yet.
-        AllocaInst *Allocation = builder.CreateAlloca(m_I8Ty, AllocaAddrSpace, BufferSize);
+
         // Set alignment of buffer to type size.
         unsigned Alignment = 16; // Cacheline
         Type* EltTy = callIt->getType()->getPointerElementType();
@@ -161,7 +161,11 @@ namespace intel{
         if (VecSize != 1 && VectorType::isValidElementType(EltTy))
           EltTy = VectorType::get(EltTy, VecSize);
         Alignment = llvm::NextPowerOf2(m_DL->getTypeAllocSize(EltTy) - 1);
-        Allocation->setAlignment(MaybeAlign(Alignment));
+        // We can't use overload without explicit alloca addrspace because the
+        // BB does not have a parent yet.
+        AllocaInst *Allocation = new AllocaInst(
+            m_I8Ty, AllocaAddrSpace, BufferSize, MaybeAlign(Alignment));
+        builder.Insert(Allocation);
         pArg = builder.CreatePointerCast(Allocation, callIt->getType());
       } else if (arg.type == CL_KRNL_ARG_PTR_BLOCK_LITERAL) {
           pArg = builder.CreateAddrSpaceCast(pGEP, callIt->getType());
@@ -176,12 +180,15 @@ namespace intel{
         // %vec = load int4 * %pVec {, align <alignment> }
         // foo(..., vec, ...)
 
-        Value* pPointerCast = builder.CreatePointerCast(pGEP, PointerType::get(callIt->getType(), 0));
-        LoadInst* pLoad = builder.CreateLoad(pPointerCast);
+        auto *DestTy = PointerType::get(callIt->getType(), 0);
+        Value *pPointerCast = builder.CreatePointerCast(pGEP, DestTy);
         size_t alignment = TypeAlignment::getAlignment(arg);
-        if (alignment > 0) {
-          pLoad->setAlignment(MaybeAlign(TypeAlignment::getAlignment(arg)));
-        }
+        MaybeAlign Align =
+            alignment > 0
+                ? MaybeAlign(alignment)
+                : m_DL->getABITypeAlign(DestTy->getPointerElementType());
+        LoadInst *pLoad = builder.CreateAlignedLoad(
+            DestTy->getPointerElementType(), pPointerCast, Align, false);
         pArg = pLoad;
       }
 
@@ -240,11 +247,13 @@ namespace intel{
           Type *slmType = ArrayType::get(m_I8Ty,
             slmSizeInBytes+STACK_PADDING_BUFFER*2);
           const auto AllocaAddrSpace = m_DL->getAllocaAddrSpace();
-          AllocaInst *slmBuffer = builder.CreateAlloca(slmType, AllocaAddrSpace);
           // Set alignment of implicit local buffer to max alignment.
           // TODO: we should choose the min required alignment size
-          slmBuffer->setAlignment(MaybeAlign(TypeAlignment::MAX_ALIGNMENT));
           // move argument up over the lower side padding.
+          AllocaInst *slmBuffer =
+              new AllocaInst(slmType, AllocaAddrSpace, nullptr,
+                             MaybeAlign(TypeAlignment::MAX_ALIGNMENT));
+          builder.Insert(slmBuffer);
           Value* castBuf = builder.CreatePointerCast(slmBuffer,
                                                      PointerType::get(m_I8Ty, 3));
           pArg = builder.CreateGEP(castBuf,
@@ -272,6 +281,7 @@ namespace intel{
         for (unsigned Dim = 0; Dim < MAX_WORK_DIM; ++Dim)
           LocalSize.push_back(
               m_IAA->GenerateGetEnqueuedLocalSize(WGInfo, Dim, builder));
+
         // Obtain values of NDRange Offsets for each dimension
         SmallVector<Value *, 4> GlobalOffsets;
         for (unsigned Dim = 0; Dim < MAX_WORK_DIM; ++Dim) {
@@ -313,10 +323,11 @@ namespace intel{
           BarrierBufferSize->setName("BarrierBufferSize");
           // alloca i8, %BarrierBufferSize
           const auto AllocaAddrSpace = m_DL->getAllocaAddrSpace();
-          AllocaInst *BarrierBuffer = builder.CreateAlloca(
-            m_I8Ty, AllocaAddrSpace, BarrierBufferSize);
           //TODO: we should choose the min required alignment size
-          BarrierBuffer->setAlignment(MaybeAlign(TypeAlignment::MAX_ALIGNMENT));
+          AllocaInst *BarrierBuffer =
+              new AllocaInst(m_I8Ty, AllocaAddrSpace, BarrierBufferSize,
+                             MaybeAlign(TypeAlignment::MAX_ALIGNMENT));
+          builder.Insert(BarrierBuffer);
           pArg = BarrierBuffer;
         }
         break;
@@ -340,7 +351,8 @@ namespace intel{
       if (m_useTLSGlobals) {
         assert(pArg && "No value was created for this TLS global!");
         GlobalVariable *GV = CompilationUtils::getTLSGlobal(m_pModule, i);
-        builder.CreateStore(pArg, GV);
+        builder.CreateAlignedStore(pArg, GV,
+                                   m_DL->getABITypeAlign(pArg->getType()));
       } else {
         assert(pArg && "No value was created for this implicit argument!");
         pArg->setName(ImplicitArgsUtils::getArgName(i));

commit 71582d39570352f9aaaa7b2c4f6259464117b005

[NFC] Fix an inadvertent error introduced by e381129a0a.
Change-Id: Ie31e9531066927305263fa1ee36584ac5ad462c3
Reviewed-on: https://git-amr-2.devtools.intel.com/gerrit/244606
Reviewed-by: Wenju He <wenju.he@intel.com>
Reviewed-by: Senran Zhang <senran.zhang@intel.com>
Reviewed-by: Dai, Chunyang <chunyang.dai@intel.com>
Tested-by: Sachkov, Alexey <alexey.sachkov@intel.com>

opencl-intel/backend/passes/ImplicitArgsAnalysis/ImplicitArgsAnalysis.h
@@ -159,7 +159,7 @@ public:
         Builder.CreateGEP(WorkInfo, ArrayRef<Value *>(params)));
     std::string Name(NDInfo::getRecordName(RecordID));
     Value *V = Builder.Insert(
-        new LoadInst(pAddr->getResultElementType(), pAddr), Name);
+        new LoadInst(pAddr->getResultElementType(), pAddr));
     if (V->getType() != Int32Ty && RecordID == NDInfo::WORK_DIM)
       V = Builder.CreateTrunc(V, Int32Ty);
     V->setName(Name);