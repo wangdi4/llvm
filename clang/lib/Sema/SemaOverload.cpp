290863e6abe8ada0ce9a613749c995759871f8cc
Abhinav Gaba <abhinav.gaba@intel.com>
2020-04-26 12:16:16 -0700

    Resolve conflicts after c77a407 [CUDA][HIP] Fix host/device based overload resolution.
    Reference URL: https://reviews.llvm.org/D77954
    Conflicting commits:
    e12fbfd CQ376357: support -fpermissive conversions.
    
    Change-Id: Id26767c3a7bd01fde632876dfe80e5fe63b213d0
    Reviewed-on: https://git-amr-2.devtools.intel.com/gerrit/245620
    Reviewed-by: Gaba, Abhinav <abhinav.gaba@intel.com>
    Tested-by: Gaba, Abhinav <abhinav.gaba@intel.com>

--- clang/lib/Sema/SemaOverload.cpp
+++ clang/lib/Sema/SemaOverload.cpp
@@ -9673,7 +9673,6 @@ bool clang::isBetterOverloadCandidate(
   else if (!Cand1.Viable)
     return false;
 
-<<<<<<< HEAD
 #if INTEL_CUSTOMIZATION
   // CQ#376357: GCC in -fpermissive mode allows weird conversions.
   bool CandHasPermissiveConversion1 = hasPermissiveConversion(Cand1);
@@ -9683,7 +9682,7 @@ bool clang::isBetterOverloadCandidate(
   else if (CandHasPermissiveConversion1 && !CandHasPermissiveConversion2)
     return false;
 #endif // INTEL_CUSTOMIZATION
-=======
+
   // [CUDA] A function with 'never' preference is marked not viable, therefore
   // is never shown up here. The worst preference shown up here is 'wrong side',
   // e.g. a host function called by a device host function in device
@@ -9728,7 +9727,6 @@ bool clang::isBetterOverloadCandidate(
     }
   }
 
->>>>>>> c77a4078e01033aa2206c31a579d217c8a07569b
   // C++ [over.match.best]p1:
   //
   //   -- if F is a static member function, ICS1(F) is defined such
