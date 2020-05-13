71e8e0dd4c696affc7813b12185b02c6d6b7fbe4
Abhinav Gaba <abhinav.gaba@intel.com>
2020-05-13 00:40:49 -0700

    Fix build after 8c72b02 [CodeGen] Use Align in MachineConstantPool.
    Reference URL: https://reviews.llvm.org/rG8c72b0271
    
    Change-Id: I6bea957f99e6f1949263d60f17e12131d1680089
    Reviewed-on: https://git-amr-2.devtools.intel.com/gerrit/248496
    Reviewed-by: Gaba, Abhinav <abhinav.gaba@intel.com>
    Tested-by: Gaba, Abhinav <abhinav.gaba@intel.com>

--- llvm/lib/Target/X86/Intel_X86FMA.cpp
+++ llvm/lib/Target/X86/Intel_X86FMA.cpp
@@ -1488,10 +1488,10 @@ unsigned X86GlobalFMA::createConstOne(MVT VT, MachineInstr *InsertPointMI) {
 
   // Create the load from the constant pool.
   MachineConstantPool *MCP = MF->getConstantPool();
-  unsigned Align = VT.getSizeInBits() / 8;
-  MaybeAlign Alignment(Align);
+  unsigned AlignVal = VT.getSizeInBits() / 8;
+  Align Alignment(AlignVal);
   Constant *FPOne = ConstantFP::get(Ty, 1.0);
-  unsigned CPI = MCP->getConstantPoolIndex(FPOne, Align);
+  unsigned CPI = MCP->getConstantPoolIndex(FPOne, Alignment);
   const TargetRegisterClass *RC = ST->getTargetLowering()->getRegClassFor(VT);
   unsigned ResultReg = MRI->createVirtualRegister(RC);
   MachineInstrBuilder MIB;
@@ -1510,8 +1510,8 @@ unsigned X86GlobalFMA::createConstOne(MVT VT, MachineInstr *InsertPointMI) {
   }
   MachineMemOperand *MMO = MF->getMachineMemOperand(
       MachinePointerInfo::getConstantPool(*MF),
-      MachineMemOperand::MOLoad | MachineMemOperand::MOInvariant, Align,
-      *Alignment);
+      MachineMemOperand::MOLoad | MachineMemOperand::MOInvariant, AlignVal,
+      Alignment);
   ArrayRef<MachineMemOperand *> ARMMOs(MMO);
   auto MMOs = extractLoadMMOs(ARMMOs, *MF);
   MIB.setMemRefs(MMOs);
