22c16f0d9d26681551c849afeb223303f1868ecc
Abhinav Gaba <abhinav.gaba@intel.com>
2020-05-12 20:01:03 -0700

    Disable Lexar/case-insensitive-include-win.c until the community code handles CPATH containing network mounted drives.
    The issue has been reported to the community, and we'll keep it disabled
    until it's fixed in llorg.
    
    Reference URLs:
    https://reviews.llvm.org/D79531
    https://reviews.llvm.org/D79595
    
    Change-Id: I7a1322ac25e29c4f0fed41da8fd07c40fcf12bba
    Reviewed-on: https://git-amr-2.devtools.intel.com/gerrit/248443
    Reviewed-by: Gaba, Abhinav <abhinav.gaba@intel.com>
    Tested-by: Gaba, Abhinav <abhinav.gaba@intel.com>
    Reviewed-by: Kan ShengChen <shengchen.kan@intel.com>

--- clang/test/Lexer/case-insensitive-include-win.c
+++ clang/test/Lexer/case-insensitive-include-win.c
@@ -1,6 +1,12 @@
 // Most Microsoft-specific testing should go in case-insensitive-include-ms.c
 // This file should only include code that really needs a Windows host OS to
 // run.
+// INTEL_CUSTOMIZATION
+// This will fail for us until community starts handling CPATH containing
+// network mounted drives on Windows. The issue has been reported to the
+// submitter of the test.
+// UNSUPPORTED: system-windows
+// end INTEL_CUSTOMIZATION
 
 // REQUIRES: system-windows
 // RUN: mkdir -p %t.dir
