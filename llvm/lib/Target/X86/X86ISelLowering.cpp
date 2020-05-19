2f570d58be63f1d1f325943b8092d491bf49954e
Abhinav Gaba <abhinav.gaba@intel.com>
2020-05-18 18:23:48 -0700

    Resolve conflicts after 57c3fe7 [x86] favor vector constant load to avoid GPR to XMM transfer.
    Reference URL: https://reviews.llvm.org/D80013
    
    Change-Id: I1db349c5cb8e8773ea269f56a84074da1eaeaa5b
    Reviewed-on: https://git-amr-2.devtools.intel.com/gerrit/249495
    Reviewed-by: Gaba, Abhinav <abhinav.gaba@intel.com>
    Tested-by: Gaba, Abhinav <abhinav.gaba@intel.com>

--- llvm/lib/Target/X86/X86ISelLowering.cpp
+++ llvm/lib/Target/X86/X86ISelLowering.cpp
@@ -10422,13 +10422,6 @@ X86TargetLowering::LowerBUILD_VECTOR(SDValue Op, SelectionDAG &DAG) const {
       if (NumZero == 0)
         return DAG.getNode(ISD::SCALAR_TO_VECTOR, dl, VT, Item);
 
-<<<<<<< HEAD
-#if INTEL_CUSTOMIZATION
-#if INTEL_FEATURE_ISA_FP16
-      if (EltVT == MVT::i32 ||
-          EltVT == MVT::f16 || EltVT == MVT::f32 || EltVT == MVT::f64 ||
-#else  // INTEL_FEATURE_ISA_FP16
-=======
       // Just load a vector integer constant. Loading is better for code size,
       // avoids move GPR immediate --> XMM, and reduces register pressure.
       if (IsAllConstants && VT.isInteger()) {
@@ -10438,7 +10431,11 @@ X86TargetLowering::LowerBUILD_VECTOR(SDValue Op, SelectionDAG &DAG) const {
           return SDValue();
       }
 
->>>>>>> 57c3fe76a31663526c78ed6ac3bbb34858c823d2
+#if INTEL_CUSTOMIZATION
+#if INTEL_FEATURE_ISA_FP16
+      if (EltVT == MVT::i32 ||
+          EltVT == MVT::f16 || EltVT == MVT::f32 || EltVT == MVT::f64 ||
+#else  // INTEL_FEATURE_ISA_FP16
       if (EltVT == MVT::i32 || EltVT == MVT::f32 || EltVT == MVT::f64 ||
 #endif // INTEL_FEATURE_ISA_FP16
 #endif // INTEL_CUSTOMIZATION
