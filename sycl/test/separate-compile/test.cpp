7b309fb36c9a822b94b0882c47874b8697621f58
Abhinav Gaba <abhinav.gaba@intel.com>
2020-05-26 11:22:19 -0700

    Resolve conflicts after b84ae1f [SYCL] Fix LIT if OpenCL GPU is missing (#1714).
    Reference URL: https://github.com/intel/llvm/pull/1714
    
    Change-Id: I481cb0f4ce401d07a3b16af3e64679f3bbc3d3fd
    Reviewed-on: https://git-amr-2.devtools.intel.com/gerrit/250790
    Reviewed-by: Gaba, Abhinav <abhinav.gaba@intel.com>
    Tested-by: Gaba, Abhinav <abhinav.gaba@intel.com>

--- sycl/test/separate-compile/test.cpp
+++ sycl/test/separate-compile/test.cpp
@@ -37,13 +37,9 @@
 //
 // >> ---- link the full hetero app
 // RUN: %clangxx wrapper.o a.o b.o -o app.exe -lsycl
-<<<<<<< HEAD
-// RUN: %CPU_RUN_PLACEHOLDER ./app.exe 2>&1 %CPU_CHECK_PLACEHOLDER
-// RUN: %GPU_RUN_PLACEHOLDER ./app.exe 2>&1 %GPU_CHECK_PLACEHOLDER
-// RUN: %ACC_RUN_PLACEHOLDER ./app.exe 2>&1 %ACC_CHECK_PLACEHOLDER
-=======
-// RUN: env SYCL_BE=%sycl_be ./app.exe | FileCheck %s
->>>>>>> b84ae1fde4dd6365bb03af3e46696ce5be78b709
+// RUN: %CPU_RUN_PLACEHOLDER env SYCL_BE=%sycl_be ./app.exe 2>&1 %CPU_CHECK_PLACEHOLDER
+// RUN: %GPU_RUN_PLACEHOLDER env SYCL_BE=%sycl_be ./app.exe 2>&1 %GPU_CHECK_PLACEHOLDER
+// RUN: %ACC_RUN_PLACEHOLDER env SYCL_BE=%sycl_be ./app.exe 2>&1 %ACC_CHECK_PLACEHOLDER
 // CHECK: pass
 
 //==----------- test.cpp - Tests SYCL separate compilation -----------------==//
