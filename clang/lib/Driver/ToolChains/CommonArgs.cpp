4a73b23b0da95f43f64df037173c552b21b3c964
Valery N Dmitriev <valery.n.dmitriev@intel.com>
2020-06-11 08:54:13 -0700

    Resolve conflicts after 718f067 [Driver] Update plugin used for LTO
    Change-Id: Ie6a55ba5b53a5af4365a445acb2504bf77070746
    Reviewed-on: https://git-amr-2.devtools.intel.com/gerrit/253957
    Reviewed-by: Dmitriev, Valery N <valery.n.dmitriev@intel.com>
    Tested-by: Dmitriev, Valery N <valery.n.dmitriev@intel.com>
    Reviewed-on: https://git-amr-2.devtools.intel.com/gerrit/254048

--- clang/lib/Driver/ToolChains/CommonArgs.cpp
+++ clang/lib/Driver/ToolChains/CommonArgs.cpp
@@ -402,15 +402,9 @@ void tools::addLTOOptions(const ToolChain &ToolChain, const ArgList &Args,
     if (Args.hasArg(options::OPT__intel))
       PluginName = "icx-lto";
     SmallString<1024> Plugin;
-<<<<<<< HEAD
-    llvm::sys::path::native(
-        Twine(D.Dir) + "/../lib" CLANG_LIBDIR_SUFFIX "/LLVMgold" + Suffix,
-        Plugin);
-=======
-    llvm::sys::path::native(Twine(ToolChain.getDriver().Dir) +
+    llvm::sys::path::native(Twine(D.Dir) +
         "/../lib" CLANG_LIBDIR_SUFFIX "/" + PluginName + Suffix, Plugin);
 #endif // INTEL_CUSTOMIZATION
->>>>>>> 718f067849339db80fa723ba200e5dbd31a5cfbf
     CmdArgs.push_back(Args.MakeArgString(Plugin));
   }
 
