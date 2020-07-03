08abc1f2b30950ca9f3141d01c966775dfb23f14
kbobrovs <Konstantin.S.Bobrovsky@intel.com>
2020-07-03 09:38:06 -0700

    [SYCL][ESIMD] Implement IR pass to lower C++ ESIMD intrinsics. (#1881)
    The pass transforms *__esimd_* Itanium - mangled C++ intrinsics to
    genx.*style parseable by the ESIMD - capable SPIRV translator.
    
    Move vc-intrinsics dependency build to llvm/lib/SYCLLowerIR.
    SYCLLowerIR is the only user of vc-intrinsics.
    
    Authors:
    Konstantin S Bobrovsky <konstantin.s.bobrovsky@intel.com>
    Gang Chen <gang.y.chen@intel.com>
    Wei Pan
    Denis Bakhvalov <denis.bakhvalov@intel.com>
    Anton Sidorenko <anton.sidorenko@intel.com>
    Kaiyu Chen <kai.yu.chen@intel.com>
    Pratik Ashar <pratik.j.ashar@intel.com>
    
    Signed-off-by: Konstantin S Bobrovsky <konstantin.s.bobrovsky@intel.com>
    Conflicts:
    	llvm/tools/opt/opt.cpp
    
    Change-Id: I8d889871e4123d92f40474b7b4c6d23b0b156379
    Reviewed-on: https://git-amr-2.devtools.intel.com/gerrit/258578
    Reviewed-by: Dmitriev, Valery N <valery.n.dmitriev@intel.com>
    Tested-by: Dmitriev, Valery N <valery.n.dmitriev@intel.com>

--- llvm/tools/opt/opt.cpp
+++ llvm/tools/opt/opt.cpp
@@ -631,6 +631,7 @@ int main(int argc, char **argv) {
   initializeFloat128ExpandPass(Registry);
   initializeIntel_OpenCLTransforms(Registry);
   initializeSYCLLowerWGScopeLegacyPassPass(Registry);
+  initializeSYCLLowerESIMDLegacyPassPass(Registry);
   initializeParseAnnotateAttributesLegacyPass(Registry);
 #if INTEL_INCLUDE_DTRANS
   initializeDTransPasses(Registry);
