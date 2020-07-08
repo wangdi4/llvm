ed4976fdfb6696df294fdb62197bf5d882898f00
Valery N Dmitriev <valery.n.dmitriev@intel.com>
2020-07-08 13:23:33 -0700

    Resolve build fail after 1eaad01 [CUDA][HIP] Let lambda be host device by default
    Change-Id: Ib298ce716f39e14d834d15dbb28e0233966b3d4e
    Reviewed-on: https://git-amr-2.devtools.intel.com/gerrit/258836
    Tested-by: Dmitriev, Valery N <valery.n.dmitriev@intel.com>
    Reviewed-by: Dmitriev, Valery N <valery.n.dmitriev@intel.com>

--- clang/include/clang/Basic/DiagnosticIDs.h
+++ clang/include/clang/Basic/DiagnosticIDs.h
@@ -36,7 +36,7 @@ namespace clang {
       DIAG_SIZE_AST           =  200,
       DIAG_SIZE_COMMENT       =  100,
       DIAG_SIZE_CROSSTU       =  100,
-      DIAG_SIZE_SEMA          = 4000,
+      DIAG_SIZE_SEMA          = 4500, // INTEL
       DIAG_SIZE_ANALYSIS      =  100,
       DIAG_SIZE_REFACTORING   = 1000,
     };
