dd0eb5273b67eab3285ab98c0b55a946d70dbb90
Lina Yu <lina.yu@intel.com>
2020-07-13 15:39:19 -0700

    [CMPLRLLVM-21146] Suppress the code generation in HIR Last Value Computation when the inst without iv is sinked out and the loop is not an empty node.
    Change-Id: I43cc1189adf7a3be8977e1c38b42b941b6b17bb2
    Reviewed-on: https://git-amr-2.devtools.intel.com/gerrit/259658
    Tested-by: xmain_checkin <lab_iclt@iclgerrit.sc.intel.com>
    Tested-by: zperf_checkin_xmain <lab_iclz@iclgerrit.sc.intel.com>
    Reviewed-by: Chawla, Pankaj <pankaj.chawla@intel.com>
    Reviewed-by: Bozhenov, Nikolai <nikolai.bozhenov@intel.com>

--- llvm/lib/Transforms/Intel_LoopTransforms/HIRLastValueComputation.h
+++ llvm/lib/Transforms/Intel_LoopTransforms/HIRLastValueComputation.h
@@ -43,7 +43,7 @@ private:
 
   bool isLegalAndProfitable(HLLoop *Lp, HLInst *HInst, unsigned LoopLevel,
                             CanonExpr *UBCE, bool IsUpperBoundComplicated,
-                            bool IsNSW);
+                            bool IsNSW, bool &HasIV);
 };
 
 //
