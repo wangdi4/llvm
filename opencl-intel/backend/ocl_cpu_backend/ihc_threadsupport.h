bd7a84eb4769d15e937220367dbdde017e6056b4
Chunyang Dai <chunyang.dai@intel.com>
2020-07-14 11:06:15 +0800

    Extension to OpenCL Windows emulation runtime
    aocl-ioc64 uses GNU mangling scheme regardless of the actual OS it's on.
    Therefore we need these runtime adapters to translate GNU mangled functions
    to functions can be recognized by MSVC.
    
    Change-Id: I14b2c0319c71dad7df9eee8321abb2f6d7209837
    Reviewed-on: https://git-amr-2.devtools.intel.com/gerrit/259701
    Reviewed-by: Shuo Niu <shuo.niu@intel.com>
    Reviewed-by: Hoffner, Tommy <tommy.hoffner@intel.com>
    Tested-by: xmain_checkin <lab_iclt@iclgerrit.sc.intel.com>
    Reviewed-by: Sotkin, Alexey <alexey.sotkin@intel.com>

--- opencl-intel/backend/ocl_cpu_backend/ihc_threadsupport.h
+++ opencl-intel/backend/ocl_cpu_backend/ihc_threadsupport.h
@@ -13,3 +13,10 @@ extern "C" LLVM_BACKEND_API int _ihc_cond_wait(void *, void *);
 extern "C" LLVM_BACKEND_API void *_ihc_pthread_create(void *(*func)(void *), void *arg);
 extern "C" LLVM_BACKEND_API int _ihc_pthread_join(void *handle);
 extern "C" LLVM_BACKEND_API int _ihc_pthread_detach(void *handle);
+
+#ifdef _WIN32
+extern "C" LLVM_BACKEND_API void *_Znwy(unsigned long long);
+extern "C" LLVM_BACKEND_API void _ZdlPvy(void*, unsigned long long);
+extern "C" LLVM_BACKEND_API void _ZSt14_Xlength_errorPKc(char const*);
+extern "C" LLVM_BACKEND_API void _ZdlPv(void*);
+#endif
