dbf760701d029d8441916f5ca8bb2016d26a4e30
Shengchen Kan <shengchen.kan@intel.com>
2020-08-06 15:30:22 +0800

    [traceback][BinaryFormat] Add utilities for traceback's binary format.
    JIRA:CMPLRLLVM-21783
    The database and utilities are used to help encode and decode the debug
    information of traceback. The format here conforms to TraceBack Spec and
    keeps the compatibility with ICC.
    
    Change-Id: Iae80c4200be7e849ff3b7575bf80d6e0b53f3a3e
    Reviewed-on: https://git-amr-2.devtools.intel.com/gerrit/264180
    Tested-by: xmain_checkin <lab_iclt@iclgerrit.sc.intel.com>
    Reviewed-by: Kan ShengChen <shengchen.kan@intel.com>
    Reviewed-by: Romanov, Vlad <vlad.romanov@intel.com>

---
+++ llvm/include/llvm/BinaryFormat/Intel_Trace.h
@@ -0,0 +1,76 @@
+//===--- Intel_Trace.h - TraceBack Tag && Attribute -------------*- C++ -*-===//
+//
+// Copyright (C) 2020 Intel Corporation. All rights reserved.
+//
+// The information and source code contained herein is the exclusive
+// property of Intel Corporation and may not be disclosed, examined
+// or reproduced in whole or in part without explicit written authorization
+// from the company.
+//
+//===----------------------------------------------------------------------===//
+///
+/// \file
+/// This file defines the enumerators and declare the utilities for TraceBack
+/// tag && attribute.
+///
+//===----------------------------------------------------------------------===//
+
+#ifndef LLVM_BINARYFORMAT_TRACE_H
+#define LLVM_BINARYFORMAT_TRACE_H
+
+namespace llvm {
+class StringRef;
+template <typename> class Optional;
+
+/// A uniform namespace for the entities in .trace section.
+namespace traceback {
+/// The contents of .trace section consists of various records, each record
+/// has one tag and one or several attributes. The tag tells what the following
+/// bytes represents while the attribute tells what's the value of specific
+/// field.
+enum Tag : unsigned char {
+#define HANDLE_TB_TAG(NAME, ENCODING) TB_TAG_##NAME,
+#include "llvm/BinaryFormat/Intel_Trace.def"
+  NUM_TAGS
+};
+
+enum Attribute : unsigned char {
+#define HANDLE_TB_AT(NAME, SIZE) TB_AT_##NAME,
+#include "llvm/BinaryFormat/Intel_Trace.def"
+  NUM_ATS
+};
+
+/// \returns the encoding value of tag \p T.
+char getTagEncoding(Tag T);
+
+/// \returns the tag if \p Byte is any tag's encoding, otherwise returns
+/// the sentinel tag.
+Tag getTagForEncoding(char Byte);
+
+/// \returns the string representation of tag \p T.
+StringRef getTagString(Tag T);
+
+/// \returns the byte size of attribute \p Att.
+unsigned getAttributeSize(Attribute Att);
+
+/// \returns the string representation of attribute \p Att.
+StringRef getAttributeString(Attribute Att);
+
+/// \returns the corresponding attribute for tag \p T if there is a
+/// 1-to-1 mapping.
+Attribute getAttributeForTag(Tag T);
+
+/// \returns the optimal line tag with given delta line \p DeltaLine.
+Tag getOptimalLineTag(int DeltaLine);
+
+/// \returns the optimal PC tag with given delta PC \p DeltaPC.
+Tag getOptimalPCTag(unsigned DeltaPC);
+
+/// \returns the optimal correlation tag with given delta line \p DeltaLine
+/// and delta pc \p DeltaPC.
+Optional<Tag> getOptimalCorrelationTag(int DeltaLine, unsigned DeltaPC);
+
+} // namespace traceback
+} // namespace llvm
+
+#endif // LLVM_BINARYFORMAT_TRACE_H
