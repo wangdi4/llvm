0af4caf1bab03d471a0dfc870af04686f54866b4
Michael D Toguchi <michael.d.toguchi@intel.com>
2020-08-14 11:01:38 -0700

    [Driver] Add -mcmodel compatible syntax
    Users could use '-mcmodel val' with icc, we should allow that syntax
    for icx as well.
    
    Change-Id: I5fd27db3993d8d3113ad59dabfac9e39eea1bf7b
    Reviewed-on: https://git-amr-2.devtools.intel.com/gerrit/265627
    Reviewed-by: Toguchi, Michael D <michael.d.toguchi@intel.com>
    Tested-by: Toguchi, Michael D <michael.d.toguchi@intel.com>

--- clang/test/Driver/mcmodel.c
+++ clang/test/Driver/mcmodel.c
@@ -3,6 +3,13 @@
 // RUN: %clang -target x86_64 -### -S -mcmodel=kernel %s 2>&1 | FileCheck --check-prefix=KERNEL %s
 // RUN: %clang -target x86_64 -### -c -mcmodel=medium %s 2>&1 | FileCheck --check-prefix=MEDIUM %s
 // RUN: %clang -target x86_64 -### -S -mcmodel=large %s 2>&1 | FileCheck --check-prefix=LARGE %s
+// INTEL_CUSTOMIZATION
+// RUN: %clang -target x86_64 -### -c -mcmodel tiny %s 2>&1 | FileCheck --check-prefix=TINY %s
+// RUN: %clang -target x86_64 -### -c -mcmodel small %s 2>&1 | FileCheck --check-prefix=SMALL %s
+// RUN: %clang -target x86_64 -### -S -mcmodel kernel %s 2>&1 | FileCheck --check-prefix=KERNEL %s
+// RUN: %clang -target x86_64 -### -c -mcmodel medium %s 2>&1 | FileCheck --check-prefix=MEDIUM %s
+// RUN: %clang -target x86_64 -### -S -mcmodel large %s 2>&1 | FileCheck --check-prefix=LARGE %s
+// end INTEL_CUSTOMIZATION
 // RUN: not %clang -c -mcmodel=lager %s 2>&1 | FileCheck --check-prefix=INVALID %s
 
 // TINY: "-mcmodel=tiny"
