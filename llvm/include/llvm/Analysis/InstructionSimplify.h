68c82074c560a988c0425a4a4000f510f095b570
Christopher M Chrulski <christopher.m.chrulski@intel.com>
2020-08-14 09:22:52 -0700

    Resolve conflicts after 5a0d6cdbd16c [InstSimplify] Make sure CanUseUndef is initialized in all cases.
    Change-Id: I97a52e9cf8d0a102db922f92856c6b61cf5e6c9f
    Reviewed-on: https://git-amr-2.devtools.intel.com/gerrit/265613
    Reviewed-by: Chrulski, Christopher M <christopher.m.chrulski@intel.com>
    Tested-by: Chrulski, Christopher M <christopher.m.chrulski@intel.com>

--- llvm/include/llvm/Analysis/InstructionSimplify.h
+++ llvm/include/llvm/Analysis/InstructionSimplify.h
@@ -102,12 +102,8 @@ struct SimplifyQuery {
   /// Controls whether simplifications are allowed to constrain the range of
   /// possible values for uses of undef. If it is false, simplifications are not
   /// allowed to assume a particular value for a use of undef for example.
-<<<<<<< HEAD
-  bool CanUseUndef;
-  const TargetTransformInfo *TTI = nullptr; // INTEL
-=======
   bool CanUseUndef = true;
->>>>>>> 5a0d6cdbd16c80d39e3a237582f6bb47aadacb67
+  const TargetTransformInfo *TTI = nullptr; // INTEL
 
   SimplifyQuery(const DataLayout &DL, const Instruction *CXTI = nullptr)
       : DL(DL), CxtI(CXTI) {}
