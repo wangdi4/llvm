bcac9fde1ae8a3f7e32730dce5dd61c847db3bbf
Michael D Toguchi <michael.d.toguchi@intel.com>
2020-08-14 17:06:07 -0700

    [Driver] Restrict unknown gcc options when doing offload
    When performing OpenMP offload using a target that uses 'gcc' to
    perform various steps, we need to be sure to filter out any options
    that do not make sense in that compilation line.
    
    Change-Id: I8c979a2492f1dc8c39969c9f0c05008cb24a2047
    Reviewed-on: https://git-amr-2.devtools.intel.com/gerrit/265663
    Tested-by: xmain_checkin <lab_iclt@iclgerrit.sc.intel.com>
    Reviewed-by: Chilamantula, Harini <harini.chilamantula@intel.com>
    Reviewed-by: Bozhenov, Nikolai <nikolai.bozhenov@intel.com>

--- clang/test/Driver/intel-openmp-offload.c
+++ clang/test/Driver/intel-openmp-offload.c
@@ -198,3 +198,8 @@
 // RUN: %clangxx -target x86_64-unknown-linux-gnu -fopenmp -fopenmp-targets=spir64 %s -### 2>&1 \
 // RUN:  | FileCheck %s -check-prefix=FOPENMP_ERROR
 // FOPENMP_ERROR: The use of '-fopenmp-targets=spir64' requires '-fiopenmp'
+
+/// check particular options aren't passed to gcc
+// RUN: %clangxx -target x86_64-unknown-linux-gnu -fopenmp -fopenmp-targets=x86_64 %s -### 2>&1 \
+// RUN:  | FileCheck %s -check-prefix=FOPENMP_NOGCC_OPT
+// FOPENMP_NOGCC_OPT-NOT: gcc{{.*}} "-fiopenmp" {{.*}} "-fheinous-gnu-extensions" "-fveclib=SVML"
