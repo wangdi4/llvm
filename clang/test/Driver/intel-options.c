2b0b80201dd36bdd8fe9d7a974db0cbbbf7cadec
Michael D Toguchi <michael.d.toguchi@intel.com>
2020-08-18 19:09:23 -0700

    [Driver] Allow for usage -fp-model=fast=2
    When -fp-model=fast=2 is used, override with -fp-model=fast and do
    it with a warning.
    
    Change-Id: Ifbce59677dccf010f7b6603dcea80c677bfc76a9
    Reviewed-on: https://git-amr-2.devtools.intel.com/gerrit/266176
    Reviewed-by: Toguchi, Michael D <michael.d.toguchi@intel.com>
    Tested-by: Toguchi, Michael D <michael.d.toguchi@intel.com>

--- clang/test/Driver/intel-options.c
+++ clang/test/Driver/intel-options.c
@@ -203,12 +203,14 @@
 // RUN: %clang_cl -### /Qfp-speculation:safe -c %s 2>&1 | FileCheck --check-prefix=CHECK-SAFE %s
 // RUN: %clang -### -fp-speculation=safe -c %s 2>&1 | FileCheck --check-prefix=CHECK-SAFE %s
 // RUN: %clang -### -fp-model=fast -S %s 2>&1 | FileCheck --check-prefix=CHECK-FAST %s
+// RUN: %clang -### -fp-model=fast=2 -S %s 2>&1 | FileCheck --check-prefixes=CHECK-FAST,CHECK-FAST2 %s
 // RUN: %clang -### -fp-model source -S %s 2>&1 | FileCheck --check-prefix=CHECK-SOURCE %s
 // RUN: %clang -### -fp-model=source -S %s 2>&1 | FileCheck --check-prefix=CHECK-SOURCE %s
 // RUN: %clang_cl -### -fp:fast -S %s 2>&1 | FileCheck --check-prefix=CHECK-FAST %s
 // CHECK-SAFE: "-ffp-exception-behavior=maytrap"
 // CHECK-STRICT: "-ffp-exception-behavior=strict"
 // CHECK-IGNORE: "-ffp-exception-behavior=ignore"
+// CHECK-FAST2: overriding
 // CHECK-FAST: "-ffp-contract=fast"
 // CHECK-SOURCE: "-fno-rounding-math"
 
