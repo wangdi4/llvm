d0349ebdff24594cd2d0a7b97847708840d85bba
Michael D Toguchi <michael.d.toguchi@intel.com>
2020-08-19 18:52:19 -0700

    [Driver] Update -fast behavior in the presence of -Qx
    When performing -fast -QxOPT, there were problems with creating the
    options for lld-link.  The options should be passed singularly, but
    were being passed multiple times.  This was due to the difference
    in -x and /Qx option handling.
    
    Change-Id: Ibd8291a7881e12672e5f08e9a4c275592f2565a5
    Reviewed-on: https://git-amr-2.devtools.intel.com/gerrit/266458
    Tested-by: xmain_checkin <lab_iclt@iclgerrit.sc.intel.com>
    Reviewed-by: Chilamantula, Harini <harini.chilamantula@intel.com>
    Reviewed-by: Bozhenov, Nikolai <nikolai.bozhenov@intel.com>

---
+++ clang/test/Driver/intel-fast-override.cpp
@@ -0,0 +1,13 @@
+// -fast -xOPT settings
+// RUN: %clang -### --intel -c -fast -xSSE4.2 %s 2>&1 | FileCheck -check-prefix CHECK-INTEL-FAST-X %s
+// RUN: %clang_cl -### --intel -c -fast -QxSSE4.2 %s 2>&1 | FileCheck -check-prefix CHECK-INTEL-FAST-X %s
+// CHECK-INTEL-FAST-X: "-flto" "-flto-unit"
+// CHECK-INTEL-FAST-X: "-target-cpu" "corei7"
+// CHECK-INTEL-FAST-X: "-O3"
+// CHECK-INTEL-FAST-X: "-mllvm" "-loopopt"
+
+// -fast -xOPT should not generate duplicate entries
+// RUN: %clang_cl -### --intel -fast -QxSSE4.2 %s 2>&1 | FileCheck -check-prefix CHECK-INTEL-FAST-NODUP %s
+// CHECK-INTEL-FAST-NODUP: "-mllvm:-mcpu=corei7"
+// CHECK-INTEL-FAST-NODUP-NOT: "-mllvm:-enable-multiversioning" "-mllvm:-enable-multiversioning"
+
