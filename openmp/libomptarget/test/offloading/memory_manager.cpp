4d2934fd1147663a096fd9d9ae37cf1164022dc9
Liu, Chen3 <chen3.liu@intel.com>
2020-08-24 14:23:48 +0800

    Disable lit test offloading/memory_manager.cpp(CMPLRLLVM-22302)
    Change-Id: Icfafeca5a8e83ffd0f83f73705590e673a6ef2f0
    Reviewed-on: https://git-amr-2.devtools.intel.com/gerrit/267002
    Reviewed-by: LiuChen <chen3.liu@intel.com>
    Tested-by: LiuChen <chen3.liu@intel.com>

--- openmp/libomptarget/test/offloading/memory_manager.cpp
+++ openmp/libomptarget/test/offloading/memory_manager.cpp
@@ -1,7 +1,8 @@
 // RUN: %libomptarget-compilexx-run-and-check-aarch64-unknown-linux-gnu
 // RUN: %libomptarget-compilexx-run-and-check-powerpc64-ibm-linux-gnu
 // RUN: %libomptarget-compilexx-run-and-check-powerpc64le-ibm-linux-gnu
-// RUN: %libomptarget-compilexx-run-and-check-x86_64-pc-linux-gnu
+// INTEL: Enable when CMPLRLLVM-22302 fixed
+// RUN;: %libomptarget-compilexx-run-and-check-x86_64-pc-linux-gnu
 // RUN: %libomptarget-compilexx-run-and-check-nvptx64-nvidia-cuda
 
 #include <omp.h>
