098bda3d5f9ee87e384aa765c7974fbe402fbb6b
Liu, Chen3 <chen3.liu@intel.com>
2020-08-27 10:41:52 +0800

    Resolve conflicts after 486ed885339d [ConstProp] Remove ConstantPropagation
    Change-Id: I36e741b1800ff02e230f4b3ea9cd3ab7e95a7b16
    Reviewed-on: https://git-amr-2.devtools.intel.com/gerrit/267701
    Reviewed-by: LiuChen <chen3.liu@intel.com>
    Tested-by: LiuChen <chen3.liu@intel.com>

--- llvm/lib/Transforms/Scalar/Scalar.cpp
+++ llvm/lib/Transforms/Scalar/Scalar.cpp
@@ -44,11 +44,7 @@ void llvm::initializeScalarOpts(PassRegistry &Registry) {
   initializeAlignmentFromAssumptionsPass(Registry);
   initializeCallSiteSplittingLegacyPassPass(Registry);
   initializeConstantHoistingLegacyPassPass(Registry);
-<<<<<<< HEAD
-  initializeConstantPropagationPass(Registry);
   initializeConvertGEPToSubscriptIntrinsicLegacyPassPass(Registry); // INTEL
-=======
->>>>>>> 486ed885339d70cd71ee55567282a43cce28d763
   initializeCorrelatedValuePropagationPass(Registry);
   initializeDCELegacyPassPass(Registry);
   initializeDeadInstEliminationPass(Registry);
