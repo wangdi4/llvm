b075ef509778da9da818b0b51886f3e95359fdb4
Erich Keane <erich.keane@intel.com>
2020-09-18 06:09:33 -0700

    Fix merge issue that caused CMPLRLLVM-23042
    It seems that a 'break' got lost from that switch during the merge
    process, which resulted in pipe code getting into swift-error handling
    code.  This patch adds the missing 'break' back.
    
    Change-Id: I7ad8e8492dc4056c4e737f5e3a3ce25b8106de2f
    Reviewed-on: https://git-amr-2.devtools.intel.com/gerrit/271725
    Reviewed-by: Cox, Robert <robert.cox@intel.com>
    Tested-by: Cox, Robert <robert.cox@intel.com>

--- clang/lib/Sema/SemaDeclAttr.cpp
+++ clang/lib/Sema/SemaDeclAttr.cpp
@@ -9233,6 +9233,7 @@ static void ProcessDeclAttribute(Sema &S, Scope *scope, Decl *D,
     break;
   case ParsedAttr::AT_SYCLIntelPipeIO:
     handleSYCLIntelPipeIOAttr(S, D, AL);
+    break;
 
   // Swift attributes.
   case ParsedAttr::AT_SwiftError:
