0ef24fe1e11092f62ed2a8703e73436e81e8916b
Michael D Toguchi <michael.d.toguchi@intel.com>
2020-09-21 15:02:58 -0700

    [Driver][SYCL] Adjust /MD settings for SYCL device compilations (#2497)
    When setting /MD (by default) we do not want to pass the _MT and _DLL
    predefines to the device compilations, only the host for clang-cl
    
    Taken from: https://github.com/intel/llvm/commit/0092d4da23a16712e1cb15bee067cb3df3565955
    Addresses: CMPLRLLVM-13952
    
    Change-Id: I141205c766aff58aa386f1a954e1af7b5aa7c6cb
    Reviewed-on: https://git-amr-2.devtools.intel.com/gerrit/272179
    Reviewed-by: Toguchi, Michael D <michael.d.toguchi@intel.com>
    Tested-by: Toguchi, Michael D <michael.d.toguchi@intel.com>

--- clang/lib/Driver/ToolChains/Clang.cpp
+++ clang/lib/Driver/ToolChains/Clang.cpp
@@ -7351,9 +7351,9 @@ void Clang::AddClangCLArgs(const ArgList &Args, types::ID InputType,
                            bool *EmitCodeView) const {
   unsigned RTOptionID = options::OPT__SLASH_MT;
   bool isNVPTX = getToolChain().getTriple().isNVPTX();
-  bool isSYCL =
-      Args.hasArg(options::OPT_fsycl) ||
+  bool isSYCLDevice =
       getToolChain().getTriple().getEnvironment() == llvm::Triple::SYCLDevice;
+  bool isSYCL = Args.hasArg(options::OPT_fsycl) || isSYCLDevice;
   // For SYCL Windows, /MD is the default.
   if (isSYCL)
     RTOptionID = options::OPT__SLASH_MD;
@@ -7365,13 +7365,23 @@ void Clang::AddClangCLArgs(const ArgList &Args, types::ID InputType,
 
   if (Arg *A = Args.getLastArg(options::OPT__SLASH_M_Group)) {
     RTOptionID = A->getOption().getID();
-    if (isSYCL && (RTOptionID == options::OPT__SLASH_MT ||
-                   RTOptionID == options::OPT__SLASH_MTd))
+    if (isSYCL && !isSYCLDevice &&
+        (RTOptionID == options::OPT__SLASH_MT ||
+         RTOptionID == options::OPT__SLASH_MTd))
       // Use of /MT or /MTd is not supported for SYCL.
       getToolChain().getDriver().Diag(diag::err_drv_unsupported_opt_dpcpp)
           << A->getOption().getName();
   }
 
+  enum { addDEBUG = 0x1, addMT = 0x2, addDLL = 0x4 };
+  auto addPreDefines = [&](unsigned Defines) {
+    if (Defines & addDEBUG)
+      CmdArgs.push_back("-D_DEBUG");
+    if (Defines & addMT && !isSYCLDevice)
+      CmdArgs.push_back("-D_MT");
+    if (Defines & addDLL && !isSYCLDevice)
+      CmdArgs.push_back("-D_DLL");
+  };
   StringRef FlagForCRT;
 #if INTEL_CUSTOMIZATION
   StringRef FlagForIntelMathLib;
@@ -7379,10 +7389,8 @@ void Clang::AddClangCLArgs(const ArgList &Args, types::ID InputType,
 #endif // INTEL_CUSTOMIZATION
   switch (RTOptionID) {
   case options::OPT__SLASH_MD:
-    if (Args.hasArg(options::OPT__SLASH_LDd))
-      CmdArgs.push_back("-D_DEBUG");
-    CmdArgs.push_back("-D_MT");
-    CmdArgs.push_back("-D_DLL");
+    addPreDefines((Args.hasArg(options::OPT__SLASH_LDd) ? addDEBUG : 0x0) |
+                  addMT | addDLL);
     FlagForCRT = "--dependent-lib=msvcrt";
 #if INTEL_CUSTOMIZATION
     FlagForIntelMathLib = "--dependent-lib=libmmd";
@@ -7390,9 +7398,7 @@ void Clang::AddClangCLArgs(const ArgList &Args, types::ID InputType,
 #endif // INTEL_CUSTOMIZATION
     break;
   case options::OPT__SLASH_MDd:
-    CmdArgs.push_back("-D_DEBUG");
-    CmdArgs.push_back("-D_MT");
-    CmdArgs.push_back("-D_DLL");
+    addPreDefines(addDEBUG | addMT | addDLL);
     FlagForCRT = "--dependent-lib=msvcrtd";
 #if INTEL_CUSTOMIZATION
     FlagForIntelMathLib = "--dependent-lib=libmmdd";
@@ -7400,9 +7406,8 @@ void Clang::AddClangCLArgs(const ArgList &Args, types::ID InputType,
 #endif // INTEL_CUSTOMIZATION
     break;
   case options::OPT__SLASH_MT:
-    if (Args.hasArg(options::OPT__SLASH_LDd))
-      CmdArgs.push_back("-D_DEBUG");
-    CmdArgs.push_back("-D_MT");
+    addPreDefines((Args.hasArg(options::OPT__SLASH_LDd) ? addDEBUG : 0x0) |
+                  addMT);
     CmdArgs.push_back("-flto-visibility-public-std");
     FlagForCRT = "--dependent-lib=libcmt";
 #if INTEL_CUSTOMIZATION
@@ -7411,8 +7416,7 @@ void Clang::AddClangCLArgs(const ArgList &Args, types::ID InputType,
 #endif // INTEL_CUSTOMIZATION
     break;
   case options::OPT__SLASH_MTd:
-    CmdArgs.push_back("-D_DEBUG");
-    CmdArgs.push_back("-D_MT");
+    addPreDefines(addDEBUG | addMT);
     CmdArgs.push_back("-flto-visibility-public-std");
     FlagForCRT = "--dependent-lib=libcmtd";
 #if INTEL_CUSTOMIZATION
