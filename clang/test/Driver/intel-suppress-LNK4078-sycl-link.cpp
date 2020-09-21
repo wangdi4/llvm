dbf350f77c62815b26af84ed0f0d677be7ee3fd7
Michael D Toguchi <michael.d.toguchi@intel.com>
2020-09-21 12:50:11 -0700

    [Driver][SYCL] suppress LNK4078 warning from linker
    When using -fsycl on Windows, suppress the link time diagnostic that
    is caused by long section names:
    warning LNK4078: multiple '__CLANG_OFFLOAD_BUNDLE_SIZE__syc' sections found with different attributes
    
    This is a temporary solution, and is going directly into xmain at this
    point in time.
    
    Change-Id: If25a04f493d37f518de60bf435f9a5924dca1a0e
    Addresses:  CMPLRLLVM-15704
    Reviewed-on: https://git-amr-2.devtools.intel.com/gerrit/272157
    Reviewed-by: Toguchi, Michael D <michael.d.toguchi@intel.com>
    Tested-by: Toguchi, Michael D <michael.d.toguchi@intel.com>

---
+++ clang/test/Driver/intel-suppress-LNK4078-sycl-link.cpp
@@ -0,0 +1,6 @@
+// REQUIRES: clang-driver
+// RUN:  %clangxx -target x86_64-pc-windows-msvc -fsycl %s -### 2>&1 \
+// RUN:  | FileCheck %s
+// RUN:  %clang_cl --target=x86_64-pc-windows-msvc -fsycl %s -### 2>&1 \
+// RUN:  | FileCheck %s
+// CHECK: /IGNORE:4078
