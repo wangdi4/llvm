wangdi4/llvm

commit 0fe0ab7b3c7b9a8886af7f1d39e4ebbfdbbbbd1f

Resolve conflict 5a2fe9ccbd17 [SYCL] Emit an aliased function only if it is used (#2430)
Change-Id: I448f75fa3407093c9590381bdac708d9a4c12c8c
Reviewed-on: https://git-amr-2.devtools.intel.com/gerrit/271681
Tested-by: Mikhail Lychkov <mikhail.lychkov@intel.com>
Reviewed-by: Mikhail Lychkov <mikhail.lychkov@intel.com>

clang/lib/CodeGen/CodeGenModule.cpp
@@ -2984,18 +2984,13 @@ void CodeGenModule::EmitGlobal(GlobalDecl GD) {
     return;
 
   // If this is an alias definition (which otherwise looks like a declaration)
-<<<<<<< HEAD
-  // emit it now.
-  if (Global->hasAttr<AliasAttr>())
-#if INTEL_COLLAB
-    if (canDefineAliasOnTarget(*this, GD))
-#endif // INTEL_COLLAB
-    return EmitAliasDefinition(GD);
-=======
   // handle it now.
   if (AliasAttr *Attr = Global->getAttr<AliasAttr>()) {
     // Emit the alias here if it is not SYCL device compilation.
     if (!LangOpts.SYCLIsDevice)
+#if INTEL_COLLAB
+      if (canDefineAliasOnTarget(*this, GD))
+#endif // INTEL_COLLAB
       return EmitAliasDefinition(GD);
     // Defer for SYCL devices, until either the alias or what it aliases
     // is used.
@@ -3005,7 +3000,6 @@ void CodeGenModule::EmitGlobal(GlobalDecl GD) {
     DeferredAliases[AliaseeName] = GD;
     return;
   }
->>>>>>> 5a2fe9ccbd17db1aa64ddfbb267685234aa41038
 
   // IFunc like an alias whose value is resolved at runtime by calling resolver.
   if (Global->hasAttr<IFuncAttr>())

commit 498cf88d1e37d6c5cfa681030fa7e14fc8397b0f

[SYCL] Emit an aliased function only if it is used (#2430)
Change-Id: I84d8a7a5bd502a197d85a3c5f39a76680a357b0b
Signed-off-by: Premanand M Rao <premanand.m.rao@intel.com>

[SYCL] Use a copy to avoid invalidated access (#2495)

This fixes a bug introduced in PR #2430 where the iterator
might get invalidated between accesses.

Signed-off-by: Premanand M Rao <premanand.m.rao@intel.com>
Change-Id: I5a413e583d291f08f303cb450713698240fe6b81
Reviewed-on: https://git-amr-2.devtools.intel.com/gerrit/272132
Tested-by: zperf_checkin_xmain <lab_iclz@iclgerrit.sc.intel.com>
Tested-by: xmain_checkin <lab_iclt@iclgerrit.sc.intel.com>
Reviewed-by: Andrews, Elizabeth <elizabeth.andrews@intel.com>
Reviewed-by: Bozhenov, Nikolai <nikolai.bozhenov@intel.com>

clang/lib/CodeGen/CodeGenModule.cpp
@@ -2559,6 +2559,31 @@ void CodeGenModule::EmitDeferred() {
   CurDeclsToEmit.swap(DeferredDeclsToEmit);
 
   for (GlobalDecl &D : CurDeclsToEmit) {
+    const ValueDecl *VD = cast<ValueDecl>(D.getDecl());
+    // If emitting for SYCL device, emit the deferred alias
+    // as well as what it aliases.
+    if (LangOpts.SYCLIsDevice) {
+      if (AliasAttr *Attr = VD->getAttr<AliasAttr>()) {
+        StringRef AliaseeName = Attr->getAliasee();
+        auto DDI = DeferredDecls.find(AliaseeName);
+        // Emit what is aliased first.
+        if (DDI != DeferredDecls.end()) {
+          GlobalDecl GD = DDI->second;
+          llvm::GlobalValue *AliaseeGV =
+              dyn_cast<llvm::GlobalValue>(GetAddrOfGlobal(GD, ForDefinition));
+          if (!AliaseeGV)
+            AliaseeGV = GetGlobalValue(getMangledName(GD));
+          assert(AliaseeGV);
+          EmitGlobalDefinition(GD, AliaseeGV);
+          // Remove the entry just added to the DeferredDeclsToEmit
+          // since we have emitted it.
+          DeferredDeclsToEmit.pop_back();
+        }
+        // Now emit the alias itself.
+        EmitAliasDefinition(D);
+        continue;
+      }
+    }
     // We should call GetAddrOfGlobal with IsForDefinition set to true in order
     // to get GlobalValue with exactly the type we need, not something that
     // might had been created for another decl with the same mangled name but
@@ -2590,6 +2615,20 @@ void CodeGenModule::EmitDeferred() {
 
     // Otherwise, emit the definition and move on to the next one.
     EmitGlobalDefinition(D, GV);
+
+    if (LangOpts.SYCLIsDevice) {
+      // If there are any aliases deferred for this, emit those now.
+      for (auto It = DeferredAliases.begin(); It != DeferredAliases.end();
+           /*no increment*/) {
+        const ValueDecl *Global = cast<ValueDecl>(It->second.getDecl());
+        if (It->first == getMangledName(D)) {
+          EmitAliasDefinition(Global);
+          It = DeferredAliases.erase(It);
+        } else {
+          ++It;
+        }
+      }
+    }
 
     // If we found out that we need to emit more decls, do that recursively.
     // This has the advantage that the decls are emitted in a DFS and related
@@ -2946,12 +2985,22 @@ void CodeGenModule::EmitGlobal(GlobalDecl GD) {
     return;
 
   // If this is an alias definition (which otherwise looks like a declaration)
-  // emit it now.
-  if (Global->hasAttr<AliasAttr>())
+  // handle it now.
+  if (AliasAttr *Attr = Global->getAttr<AliasAttr>()) {
+    // Emit the alias here if it is not SYCL device compilation.
+    if (!LangOpts.SYCLIsDevice)
 #if INTEL_COLLAB
     if (canDefineAliasOnTarget(*this, GD))
 #endif // INTEL_COLLAB
-    return EmitAliasDefinition(GD);
+      return EmitAliasDefinition(GD);
+    // Defer for SYCL devices, until either the alias or what it aliases
+    // is used.
+    StringRef MangledName = getMangledName(GD);
+    DeferredDecls[MangledName] = GD;
+    StringRef AliaseeName = Attr->getAliasee();
+    DeferredAliases[AliaseeName] = GD;
+    return;
+  }
 
   // IFunc like an alias whose value is resolved at runtime by calling resolver.
   if (Global->hasAttr<IFuncAttr>())
@@ -5461,20 +5510,21 @@ void CodeGenModule::EmitAliasDefinition(GlobalDecl GD) {
   // if a deferred decl.
   llvm::Constant *Aliasee;
   llvm::GlobalValue::LinkageTypes LT;
+  unsigned AS;
   if (isa<llvm::FunctionType>(DeclTy)) {
     Aliasee = GetOrCreateLLVMFunction(AA->getAliasee(), DeclTy, GD,
                                       /*ForVTable=*/false);
     LT = getFunctionLinkage(GD);
+    AS = Aliasee->getType()->getPointerAddressSpace();
   } else {
-    Aliasee = GetOrCreateLLVMGlobal(AA->getAliasee(),
-                                    llvm::PointerType::getUnqual(DeclTy),
+    AS = ArgInfoAddressSpace(GetGlobalVarAddressSpace(/*D=*/nullptr));
+    Aliasee = GetOrCreateLLVMGlobal(AA->getAliasee(), DeclTy->getPointerTo(AS),
                                     /*D=*/nullptr);
     LT = getLLVMLinkageVarDefinition(cast<VarDecl>(GD.getDecl()),
                                      D->getType().isConstQualified());
   }
 
   // Create the new alias itself, but don't set a name yet.
-  unsigned AS = Aliasee->getType()->getPointerAddressSpace();
   auto *GA =
       llvm::GlobalAlias::create(DeclTy, AS, LT, "", Aliasee, &getModule());
 
@@ -5495,8 +5545,8 @@ void CodeGenModule::EmitAliasDefinition(GlobalDecl GD) {
     // Remove it and replace uses of it with the alias.
     GA->takeName(Entry);
 
-    Entry->replaceAllUsesWith(llvm::ConstantExpr::getBitCast(GA,
-                                                          Entry->getType()));
+    Entry->replaceAllUsesWith(
+        llvm::ConstantExpr::getBitCast(GA, Entry->getType()));
     Entry->eraseFromParent();
   } else {
     GA->setName(MangledName);

clang/lib/CodeGen/CodeGenModule.h
@@ -360,6 +360,11 @@ private:
   /// yet.
   std::map<StringRef, GlobalDecl> DeferredDecls;
 
+  /// This contains all the aliases that are deferred for emission until
+  /// they or what they alias are actually used.  Note that the StringRef
+  /// associated in this map is that of the aliasee.
+  std::map<StringRef, GlobalDecl> DeferredAliases;
+
   /// This is a list of deferred decls which we have seen that *are* actually
   /// referenced. These get code generated when the module is done.
   std::vector<GlobalDecl> DeferredDeclsToEmit;

clang/test/CodeGenSYCL/sycl-device-alias.cpp
@@ -1,0 +1,45 @@
+// RUN: %clang_cc1 -fsycl -fsycl-is-device -triple spir64-unknown-unknown-sycldevice -disable-llvm-passes -emit-llvm %s -o - | FileCheck %s
+// Test that aliasing does not force an unused entity to be emitted
+
+// CHECK-NOT: define spir_func void @unused_func()
+extern "C" void unused_func() {}
+// CHECK-NOT: @unused_aliaser
+extern "C" void unused_aliaser() __attribute__((alias("unused_func")));
+// CHECK-NOT: @unused_int
+int unused_int = 3;
+// CHECK-NOT: @alias_unused_int
+extern int alias_unused_int __attribute__((alias("unused_int")));
+
+// CHECK-DAG: define spir_func void @used_func()
+extern "C" void used_func() {}
+// CHECK-DAG: @aliaser = alias void (), void ()* @used_func
+extern "C" void aliaser() __attribute__((alias("used_func")));
+
+// CHECK-DAG: define spir_func void @func()
+extern "C" void func() {}
+// CHECK-DAG: @used_aliaser = alias void (), void ()* @func
+extern "C" void used_aliaser() __attribute__((alias("func")));
+
+// CHECK-DAG: @used_int = addrspace(1) constant i32 5, align 4
+extern "C" const int used_int = 5;
+// CHECK-DAG: @alias_used_int = alias i32, i32 addrspace(1)* @used_int
+extern "C" const int alias_used_int __attribute__((alias("used_int")));
+// CHECK-DAG: @vint = addrspace(1) constant i32 7, align 4
+extern "C" const int vint = 7;
+// CHECK-DAG: @used_alias_used_int = alias i32, i32 addrspace(1)* @vint
+extern "C" const int used_alias_used_int __attribute__((alias("vint")));
+
+// CHECK-DAG: define spir_func void @{{.*}}bar{{.*}}
+void bar(const int &i) {}
+
+// CHECK-DAG: define spir_func void @{{.*}}foo{{.*}}
+void __attribute__((sycl_device)) foo() {
+  // CHECK-DAG: call spir_func void @{{.*}}bar{{.*}}@used_int
+  bar(used_int);
+  // CHECK-DAG: call spir_func void @{{.*}}bar{{.*}}@used_alias_used_int
+  bar(used_alias_used_int);
+  // CHECK-DAG: call spir_func void @used_func()
+  used_func();
+  // CHECK-DAG: call spir_func void @used_aliaser()
+  used_aliaser();
+}