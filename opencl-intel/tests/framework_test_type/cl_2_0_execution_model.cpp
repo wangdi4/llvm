5983a30152be5065f3bdd5216bd85e2587e58fb4
Link Ren <link.ren@intel.com>
2020-09-28 15:43:20 +0800

    Fix mismatch of profiling info
    This bug is exposed by OpenCL-CTS. Profiling timer at command state
    CL_PROFILING_COMMAND_END and CL_PROFILING_COMMAND_COMPLETE is mismatched
    if the kernel has no child.
    
    Change-Id: I36b2de183bcec8d173f11f0c117cb01290331cac
    Reviewed-on: https://git-amr-2.devtools.intel.com/gerrit/273418
    Reviewed-by: Senran Zhang <senran.zhang@intel.com>
    Tested-by: xmain_checkin <lab_iclt@iclgerrit.sc.intel.com>
    Reviewed-by: Wenju He <wenju.he@intel.com>
    Reviewed-by: Dai, Chunyang <chunyang.dai@intel.com>
    Reviewed-by: Pengfei Wang <pengfei.wang@intel.com>

--- opencl-intel/tests/framework_test_type/cl_2_0_execution_model.cpp
+++ opencl-intel/tests/framework_test_type/cl_2_0_execution_model.cpp
@@ -58,15 +58,20 @@ static const char* sProgSrc[] = {
     "static void waste_time()"
     "{"
     "    printf(\"child begins execution\\n\");"
-	  "  for (int i = 0; i < 1000000; i++)"
-		"    do_nothing();"
+    "    for (int i = 0; i < 1000000; i++)"
+    "        do_nothing();"
     "}"
-    "void kernel parent()"
+    "void kernel parent(int level)"
     "{"
+    "    if (level == 0) {"
+    "        printf(\"kernel ends execution\\n\");"
+    "        return;"
+    "    }"
     "    ndrange_t ndrange = ndrange_1D(1);"
     "    clk_event_t child;"
     "    queue_t queue = get_default_queue();"
-    "    enqueue_kernel(queue, CLK_ENQUEUE_FLAGS_WAIT_KERNEL, ndrange, 0, NULL, &child, ^{ waste_time(); });"
+    "    enqueue_kernel(queue, CLK_ENQUEUE_FLAGS_WAIT_KERNEL, ndrange, 0, NULL,"
+    "                   &child, ^{ waste_time(); });"
     "    printf(\"parent ends execution\\n\");"
     "}"
 };
@@ -77,66 +82,114 @@ static void TestProfilingCommandComplete(cl_context context, cl_device_id dev)
 {
     cl_int iRet = CL_SUCCESS;
     const size_t szLen = strlen(sProgSrc[0]);
-    cl_program prog = clCreateProgramWithSource(context, 1, sProgSrc, &szLen, &iRet);
+    cl_program prog =
+        clCreateProgramWithSource(context, 1, sProgSrc, &szLen, &iRet);
     CheckException("clCreateProgramWithSource", CL_SUCCESS, iRet);
     iRet = clBuildProgram(prog, 1, &dev, "-cl-std=CL2.0", NULL, NULL);
     if (iRet != CL_SUCCESS)
     {
         size_t szParamValSize;
-        cl_int iRet = clGetProgramBuildInfo(prog, dev, CL_PROGRAM_BUILD_LOG, 0, NULL, &szParamValSize);
+        cl_int iRet = clGetProgramBuildInfo(prog, dev, CL_PROGRAM_BUILD_LOG, 0,
+                                            NULL, &szParamValSize);
         CheckException("clGetProgramBuildInfo", CL_SUCCESS, iRet);
         std::vector<char> paramVal(szParamValSize);
-        iRet = clGetProgramBuildInfo(prog, dev, CL_PROGRAM_BUILD_LOG, szParamValSize, &paramVal[0], NULL);
+        iRet = clGetProgramBuildInfo(prog, dev, CL_PROGRAM_BUILD_LOG,
+                                     szParamValSize, &paramVal[0], NULL);
         CheckException("clGetProgramBuildInfo", CL_SUCCESS, iRet);
         cout << "Program build failed:" << endl;
         cout << &paramVal[0] << endl;
     }
     CheckException("clBuildProgram", CL_SUCCESS, iRet);
-        
+
     cl_kernel kernel = clCreateKernel(prog, "parent", &iRet);
     CheckException("clCreateKernel", CL_SUCCESS, iRet);
 
-    const cl_queue_properties queueProps[] = { CL_QUEUE_PROPERTIES, CL_QUEUE_PROFILING_ENABLE, 0 };
-    cl_command_queue queue = clCreateCommandQueueWithProperties(context, dev, queueProps, &iRet);
+    const cl_queue_properties queueProps[] = {CL_QUEUE_PROPERTIES,
+                                              CL_QUEUE_PROFILING_ENABLE, 0};
+    cl_command_queue queue =
+        clCreateCommandQueueWithProperties(context, dev, queueProps, &iRet);
     CheckException("clCreateCommandQueueWithProperties", CL_SUCCESS, iRet);
-                
+
     cl_event userEvent = clCreateUserEvent(context, &iRet);
     CheckException("clCreateUserEvent", CL_SUCCESS, iRet);
 
-    cl_event kernelEvent;
+    int level;
+    cl_ulong ulCmdEnd, ulCmdComplete;
     const size_t szGlobalWorkOffset = 0, szGlobalWorkSize = 1;
-    iRet = clEnqueueNDRangeKernel(queue, kernel, 1, &szGlobalWorkOffset, &szGlobalWorkSize, NULL, 1, &userEvent, &kernelEvent);
+
+    // Enqueue kernel without children.
+    level = 0;
+    cl_event kernelEvent0;
+
+    iRet = clSetKernelArg(kernel, 0, sizeof(level), &level);
+    CheckException("clSetKernelArg", CL_SUCCESS, iRet);
+
+    iRet = clEnqueueNDRangeKernel(queue, kernel, 1, &szGlobalWorkOffset,
+                                  &szGlobalWorkSize, NULL, 1, &userEvent,
+                                  &kernelEvent0);
     CheckException("clEnqueueNDRangeKernel", CL_SUCCESS, iRet);
 
-    cl_ulong ulCmdEnd, ulCmdComplete;
-    iRet = clGetEventProfilingInfo(kernelEvent, CL_PROFILING_COMMAND_COMPLETE, sizeof(ulCmdComplete), &ulCmdComplete, NULL);
-    CheckException("clGetEventProfilingInfo", CL_PROFILING_INFO_NOT_AVAILABLE, iRet);
+    // Enqueue kernel with children.
+    level = 1;
+    cl_event kernelEvent1;
+    iRet = clSetKernelArg(kernel, 0, sizeof(level), &level);
+    CheckException("clSetKernelArg", CL_SUCCESS, iRet);
+
+    iRet = clEnqueueNDRangeKernel(queue, kernel, 1, &szGlobalWorkOffset,
+                                  &szGlobalWorkSize, NULL, 1, &userEvent,
+                                  &kernelEvent1);
+    CheckException("clEnqueueNDRangeKernel", CL_SUCCESS, iRet);
+
+    iRet = clGetEventProfilingInfo(kernelEvent1, CL_PROFILING_COMMAND_COMPLETE,
+                                   sizeof(ulCmdComplete), &ulCmdComplete, NULL);
+    CheckException("clGetEventProfilingInfo", CL_PROFILING_INFO_NOT_AVAILABLE,
+                   iRet);
 
     iRet = clSetUserEventStatus(userEvent, CL_COMPLETE);
     CheckException("clSetUserEventStatus", CL_SUCCESS, iRet);
 
+    // Enqueue native kernel.
     cl_event nativeKernelEvent;
-    iRet = clEnqueueNativeKernel(queue, NativeKernel, NULL, 0, 0, NULL, NULL, 0, NULL, &nativeKernelEvent);
+    iRet = clEnqueueNativeKernel(queue, NativeKernel, NULL, 0, 0, NULL, NULL, 0,
+                                 NULL, &nativeKernelEvent);
 
     iRet = clFinish(queue);
     CheckException("clFinish", CL_SUCCESS, iRet);
-        
-    // check profiling of kernelEvent
-    iRet = clGetEventProfilingInfo(kernelEvent, CL_PROFILING_COMMAND_END, sizeof(ulCmdEnd), &ulCmdEnd, NULL);
-    CheckException("CL_PROFILING_COMMAND_END", CL_SUCCESS, iRet);
-    iRet = clGetEventProfilingInfo(kernelEvent, CL_PROFILING_COMMAND_COMPLETE, sizeof(ulCmdComplete), &ulCmdComplete, NULL);
-    CheckException("CL_PROFILING_COMMAND_COMPLETE", CL_SUCCESS, iRet);
-    CheckException("complete time should be after end time", ulCmdEnd < ulCmdComplete, true);
 
-    // check profiling of nativeKernelEvent
-    iRet = clGetEventProfilingInfo(nativeKernelEvent, CL_PROFILING_COMMAND_END, sizeof(ulCmdEnd), &ulCmdEnd, NULL);
+    // Check profiling of kernelEvent0.
+    iRet = clGetEventProfilingInfo(kernelEvent0, CL_PROFILING_COMMAND_END,
+                                   sizeof(ulCmdEnd), &ulCmdEnd, NULL);
     CheckException("CL_PROFILING_COMMAND_END", CL_SUCCESS, iRet);
-    iRet = clGetEventProfilingInfo(nativeKernelEvent, CL_PROFILING_COMMAND_COMPLETE, sizeof(ulCmdComplete), &ulCmdComplete, NULL);
+    iRet = clGetEventProfilingInfo(kernelEvent0, CL_PROFILING_COMMAND_COMPLETE,
+                                   sizeof(ulCmdComplete), &ulCmdComplete, NULL);
     CheckException("CL_PROFILING_COMMAND_COMPLETE", CL_SUCCESS, iRet);
-    CheckException("complete time should equal end time", ulCmdEnd == ulCmdComplete, true);
+    CheckException("complete time should be after end time",
+                   ulCmdEnd == ulCmdComplete, true);
+
+    // Check profiling of kernelEvent1.
+    iRet = clGetEventProfilingInfo(kernelEvent1, CL_PROFILING_COMMAND_END,
+                                   sizeof(ulCmdEnd), &ulCmdEnd, NULL);
+    CheckException("CL_PROFILING_COMMAND_END", CL_SUCCESS, iRet);
+    iRet = clGetEventProfilingInfo(kernelEvent1, CL_PROFILING_COMMAND_COMPLETE,
+                                   sizeof(ulCmdComplete), &ulCmdComplete, NULL);
+    CheckException("CL_PROFILING_COMMAND_COMPLETE", CL_SUCCESS, iRet);
+    CheckException("complete time should be after end time",
+                   ulCmdEnd < ulCmdComplete, true);
+
+    // Check profiling of nativeKernelEvent.
+    iRet = clGetEventProfilingInfo(nativeKernelEvent, CL_PROFILING_COMMAND_END,
+                                   sizeof(ulCmdEnd), &ulCmdEnd, NULL);
+    CheckException("CL_PROFILING_COMMAND_END", CL_SUCCESS, iRet);
+    iRet = clGetEventProfilingInfo(nativeKernelEvent,
+                                   CL_PROFILING_COMMAND_COMPLETE,
+                                   sizeof(ulCmdComplete), &ulCmdComplete, NULL);
+    CheckException("CL_PROFILING_COMMAND_COMPLETE", CL_SUCCESS, iRet);
+    CheckException("complete time should equal end time",
+                   ulCmdEnd == ulCmdComplete, true);
 
     clReleaseEvent(userEvent);
-    clReleaseEvent(kernelEvent);
+    clReleaseEvent(kernelEvent0);
+    clReleaseEvent(kernelEvent1);
     clReleaseEvent(nativeKernelEvent);
     clReleaseCommandQueue(queue);
     clReleaseKernel(kernel);
@@ -160,38 +213,38 @@ bool cl20ExecutionModel()
 	{
 		// create context
 		iRet = clGetPlatformIDs(1, &platform, NULL);
-    CheckException("clGetPlatformIDs", CL_SUCCESS, iRet);        
+    CheckException("clGetPlatformIDs", CL_SUCCESS, iRet);
     iRet = clGetDeviceIDs(platform, gDeviceType, 1, &device, NULL);
-    CheckException("clGetDeviceIDs", CL_SUCCESS, iRet);        
+    CheckException("clGetDeviceIDs", CL_SUCCESS, iRet);
     // check that device supports OpenCL 2.0
     size_t szParamValSize;
     iRet = clGetDeviceInfo(device, CL_DEVICE_VERSION, 0, NULL, &szParamValSize);
     CheckException("clGetDeviceInfo", CL_SUCCESS, iRet);
     vector<char> devVersion(szParamValSize);
     iRet = clGetDeviceInfo(device, CL_DEVICE_VERSION, szParamValSize, &devVersion[0], NULL);
-    CheckException("clGetDeviceInfo", CL_SUCCESS, iRet);    
-    if (string(&devVersion[0]).find("OpenCL 2.0") == string::npos)
+    CheckException("clGetDeviceInfo", CL_SUCCESS, iRet);
+    if (string(&devVersion[0]) >= string("OpenCL 2.0"))
     {
         cout << "device doesn't support OpenCL 2.0 - skip this sub-test" << endl;
         return true;
     }
 
-    const cl_context_properties prop[3] = { CL_CONTEXT_PLATFORM, (cl_context_properties)platform, 0 };    
-    context = clCreateContextFromType(prop, gDeviceType, NULL, NULL, &iRet);    
-    CheckException("clCreateContextFromType", CL_SUCCESS, iRet);    
+    const cl_context_properties prop[3] = { CL_CONTEXT_PLATFORM, (cl_context_properties)platform, 0 };
+    context = clCreateContextFromType(prop, gDeviceType, NULL, NULL, &iRet);
+    CheckException("clCreateContextFromType", CL_SUCCESS, iRet);
 
-		// do the real testing		
+		// do the real testing
 
 		cl_queue_properties props[] = { CL_QUEUE_PROPERTIES, CL_QUEUE_OUT_OF_ORDER_EXEC_MODE_ENABLE | CL_QUEUE_ON_DEVICE, 0 };
 		queue = clCreateCommandQueueWithProperties(context, device, props, &iRet);
 		CheckException("clCreateCommandQueueWithProperties", CL_SUCCESS, iRet);
-		CHECK_COND("clCreateCommandQueueWithProperties", NULL != queue);		
+		CHECK_COND("clCreateCommandQueueWithProperties", NULL != queue);
 
 		cl_uint uiQueueSize, uiDevQueuePreferredSize;
 		size_t szQueueSizeSize, szDevQueuePreferredSizeSize;
 		iRet = clGetDeviceInfo(device, CL_DEVICE_QUEUE_ON_DEVICE_PREFERRED_SIZE, sizeof(uiDevQueuePreferredSize), &uiDevQueuePreferredSize, &szDevQueuePreferredSizeSize);
 		CheckException("clGetDeviceInfo", CL_SUCCESS, iRet);
-		CheckException("clGetDeviceInfo", sizeof(uiDevQueuePreferredSize), szDevQueuePreferredSizeSize);		
+		CheckException("clGetDeviceInfo", sizeof(uiDevQueuePreferredSize), szDevQueuePreferredSizeSize);
 
 		cl_uint uiOnDeviceQueues;
 		size_t szOnDeviceQueuesSize;
