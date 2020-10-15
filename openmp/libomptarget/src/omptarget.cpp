34d84476e9cb2e1cf3d55d73d0548ff40af7b202
George Rokos <georgios.rokos@intel.com>
2020-10-15 04:07:16 -0700

    [libomptarget] Process motion clauses for the pointee of a PTR_AND_OBJ pair independently of the parent struct.
    Change-Id: Iada4a214b6bf470b04d53785527418f8b73416f2
    Reviewed-on: https://git-amr-2.devtools.intel.com/gerrit/277216
    Reviewed-by: Narayanaswamy, Ravi <ravi.narayanaswamy@intel.com>
    Tested-by: alloy <lab_icla@iclgerrit.sc.intel.com>
    Reviewed-by: Su, Ernesto <ernesto.su@intel.com>
    Reviewed-by: Huang, Zao <zao.huang@intel.com>

--- openmp/libomptarget/src/omptarget.cpp
+++ openmp/libomptarget/src/omptarget.cpp
@@ -399,8 +399,20 @@ int targetDataBegin(DeviceTy &Device, int32_t arg_num, void **args_base,
 #endif // INTEL_COLLAB
         if (IsNew || (arg_types[i] & OMP_TGT_MAPTYPE_ALWAYS)) {
           copy = true;
+#if INTEL_COLLAB
+        // The same patch is being committed to llorg:
+        // https://reviews.llvm.org/D89597
+        // When pulling down from llorg into xmain, INTEL_COLLAB should be
+        // removed.
+        } else if ((arg_types[i] & OMP_TGT_MAPTYPE_MEMBER_OF) &&
+                   !(arg_types[i] & OMP_TGT_MAPTYPE_PTR_AND_OBJ)) {
+          // Copy data only if the "parent" struct has RefCount==1.
+          // If this is a PTR_AND_OBJ entry, the OBJ is not part of the struct,
+          // so exclude it from this check.
+#else // INTEL_COLLAB
         } else if (arg_types[i] & OMP_TGT_MAPTYPE_MEMBER_OF) {
           // Copy data only if the "parent" struct has RefCount==1.
+#endif // INTEL_COLLAB
           int32_t parent_idx = getParentIndex(arg_types[i]);
           uint64_t parent_rc = Device.getMapEntryRefCnt(args[parent_idx]);
           assert(parent_rc > 0 && "parent struct not found");
