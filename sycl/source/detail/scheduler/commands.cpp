5188be03b9586e0762b3cbbd49771f7b7a023b0c
Vodopyanov, Dmitry <dmitry.vodopyanov@intel.com>
2020-10-15 13:09:58 +0300

    Revert commit c0e4fa6bcbe1f51c35a0db7c676602760fb2b229
    Change-Id: Ib37ecfa5345e58de829e4ef48947862fdabb92a7
    Reviewed-on: https://git-amr-2.devtools.intel.com/gerrit/277199
    Reviewed-by: Kurkov, Vasiliy A <vasiliy.a.kurkov@intel.com>
    Tested-by: Vodopyanov, Dmitry <dmitry.vodopyanov@intel.com>

--- sycl/source/detail/scheduler/commands.cpp
+++ sycl/source/detail/scheduler/commands.cpp
@@ -1609,7 +1609,20 @@ static void adjustNDRangePerKernel(NDRDescT &NDR, RT::PiKernel Kernel,
       get(Kernel, DeviceImpl.getHandleRef(), DeviceImpl.getPlugin());
 
   if (WGSize[0] == 0) {
-    WGSize = {1, 1, 1};
+    // kernel does not request specific workgroup shape - set one
+    id<3> MaxWGSizes =
+        get_device_info<id<3>, cl::sycl::info::device::max_work_item_sizes>::
+            get(DeviceImpl.getHandleRef(), DeviceImpl.getPlugin());
+
+    size_t WGSize1D = get_kernel_work_group_info<
+        size_t, cl::sycl::info::kernel_work_group::work_group_size>::
+        get(Kernel, DeviceImpl.getHandleRef(), DeviceImpl.getPlugin());
+
+    assert(MaxWGSizes[2] != 0);
+
+    // Set default work-group size in the Z-direction to either the max
+    // number of work-items or the maximum work-group size in the Z-direction.
+    WGSize = {1, 1, min(WGSize1D, MaxWGSizes[2])};
   }
   NDR.set(NDR.Dims, nd_range<3>(NDR.NumWorkGroups * WGSize, WGSize));
 }
