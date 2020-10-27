cb8bc8cf0edfe3912276087a55544eb659e3d0e0
Wei Wu <wei.wu@intel.com>
2020-10-27 10:49:06 +0800

    [framework_test_type] Disable UniformWorkGroupTest
    Change-Id: I6aa59da71d760b932c68360158519151a12886a4
    Reviewed-on: https://git-amr-2.devtools.intel.com/gerrit/279300
    Reviewed-by: Wenju He <wenju.he@intel.com>
    Reviewed-by: Dai, Chunyang <chunyang.dai@intel.com>
    Tested-by: xmain_checkin <lab_iclt@iclgerrit.sc.intel.com>
    Reviewed-by: Pengfei Wang <pengfei.wang@intel.com>

--- opencl-intel/tests/framework_test_type/framework_test_type.cpp
+++ opencl-intel/tests/framework_test_type/framework_test_type.cpp
@@ -644,7 +644,7 @@ TEST(FrameworkTestType, Test_globalVariableQueryTest)
     globalVariableSizeQueryTest();
 }
 
-TEST(FrameworkTestType, Test_UniformWorkGroupTest)
+TEST(FrameworkTestType, DISABLED_Test_UniformWorkGroupTest)
 {
     UniformWorkGroupTest();
 }
