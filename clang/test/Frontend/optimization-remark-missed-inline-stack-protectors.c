b8b8fc26c9f69ac5d3f33cf5b792e93a7444dd95
Andrei Elovikov <andrei.elovikov@intel.com>
2020-11-02 12:51:51 -0800

    Resolve failure on optimization-remark-missed-inline-stack-protectors.c
    Change-Id: If9a699801c5a9713be1902e06f23b8f07c23da0b
    Reviewed-on: https://git-amr-2.devtools.intel.com/gerrit/295447
    Reviewed-by: Cox, Robert <robert.cox@intel.com>
    Reviewed-by: Ammarguellat, Zahira <zahira.ammarguellat@intel.com>
    Reviewed-by: Elovikov, Andrei <andrei.elovikov@intel.com>
    Tested-by: Elovikov, Andrei <andrei.elovikov@intel.com>

--- clang/test/Frontend/optimization-remark-missed-inline-stack-protectors.c
+++ clang/test/Frontend/optimization-remark-missed-inline-stack-protectors.c
@@ -6,12 +6,18 @@ void foo(void) {
   side_effect();
 }
 
+// INTEL_CUSTOMIZATION
+// expected-remark@+5 {{foo not inlined into bar because it should never be inlined (cost=never): no alwaysinline attribute}}
+// END INTEL_CUSTOMIZATION
 // expected-remark@+3 {{foo will not be inlined into bar: stack protected callee but caller requested no stack protector}}
 __attribute__((no_stack_protector))
 void bar(void) {
   foo();
 }
 
+// INTEL_CUSTOMIZATION
+// expected-remark@+4 {{bar not inlined into baz because it should never be inlined (cost=never): no alwaysinline attribute}}
+// END INTEL_CUSTOMIZATION
 // expected-remark@+2 {{bar will not be inlined into baz: stack protected caller but callee requested no stack protector}}
 void baz(void) {
   bar();
