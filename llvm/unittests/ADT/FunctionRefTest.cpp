59136266ba3c4bff5fadb60795f29a07fc5b80f7
Andrei Elovikov <andrei.elovikov@intel.com>
2020-11-04 13:20:45 -0800

    Remove customization in llvm/unittests/ADT/FunctionRefTest.cpp
    Community fix for the issue is incompatible (compfail) with our temporary
    solution. Just revert the customization and bring the file back to community
    version.
    
    Change-Id: I55449e9adfcd1b6000004831e37df98aecc70f8f
    Reviewed-on: https://git-amr-2.devtools.intel.com/gerrit/295847
    Reviewed-by: Elovikov, Andrei <andrei.elovikov@intel.com>
    Tested-by: Elovikov, Andrei <andrei.elovikov@intel.com>

--- llvm/unittests/ADT/FunctionRefTest.cpp
+++ llvm/unittests/ADT/FunctionRefTest.cpp
@@ -54,12 +54,10 @@ std::string returns(function_ref<double()> F) { return "number"; }
 std::string returns(function_ref<StringRef()> F) { return "string"; }
 
 TEST(FunctionRefTest, SFINAE) {
-#if INTEL_CUSTOMIZATION
   // Use STREQ instead of just EQ.
-  EXPECT_STREQ("not a function", returns("boo!"));
-  EXPECT_STREQ("number", returns([] { return 42; }));
-  EXPECT_STREQ("string", returns([] { return "hello"; }));
-#endif // INTEL_CUSTOMIZATION
+  EXPECT_EQ("not a function", returns("boo!"));
+  EXPECT_EQ("number", returns([] { return 42; }));
+  EXPECT_EQ("string", returns([] { return "hello"; }));
 }
 
 } // namespace
