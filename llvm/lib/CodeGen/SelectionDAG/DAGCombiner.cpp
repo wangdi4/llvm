3942676988ce79ef7132e19d91a1ce388f833b75
Bing1 Yu <bing1.yu@intel.com>
2020-12-07 08:57:04 +0800

    [CodeGen] Modify the refineIndexType(...)'s code to fix a bug in D90942 In previous code, when refineIndexType(...) is called and Index is undef, Index.getOperand(0) will raise a assertion fail.
    Change-Id: Ibadfd84e73063d781d35410fc775636081460db9
    Reviewed-on: https://git-amr-2.devtools.intel.com/gerrit/301458
    Reviewed-by: Pengfei Wang <pengfei.wang@intel.com>
    Tested-by: Link Ren <link.ren@intel.com>
    Reviewed-by: Link Ren <link.ren@intel.com>

--- llvm/lib/CodeGen/SelectionDAG/DAGCombiner.cpp
+++ llvm/lib/CodeGen/SelectionDAG/DAGCombiner.cpp
@@ -9423,9 +9423,11 @@ bool refineUniformBase(SDValue &BasePtr, SDValue &Index, SelectionDAG &DAG) {
 bool refineIndexType(MaskedScatterSDNode *MSC, SDValue &Index, bool Scaled,
                      SelectionDAG &DAG) {
   const TargetLowering &TLI = DAG.getTargetLoweringInfo();
-  SDValue Op = Index.getOperand(0);
 
   if (Index.getOpcode() == ISD::ZERO_EXTEND) {
+#if INTEL_CUSTOMIZATION
+    SDValue Op = Index.getOperand(0);
+#endif // INTEL_CUSTOMIZATION
     MSC->setIndexType(Scaled ? ISD::UNSIGNED_SCALED : ISD::UNSIGNED_UNSCALED);
     if (TLI.shouldRemoveExtendFromGSIndex(Op.getValueType())) {
       Index = Op;
@@ -9434,6 +9436,9 @@ bool refineIndexType(MaskedScatterSDNode *MSC, SDValue &Index, bool Scaled,
   }
 
   if (Index.getOpcode() == ISD::SIGN_EXTEND) {
+#if INTEL_CUSTOMIZATION
+    SDValue Op = Index.getOperand(0);
+#endif // INTEL_CUSTOMIZATION
     MSC->setIndexType(Scaled ? ISD::SIGNED_SCALED : ISD::SIGNED_UNSCALED);
     if (TLI.shouldRemoveExtendFromGSIndex(Op.getValueType())) {
       Index = Op;
