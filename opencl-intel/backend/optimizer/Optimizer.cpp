bcc80a3006f6e52fdde3e17cf09fdebc6fd93cea
Olga Chupina <olga.chupina@intel.com>
2021-01-14 23:04:23 -0800

    Fix openCL compiler build.
    Change-Id: I94a0cdff076376b886cf17d0919f79870cdd9cec
    Reviewed-on: https://git-amr-2.devtools.intel.com/gerrit/306917
    Tested-by: Chupina, Olga <olga.chupina@intel.com>
    Reviewed-by: Wenju He <wenju.he@intel.com>
    Reviewed-by: Sachkov, Alexey <alexey.sachkov@intel.com>

--- opencl-intel/backend/optimizer/Optimizer.cpp
+++ opencl-intel/backend/optimizer/Optimizer.cpp
@@ -1068,7 +1068,7 @@ void Optimizer::initializePasses() {
   initializeAtomicExpandPass(Registry);
   initializeRewriteSymbolsLegacyPassPass(Registry);
   initializeWinEHPreparePass(Registry);
-  initializeDwarfEHPreparePass(Registry);
+  initializeDwarfEHPrepareLegacyPassPass(Registry);
   initializeSafeStackLegacyPassPass(Registry);
   initializeSjLjEHPreparePass(Registry);
   initializePreISelIntrinsicLoweringLegacyPassPass(Registry);
