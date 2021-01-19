5de7b858a2c2a497c40e4be9f71878d37392801a
Michael D Toguchi <michael.d.toguchi@intel.com>
2021-01-19 13:54:00 -0800

    [Driver][NFC] Update tests to match new expected behavior
    A couple of Windows tests have been disabled due to problems after
    some updates to the driver behavior for static libraries.  Fix up
    these tests to work with the updated expectations.
    
    Change-Id: I6869e612337ce30e603d3cf11ec1fb871d5e9f24
    Reviewed-on: https://git-amr-2.devtools.intel.com/gerrit/307488
    Reviewed-by: Chilamantula, Harini <harini.chilamantula@intel.com>
    Tested-by: xmain_checkin <lab_iclt@iclgerrit.sc.intel.com>
    Reviewed-by: Creech, Timothy M <timothy.m.creech@intel.com>
    (cherry picked from commit ae587a64461b63d512debb0d46a2fc5bf928f2e2)
    Reviewed-on: https://git-amr-2.devtools.intel.com/gerrit/307658
    Tested-by: alloy <lab_icla@iclgerrit.sc.intel.com>
    Reviewed-by: Kurkov, Vasiliy A <vasiliy.a.kurkov@intel.com>

--- clang/test/Driver/intel-openmp-offload-static-lib-win.cpp
+++ clang/test/Driver/intel-openmp-offload-static-lib-win.cpp
@@ -4,8 +4,6 @@
 // REQUIRES: clang-driver
 // REQUIRES: x86-registered-target
 // UNSUPPORTED: linux
-// INTEL TODO: Fix the Windows failures (see CMPLRLLVM-25319)
-// INTEL XFAIL: windows
 
 /// ###########################################################################
 
@@ -24,9 +22,8 @@
 // RUN:   | FileCheck %s -check-prefix=STATIC_LIB
 // RUN: %clangxx -target x86_64-unknown-linux-gnu -fiopenmp -fopenmp-targets=spir64 -L/dummy/dir %t.lo -### %t.o 2>&1 \
 // RUN:   | FileCheck %s -check-prefix=STATIC_LIB
-// STATIC_LIB: ld{{(.exe)?}}" "-r" "-o" "[[INPUTLD:[^ ]+\.o]]" {{.*}} "-L/dummy/dir"{{.*}} 
-// STATIC_LIB: clang-offload-bundler{{.*}} "-type=oo" {{.*}} "-inputs=[[INPUTLD]]" "-outputs=[[LISTFILE:.+\.txt]]"
-// STATIC_LIB: llvm-link{{.*}} "@[[LISTFILE]]"
+// STATIC_LIB: clang-offload-bundler{{.*}} "-type=a" {{.*}} "-inputs={{.*}}" "-outputs=[[LIBDEVICE:.+\.a]]"
+// STATIC_LIB: llvm-link{{.*}} "[[LIBDEVICE]]"
 // STATIC_LIB: ld{{.*}}
 
 /// ###########################################################################
@@ -38,9 +35,14 @@
 // RUN: touch %t-3.o
 // RUN: %clangxx -target x86_64-unknown-linux-gnu -fiopenmp -fopenmp-targets=spir64 %t.a -### %t-1.o %t-2.o %t-3.o 2>&1 \
 // RUN:   | FileCheck %s -check-prefix=STATIC_LIB_MULTI_O
-// STATIC_LIB_MULTI_O: ld{{(.exe)?}}" "-r" "-o" {{.*}} "[[INPUT:.+\-1.o]]" "[[INPUT:.+\-2.o]]" "[[INPUT:.+\-3.o]]" "[[INPUT:.+\.a]]"
-// STATIC_LIB_MULTI_O: clang-offload-bundler{{.*}} "-type=oo"
-// STATIC_LIB_MULTI_O: llvm-link{{.*}} "@{{.*}}"
+// STATIC_LIB_MULTI_O: clang-offload-bundler{{.*}} "-type=o" {{.*}} "-outputs=[[OBJHOST1:.+\.o]],[[OBJDEVICE1:.+\.o]]"
+// STATIC_LIB_MULTI_O: clang-offload-bundler{{.*}} "-type=o" {{.*}} "-outputs=[[OBJHOST2:.+\.o]],[[OBJDEVICE2:.+\.o]]"
+// STATIC_LIB_MULTI_O: clang-offload-bundler{{.*}} "-type=o" {{.*}} "-outputs=[[OBJHOST3:.+\.o]],[[OBJDEVICE3:.+\.o]]"
+// STATIC_LIB_MULTI_O: ld{{.*}} "[[OBJHOST1]]" "[[OBJHOST2]]" "[[OBJHOST3]]"
+// STATIC_LIB_MULTI_O: clang-offload-deps{{.*}}
+// STATIC_LIB_MULTI_O: clang-offload-bundler{{.*}} "-type=a" {{.*}} "-outputs=[[LIBDEVICE1:.+\.a]]"
+// STATIC_LIB_MULTI_O: llvm-link{{.*}} "[[OBJDEVICE1]]" "[[OBJDEVICE2]]" "[[OBJDEVICE3]]"{{.*}} "-o" "[[LINKDEVICEOBJ:.+\.bc]]"
+// STATIC_LIB_MULTI_O: llvm-link{{.*}} "[[LINKDEVICEOBJ]]" "[[LIBDEVICE1]]"
 
 /// ###########################################################################
 
@@ -48,9 +50,8 @@
 // RUN: touch %t.a
 // RUN: %clangxx -target x86_64-unknown-linux-gnu -fiopenmp -fopenmp-targets=spir64  %t.a -### %s 2>&1 \
 // RUN:   | FileCheck %s -check-prefix=STATIC_LIB_SRC
-// STATIC_LIB_SRC: ld{{(.exe)?}}" "-r" "-o" {{.*}} "[[INPUT:.+\.a]]"
-// STATIC_LIB_SRC: clang-offload-bundler{{.*}} "-type=oo"
-// STATIC_LIB_SRC: llvm-link{{.*}} "@{{.*}}"
+// STATIC_LIB_SRC: clang-offload-bundler{{.*}} "-type=a" {{.*}} "-outputs=[[LIBDEVICE:.+\.a]]"
+// STATIC_LIB_SRC: llvm-link{{.*}} "[[LIBDEVICE]]"
 
 /// ###########################################################################
 
@@ -60,9 +61,8 @@
 // RUN: %clangxx -target x86_64-unknown-linux-gnu -fiopenmp -fopenmp-targets=spir64 -L/dummy/dir %t.lo -### 2>&1 \
 // RUN:   | FileCheck %s -check-prefix=STATIC_LIB_NOSRC
 // STATIC_LIB_NOSRC: clang-offload-bundler{{.*}} "-type=ao" "-targets=host-x86_64-unknown-linux-gnu" "-inputs={{.*}}" "-check-section"
-// STATIC_LIB_NOSRC: ld{{.*}} "-r" "-o" "[[PARTIALOBJ:.+\.o]]" "{{.*}}crt1.o" {{.*}} "-L/dummy/dir" {{.*}}
-// STATIC_LIB_NOSRC: clang-offload-bundler{{.*}} "-type=oo" "-targets=openmp-spir64" "-inputs=[[PARTIALOBJ]]" "-outputs=[[DEVICELIST:.+\.txt]]" "-unbundle"
-// STATIC_LIB_NOSRC: llvm-link{{.*}} "@[[DEVICELIST]]" "-o" "[[OUTFILE:.+\.bc]]"
+// STATIC_LIB_NOSRC: clang-offload-bundler{{.*}} "-type=a" "-targets=openmp-spir64" "-inputs={{.*}}" "-outputs=[[DEVICELIB:.+\.a]]" "-unbundle"
+// STATIC_LIB_NOSRC: llvm-link{{.*}} "[[DEVICELIB]]" "-o" "[[OUTFILE:.+\.bc]]"
 // STATIC_LIB_NOSRC: sycl-post-link{{.*}} "-o" "[[BCFILE:.+\.bc]]" "[[OUTFILE]]"
 // STATIC_LIB_NOSRC: llvm-spirv{{.*}} "-o" "[[OUTFILE2:.+\.spv]]" {{.*}} "[[BCFILE]]"
 // STATIC_LIB_NOSRC: clang-offload-wrapper{{.*}} "-host" "x86_64-unknown-linux-gnu" "-o" "[[BCFILE2:.+\.bc]]" "-kind=openmp" "-target=spir64" "[[OUTFILE2]]"
@@ -80,8 +80,8 @@
 // RUN: env LIB=%t_dir \
 // RUN: %clang_cl --target=x86_64-pc-windows-msvc -Qiopenmp -Qopenmp-targets=spir64 %s liboffload.lib -### 2>&1 \
 // RUN:  | FileCheck %s -check-prefix=STATIC_LIB_WIN_LIBENV
-// STATIC_LIB_WIN_LIBENV: clang-offload-bundler{{.*}} "-type=aoo" "-targets=openmp-spir64" "-inputs={{.*}}liboffload.lib" "-outputs=[[BUNDLETXT:.+\.txt]]" "-unbundle"
-// STATIC_LIB_WIN_LIBENV: llvm-link{{.*}} "@[[BUNDLETXT]]" "-o" "[[LINKOUT:.+\.bc]]"
+// STATIC_LIB_WIN_LIBENV: clang-offload-bundler{{.*}} "-type=a" "-targets=openmp-spir64" "-inputs={{.*}}liboffload.lib" "-outputs=[[DEVICELIB:.+\.a]]" "-unbundle"
+// STATIC_LIB_WIN_LIBENV: llvm-link{{.*}} "[[DEVICELIB]]" "-o" "[[LINKOUT:.+\.bc]]"
 // STATIC_LIB_WIN_LIBENV: sycl-post-link{{.*}} "--ompoffload-link-entries" "--ompoffload-sort-entries" "--ompoffload-make-globals-static" "-ir-output-only" "-spec-const=rt" "-o" "[[POSTLINKBC:.+\.bc]]" "[[LINKOUT]]"
 // STATIC_LIB_WIN_LIBENV: llvm-spirv{{.*}} "-o" "[[SPIRVOUT:.+\.spv]]" "-spirv-ext={{.*}}" "[[POSTLINKBC]]"
 // STATIC_LIB_WIN_LIBENV: clang-offload-wrapper{{.*}} "-host" "x86_64-pc-windows-msvc{{.*}}" "-o" "[[WRAPPEROUT:.+\.bc]]" "-kind=openmp" "-target=spir64" "[[SPIRVOUT]]"
