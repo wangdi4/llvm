765a102684a83fb35654a23e38586e937e8ba691
Harini Chilamantula <harini.chilamantula@intel.com>
2021-01-21 11:19:05 -0800

    CMPLRLLVM-25644: port icx branch align options on windows
    Change-Id: I501b364492c72cd03f3ed085156021298202d9f1
    Reviewed-on: https://git-amr-2.devtools.intel.com/gerrit/307887
    Reviewed-by: Chilamantula, Harini <harini.chilamantula@intel.com>
    Tested-by: Chilamantula, Harini <harini.chilamantula@intel.com>

--- clang/test/Driver/x86-malign-branch.c
+++ clang/test/Driver/x86-malign-branch.c
@@ -2,36 +2,65 @@
 
 /// Test -malign-branch-boundary=
 // RUN: %clang -target x86_64 -malign-branch-boundary=16 %s -c -### 2>&1 | FileCheck %s --check-prefix=BOUNDARY
+#if INTEL_CUSTOMIZATION
+// RUN: %clang_cl --target=x86_64 /Qalign-branch-boundary=16 %s -c -### 2>&1 | FileCheck %s --check-prefix=BOUNDARY
+// RUN: %clang_cl --target=x86_64 /Qalign-branch-boundary:16 %s -c -### 2>&1 | FileCheck %s --check-prefix=BOUNDARY
+#endif // INTEL_CUSTOMIZATION
 // BOUNDARY: "-mllvm" "-x86-align-branch-boundary=16"
 // RUN: %clang -target x86_64-unknown-linux -malign-branch-boundary=16 -flto %s -### 2>&1 | FileCheck %s --check-prefix=BOUNDARY-LTO
 // BOUNDARY-LTO: "-plugin-opt=-x86-align-branch-boundary=16"
 
 // RUN: %clang -target x86_64 -malign-branch-boundary=8 %s -c -### 2>&1 | FileCheck %s --check-prefix=BOUNDARY-ERR
 // RUN: %clang -target x86_64 -malign-branch-boundary=15 %s -c -### 2>&1 | FileCheck %s --check-prefix=BOUNDARY-ERR
+#if INTEL_CUSTOMIZATION
+// RUN: %clang_cl --target=x86_64 /Qalign-branch-boundary=8 %s -c -### 2>&1 | FileCheck %s --check-prefix=BOUNDARY-ERR
+// RUN: %clang_cl --target=x86_64 /Qalign-branch-boundary=15 %s -c -### 2>&1 | FileCheck %s --check-prefix=BOUNDARY-ERR
+#endif // INTEL_CUSTOMIZATION
 // BOUNDARY-ERR: invalid argument {{.*}} to -malign-branch-boundary=
 
 /// Test -malign-branch=
 // RUN: %clang -target x86_64 -malign-branch=fused,jcc,jmp %s -c -### %s 2>&1 | FileCheck %s --check-prefix=TYPE0
+#if INTEL_CUSTOMIZATION
+// RUN: %clang_cl --target=x86_64 /Qalign-branch=fused,jcc,jmp %s -c -### %s 2>&1 | FileCheck %s --check-prefix=TYPE0
+// RUN: %clang_cl --target=x86_64 /Qalign-branch:fused,jcc,jmp %s -c -### %s 2>&1 | FileCheck %s --check-prefix=TYPE0
+#endif // INTEL_CUSTOMIZATION
 // TYPE0: "-mllvm" "-x86-align-branch=fused+jcc+jmp"
 // RUN: %clang -target x86_64 -malign-branch=fused,jcc,jmp,ret,call,indirect %s -c -### %s 2>&1 | FileCheck %s --check-prefix=TYPE1
+#if INTEL_CUSTOMIZATION
+// RUN: %clang_cl --target=x86_64 /Qalign-branch=fused,jcc,jmp,ret,call,indirect %s -c -### %s 2>&1 | FileCheck %s --check-prefix=TYPE1
+// RUN: %clang_cl --target=x86_64 /Qalign-branch:fused,jcc,jmp,ret,call,indirect %s -c -### %s 2>&1 | FileCheck %s --check-prefix=TYPE1
+#endif // INTEL_CUSTOMIZATION
 // TYPE1: "-mllvm" "-x86-align-branch=fused+jcc+jmp+ret+call+indirect"
 // RUN: %clang -target x86_64-unknown-linux -malign-branch=fused,jcc,jmp -flto %s -### %s 2>&1 | FileCheck %s --check-prefix=TYPE0-LTO
 // TYPE0-LTO: "-plugin-opt=-x86-align-branch=fused+jcc+jmp"
 
 // RUN: %clang -target x86_64 -malign-branch=fused,foo,bar %s -c -### %s 2>&1 | FileCheck %s --check-prefix=TYPE-ERR
+#if INTEL_CUSTOMIZATION
+// RUN: %clang_cl --target=x86_64 /Qalign-branch=fused,foo,bar %s -c -### %s 2>&1 | FileCheck %s --check-prefix=TYPE-ERR
+#endif // INTEL_CUSTOMIZATION
 // TYPE-ERR: invalid argument 'foo' to -malign-branch=; each element must be one of: fused, jcc, jmp, call, ret, indirect
 // TYPE-ERR: invalid argument 'bar' to -malign-branch=; each element must be one of: fused, jcc, jmp, call, ret, indirect
 
 /// Test -mpad-max-prefix-size=
 // RUN: %clang -target x86_64 -mpad-max-prefix-size=0 %s -c -### 2>&1 | FileCheck %s --check-prefix=PREFIX-0
+#if INTEL_CUSTOMIZATION
+// RUN: %clang_cl --target=x86_64 /Qpad-max-prefix-size=0 %s -c -### 2>&1 | FileCheck %s --check-prefix=PREFIX-0
+// RUN: %clang_cl --target=x86_64 /Qpad-max-prefix-size:0 %s -c -### 2>&1 | FileCheck %s --check-prefix=PREFIX-0
+#endif // INTEL_CUSTOMIZATION
 // PREFIX-0: "-mllvm" "-x86-pad-max-prefix-size=0"
 // RUN: %clang -target x86_64 -mpad-max-prefix-size=15 %s -c -### 2>&1 | FileCheck %s --check-prefix=PREFIX-15
+#if INTEL_CUSTOMIZATION
+// RUN: %clang_cl --target=x86_64 /Qpad-max-prefix-size=15 %s -c -### 2>&1 | FileCheck %s --check-prefix=PREFIX-15
+#endif // INTEL_CUSTOMIZATION
 // PREFIX-15: "-mllvm" "-x86-pad-max-prefix-size=15"
 // RUN: %clang -target x86_64-unknown-linux -mpad-max-prefix-size=0 -flto %s -### 2>&1 | FileCheck %s --check-prefix=PREFIX-0-LTO
 // PREFIX-0-LTO: "-plugin-opt=-x86-pad-max-prefix-size=0"
 
 /// Test -mbranches-within-32B-boundaries
 // RUN: %clang -target x86_64 -mbranches-within-32B-boundaries %s -c -### 2>&1 | FileCheck %s --check-prefix=32B
+#if INTEL_CUSTOMIZATION
+// RUN: %clang_cl --target=x86_64 /Qbranches-within-32B-boundaries %s -c -### 2>&1 | FileCheck %s --check-prefix=32B
+#endif // INTEL_CUSTOMIZATION
 // 32B: "-mllvm" "-x86-branches-within-32B-boundaries"
 // RUN: %clang -target x86_64-unknown-linux -mbranches-within-32B-boundaries -flto %s -### 2>&1 | FileCheck %s --check-prefix=32B-LTO
 // 32B-LTO: "-plugin-opt=-x86-branches-within-32B-boundaries"
