eb6c95fd40ff2eff0b8ce25730a3cb7fb2a6a470
Maosu Zhao <maosu.zhao@intel.com>
2021-01-21 14:50:10 +0800

    Add env "OPENCL_PROGRAM_COMPILE_OPTIONS" to pass build option to ocl
    Change-Id: I32f12eb96a280f79ea95dfc92ac6e47c491bfa09
    Reviewed-on: https://git-amr-2.devtools.intel.com/gerrit/307762
    Tested-by: xmain_checkin <lab_iclt@iclgerrit.sc.intel.com>
    Reviewed-by: Wenju He <wenju.he@intel.com>
    Reviewed-by: Dai, Chunyang <chunyang.dai@intel.com>
    Reviewed-by: Creech, Timothy M <timothy.m.creech@intel.com>

--- opencl-intel/tests/framework_test_type/EnqueueBlockProfiling.cpp
+++ opencl-intel/tests/framework_test_type/EnqueueBlockProfiling.cpp
@@ -8,34 +8,34 @@
 
 extern cl_device_type gDeviceType;
 
-const char *source = "\
-  __global ulong value[20] = {0};\n\
-  void block_fn(size_t tid, __global int* res)\n\
-  {\n\
-      res[tid] = -2;\n\
-  }\n\
-  void check_res(size_t tid, const clk_event_t evt, __global int* res)\n\
-  {\n\
-      capture_event_profiling_info (evt, CLK_PROFILING_COMMAND_EXEC_TIME, &value[tid*2]);\n\
-      if (value[tid*2] > 0 && value[tid*2+1] > 0) res[tid] =  0;\n\
-      else                                        res[tid] = -4;\n\
-      release_event(evt);\n\
-  }\n\
-  kernel void enqueue_block_profiling(__global int* res)\n\
-  {\n\
-      size_t tid = get_global_id(0);\n\
-      res[tid] = -1;\n\
-      queue_t def_q = get_default_queue();\n\
-      ndrange_t ndrange = ndrange_1D(1);\n\
-      clk_event_t block_evt1;\n\
-      void (^kernelBlock)(void)  = ^{ block_fn (tid, res); };\n\
-      int enq_res = enqueue_kernel(def_q, CLK_ENQUEUE_FLAGS_NO_WAIT, ndrange, 0, NULL, &block_evt1, kernelBlock);\n\
-      if(enq_res != CLK_SUCCESS) { res[tid] = -1; return; }\n\
-      void (^checkBlock) (void)  = ^{ check_res(tid, block_evt1, res); };\n\
-      enq_res = enqueue_kernel(def_q, CLK_ENQUEUE_FLAGS_NO_WAIT, ndrange, 1, &block_evt1, NULL, checkBlock);\n\
-      if(enq_res != CLK_SUCCESS) { res[tid] = -3; return; }\n\
-  }\n\
-  ";
+static const char *source = R"(
+  __global ulong value[20] = {0};
+  void block_fn(size_t tid, __global int* res)
+  {
+      res[tid] = -2;
+  }
+  void check_res(size_t tid, const clk_event_t evt, __global int* res)
+  {
+      capture_event_profiling_info (evt, CLK_PROFILING_COMMAND_EXEC_TIME, &value[tid*2]);
+      if (value[tid*2] > 0 && value[tid*2+1] > 0) res[tid] =  0;
+      else                                        res[tid] = -4;
+      release_event(evt);
+  }
+  kernel void enqueue_block_profiling(__global int* res)
+  {
+      size_t tid = get_global_id(0);
+      res[tid] = -1;
+      queue_t def_q = get_default_queue();
+      ndrange_t ndrange = ndrange_1D(1);
+      clk_event_t block_evt1;
+      void (^kernelBlock)(void)  = ^{ block_fn (tid, res); };
+      int enq_res = enqueue_kernel(def_q, CLK_ENQUEUE_FLAGS_NO_WAIT, ndrange, 0, NULL, &block_evt1, kernelBlock);
+      if(enq_res != CLK_SUCCESS) { res[tid] = -1; return; }
+      void (^checkBlock) (void)  = ^{ check_res(tid, block_evt1, res); };
+      enq_res = enqueue_kernel(def_q, CLK_ENQUEUE_FLAGS_NO_WAIT, ndrange, 1, &block_evt1, NULL, checkBlock);
+      if(enq_res != CLK_SUCCESS) { res[tid] = -3; return; }
+  }
+  )";
 
 void enqueueBlockProfilingTest() {
   cl_int err = CL_SUCCESS;
--- opencl-intel/tests/framework_test_type/framework_test_type.cpp
+++ opencl-intel/tests/framework_test_type/framework_test_type.cpp
@@ -632,6 +632,16 @@ TEST(FrameworkTestType, Test_enqueueBlockProfilingTest)
     enqueueBlockProfilingTest();
 }
 
+#ifndef _WIN32
+// For windows, framework_test_type is directly linked against intelocl*.dll,
+// setenv in tests application has no effect on intelocl*.dll. This will cause
+// this test to fail again and again. So, we need to disable this test on
+// windows.
+TEST(FrameworkTestType, Test_passBuildOptionByEnvTest) {
+    passBuildOptionByEnvTest();
+}
+#endif
+
 TEST(FrameworkTestType, DISABLED_Test_UniformWorkGroupTest)
 {
     UniformWorkGroupTest();
