943174326117906637d04b509ec8413df7c1a721
Prasad V Battini <prasad.v.battini@intel.com>
2021-01-28 13:30:31 -0500

    [MemManageTrans] CMPLRLLVM-1541 (Part 1): Basic framework for Memory Management Transformation.
    Change-Id: Ided79e455c31241dbca3faf90851bb58ad102e92
    Reviewed-on: https://git-amr-2.devtools.intel.com/gerrit/309060
    Reviewed-by: Battini, Prasad V <prasad.v.battini@intel.com>
    Tested-by: Battini, Prasad V <prasad.v.battini@intel.com>

---
+++ llvm/Intel_OptionalComponents/DTrans/lib/Transforms/MemManageTrans.cpp
@@ -0,0 +1,136 @@
+//===-------------- MemManageTrans.cpp - DTransMemManageTransPass ---------===//
+//
+// Copyright (C) 2021-2021 Intel Corporation. All rights reserved.
+//
+// The information and source code contained herein is the exclusive property
+// of Intel Corporation and may not be disclosed, examined or reproduced in
+// whole or in part without explicit written authorization from the company.
+//
+//===----------------------------------------------------------------------===//
+//
+// This file implements the DTrans Initial Memory Management Transformation.
+//
+// Detects memory pool allocator class by analyzing member functions of
+// the class like allocateBlock, destroyObject, commitAllocationObject etc and
+// increases size of block (i.e number of objects allocated each time) if
+// there are no legality issues.
+//
+//===----------------------------------------------------------------------===//
+
+#include "Intel_DTrans/Transforms/MemManageTrans.h"
+#include "Intel_DTrans/Analysis/DTrans.h"
+#include "Intel_DTrans/Analysis/DTransAnalysis.h"
+#include "Intel_DTrans/DTransCommon.h"
+#include "llvm/Analysis/Intel_WP.h"
+#include "llvm/IR/Module.h"
+#include "llvm/InitializePasses.h"
+#include "llvm/Pass.h"
+
+using namespace llvm;
+
+#define DTRANS_MEMMANAGETRANS "dtrans-memmanagetrans"
+
+namespace {
+
+class DTransMemManageTransWrapper : public ModulePass {
+private:
+  dtrans::MemManageTransPass Impl;
+
+public:
+  static char ID;
+
+  DTransMemManageTransWrapper() : ModulePass(ID) {
+    initializeDTransMemManageTransWrapperPass(*PassRegistry::getPassRegistry());
+  }
+
+  bool runOnModule(Module &M) override {
+    if (skipModule(M))
+      return false;
+    DTransAnalysisWrapper &DTAnalysisWrapper =
+        getAnalysis<DTransAnalysisWrapper>();
+    DTransAnalysisInfo &DTInfo = DTAnalysisWrapper.getDTransInfo(M);
+
+    bool Changed = Impl.runImpl(
+        M, DTInfo, getAnalysis<WholeProgramWrapperPass>().getResult());
+    return Changed;
+  }
+
+  void getAnalysisUsage(AnalysisUsage &AU) const override {
+    AU.addRequired<DTransAnalysisWrapper>();
+    AU.addRequired<WholeProgramWrapperPass>();
+    AU.addPreserved<DTransAnalysisWrapper>();
+    AU.addPreserved<WholeProgramWrapperPass>();
+  }
+};
+
+} // end anonymous namespace
+
+char DTransMemManageTransWrapper::ID = 0;
+INITIALIZE_PASS_BEGIN(DTransMemManageTransWrapper, "dtrans-memmanagetrans",
+                      "DTrans Memory Manage Trans", false, false)
+INITIALIZE_PASS_DEPENDENCY(DTransAnalysisWrapper)
+INITIALIZE_PASS_DEPENDENCY(WholeProgramWrapperPass)
+INITIALIZE_PASS_END(DTransMemManageTransWrapper, "dtrans-memmanagetrans",
+                    "DTrans Memory Manage Trans", false, false)
+
+ModulePass *llvm::createDTransMemManageTransWrapperPass() {
+  return new DTransMemManageTransWrapper();
+}
+
+namespace llvm {
+
+namespace dtrans {
+
+class MemManageTransImpl {
+
+public:
+  MemManageTransImpl(Module &M, const DataLayout &DL,
+                     DTransAnalysisInfo &DTInfo)
+      : M(M), DL(DL), DTInfo(DTInfo){};
+
+  bool run(void);
+
+private:
+  Module &M;
+  const DataLayout &DL;
+  DTransAnalysisInfo &DTInfo;
+
+  constexpr static int MaxNumCandidates = 1;
+};
+
+bool MemManageTransImpl::run(void) { return false; }
+
+bool MemManageTransPass::runImpl(Module &M, DTransAnalysisInfo &DTInfo,
+                                 WholeProgramInfo &WPInfo) {
+
+  auto TTIAVX2 = TargetTransformInfo::AdvancedOptLevel::AO_TargetHasIntelAVX2;
+  if (!WPInfo.isWholeProgramSafe() || !WPInfo.isAdvancedOptEnabled(TTIAVX2))
+    return false;
+
+  if (!DTInfo.useDTransAnalysis())
+    return false;
+
+  auto &DL = M.getDataLayout();
+
+  MemManageTransImpl MemManageTransI(M, DL, DTInfo);
+  return MemManageTransI.run();
+}
+
+PreservedAnalyses MemManageTransPass::run(Module &M,
+                                          ModuleAnalysisManager &AM) {
+  auto &DTransInfo = AM.getResult<DTransAnalysis>(M);
+  auto &WPInfo = AM.getResult<WholeProgramAnalysis>(M);
+
+  if (!runImpl(M, DTransInfo, WPInfo))
+    return PreservedAnalyses::all();
+
+  // TODO: We could add more preserved analyses.
+  PreservedAnalyses PA;
+  PA.preserve<WholeProgramAnalysis>();
+  PA.preserve<DTransAnalysis>();
+  return PA;
+}
+
+} // namespace dtrans
+
+} // namespace llvm
