eba323dcc6aa196923b8060e9dd322a037d5e7c6
alexanderfle <61590714+alexanderfle@users.noreply.github.com>
2021-01-29 12:01:01 +0300

    [SYCL] Fix get() method for non-opencl backends (#3070)
    Before this patch, if you use non-opencl backend, then get() method returns casted wrapper of the native handle and SYCL user can’t use it in his own code. As the user can't release the returned object, it used to lead to a memory leak.
    
    Since, get() method is applicable only for OpenCL backend, this patch adds an exception for cases when get() method is called for non-opencl backend.
    
    Signed-off-by: Alexander Flegontov <alexander.flegontov@intel.com>
    (cherry picked from commit 5e7cbefb4d258465f7d758bbfd94e2902d8e01e6)
    
    Change-Id: I93b259d5da594fd963de5911e53acc7cd944b535
    Reviewed-on: https://git-amr-2.devtools.intel.com/gerrit/309259
    Tested-by: xmain_checkin <lab_iclt@iclgerrit.sc.intel.com>
    Reviewed-by: Maslov, Sergey V <sergey.v.maslov@intel.com>
    Reviewed-by: Creech, Timothy M <timothy.m.creech@intel.com>

--- sycl/source/detail/scheduler/commands.cpp
+++ sycl/source/detail/scheduler/commands.cpp
@@ -2018,8 +2018,6 @@ cl_int ExecCGCommand::enqueueImp() {
     ExecInterop->MInteropTask->call(InteropHandler);
     Plugin.call<PiApiKind::piEnqueueEventsWait>(MQueue->getHandleRef(), 0,
                                                 nullptr, &Event);
-    Plugin.call<PiApiKind::piQueueRelease>(
-        reinterpret_cast<pi_queue>(MQueue->get()));
 
     return CL_SUCCESS;
   }
