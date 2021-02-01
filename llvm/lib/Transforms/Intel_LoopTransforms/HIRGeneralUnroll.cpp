3ab8c5c42dfe2231df71e5d65d75e18d0328094b
Chenyang Liu <chenyang.liu@intel.com>
2021-02-01 12:31:40 -0800

    [Loopopt][CMPLRLLVM-25888] Add option and default value to disable Loop Switching for Unrolled Loops if nesting level deeper than threshold.
    Change-Id: Icc3151cee81bd809f762fa8ef2b46cbd6f3847ef
    Reviewed-on: https://git-amr-2.devtools.intel.com/gerrit/309642
    Reviewed-by: chenyang liu <chenyang.liu@intel.com>
    Tested-by: chenyang liu <chenyang.liu@intel.com>
    Reviewed-by: Creech, Timothy M <timothy.m.creech@intel.com>

--- llvm/lib/Transforms/Intel_LoopTransforms/HIRGeneralUnroll.cpp
+++ llvm/lib/Transforms/Intel_LoopTransforms/HIRGeneralUnroll.cpp
@@ -142,6 +142,11 @@ static cl::opt<bool> DisableReplaceByFirstIteration(
     cl::Hidden,
     cl::desc("Disable replace by first iteration in HIR General Unroll"));
 
+// This sets the maximum loop depth for switch transformation of remainder loops.
+static cl::opt<unsigned> MaxSwitchLoopDepthThreshold(
+    "hir-swith-loop-depth-threshold", cl::init(3), cl::Hidden,
+    cl::desc("Maximum Loop Depth threshold that enables switch generation"));
+
 namespace {
 
 class HIRGeneralUnroll {
@@ -346,6 +351,14 @@ static bool isSwitchGenerationProfitable(HLLoop *RemainderLoop,
   }
 
   if (UnrollFactor > 8) {
+    return false;
+  }
+
+  // TODO: investigate further profitability heuristics. This check returns false
+  // for loops that are deeply nested, implying higher register pressure that
+  // may cause degradations.
+  unsigned LoopDepth = RemainderLoop->getNestingLevel();
+  if (LoopDepth > MaxSwitchLoopDepthThreshold) {
     return false;
   }
 
