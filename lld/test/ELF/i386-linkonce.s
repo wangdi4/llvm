d357cf945eb1a521fd693f8d70d748c1455c5cbd
Axel Y Rivera <axel.y.rivera@intel.com>
2021-02-17 18:58:15 -0800

    [LLD][ELF] CMPLRLLVM-26105: Support for .gnu.linkonce. section
    LLD currently doesn't handle the sections with the name ".gnu.linkonce.",
    and discard the sections named .gnu.linkonce.t.__x86.get_pc_thunk.bx
    and .gnu.linkonce.t.__i686.get_pc_thunk.bx. This could produce two
    problems: duplicated symbols error when "linkonce" is not handled, and
    reference to a discarded section for symbols __x86.get_pc_thunk.bx and
    __i686.get_pc_thunk.bx.
    
    This change set enables the ".gnu.linkonce." by collecting one section
    and discarding the repeated ones. During symbol resolution, LLD will
    prefer the definition from the file related to the section collected,
    and will discard the repeated ones.
    
    Change-Id: Ic02a46509456fbda14c9273d4c96019f9fad544c
    Reviewed-on: https://git-amr-2.devtools.intel.com/gerrit/312176
    Reviewed-by: Cox, Robert <robert.cox@intel.com>
    Reviewed-by: Chrulski, Christopher M <christopher.m.chrulski@intel.com>
    Tested-by: xmain_checkin <lab_iclt@iclgerrit.sc.intel.com>
    Tested-by: zperf_checkin_xmain <lab_iclz@iclgerrit.sc.intel.com>
    Reviewed-by: Sergey Kanaev <sergey.kanaev@intel.com>

--- lld/test/ELF/i386-linkonce.s
+++ lld/test/ELF/i386-linkonce.s
@@ -1,10 +1,22 @@
+// INTEL_CUSTOMIZATION
+// Now this test case should not produce a reference to a discardable section
+// since we have fixed the ".gnu.linkonce." issue.
+// END INTEL_CUSTOMIZATION
+
 // REQUIRES: x86
 // RUN: llvm-mc -filetype=obj -triple=i386-linux-gnu %s -o %t.o
 // RUN: llvm-mc -filetype=obj -triple=i386-linux-gnu %p/Inputs/i386-linkonce.s -o %t2.o
 // RUN: llvm-ar rcs %t2.a %t2.o
-// RUN: not ld.lld %t.o %t2.a -o /dev/null 2>&1 | FileCheck %s
+// INTEL_CUSTOMIZATION
+// BEGIN OLD CHECKS
+// RUN-: not ld.lld %t.o %t2.a -o /dev/null 2>&1 | FileCheck %s
 
-// CHECK: error: relocation refers to a symbol in a discarded section: __i686.get_pc_thunk.bx
+// CHECK-: error: relocation refers to a symbol in a discarded section: __i686.get_pc_thunk.bx
+// END OLD CHECKS
+
+// RUN: ld.lld %t.o %t2.a -o /dev/null 2>&1 | FileCheck --allow-empty %s
+// CHECK-NOT: error: relocation refers to a symbol in a discarded section: __i686.get_pc_thunk.bx
+// END INTEL_CUSTOMIZATION
 
     .globl _start
 _start:
