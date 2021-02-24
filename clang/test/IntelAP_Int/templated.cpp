5bc3df84fbe17f9e516264365b7027b56adb9313
Erich Keane <erich.keane@intel.com>
2021-02-24 06:49:50 -0800

    Fix SYCL AP-Int tests
    SYCL has made the default address space for things now '4' instead of
    not setting anything, so the AP-int tests that run under SYCL mode
    needed to be updated to have the address space.  This patch does so.
    
    Change-Id: I02421af8ed6cc58b380a62e54d625eeb053ad3ef
    Reviewed-on: https://git-amr-2.devtools.intel.com/gerrit/313049
    Reviewed-by: Keane, Erich <erich.keane@intel.com>
    Tested-by: Keane, Erich <erich.keane@intel.com>

--- clang/test/IntelAP_Int/templated.cpp
+++ clang/test/IntelAP_Int/templated.cpp
@@ -9,28 +9,38 @@
 // RUN: %clang -cc1 -O3 -disable-llvm-passes -triple spir64-unknown-linux-sycldevice -fsycl -fsycl-is-device %s -emit-llvm -o - | FileCheck %s -check-prefixes=CHECK,SYCL
 
 // CHECK: %a = alloca i3, align 1
+// SYCL: %a.ascast = addrspacecast i3* %a to i3 addrspace(4)*
 // CHECK: %b = alloca i3, align 1
+// SYCL: %b.ascast = addrspacecast i3* %b to i3 addrspace(4)*
 
 // HLS: %[[U0:[0-9]+]] = load i2, i2* %value{{([0-9]+)?}}, align 1
 // SYCL: %[[U0:[0-9]+]] = load i2, i2 addrspace(4)* %value{{([0-9]+)?}}, align 1
 // CHECK: %[[U1:[a-zA-Z0-9]+]] = zext i2 %[[U0]] to i3
-// CHECK: store i3 %[[U1]], i3* %a, align 1
+// HLS: store i3 %[[U1]], i3* %a, align 1
+// SYCL: store i3 %[[U1]], i3 addrspace(4)* %a.ascast, align 1
 
 // HLS: %[[U2:[0-9]+]] = load i2, i2* %value{{([0-9]+)?}}, align 1
 // SYCL: %[[U2:[0-9]+]] = load i2, i2 addrspace(4)* %value{{([0-9]+)?}}, align 1
 // CHECK: %[[U3:[a-zA-Z0-9]+]] = zext i2 %[[U2]] to i3
-// CHECK: store i3 %[[U3]], i3* %b, align 1
+// HLS: store i3 %[[U3]], i3* %b, align 1
+// SYCL: store i3 %[[U3]], i3 addrspace(4)* %b.ascast, align 1
 
-// CHECK: %[[U4:[0-9]+]] = load i3, i3* %a, align 1
-// CHECK: %[[U5:[0-9]+]] = load i3, i3* %b, align 1
+// HLS: %[[U4:[0-9]+]] = load i3, i3* %a, align 1
+// SYCL: %[[U4:[0-9]+]] = load i3, i3 addrspace(4)* %a.ascast, align 1
+// HLS: %[[U5:[0-9]+]] = load i3, i3* %b, align 1
+// SYCL: %[[U5:[0-9]+]] = load i3, i3 addrspace(4)* %b.ascast, align 1
 // CHECK: %[[U6:[a-zA-Z0-9]+]] = icmp eq i3 %[[U4]], %[[U5]]
 // CHECK: ret i1 %[[U6]]
 
 // CHECK: %[[ONEBIT:[a-zA-Z0-9_]+]] = alloca i1, align 1
-// CHECK: store i1 false, i1* %[[ONEBIT]], align 1
-// CHECK: %[[OB1:[0-9]+]] = load i1, i1* %[[ONEBIT]], align 1
+// SYCL: %[[ONEBIT_CAST:.+]] = addrspacecast i1* %[[ONEBIT]] to i1 addrspace(4)*
+// HLS: store i1 false, i1* %[[ONEBIT]], align 1
+// SYCL: store i1 false, i1 addrspace(4)* %[[ONEBIT_CAST]], align 1
+// HLS: %[[OB1:[0-9]+]] = load i1, i1* %[[ONEBIT]], align 1
+// SYCL: %[[OB1:[0-9]+]] = load i1, i1 addrspace(4)* %[[ONEBIT_CAST]], align 1
 // CHECK: %[[OB_CONV:[a-zA-Z0-9]+]] = zext i1 %[[OB1]] to i32
-// CHECK: store i32 %[[OB_CONV]], i32*
+// HLS: store i32 %[[OB_CONV]], i32*
+// SYCL: i32 %[[OB_CONV]], i32 addrspace(4)*
 
 #define AC_MAX(a, b) ((a) > (b) ? (a) : (b))
 #ifndef SYCL_EXTERNAL
