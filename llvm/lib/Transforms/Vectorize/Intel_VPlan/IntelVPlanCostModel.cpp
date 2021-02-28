12d5cee8337279892825776f9087a59d55315734
Sergey Zverev <sergey.i.zverev@intel.com>
2021-02-27 16:38:05 -0800

    [VPlan][CM] Remove assert for non vector input type.
    The change removes assert for that input type is not vector on math
    Instructions in Cost Model code.  Vector input types are expected to be
    supported.
    
    Change-Id: I801af449c3436560d8c1b27879b26169a03fbd63
    Reviewed-on: https://git-amr-2.devtools.intel.com/gerrit/313454
    Tested-by: xmain_checkin_sanity <lab_icls@iclgerrit.sc.intel.com>
    Reviewed-by: Elovikov, Andrei <andrei.elovikov@intel.com>
    Reviewed-by: Creech, Timothy M <timothy.m.creech@intel.com>

--- llvm/lib/Transforms/Vectorize/Intel_VPlan/IntelVPlanCostModel.cpp
+++ llvm/lib/Transforms/Vectorize/Intel_VPlan/IntelVPlanCostModel.cpp
@@ -660,8 +660,6 @@ unsigned VPlanCostModel::getCostForVF(
     if (!BaseDstTy || !BaseSrcTy)
       return UnknownCost;
 
-    assert(!BaseDstTy->isVectorTy() &&
-           "Vector base types are not yet implemented!");
     assert(!BaseDstTy->isAggregateType() && "Unexpected aggregate type!");
 
     Type *VecDstTy = getWidenedType(BaseDstTy, VF);
