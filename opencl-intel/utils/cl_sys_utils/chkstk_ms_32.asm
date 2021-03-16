347426458b87d9f24e7fe0f257fe7710340a3988
Yilong Guo <yilong.guo@intel.com>
2021-03-16 10:32:14 +0800

    Fix icx compilation error on registering _alloca
    icx (using clang FE) recognizes _alloca as a builtin and doesn't allow
    the function pointer of a builtin being passed around. Thus we change
    the name to _chkstk to bypass the FE check.
    
    Also fix the implementation of _chkstk for x86 win platform.
    
    Change-Id: Ib18c1e1d927614dc78921e29b025cfc213c56d25
    Reviewed-on: https://git-amr-2.devtools.intel.com/gerrit/315507
    Reviewed-by: Yilong Guo <yilong.guo@intel.com>
    Tested-by: Yilong Guo <yilong.guo@intel.com>

--- opencl-intel/utils/cl_sys_utils/chkstk_ms_32.asm
+++ opencl-intel/utils/cl_sys_utils/chkstk_ms_32.asm
@@ -1,4 +1,4 @@
-; This file is modified from lib/builtins/i386/chkstk.S in LLVM compiler-rt project.
+; This file is modified from lib/builtins/i386/chkstk2.S in LLVM compiler-rt project.
 
 ; Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 ; See https://llvm.org/LICENSE.txt for license information.
@@ -10,35 +10,32 @@
 
 ; Translated from AT&T assembly to intel assembly syntax.
 
-PUBLIC  ____chkstk_ms
-PUBLIC  __alloca
+PUBLIC ____chkstk
 
 _TEXT   SEGMENT
 
-____chkstk_ms PROC
+____chkstk PROC
     push    ecx
-    push    eax
     cmp     eax, 1000h
-    lea     ecx, [esp + 12]
-    jb      ____chkstk_ms_1
-____chkstk_ms_2:
+    lea     ecx, [esp + 8]  ; esp before calling this routine -> ecx
+    jb      ____chkstk_1
+____chkstk_2:
     sub     ecx, 1000h
     test    [ecx], ecx
     sub     eax, 1000h
     cmp     eax, 1000h
-    ja      ____chkstk_ms_2
-____chkstk_ms_1:
+    ja      ____chkstk_2
+____chkstk_1:
     sub     ecx, eax
     test    [ecx], ecx
-    pop     eax
-    pop     ecx
-    ret
-____chkstk_ms ENDP
 
-__alloca PROC
-    call    ____chkstk_ms
+    lea     eax, [esp + 4]  ; load pointer to the return address into eax
+    mov     esp, ecx        ; install the new top of stack pointer into esp
+    mov     ecx, [eax - 4]  ; restore ecx
+    push    [eax]           ; push return address onto the stack
+    sub     eax, esp        ; restore the original value in eax
     ret
-__alloca ENDP
+____chkstk ENDP
 
 _TEXT ENDS
 END
