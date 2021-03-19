60be31f42a4545ad358c2fbc27e08f1919bf855b
Anton Zabaznov <anton.zabaznov@intel.com>
2021-03-19 15:51:25 +0300

    // INTEL_FEATURE_ESIMD_EMBARGO [DPCPP][ESIMD][PVC] Add support for esimd_nbarrier_wait, esimd_nbarrier_signal and esimd_nbarrier_init // end INTEL_FEATURE_ESIMD_EMBARGO
    Change-Id: Iae81475991b571583e3b4f768e764b3ac8acbcda
    Reviewed-on: https://git-amr-2.devtools.intel.com/gerrit/316355
    Tested-by: alloy <lab_icla@iclgerrit.sc.intel.com>
    Reviewed-by: Anton Zabaznov <anton.zabaznov@intel.com>
    Reviewed-by: Zverev, Sergey I <sergey.i.zverev@intel.com>

---
+++ sycl/test/esimd/intel_nbarriers.cpp
@@ -0,0 +1,22 @@
+// INTEL_FEATURE_ESIMD_EMBARGO
+// REQUIRES: intel_feature_esimd_embargo
+// RUN: %clangxx -fsycl -c -fsycl-device-only -Xclang -emit-llvm %s -o %t
+
+#include <CL/sycl.hpp>
+#include <CL/sycl/INTEL/esimd.hpp>
+
+using namespace sycl::INTEL::gpu;
+
+template <typename name, typename Func>
+__attribute__((sycl_kernel)) void kernel(Func kernelFunc) {
+  kernelFunc();
+}
+
+void caller(int x) {
+  kernel<class kernel_esimd>([=]() SYCL_ESIMD_KERNEL {
+   esimd_nbarrier_init<7>();
+   esimd_nbarrier_wait(2);
+   esimd_nbarrier_signal(0, 0, 4, 4);
+  });
+}
+// end INTEL_FEATURE_ESIMD_EMBARGO
