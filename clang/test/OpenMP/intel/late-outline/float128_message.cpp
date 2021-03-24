10c1ddc7bd1ec98799794a20cc54305f840d84d5
Jennifer Yu <jennifer.yu@intel.com>
2021-03-23 18:05:10 -0700

    cmplrllvm-27012:  Fix test. We now only emit one error instaed 3. Community fix bug: https://reviews.llvm.org/D95928
    Change-Id: Ia9928e3290e0b23a784aa3fb4d740ede19418a89
    Reviewed-on: https://git-amr-2.devtools.intel.com/gerrit/317262
    Reviewed-by: Artem Gindinson <artem.gindinson@intel.com>
    Reviewed-by: Yu, Jennifer <jennifer.yu@intel.com>
    Tested-by: Yu, Jennifer <jennifer.yu@intel.com>

--- clang/test/OpenMP/intel/late-outline/float128_message.cpp
+++ clang/test/OpenMP/intel/late-outline/float128_message.cpp
@@ -9,19 +9,19 @@
 // RUN: -fopenmp-is-device -fopenmp-host-ir-file-path %t-host.bc -fsyntax-only
 
 #define floatDef __float128
-// expected-note@+1 3{{'f' defined here}}
+// expected-note@+1 {{'f' defined here}}
 floatDef foo(floatDef f) {
 #pragma omp target map(f)
-  // expected-error@+1 3{{'f' requires 128 bit size '__float128' type support, but device 'spir64' does not support it}}
+  // expected-error@+1 {{'f' requires 128 bit size '__float128' type support, but device 'spir64' does not support it}}
   f = 1.0000;
   return f;
 }
 
-// expected-note@+1 3{{'d' defined here}}
+// expected-note@+1 {{'d' defined here}}
 long double zoo(long double d)
 {
 #pragma omp target map(d)
-  // expected-error@+1 3{{'d' requires 128 bit size 'long double' type support, but device 'spir64' does not support it}}
+  // expected-error@+1 {{'d' requires 128 bit size 'long double' type support, but device 'spir64' does not support it}}
   d = 1.0;
   return d;
 }
