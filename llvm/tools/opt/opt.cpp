2d26000ab25f6fca846c4786250785f73273d4b3
Artem Gindinson <artem.gindinson@intel.com>
2021-03-26 21:19:52 +0300

    Fix build after e3ce085580988876704cd4f4f26eed8d284279ee
    Change-Id: I6987d1af4fba9b6ef7974f3fe43f7594e2d42182
    Signed-off-by: Artem Gindinson <artem.gindinson@intel.com>
    Reviewed-on: https://git-amr-2.devtools.intel.com/gerrit/318023

--- llvm/tools/opt/opt.cpp
+++ llvm/tools/opt/opt.cpp
@@ -552,8 +552,8 @@ static bool shouldPinPassToLegacyPM(StringRef Pass) {
       "expand-reductions",    "indirectbr-expand",
       "generic-to-nvvm",      "expandmemcmp",
       "loop-reduce",          "lower-amx-type",
-      "lower-amx-intrinsics", "polyhedral-info"};
-      "polyhedral-info",      "replace-with-veclib"};
+      "lower-amx-intrinsics", "polyhedral-info",
+      "replace-with-veclib"};
   for (const auto &P : PassNamePrefix)
     if (Pass.startswith(P))
       return true;
