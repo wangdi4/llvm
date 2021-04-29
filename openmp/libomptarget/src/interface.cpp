3d5dd0b24164fc270590eb3e5ab7671e73b63ea1
Arvind Sudarsanam <arvind.sudarsanam@intel.com>
2021-04-29 14:12:15 -0700

    Cherry-picking 59b68490122ae6ef92b1ebe45e8a5f2f7d88a401 from llorg to avoid debug build fail
    Change-Id: Icfa5fc62b48e8fac334693f0c098ebc21794661d
    Reviewed-on: https://git-amr-2.devtools.intel.com/gerrit/324730
    Reviewed-by: Sudarsanam, Arvind <arvind.sudarsanam@intel.com>
    Tested-by: Sudarsanam, Arvind <arvind.sudarsanam@intel.com>

--- openmp/libomptarget/src/interface.cpp
+++ openmp/libomptarget/src/interface.cpp
@@ -878,12 +878,10 @@ EXTERN void __tgt_add_build_options(
 }
 #endif // INTEL_COLLAB
 EXTERN void __tgt_set_info_flag(uint32_t NewInfoLevel) {
+  std::atomic<uint32_t> &InfoLevel = getInfoLevelInternal();
   InfoLevel.store(NewInfoLevel);
   for (auto &R : PM->RTLs.AllRTLs) {
     if (R.set_info_flag)
       R.set_info_flag(NewInfoLevel);
   }
 }
-
-// Libomptarget's InfoLevel storage.
-std::atomic<uint32_t> InfoLevel;
