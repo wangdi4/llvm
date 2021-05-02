ce5d72fc03481f5196b2e036c5f59543d209bbbf
Chang-Sun Lin Jr <chang-sun.lin.jr@intel.com>
2021-05-01 21:57:58 -0700

    27987: Set a recursion limit (currently 32) on same-phi-operand analysis in SCEV. Fixes stack overflow in very large functions.
    Change-Id: I90e220baef16f5e121b9acb0ea6e09d19d4ad1c5
    Reviewed-on: https://git-amr-2.devtools.intel.com/gerrit/325460
    Reviewed-by: Chawla, Pankaj <pankaj.chawla@intel.com>
    Tested-by: xmain_checkin <lab_iclt@iclgerrit.sc.intel.com>
    Tested-by: zperf_checkin_xmain <lab_iclz@iclgerrit.sc.intel.com>
    Reviewed-by: Zverev, Sergey I <sergey.i.zverev@intel.com>

--- llvm/lib/Analysis/ScalarEvolution.cpp
+++ llvm/lib/Analysis/ScalarEvolution.cpp
@@ -5834,6 +5834,9 @@ const SCEV *ScalarEvolution::createNodeForIdenticalOperandsPHI(PHINode *PN) {
     }
   }
 
+  if (PhiSimplificationCandidates.size() > MaxSCEVCompareDepth)
+    return getUnknown(PN);
+
   // Bail out if any of the predecessors are unreachable from entry block.
   for (auto *Pred : predecessors(PN->getParent()))
     if (!DT.isReachableFromEntry(Pred))
