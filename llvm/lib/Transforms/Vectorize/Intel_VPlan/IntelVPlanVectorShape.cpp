eb15b29958e3021fd8b9388bc7131733a611e9a0
Pushkar V Ratnalikar <pushkar.v.ratnalikar@intel.com>
2021-05-10 11:32:10 -0700

    [VPlan][SOA] Teach 'joinVectorShapes' to correctly handle and return value for SOA vector-shapes.
    This change handles input combinations of either SOASeq, SOAStr and
    SOARnd shapes to the joinVectorShapes functions. Conservatively, we
    return SOARnd shape if both the inputs are not SOASeq.
    
    Change-Id: I780a0ec8a4324d8d5f46eabd2c0598f485bf3fb7
    Reviewed-on: https://git-amr-2.devtools.intel.com/gerrit/326712
    Reviewed-by: Pushkar Ratnalikar <pushkar.v.ratnalikar@intel.com>
    Tested-by: Pushkar Ratnalikar <pushkar.v.ratnalikar@intel.com>
    Reviewed-by: Zverev, Sergey I <sergey.i.zverev@intel.com>

--- llvm/lib/Transforms/Vectorize/Intel_VPlan/IntelVPlanVectorShape.cpp
+++ llvm/lib/Transforms/Vectorize/Intel_VPlan/IntelVPlanVectorShape.cpp
@@ -33,17 +33,35 @@ VPVectorShape VPVectorShape::joinShapes(VPVectorShape Shape1,
     if (Shape1.isUniform())
       return {VPVectorShape::Uni, Shape1.getStride()};
 
+    if (Shape1.isSOAUnitStride())
+      return {VPVectorShape::SOASeq, Shape1.getStride()};
     if (Shape1.isUnitStride())
       return {VPVectorShape::Seq, Shape1.getStride()};
     else
       return {VPVectorShape::Str, Shape1.getStride()};
   }
 
+  // We want to return SOARand for the following inputs
+  // Shape1 = SOAStr, Shape2 = SOASeq
+  // Shape1 = SOASeq, Shape2 = SOAStr
+  // Shape1 = SOAStr, Shape2 = SOARnd
+  // ....
+  // i.e., if either of Shape1 or Shape2 is not SOASeq,
+  // we want to return SOARnd.
+  // The case where Shape1 is SOASeq and Shape2 is SOASeq is handled in the
+  // previous block.
+  if (Shape1.isSOAShape() && Shape2.isSOAShape())
+    return {VPShapeDescriptor::SOARnd};
+
   return {VPShapeDescriptor::Rnd};
 }
 
 bool VPVectorShape::shapesHaveSameStride(VPVectorShape Shape1,
                                          VPVectorShape Shape2) {
-  return Shape1.hasKnownStride() && Shape2.hasKnownStride() &&
+  // Shapes have to be of the same class, i.e., SOA or non-SOA.
+  // It is possible that we encounter SOA UnitStride and UnitStride
+  // with the same stride. We return 'false' in such cases.
+  return Shape1.isSOAShape() == Shape2.isSOAShape() &&
+         Shape1.hasKnownStride() && Shape2.hasKnownStride() &&
          Shape1.getStrideVal() == Shape2.getStrideVal();
 }
