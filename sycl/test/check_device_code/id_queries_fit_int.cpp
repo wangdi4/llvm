ced66ac09837fa7ef224e06f8cfbf80b0726d1a2
Sergey Kanaev <sergey.kanaev@intel.com>
2021-05-13 16:49:40 +0300

    [SYCL] Fix 'ID queries fit in int' test
    Change-Id: I08db96e401d2d9595abd7e7b2b5b9f1b7c31bfdb
    Signed-off-by: Sergey Kanaev <sergey.kanaev@intel.com>
    Reviewed-on: https://git-amr-2.devtools.intel.com/gerrit/329374
    Tested-by: alloy <lab_icla@iclgerrit.sc.intel.com>
    Reviewed-by: Romanov, Vlad <vlad.romanov@intel.com>
    Reviewed-by: Kurkov, Vasiliy A <vasiliy.a.kurkov@intel.com>

--- sycl/test/check_device_code/id_queries_fit_int.cpp
+++ sycl/test/check_device_code/id_queries_fit_int.cpp
@@ -1,58 +1,45 @@
-// RUN: %clangxx -fsycl -fsycl-id-queries-fit-in-int -O0 -c -S -emit-llvm -o %t.ll %s
+// RUN: %clangxx -fsycl-device-only -fsycl-id-queries-fit-in-int -fno-sycl-early-optimizations -S -o %t.ll %s
 // RUN: FileCheck %s --input-file %t.ll
 
 #include <CL/sycl.hpp>
 
 using namespace sycl;
 
-// CHECK-ESIMD-NOT: call void @llvm.assume(i1 {{.*}})
-
-// CHECK: define {{.*}}spir_kernel void @"_ZTSZZ4mainENK3$_0clERN2cl4sycl7handlerEE15kernel_function"{{.*}}
-int main() {
-  sycl::queue Queue;
-  Queue.submit([&](sycl::handler &CGH) {
-    CGH.single_task<class kernel_function>([=]() {
-      item<1, true> TestItem =
-          detail::Builder::createItem<1, true>({3}, {2}, {1});
-      // CHECK: call void @llvm.assume(i1 {{.*}})
-      int Id = TestItem.get_id(0);
-      // CHECK: call void @llvm.assume(i1 {{.*}})
-      int Range = TestItem.get_range(0);
-      // CHECK: call void @llvm.assume(i1 {{.*}})
-      int LinearId = TestItem.get_linear_id();
-
-      cl::sycl::nd_item<1> TestNDItem = detail::Builder::createNDItem<1>(
-          detail::Builder::createItem<1, false>({4}, {2}),
-          detail::Builder::createItem<1, false>({2}, {0}),
-          detail::Builder::createGroup<1>({4}, {2}, {1}));
-
-      // CHECK: call void @llvm.assume(i1 {{.*}})
-      int GlobalId = TestNDItem.get_global_id(0);
-      // CHECK: call void @llvm.assume(i1 {{.*}})
-      int GlobalLinearId = TestNDItem.get_global_linear_id();
-      // CHECK: call void @llvm.assume(i1 {{.*}})
-      int LocalId = TestNDItem.get_local_id(0);
-      // CHECK: call void @llvm.assume(i1 {{.*}})
-      int LocalLinearId = TestNDItem.get_local_linear_id();
-      // CHECK: call void @llvm.assume(i1 {{.*}})
-      int GroupRange = TestNDItem.get_group_range(0);
-      // CHECK: call void @llvm.assume(i1 {{.*}})
-      int GroupId = TestNDItem.get_group(0);
-      // CHECK: call void @llvm.assume(i1 {{.*}})
-      int GroupLinearId = TestNDItem.get_group_linear_id();
-      // CHECK: call void @llvm.assume(i1 {{.*}})
-      int GlobalRange = TestNDItem.get_global_range(0);
-      // CHECK: call void @llvm.assume(i1 {{.*}})
-      int LocalRange = TestNDItem.get_local_range(0);
-
-      int GlobalIdConverted = TestNDItem.get_global_id();
-      // CHECK: call void @llvm.assume(i1 {{.*}})
-      int LocalIdConverted = TestNDItem.get_local_id();
-      // CHECK: call void @llvm.assume(i1 {{.*}})
-      int OffsetConferted = TestNDItem.get_offset();
-      // CHECK: call void @llvm.assume(i1 {{.*}})
-    });
-  });
-  return 0;
+// CHECK: define {{.*}}dso_local spir_func void @{{.*}}testItem{{.*}}(%"class.{{.*}}item"*{{.*}}%{{.*}})
+SYCL_EXTERNAL void testItem(item<1> TestItem) {
+  // CHECK: call void @llvm.assume(i1 {{.*}})
+  int Id = TestItem.get_id(0);
+  // CHECK: call void @llvm.assume(i1 {{.*}})
+  int Range = TestItem.get_range(0);
+  // CHECK: call void @llvm.assume(i1 {{.*}})
+  int LinearId = TestItem.get_linear_id();
 }
-// CHECK: }
+
+// CHECK: define {{.*}}dso_local spir_func void @{{.*}}testNDItem{{.*}}(%"class.{{.*}}nd_item"*{{.*}}%{{.*}})
+SYCL_EXTERNAL void testNDItem(nd_item<1> TestNDItem) {
+  // CHECK: call void @llvm.assume(i1 {{.*}})
+  int GlobalId = TestNDItem.get_global_id(0);
+  // CHECK: call void @llvm.assume(i1 {{.*}})
+  int GlobalLinearId = TestNDItem.get_global_linear_id();
+  // CHECK: call void @llvm.assume(i1 {{.*}})
+  int LocalId = TestNDItem.get_local_id(0);
+  // CHECK: call void @llvm.assume(i1 {{.*}})
+  int LocalLinearId = TestNDItem.get_local_linear_id();
+  // CHECK: call void @llvm.assume(i1 {{.*}})
+  int GroupRange = TestNDItem.get_group_range(0);
+  // CHECK: call void @llvm.assume(i1 {{.*}})
+  int GroupId = TestNDItem.get_group(0);
+  // CHECK: call void @llvm.assume(i1 {{.*}})
+  int GroupLinearId = TestNDItem.get_group_linear_id();
+  // CHECK: call void @llvm.assume(i1 {{.*}})
+  int GlobalRange = TestNDItem.get_global_range(0);
+  // CHECK: call void @llvm.assume(i1 {{.*}})
+  int LocalRange = TestNDItem.get_local_range(0);
+
+  // CHECK: call void @llvm.assume(i1 {{.*}})
+  int GlobalIdConverted = TestNDItem.get_global_id();
+  // CHECK: call void @llvm.assume(i1 {{.*}})
+  int LocalIdConverted = TestNDItem.get_local_id();
+  // CHECK: call void @llvm.assume(i1 {{.*}})
+  int OffsetConferted = TestNDItem.get_offset();
+}
