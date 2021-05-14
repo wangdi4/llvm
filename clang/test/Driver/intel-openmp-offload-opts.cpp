e3e0b62409797702247cd9fd0ed00aeaf35c2dd7
Michael D Toguchi <michael.d.toguchi@intel.com>
2021-05-13 18:59:52 -0700

    [Driver] Fix optimization level settings with OpenMP offload Windows
    When using Windows, the capability to set the optimization level across
    all compilations was not occuring on Windows.  This is due to a disconnect
    between the /O usage and -O usage.
    Addresses:  CMPLRLLVM-28212
    
    Change-Id: I50e1e6693ae23b913f748dcdd28fe8875371f7d6
    Reviewed-on: https://git-amr-2.devtools.intel.com/gerrit/327308
    Reviewed-by: Chilamantula, Harini <harini.chilamantula@intel.com>
    Tested-by: alloy <lab_icla@iclgerrit.sc.intel.com>
    Reviewed-on: https://git-amr-2.devtools.intel.com/gerrit/327554
    Reviewed-by: Wenju He <wenju.he@intel.com>

---
+++ clang/test/Driver/intel-openmp-offload-opts.cpp
@@ -0,0 +1,28 @@
+/// Covers optimization level setting for offload
+// RUN: %clangxx --intel -fiopenmp -fopenmp-targets=spir64 -target x86_64-unknown-linux-gnu -### %s 2>&1 \
+// RUN:  | FileCheck -check-prefix DEFAULT_OPTS %s
+// RUN: %clang_cl --intel -Qiopenmp -Qopenmp-targets=spir64 --target=x86_64-pc-windows-msvc -### %s 2>&1 \
+// RUN:  | FileCheck -check-prefix DEFAULT_OPTS %s
+// RUN: %clangxx --intel -O2 -fiopenmp -fopenmp-targets=spir64 -target x86_64-unknown-linux-gnu -### %s 2>&1 \
+// RUN:  | FileCheck -check-prefix DEFAULT_OPTS %s
+// RUN: %clang_cl --intel -O2 -Qiopenmp -Qopenmp-targets=spir64 --target=x86_64-pc-windows-msvc -### %s 2>&1 \
+// RUN:  | FileCheck -check-prefix DEFAULT_OPTS %s
+// DEFAULT_OPTS: clang{{.*}} "-triple" "x86_64-{{.*}}"{{.*}} "-O2"
+// DEFAULT_OPTS: clang{{.*}} "-triple" "spir64"{{.*}} "-O2"
+// DEFAULT_OPTS: sycl-post-link{{.*}} "-O2"
+
+// RUN: %clangxx --intel -O3 -fiopenmp -fopenmp-targets=spir64 -target x86_64-unknown-linux-gnu -### %s 2>&1 \
+// RUN:  | FileCheck -check-prefix O3_OPTS %s
+// RUN: %clang_cl --intel -O3 -Qiopenmp -Qopenmp-targets=spir64 --target=x86_64-pc-windows-msvc -### %s 2>&1 \
+// RUN:  | FileCheck -check-prefix O3_OPTS %s
+// O3_OPTS: clang{{.*}} "-triple" "x86_64-{{.*}}"{{.*}} "-O3"
+// O3_OPTS: clang{{.*}} "-triple" "spir64"{{.*}} "-O3"
+// O3_OPTS: sycl-post-link{{.*}} "-O3"
+
+// RUN: %clangxx --intel -O0 -fiopenmp -fopenmp-targets=spir64 -target x86_64-unknown-linux-gnu -### %s 2>&1 \
+// RUN:  | FileCheck -check-prefix O0_OPTS %s
+// RUN: %clang_cl --intel -Od -Qiopenmp -Qopenmp-targets=spir64 --target=x86_64-pc-windows-msvc -### %s 2>&1 \
+// RUN:  | FileCheck -check-prefix O0_OPTS %s
+// O0_OPTS: clang{{.*}} "-triple" "x86_64-{{.*}}"{{.*}} "-O0"
+// O0_OPTS: clang{{.*}} "-triple" "spir64"{{.*}} "-O0"
+// O0_OPTS: sycl-post-link{{.*}} "-O0"
