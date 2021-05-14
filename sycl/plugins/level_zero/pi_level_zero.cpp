b404638d18b628ca67707c43e78d45b37088f16e
smaslov-intel <48694368+smaslov-intel@users.noreply.github.com>
2021-05-14 10:31:45 -0700

    [SYCL] Meet queue capabilities on how large pattern fill it can process. (#3706)
    Signed-off-by: Sergey V Maslov <sergey.v.maslov@intel.com>
    Change-Id: I453da4061513098741bf001c91f7ec4a851fe807
    Reviewed-on: https://git-amr-2.devtools.intel.com/gerrit/327519
    Reviewed-by: Zverev, Sergey I <sergey.i.zverev@intel.com>

--- sycl/plugins/level_zero/pi_level_zero.cpp
+++ sycl/plugins/level_zero/pi_level_zero.cpp
@@ -530,14 +530,14 @@ pi_result _pi_device::initialize() {
   if (numQueueGroups == 0) {
     return PI_ERROR_UNKNOWN;
   }
-  std::vector<ze_command_queue_group_properties_t> queueProperties(
+  std::vector<ze_command_queue_group_properties_t> QueueProperties(
       numQueueGroups);
   ZE_CALL(zeDeviceGetCommandQueueGroupProperties,
-          (ZeDevice, &numQueueGroups, queueProperties.data()));
+          (ZeDevice, &numQueueGroups, QueueProperties.data()));
 
   int ComputeGroupIndex = -1;
   for (uint32_t i = 0; i < numQueueGroups; i++) {
-    if (queueProperties[i].flags &
+    if (QueueProperties[i].flags &
         ZE_COMMAND_QUEUE_GROUP_PROPERTY_FLAG_COMPUTE) {
       ComputeGroupIndex = i;
       break;
@@ -547,16 +547,17 @@ pi_result _pi_device::initialize() {
   if (ComputeGroupIndex < 0) {
     return PI_ERROR_UNKNOWN;
   }
-  this->ZeComputeQueueGroupIndex = ComputeGroupIndex;
+  ZeComputeQueueGroupIndex = ComputeGroupIndex;
+  ZeComputeQueueGroupProperties = QueueProperties[ComputeGroupIndex];
 
   int CopyGroupIndex = -1;
   const char *CopyEngine = std::getenv("SYCL_PI_LEVEL_ZERO_USE_COPY_ENGINE");
   bool UseCopyEngine = (!CopyEngine || (std::stoi(CopyEngine) != 0));
   if (UseCopyEngine) {
     for (uint32_t i = 0; i < numQueueGroups; i++) {
-      if (((queueProperties[i].flags &
+      if (((QueueProperties[i].flags &
             ZE_COMMAND_QUEUE_GROUP_PROPERTY_FLAG_COMPUTE) == 0) &&
-          (queueProperties[i].flags &
+          (QueueProperties[i].flags &
            ZE_COMMAND_QUEUE_GROUP_PROPERTY_FLAG_COPY)) {
         CopyGroupIndex = i;
         break;
@@ -568,7 +569,10 @@ pi_result _pi_device::initialize() {
     else
       zePrint("NOTE: blitter/copy engine is available\n");
   }
-  this->ZeCopyQueueGroupIndex = CopyGroupIndex;
+  ZeCopyQueueGroupIndex = CopyGroupIndex;
+  if (CopyGroupIndex >= 0) {
+    ZeCopyQueueGroupProperties = QueueProperties[CopyGroupIndex];
+  }
 
   // Cache device properties
   ZeDeviceProperties = {};
@@ -4889,10 +4893,31 @@ enqueueMemFillHelper(pi_command_type CommandType, pi_queue Queue, void *Ptr,
   // Get a new command list to be used on this call
   ze_command_list_handle_t ZeCommandList = nullptr;
   ze_fence_handle_t ZeFence = nullptr;
+
+  bool PreferCopyEngine = false;
+  size_t MaxPatternSize =
+      Queue->Device->ZeComputeQueueGroupProperties.maxMemoryFillPatternSize;
+
   // Performance analysis on a simple SYCL data "fill" test shows copy engine
   // is faster than compute engine for such operations.
+  //
+  // Make sure that pattern size matches the capability of the copy queue.
+  //
+  if (Queue->Device->hasCopyEngine() &&
+      PatternSize <=
+          Queue->Device->ZeCopyQueueGroupProperties.maxMemoryFillPatternSize) {
+    MaxPatternSize =
+        Queue->Device->ZeCopyQueueGroupProperties.maxMemoryFillPatternSize;
+    PreferCopyEngine = true;
+  }
+  // Pattern size must fit the queue.
+  PI_ASSERT(PatternSize <= MaxPatternSize, PI_INVALID_VALUE);
+  // Pattern size must be a power of two.
+  PI_ASSERT((PatternSize > 0) && ((PatternSize & (PatternSize - 1)) == 0),
+            PI_INVALID_VALUE);
+
   if (auto Res = Queue->Context->getAvailableCommandList(
-          Queue, &ZeCommandList, &ZeFence, true /* PreferCopyEngine */))
+          Queue, &ZeCommandList, &ZeFence, PreferCopyEngine))
     return Res;
 
   ze_event_handle_t ZeEvent = nullptr;
@@ -4909,9 +4934,6 @@ enqueueMemFillHelper(pi_command_type CommandType, pi_queue Queue, void *Ptr,
     ZE_CALL(zeCommandListAppendWaitOnEvents,
             (ZeCommandList, WaitList.Length, WaitList.ZeEventList));
   }
-  // Pattern size must be a power of two
-  PI_ASSERT((PatternSize > 0) && ((PatternSize & (PatternSize - 1)) == 0),
-            PI_INVALID_VALUE);
 
   ZE_CALL(
       zeCommandListAppendMemoryFill,
