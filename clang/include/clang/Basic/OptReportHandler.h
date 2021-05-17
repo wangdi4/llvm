f4b87952cc93942481f6bc3f9b1b833869cdfcc1
Elizabeth Andrews <elizabeth.andrews@intel.com>
2021-05-16 21:13:49 -0700

    [SYCL] Improve output emitted in opt-report (#3730)
    This patch attempts to make the output generated in opt-report
    more user-friendly by adding a description for OpenCL kernel
    arguments. It also removes compiler generated names such as
    '__wrapper_class'.
    
    Change-Id: I7498370190a386f3a1095143289b154f3c82bb3e
    Reviewed-on: https://git-amr-2.devtools.intel.com/gerrit/327638
    Reviewed-by: Andrews, Elizabeth <elizabeth.andrews@intel.com>
    Tested-by: Andrews, Elizabeth <elizabeth.andrews@intel.com>

--- clang/include/clang/Basic/OptReportHandler.h
+++ clang/include/clang/Basic/OptReportHandler.h
@@ -29,14 +29,22 @@ class FunctionDecl;
 class OptReportHandler { // INTEL
 private:
   struct SyclOptReportInfo { // INTEL
-    std::string KernelArgName;
+    std::string KernelArgDescName; // Kernel argument name itself, or the name
+                                   // of the parent class if the kernel argument
+                                   // is a decomposed member.
     std::string KernelArgType;
     SourceLocation KernelArgLoc;
+    unsigned KernelArgSize;
+    std::string KernelArgDesc;
+    std::string KernelArgDecomposedField;
 
-    SyclOptReportInfo(std::string ArgName, std::string ArgType, // INTEL
-                      SourceLocation ArgLoc)
-        : KernelArgName(std::move(ArgName)), KernelArgType(std::move(ArgType)),
-          KernelArgLoc(ArgLoc) {}
+    SyclOptReportInfo(std::string ArgDescName, std::string ArgType, // INTEL
+                      SourceLocation ArgLoc, unsigned ArgSize,
+                      std::string ArgDesc, std::string ArgDecomposedField)
+        : KernelArgDescName(std::move(ArgDescName)),
+          KernelArgType(std::move(ArgType)), KernelArgLoc(ArgLoc),
+          KernelArgSize(ArgSize), KernelArgDesc(std::move(ArgDesc)),
+          KernelArgDecomposedField(std::move(ArgDecomposedField)) {}
   };
   llvm::DenseMap<const FunctionDecl *, SmallVector<SyclOptReportInfo>>
       SyclMap; // INTEL
@@ -57,9 +65,12 @@ private:
       OpenMPMap;
 
 public:
-  void AddKernelArgs(const FunctionDecl *FD, std::string ArgName,
-                     std::string ArgType, SourceLocation ArgLoc) {
-    SyclMap[FD].emplace_back(ArgName, ArgType, ArgLoc);
+  void AddKernelArgs(const FunctionDecl *FD, StringRef ArgDescName,
+                     StringRef ArgType, SourceLocation ArgLoc, unsigned ArgSize,
+                     StringRef ArgDesc, StringRef ArgDecomposedField) {
+    SyclMap[FD].emplace_back(ArgDescName.data(), ArgType.data(), ArgLoc,
+                             ArgSize, ArgDesc.data(),
+                             ArgDecomposedField.data());
   }
   SmallVector<SyclOptReportInfo> &GetSyclInfo(const FunctionDecl *FD) {
     auto It = SyclMap.find(FD);
