05a0d8bf7c885bed9ea5e8d4a91c4deda342d214
Xiang1 Zhang <xiang1.zhang@intel.com>
2021-05-28 08:28:07 +0800

    [bugfix] Remove redundant inlcude file LostDebugLocObserver.h
    Change-Id: Ie621678f67bd15304b14783442409d95ea41a5a9
    Reviewed-on: https://git-amr-2.devtools.intel.com/gerrit/329588
    Tested-by: xiangzhang <xiang1.zhang@intel.com>
    Reviewed-by: xiangzhang <xiang1.zhang@intel.com>

--- llvm/unittests/CodeGen/GlobalISel/GISelMITest.h
+++ llvm/unittests/CodeGen/GlobalISel/GISelMITest.h
@@ -11,7 +11,6 @@
 #include "llvm/CodeGen/GlobalISel/GISelChangeObserver.h"
 #include "llvm/CodeGen/GlobalISel/LegalizerHelper.h"
 #include "llvm/CodeGen/GlobalISel/LegalizerInfo.h"
-#include "llvm/CodeGen/GlobalISel/LostDebugLocObserver.h"
 #include "llvm/CodeGen/GlobalISel/MIPatternMatch.h"
 #include "llvm/CodeGen/GlobalISel/MachineIRBuilder.h"
 #include "llvm/CodeGen/GlobalISel/Utils.h"
