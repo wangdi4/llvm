1154a784cc76b388d5a21b6898c88c8251cb426e
Vyacheslav Zakharin <vyacheslav.p.zakharin@intel.com>
2021-07-09 12:32:20 -0700

    CMPLRLIBS-33437: build and link libdevice cstring for OpenMP offload.
    Change-Id: Ic573eeb2b01918450af47956af594c6c1145c304
    Reviewed-on: https://git-amr-2.devtools.intel.com/gerrit/333689
    Reviewed-by: Gaba, Abhinav <abhinav.gaba@intel.com>
    Tested-by: Gaba, Abhinav <abhinav.gaba@intel.com>
    (cherry picked from commit 43939e9b7de8c0429b6ab5744dcbeca3e736b013)
    Reviewed-on: https://git-amr-2.devtools.intel.com/gerrit/333714

--- libdevice/fallback-cstring.cpp
+++ libdevice/fallback-cstring.cpp
@@ -9,7 +9,17 @@
 #include "wrapper.h"
 
 #ifdef __SPIR__
+#if INTEL_COLLAB
+#if OMP_LIBDEVICE
+#pragma omp declare target
+#endif  // OMP_LIBDEVICE
+#endif  // INTEL_COLLAB
 void *__devicelib_memcpy(void *dest, const void *src, size_t n) {
   return __builtin_memcpy(dest, src, n);
 }
+#if INTEL_COLLAB
+#if OMP_LIBDEVICE
+#pragma omp end declare target
+#endif  // OMP_LIBDEVICE
+#endif  // INTEL_COLLAB
 #endif // __SPIR__
