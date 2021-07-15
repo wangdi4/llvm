a15811d767bcbf138a30c794510c54d1bad10679
Yoonseo Choi <yoonseo.choi@intel.com>
2021-07-15 09:54:47 -0700

    [loopopt] Narrow down profitable inst combines regarding compare insts.
    Change-Id: I2373759c8bc2b4ef86a3b8a2baf0fe43ee66a081
    Reviewed-on: https://git-amr-2.devtools.intel.com/gerrit/334066
    Reviewed-by: Chawla, Pankaj <pankaj.chawla@intel.com>
    Tested-by: xmain_checkin_sanity <lab_icls@iclgerrit.sc.intel.com>
    Tested-by: alloy <lab_icla@iclgerrit.sc.intel.com>
    Reviewed-by: chenyang liu <chenyang.liu@intel.com>

--- llvm/lib/Transforms/InstCombine/InstCombineCompares.cpp
+++ llvm/lib/Transforms/InstCombine/InstCombineCompares.cpp
@@ -2734,6 +2734,12 @@ Instruction *InstCombinerImpl::foldICmpAddConstant(ICmpInst &Cmp,
   if (Pred == CmpInst::ICMP_ULT && C == *C2 + SMin)
     return new ICmpInst(ICmpInst::ICMP_SGT, X, ConstantInt::get(Ty, ~(*C2)));
 
+
+#if INTEL_CUSTOMIZATION
+  // Following combinations change signed comps into unsigned comps,
+  // which often can hinder loop trip count recognition.
+  if (!Cmp.getFunction()->isPreLoopOpt()) {
+#endif // INTEL_CUSTOMIZATION
   // (X + C2) >s C --> X <u (SMAX - C) (if C == C2 - 1)
   if (Pred == CmpInst::ICMP_SGT && C == *C2 - 1)
     return new ICmpInst(ICmpInst::ICMP_ULT, X, ConstantInt::get(Ty, SMax - C));
@@ -2741,6 +2747,9 @@ Instruction *InstCombinerImpl::foldICmpAddConstant(ICmpInst &Cmp,
   // (X + C2) <s C --> X >u (C ^ SMAX) (if C == C2)
   if (Pred == CmpInst::ICMP_SLT && C == *C2)
     return new ICmpInst(ICmpInst::ICMP_UGT, X, ConstantInt::get(Ty, C ^ SMax));
+#if INTEL_CUSTOMIZATION
+  }
+#endif // INTEL_CUSTOMIZATION
 
 #if INTEL_CUSTOMIZATION
   // Disabling this particular optimization before loopopt as it interferes with
