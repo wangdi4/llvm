415acfd24ba99b40d83945c6984b8777efd05aa5
elizabethandrews <50596170+elizabethandrews@users.noreply.github.com>
2021-07-16 16:20:04 -0400

    [SYCL] Add new SYCL attribute readonly
    If a SYCL accessor is templated on access::mode::read, then represent
    this in LLVM IR as the readonly attribute applied on the kernel argument
    corresponding to the accessor.
    
    Change-Id: I49cddf2780cccc09821038311ab21e936f11a074
    Reviewed-on: https://git-amr-2.devtools.intel.com/gerrit/334310
    Tested-by: xmain_checkin <lab_iclt@iclgerrit.sc.intel.com>
    Reviewed-by: Aaron Ballman <aaron.ballman@intel.com>
    Reviewed-by: chenyang liu <chenyang.liu@intel.com>

--- clang/lib/CodeGen/CGCall.cpp
+++ clang/lib/CodeGen/CGCall.cpp
@@ -2751,6 +2751,9 @@ void CodeGenFunction::EmitFunctionProlog(const CGFunctionInfo &FI,
     unsigned FirstIRArg, NumIRArgs;
     std::tie(FirstIRArg, NumIRArgs) = IRFunctionArgs.getIRArgs(ArgNo);
 
+    if (Arg->hasAttr<SYCLAccessorReadonlyAttr>())
+      Fn->getArg(FirstIRArg)->addAttr(llvm::Attribute::ReadOnly);
+
     switch (ArgI.getKind()) {
     case ABIArgInfo::InAlloca: {
       assert(NumIRArgs == 0);
