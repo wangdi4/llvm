81195812c2c8f09403ac28e33101560f1c58312d
smaslov-intel <sergey.v.maslov@intel.com>
2021-07-28 02:56:41 -0700

    [SYCL][L0] Add event interop (#4178)
    Added SYCL<->L0 interop of events.
    
    The PI events created from L0 events do not know of event's pool or event's queue.
    They are just synchronizations purposes.
    
    E2E test: intel/llvm-test-suite#378
    
    Change-Id: Ie4b4ae38543b4e9e18b276b1128f29218957874d
    Signed-off-by: Sergey V Maslov <sergey.v.maslov@intel.com>
    Reviewed-on: https://git-amr-2.devtools.intel.com/gerrit/336238
    Tested-by: alloy <lab_icla@iclgerrit.sc.intel.com>
    Reviewed-by: Kurkov, Vasiliy A <vasiliy.a.kurkov@intel.com>

--- sycl/plugins/opencl/pi_opencl.cpp
+++ sycl/plugins/opencl/pi_opencl.cpp
@@ -713,8 +713,16 @@ pi_result piEventCreate(pi_context context, pi_event *ret_event) {
 }
 
 pi_result piextEventCreateWithNativeHandle(pi_native_handle nativeHandle,
+                                           pi_context context,
+                                           bool ownNativeHandle,
                                            pi_event *piEvent) {
+  (void)context;
+  (void)ownNativeHandle;
+
   assert(piEvent != nullptr);
+  assert(nativeHandle);
+  assert(context);
+
   *piEvent = reinterpret_cast<pi_event>(nativeHandle);
   return PI_SUCCESS;
 }
