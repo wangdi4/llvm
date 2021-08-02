90d96e9ce458c9eae518c6ab81a5680531726424
Erich Keane <erich.keane@intel.com>
2021-08-02 08:00:47 -0700

    Change __pragma registration to intel-compat only, add docs.
    Community submitted a test that depends on __pragma not being a
    registered-macro in non-microsoft-ext mode.  This reverts our
    unconditional change to instead register this token only when in
    intel-compatibility mode.  This also adds documentation.
    
    Change-Id: I7a4b7fcf1cb50744116a1f4e909be1432af05ade
    Reviewed-on: https://git-amr-2.devtools.intel.com/gerrit/335838
    Tested-by: xmain_checkin <lab_iclt@iclgerrit.sc.intel.com>
    Reviewed-by: Rice, Michael P <michael.p.rice@intel.com>
    Reviewed-by: chenyang liu <chenyang.liu@intel.com>

---
+++ clang/test/Lexer/intel/underbar-pragma.cpp
@@ -0,0 +1,8 @@
+// RUN: %clang_cc1 %s -fintel-compatibility-enable=UnderbarPragmaEnabled -verify=enabled
+// RUN: %clang_cc1 %s -verify
+
+// enabled-no-diagnostics
+__pragma(warning(push))
+// expected-error@-1 {{C++ requires a type specifier for all declarations}}
+// expected-error@-2 {{use of undeclared identifier 'push'}}
+// expected-error@-3 {{expected ';' after top level declarator}}
