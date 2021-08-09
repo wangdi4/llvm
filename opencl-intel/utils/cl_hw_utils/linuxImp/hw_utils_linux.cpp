7936b6df4abeee932814d419a22ebe8bc35c61be
Wenwan Xing <wenwan.xing@intel.com>
2021-08-09 17:31:45 +0800

    [NFC]Apply clang-format for utils/cl_hw_utils/linuxImp/
    Change-Id: Ief3096ef03ed6e46e035773d56b543df81331e08
    Reviewed-on: https://git-amr-2.devtools.intel.com/gerrit/336401
    Tested-by: xmain_checkin <lab_iclt@iclgerrit.sc.intel.com>
    Reviewed-by: Dai, Chunyang <chunyang.dai@intel.com>
    Reviewed-by: chenyang liu <chenyang.liu@intel.com>

--- opencl-intel/utils/cl_hw_utils/linuxImp/hw_utils_linux.cpp
+++ opencl-intel/utils/cl_hw_utils/linuxImp/hw_utils_linux.cpp
@@ -22,36 +22,28 @@
 #define RESTORE_EBX "xchg %%edi, %%ebx\n\r"
 #endif
 
-extern "C" void ASM_FUNCTION  Intel::OpenCL::Utils::cl_hw_cpuid(CPUID_PARAMS *params)
-{
-    UINT32 type = (UINT32)params->m_rax;
-    UINT32 eax, ebx, ecx, edx;
+extern "C" void ASM_FUNCTION
+Intel::OpenCL::Utils::cl_hw_cpuid(CPUID_PARAMS *params) {
+  UINT32 type = (UINT32)params->m_rax;
+  UINT32 eax, ebx, ecx, edx;
 
-    // Android NDK 32-bit compiler ignores clobbered registers
-    // list, hence, i am preserving here the %ebx register
-    // which might be used by the compiler
-    __asm__ __volatile__(SAVE_EBX
-			 "cpuid\n\r"
-			 RESTORE_EBX
-			 : "=a" (eax),
-			   "=D" (ebx),
-			   "=c" (ecx),
-			   "=d" (edx)
-                         : "a" (type));
+  // Android NDK 32-bit compiler ignores clobbered registers
+  // list, hence, i am preserving here the %ebx register
+  // which might be used by the compiler
+  __asm__ __volatile__(SAVE_EBX "cpuid\n\r" RESTORE_EBX
+                       : "=a"(eax), "=D"(ebx), "=c"(ecx), "=d"(edx)
+                       : "a"(type));
 
-    params->m_rax = eax;
-    params->m_rbx = ebx;
-    params->m_rcx = ecx;
-    params->m_rdx = edx;
+  params->m_rax = eax;
+  params->m_rbx = ebx;
+  params->m_rcx = ecx;
+  params->m_rdx = edx;
 }
 
-extern "C" void ASM_FUNCTION  Intel::OpenCL::Utils::hw_pause()
-{
-    __asm__ __volatile__ ("pause");
+extern "C" void ASM_FUNCTION Intel::OpenCL::Utils::hw_pause() {
+  __asm__ __volatile__("pause");
 }
 
-extern "C" void* ASM_FUNCTION  get_next_line_address()
-{
-    return __builtin_return_address(0);
+extern "C" void *ASM_FUNCTION get_next_line_address() {
+  return __builtin_return_address(0);
 }
-
