c39e25c28cc90b5fef5aee33ccc7679a803b40a6
Kevin B Smith <kevin.b.smith@intel.com>
2021-08-11 12:31:12 -0700

    Adds unit tests for intrinsics headers with fp exception model.
    These changes enhance the unit testing for the float_control
    pragmas in the intrinsics files to make sure they do not turn
    off the generation of constrained floating point builtins
    when the -ffp-execption-behavior=strict option is in use.
    
    Follow-on additional testing for CMPLRLLVM-29650.
    
    Change-Id: I215f5e2d888045191701bc3b84fd363accee65bc
    Reviewed-on: https://git-amr-2.devtools.intel.com/gerrit/336761
    Reviewed-by: Kaylor, Andrew <andrew.kaylor@intel.com>
    Tested-by: xmain_checkin <lab_iclt@iclgerrit.sc.intel.com>
    Reviewed-by: Mikhail Lychkov <mikhail.lychkov@intel.com>

--- clang/test/Headers/emmintrin_intel.c
+++ clang/test/Headers/emmintrin_intel.c
@@ -1,18 +1,26 @@
-// RUN: %clang_cc1 %s -ffreestanding -ffast-math -triple x86_64-unknown-unknown -emit-llvm -o - \
-// RUN:     | FileCheck -check-prefix=CKFMATH %s
+// RUN: %clang_cc1 %s -ffreestanding -ffast-math \
+// RUN:     -triple x86_64-unknown-unknown -emit-llvm -o - \
+// RUN:     | FileCheck -check-prefixes=CKALL,CKFMATH %s
+//
+// RUN: %clang_cc1 %s -ffreestanding -ffp-exception-behavior=strict \
+// RUN:      -triple x86_64-unknown-unknown -emit-llvm -o - \
+// RUN:     | FileCheck -check-prefixes=CKALL,CKSTRICT %s
 //
 #include <emmintrin.h>
 
 // Make sure that the llvm IR for _mm_add_pd doesn't have any fast math flags
-// CKFMATH: define{{.*}} <2 x double> @test_emmintrin_no_reassoc
+// CKALL: define{{.*}} <2 x double> @test_emmintrin_no_reassoc
 // CKFMATH: fadd <2 x double>
+// CKSTRICT: call <2 x double> @llvm.experimental.constrained.fadd.v2f64
 __m128d test_emmintrin_no_reassoc(__m128d __a, __m128d __b) {
   return _mm_add_pd(__a, __b);
 }
 
 // Make sure that all fast flags were restored outside of the include file.
-// CKFMATH: define{{.*}} double @test_fast
+// For strict, make sure constrained intrinsics are still used
+// CKALL: define{{.*}} double @test_fast
 // CKFMATH: fadd reassoc nnan ninf nsz arcp afn double
+// CKSTRICT: call double @llvm.experimental.constrained.fadd.f64
 double test_fast(double __a, double __b) {
   return __a + __b;
 }
--- clang/test/Headers/immintrin_intel.c
+++ clang/test/Headers/immintrin_intel.c
@@ -1,22 +1,34 @@
-// RUN: %clang_cc1 %s -ffreestanding -ffast-math -triple i386-unknown-unknown -emit-llvm -o - \
-// RUN:     | FileCheck -check-prefix=CKFMATH %s
+// RUN: %clang_cc1 %s -ffreestanding -ffast-math \
+// RUN:     -triple i386-unknown-unknown -emit-llvm -o - \
+// RUN:     | FileCheck -check-prefixes=CKALL,CKFMATH %s
 //
-// RUN: %clang_cc1 %s -ffreestanding -ffast-math -triple x86_64-unknown-unknown -emit-llvm -o - \
-// RUN:     | FileCheck -check-prefix=CKFMATH %s
+// RUN: %clang_cc1 %s -ffreestanding -ffast-math \
+// RUN:     -triple i386-unknown-unknown -emit-llvm -o - \
+// RUN:     | FileCheck -check-prefixes=CKALL,CKFMATH %s
+//
+// RUN: %clang_cc1 %s -ffreestanding -ffp-exception-behavior=strict \
+// RUN:     -triple x86_64-unknown-unknown -emit-llvm -o - \
+// RUN:     | FileCheck -check-prefixes=CKALL,CKSTRICT %s
+//
+// RUN: %clang_cc1 %s -ffreestanding -ffp-exception-behavior=strict \
+// RUN:     -triple x86_64-unknown-unknown -emit-llvm -o - \
+// RUN:     | FileCheck -check-prefixes=CKALL,CKSTRICT %s
 
 // Include the metaheader that includes all intel intrinsic headers.
 #include <immintrin.h>
 
 // Make sure that the llvm IR for _mm_add_ps doesn't have fast math flags set
-// CKFMATH: define{{.*}} <4 x float> @test_xmmintrin_no_reassoc
+// CKALL: define{{.*}} <4 x float> @test_xmmintrin_no_reassoc
 // CKFMATH: fadd <4 x float>
+// CKSTRICT: call <4 x float> @llvm.experimental.constrained.fadd.v4f32
 __m128 __attribute__((__target__("sse"))) test_xmmintrin_no_reassoc(__m128 __a, __m128 __b) {
   return _mm_add_ps(__a, __b);
 }
 
 // Make sure that the llvm IR for _mm_add_pd doesn't have fast math flags set
-// CKFMATH: define{{.*}} <2 x double> @test_emmintrin_no_reassoc
+// CKALL: define{{.*}} <2 x double> @test_emmintrin_no_reassoc
 // CKFMATH: fadd <2 x double>
+// CKSTRICT: call <2 x double> @llvm.experimental.constrained.fadd.v2f64
 __m128d __attribute__((__target__("sse2"))) test_emmintrin_no_reassoc(__m128d __a, __m128d __b) {
   return _mm_add_pd(__a, __b);
 }
@@ -24,23 +36,27 @@ __m128d __attribute__((__target__("sse2"))) test_emmintrin_no_reassoc(__m128d __
 // Make sure that the llvm IR for _mm256_add_ps doesn't have fast math flags set
 // This intrinsic comes from avxintrin.h, and so is checking that
 // changes in immintrin.h affect the files it includes as well.
-// CKFMATH: define{{.*}} <8 x float> @test_mm256intrin_no_reassoc
+// CKALL: define{{.*}} <8 x float> @test_mm256intrin_no_reassoc
 // CKFMATH: fadd <8 x float>
+// CKSTRICT: call <8 x float> @llvm.experimental.constrained.fadd.v8f32
 __m256 __attribute__((__target__(("avx")))) test_mm256intrin_no_reassoc(__m256 __a, __m256 __b) {
   return _mm256_add_ps(__a, __b);
 }
 
 // Make sure that the llvm IR for _mm512_add_ps doesn't have fast math flags set
 // This intrinsic comes from avxintrin.h
-// CKFMATH: define{{.*}} <16 x float> @test_mm512intrin_no_reassoc
+// CKALL: define{{.*}} <16 x float> @test_mm512intrin_no_reassoc
 // CKFMATH: fadd <16 x float>
+// CKSTRICT: call <16 x float> @llvm.experimental.constrained.fadd.v16f32
 __m512 __attribute__((__target__(("avx512f")))) test_mm512intrin_no_reassoc(__m512 __a, __m512 __b) {
   return _mm512_add_ps(__a, __b);
 }
 
 // Make sure that all fast flags were restored outside of the include file.
-// CKFMATH: define{{.*}} double @test_fast
+// For strict, make sure constrained intrinsics are still used
+// CKALL: define{{.*}} double @test_fast
 // CKFMATH: fadd reassoc nnan ninf nsz arcp afn double
+// CKSTRICT: call double @llvm.experimental.constrained.fadd.f64
 double test_fast(double __a, double __b) {
   return __a + __b;
 }
