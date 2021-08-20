wangdi4/llvm

commit 3f5e2a06e3111bbbd0a8f290a59b3837694cad78

[USM] Release USM tracker events in advance
Releasing all USM tracker events during destruction of ContextModule
will make the memory consumption accumulate gradually. Fix this by
releasing tracker event when the related command completes.

Change-Id: I3db62c64a9b7a5e74de2940f55e1269ef79dc101
Reviewed-on: https://git-amr-2.devtools.intel.com/gerrit/336926
Tested-by: alloy <lab_icla@iclgerrit.sc.intel.com>
Reviewed-by: Wenju He <wenju.he@intel.com>
Reviewed-by: Mikhail Lychkov <mikhail.lychkov@intel.com>

opencl-intel/framework/Execution/enqueue_commands.cpp
@@ -65,7 +65,7 @@ Command::Command( const SharedPtr<IOclCommandQueueBase>& cmdQueue ):
     m_returnCode(CL_SUCCESS),
     m_bIsBeingDeleted(false),
     m_bEventDetached(false),
-    m_memory_objects_acquired(false)    
+    m_memory_objects_acquired(false)
 {
     memset(&m_DevCmd, 0, sizeof(cl_dev_cmd_desc));
     m_Event->SetCommand(this);
@@ -74,6 +74,7 @@ Command::Command( const SharedPtr<IOclCommandQueueBase>& cmdQueue ):
     m_pDevice = m_pCommandQueue->GetDefaultDevice();
 
     m_pGpaCommand = nullptr;
+    m_pContextModule = FrameworkProxy::Instance()->GetContextModule();
     INIT_LOGGER_CLIENT(TEXT("Command Logger Client"),LL_DEBUG);
 }
 
@@ -121,14 +122,21 @@ cl_err_code Command::EnqueueSelf(cl_bool bBlocking, cl_uint uNumEventsInWaitList
 
 /******************************************************************
  *
- * Since the command holds the shared pointer to the event that is responsible for its own deletion, 
- * we need to do this here to break this cycle.
+ * Since the command holds the shared pointer to the event that is responsible
+ * for its own deletion, we need to do this here to break this cycle.
  *
  ******************************************************************/
 void inline Command::DetachEventSharedPtr()
 {
     m_bEventDetached = true;
     m_Event.DecRefCnt();
+}
+
+void inline Command::UnregisterUSMFreeWaitEvent() {
+    for (auto usmPtr : m_UsmPtrs) {
+        m_pContextModule->UnregisterUSMFreeWaitEvent(
+                usmPtr, this->GetEvent()->GetHandle());
+    }
 }
 
 /******************************************************************
@@ -215,8 +223,9 @@ cl_err_code Command::NotifyCmdStatusChanged(cl_int iCmdStatus, cl_int iCompletio
         m_Event->SetEventState(EVENT_STATE_DONE);
 
         m_Event->AddProfilerMarker("COMPLETED", ITT_SHOW_COMPLETED_MARKER);
-        DetachEventSharedPtr();    
-        
+        UnregisterUSMFreeWaitEvent();
+        DetachEventSharedPtr();
+
         break;
 
     default:
@@ -2688,17 +2697,15 @@ cl_err_code FillMemObjCommand::CommandDone()
  ******************************************************************/
 MigrateSVMMemCommand::MigrateSVMMemCommand(
     const SharedPtr<IOclCommandQueueBase>&  cmdQueue,
-    ContextModule*         pContextModule,
     cl_mem_migration_flags clFlags,
     cl_uint                uNumMemObjects,
     const void**           pMemObjects,
     const size_t*          sizes):
     Command(cmdQueue), m_pMemObjects(pMemObjects),
-    m_pSizes(sizes), m_pContextModule( pContextModule )
+    m_pSizes(sizes)
 {
     assert( 0 != uNumMemObjects );
     assert( nullptr != pMemObjects );
-    assert( nullptr != pContextModule );
 
     m_commandType = CL_COMMAND_SVM_MIGRATE_MEM;
     memset( &m_migrateCmdParams, 0, sizeof(cl_dev_cmd_param_migrate));
@@ -2802,14 +2809,13 @@ cl_err_code MigrateSVMMemCommand::CommandDone()
  ******************************************************************/
 MigrateMemObjCommand::MigrateMemObjCommand(
     const SharedPtr<IOclCommandQueueBase> &cmdQueue,
-    ocl_entry_points * /*pOclEntryPoints*/, ContextModule *pContextModule,
+    ocl_entry_points * /*pOclEntryPoints*/,
     cl_mem_migration_flags clFlags, cl_uint uNumMemObjects,
     const cl_mem *pMemObjects)
-    : Command(cmdQueue), m_pMemObjects(pMemObjects),
-      m_pContextModule(pContextModule) {
+    : Command(cmdQueue), m_pMemObjects(pMemObjects)
+{
   assert(0 != uNumMemObjects);
   assert(nullptr != pMemObjects);
-  assert(nullptr != pContextModule);
 
   m_commandType = CL_COMMAND_MIGRATE_MEM_OBJECTS;
   memset(&m_migrateCmdParams, 0, sizeof(cl_dev_cmd_param_migrate));
@@ -2910,15 +2916,12 @@ cl_err_code MigrateMemObjCommand::CommandDone()
  ******************************************************************/
 MigrateUSMMemCommand::MigrateUSMMemCommand(
     const SharedPtr<IOclCommandQueueBase>&  cmdQueue,
-    ContextModule*         contextModule,
     cl_mem_migration_flags_intel clFlags,
     const void* ptr,
     size_t size):
-    Command(cmdQueue), m_ptr(ptr),
-    m_contextModule(contextModule)
+    Command(cmdQueue), m_ptr(ptr)
 {
     assert(nullptr != ptr);
-    assert(nullptr != contextModule);
 
     m_commandType = CL_COMMAND_MIGRATEMEM_INTEL;
     memset(&m_migrateCmdParams, 0, sizeof(m_migrateCmdParams));
@@ -2938,7 +2941,7 @@ cl_err_code MigrateUSMMemCommand::Init()
             : MemoryObject::READ_ONLY;
 
     SharedPtr<Context> queueContext =
-        m_contextModule->GetContext(m_pCommandQueue->GetParentHandle());
+        m_pContextModule->GetContext(m_pCommandQueue->GetParentHandle());
 
     SharedPtr<USMBuffer> memObj = queueContext->GetUSMBufferContainingAddr(
         const_cast<void*>(m_ptr));
@@ -2977,7 +2980,7 @@ cl_err_code MigrateUSMMemCommand::Execute()
     LogDebugA("Command - EXECUTE: %s (Id: %d)", GetCommandName(),
               m_Event->GetId());
 
-    SharedPtr<Context> queueContext = m_contextModule->GetContext(
+    SharedPtr<Context> queueContext = m_pContextModule->GetContext(
         m_pCommandQueue->GetParentHandle());
     SharedPtr<USMBuffer> memObj = queueContext->GetUSMBufferContainingAddr(
         const_cast<void*>(m_ptr));
@@ -3007,14 +3010,12 @@ cl_err_code MigrateUSMMemCommand::CommandDone()
  ******************************************************************/
 AdviseUSMMemCommand::AdviseUSMMemCommand(
     const SharedPtr<IOclCommandQueueBase>& cmdQueue,
-    ContextModule* contextModule,
     const void* ptr,
     size_t size,
     cl_mem_advice_intel advice):
-    Command(cmdQueue), m_ptr(ptr), m_contextModule(contextModule)
+    Command(cmdQueue), m_ptr(ptr)
 {
     assert(nullptr != ptr);
-    assert(nullptr != contextModule);
 
     m_commandType = CL_COMMAND_MEMADVISE_INTEL;
     memset(&m_adviseCmdParams, 0, sizeof(m_adviseCmdParams));
@@ -3037,7 +3038,7 @@ cl_err_code AdviseUSMMemCommand::Init()
 
     MemoryObject::MemObjUsage access = MemoryObject::WRITE_ENTIRE;
 
-    SharedPtr<Context> queueContext = m_contextModule->GetContext(
+    SharedPtr<Context> queueContext = m_pContextModule->GetContext(
         m_pCommandQueue->GetParentHandle());
 
     SharedPtr<USMBuffer> memObj =

opencl-intel/framework/Execution/enqueue_commands.h
@@ -115,13 +115,15 @@ namespace Intel { namespace OpenCL { namespace Framework {
         
         // ICmdStatusChangedObserver function
         cl_err_code NotifyCmdStatusChanged(cl_int iCmdStatus, cl_int iCompletionResult, cl_ulong ulTimer) override;
-        
+
         // Command general functions
         const SharedPtr<QueueEvent>& GetEvent()                             { return m_Event; }
         void            SetDevCmdListId    (cl_dev_cmd_list clDevCmdListId) { m_clDevCmdListId = clDevCmdListId; }
         cl_dev_cmd_list GetDevCmdListId    () const                         { return m_clDevCmdListId; }
         void            SetDevice(const SharedPtr<FissionableDevice>& pDevice)               { m_pDevice = pDevice; }
         const SharedPtr<FissionableDevice>& GetDevice() const                                { return m_pDevice; }
+
+        void SetUsmPtrList(const std::vector<const void *> &usmPtrs) { m_UsmPtrs = usmPtrs; }
 
         cl_dev_cmd_desc* GetDeviceCommandDescriptor();
 
@@ -165,7 +167,9 @@ namespace Intel { namespace OpenCL { namespace Framework {
 
         // call this to break event<>command sharedPtr loop and initiate command deletion
         void DetachEventSharedPtr();
-        
+
+        void UnregisterUSMFreeWaitEvent();
+
         // retrieve device specific descriptor of the memory object.
         // If descriptor is not ready on a device:
         //  1. The descriptor value will be set with NULL
@@ -217,24 +221,27 @@ namespace Intel { namespace OpenCL { namespace Framework {
         SharedPtr<IOclCommandQueueBase> m_pCommandQueue;            // A pointer to the command queue on which the command resides
         cl_int                          m_returnCode;               // The result of the completed command. Can be CL_SUCCESS or one of the errors defined by the spec.
         cl_command_type                 m_commandType;              // Command type
-        
+
         ocl_gpa_command*                m_pGpaCommand;
         bool                            m_bIsBeingDeleted;          // Command destructor is active - to be check during internal event destructor 
         volatile bool                   m_bEventDetached;           // event already detached from the command
-        
 
         // Intermediate data
         MemoryObjectArgList             m_MemOclObjects;
-        
+
+        ContextModule*                  m_pContextModule;
+        // A list of pointers to USM whose free may be blocked by this command.
+        std::vector<const void *>       m_UsmPtrs;
+
         DECLARE_LOGGER_CLIENT;
     private:
-        
+
         // disable possibility to create two instances of Command with the same logger pointer.
         Command(const Command& s);
         Command& operator=(const Command& s);
         // return CL_SUCCESS if ready and succeeded, CL_NOT_READY if not ready yet and succeeded, other error code in case of error
         cl_err_code AcquireSingleMemoryObject( MemoryObjectArg& arg, const SharedPtr<FissionableDevice>& pDev );
-                
+
         bool                        m_memory_objects_acquired;
        
     };
@@ -1179,7 +1186,6 @@ namespace Intel { namespace OpenCL { namespace Framework {
     public:
         MigrateSVMMemCommand(
             const SharedPtr<IOclCommandQueueBase>&  cmdQueue,
-            ContextModule*         pContextModule,
             cl_mem_migration_flags clFlags,
             cl_uint                uNumMemObjects,
             const void**           pMemObject,
@@ -1199,7 +1205,6 @@ namespace Intel { namespace OpenCL { namespace Framework {
       protected:
         const void**                m_pMemObjects;      // used temporary to pass info from contructor to init()
         const size_t*               m_pSizes;
-        ContextModule*              m_pContextModule;
 
         cl_dev_cmd_param_migrate    m_migrateCmdParams;
     };
@@ -1214,7 +1219,6 @@ namespace Intel { namespace OpenCL { namespace Framework {
         MigrateMemObjCommand(
             const SharedPtr<IOclCommandQueueBase>&  cmdQueue,
             ocl_entry_points *     pOclEntryPoints,
-            ContextModule*         pContextModule,
             cl_mem_migration_flags clFlags,
             cl_uint                uNumMemObjects,
             const cl_mem*          pMemObjects
@@ -1232,7 +1236,6 @@ namespace Intel { namespace OpenCL { namespace Framework {
 
       protected:
         const cl_mem*               m_pMemObjects;      // used temporary to pass info from contructor to init()
-        ContextModule*              m_pContextModule;
 
         cl_dev_cmd_param_migrate    m_migrateCmdParams;
     };
@@ -1247,7 +1250,6 @@ namespace Intel { namespace OpenCL { namespace Framework {
     public:
         MigrateUSMMemCommand(
             const SharedPtr<IOclCommandQueueBase>& cmdQueue,
-            ContextModule* contextModule,
             cl_mem_migration_flags_intel clFlags,
             const void* ptr,
             size_t size
@@ -1265,7 +1267,6 @@ namespace Intel { namespace OpenCL { namespace Framework {
 
       protected:
         const void*                  m_ptr;
-        ContextModule*               m_contextModule;
 
         cl_dev_cmd_param_migrate_usm m_migrateCmdParams;
     };
@@ -1278,7 +1279,6 @@ namespace Intel { namespace OpenCL { namespace Framework {
     public:
         AdviseUSMMemCommand(
             const SharedPtr<IOclCommandQueueBase>& cmdQueue,
-            ContextModule* pContextModule,
             const void* ptr,
             size_t size,
             cl_mem_advice_intel advice
@@ -1296,7 +1296,6 @@ namespace Intel { namespace OpenCL { namespace Framework {
 
       protected:
         const void*                 m_ptr;
-        ContextModule*              m_contextModule;
 
         cl_dev_cmd_param_advise_usm m_adviseCmdParams;
     };

opencl-intel/framework/Context/context_module.cpp
@@ -3607,3 +3607,19 @@ void ContextModule::RegisterUSMFreeWaitEvent(
     OclAutoMutex mu(&m_SvmUsmMutex);
     m_mapUSMFreeWaitList[usmPtr].push_back(eventSPtr);
 }
+
+void ContextModule::UnregisterUSMFreeWaitEvent(const void *usmPtr, cl_event evt)
+{
+    assert(usmPtr != nullptr && "Not a valid USM pointer");
+    OclAutoMutex mu(&m_SvmUsmMutex);
+    auto WaitListIter = m_mapUSMFreeWaitList.find(usmPtr);
+    if (WaitListIter == m_mapUSMFreeWaitList.end())
+        return;
+
+    auto &WaitList = WaitListIter->second;
+    auto It = std::find_if(WaitList.begin(), WaitList.end(), [evt](auto &I) {
+        return evt == I.get();
+    });
+    if (It != WaitList.end())
+        WaitList.erase(It);
+}

opencl-intel/framework/Context/context_module.h
@@ -340,6 +340,7 @@ namespace Intel { namespace OpenCL { namespace Framework {
         // Register event that blocking USMFree need to wait.
         void RegisterUSMFreeWaitEvent(const void *usmPtr,
                                       std::shared_ptr<_cl_event> eventSPtr);
+        void UnregisterUSMFreeWaitEvent(const void *usmPtr, cl_event event);
 
 
     private:

opencl-intel/framework/Execution/execution_module.cpp
@@ -900,7 +900,6 @@ cl_err_code ExecutionModule::EnqueueMigrateMemObjects(cl_command_queue clCommand
 
     MigrateMemObjCommand* pMigrateCommand = new MigrateMemObjCommand(
                                         pCommandQueue, (ocl_entry_points*)((_cl_command_queue_int*)pCommandQueue->GetHandle())->dispatch,
-                                        m_pContextModule,
                                         clFlags,
                                         uiNumMemObjects, pMemObjects );
     if (NULL == pMigrateCommand)
@@ -2385,7 +2384,8 @@ cl_err_code ExecutionModule::EnqueueNDRangeKernel(
         for (auto buf : nonArgUsmBufs)
             usmPtrList.push_back(buf->GetAddr());
 
-        SetTrackerForUSM(usmPtrList, trackerEvent, pEvent != nullptr);
+        SetTrackerForUSM(pNDRangeKernelCmd, usmPtrList, trackerEvent,
+                         pEvent != nullptr);
     }
 
     return  errVal;
@@ -3403,8 +3403,8 @@ cl_err_code ExecutionModule::EnqueueSVMMigrateMem(cl_command_queue clCommandQueu
       return CL_INVALID_COMMAND_QUEUE;
     }
 
-    MigrateSVMMemCommand* pMigrateSVMCommand = new MigrateSVMMemCommand(pQueue, m_pContextModule, flags,
-                                                                    num_svm_pointers, svm_pointers, sizes);
+    MigrateSVMMemCommand *pMigrateSVMCommand = new MigrateSVMMemCommand(
+        pQueue, flags, num_svm_pointers, svm_pointers, sizes);
 
     if (NULL == pMigrateSVMCommand)
     {
@@ -3756,10 +3756,13 @@ bool ExecutionModule::CanAccessUSM(SharedPtr<IOclCommandQueueBase> &queue,
     return true;
 }
 
-void ExecutionModule::SetTrackerForUSM(
+void ExecutionModule::SetTrackerForUSM(Command *command,
         const std::vector<const void *> &usmPtrList, cl_event tracker,
         bool isTrackerVisible) {
-    std::shared_ptr<_cl_event> trackerEvtSPtr(tracker, [&](cl_event e) {
+    if (!isTrackerVisible)
+        command->SetUsmPtrList(usmPtrList);
+
+    std::shared_ptr<_cl_event> trackerEvtSPtr(tracker, [=](cl_event e) {
         // If tracker event is not visible to user, we should release it.
         // Otherwise, it is expected to be released by user.
         if (!isTrackerVisible)
@@ -3844,7 +3847,7 @@ cl_err_code ExecutionModule::EnqueueUSMMemset(cl_command_queue command_queue,
         return err;
     }
 
-    SetTrackerForUSM({dst_ptr}, trackerEvent, event != nullptr);
+    SetTrackerForUSM(memsetCommand, {dst_ptr}, trackerEvent, event != nullptr);
 
     return CL_SUCCESS;
 }
@@ -3924,7 +3927,7 @@ cl_err_code ExecutionModule::EnqueueUSMMemFill(cl_command_queue command_queue,
         return err;
     }
 
-    SetTrackerForUSM({dst_ptr}, trackerEvent, event != nullptr);
+    SetTrackerForUSM(memFillCommand, {dst_ptr}, trackerEvent, event != nullptr);
 
     return CL_SUCCESS;
 }
@@ -4045,7 +4048,7 @@ cl_err_code ExecutionModule::EnqueueUSMMemcpy(cl_command_queue command_queue,
         usmPtrs.push_back(src_ptr);
         usmPtrs.push_back(dst_ptr);
     }
-    SetTrackerForUSM(usmPtrs, trackerEvent, event != nullptr);
+    SetTrackerForUSM(memcpyCommand, usmPtrs, trackerEvent, event != nullptr);
 
     return CL_SUCCESS;
 }
@@ -4078,8 +4081,8 @@ cl_err_code ExecutionModule::EnqueueUSMMigrateMem(
     if (CL_FAILED(err))
         return err;
 
-    MigrateUSMMemCommand* migrateCommand = new MigrateUSMMemCommand(
-        queue, m_pContextModule, flags, ptr, size );
+    MigrateUSMMemCommand *migrateCommand =
+        new MigrateUSMMemCommand(queue, flags, ptr, size);
 
     err = migrateCommand->Init();
     if (CL_FAILED(err))
@@ -4104,7 +4107,7 @@ cl_err_code ExecutionModule::EnqueueUSMMigrateMem(
         return err;
     }
 
-    SetTrackerForUSM({ptr}, trackerEvent, event != nullptr);
+    SetTrackerForUSM(migrateCommand, {ptr}, trackerEvent, event != nullptr);
 
     return CL_SUCCESS;
 }
@@ -4140,8 +4143,8 @@ cl_err_code ExecutionModule::EnqueueUSMMemAdvise(
     if (0 == advice)
         return CL_INVALID_VALUE;
 
-    AdviseUSMMemCommand* adviseCommand = new AdviseUSMMemCommand(
-        queue, m_pContextModule, ptr, size, advice);
+    AdviseUSMMemCommand *adviseCommand =
+        new AdviseUSMMemCommand(queue, ptr, size, advice);
 
     err = adviseCommand->Init();
     if (CL_FAILED(err))
@@ -4166,7 +4169,7 @@ cl_err_code ExecutionModule::EnqueueUSMMemAdvise(
         return err;
     }
 
-    SetTrackerForUSM({ptr}, trackerEvent, event != nullptr);
+    SetTrackerForUSM(adviseCommand, {ptr}, trackerEvent, event != nullptr);
 
     return CL_SUCCESS;
 }
@@ -4248,7 +4251,7 @@ cl_err_code ExecutionModule::EnqueueLibraryCopy(
         if (is_dst_usm)
             usmPtrs.push_back(dst);
     }
-    SetTrackerForUSM(usmPtrs, trackerEvent, event != nullptr);
+    SetTrackerForUSM(cmd, usmPtrs, trackerEvent, event != nullptr);
 
     return CL_SUCCESS;
 }
@@ -4336,7 +4339,7 @@ cl_err_code ExecutionModule::EnqueueLibrarySet(
     if (is_dst_usm) {
         usmPtrs.push_back(dst);
     }
-    SetTrackerForUSM(usmPtrs, trackerEvent, event != nullptr);
+    SetTrackerForUSM(cmd, usmPtrs, trackerEvent, event != nullptr);
 
 
     return CL_SUCCESS;

opencl-intel/framework/Execution/execution_module.h
@@ -336,7 +336,8 @@ namespace Intel { namespace OpenCL { namespace Framework {
                           SharedPtr<USMBuffer> &buf);
 
         // Register tracker event for blocking USMFree
-        void SetTrackerForUSM(const std::vector<const void *> &usmPtrList,
+        void SetTrackerForUSM(Command *command,
+                              const std::vector<const void *> &usmPtrList,
                               cl_event tracker, bool isTrackerVisible);
 
         size_t              CalcRegionSizeInBytes(SharedPtr<MemoryObject> pImage, const size_t* szRegion);

commit 3864f44101ce8836dd4babdab7721c601809da52

[USM] Fix race condition introduced by commit 3f5e2a06e3
Change-Id: Ic71cbbb54b3418ff2372c31fe48973c6aff86447
Reviewed-on: https://git-amr-2.devtools.intel.com/gerrit/337566
Reviewed-by: Dai, Chunyang <chunyang.dai@intel.com>
Tested-by: xmain_checkin <lab_iclt@iclgerrit.sc.intel.com>
Reviewed-by: Mikhail Lychkov <mikhail.lychkov@intel.com>

opencl-intel/framework/Execution/enqueue_commands.cpp
@@ -133,6 +133,7 @@ void inline Command::DetachEventSharedPtr()
 }
 
 void inline Command::UnregisterUSMFreeWaitEvent() {
+    std::lock_guard<std::mutex> lock(m_UsmPtrsMutex);
     for (auto usmPtr : m_UsmPtrs) {
         m_pContextModule->UnregisterUSMFreeWaitEvent(
                 usmPtr, this->GetEvent()->GetHandle());

opencl-intel/framework/Execution/enqueue_commands.h
@@ -123,7 +123,10 @@ namespace Intel { namespace OpenCL { namespace Framework {
         void            SetDevice(const SharedPtr<FissionableDevice>& pDevice)               { m_pDevice = pDevice; }
         const SharedPtr<FissionableDevice>& GetDevice() const                                { return m_pDevice; }
 
-        void SetUsmPtrList(const std::vector<const void *> &usmPtrs) { m_UsmPtrs = usmPtrs; }
+        void SetUsmPtrList(const std::vector<const void *> &usmPtrs) {
+            std::lock_guard<std::mutex> lock(m_UsmPtrsMutex);
+            m_UsmPtrs = usmPtrs;
+        }
 
         cl_dev_cmd_desc* GetDeviceCommandDescriptor();
 
@@ -232,6 +235,7 @@ namespace Intel { namespace OpenCL { namespace Framework {
         ContextModule*                  m_pContextModule;
         // A list of pointers to USM whose free may be blocked by this command.
         std::vector<const void *>       m_UsmPtrs;
+        std::mutex m_UsmPtrsMutex;
 
         DECLARE_LOGGER_CLIENT;
     private: