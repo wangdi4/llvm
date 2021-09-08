bc57c6e47c9b7f8ac86c8a80dba2d5bb308c89b3
Vodopyanov, Dmitry <dmitry.vodopyanov@intel.com>
2021-09-08 03:09:14 -0700

    Revert "[SYCL] Workaround regression in gdb due to some missing debug info"
    This reverts commit 583270a8e8a4b155023397abaddcefac3e6d0409.
    
    Change-Id: Idcf1194ed33886be0b22d337944819a18ee9e655
    Reviewed-on: https://git-amr-2.devtools.intel.com/gerrit/338800
    Reviewed-by: Vodopyanov, Dmitry <dmitry.vodopyanov@intel.com>
    Tested-by: Vodopyanov, Dmitry <dmitry.vodopyanov@intel.com>

--- sycl/include/CL/sycl/image.hpp
+++ sycl/include/CL/sycl/image.hpp
@@ -8,7 +8,6 @@
 
 #pragma once
 
-#include <sycl/ext/oneapi/accessor_property_list.hpp>
 #include <CL/sycl/detail/common.hpp>
 #include <CL/sycl/detail/generic_type_traits.hpp>
 #include <CL/sycl/detail/image_impl.hpp>
@@ -16,6 +15,7 @@
 #include <CL/sycl/stl.hpp>
 #include <CL/sycl/types.hpp>
 #include <cstddef>
+#include <sycl/ext/oneapi/accessor_property_list.hpp>
 
 // sRGB Extension Support
 #define SYCL_EXT_ONEAPI_SRGB 1
