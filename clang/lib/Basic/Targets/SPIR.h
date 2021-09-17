c8b95ca44e55aea4a458c699637df1d274758827
smanna12 <soumi.manna@intel.com>
2021-09-17 11:20:24 -0400

    [Xmain-web] Remove assert to fix three Lit test failures (#70)

--- clang/lib/Basic/Targets/SPIR.h
+++ clang/lib/Basic/Targets/SPIR.h
@@ -79,12 +79,6 @@ class LLVM_LIBRARY_VISIBILITY SPIRTargetInfo : public TargetInfo {
 public:
   SPIRTargetInfo(const llvm::Triple &Triple, const TargetOptions &)
       : TargetInfo(Triple) {
-#if INTEL_CUSTOMIZATION
-    assert((getTriple().getEnvironment() == llvm::Triple::UnknownEnvironment ||
-            getTriple().getEnvironment() == llvm::Triple::IntelFPGA ||
-            getTriple().getEnvironment() == llvm::Triple::IntelEyeQ) &&
-           "SPIR target must use unknown environment type");
-#endif // INTEL_CUSTOMIZATION
     TLSSupported = false;
     VLASupported = false;
     LongWidth = LongAlign = 64;
