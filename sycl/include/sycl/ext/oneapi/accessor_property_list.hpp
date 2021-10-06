d4a21db2950003213eaf1273cf1270f63459aaa1
maximdimakov <maxim.dimakov@intel.com>
2021-10-06 15:14:51 +0300

    [SYCL] Remove old namespaces (#285)
    ONEAPI/INTEL namespaces are deprecated in SYCL 2020

--- sycl/include/sycl/ext/oneapi/accessor_property_list.hpp
+++ sycl/include/sycl/ext/oneapi/accessor_property_list.hpp
@@ -235,8 +235,5 @@ private:
 } // namespace oneapi
 } // namespace ext
 
-namespace __SYCL2020_DEPRECATED("use 'ext::oneapi' instead") ONEAPI {
-  using namespace ext::oneapi;
-}
 } // namespace sycl
 } // __SYCL_INLINE_NAMESPACE(cl)
