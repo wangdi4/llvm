11b971f297a25b0c383d917029162d5f6fedf8d5
iclsrc <iclsrc@intel.com>
2021-10-08 01:15:37 -0700

    Resolve of merge (conflict) 24cde2f6023a [SCEV] Remove invariant requirement from isSCEVExprNeverPo.. '"main"' -> '"xmain-web"' (#561)
    * Resolve of merge (conflict) 24cde2f6023a [SCEV] Remove invariant requirement from isSCEVExprNeverPo.. '"main"' -> '"xmain-web"'
    
      CONFLICT (content): Merge conflict in llvm/lib/Analysis/ScalarEvolution.cpp
    
      commit 24cde2f6023a765b66923f4a4ac033a91878b856
      Author: Philip Reames <listmail@philipreames.com>
      Date:   Fri Oct 1 15:57:37 2021 -0700
    
          [SCEV] Remove invariant requirement from isSCEVExprNeverPoison
    
          This code is attempting to prove that I must execute if we enter the defining scope of the SCEV which will be created from I. In the case where it found a defining addrec scope, it had a rather odd restriction that all of the other operands must be loop invariant in that addrec's loop.
    
          As near as I can tell here, we really only need a upper bound on the defining scope. If we can prove the stronger property, then we must also have proven the property on the exact defining scope as well.
    
          In practice, the actual effect of this change is narrow. The compile time restriction at the top of the routine basically limits us to I being an arithmetic in some loop L with both an addrec operand in L, and a unknown operands in L. Possible to demonstrate, but the main value of the change is removing unneeded code.
    
          Differential Revision: https://reviews.llvm.org/D110892
    
    * Resolve of merge (conflict) 24cde2f6023a [SCEV] Remove invariant requirement from isSCEVExprNeverPo.. '"main"' -> '"xmain-web"'
    
      CONFLICT (content): Merge conflict in llvm/lib/Analysis/ScalarEvolution.cpp
    
      commit 24cde2f6023a765b66923f4a4ac033a91878b856
      Author: Philip Reames <listmail@philipreames.com>
      Date:   Fri Oct 1 15:57:37 2021 -0700
    
          [SCEV] Remove invariant requirement from isSCEVExprNeverPoison
    
          This code is attempting to prove that I must execute if we enter the defining scope of the SCEV which will be created from I. In the case where it found a defining addrec scope, it had a rather odd restriction that all of the other operands must be loop invariant in that addrec's loop.
    
          As near as I can tell here, we really only need a upper bound on the defining scope. If we can prove the stronger property, then we must also have proven the property on the exact defining scope as well.
    
          In practice, the actual effect of this change is narrow. The compile time restriction at the top of the routine basically limits us to I being an arithmetic in some loop L with both an addrec operand in L, and a unknown operands in L. Possible to demonstrate, but the main value of the change is removing unneeded code.
    
          Differential Revision: https://reviews.llvm.org/D110892
    
    Co-authored-by: iclsrc <ia.compiler.tools.git@intel.com>

--- llvm/lib/Analysis/ScalarEvolution.cpp
+++ llvm/lib/Analysis/ScalarEvolution.cpp
@@ -7172,7 +7172,6 @@ ConstantRange ScalarEvolution::getRangeBoundedByLoop(const PHINode &HeaderPhi) {
   if (!LatchValue || !PhiLoop->contains(LatchValue))
     return ConstantRange::getFull(BitWidth);
 
-<<<<<<< HEAD
   // Get a bound on the amount that this value could change over the course of a
   // loop iteration.
   ConstantRange IncSRange = ConstantRange::getEmpty(BitWidth);
@@ -7181,29 +7180,6 @@ ConstantRange ScalarEvolution::getRangeBoundedByLoop(const PHINode &HeaderPhi) {
     const SCEV *Inc = getMinusSCEV(getSCEV(Op), SCEVPhi);
     IncSRange = IncSRange.unionWith(getRangeRef(Inc, HINT_RANGE_SIGNED));
     IncURange = IncURange.unionWith(getRangeRef(Inc, HINT_RANGE_UNSIGNED));
-=======
-  // At this point we know that if I is executed, then it does not wrap
-  // according to at least one of NSW or NUW. If I is not executed, then we do
-  // not know if the calculation that I represents would wrap. Multiple
-  // instructions can map to the same SCEV. If we apply NSW or NUW from I to
-  // the SCEV, we must guarantee no wrapping for that SCEV also when it is
-  // derived from other instructions that map to the same SCEV. We cannot make
-  // that guarantee for cases where I is not executed. So we need to find a
-  // upper bound on the defining scope for the SCEV, and prove that I is
-  // executed every time we enter that scope.  When the bounding scope is a
-  // loop (the common case), this is equivalent to proving I executes on every
-  // iteration of that loop.
-  for (const Use &Op : I->operands()) {
-    // I could be an extractvalue from a call to an overflow intrinsic.
-    // TODO: We can do better here in some cases.
-    if (!isSCEVable(Op->getType()))
-      return false;
-    const SCEV *OpS = getSCEV(Op);
-    if (auto *AddRecS = dyn_cast<SCEVAddRecExpr>(OpS)) {
-      if (isGuaranteedToExecuteForEveryIteration(I, AddRecS->getLoop()))
-        return true;
-    }
->>>>>>> 24cde2f6023a765b66923f4a4ac033a91878b856
   }
 
   // Now we know the starting values, and bounds on the increment. Akin to the
@@ -7522,15 +7498,11 @@ bool ScalarEvolution::isSCEVExprNeverPoison(const Instruction *I) {
   // instructions can map to the same SCEV. If we apply NSW or NUW from I to
   // the SCEV, we must guarantee no wrapping for that SCEV also when it is
   // derived from other instructions that map to the same SCEV. We cannot make
-  // that guarantee for cases where I is not executed. So we need to find the
-  // loop that I is considered in relation to and prove that I is executed for
-  // every iteration of that loop. That implies that the value that I
-  // calculates does not wrap anywhere in the loop, so then we can apply the
-  // flags to the SCEV.
-  //
-  // We check isLoopInvariant to disambiguate in case we are adding recurrences
-  // from different loops, so that we know which loop to prove that I is
-  // executed in.
+  // that guarantee for cases where I is not executed. So we need to find a
+  // upper bound on the defining scope for the SCEV, and prove that I is
+  // executed every time we enter that scope.  When the bounding scope is a
+  // loop (the common case), this is equivalent to proving I executes on every
+  // iteration of that loop.
   for (const Use &Op : I->operands()) {
     // I could be an extractvalue from a call to an overflow intrinsic.
     // TODO: We can do better here in some cases.
@@ -7538,15 +7510,7 @@ bool ScalarEvolution::isSCEVExprNeverPoison(const Instruction *I) {
       return false;
     const SCEV *OpS = getSCEV(Op);
     if (auto *AddRecS = dyn_cast<SCEVAddRecExpr>(OpS)) {
-      const bool AllOtherOpsLoopInvariant =
-        llvm::all_of(I->operands(), [&](const Use &Op2) -> bool {
-          if (Op.getOperandNo() == Op2.getOperandNo())
-            return true;
-          const SCEV *OtherOp = getSCEV(Op2);
-          return isLoopInvariant(OtherOp, AddRecS->getLoop());
-        });
-      if (AllOtherOpsLoopInvariant &&
-          isGuaranteedToExecuteForEveryIteration(I, AddRecS->getLoop()))
+      if (isGuaranteedToExecuteForEveryIteration(I, AddRecS->getLoop()))
         return true;
     }
   }
