a7326e8aa23360020901763c695aebec533b527b
iclsrc <iclsrc@intel.com>
2021-10-13 20:45:07 -0700

    Resolve of merge (build) b3a6d096d7c3 Fix shlib builds for all lib/Target/*/TargetInfo libs 'main' -> 'xmain-web' (#765)
    * Resolve of merge (build) b3a6d096d7c3 Fix shlib builds for all lib/Target/*/TargetInfo libs 'main' -> 'xmain-web'
    
      commit b3a6d096d7c369f5290172c126bd699716bb383c
      Author: Reid Kleckner <rnk@google.com>
      Date:   Fri Oct 8 15:18:58 2021 -0700
    
          Fix shlib builds for all lib/Target/*/TargetInfo libs
    
          They all must depend on MC now that the target registry is in MC.
          Also fix llvm-cxxdump
    
    * Resolve of merge (build) b3a6d096d7c3 Fix shlib builds for all lib/Target/*/TargetInfo libs 'main' -> 'xmain-web'
    
      commit b3a6d096d7c369f5290172c126bd699716bb383c
      Author: Reid Kleckner <rnk@google.com>
      Date:   Fri Oct 8 15:18:58 2021 -0700
    
          Fix shlib builds for all lib/Target/*/TargetInfo libs
    
          They all must depend on MC now that the target registry is in MC.
          Also fix llvm-cxxdump
    
    Co-authored-by: iclsrc <ia.compiler.tools.git@intel.com>

--- llvm/lib/Target/Intel_CSA/CSAAsmPrinter.cpp
+++ llvm/lib/Target/Intel_CSA/CSAAsmPrinter.cpp
@@ -39,8 +39,8 @@
 #include "llvm/MC/MCSectionELF.h"
 #include "llvm/MC/MCStreamer.h"
 #include "llvm/MC/MCSymbol.h"
+#include "llvm/MC/TargetRegistry.h"
 #include "llvm/Support/Path.h"
-#include "llvm/Support/TargetRegistry.h"
 #include "llvm/Support/raw_ostream.h"
 #include "llvm/Target/TargetLoweringObjectFile.h"
 #include <fstream>
