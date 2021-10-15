6c9296afd067d5a5c5f8c7f313e337e13aa4d48d
iclsrc <iclsrc@intel.com>
2021-10-15 10:20:35 -0700

    Resolve of merge (conflict) f9bf03006587 [SYCL] Disable fallback assert for interop kernels (#729) '"xmain"' -> '"xmain-web"' (1 commits) (#830)
    * Resolve of merge (conflict) f9bf03006587 [SYCL] Disable fallback assert for interop kernels (#729) '"xmain"' -> '"xmain-web"' (1 commits)
    
      CONFLICT (content): Merge conflict in sycl/unittests/assert/assert.cpp
    
      commit f9bf0300658708fb1e168907cd3c5f3ea4477f49
      Author: sergei <sergey.kanaev@intel.com>
      Date:   Fri Oct 15 18:51:25 2021 +0300
    
          [SYCL] Disable fallback assert for interop kernels (#729)
    
          Signed-off-by: Sergey Kanaev <sergey.kanaev@intel.com>
    
    * Resolve of merge (conflict) f9bf03006587 [SYCL] Disable fallback assert for interop kernels (#729) '"xmain"' -> '"xmain-web"' (1 commits)
    
      CONFLICT (content): Merge conflict in sycl/unittests/assert/assert.cpp
    
      commit f9bf0300658708fb1e168907cd3c5f3ea4477f49
      Author: sergei <sergey.kanaev@intel.com>
      Date:   Fri Oct 15 18:51:25 2021 +0300
    
          [SYCL] Disable fallback assert for interop kernels (#729)
    
          Signed-off-by: Sergey Kanaev <sergey.kanaev@intel.com>
    
    Co-authored-by: iclsrc <ia.compiler.tools.git@intel.com>

--- sycl/unittests/assert/assert.cpp
+++ sycl/unittests/assert/assert.cpp
@@ -549,7 +549,6 @@ TEST(Assert, TestPositive) {
 #endif // _WIN32
 }
 
-<<<<<<< HEAD
 TEST(Assert, TestAssertServiceKernelHidden) {
   const char *AssertServiceKernelName = sycl::detail::KernelInfo<
       sycl::detail::__sycl_service_kernel__::AssertInfoCopier>::getName();
@@ -564,8 +563,6 @@ TEST(Assert, TestAssertServiceKernelHidden) {
   EXPECT_TRUE(NoFoundServiceKernelID);
 }
 
-=======
->>>>>>> f9bf0300658708fb1e168907cd3c5f3ea4477f49
 TEST(Assert, TestInteropKernelNegative) {
   sycl::platform Plt{sycl::default_selector()};
 
