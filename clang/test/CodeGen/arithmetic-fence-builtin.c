0bf0a033908e16d30496ad38b497233eca79a7df
iclsrc <iclsrc@intel.com>
2021-10-17 01:19:40 -0700

    Resolve of merge (conflict) d88c0861916a Merge from '"main"' to '"sycl-web"' (2 commits) '"sycl-web"' -> '"xmain-web"' (#861)
    * Resolve of merge (conflict) d88c0861916a Merge from '"main"' to '"sycl-web"' (2 commits) '"sycl-web"' -> '"xmain-web"'
    
      CONFLICT (content): Merge conflict in clang/test/CodeGenObjC/exceptions.m
      CONFLICT (content): Merge conflict in clang/test/CodeGen/arithmetic-fence-builtin.c
    
      commit d88c0861916aa10e409b70f4cfe88020cc5d0417
      Merge: ebc3864f5ffc 8ca4b3ef19fe
      Author: Georgios Rokos <georgios.rokos@intel.com>
      Date:   Sat Oct 16 14:53:12 2021 -0700
    
          Merge from '"main"' to '"sycl-web"' (2 commits)
    
            CONFLICT (content): Merge conflict in clang/test/CodeGenSYCL/unique_stable_name.cpp
            CONFLICT (content): Merge conflict in clang/test/CodeGenSYCL/address-space-mangling.cpp
    
    * Resolve of merge (conflict) d88c0861916a Merge from '"main"' to '"sycl-web"' (2 commits) '"sycl-web"' -> '"xmain-web"'
    
      CONFLICT (content): Merge conflict in clang/test/CodeGenObjC/exceptions.m
      CONFLICT (content): Merge conflict in clang/test/CodeGen/arithmetic-fence-builtin.c
    
      commit d88c0861916aa10e409b70f4cfe88020cc5d0417
      Merge: ebc3864f5ffc 8ca4b3ef19fe
      Author: Georgios Rokos <georgios.rokos@intel.com>
      Date:   Sat Oct 16 14:53:12 2021 -0700
    
          Merge from '"main"' to '"sycl-web"' (2 commits)
    
            CONFLICT (content): Merge conflict in clang/test/CodeGenSYCL/unique_stable_name.cpp
            CONFLICT (content): Merge conflict in clang/test/CodeGenSYCL/address-space-mangling.cpp
    
    Co-authored-by: iclsrc <ia.compiler.tools.git@intel.com>

--- clang/test/CodeGen/arithmetic-fence-builtin.c
+++ clang/test/CodeGen/arithmetic-fence-builtin.c
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 // Test with fast math
 // RUN: %clang_cc1 -triple x86_64-linux-gnu -emit-llvm -DFAST \
 // RUN: -mreassociate \
@@ -22,7 +21,7 @@
 int v;
 
 int addit(float a, float b) {
-  // CHECK: define {{.*}}@addit(float {{.*}}, float {{.*}}) #0 {
+  // CHECK: define {{.*}}@addit(float noundef {{.*}}, float noundef {{.*}}) #0 {
 
   // CHECKSTINT-32: [[P_32:%.*]] = alloca i32*, align 4
   // CHECKSTINT-32-NEXT: [[I_32:%.*]] = alloca i32, align 4
@@ -135,94 +134,18 @@ int addit(float a, float b) {
 }
 
 int addit1(int a, int b) {
-  // CHECK: define {{.*}}@addit1(i32 {{.*}}, i32 {{.*}}
+  // CHECK: define {{.*}}@addit1(i32 noundef {{.*}}, i32 noundef {{.*}}
   v = (a + b);
-  // CHECK-NOT: call{{.*}} float @llvm.arithmetic.fence.int(float {{.*}})
+  // CHECK-NOT: call{{.*}} float @llvm.arithmetic.fence.int(float noundef {{.*}})
   return 0;
 }
 #ifdef FAST
 #pragma float_control(precise, on)
 int subit(float a, float b, float *fp) {
-  // CHECKFAST: define {{.*}}@subit(float {{.*}}, float {{.*}}
+  // CHECKFAST: define {{.*}}@subit(float noundef {{.*}}, float noundef {{.*}}
   *fp = __arithmetic_fence(a - b);
   *fp = (a + b);
-  // CHECK-NOT: call{{.*}} float @llvm.arithmetic.fence.f32(float {{.*}})
+  // CHECK-NOT: call{{.*}} float @llvm.arithmetic.fence.f32(float noundef {{.*}})
   return 0;
 }
 #endif
-=======
-// Test with fast math
-// RUN: %clang_cc1 -triple i386-pc-linux-gnu -emit-llvm -DFAST \
-// RUN: -mreassociate \
-// RUN: -o - %s | FileCheck --check-prefixes CHECK,CHECKFAST,CHECKNP %s
-//
-// Test with fast math and fprotect-parens
-// RUN: %clang_cc1 -triple i386-pc-linux-gnu -emit-llvm -DFAST \
-// RUN: -mreassociate -fprotect-parens -ffp-contract=on\
-// RUN: -o - %s | FileCheck --check-prefixes CHECK,CHECKFAST,CHECKPP %s
-//
-// Test without fast math: llvm intrinsic not created
-// RUN: %clang_cc1 -triple i386-pc-linux-gnu -emit-llvm -fprotect-parens\
-// RUN: -o - %s | FileCheck --implicit-check-not="llvm.arithmetic.fence" %s
-//
-int v;
-int addit(float a, float b) {
-  // CHECK: define {{.*}}@addit(float noundef %a, float noundef %b) #0 {
-  _Complex double cd, cd1;
-  cd = __arithmetic_fence(cd1);
-  // CHECKFAST: call{{.*}} double @llvm.arithmetic.fence.f64({{.*}}real)
-  // CHECKFAST: call{{.*}} double @llvm.arithmetic.fence.f64({{.*}}imag)
-  // Vector should be supported.
-  typedef float __v2f32 __attribute__((__vector_size__(8)));
-  __v2f32 vec1, vec2;
-  vec1 = __arithmetic_fence(vec2);
-  // CHECKFAST: call{{.*}} <2 x float> @llvm.arithmetic.fence.v2f32
-  vec2 = (vec2 + vec1);
-  // CHECKPP: call{{.*}} <2 x float> @llvm.arithmetic.fence.v2f32
-
-  v = __arithmetic_fence(a + b);
-  // CHECKFAST: call{{.*}} float @llvm.arithmetic.fence.f32(float %add{{.*}})
-
-  v = (a + b);
-  // CHECKPP: call{{.*}} float @llvm.arithmetic.fence.f32(float %add{{.*}})
-  v = a + (b*b);
-  // CHECKPP: fmul reassoc
-  // CHECKPP-NEXT: call{{.*}} float @llvm.arithmetic.fence.f32(float %mul)
-  // CHECKNP: fmul
-  // CHECKNP: fadd
-  v = b + a*a;
-  // CHECKPP: call{{.*}} float @llvm.fmuladd.f32
-  // CHECKNP: fmul
-  // CHECKNP: fadd
-  v = b + __arithmetic_fence(a*a); // Fence blocks recognition of FMA
-  // CHECKPP: fmul
-  // CHECKNP: fmul
-
-  b = (a);
-  (a) = b;
-  // CHECK-NEXT fptosi
-  // CHECK-NEXT store i32
-  // CHECK-NEXT load float
-  // CHECK-NEXT store float
-  // CHECK-NEXT load float
-  // CHECK-NEXT store float
-  return 0;
-  // CHECK-NEXT ret i32 0
-}
-int addit1(int a, int b) {
-  // CHECK: define {{.*}}@addit1(i32 noundef %a, i32 noundef %b{{.*}}
-  v = (a + b);
-  // CHECK-NOT: call{{.*}} float @llvm.arithmetic.fence.int(float noundef %add)
-  return 0;
-}
-#ifdef FAST
-#pragma float_control(precise, on)
-int subit(float a, float b, float *fp) {
-  // CHECKFAST: define {{.*}}@subit(float noundef %a, float noundef %b{{.*}}
-  *fp = __arithmetic_fence(a - b);
-  *fp = (a + b);
-  // CHECK-NOT: call{{.*}} float @llvm.arithmetic.fence.f32(float noundef %add)
-  return 0;
-}
-#endif
->>>>>>> d88c0861916aa10e409b70f4cfe88020cc5d0417
