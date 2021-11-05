486194de08d3a36243eaf9dea816f660bdaf9087
Wenju He <wenju.he@intel.com>
2021-11-06 00:26:32 +0800

    [DumpEnvTest] Add AOT dump and hash test (#85)

--- opencl-intel/tests/framework_test_type/DumpEnvTest.cpp
+++ opencl-intel/tests/framework_test_type/DumpEnvTest.cpp
@@ -24,6 +24,10 @@ protected:
     err = clReleaseContext(m_context);
     EXPECT_OCL_SUCCESS(err, "clReleaseContext");
 
+    for (auto &e : m_env)
+      ASSERT_TRUE(UNSETENV(e.first.c_str()))
+          << ("Failed to unset env " + e.first);
+
     // Delete dumped files
     for (auto &filename : m_dumpFilenames)
       (void)std::remove(filename.c_str());
@@ -54,18 +58,16 @@ protected:
     ASSERT_OCL_SUCCESS(err, "clCreateProgramWithSource");
   }
 
-  void testBody(const std::vector<std::pair<std::string, std::string>> &env,
-                const std::vector<std::string> &suffix,
+  void testBody(const std::vector<std::string> &suffix,
                 const std::vector<std::vector<std::string>> &patterns) {
     std::string prefix("framework_test_type");
-    testBody(env, prefix, suffix, patterns);
+    testBody(prefix, suffix, patterns);
   }
 
-  void testBody(const std::vector<std::pair<std::string, std::string>> &env,
-                const std::string &prefix,
+  void testBody(const std::string &prefix,
                 const std::vector<std::string> &suffix,
                 const std::vector<std::vector<std::string>> &patterns) {
-    for (auto &e : env)
+    for (auto &e : m_env)
       ASSERT_TRUE(SETENV(e.first.c_str(), e.second.c_str()))
           << ("Failed to set env " + e.first);
 
@@ -75,10 +77,6 @@ protected:
     cl_int err =
         clBuildProgram(m_program, 1, &m_device, nullptr, nullptr, nullptr);
     ASSERT_OCL_SUCCESS(err, "clBuildProgram");
-
-    for (auto &e : env)
-      ASSERT_TRUE(UNSETENV(e.first.c_str()))
-          << ("Failed to unset env " + e.first);
 
     size_t numFiles = suffix.size();
     size_t numPatterns = patterns.size();
@@ -100,35 +98,33 @@ protected:
   cl_program m_program;
   std::string m_kernelName;
   std::vector<std::string> m_dumpFilenames;
+  std::vector<std::pair<std::string, std::string>> m_env;
 };
 
 TEST_F(DumpEnvTest, StatsAndPrefix) {
-  std::vector<std::pair<std::string, std::string>> env = {
-      {"VOLCANO_STATS", "InstCounter"},
-      {"CL_CONFIG_DUMP_FILE_NAME_PREFIX", "StatFile"}};
+  m_env = {{"VOLCANO_STATS", "InstCounter"},
+           {"CL_CONFIG_DUMP_FILE_NAME_PREFIX", "StatFile"}};
   std::string prefix("StatFile");
   std::vector<std::string> suffix = {".ll"};
   std::vector<std::vector<std::string>> patterns = {
       {m_kernelName, "InstCounter.CanVect"}};
-  ASSERT_NO_FATAL_FAILURE(testBody(env, prefix, suffix, patterns));
+  ASSERT_NO_FATAL_FAILURE(testBody(prefix, suffix, patterns));
 }
 
 TEST_F(DumpEnvTest, StatsEqualizerAll) {
-  std::vector<std::pair<std::string, std::string>> env = {
-      {"VOLCANO_EQUALIZER_STATS", "All"}};
+  m_env = {{"VOLCANO_EQUALIZER_STATS", "All"}};
   std::vector<std::string> suffix = {"_eq.ll"};
   std::vector<std::vector<std::string>> patterns = {
       {m_kernelName, "spir_kernel"}};
-  ASSERT_NO_FATAL_FAILURE(testBody(env, suffix, patterns));
+  ASSERT_NO_FATAL_FAILURE(testBody(suffix, patterns));
 }
 
 TEST_F(DumpEnvTest, StatsAll) {
-  std::vector<std::pair<std::string, std::string>> env = {
-      {"VOLCANO_STATS", "All"}};
+  m_env = {{"VOLCANO_STATS", "All"}};
   std::vector<std::string> suffix = {".ll"};
   std::vector<std::vector<std::string>> patterns = {
       {m_kernelName, "!sycl.kernels"}};
-  ASSERT_NO_FATAL_FAILURE(testBody(env, suffix, patterns));
+  ASSERT_NO_FATAL_FAILURE(testBody(suffix, patterns));
 }
 
 TEST_F(DumpEnvTest, OptIRAsm) {
@@ -183,35 +179,32 @@ TEST_F(DumpEnvTest, OptIRAsmDebug) {
 }
 
 TEST_F(DumpEnvTest, Asm) {
-  std::vector<std::pair<std::string, std::string>> env = {
-      {"CL_CONFIG_DUMP_ASM", "True"}};
+  m_env = {{"CL_CONFIG_DUMP_ASM", "True"}};
   std::vector<std::string> suffix = {".asm"};
   std::vector<std::vector<std::string>> patterns = {
       {m_kernelName, "Disassembly of section"}};
-  ASSERT_NO_FATAL_FAILURE(testBody(env, suffix, patterns));
+  ASSERT_NO_FATAL_FAILURE(testBody(suffix, patterns));
 }
 
 TEST_F(DumpEnvTest, Bin) {
-  std::vector<std::pair<std::string, std::string>> env = {
-      {"CL_CONFIG_DUMP_BIN", "True"}};
+  m_env = {{"CL_CONFIG_DUMP_BIN", "True"}};
   std::vector<std::string> suffix = {".bin"};
   std::vector<std::vector<std::string>> patterns = {
       {m_kernelName, {0x7f, 'E', 'L', 'F'}}};
-  ASSERT_NO_FATAL_FAILURE(testBody(env, suffix, patterns));
+  ASSERT_NO_FATAL_FAILURE(testBody(suffix, patterns));
 }
 
 /// Check that all dumped filenames contains the same hash.
 TEST_F(DumpEnvTest, CheckHashSame) {
   std::string prefix = "TMP";
-  std::vector<std::pair<std::string, std::string>> env = {
-      {"CL_CONFIG_DUMP_FILE_NAME_PREFIX", prefix},
-      {"VOLCANO_EQUALIZER_STATS", "All"},
-      {"VOLCANO_STATS", "All"},
-      {"CL_CONFIG_DUMP_ASM", "True"},
-      {"CL_CONFIG_DUMP_BIN", "True"}};
+  m_env = {{"CL_CONFIG_DUMP_FILE_NAME_PREFIX", prefix},
+           {"VOLCANO_EQUALIZER_STATS", "All"},
+           {"VOLCANO_STATS", "All"},
+           {"CL_CONFIG_DUMP_ASM", "True"},
+           {"CL_CONFIG_DUMP_BIN", "True"}};
   std::vector<std::string> suffix = {"_eq.ll", ".ll", ".asm", ".bin"};
   std::vector<std::vector<std::string>> patterns;
-  ASSERT_NO_FATAL_FAILURE(testBody(env, prefix, suffix, patterns));
+  ASSERT_NO_FATAL_FAILURE(testBody(prefix, suffix, patterns));
 
   // Check hashes are the same.
   ASSERT_EQ(suffix.size(), m_dumpFilenames.size());
@@ -225,4 +218,41 @@ TEST_F(DumpEnvTest, CheckHashSame) {
     ASSERT_EQ(hash, hashCurr)
         << (filename + " has different hash than " + hash);
   }
+
+  // Check that asm and bin files are dumped in AOT and hash is the same as JIT.
+  auto checkAOT = [&]() {
+    size_t binarySize;
+    cl_int err = clGetProgramInfo(m_program, CL_PROGRAM_BINARY_SIZES,
+                                  sizeof(binarySize), &binarySize, nullptr);
+    ASSERT_OCL_SUCCESS(err, "clGetProgramInfo CL_PROGRAM_BINARY_SIZES");
+    std::vector<unsigned char> binary(binarySize);
+    unsigned char *binaries[1] = {&binary[0]};
+    err = clGetProgramInfo(m_program, CL_PROGRAM_BINARIES, sizeof(binaries),
+                           &binaries, nullptr);
+    ASSERT_OCL_SUCCESS(err, "clGetProgramInfo CL_PROGRAM_BINARIES");
+
+    cl_int binaryStatus;
+    cl_program programAOT = clCreateProgramWithBinary(
+        m_context, 1, &m_device, &binarySize, (const unsigned char **)binaries,
+        &binaryStatus, &err);
+    ASSERT_OCL_SUCCESS(binaryStatus, "clCreateProgramWithBinary binaryStatus");
+    ASSERT_OCL_SUCCESS(err, "clCreateProgramWithBinary");
+
+    err = clBuildProgram(programAOT, 1, &m_device, nullptr, nullptr, nullptr);
+    ASSERT_OCL_SUCCESS(err, "clBuildProgram");
+    err = clReleaseProgram(programAOT);
+    ASSERT_OCL_SUCCESS(err, "clReleaseProgram");
+
+    suffix = {".asm", ".bin"};
+    for (auto &s : suffix) {
+      Regex r(prefix + "_2_[0-9a-f]{16}" + s);
+      std::string filename = findFileInDir(".", r);
+      ASSERT_TRUE(!filename.empty()) << ("AOT " + filename + " is not dumped");
+      std::string asmHash = filename.substr(prefix.length() + 3, 16);
+      ASSERT_EQ(hash, asmHash)
+          << ("AOT dump " + filename + " has different hash than " + hash);
+      m_dumpFilenames.push_back(filename);
+    }
+  };
+  checkAOT();
 }
