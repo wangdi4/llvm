08e9f484e791a0baf2d6aa0e8101d00e457e18c2
mdtoguchi <michael.d.toguchi@intel.com>
2021-12-06 16:36:09 -0700

    [Driver] Port over existing -falign-stack behaviors (#1809)
    -falign-stack was not properly ported over when we moved to the Clang
    driver.  Add these back in, which map to existing -mstack-alignment
    options.

--- clang/test/Driver/intel-options.c
+++ clang/test/Driver/intel-options.c
@@ -526,6 +526,11 @@
 // CHECK-FNO-ALN-NOT: "-function-alignment"
 // CHECK-FUN-ALN-EQ: "-function-alignment" "2"
 
+// -falign-stack
+// RUN: %clang -### -falign-stack=assume-4-byte %s 2>&1 | FileCheck %s -check-prefix CHECK-ALIGN-STACK -DBYTESIZE=4
+// RUN: %clang -### -falign-stack=assume-16-byte %s 2>&1 | FileCheck %s -check-prefix CHECK-ALIGN-STACK -DBYTESIZE=16
+// CHECK-ALIGN-STACK: "-mstack-alignment=[[BYTESIZE]]"
+
 // Behavior with /Qcf-protection option
 // RUN: %clang_cl -### -c /Qcf-protection %s 2>&1 | FileCheck -check-prefix CHECK-QCF-PROTECTION %s
 // RUN: %clang_cl -### -c /Qcf-protection=full %s 2>&1 | FileCheck -check-prefix CHECK-QCF-PROTECTION %s
