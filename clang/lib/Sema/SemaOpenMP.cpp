75aa5fe2a575465b72fa7c283b6c970b83745d08
iclsrc <iclsrc@intel.com>
2021-12-13 16:10:37 -0800

    Resolve of merge (conflict) 5c23acbc023f [NFC][Clang][OpenMP] Use switch-case statement to process .. '"main"' -> '"xmain-web"' (#1921)
    * Resolve of merge (conflict) 5c23acbc023f [NFC][Clang][OpenMP] Use switch-case statement to process .. '"main"' -> '"xmain-web"'
    
      CONFLICT (content): Merge conflict in clang/lib/Sema/SemaOpenMP.cpp
    
      commit 5c23acbc023fbb9fdc6c56f4d2212ecb0432874e
      Author: Shilei Tian <tianshilei1992@gmail.com>
      Date:   Mon Dec 13 16:20:01 2021 -0500
    
          [NFC][Clang][OpenMP] Use switch-case statement to process clauses of atomic directive
    
          This patch makes the process of clauses of atomic directive more clear
          and preparation for the support for `atomic compare capture`.
    
          Reviewed By: jdoerfert
    
          Differential Revision: https://reviews.llvm.org/D115586
    
    * Resolving conflicts
    
    Co-authored-by: iclsrc <ia.compiler.tools.git@intel.com>
    Co-authored-by: Ivan Kelarev <ivan.kelarev@intel.com>

--- clang/lib/Sema/SemaOpenMP.cpp
+++ clang/lib/Sema/SemaOpenMP.cpp
@@ -12595,13 +12595,14 @@ StmtResult Sema::ActOnOpenMPAtomicDirective(ArrayRef<OMPClause *> Clauses,
   bool IsCompareCapture = false;
 #endif // INTEL_COLLAB
   for (const OMPClause *C : Clauses) {
-<<<<<<< HEAD
-    if (C->getClauseKind() == OMPC_read || C->getClauseKind() == OMPC_write ||
-        C->getClauseKind() == OMPC_update ||
+    switch (C->getClauseKind()) {
+    case OMPC_read:
+    case OMPC_write:
+    case OMPC_update:
 #if INTEL_COLLAB
-        C->getClauseKind() == OMPC_compare ||
+    case OMPC_compare:
 #endif // INTEL_COLLAB
-        C->getClauseKind() == OMPC_capture) {
+    case OMPC_capture: {
 #if INTEL_COLLAB
       if (!IsCompareCapture &&
           ((C->getClauseKind() == OMPC_capture && AtomicKind == OMPC_compare) ||
@@ -12610,13 +12611,6 @@ StmtResult Sema::ActOnOpenMPAtomicDirective(ArrayRef<OMPClause *> Clauses,
         IsCompareCapture = true;
       } else
 #endif // INTEL_COLLAB
-=======
-    switch (C->getClauseKind()) {
-    case OMPC_read:
-    case OMPC_write:
-    case OMPC_update:
-    case OMPC_capture: {
->>>>>>> 5c23acbc023fbb9fdc6c56f4d2212ecb0432874e
       if (AtomicKind != OMPC_unknown) {
         Diag(C->getBeginLoc(), diag::err_omp_atomic_several_clauses)
             << SourceRange(C->getBeginLoc(), C->getEndLoc());
