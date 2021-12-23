44670bde4a44067fc9938fc8245817e858a61894
iclsrc <iclsrc@intel.com>
2021-12-22 21:01:03 -0800

    Resolve of merge (conflict) a364e8f6adeb [NFC][Clang] Move function implementation of `OpenMPAtomic.. '"main"' -> '"xmain-web"' (#2082)
    * Resolve of merge (conflict) a364e8f6adeb [NFC][Clang] Move function implementation of `OpenMPAtomic.. '"main"' -> '"xmain-web"'
    
      CONFLICT (content): Merge conflict in clang/lib/Sema/SemaOpenMP.cpp
    
      commit a364e8f6adeb4b42296413b6112a8bdfbd2ea116
      Author: Shilei Tian <tianshilei1992@gmail.com>
      Date:   Wed Dec 22 11:24:06 2021 -0500
    
          [NFC][Clang] Move function implementation of `OpenMPAtomicUpdateChecker` into anonymous namespace
    
          Just to keep code consistent as `OpenMPAtomicUpdateChecker` is defined
          in anonymous namespace.
    
          Reviewed By: ABataev
    
          Differential Revision: https://reviews.llvm.org/D116068
    
    * Solve merge conflicts
    
    Co-authored-by: iclsrc <ia.compiler.tools.git@intel.com>
    Co-authored-by: Bing1 Yu <bing1.yu@intel.com>

--- clang/lib/Sema/SemaOpenMP.cpp
+++ clang/lib/Sema/SemaOpenMP.cpp
@@ -12594,11 +12594,8 @@ bool OpenMPAtomicCompareCaptureChecker::checkStatement(Stmt *S, unsigned DiagId,
     X = V = Result = E = Expected = Desired = nullptr;
   return ErrorFound != NoError;
 }
-<<<<<<< HEAD
 #endif // INTEL_COLLAB
-=======
 } // namespace
->>>>>>> a364e8f6adeb4b42296413b6112a8bdfbd2ea116
 
 StmtResult Sema::ActOnOpenMPAtomicDirective(ArrayRef<OMPClause *> Clauses,
                                             Stmt *AStmt,
