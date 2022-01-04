1a8c3a7310655013f4d7ec19da9a59cc04f0eb78
iclsrc <iclsrc@intel.com>
2022-01-04 08:53:39 -0800

    Resolve of merge (conflict) 961f51fdf04f [LoopVectorize][CostModel] Choose smaller VFs for in-loop .. '"main"' -> '"xmain-web"' (1 commits) (#2164)
    * Resolve of merge (conflict) 961f51fdf04f [LoopVectorize][CostModel] Choose smaller VFs for in-loop .. '"main"' -> '"xmain-web"' (1 commits)
    
      CONFLICT (content): Merge conflict in llvm/test/Transforms/LoopVectorize/X86/funclet.ll
      CONFLICT (content): Merge conflict in llvm/include/llvm/Analysis/IVDescriptors.h
    
      commit 961f51fdf04fd14f5dc5e7a6d53a5460249d947c
      Author: Rosie Sumpter <rosie.sumpter@arm.com>
      Date:   Mon Nov 8 13:15:45 2021 +0000
    
          [LoopVectorize][CostModel] Choose smaller VFs for in-loop reductions without loads/stores
    
          For loops that contain in-loop reductions but no loads or stores, large
          VFs are chosen because LoopVectorizationCostModel::getSmallestAndWidestTypes
          has no element types to check through and so returns the default widths
          (-1U for the smallest and 8 for the widest). This results in the widest
          VF being chosen for the following example,
    
          float s = 0;
          for (int i = 0; i < N; ++i)
            s += (float) i*i;
    
          which, for more computationally intensive loops, leads to large loop
          sizes when the operations end up being scalarized.
    
          In this patch, for the case where ElementTypesInLoop is empty, the widest
          type is determined by finding the smallest type used by recurrences in
          the loop instead of falling back to a default value of 8 bits. This
          results in the cost model choosing a more sensible VF for loops like
          the one above.
    
          Differential Revision: https://reviews.llvm.org/D113973
    
    * Resolving conflicts
    
    Co-authored-by: iclsrc <ia.compiler.tools.git@intel.com>
    Co-authored-by: Ivan Kelarev <ivan.kelarev@intel.com>

--- llvm/include/llvm/Analysis/IVDescriptors.h
+++ llvm/include/llvm/Analysis/IVDescriptors.h
@@ -192,20 +192,13 @@ public:
   RecurrenceDescriptor(Value *Start, Instruction *Exit, RecurKind K,
                        FastMathFlags FMF, Instruction *ExactFP, Type *RT,
                        bool Signed, bool Ordered,
-<<<<<<< HEAD
-                       SmallPtrSetImpl<Instruction *> &CI)
-#if INTEL_CUSTOMIZATION
-      : RDTempl(Start, Exit, K, FMF, RT, Signed, Ordered),
-        ExactFPMathInst(ExactFP) {
-#endif
-=======
                        SmallPtrSetImpl<Instruction *> &CI,
                        unsigned MinWidthCastToRecurTy)
-      : StartValue(Start), LoopExitInstr(Exit), Kind(K), FMF(FMF),
-        ExactFPMathInst(ExactFP), RecurrenceType(RT), IsSigned(Signed),
-        IsOrdered(Ordered),
+#if INTEL_CUSTOMIZATION
+      : RDTempl(Start, Exit, K, FMF, RT, Signed, Ordered),
+        ExactFPMathInst(ExactFP),
         MinWidthCastToRecurrenceType(MinWidthCastToRecurTy) {
->>>>>>> 961f51fdf04fd14f5dc5e7a6d53a5460249d947c
+#endif
     CastInsts.insert(CI.begin(), CI.end());
   }
 
@@ -338,20 +331,11 @@ public:
   /// recurrence.
   const SmallPtrSet<Instruction *, 8> &getCastInsts() const { return CastInsts; }
 
-<<<<<<< HEAD
-=======
   /// Returns the minimum width used by the recurrence in bits.
   unsigned getMinWidthCastToRecurrenceTypeInBits() const {
     return MinWidthCastToRecurrenceType;
   }
 
-  /// Returns true if all source operands of the recurrence are SExtInsts.
-  bool isSigned() const { return IsSigned; }
-
-  /// Expose an ordered FP reduction to the instance users.
-  bool isOrdered() const { return IsOrdered; }
-
->>>>>>> 961f51fdf04fd14f5dc5e7a6d53a5460249d947c
   /// Attempts to find a chain of operations from Phi to LoopExitInst that can
   /// be treated as a set of reductions instructions for in-loop reductions.
   SmallVector<Instruction *, 4> getReductionOpChain(PHINode *Phi,
