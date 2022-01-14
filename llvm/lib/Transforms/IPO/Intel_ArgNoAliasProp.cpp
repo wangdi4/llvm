0cd834120e0064ade95fb53450344dab038f1c51
Sergey Dmitriev <serguei.n.dmitriev@intel.com>
2022-01-14 21:13:10 +0700

    CMPLRLLVM-33901: force phi values analysis to be available when new pm is used (#2287)
    * Updated copyright notice
    
    * Remove 'require' from the existing new pm run command instead of adding a new one.

--- llvm/lib/Transforms/IPO/Intel_ArgNoAliasProp.cpp
+++ llvm/lib/Transforms/IPO/Intel_ArgNoAliasProp.cpp
@@ -1,6 +1,6 @@
 //===- ArgNoAliasProp.cpp ---- Add noalias to function args -----*- C++ -*-===//
 //
-// Copyright (C) 2020 Intel Corporation. All rights reserved.
+// Copyright (C) 2020-2022 Intel Corporation. All rights reserved.
 //
 // The information and source code contained herein is the exclusive property
 // of Intel Corporation and may not be disclosed, examined or reproduced in
@@ -207,6 +207,9 @@ PreservedAnalyses ArgNoAliasPropPass::run(Module &M,
   auto &CG = AM.getResult<CallGraphAnalysis>(M);
 
   auto AARGetter = [&](Function &F) -> AAResults & {
+    // BasicAA uses phi values analysis only if it is available, so force it to
+    // be run on the function.
+    FAM.getResult<PhiValuesAnalysis>(F);
     return FAM.getResult<AAManager>(F);
   };
 
