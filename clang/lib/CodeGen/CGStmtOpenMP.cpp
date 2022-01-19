ea196cf1496b53f1304f179594bb0cbb36f9d3d5
jyu2-git <jennifer.yu@intel.com>
2022-01-19 14:41:01 -0800

    [clang][OpenMP5.1] Initial parsing/sema for 'indirect' clause (#2285)
    This has checked in into community with
    Differential Revision: https://reviews.llvm.org/D116764

--- clang/lib/CodeGen/CGStmtOpenMP.cpp
+++ clang/lib/CodeGen/CGStmtOpenMP.cpp
@@ -6213,6 +6213,7 @@ static void emitOMPAtomicExpr(CodeGenFunction &CGF, OpenMPClauseKind Kind,
   case OMPC_inbranch:
   case OMPC_notinbranch:
   case OMPC_link:
+  case OMPC_indirect:
   case OMPC_use:
   case OMPC_novariants:
   case OMPC_nocontext:
