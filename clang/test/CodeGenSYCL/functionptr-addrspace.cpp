5b42663748b45d82c9d0ad6c31838a5d573a1601
iclsrc <iclsrc@intel.com>
2022-01-24 11:27:39 -0800

    Resolve of merge (conflict) 17b69935e3c7 Merge from '"sycl"' to '"sycl-web"' (16 commits) '"sycl-web"' -> '"xmain-web"' (#2550)
    * Resolve of merge (conflict) 17b69935e3c7 Merge from '"sycl"' to '"sycl-web"' (16 commits) '"sycl-web"' -> '"xmain-web"'
    
      CONFLICT (content): Merge conflict in clang/test/CodeGenSYCL/functionptr-addrspace.cpp
    
      commit 17b69935e3c78f89a6dc1ab54a70fa8f082ccc62
      Merge: 81c3a6c2a5af 30c834aa6317
      Author: Ivan Kelarev <ivan.kelarev@intel.com>
      Date:   Fri Jan 21 08:57:42 2022 -0800
    
          Merge from '"sycl"' to '"sycl-web"' (16 commits)
    
            CONFLICT (content): Merge conflict in clang/test/CodeGenSYCL/functionptr-addrspace.cpp
    
    * Resolving conflicts
    
    * Fix test failure
    
    Co-authored-by: iclsrc <ia.compiler.tools.git@intel.com>
    Co-authored-by: Ivan Kelarev <ivan.kelarev@intel.com>

--- clang/test/CodeGenSYCL/functionptr-addrspace.cpp
+++ clang/test/CodeGenSYCL/functionptr-addrspace.cpp
@@ -7,13 +7,9 @@ __attribute__((sycl_kernel)) void kernel_single_task(const Func &kernelFunc) {
   kernelFunc();
 }
 
-<<<<<<< HEAD
 // INTEL: added nocapture
 // CHECK: define dso_local spir_func{{.*}}invoke_function{{.*}}(i32 ()* nocapture %fptr, i32 addrspace(4)* nocapture %ptr)
 // INTEL
-=======
-// CHECK: define dso_local spir_func{{.*}}invoke_function{{.*}}(i32 ()* nocapture noundef %fptr, i32 addrspace(4)* nocapture noundef %ptr)
->>>>>>> 17b69935e3c78f89a6dc1ab54a70fa8f082ccc62
 void invoke_function(int (*fptr)(), int *ptr) {}
 
 int f() { return 0; }
