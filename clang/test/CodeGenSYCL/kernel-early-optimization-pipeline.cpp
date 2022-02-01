4d5d6663f9b3067e7f22af6a7758f7eaa5b53f85
Sergey Semenov <sergey.semenov@intel.com>
2022-02-01 17:41:03 +0300

    Combine SYCL optimization pipeline tests (#2753)
    intel-kernel-early-optimization-pipeline.cpp started to fail since it
    was descynced from kernel-early-optimization-pipeline.cpp after recent
    changes. This patch combines the two tests into one to fix this failure
    and make sure that future open source changes are reflected in both
    checks.

--- clang/test/CodeGenSYCL/kernel-early-optimization-pipeline.cpp
+++ clang/test/CodeGenSYCL/kernel-early-optimization-pipeline.cpp
@@ -2,10 +2,16 @@
 // SYCL device target, and can be disabled with -fno-sycl-early-optimizations.
 //
 // RUN: %clang_cc1 -O2 -fsycl-is-device -triple spir64-unknown-unknown %s -flegacy-pass-manager -mllvm -debug-pass=Structure -emit-llvm -o /dev/null 2>&1 | FileCheck %s --check-prefix=CHECK-EARLYOPT
+// INTEL_CUSTOMIZATION
+// RUN: %clangxx -fsycl -target x86_64 -O2 -fsycl-device-only %s -mllvm -debug-pass=Structure -emit-llvm -o - 2>&1 | FileCheck %s --check-prefixes=CHECK-EARLYOPT --implicit-check-not "Transform sin and cos calls"
+// end INTEL_CUSTOMIZATION
 // CHECK-EARLYOPT: Combine redundant instructions
 // CHECK-EARLYOPT: Move SYCL printf literal arguments to constant address space
 //
 // RUN: %clang_cc1 -O2 -fsycl-is-device -triple spir64-unknown-unknown %s -flegacy-pass-manager -mllvm -debug-pass=Structure -emit-llvm -fno-sycl-early-optimizations -o /dev/null 2>&1 | FileCheck %s --check-prefix=CHECK-NOEARLYOPT
+// INTEL_CUSTOMIZATION
+// RUN: %clangxx -fsycl -target x86_64 -O2 -fsycl -fsycl-device-only -fno-sycl-early-optimizations %s -mllvm -debug-pass=Structure -emit-llvm -o - 2>&1 | FileCheck %s --check-prefix=CHECK-NOEARLYOPT --implicit-check-not "Transform sin and cos calls"
+// end INTEL_CUSTOMIZATION
 // CHECK-NOEARLYOPT-NOT: Combine redundant instructions
 // CHECK-NOEARLYOPT: Move SYCL printf literal arguments to constant address space
 //
@@ -19,3 +25,9 @@
 // RUN: %clang_cc1 -O2 -fsycl-is-device -triple spir64-unknown-unknown %s -fno-legacy-pass-manager -mdebug-pass Structure -emit-llvm -fno-sycl-early-optimizations -o /dev/null 2>&1 | FileCheck %s --check-prefix=CHECK-NEWPM-NOEARLYOPT
 // CHECK-NEWPM-NOEARLYOPT-NOT: ConstantMergePass
 // CHECK-NEWPM-NOEARLYOPT: SYCLMutatePrintfAddrspacePass
+//
+// INTEL_CUSTOMIZATION
+// Additionally check regular C++:
+// RUN: %clang_cc1 -O2 -triple x86_64 %s -mllvm -debug-pass=Structure -emit-llvm -o - 2>&1 | FileCheck %s --check-prefix=CHECK-CPP
+// CHECK-CPP: Transform sin and cos calls
+// end INTEL_CUSTOMIZATION
