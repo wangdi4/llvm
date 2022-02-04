25579bade47648ad93487e4c10978aeedb55afc7
smanna12 <soumi.manna@intel.com>
2022-02-04 17:27:39 -0500

    [Xmain-web][DTrans] Fix CodeGen Lit tests failures (#2821)
    * [Xmain-web][DTrans] Fix CodeGen Lit tests failures
    
    * Update tests

--- clang/test/CodeGen/intel/DTrans/PtrToMemFun.cpp
+++ clang/test/CodeGen/intel/DTrans/PtrToMemFun.cpp
@@ -13,11 +13,11 @@ class a {
 void a::d() { c<a>(this, &a::e); }
 
 // a::d()
-// CHECK: define dso_local void @_ZN1a1dEv(%class._ZTS1a.a* noundef "intel_dtrans_func_index"="1" %{{.*}}){{.*}}!intel.dtrans.func.type ![[A_D:[0-9]+]]
+// CHECK: define dso_local void @_ZN1a1dEv(%class._ZTS1a.a* {{[^,]*}}"intel_dtrans_func_index"="1" %{{.*}}){{.*}}!intel.dtrans.func.type ![[A_D:[0-9]+]]
 // a::e()
-// CHECK: declare !intel.dtrans.func.type ![[A_E:[0-9]+]] void @_ZN1a1eEv(%class._ZTS1a.a* noundef "intel_dtrans_func_index"="1")
+// CHECK: declare !intel.dtrans.func.type ![[A_E:[0-9]+]] void @_ZN1a1eEv(%class._ZTS1a.a* {{[^,]*}}"intel_dtrans_func_index"="1")
 // c<a>::c(a*, void (a::*)())
-// CHECK: declare !intel.dtrans.func.type ![[C_C:[0-9]+]] void @_ZN1cI1aEC1EPS0_MS0_FvvE(%class._ZTS1cI1aE.c* noundef "intel_dtrans_func_index"="1", %class._ZTS1a.a* noundef "intel_dtrans_func_index"="2", i64, i64)
+// CHECK: declare !intel.dtrans.func.type ![[C_C:[0-9]+]] void @_ZN1cI1aEC1EPS0_MS0_FvvE(%class._ZTS1cI1aE.c* {{[^,]*}}"intel_dtrans_func_index"="1", %class._ZTS1a.a* noundef "intel_dtrans_func_index"="2", i64, i64)
 
 // CHECK: intel.dtrans.types = !{![[A:[0-9]+]], ![[C:[0-9]+]]}
 // CHECK: ![[A]] = !{!"S", %class._ZTS1a.a zeroinitializer, i32 1, ![[CHAR:[0-9]+]]}
