3eb6cbd08abb40208b5dddfa1ab0b9e3f6ebdd47
iclsrc <iclsrc@intel.com>
2022-02-08 11:05:13 -0800

    Resolve of merge (conflict) 14aaaa12366f Re-apply 3fab2d138e30, now with a triple  added '"main"' -> '"xmain-web"' (#2947)
    * Resolve of merge (conflict) 14aaaa12366f Re-apply 3fab2d138e30, now with a triple  added '"main"' -> '"xmain-web"'
    
      CONFLICT (content): Merge conflict in llvm/unittests/CodeGen/InstrRefLDVTest.cpp
    
      commit 14aaaa12366f7d1328b5ec81c71c63de9d878e75
      Author: Jeremy Morse <jeremy.morse@sony.com>
      Date:   Tue Feb 1 19:19:20 2022 +0000
    
          Re-apply 3fab2d138e30, now with a triple  added
    
          Was reverted in 1c1b670a73a9 as it broke all non-x86 bots. Original commit
          message:
    
          [DebugInfo][InstrRef] Add a max-stack-slots-to-track cut-out
    
          In certain circumstances with things like autogenerated code and asan, you
          can end up with thousands of Values live at the same time, causing a large
          working set and a lot of information spilled to the stack. Unfortunately
          InstrRefBasedLDV doesn't cope well with this and consumes a lot of memory
          when there are many many stack slots. See the reproducer in D116821.
    
          It seems very unlikely that a developer would be able to reason about
          hundreds of live named local variables at the same time, so a huge working
          set and many stack slots is an indicator that we're likely analysing
          autogenerated or instrumented code. In those cases: gracefully degrade by
          setting an upper bound on the amount of stack slots to track. This limits
          peak memory consumption, at the cost of dropping some variable locations,
          but in a rare scenario where it's unlikely someone is actually going to
          use them.
    
          In terms of the patch, this adds a cl::opt for max number of stack slots to
          track, and has the stack-slot-numbering code optionally return None. That
          then filters through a number of code paths, which can then chose to not
          track a spill / restore if it touches an untracked spill slot. The added
          test checks that we drop variable locations that are on the stack, if we
          set the limit to zero.
    
          Differential Revision: https://reviews.llvm.org/D118601
    
    * Resolving conflicts
    
    Co-authored-by: iclsrc <ia.compiler.tools.git@intel.com>
    Co-authored-by: Ivan Kelarev <ivan.kelarev@intel.com>

--- llvm/unittests/CodeGen/InstrRefLDVTest.cpp
+++ llvm/unittests/CodeGen/InstrRefLDVTest.cpp
@@ -1097,17 +1097,13 @@ TEST_F(InstrRefLDVTest, MLocDiamondSpills) {
 
   // Create a stack location and ensure it's tracked.
   SpillLoc SL = {getRegByName("RSP"), StackOffset::getFixed(-8)};
-<<<<<<< HEAD
-  SpillLocationNo SpillNo = MTracker->getOrTrackSpillLoc(SL);
+  SpillLocationNo SpillNo = *MTracker->getOrTrackSpillLoc(SL);
 #if INTEL_CUSTOMIZATION
 #if INTEL_FEATURE_XISA_COMMON
   // Some new ISAs like AMX2 etc. introduce more sub register indicators. See
   // X86SubRegIdxRanges. We should change this value when upstream.
   ASSERT_EQ(MTracker->getNumLocs(), 55u); // Tracks all possible stack locs.
 #else // INTEL_FEATURE_XISA_COMMON
-=======
-  SpillLocationNo SpillNo = *MTracker->getOrTrackSpillLoc(SL);
->>>>>>> 14aaaa12366f7d1328b5ec81c71c63de9d878e75
   ASSERT_EQ(MTracker->getNumLocs(), 10u); // Tracks all possible stack locs.
 #endif // INTEL_FEATURE_XISA_COMMON
 #endif // INTEL_CUSTOMIZATION
