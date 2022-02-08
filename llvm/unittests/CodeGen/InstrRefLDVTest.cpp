31325a1c181e95230b46de5258596d101f26c505
iclsrc <iclsrc@intel.com>
2022-02-07 16:06:44 -0800

    Resolve of merge (conflict) 1c1b670a73a9 Revert "[DebugInfo][InstrRef] Add a max-stack-slots-to-tra.. '"main"' -> '"xmain-web"' (#2922)
    * Resolve of merge (conflict) 1c1b670a73a9 Revert "[DebugInfo][InstrRef] Add a max-stack-slots-to-tra.. '"main"' -> '"xmain-web"'
    
      CONFLICT (content): Merge conflict in llvm/unittests/CodeGen/InstrRefLDVTest.cpp
    
      commit 1c1b670a73a9d0bbb3d827fc26a281b4b8f07508
      Author: Kevin Athey <kda@google.com>
      Date:   Tue Feb 1 17:19:39 2022 -0800
    
          Revert "[DebugInfo][InstrRef] Add a max-stack-slots-to-track cut-out"
    
          This reverts commit 3fab2d138e30c65249e1eaea6cc68b2b7f50955a.
    
          Breaking PPC sanitizer build:
          https://lab.llvm.org/buildbot/#/builders/105/builds/20857
    
    * Resolving conflicts
    
    Co-authored-by: iclsrc <ia.compiler.tools.git@intel.com>
    Co-authored-by: Ivan Kelarev <ivan.kelarev@intel.com>

--- llvm/unittests/CodeGen/InstrRefLDVTest.cpp
+++ llvm/unittests/CodeGen/InstrRefLDVTest.cpp
@@ -1097,17 +1097,13 @@ TEST_F(InstrRefLDVTest, MLocDiamondSpills) {
 
   // Create a stack location and ensure it's tracked.
   SpillLoc SL = {getRegByName("RSP"), StackOffset::getFixed(-8)};
-<<<<<<< HEAD
-  SpillLocationNo SpillNo = *MTracker->getOrTrackSpillLoc(SL);
+  SpillLocationNo SpillNo = MTracker->getOrTrackSpillLoc(SL);
 #if INTEL_CUSTOMIZATION
 #if INTEL_FEATURE_XISA_COMMON
   // Some new ISAs like AMX2 etc. introduce more sub register indicators. See
   // X86SubRegIdxRanges. We should change this value when upstream.
   ASSERT_EQ(MTracker->getNumLocs(), 55u); // Tracks all possible stack locs.
 #else // INTEL_FEATURE_XISA_COMMON
-=======
-  SpillLocationNo SpillNo = MTracker->getOrTrackSpillLoc(SL);
->>>>>>> 1c1b670a73a9d0bbb3d827fc26a281b4b8f07508
   ASSERT_EQ(MTracker->getNumLocs(), 10u); // Tracks all possible stack locs.
 #endif // INTEL_FEATURE_XISA_COMMON
 #endif // INTEL_CUSTOMIZATION
