e61ca398f49098f0b98b97b9c13b16ddcb6e578d
Sergey Semenov <sergey.semenov@intel.com>
2022-02-14 18:27:23 +0300

    Cherry-pick multiple release build warning fixes (#3080)
    * Fix the warning after D118805
    
    A variable was used within assert() only.
    
    * [GlobalIsel] Fix another "unused variable" warning
    
    * [SLPVectorizer] Fix "unused variable" build warning
    
    Co-authored-by: Djordje Todorovic <djordje.todorovic@syrmia.com>

--- llvm/lib/CodeGen/GlobalISel/CombinerHelper.cpp
+++ llvm/lib/CodeGen/GlobalISel/CombinerHelper.cpp
@@ -4606,6 +4606,7 @@ bool CombinerHelper::matchMulOBy2(MachineInstr &MI, BuildFnTy &MatchInfo) {
 bool CombinerHelper::matchMulOBy0(MachineInstr &MI, BuildFnTy &MatchInfo) {
   // (G_*MULO x, 0) -> 0 + no carry out
   unsigned Opc = MI.getOpcode();
+  (void)Opc;
   assert(Opc == TargetOpcode::G_UMULO || Opc == TargetOpcode::G_SMULO);
   if (!mi_match(MI.getOperand(3).getReg(), MRI, m_SpecificICstOrSplat(0)))
     return false;
@@ -4624,6 +4625,7 @@ bool CombinerHelper::matchMulOBy0(MachineInstr &MI, BuildFnTy &MatchInfo) {
 bool CombinerHelper::matchAddOBy0(MachineInstr &MI, BuildFnTy &MatchInfo) {
   // (G_*ADDO x, 0) -> x + no carry out
   unsigned Opc = MI.getOpcode();
+  (void)Opc;
   assert(Opc == TargetOpcode::G_UADDO || Opc == TargetOpcode::G_SADDO);
   if (!mi_match(MI.getOperand(3).getReg(), MRI, m_SpecificICstOrSplat(0)))
     return false;
