0c7c7a279149790f250bf92a3461b10b8980a5e4
iclsrc <iclsrc@intel.com>
2022-02-28 04:20:08 -0800

    Resolve of merge (conflict) 7e3606f43c63 [ScalarEvolution] Control flag for nonstrict inequalities .. '"main"' -> '"xmain-web"' (#3372)
    * Resolve of merge (conflict) 7e3606f43c63 [ScalarEvolution] Control flag for nonstrict inequalities .. '"main"' -> '"xmain-web"'
    
      CONFLICT (content): Merge conflict in llvm/lib/Analysis/ScalarEvolution.cpp
    
      commit 7e3606f43c63f9622f176a786424c3c92c15f5c0
      Author: Mircea Trofin <mtrofin@google.com>
      Date:   Wed Feb 23 17:45:32 2022 -0800
    
          [ScalarEvolution] Control flag for nonstrict inequalities in finite loops
    
          D118090 causes a pretty significant (19%) regression in some Eigen
          benchmarks. Investigating is a bit time consuming as the compilation
          unit where this occurs is large. Rather than revert, this patch adds a
          flag controlling that behavior (enabled by default).
    
    * Conflict resolution
    
    Co-authored-by: iclsrc <ia.compiler.tools.git@intel.com>
    Co-authored-by: Dmitry Sidorov <dmitry.sidorov@intel.com>

--- llvm/lib/Analysis/ScalarEvolution.cpp
+++ llvm/lib/Analysis/ScalarEvolution.cpp
@@ -9750,14 +9750,9 @@ ScalarEvolution::computeExitLimitFromICmp(const Loop *L,
   bool ControllingFiniteLoop =
       ControlsExit && loopHasNoAbnormalExits(L) && loopIsFiniteByAssumption(L);
   // Simplify the operands before analyzing them.
-<<<<<<< HEAD
   (void)SimplifyICmpOperands(Pred, LHS, RHS, ExitCond, /*Depth=*/0, // INTEL
-                             ControllingFiniteLoop);
-=======
-  (void)SimplifyICmpOperands(Pred, LHS, RHS, /*Depth=*/0,
                              (EnableFiniteLoopControl ? ControllingFiniteLoop
                                                      : false));
->>>>>>> 7e3606f43c63f9622f176a786424c3c92c15f5c0
 
   // If we have a comparison of a chrec against a constant, try to use value
   // ranges to answer this query.
