09180ab7f6719a14b6616a64b2c092348c915291
FreddyLeaf <freddy.ye@intel.com>
2022-03-08 05:56:35 +0800

    Revert "[CMPLRLLVM-34990] LSR register cost model tuning. (#3121)" (#3475)
    This reverts commit a6e87723f6560f8292b4d40b552df112608fd3ee. The patch has two perf regressions and no stable performance gain.

--- llvm/lib/Target/X86/X86TargetTransformInfo.cpp
+++ llvm/lib/Target/X86/X86TargetTransformInfo.cpp
@@ -5505,13 +5505,6 @@ bool X86TTIImpl::isLSRCostLess(TargetTransformInfo::LSRCost &C1,
                     C2.ScaleCost, C2.ImmCost, C2.SetupCost);
 }
 
-#if INTEL_CUSTOMIZATION
-bool X86TTIImpl::isLSRCostExceedTTIRegNum(TargetTransformInfo::LSRCost &Cost) {
-  return (Cost.NumRegs > getNumberOfRegisters(false)) &&
-         (Cost.Insns > getNumberOfRegisters(false));
-}
-#endif // INTEL_CUSTOMIZATION
-
 bool X86TTIImpl::canMacroFuseCmp() {
   return ST->hasMacroFusion() || ST->hasBranchFusion();
 }
