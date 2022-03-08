bcafd515089f8a49a7de844eb7e65115ba140f11
iclsrc <iclsrc@intel.com>
2022-03-08 11:10:04 -0800

    Resolve of merge (conflict) 38151a08c21e Reapply "[cmake] Prefix gtest and gtest_main with "llvm_"." 'main' -> 'xmain-web' (#3544)
    * Resolve of merge (conflict) 38151a08c21e Reapply "[cmake] Prefix gtest and gtest_main with "llvm_"." 'main' -> 'xmain-web'
    
      CONFLICT (content): Merge conflict in llvm/utils/unittest/CMakeLists.txt
      CONFLICT (content): Merge conflict in llvm/lib/Testing/Support/CMakeLists.txt
    
      commit 38151a08c21e0cdacd52af03e4bdff258f0bac6f
      Author: Stella Laurenzo <laurenzo@google.com>
      Date:   Fri Mar 4 12:08:04 2022 -0800
    
          Reapply "[cmake] Prefix gtest and gtest_main with "llvm_"."
    
          This reverts commit 7cdda6b8ce49ae3c90c068cff4dc355bba5d77f2.
    
          Differential Revision: https://reviews.llvm.org/D121020
    
    * Resolving conflict
    
    Co-authored-by: iclsrc <ia.compiler.tools.git@intel.com>
    Co-authored-by: Pavel V Chupin <pavel.v.chupin@intel.com>

--- llvm/cmake/modules/AddLLVM.cmake
+++ llvm/cmake/modules/AddLLVM.cmake
@@ -1588,7 +1588,7 @@ function(add_unittest test_suite test_name)
     add_llvm_executable(${test_name} IGNORE_EXTERNALIZE_DEBUGINFO NO_INSTALL_RPATH ${ARGN})
     set(outdir ${CMAKE_CURRENT_BINARY_DIR}/${CMAKE_CFG_INTDIR})
     set_output_directory(${test_name} BINARY_DIR ${outdir} LIBRARY_DIR ${outdir})
-    target_link_libraries(${test_name} PRIVATE gtest_main_stdcpp gtest_stdcpp stdc++ ${LLVM_PTHREAD_LIB})
+    target_link_libraries(${test_name} PRIVATE llvm_gtest_main_stdcpp llvm_gtest_stdcpp stdc++ ${LLVM_PTHREAD_LIB})
     set_property(TARGET ${test_name} APPEND_STRING PROPERTY
                  COMPILE_FLAGS " -stdlib=libstdc++")
   elseif (WIN32 AND NOT (${IS_SYCL_TEST} STREQUAL "-1"))
@@ -1596,7 +1596,7 @@ function(add_unittest test_suite test_name)
     add_llvm_executable(${test_name} IGNORE_EXTERNALIZE_DEBUGINFO NO_INSTALL_RPATH ${ARGN})
     set(outdir ${CMAKE_CURRENT_BINARY_DIR}/${CMAKE_CFG_INTDIR})
     set_output_directory(${test_name} BINARY_DIR ${outdir} LIBRARY_DIR ${outdir})
-    target_link_libraries(${test_name} PRIVATE gtest_main_dyn gtest_dyn ${LLVM_PTHREAD_LIB})
+    target_link_libraries(${test_name} PRIVATE llvm_gtest_main_dyn llvm_gtest_dyn ${LLVM_PTHREAD_LIB})
     foreach(flag_var
         CMAKE_CXX_FLAGS CMAKE_CXX_FLAGS_DEBUG CMAKE_CXX_FLAGS_RELEASE
         CMAKE_CXX_FLAGS_MINSIZEREL CMAKE_CXX_FLAGS_RELWITHDEBINFO)
@@ -1612,7 +1612,7 @@ function(add_unittest test_suite test_name)
     # libpthreads overrides some standard library symbols, so main
     # executable must be linked with it in order to provide consistent
     # API for all shared libaries loaded by this executable.
-    target_link_libraries(${test_name} PRIVATE gtest_main gtest ${LLVM_PTHREAD_LIB})
+    target_link_libraries(${test_name} PRIVATE llvm_gtest_main llvm_gtest ${LLVM_PTHREAD_LIB})
   endif()
   # end INTEL_CUSTOMIZATION
 
