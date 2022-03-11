wangdi4/llvm

commit 136089e8c6d4537b93a11cf7bb62fdf37e4ffc6e

[SYCL][ESIMD] Correct conflict resolution after open-sourcing lsc API (#3597)
Signed-off-by: Vyacheslav N Klochkov <vyacheslav.n.klochkov@intel.com>

sycl/include/sycl/ext/intel/experimental/esimd/memory.hpp
@@ -261,6 +261,15 @@ __ESIMD_API void nbarrier_signal(uint8_t barrier_id,
 
 /// @} sycl_esimd_memory_nbarrier
 
+/* INTEL_CUSTOMIZATION */
+/* INTEL_FEATURE_ESIMD_EMBARGO */
+
+// Wait for source val to be ready
+__ESIMD_API void wait(uint16_t val) { __esimd_wait(val); }
+
+/* INTEL_FEATURE_ESIMD_EMBARGO */
+/* INTEL_CUSTOMIZATION */
+
 /// @defgroup sycl_esimd_memory_lsc LSC memory access APIs.
 /// @ingroup sycl_esimd_memory
 
@@ -268,7 +277,19 @@ __ESIMD_API void nbarrier_signal(uint8_t barrier_id,
 /// @{
 
 namespace detail {
+
 // Compute the data size for 2d block load or store.
+/* INTEL_CUSTOMIZATION */
+/* INTEL_FEATURE_ESIMD_EMBARGO */
+template <int NBlocks, int Height, int Width, bool Transposed>
+constexpr int get_block_2d_data_size() {
+  if (Transposed)
+    return detail::getNextPowerOf2<Height>() * Width * NBlocks;
+  return detail::getNextPowerOf2<Width>() * Height * NBlocks;
+}
+/* end INTEL_FEATURE_ESIMD_EMBARGO */
+/* end INTEL_CUSTOMIZATION */
+
 template <typename T, int NBlocks, int Height, int Width, bool Transposed,
           bool Transformed>
 constexpr int get_lsc_block_2d_data_size() {
@@ -292,7 +313,9 @@ lsc_format_ret(__ESIMD_NS::simd<T1, N> Vals) {
 /* INTEL_FEATURE_ESIMD_EMBARGO */
 
 /// @defgroup sycl_esimd_2d_stateless 2D stateless functions
-/// \ingroup sycl_esimd
+/// @ingroup sycl_esimd
+
+/// @addtogroup sycl_esimd_2d_stateless
 /// @{
 /// 2D stateless block load.
 /// \param T is the element data type
@@ -318,21 +341,23 @@ lsc_format_ret(__ESIMD_NS::simd<T1, N> Vals) {
 ///
 template <typename T, int Width, int Height = 1, int NBlks = 1,
           bool Transposed = false, bool Transformed = false,
-          CacheHint L1H = CacheHint::None, CacheHint L3H = CacheHint::None,
+          cache_hint L1H = cache_hint::none, cache_hint L3H = cache_hint::none,
           int N = get_block_2d_data_size<NBlks, Height, Width, Transposed>()>
 ESIMD_INLINE SYCL_ESIMD_FUNCTION simd<T, N>
 load_2d_stateless(const T *Ptr, unsigned SurfaceWidth, unsigned SurfaceHeight,
                   unsigned SurfacePitch, int X, int Y) {
   simd<T, N> oldDst;
   simd<unsigned int, 16> payload(0);
-  payload.template bit_cast_view<uint64_t>().template select<1, 1>(0) = (uint64_t)Ptr;
+  payload.template bit_cast_view<uint64_t>().template select<1, 1>(0) =
+      (uint64_t)Ptr;
   payload.template select<1, 1>(2) = SurfaceWidth;
   payload.template select<1, 1>(3) = SurfaceHeight;
   payload.template select<1, 1>(4) = SurfacePitch;
   payload.template select<1, 1>(5) = X;
   payload.template select<1, 1>(6) = Y;
   payload.template bit_cast_view<uchar>().template select<1, 1>(28) = Width - 1;
-  payload.template bit_cast_view<uchar>().template select<1, 1>(29) = Height - 1;
+  payload.template bit_cast_view<uchar>().template select<1, 1>(29) =
+      Height - 1;
   uint exDesc = 0x0;
   uint desc = 0x28A0403;
   constexpr uchar execSize = 0x0;
@@ -365,21 +390,23 @@ load_2d_stateless(const T *Ptr, unsigned SurfaceWidth, unsigned SurfaceHeight,
 /// \param Data is the data to be stored.
 ///
 template <typename T, int Width, int Height = 1,
-          CacheHint L1H = CacheHint::None, CacheHint L3H = CacheHint::None,
+          cache_hint L1H = cache_hint::none, cache_hint L3H = cache_hint::none,
           int N = get_block_2d_data_size</*NBlks*/ 1, Height, Width,
                                          /*Transposed*/ false>()>
 ESIMD_INLINE SYCL_ESIMD_FUNCTION void
 store_2d_stateless(T *Ptr, unsigned SurfaceWidth, unsigned SurfaceHeight,
                    unsigned SurfacePitch, int X, int Y, simd<T, N> Data) {
   simd<unsigned int, 16> payload = 0;
-  payload.template bit_cast_view<uint64_t>().template select<1, 1>(0) = (uint64_t)Ptr;
+  payload.template bit_cast_view<uint64_t>().template select<1, 1>(0) =
+      (uint64_t)Ptr;
   payload.template select<1, 1>(2) = SurfaceWidth;
   payload.template select<1, 1>(3) = SurfaceHeight;
   payload.template select<1, 1>(4) = SurfacePitch;
   payload.template select<1, 1>(5) = X;
   payload.template select<1, 1>(6) = Y;
   payload.template bit_cast_view<uchar>().template select<1, 1>(28) = Width - 1;
-  payload.template bit_cast_view<uchar>().template select<1, 1>(29) = Height - 1;
+  payload.template bit_cast_view<uchar>().template select<1, 1>(29) =
+      Height - 1;
   uint exDesc = 0x0;
   uint desc = 0x2040407;
   constexpr uchar execSize = 0x0;
@@ -390,6 +417,8 @@ store_2d_stateless(T *Ptr, unsigned SurfaceWidth, unsigned SurfaceHeight,
   raw_sends_store(payload, Data, exDesc, desc, execSize, sfid, numSrc0,
                   numSrc1);
 }
+
+/// @} sycl_esimd_2d_stateless
 
 /* end INTEL_FEATURE_ESIMD_EMBARGO */
 /* end INTEL_CUSTOMIZATION */

llvm/lib/SYCLLowerIR/ESIMD/LowerESIMD.cpp
@@ -513,8 +513,6 @@ public:
         {"nbarrier", {"nbarrier", {a(0), a(1), a(2)}}},
         {"raw_send_nbarrier_signal",
          {"raw.send.noresult", {a(0), ai1(4), a(1), a(2), a(3)}}},
-#if INTEL_CUSTOMIZATION
-#if INTEL_FEATURE_ESIMD_EMBARGO
         {"lsc_load_slm",
          {"lsc.load.slm",
           {ai1(0), c8(lsc_subopcode::load), t8(1), t8(2), t16(3), t32(4), t8(5),
@@ -596,8 +594,6 @@ public:
           {ai1(0), t8(1), t8(2), t8(3), t16(4), t32(5), t8(6), t8(7), t8(8),
            c8(0), a(1), a(2), a(3), c32(0), u(-1)}}},
         {"lsc_fence", {"lsc.fence", {ai1(0), t8(0), t8(1), t8(2)}}},
-#endif // INTEL_FEATURE_ESIMD_EMBARGO
-#endif // INTEL_CUSTOMIZATION
         {"sat", {"sat", {a(0)}}},
         {"fptoui_sat", {"fptoui.sat", {a(0)}}},
         {"fptosi_sat", {"fptosi.sat", {a(0)}}},
@@ -867,16 +863,12 @@ static std::string getESIMDIntrinSuffix(id::FunctionEncoding *FE,
     case 0x12:
       Suff = ".fcmpwr";
       break;
-#if INTEL_CUSTOMIZATION
-#if INTEL_FEATURE_ESIMD_EMBARGO
     case 0x13:
       Suff = ".fadd";
       break;
     case 0x14:
       Suff = ".fsub";
       break;
-#endif // INTEL_FEATURE_ESIMD_EMBARGO
-#endif // INTEL_CUSTOMIZATION
     case 0xff:
       Suff = ".predec";
       break;

sycl/include/sycl/ext/intel/experimental/esimd/common.hpp
@@ -76,9 +76,6 @@ enum class argument_type {
 };
 
 
-// DO NOT MODIFY THE FOLLOWING ENCODING
-/// The scope that lsc_fence operation should apply to
-/// Supported platforms: XEHP, DG2, PVC, PVC_XT, ELG+
 /// The scope that lsc_fence operation should apply to
 /// Supported platforms: DG2, PVC
 enum class lsc_scope : uint8_t {
@@ -91,9 +88,6 @@ enum class lsc_scope : uint8_t {
   sysacq = 6, /// the entire system memory space with system-acquire semantics
 };
 
-// DO NOT MODIFY THE FOLLOWING ENCODING
-/// The lsc_fence operation to apply to caches
-/// Supported platforms: XEHP, DG2, PVC, PVC_XT, ELG+
 /// The lsc_fence operation to apply to caches
 /// Supported platforms: DG2, PVC
 enum class lsc_fence_op : uint8_t {
@@ -127,25 +121,6 @@ enum class lsc_data_size : uint8_t {
   u16u32h = 7, /// load 16b into high 16 of each 32b; store the high 16
 };
 
-/* INTEL_CUSTOMIZATION */
-/* INTEL_FEATURE_ESIMD_EMBARGO */
-
-// TODO FIXME Remove after embargo API open-source
-// // TODO Cache hints APIs are being reworked.
-// // L1 or L3 cache hint kinds.
-enum class CacheHint : uint8_t {
-  None = 0,
-  Uncached = 1,
-  Cached = 2,
-  WriteBack = 3,
-  WriteThrough = 4,
-  Streaming = 5,
-  ReadInvalidate = 6
-};
-
-/* end INTEL_FEATURE_ESIMD_EMBARGO */
-/* end INTEL_CUSTOMIZATION */
-
 namespace detail {
 /// LSC atomic operations op codes
 enum class lsc_atomic_op : uint8_t {
@@ -170,7 +145,6 @@ enum class lsc_atomic_op : uint8_t {
   bit_xor = 0x1a, // logical (bitwise) XOR
 };
 
-// DO NOT MODIFY THE FOLLOWING ENCODING
 enum class lsc_vector_size : uint8_t {
   n1 = 1,
   n2 = 2,
@@ -182,7 +156,6 @@ enum class lsc_vector_size : uint8_t {
   n64 = 8,
 };
 
-// DO NOT MODIFY THE FOLLOWING ENCODING
 enum class lsc_data_order : uint8_t {
   nontranspose = 1,
   transpose = 2,

sycl/include/sycl/ext/intel/experimental/esimd/detail/memory_intrin.hpp
@@ -267,6 +267,9 @@ __ESIMD_INTRIN void __esimd_raw_send_nbarrier_signal(
 // Wait for val to be ready
 SYCL_EXTERNAL SYCL_ESIMD_FUNCTION void __esimd_wait(uint16_t val);
 
+/* end INTEL_FEATURE_ESIMD_EMBARGO */
+/* end INTEL_CUSTOMIZATION */
+
 /// SLM gather.
 /// Supported platforms: DG2, PVC
 ///
@@ -1013,9 +1016,5 @@ __ESIMD_INTRIN void __esimd_lsc_fence(__ESIMD_DNS::simd_mask_storage_t<N> pred)
   throw cl::sycl::feature_not_supported();
 }
 #endif // __SYCL_DEVICE_ONLY__
-
-
-/* end INTEL_FEATURE_ESIMD_EMBARGO */
-/* end INTEL_CUSTOMIZATION */
 
 /// @endcond ESIMD_DETAIL

commit c2cca95df7ae12c38c6320ba7462b4fc94dbe599

[SYCL][ESIMD] Fix macro copy-paste typo (#3620)
It was introduced in #3597
Should be end macro here

sycl/include/sycl/ext/intel/experimental/esimd/memory.hpp
@@ -267,8 +267,8 @@ __ESIMD_API void nbarrier_signal(uint8_t barrier_id,
 // Wait for source val to be ready
 __ESIMD_API void wait(uint16_t val) { __esimd_wait(val); }
 
-/* INTEL_FEATURE_ESIMD_EMBARGO */
-/* INTEL_CUSTOMIZATION */
+/* end INTEL_FEATURE_ESIMD_EMBARGO */
+/* end INTEL_CUSTOMIZATION */
 
 /// @defgroup sycl_esimd_memory_lsc LSC memory access APIs.
 /// @ingroup sycl_esimd_memory