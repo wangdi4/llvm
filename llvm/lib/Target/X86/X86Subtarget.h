5554bad2988a9e5812766b0effdd3d029084ada9
iclsrc <iclsrc@intel.com>
2022-03-14 18:53:28 -0700

    Resolve of merge (conflict) 294eca35a00f [regalloc] Remove -consider-local-interval-cost 'main' -> 'xmain-web' (#3667)
    * Resolve of merge (conflict) 294eca35a00f [regalloc] Remove -consider-local-interval-cost 'main' -> 'xmain-web'
    
      CONFLICT (content): Merge conflict in llvm/lib/Target/X86/X86Subtarget.h
    
      commit 294eca35a00f89dff474044ebd478a7f83ccc310
      Author: Mircea Trofin <mtrofin@google.com>
      Date:   Mon Mar 7 08:23:05 2022 -0800
    
          [regalloc] Remove -consider-local-interval-cost
    
          Discussed extensively on D98232. The functionality introduced in D35816
          never worked correctly. In D98232, it was fixed, but, as it was
          introducing a large compile-time regression, and the value of the
          original patch was called into doubt, we disabled it by default
          everywhere. A year later, it appears that caused no grief, so it seems
          safe to remove the disabled code.
    
          This should be accompanied by re-opening bug 26810.
    
          Differential Revision: https://reviews.llvm.org/D121128
    
    * Conflict resolution
    
    Co-authored-by: iclsrc <ia.compiler.tools.git@intel.com>
    Co-authored-by: Lina Yu <lina.yu@intel.com>

--- llvm/lib/Target/X86/X86Subtarget.h
+++ llvm/lib/Target/X86/X86Subtarget.h
@@ -1488,9 +1488,6 @@ public:
   AntiDepBreakMode getAntiDepBreakMode() const override {
     return TargetSubtargetInfo::ANTIDEP_CRITICAL;
   }
-<<<<<<< HEAD
-
-  bool enableAdvancedRASplitCost() const override { return false; }
 #if INTEL_CUSTOMIZATION
   void overrideSchedPolicy(MachineSchedPolicy &Policy,
                            unsigned NumRegionInstrs) const override;
@@ -1501,8 +1498,6 @@ public:
                            MachineBasicBlock::iterator B,
                            MachineBasicBlock::iterator E) const override;
 #endif // INTEL_CUSTOMIZATION
-=======
->>>>>>> 294eca35a00f89dff474044ebd478a7f83ccc310
 };
 
 } // end namespace llvm
