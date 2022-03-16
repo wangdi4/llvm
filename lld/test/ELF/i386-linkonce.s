47f33461f55be7e8c8c6218cc2c12fe31ae75ed8
iclsrc <iclsrc@intel.com>
2022-03-15 17:06:33 -0700

    Resolve of merge (conflict) 6be457c14daf [ELF] Work around not-fully-supported .gnu.linkonce.t.__x8.. 'main' -> 'xmain-web' (#3694)
    * Resolve of merge (conflict) 6be457c14daf [ELF] Work around not-fully-supported .gnu.linkonce.t.__x8.. 'main' -> 'xmain-web'
    
      CONFLICT (content): Merge conflict in lld/test/ELF/i386-linkonce.s
    
      commit 6be457c14dafd634989c2c0b702a9231b438e2c4
      Author: Fangrui Song <i@maskray.me>
      Date:   Tue Mar 15 14:48:29 2022 -0700
    
          [ELF] Work around not-fully-supported .gnu.linkonce.t.__x86.get_pc_thunk.bx
    
    * Conflict resolution
    
    Co-authored-by: iclsrc <ia.compiler.tools.git@intel.com>
    Co-authored-by: Lina Yu <lina.yu@intel.com>

--- lld/test/ELF/i386-linkonce.s
+++ lld/test/ELF/i386-linkonce.s
@@ -7,7 +7,6 @@
 // RUN: llvm-mc -filetype=obj -triple=i386-linux-gnu %s -o %t.o
 // RUN: llvm-mc -filetype=obj -triple=i386-linux-gnu %p/Inputs/i386-linkonce.s -o %t2.o
 // RUN: llvm-ar rcs %t2.a %t2.o
-<<<<<<< HEAD
 // INTEL_CUSTOMIZATION
 // BEGIN OLD CHECKS
 // RUN-: not ld.lld %t.o %t2.a -o /dev/null 2>&1 | FileCheck %s
@@ -18,13 +17,11 @@
 // RUN: ld.lld %t.o %t2.a -o /dev/null 2>&1 | FileCheck --allow-empty %s
 // CHECK-NOT: error: relocation refers to a symbol in a discarded section: __i686.get_pc_thunk.bx
 // END INTEL_CUSTOMIZATION
-=======
 
 /// crti.o in i386 glibc<2.32 has .gnu.linkonce.t.__x86.get_pc_thunk.bx that is
 /// not fully supported. Test that we don't report
 /// "relocation refers to a symbol in a discarded section: __x86.get_pc_thunk.bx".
 // RUN: ld.lld %t.o %t2.a -o /dev/null
->>>>>>> 6be457c14dafd634989c2c0b702a9231b438e2c4
 
     .globl _start
 _start:
