a2156d338439972a7f22a182270eeb230a4bd0ab
iclsrc <iclsrc@intel.com>
2022-03-22 03:35:16 -0700

    Resolve of merge (conflict) 5fd945166862 [X86][AVX512] lower1BitShuffle - fold broadcast(setcc(x,y).. 'main' -> 'xmain-web' (#3815)
    * Resolve of merge (conflict) 5fd945166862 [X86][AVX512] lower1BitShuffle - fold broadcast(setcc(x,y).. 'main' -> 'xmain-web'
    
      CONFLICT (content): Merge conflict in llvm/lib/Target/X86/X86ISelLowering.cpp
    
      commit 5fd945166862377be390948c9ab7dab2f47df217
      Author: Simon Pilgrim <llvm-dev@redking.me.uk>
      Date:   Mon Mar 21 17:38:06 2022 +0000
    
          [X86][AVX512] lower1BitShuffle - fold broadcast(setcc(x,y)) -> setcc(broadcast(x),broadcast(y)) (PR52500)
    
          AVX512 has excellent broadcast ops for everything but vXi1 bool vectors - so if we're broadcasting a comparison result, see if we can broadcast the comparison operands instead.
    
    * Conflict resolution
    
    Co-authored-by: iclsrc <ia.compiler.tools.git@intel.com>
    Co-authored-by: Dmitry Sidorov <dmitry.sidorov@intel.com>

--- llvm/lib/Target/X86/X86ISelLowering.cpp
+++ llvm/lib/Target/X86/X86ISelLowering.cpp
@@ -19366,7 +19366,6 @@ static SDValue lower1BitShuffle(const SDLoc &DL, ArrayRef<int> Mask,
     Offset += NumElts; // Increment for next iteration.
   }
 
-<<<<<<< HEAD
 #if INTEL_CUSTOMIZATION
   // lower the following DAGs:
   //   t12: v8i1 = vector_shuffle<0,0,1,1,2,2,3,3> t6, undef:v8i1
@@ -19386,7 +19385,7 @@ static SDValue lower1BitShuffle(const SDLoc &DL, ArrayRef<int> Mask,
     }
   }
 #endif // INTEL_CUSTOMIZATION
-=======
+
   // If we're broadcasting a SETCC result, try to broadcast the ops instead.
   // TODO: What other unary shuffles would benefit from this?
   if (isBroadcastShuffleMask(Mask) && V1.getOpcode() == ISD::SETCC &&
@@ -19399,7 +19398,6 @@ static SDValue lower1BitShuffle(const SDLoc &DL, ArrayRef<int> Mask,
         DL, VT, DAG.getVectorShuffle(OpVT, DL, Op0, DAG.getUNDEF(OpVT), Mask),
         DAG.getVectorShuffle(OpVT, DL, Op1, DAG.getUNDEF(OpVT), Mask), CC);
   }
->>>>>>> 5fd945166862377be390948c9ab7dab2f47df217
 
   MVT ExtVT;
   switch (VT.SimpleTy) {
