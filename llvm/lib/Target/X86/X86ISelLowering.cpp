0f0f2644a7d0807ba6f0b128b703f76f7e810d71
xiangzh1 <xiang1.zhang@intel.com>
2022-04-06 09:20:10 +0800

    Check use number in combineADC JIRA: CMPLRLLVM-36504 (#4109)
    * Check use number in combineADC
    JIRA: CMPLRLLVM-36504
    
    * Check use number in combineADC (add test)
    JIRA: CMPLRLLVM-36504

--- llvm/lib/Target/X86/X86ISelLowering.cpp
+++ llvm/lib/Target/X86/X86ISelLowering.cpp
@@ -55411,7 +55411,7 @@ static SDValue combineADC(SDNode *N, SelectionDAG &DAG,
   // Fold ADC(ADD(X,Y),0,Carry) -> ADC(X,Y,Carry)
   // iff the flag result is dead.
   if (LHS.getOpcode() == ISD::ADD && isNullConstant(RHS) &&
-      !N->hasAnyUseOfValue(1))
+      !N->hasAnyUseOfValue(1) && LHS.getNode()->hasOneUse())  // INTEL
     return DAG.getNode(X86ISD::ADC, SDLoc(N), N->getVTList(), LHS.getOperand(0),
                        LHS.getOperand(1), CarryIn);
 
