9d13b0f0ab616178f32a464a8e8a1a5c046df6f2
iclsrc <iclsrc@intel.com>
2022-04-14 15:21:48 -0700

    Resolve of merge (conflict) ab8abeaf48ab [Driver] Change CLANG_ENABLE_OPAQUE_POINTERS_INTERNAL to a.. 'main' -> 'xmain-web' (#4424)
    * Resolve of merge (conflict) ab8abeaf48ab [Driver] Change CLANG_ENABLE_OPAQUE_POINTERS_INTERNAL to a.. 'main' -> 'xmain-web'
    
      CONFLICT (content): Merge conflict in clang/lib/Driver/ToolChains/Clang.cpp
    
      commit ab8abeaf48abb3addcbdb6136b1975b4afa3b37c
      Author: Fangrui Song <i@maskray.me>
      Date:   Wed Apr 13 16:58:00 2022 -0700
    
          [Driver] Change CLANG_ENABLE_OPAQUE_POINTERS_INTERNAL to affect driver default instead of cc1 default
    
          The current cc1 CLANG_ENABLE_OPAQUE_POINTERS=on default difference is not ideal
          in that people contribute %clang_cc1 tests may assume the default ON behavior,
          which will cause failures on systems set to OFF.
    
          cc1 option default dependent on CMake options should be used prudently
          (generally avoided). We prefer to limit target differences to Driver.
    
          Change the CLANG_ENABLE_OPAQUE_POINTERS_INTERNAL mechanism introduced in D123122
          to use a driver default instead. This is similar to the mechanism used for the
          -flegacy-pass-manager transition to new PM transition.
    
          Reviewed By: #opaque-pointers, rsmith, aeubanks
    
          Differential Revision: https://reviews.llvm.org/D123744
    
    * Conflict resolution
    
    Co-authored-by: iclsrc <ia.compiler.tools.git@intel.com>
    Co-authored-by: Soumi Manna <soumi.manna@intel.com>

--- clang/lib/Driver/ToolChains/Clang.cpp
+++ clang/lib/Driver/ToolChains/Clang.cpp
@@ -7772,15 +7772,12 @@ void Clang::ConstructJob(Compilation &C, const JobAction &JA,
                    false))
     CmdArgs.push_back("-fmodules-debuginfo");
 
-<<<<<<< HEAD
 #ifdef INTEL_CUSTOMIZATION
   Args.AddLastArg(CmdArgs, options::OPT_flegacy_pass_manager,
                   options::OPT_fno_legacy_pass_manager);
 #endif // INTEL_CUSTOMIZATION
-=======
   if (!CLANG_ENABLE_OPAQUE_POINTERS_INTERNAL)
     CmdArgs.push_back("-no-opaque-pointers");
->>>>>>> ab8abeaf48abb3addcbdb6136b1975b4afa3b37c
 
   ObjCRuntime Runtime = AddObjCRuntimeArgs(Args, Inputs, CmdArgs, rewriteKind);
   RenderObjCOptions(TC, D, RawTriple, Args, Runtime, rewriteKind != RK_None,
