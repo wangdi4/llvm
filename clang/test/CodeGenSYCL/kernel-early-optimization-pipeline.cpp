36f0cc8f92c1ac89c0c6d6278863f371aff008e2
Andy Kaylor <andrew.kaylor@intel.com>
2022-04-20 17:49:11 -0700

    [New pass manager] Fix a clang test that expects the legacy pass manager (#4521)
    This test has run lines for both the new and legacy pass manager, but
    the legacy pass manager run lines were assuming that the legacy pass
    manager would be the default. The test fails when it isn't.

--- clang/test/CodeGenSYCL/kernel-early-optimization-pipeline.cpp
+++ clang/test/CodeGenSYCL/kernel-early-optimization-pipeline.cpp
@@ -3,14 +3,14 @@
 //
 // RUN: %clang_cc1 -O2 -fsycl-is-device -triple spir64-unknown-unknown %s -flegacy-pass-manager -mllvm -debug-pass=Structure -emit-llvm -o /dev/null 2>&1 | FileCheck %s --check-prefix=CHECK-EARLYOPT
 // INTEL_CUSTOMIZATION
-// RUN: %clangxx -fsycl -target x86_64 -O2 -fsycl-device-only %s -mllvm -debug-pass=Structure -emit-llvm -o - 2>&1 | FileCheck %s --check-prefixes=CHECK-EARLYOPT --implicit-check-not "Transform sin and cos calls"
+// RUN: %clangxx -fsycl -target x86_64 -O2 -fsycl-device-only %s -flegacy-pass-manager -mllvm -debug-pass=Structure -emit-llvm -o - 2>&1 | FileCheck %s --check-prefixes=CHECK-EARLYOPT --implicit-check-not "Transform sin and cos calls"
 // end INTEL_CUSTOMIZATION
 // CHECK-EARLYOPT: Combine redundant instructions
 // CHECK-EARLYOPT: Move SYCL printf literal arguments to constant address space
 //
 // RUN: %clang_cc1 -O2 -fsycl-is-device -triple spir64-unknown-unknown %s -flegacy-pass-manager -mllvm -debug-pass=Structure -emit-llvm -fno-sycl-early-optimizations -o /dev/null 2>&1 | FileCheck %s --check-prefix=CHECK-NOEARLYOPT
 // INTEL_CUSTOMIZATION
-// RUN: %clangxx -fsycl -target x86_64 -O2 -fsycl -fsycl-device-only -fno-sycl-early-optimizations %s -mllvm -debug-pass=Structure -emit-llvm -o - 2>&1 | FileCheck %s --check-prefix=CHECK-NOEARLYOPT --implicit-check-not "Transform sin and cos calls"
+// RUN: %clangxx -fsycl -target x86_64 -O2 -fsycl -fsycl-device-only -fno-sycl-early-optimizations %s -flegacy-pass-manager -mllvm -debug-pass=Structure -emit-llvm -o - 2>&1 | FileCheck %s --check-prefix=CHECK-NOEARLYOPT --implicit-check-not "Transform sin and cos calls"
 // end INTEL_CUSTOMIZATION
 // CHECK-NOEARLYOPT-NOT: Combine redundant instructions
 // CHECK-NOEARLYOPT: Move SYCL printf literal arguments to constant address space
@@ -28,6 +28,6 @@
 //
 // INTEL_CUSTOMIZATION
 // Additionally check regular C++:
-// RUN: %clang_cc1 -O2 -triple x86_64 %s -mllvm -debug-pass=Structure -emit-llvm -o - 2>&1 | FileCheck %s --check-prefix=CHECK-CPP
+// RUN: %clang_cc1 -O2 -triple x86_64 %s -flegacy-pass-manager -mllvm -debug-pass=Structure -emit-llvm -o - 2>&1 | FileCheck %s --check-prefix=CHECK-CPP
 // CHECK-CPP: Transform sin and cos calls
 // end INTEL_CUSTOMIZATION
