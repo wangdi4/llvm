e6de40105054851eb4dd2272f8202910ea377551
pvbattin <prasad.v.battini@intel.com>
2022-04-25 19:37:30 -0400

    [CodeAlign] (CMPLRLLVM36609) Inhibit CodeAlign for opaque pointers to avoid crashing (#4652)
    * [CodeAlign] (CMPLRLLVM36609) Inhibit CodeAlign for opaque pointers to avoid crashing
    
    * On branch xmainv4
     Changes to be committed:
    	modified:   llvm/Intel_OptionalComponents/DTrans/lib/Transforms/CodeAlign.cpp

--- llvm/Intel_OptionalComponents/DTrans/lib/Transforms/CodeAlign.cpp
+++ llvm/Intel_OptionalComponents/DTrans/lib/Transforms/CodeAlign.cpp
@@ -1,6 +1,6 @@
 //===---------------- CodeAlign.cpp - DTransCodeAlignPass -----------------===//
 //
-// Copyright (C) 2021-2021 Intel Corporation. All rights reserved.
+// Copyright (C) 2021-2022 Intel Corporation. All rights reserved.
 //
 // The information and source code contained herein is the exclusive property
 // of Intel Corporation and may not be disclosed, examined or reproduced in
@@ -426,6 +426,10 @@ bool CodeAlignPass::runImpl(Module &M, WholeProgramInfo &WPInfo) {
   if (!WPInfo.isWholeProgramSafe() || !WPInfo.isAdvancedOptEnabled(TTIAVX2)) {
     LLVM_DEBUG(
         dbgs() << "DTRANS Code Align: inhibited -- not whole program safe");
+    return false;
+  }
+  if (!M.getContext().supportsTypedPointers()) {
+    LLVM_DEBUG(dbgs() << "DTRANS Code Align: inhibited -- opaque pointers");
     return false;
   }
 
