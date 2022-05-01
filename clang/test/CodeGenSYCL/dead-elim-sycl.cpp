537456847a38bb3f2685b78d84bb74b9af4c0701
HaohaiWen <haohai.wen@intel.com>
2022-05-01 14:17:55 +0800

    Add -fno-legacy-pass-manager explicitly to sycl test. (#4784)

--- clang/test/CodeGenSYCL/dead-elim-sycl.cpp
+++ clang/test/CodeGenSYCL/dead-elim-sycl.cpp
@@ -1,8 +1,10 @@
 // Verify that Dead Arguments Elimination for SYCL kernels is/is not added to the PM.
 
-// RUN: %clang -cc1 -triple spir64-unknown-unknown  -fsycl-is-device  -emit-llvm-bc  -O1 -fdebug-pass-manager -fenable-sycl-dae -o /dev/null -x c++ < %s 2>&1 | FileCheck %s
-// RUN: %clang -cc1 -triple spir64-unknown-unknown  -fsycl-is-device  -emit-llvm-bc  -O0 -fdebug-pass-manager  -o /dev/null -x c++ < %s 2>&1 | FileCheck %s --check-prefix DISABLE
-// RUN: %clang -cc1 -triple spir64-unknown-unknown  -fsycl-is-device  -emit-llvm-bc  -O1 -fdebug-pass-manager -fenable-sycl-dae -disable-llvm-passes -o /dev/null -x c++ < %s 2>&1 | FileCheck %s --check-prefix DISABLE
+// INTEL_CUSTOMIZATION
+// RUN: %clang -cc1 -triple spir64-unknown-unknown -fno-legacy-pass-manager -fsycl-is-device  -emit-llvm-bc  -O1 -fdebug-pass-manager -fenable-sycl-dae -o /dev/null -x c++ < %s 2>&1 | FileCheck %s
+// RUN: %clang -cc1 -triple spir64-unknown-unknown -fno-legacy-pass-manager -fsycl-is-device  -emit-llvm-bc  -O0 -fdebug-pass-manager  -o /dev/null -x c++ < %s 2>&1 | FileCheck %s --check-prefix DISABLE
+// RUN: %clang -cc1 -triple spir64-unknown-unknown -fno-legacy-pass-manager -fsycl-is-device  -emit-llvm-bc  -O1 -fdebug-pass-manager -fenable-sycl-dae -disable-llvm-passes -o /dev/null -x c++ < %s 2>&1 | FileCheck %s --check-prefix DISABLE
+// end INTEL_CUSTOMIZATION
 
 
 // CHECK: Running pass: DeadArgumentEliminationSYCLPass on [module]
