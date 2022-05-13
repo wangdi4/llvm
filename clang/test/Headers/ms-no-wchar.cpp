4f8f51a50155ceb4bf14f217bdce573c2de6b87f
Zahira Ammarguellat <zahira.ammarguellat@intel.com>
2022-05-13 13:29:40 -0400

    Fix for https://jira.devtools.intel.com/browse/CMPLRLLVM-37604 (#5020)

--- clang/test/Headers/ms-no-wchar.cpp
+++ clang/test/Headers/ms-no-wchar.cpp
@@ -1,4 +1,6 @@
-// RUN: %clang_cc1 -fsyntax-only -triple x86_64-pc-windows-msvc -fms-compatibility-version=17.00 -fno-wchar %s
+// INTEL RUN: %clang_cc1 -fsyntax-only -triple x86_64-pc-windows-msvc \
+// INTEL RUN: -fms-compatibility-version=17.00 -fno-wchar \
+// INTEL RUN: -isystem %S/Inputs/intel %s
 // MSVC defines wchar_t instead of using the builtin if /Zc:wchar_t- is passed
 
 #include <stddef.h>
