c2a895957bbe7a9cea607baabbc7698d65555f42
iclsrc <iclsrc@intel.com>
2022-05-20 12:50:10 -0700

    Resolve of merge (build) 6d2bfb00dcd9 Merge from 'sycl' to 'sycl-web' 'sycl-web' -> 'xmain-web' (1 commits) (#5145)
    * Resolve of merge (build) 6d2bfb00dcd9 Merge from 'sycl' to 'sycl-web' 'sycl-web' -> 'xmain-web' (1 commits)
    
      commit 6d2bfb00dcd99d50c9d45fed9c60031f714eddc1
      Merge: ca7ef74689a0 a37c10b062a0
      Author: iclsrc <ia.compiler.tools.git@intel.com>
      Date:   Fri May 20 03:05:08 2022 -0700
    
          Merge from 'sycl' to 'sycl-web'
    
    * [SYCL] Remove unnecessary customization for pi_sampler in the L0 plugin
    
    Co-authored-by: iclsrc <ia.compiler.tools.git@intel.com>
    Co-authored-by: Artur Gainullin <artur.gainullin@intel.com>

--- sycl/plugins/level_zero/pi_level_zero.hpp
+++ sycl/plugins/level_zero/pi_level_zero.hpp
@@ -1,21 +1,4 @@
 //===--------- pi_level_zero.hpp - Level Zero Plugin ----------------------===//
-// INTEL_CUSTOMIZATION
-//
-// INTEL CONFIDENTIAL
-//
-// Modifications, Copyright (C) 2021 Intel Corporation
-//
-// This software and the related documents are Intel copyrighted materials, and
-// your use of them is governed by the express license under which they were
-// provided to you ("License"). Unless the License provides otherwise, you may not
-// use, modify, copy, publish, distribute, disclose or transmit this software or
-// the related documents without Intel's prior written permission.
-//
-// This software and the related documents are provided as is, with no express
-// or implied warranties, other than those that are expressly stated in the
-// License.
-//
-// end INTEL_CUSTOMIZATION
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
@@ -1481,16 +1464,11 @@ struct _pi_kernel : _pi_object {
   ZeCache<std::string> ZeKernelName;
 };
 
-#if INTEL_CUSTOMIZATION
-struct _pi_sampler {
-  _pi_sampler(ze_sampler_handle_t Sampler) : ZeSampler{Sampler}, RefCount{1} {}
+struct _pi_sampler : _pi_object {
+  _pi_sampler(ze_sampler_handle_t Sampler) : ZeSampler{Sampler} {}
 
   // Level Zero sampler handle.
   ze_sampler_handle_t ZeSampler;
-
-  // TODO: base PI sampler on _pi_object when the above limitation is removed.
-  std::atomic<pi_uint32> RefCount;
 };
-#endif // INTEL_CUSTOMIZATION
 
 #endif // PI_LEVEL_ZERO_HPP
