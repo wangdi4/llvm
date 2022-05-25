carbon-language/carbon-lang

commit 802e15c576997f76bffb4c08b6f81d6c79c320e0

[SLP] Cluster ordering for loads
Given a load without a better order, this patch partially sorts the
elements to form clusters of adjacent elements in memory. These clusters
can potentially be loaded in fewer loads, meaning less overall shuffling
(for example loading v4i8 clusters of a v16i8 as a single f32 loads, as
opposed to multiple independent bytes loads and inserts).

Differential Revision: https://reviews.llvm.org/D122145

llvm/lib/Transforms/Vectorize/SLPVectorizer.cpp
@@ -929,6 +929,9 @@ public:
   /// ExtractElement, ExtractValue), which can be part of the graph.
   Optional<OrdersType> findReusedOrderedScalars(const TreeEntry &TE);
 
+  /// Sort loads into increasing pointers offsets to allow greater clustering.
+  Optional<OrdersType> findPartiallyOrderedLoads(const TreeEntry &TE);
+
   /// Gets reordering data for the given tree entry. If the entry is vectorized
   /// - just return ReorderIndices, otherwise check if the scalars can be
   /// reordered and return the most optimal order.
@@ -3441,6 +3444,93 @@ BoUpSLP::findReusedOrderedScalars(const BoUpSLP::TreeEntry &TE) {
   return None;
 }
 
+bool clusterSortPtrAccesses(ArrayRef<Value *> VL, Type *ElemTy,
+                            const DataLayout &DL, ScalarEvolution &SE,
+                            SmallVectorImpl<unsigned> &SortedIndices) {
+  assert(llvm::all_of(
+             VL, [](const Value *V) { return V->getType()->isPointerTy(); }) &&
+         "Expected list of pointer operands.");
+  // Map from bases to a vector of (Ptr, Offset, OrigIdx), which we insert each
+  // Ptr into, sort and return the sorted indices with values next to one
+  // another.
+  MapVector<Value *, SmallVector<std::tuple<Value *, int, unsigned>>> Bases;
+  Bases[VL[0]].push_back(std::make_tuple(VL[0], 0U, 0U));
+
+  unsigned Cnt = 1;
+  for (Value *Ptr : VL.drop_front()) {
+    bool Found = any_of(Bases, [&](auto &Base) {
+      Optional<int> Diff =
+          getPointersDiff(ElemTy, Base.first, ElemTy, Ptr, DL, SE,
+                          /*StrictCheck=*/true);
+      if (!Diff)
+        return false;
+
+      Base.second.emplace_back(Ptr, *Diff, Cnt++);
+      return true;
+    });
+
+    if (!Found) {
+      // If we haven't found enough to usefully cluster, return early.
+      if (Bases.size() > VL.size() / 2 - 1)
+        return false;
+
+      // Not found already - add a new Base
+      Bases[Ptr].emplace_back(Ptr, 0, Cnt++);
+    }
+  }
+
+  // For each of the bases sort the pointers by Offset and check if any of the
+  // base become consecutively allocated.
+  bool AnyConsecutive = false;
+  for (auto &Base : Bases) {
+    auto &Vec = Base.second;
+    if (Vec.size() > 1) {
+      llvm::stable_sort(Vec, [](const std::tuple<Value *, int, unsigned> &X,
+                                const std::tuple<Value *, int, unsigned> &Y) {
+        return std::get<1>(X) < std::get<1>(Y);
+      });
+      int InitialOffset = std::get<1>(Vec[0]);
+      AnyConsecutive |= all_of(enumerate(Vec), [InitialOffset](auto &P) {
+        return std::get<1>(P.value()) == int(P.index()) + InitialOffset;
+      });
+    }
+  }
+
+  // Fill SortedIndices array only if it looks worth-while to sort the ptrs.
+  SortedIndices.clear();
+  if (!AnyConsecutive)
+    return false;
+
+  for (auto &Base : Bases) {
+    for (auto &T : Base.second)
+      SortedIndices.push_back(std::get<2>(T));
+  }
+
+  assert(SortedIndices.size() == VL.size() &&
+         "Expected SortedIndices to be the size of VL");
+  return true;
+}
+
+Optional<BoUpSLP::OrdersType>
+BoUpSLP::findPartiallyOrderedLoads(const BoUpSLP::TreeEntry &TE) {
+  assert(TE.State == TreeEntry::NeedToGather && "Expected gather node only.");
+  Type *ScalarTy = TE.Scalars[0]->getType();
+
+  SmallVector<Value *> Ptrs;
+  Ptrs.reserve(TE.Scalars.size());
+  for (Value *V : TE.Scalars) {
+    auto *L = dyn_cast<LoadInst>(V);
+    if (!L || !L->isSimple())
+      return None;
+    Ptrs.push_back(L->getPointerOperand());
+  }
+
+  BoUpSLP::OrdersType Order;
+  if (clusterSortPtrAccesses(Ptrs, ScalarTy, *DL, *SE, Order))
+    return Order;
+  return None;
+}
+
 Optional<BoUpSLP::OrdersType> BoUpSLP::getReorderingData(const TreeEntry &TE,
                                                          bool TopToBottom) {
   // No need to reorder if need to shuffle reuses, still need to shuffle the
@@ -3481,6 +3571,9 @@ Optional<BoUpSLP::OrdersType> BoUpSLP::getReorderingData(const TreeEntry &TE,
     }
     if (Optional<OrdersType> CurrentOrder = findReusedOrderedScalars(TE))
       return CurrentOrder;
+    if (TE.Scalars.size() >= 4)
+      if (Optional<OrdersType> Order = findPartiallyOrderedLoads(TE))
+        return Order;
   }
   return None;
 }

llvm/test/Transforms/SLPVectorizer/X86/split-load8_2-unord.ll
@@ -110,12 +110,12 @@ define dso_local void @test_unordered_splits(%struct.S* nocapture %p) local_unna
 ; CHECK-NEXT:    [[TMP1:%.*]] = load <4 x i32>, <4 x i32>* [[TMP0]], align 4
 ; CHECK-NEXT:    [[TMP2:%.*]] = bitcast i32* [[G20]] to <4 x i32>*
 ; CHECK-NEXT:    [[TMP3:%.*]] = load <4 x i32>, <4 x i32>* [[TMP2]], align 4
-; CHECK-NEXT:    [[TMP4:%.*]] = shufflevector <4 x i32> [[TMP1]], <4 x i32> poison, <8 x i32> <i32 1, i32 0, i32 2, i32 3, i32 undef, i32 undef, i32 undef, i32 undef>
-; CHECK-NEXT:    [[TMP5:%.*]] = shufflevector <8 x i32> poison, <8 x i32> [[TMP4]], <8 x i32> <i32 8, i32 9, i32 10, i32 11, i32 4, i32 5, i32 6, i32 7>
-; CHECK-NEXT:    [[TMP6:%.*]] = shufflevector <4 x i32> [[TMP3]], <4 x i32> poison, <8 x i32> <i32 3, i32 1, i32 2, i32 0, i32 undef, i32 undef, i32 undef, i32 undef>
-; CHECK-NEXT:    [[TMP7:%.*]] = shufflevector <8 x i32> [[TMP5]], <8 x i32> [[TMP6]], <8 x i32> <i32 0, i32 1, i32 2, i32 3, i32 8, i32 9, i32 10, i32 11>
-; CHECK-NEXT:    [[TMP8:%.*]] = bitcast i32* [[ARRAYIDX2]] to <8 x i32>*
-; CHECK-NEXT:    store <8 x i32> [[TMP7]], <8 x i32>* [[TMP8]], align 4
+; CHECK-NEXT:    [[TMP4:%.*]] = shufflevector <4 x i32> [[TMP1]], <4 x i32> poison, <8 x i32> <i32 0, i32 1, i32 2, i32 3, i32 undef, i32 undef, i32 undef, i32 undef>
+; CHECK-NEXT:    [[TMP5:%.*]] = shufflevector <4 x i32> [[TMP3]], <4 x i32> poison, <8 x i32> <i32 0, i32 1, i32 2, i32 3, i32 undef, i32 undef, i32 undef, i32 undef>
+; CHECK-NEXT:    [[TMP6:%.*]] = shufflevector <8 x i32> [[TMP4]], <8 x i32> [[TMP5]], <8 x i32> <i32 0, i32 1, i32 2, i32 3, i32 8, i32 9, i32 10, i32 11>
+; CHECK-NEXT:    [[SHUFFLE:%.*]] = shufflevector <8 x i32> [[TMP6]], <8 x i32> poison, <8 x i32> <i32 1, i32 0, i32 2, i32 3, i32 7, i32 5, i32 6, i32 4>
+; CHECK-NEXT:    [[TMP7:%.*]] = bitcast i32* [[ARRAYIDX2]] to <8 x i32>*
+; CHECK-NEXT:    store <8 x i32> [[SHUFFLE]], <8 x i32>* [[TMP7]], align 4
 ; CHECK-NEXT:    ret void
 ;
 entry:

wangdi4/llvm

commit a73052e5801c9072ddbc21740e936e7f4435be99

[SLP] Enable reordering for split loads. (#5235)
This was never used (and missing) functionality of split-load
feature guarded by assertion which started to hit
after "802e15c57699 [SLP] Cluster ordering for loads"

Fixes CMPLRLLVM-37769.

llvm/lib/Transforms/Vectorize/SLPVectorizer.cpp
@@ -10094,8 +10094,6 @@ Value *BoUpSLP::vectorizeTree(TreeEntry *E) {
 #if INTEL_CUSTOMIZATION
       }
 
-      assert(E->ReorderIndices.empty() && "Unsupported split-load case.");
-
       auto createVecLoad = [&](TreeEntry *E, Value *VL0, VectorType *VecLdTy) {
         auto *LI = cast<LoadInst>(VL0);
         unsigned AS = LI->getPointerAddressSpace();
@@ -10202,6 +10200,17 @@ Value *BoUpSLP::vectorizeTree(TreeEntry *E) {
           std::iota(ShuffleMask.begin(), ShuffleMask.end(), 0);
         }
 
+        // If we are about to generate finalizing shuffle
+        // apply reording idices directly to the shuffle mask
+        // rather than generate another one afterwards.
+        if (WorkList.empty() && !E->ReorderIndices.empty()) {
+          assert(ShuffleMask.size() == E->ReorderIndices.size() &&
+                 "Reordering indices mask does not match vector size");
+          SmallVector<int> Mask;
+          inversePermutation(E->ReorderIndices, Mask);
+          addMask(ShuffleMask, Mask);
+        }
+
         Value *Shuffle = Builder.CreateShuffleVector(Vec0, Vec1, ShuffleMask,
                                                      "SplitLoadShuffle");
         if (WorkList.empty()) {
@@ -10211,17 +10220,8 @@ Value *BoUpSLP::vectorizeTree(TreeEntry *E) {
         VecOrder0.clear();
         WorkList.emplace_front(Shuffle, VecOrder0);
       }
-      // When split load support is enabled we may need to shuffle resulting
-      // vector.
-      if (NeedToShuffleReuses) {
-        RetValue = Builder.CreateShuffleVector(RetValue, E->ReuseShuffleIndices,
-                                               "SplitLoadReuseShuffle");
-        if (auto *I = dyn_cast<Instruction>(RetValue)) {
-          GatherShuffleSeq.insert(I);
-          CSEBlocks.insert(I->getParent());
-        }
-      }
-
+      ShuffleBuilder.addMask(E->ReuseShuffleIndices);
+      RetValue = ShuffleBuilder.finalize(RetValue);
       E->VectorizedValue = RetValue;
       return RetValue;
 #endif // INTEL_CUSTOMIZATION

llvm/test/Transforms/Intel_VPO/Vecopt/slp/split_load_reorder.ll
@@ -34,14 +34,14 @@ define void @test(float* nocapture readonly %p1, float* nocapture readonly %p2) 
 ; CHECK-NEXT:    [[TMP1:%.*]] = load <2 x float>, <2 x float>* [[TMP0]], align 4
 ; CHECK-NEXT:    [[TMP2:%.*]] = bitcast float* [[I49]] to <2 x float>*
 ; CHECK-NEXT:    [[TMP3:%.*]] = load <2 x float>, <2 x float>* [[TMP2]], align 4
-; CHECK-NEXT:    [[SPLITLOADSHUFFLE:%.*]] = shufflevector <2 x float> [[TMP1]], <2 x float> [[TMP3]], <4 x i32> <i32 1, i32 0, i32 3, i32 2>
+; CHECK-NEXT:    [[SPLITLOADSHUFFLE:%.*]] = shufflevector <2 x float> [[TMP1]], <2 x float> [[TMP3]], <4 x i32> <i32 0, i32 1, i32 2, i32 3>
 ; CHECK-NEXT:    br label [[BB63:%.*]]
 ; CHECK:       bb53:
 ; CHECK-NEXT:    [[I54:%.*]] = getelementptr float, float* null, i32 0
 ; CHECK-NEXT:    [[I56:%.*]] = getelementptr float, float* [[P2:%.*]], i32 1
 ; CHECK-NEXT:    br i1 undef, label [[BB146:%.*]], label [[BB136:%.*]]
 ; CHECK:       bb58:
-; CHECK-NEXT:    [[TMP4:%.*]] = phi <4 x float> [ [[TMP19:%.*]], [[BB147:%.*]] ], [ [[TMP25:%.*]], [[BB150:%.*]] ], [ [[TMP13:%.*]], [[BB139:%.*]] ]
+; CHECK-NEXT:    [[TMP4:%.*]] = phi <4 x float> [ [[TMP18:%.*]], [[BB147:%.*]] ], [ [[TMP23:%.*]], [[BB150:%.*]] ], [ [[TMP12:%.*]], [[BB139:%.*]] ]
 ; CHECK-NEXT:    br label [[BB63]]
 ; CHECK:       bb63:
 ; CHECK-NEXT:    [[TMP5:%.*]] = phi <4 x float> [ [[TMP4]], [[BB58:%.*]] ], [ [[SPLITLOADSHUFFLE]], [[BB45]] ]
@@ -53,28 +53,26 @@ define void @test(float* nocapture readonly %p1, float* nocapture readonly %p2) 
 ; CHECK:       bb139:
 ; CHECK-NEXT:    [[TMP8:%.*]] = bitcast float* [[I56]] to <2 x float>*
 ; CHECK-NEXT:    [[TMP9:%.*]] = load <2 x float>, <2 x float>* [[TMP8]], align 4
-; CHECK-NEXT:    [[TMP10:%.*]] = shufflevector <2 x float> [[TMP7]], <2 x float> poison, <4 x i32> <i32 1, i32 0, i32 undef, i32 undef>
-; CHECK-NEXT:    [[TMP11:%.*]] = shufflevector <4 x float> poison, <4 x float> [[TMP10]], <4 x i32> <i32 4, i32 5, i32 2, i32 3>
-; CHECK-NEXT:    [[TMP12:%.*]] = shufflevector <2 x float> [[TMP9]], <2 x float> poison, <4 x i32> <i32 1, i32 0, i32 undef, i32 undef>
-; CHECK-NEXT:    [[TMP13]] = shufflevector <4 x float> [[TMP11]], <4 x float> [[TMP12]], <4 x i32> <i32 0, i32 1, i32 4, i32 5>
+; CHECK-NEXT:    [[TMP10:%.*]] = shufflevector <2 x float> [[TMP7]], <2 x float> poison, <4 x i32> <i32 0, i32 1, i32 undef, i32 undef>
+; CHECK-NEXT:    [[TMP11:%.*]] = shufflevector <2 x float> [[TMP9]], <2 x float> poison, <4 x i32> <i32 0, i32 1, i32 undef, i32 undef>
+; CHECK-NEXT:    [[TMP12]] = shufflevector <4 x float> [[TMP10]], <4 x float> [[TMP11]], <4 x i32> <i32 0, i32 1, i32 4, i32 5>
 ; CHECK-NEXT:    br i1 undef, label [[BB58]], label [[BB146]]
 ; CHECK:       bb146:
 ; CHECK-NEXT:    br i1 undef, label [[BB159:%.*]], label [[BB147]]
 ; CHECK:       bb147:
-; CHECK-NEXT:    [[TMP14:%.*]] = bitcast float* [[I54]] to <2 x float>*
-; CHECK-NEXT:    [[TMP15:%.*]] = load <2 x float>, <2 x float>* [[TMP14]], align 4
-; CHECK-NEXT:    [[TMP16:%.*]] = extractelement <2 x float> [[TMP15]], i32 1
-; CHECK-NEXT:    [[TMP17:%.*]] = insertelement <4 x float> poison, float [[TMP16]], i32 0
-; CHECK-NEXT:    [[TMP18:%.*]] = extractelement <2 x float> [[TMP15]], i32 0
-; CHECK-NEXT:    [[TMP19]] = insertelement <4 x float> [[TMP17]], float [[TMP18]], i32 1
+; CHECK-NEXT:    [[TMP13:%.*]] = bitcast float* [[I54]] to <2 x float>*
+; CHECK-NEXT:    [[TMP14:%.*]] = load <2 x float>, <2 x float>* [[TMP13]], align 4
+; CHECK-NEXT:    [[TMP15:%.*]] = extractelement <2 x float> [[TMP14]], i32 0
+; CHECK-NEXT:    [[TMP16:%.*]] = insertelement <4 x float> poison, float [[TMP15]], i32 0
+; CHECK-NEXT:    [[TMP17:%.*]] = extractelement <2 x float> [[TMP14]], i32 1
+; CHECK-NEXT:    [[TMP18]] = insertelement <4 x float> [[TMP16]], float [[TMP17]], i32 1
 ; CHECK-NEXT:    br i1 undef, label [[BB150]], label [[BB58]]
 ; CHECK:       bb150:
-; CHECK-NEXT:    [[TMP20:%.*]] = bitcast float* [[I56]] to <2 x float>*
-; CHECK-NEXT:    [[TMP21:%.*]] = load <2 x float>, <2 x float>* [[TMP20]], align 4
-; CHECK-NEXT:    [[TMP22:%.*]] = shufflevector <2 x float> [[TMP15]], <2 x float> poison, <4 x i32> <i32 1, i32 0, i32 undef, i32 undef>
-; CHECK-NEXT:    [[TMP23:%.*]] = shufflevector <4 x float> poison, <4 x float> [[TMP22]], <4 x i32> <i32 4, i32 5, i32 2, i32 3>
-; CHECK-NEXT:    [[TMP24:%.*]] = shufflevector <2 x float> [[TMP21]], <2 x float> poison, <4 x i32> <i32 1, i32 0, i32 undef, i32 undef>
-; CHECK-NEXT:    [[TMP25]] = shufflevector <4 x float> [[TMP23]], <4 x float> [[TMP24]], <4 x i32> <i32 0, i32 1, i32 4, i32 5>
+; CHECK-NEXT:    [[TMP19:%.*]] = bitcast float* [[I56]] to <2 x float>*
+; CHECK-NEXT:    [[TMP20:%.*]] = load <2 x float>, <2 x float>* [[TMP19]], align 4
+; CHECK-NEXT:    [[TMP21:%.*]] = shufflevector <2 x float> [[TMP14]], <2 x float> poison, <4 x i32> <i32 0, i32 1, i32 undef, i32 undef>
+; CHECK-NEXT:    [[TMP22:%.*]] = shufflevector <2 x float> [[TMP20]], <2 x float> poison, <4 x i32> <i32 0, i32 1, i32 undef, i32 undef>
+; CHECK-NEXT:    [[TMP23]] = shufflevector <4 x float> [[TMP21]], <4 x float> [[TMP22]], <4 x i32> <i32 0, i32 1, i32 4, i32 5>
 ; CHECK-NEXT:    br i1 undef, label [[BB58]], label [[BB159]]
 ; CHECK:       bb159:
 ; CHECK-NEXT:    br i1 undef, label [[BB124:%.*]], label [[BB39]]