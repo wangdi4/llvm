98635effede3b1c506c46e1eb0513172dbff7ed4
Ernesto Su <ernesto.su@intel.com>
2022-05-25 09:10:34 -0700

    [Paropt] Add methods to base WRegionNode class to query loop-related info. (#5204)
    New methods are added to the base WRegionNode class needed by the
    implementation to support TILE:
    
    1. getIsOmpLoopOrLoopTransform(), which returns true for a loop-type construct
       or a loop-tranform construct such as TILE.
    2. virtual getOmpLoopDepth(). This method was defined in derived WRN classes
       but not in the base class, preventing it from being called from the base
       class directly.

--- llvm/include/llvm/Analysis/VPO/WRegionInfo/WRegionNode.h
+++ llvm/include/llvm/Analysis/VPO/WRegionInfo/WRegionNode.h
@@ -637,6 +637,7 @@ public:
   virtual WRNLoopInfo &getWRNLoopInfo()         {WRNERROR("WRNLoopInfo");     }
   virtual const WRNLoopInfo &getWRNLoopInfo() const
                                                 {WRNERROR("WRNLoopInfo");     }
+  virtual int getOmpLoopDepth()           const {WRNERROR("Loop Depth");      }
 
   // Task
 
@@ -874,6 +875,9 @@ public:
   bool getIsOmpLoop()      const { return Attributes & WRNIsOmpLoop; }
   bool getIsOmpLoopTransform() const {
     return Attributes & WRNIsOmpLoopTransform;
+  }
+  bool getIsOmpLoopOrLoopTransform() const {
+    return Attributes & (WRNIsOmpLoop | WRNIsOmpLoopTransform);
   }
   bool getIsSections()     const { return Attributes & WRNIsSections; }
   bool getIsTarget()       const { return Attributes & WRNIsTarget; }
