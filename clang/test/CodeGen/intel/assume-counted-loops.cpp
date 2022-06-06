a880e4667500f044db71169ef1e083dad974c3fa
Mike Rice <michael.p.rice@intel.com>
2022-06-06 16:22:40 -0700

    Add frontend option for assume-counted-loops (#5429)
    Currently not used, implementation will be a separate change.
    
    See CMPLRLLVM-34813.

---
+++ clang/test/CodeGen/intel/assume-counted-loops.cpp
@@ -0,0 +1,22 @@
+// RUN: %clang_cc1 -fassume-counted-loops -triple x86_64-unknown-linux-gnu \
+// RUN:   -verify -emit-llvm -opaque-pointers %s -o - | FileCheck %s
+
+// RUN: %clang_cc1 -fno-assume-counted-loops -triple x86_64-unknown-linux-gnu \
+// RUN:   -verify -emit-llvm -opaque-pointers %s -o - | FileCheck %s
+
+// expected-no-diagnostics
+
+struct S {
+  int Size;
+  int Increment;
+  int *Elem1;
+  int *Elem2;
+};
+
+// CHECK: define{{.*}}test1
+void test1(S *P, int N) {
+  for (unsigned int I = 0; I < P->Size; I += P->Increment) {
+    P->Elem1[I] = -1;
+    P->Elem2[I] = 0;
+  }
+}
