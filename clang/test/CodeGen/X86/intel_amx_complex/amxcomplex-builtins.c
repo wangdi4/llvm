6b2ef490858307175134121674e4bf622fc4030e
Luo, Yuanke <yuanke.luo@intel.com>
2022-06-07 13:24:27 +0800

    [X86][AMX] Modify new ISA intrinsics name (#5385)
    // INTEL_FEATURE_ISA_AMX_COMPLEX
    Rename amx-complex intrinsics name to follow the naming convention.
    // end INTEL_FEATURE_ISA_AMX_COMPLEX

--- clang/test/CodeGen/X86/intel_amx_complex/amxcomplex-builtins.c
+++ clang/test/CodeGen/X86/intel_amx_complex/amxcomplex-builtins.c
@@ -4,39 +4,39 @@
 
 #include <immintrin.h>
 #include <stddef.h>
+void test_tile_cmmimfp16ps(void) {
+  // CHECK-LABEL: @test_tile_cmmimfp16ps
+  // CHECK: call void @llvm.x86.tcmmimfp16ps(i8 1, i8 2, i8 3)
+  _tile_cmmimfp16ps(1, 2, 3);
+}
+
+void test_tile_cmmrlfp16ps(void) {
+  // CHECK-LABEL: @test_tile_cmmrlfp16ps
+  // CHECK: call void @llvm.x86.tcmmrlfp16ps(i8 1, i8 2, i8 3)
+  _tile_cmmrlfp16ps(1, 2, 3);
+}
+
+void test_tile_conjtcmmimfp16ps(void) {
+  // CHECK-LABEL: @test_tile_conjtcmmimfp16ps
+  // CHECK: call void @llvm.x86.tconjtcmmimfp16ps(i8 1, i8 2, i8 3)
+  _tile_conjtcmmimfp16ps(1, 2, 3);
+}
+
+void test_tile_conjtfp16(void) {
+  // CHECK-LABEL: @test_tile_conjtfp16
+  // CHECK: call void @llvm.x86.tconjtfp16(i8 1, i8 2)
+  _tile_conjtfp16(1, 2);
+}
+
 void test_tile_tcmmimfp16ps(void) {
   // CHECK-LABEL: @test_tile_tcmmimfp16ps
-  // CHECK: call void @llvm.x86.tcmmimfp16ps(i8 1, i8 2, i8 3)
+  // CHECK: call void @llvm.x86.ttcmmimfp16ps(i8 1, i8 2, i8 3)
   _tile_tcmmimfp16ps(1, 2, 3);
 }
 
 void test_tile_tcmmrlfp16ps(void) {
   // CHECK-LABEL: @test_tile_tcmmrlfp16ps
-  // CHECK: call void @llvm.x86.tcmmrlfp16ps(i8 1, i8 2, i8 3)
-  _tile_tcmmrlfp16ps(1, 2, 3);
-}
-
-void test_tile_tconjtcmmimfp16ps(void) {
-  // CHECK-LABEL: @test_tile_tconjtcmmimfp16ps
-  // CHECK: call void @llvm.x86.tconjtcmmimfp16ps(i8 1, i8 2, i8 3)
-  _tile_tconjtcmmimfp16ps(1, 2, 3);
-}
-
-void test_tile_tconjtfp16(void) {
-  // CHECK-LABEL: @test_tile_tconjtfp16
-  // CHECK: call void @llvm.x86.tconjtfp16(i8 1, i8 2)
-  _tile_tconjtfp16(1, 2);
-}
-
-void test_tile_ttcmmimfp16ps(void) {
-  // CHECK-LABEL: @test_tile_ttcmmimfp16ps
-  // CHECK: call void @llvm.x86.ttcmmimfp16ps(i8 1, i8 2, i8 3)
-  _tile_ttcmmimfp16ps(1, 2, 3);
-}
-
-void test_tile_ttcmmrlfp16ps(void) {
-  // CHECK-LABEL: @test_tile_ttcmmrlfp16ps
   // CHECK: call void @llvm.x86.ttcmmrlfp16ps(i8 1, i8 2, i8 3)
-  _tile_ttcmmrlfp16ps(1, 2, 3);
+  _tile_tcmmrlfp16ps(1, 2, 3);
 }
 
