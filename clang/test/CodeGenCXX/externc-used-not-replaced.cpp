c6919b546402497a1466ae0b7ae5caaa4405d529
iclsrc <iclsrc@intel.com>
2022-06-13 02:13:28 -0700

    Resolve of merge (conflict) ded2b513e5a4 Merge from 'xmain-cand' to 'xmain' (xmain-cand@head => xma.. 'xmain' -> 'xmain-web' (1 commits) (#5531)
    * Resolve of merge (conflict) ded2b513e5a4 Merge from 'xmain-cand' to 'xmain' (xmain-cand@head => xma.. 'xmain' -> 'xmain-web' (1 commits)
    
      CONFLICT (content): Merge conflict in clang/test/CodeGenCXX/externc-used-not-replaced.cpp
    
      commit ded2b513e5a4b4ccd73c27dff577d6f1c3a5e664
      Merge: b58e806e4e9f c2f9efd62e2e
      Author: Wen, Haohai <haohai.wen@intel.com>
      Date:   Sun Jun 12 19:48:01 2022 -0700
    
          Merge from 'xmain-cand' to 'xmain' (xmain-cand@head => xmain)
    
    * Resolve conflict
    
    Co-authored-by: iclsrc <ia.compiler.tools.git@intel.com>
    Co-authored-by: Haohai Wen <haohai.wen@intel.com>

--- clang/test/CodeGenCXX/externc-used-not-replaced.cpp
+++ clang/test/CodeGenCXX/externc-used-not-replaced.cpp
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
 // RUN: %clang_cc1 -opaque-pointers -triple x86_64-windows -emit-llvm -o - %s | FileCheck %s
-=======
-// INTEL RUN: %clang_cc1 -opaque-pointers -triple x86_64-windows -emit-llvm -o - %s | FileCheck %s
->>>>>>> ded2b513e5a4b4ccd73c27dff577d6f1c3a5e664
 
 extern "C" {
   const char a __attribute__((used)){};
