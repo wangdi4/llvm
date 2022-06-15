c3c5624a8a19f44aad08381f2b8f25685880c5f9
iclsrc <iclsrc@intel.com>
2022-06-14 18:57:33 -0700

    Resolve of merge (conflict) 5c41b0f42965 [Analysis] Remove getUniqueInstruction (NFC) 'main' -> 'xmain-web' (#5561)
    * Resolve of merge (conflict) 5c41b0f42965 [Analysis] Remove getUniqueInstruction (NFC) 'main' -> 'xmain-web'
    
      CONFLICT (content): Merge conflict in llvm/lib/Analysis/AliasSetTracker.cpp
    
      commit 5c41b0f429652ef50b9378e890f17a1426958e17
      Author: Kazu Hirata <kazu@google.com>
      Date:   Mon Jun 13 14:26:20 2022 -0700
    
          [Analysis] Remove getUniqueInstruction (NFC)
    
          The last use was removed on Apr 7, 2022 in commit
          5cefe7d9f531fe6eac4cd22978861752498b2265.
    
    * Resolve conflict
    
    Co-authored-by: iclsrc <ia.compiler.tools.git@intel.com>
    Co-authored-by: Haohai Wen <haohai.wen@intel.com>

--- llvm/lib/Analysis/AliasSetTracker.cpp
+++ llvm/lib/Analysis/AliasSetTracker.cpp
@@ -279,7 +279,6 @@ bool AliasSet::aliasesUnknownInst(const Instruction *Inst,
   return false;
 }
 
-<<<<<<< HEAD
 #if INTEL_COLLAB
 bool AliasSet::aliases(const AliasSet &AS, AliasAnalysis &AA) const {
   if (AliasAny)
@@ -299,33 +298,7 @@ bool AliasSet::aliases(const AliasSet &AS, AliasAnalysis &AA) const {
 }
 
 #endif // INTEL_COLLAB
-Instruction* AliasSet::getUniqueInstruction() {
-  if (AliasAny)
-    // May have collapses alias set
-    return nullptr;
-  if (begin() != end()) {
-    if (!UnknownInsts.empty())
-      // Another instruction found
-      return nullptr;
-    if (std::next(begin()) != end())
-      // Another instruction found
-      return nullptr;
-    Value *Addr = begin()->getValue();
-    assert(!Addr->user_empty() &&
-           "where's the instruction which added this pointer?");
-    if (std::next(Addr->user_begin()) != Addr->user_end())
-      // Another instruction found -- this is really restrictive
-      // TODO: generalize!
-      return nullptr;
-    return cast<Instruction>(*(Addr->user_begin()));
-  }
-  if (1 != UnknownInsts.size())
-    return nullptr;
-  return cast<Instruction>(UnknownInsts[0]);
-}
 
-=======
->>>>>>> 5c41b0f429652ef50b9378e890f17a1426958e17
 void AliasSetTracker::clear() {
   // Delete all the PointerRec entries.
   for (auto &I : PointerMap)
