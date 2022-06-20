4dc7bd901da1d8ac86eff8acc789a6f71d184bb7
iclsrc <iclsrc@intel.com>
2022-06-19 23:09:29 -0700

    Resolve of merge (conflict) 15c096f39c0f LLVM and SPIRV-LLVM-Translator pulldown (WW26) 'sycl' -> 'xmain-web' (1 commits) (#5642)
    * Resolve of merge (conflict) 15c096f39c0f LLVM and SPIRV-LLVM-Translator pulldown (WW26) 'sycl' -> 'xmain-web' (1 commits)
    
      CONFLICT (content): Merge conflict in clang/test/CodeGen/mangle-wasm-main.c
      CONFLICT (content): Merge conflict in clang/test/CodeGen/mangle-wasm-main-void.c
      CONFLICT (content): Merge conflict in clang/test/CodeGen/mangle-wasm-main-noproto.c
    
      commit 15c096f39c0f12576716b099a0f82705955958a9
      Merge: bf0fc4364d18 8187aea4ad79
      Author: bb-sycl <bb-sycl@intel.com>
      Date:   Fri Jun 17 17:27:21 2022 -0700
    
          LLVM and SPIRV-LLVM-Translator pulldown (WW26)
    
          LLVM: llvm/llvm-project@daf897d
          SPIRV-LLVM-Translator: KhronosGroup/SPIRV-LLVM-Translator@3f5e65d
    
    * Resolve conflict
    
    Co-authored-by: iclsrc <ia.compiler.tools.git@intel.com>
    Co-authored-by: Haohai Wen <haohai.wen@intel.com>

--- clang/test/CodeGen/mangle-wasm-main-void.c
+++ clang/test/CodeGen/mangle-wasm-main-void.c
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
-// INTEL RUN: %clang_cc1 -opaque-pointers -emit-llvm %s -o - -triple=wasm32-unknown-unknown | FileCheck %s
-=======
 // RUN: %clang_cc1 -opaque-pointers -emit-llvm %s -o - -triple=wasm32-unknown-unknown | FileCheck %s
->>>>>>> 15c096f39c0f12576716b099a0f82705955958a9
 
 int main(void) {
   return 0;
