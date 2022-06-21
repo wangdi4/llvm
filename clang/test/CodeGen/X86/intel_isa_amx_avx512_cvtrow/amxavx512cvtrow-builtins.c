82ec94295b517065c7351c0f7fd9a8b1e69c61ea
Luo, Yuanke <yuanke.luo@intel.com>
2022-06-21 21:08:42 +0800

    [X86][AMX] Modify intrinsics name following naming convention (#5523)

--- clang/test/CodeGen/X86/intel_isa_amx_avx512_cvtrow/amxavx512cvtrow-builtins.c
+++ clang/test/CodeGen/X86/intel_isa_amx_avx512_cvtrow/amxavx512cvtrow-builtins.c
@@ -5,62 +5,62 @@
 #include <immintrin.h>
 #include <stddef.h>
 
-__m512 test_tile_tcvtrowd2pse(unsigned int A) {
-  // CHECK-LABEL: @test_tile_tcvtrowd2pse(
+__m512 test_tile_cvtrowd2pse(unsigned int A) {
+  // CHECK-LABEL: @test_tile_cvtrowd2pse(
   // CHECK: call <16 x float> @llvm.x86.tcvtrowd2pse(i8 1, i32 %{{.*}})
-  return _tile_tcvtrowd2pse(1, A);
+  return _tile_cvtrowd2pse(1, A);
 }
 
-__m512 test_tile_tcvtrowd2psi(void) {
-  // CHECK-LABEL: @test_tile_tcvtrowd2psi(
+__m512 test_tile_cvtrowd2psi(void) {
+  // CHECK-LABEL: @test_tile_cvtrowd2psi(
   // CHECK: call <16 x float> @llvm.x86.tcvtrowd2psi(i8 1, i32 127)
-  return _tile_tcvtrowd2psi(1, 127);
+  return _tile_cvtrowd2psi(1, 127);
 }
 
-__m512bh test_tile_tcvtrowps2pbf16he(unsigned int A) {
-  // CHECK-LABEL: @test_tile_tcvtrowps2pbf16he(
+__m512bh test_tile_cvtrowps2pbf16he(unsigned int A) {
+  // CHECK-LABEL: @test_tile_cvtrowps2pbf16he(
   // CHECK: call <32 x i16> @llvm.x86.tcvtrowps2pbf16he(i8 1, i32 %{{.*}})
-  return _tile_tcvtrowps2pbf16he(1, A);
+  return _tile_cvtrowps2pbf16he(1, A);
 }
 
-__m512bh test_tile_tcvtrowps2pbf16hi(void) {
-  // CHECK-LABEL: @test_tile_tcvtrowps2pbf16hi(
+__m512bh test_tile_cvtrowps2pbf16hi(void) {
+  // CHECK-LABEL: @test_tile_cvtrowps2pbf16hi(
   // CHECK: call <32 x i16> @llvm.x86.tcvtrowps2pbf16hi(i8 1, i32 127)
-  return _tile_tcvtrowps2pbf16hi(1, 127);
+  return _tile_cvtrowps2pbf16hi(1, 127);
 }
 
-__m512bh test_tile_tcvtrowps2pbf16le(unsigned int A) {
-  // CHECK-LABEL: @test_tile_tcvtrowps2pbf16le(
+__m512bh test_tile_cvtrowps2pbf16le(unsigned int A) {
+  // CHECK-LABEL: @test_tile_cvtrowps2pbf16le(
   // CHECK: call <32 x i16> @llvm.x86.tcvtrowps2pbf16le(i8 1, i32 %{{.*}})
-  return _tile_tcvtrowps2pbf16le(1, A);
+  return _tile_cvtrowps2pbf16le(1, A);
 }
 
-__m512bh test_tile_tcvtrowps2pbf16li(void) {
-  // CHECK-LABEL: @test_tile_tcvtrowps2pbf16li(
+__m512bh test_tile_cvtrowps2pbf16li(void) {
+  // CHECK-LABEL: @test_tile_cvtrowps2pbf16li(
   // CHECK: call <32 x i16> @llvm.x86.tcvtrowps2pbf16li(i8 1, i32 127)
-  return _tile_tcvtrowps2pbf16li(1, 127);
+  return _tile_cvtrowps2pbf16li(1, 127);
 }
 
-__m512h test_tile_tcvtrowps2phhe(unsigned int A) {
-  // CHECK-LABEL: @test_tile_tcvtrowps2phhe(
+__m512h test_tile_cvtrowps2phhe(unsigned int A) {
+  // CHECK-LABEL: @test_tile_cvtrowps2phhe(
   // CHECK: call <32 x half> @llvm.x86.tcvtrowps2phhe(i8 1, i32 %{{.*}})
-  return _tile_tcvtrowps2phhe(1, A);
+  return _tile_cvtrowps2phhe(1, A);
 }
 
-__m512h test_tile_tcvtrowps2phhi(void) {
-  // CHECK-LABEL: @test_tile_tcvtrowps2phhi(
+__m512h test_tile_cvtrowps2phhi(void) {
+  // CHECK-LABEL: @test_tile_cvtrowps2phhi(
   // CHECK: call <32 x half> @llvm.x86.tcvtrowps2phhi(i8 1, i32 127)
-  return _tile_tcvtrowps2phhi(1, 127);
+  return _tile_cvtrowps2phhi(1, 127);
 }
 
-__m512h test_tile_tcvtrowps2phle(unsigned int A) {
-  // CHECK-LABEL: @test_tile_tcvtrowps2phle(
+__m512h test_tile_cvtrowps2phle(unsigned int A) {
+  // CHECK-LABEL: @test_tile_cvtrowps2phle(
   // CHECK: call <32 x half> @llvm.x86.tcvtrowps2phle(i8 1, i32 %{{.*}})
-  return _tile_tcvtrowps2phle(1, A);
+  return _tile_cvtrowps2phle(1, A);
 }
 
-__m512h test_tile_tcvtrowps2phli(void) {
-  // CHECK-LABEL: @test_tile_tcvtrowps2phli(
+__m512h test_tile_cvtrowps2phli(void) {
+  // CHECK-LABEL: @test_tile_cvtrowps2phli(
   // CHECK: call <32 x half> @llvm.x86.tcvtrowps2phli(i8 1, i32 127)
-  return _tile_tcvtrowps2phli(1, 127);
+  return _tile_cvtrowps2phli(1, 127);
 }
--- clang/test/CodeGen/X86/intel_isa_amx_avx512_cvtrow/intel_amx_avx512_cvtrow_api.c
+++ clang/test/CodeGen/X86/intel_isa_amx_avx512_cvtrow/intel_amx_avx512_cvtrow_api.c
@@ -10,72 +10,72 @@ char buf[1024];
 
 char buf2[1024];
 
-__m512 test_tile_tcvtrowd2pse(__tile1024i a, unsigned b) {
-  //CHECK-LABEL: @test_tile_tcvtrowd2pse
+__m512 test_tile_cvtrowd2pse(__tile1024i a, unsigned b) {
+  //CHECK-LABEL: @test_tile_cvtrowd2pse
   //CHECK-DAG: call x86_amx @llvm.x86.cast.vector.to.tile.v256i32(<256 x i32> {{%.*}})
   //CHECK-DAG: call <16 x float> @llvm.x86.tcvtrowd2pse.internal
- return __tile_tcvtrowd2pse(a, b);
+ return __tile_cvtrowd2pse(a, b);
 }
 
-__m512 test_tile_tcvtrowd2psi(__tile1024i a) {
-  //CHECK-LABEL: @test_tile_tcvtrowd2psi
+__m512 test_tile_cvtrowd2psi(__tile1024i a) {
+  //CHECK-LABEL: @test_tile_cvtrowd2psi
   //CHECK-DAG: call x86_amx @llvm.x86.cast.vector.to.tile.v256i32(<256 x i32> {{%.*}})
   //CHECK-DAG: call <16 x float> @llvm.x86.tcvtrowd2psi.internal
- return __tile_tcvtrowd2psi(a, 15);
+ return __tile_cvtrowd2psi(a, 15);
 }
 
-__m512bh test_tile_tcvtrowps2pbf16he(__tile1024i a, unsigned b) {
-  //CHECK-LABEL: @test_tile_tcvtrowps2pbf16he
+__m512bh test_tile_cvtrowps2pbf16he(__tile1024i a, unsigned b) {
+  //CHECK-LABEL: @test_tile_cvtrowps2pbf16he
   //CHECK-DAG: call x86_amx @llvm.x86.cast.vector.to.tile.v256i32(<256 x i32> {{%.*}})
   //CHECK-DAG: call <32 x i16> @llvm.x86.tcvtrowps2pbf16he.internal
- return __tile_tcvtrowps2pbf16he(a, b);
+ return __tile_cvtrowps2pbf16he(a, b);
 }
 
-__m512bh test_tile_tcvtrowps2pbf16hi(__tile1024i a) {
-  //CHECK-LABEL: @test_tile_tcvtrowps2pbf16hi
+__m512bh test_tile_cvtrowps2pbf16hi(__tile1024i a) {
+  //CHECK-LABEL: @test_tile_cvtrowps2pbf16hi
   //CHECK-DAG: call x86_amx @llvm.x86.cast.vector.to.tile.v256i32(<256 x i32> {{%.*}})
   //CHECK-DAG: call <32 x i16> @llvm.x86.tcvtrowps2pbf16hi.internal
- return __tile_tcvtrowps2pbf16hi(a, 15);
+ return __tile_cvtrowps2pbf16hi(a, 15);
 }
 
-__m512bh test_tile_tcvtrowps2pbf16le(__tile1024i a, unsigned b) {
-  //CHECK-LABEL: @test_tile_tcvtrowps2pbf16le
+__m512bh test_tile_cvtrowps2pbf16le(__tile1024i a, unsigned b) {
+  //CHECK-LABEL: @test_tile_cvtrowps2pbf16le
   //CHECK-DAG: call x86_amx @llvm.x86.cast.vector.to.tile.v256i32(<256 x i32> {{%.*}})
   //CHECK-DAG: call <32 x i16> @llvm.x86.tcvtrowps2pbf16le.internal
- return __tile_tcvtrowps2pbf16le(a, b);
+ return __tile_cvtrowps2pbf16le(a, b);
 }
 
-__m512bh test_tile_tcvtrowps2pbf16li(__tile1024i a) {
-  //CHECK-LABEL: @test_tile_tcvtrowps2pbf16li
+__m512bh test_tile_cvtrowps2pbf16li(__tile1024i a) {
+  //CHECK-LABEL: @test_tile_cvtrowps2pbf16li
   //CHECK-DAG: call x86_amx @llvm.x86.cast.vector.to.tile.v256i32(<256 x i32> {{%.*}})
   //CHECK-DAG: call <32 x i16> @llvm.x86.tcvtrowps2pbf16li.internal
- return __tile_tcvtrowps2pbf16li(a, 15);
+ return __tile_cvtrowps2pbf16li(a, 15);
 }
 
-__m512h test_tile_tcvtrowps2phhe(__tile1024i a, unsigned b) {
-  //CHECK-LABEL: @test_tile_tcvtrowps2phhe
+__m512h test_tile_cvtrowps2phhe(__tile1024i a, unsigned b) {
+  //CHECK-LABEL: @test_tile_cvtrowps2phhe
   //CHECK-DAG: call x86_amx @llvm.x86.cast.vector.to.tile.v256i32(<256 x i32> {{%.*}})
   //CHECK-DAG: call <32 x half> @llvm.x86.tcvtrowps2phhe.internal
- return __tile_tcvtrowps2phhe(a, b);
+ return __tile_cvtrowps2phhe(a, b);
 }
 
-__m512h test_tile_tcvtrowps2phhi(__tile1024i a) {
-  //CHECK-LABEL: @test_tile_tcvtrowps2phhi
+__m512h test_tile_cvtrowps2phhi(__tile1024i a) {
+  //CHECK-LABEL: @test_tile_cvtrowps2phhi
   //CHECK-DAG: call x86_amx @llvm.x86.cast.vector.to.tile.v256i32(<256 x i32> {{%.*}})
   //CHECK-DAG: call <32 x half> @llvm.x86.tcvtrowps2phhi.internal
- return __tile_tcvtrowps2phhi(a, 15);
+ return __tile_cvtrowps2phhi(a, 15);
 }
 
-__m512h test_tile_tcvtrowps2phle(__tile1024i a, unsigned b) {
-  //CHECK-LABEL: @test_tile_tcvtrowps2phle
+__m512h test_tile_cvtrowps2phle(__tile1024i a, unsigned b) {
+  //CHECK-LABEL: @test_tile_cvtrowps2phle
   //CHECK-DAG: call x86_amx @llvm.x86.cast.vector.to.tile.v256i32(<256 x i32> {{%.*}})
   //CHECK-DAG: call <32 x half> @llvm.x86.tcvtrowps2phle.internal
- return __tile_tcvtrowps2phle(a, b);
+ return __tile_cvtrowps2phle(a, b);
 }
 
-__m512h test_tile_tcvtrowps2phli(__tile1024i a) {
-  //CHECK-LABEL: @test_tile_tcvtrowps2phli
+__m512h test_tile_cvtrowps2phli(__tile1024i a) {
+  //CHECK-LABEL: @test_tile_cvtrowps2phli
   //CHECK-DAG: call x86_amx @llvm.x86.cast.vector.to.tile.v256i32(<256 x i32> {{%.*}})
   //CHECK-DAG: call <32 x half> @llvm.x86.tcvtrowps2phli.internal
- return __tile_tcvtrowps2phli(a, 15);
+ return __tile_cvtrowps2phli(a, 15);
 }
