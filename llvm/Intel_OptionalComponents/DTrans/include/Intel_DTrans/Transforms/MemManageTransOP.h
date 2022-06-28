9f9c56585ee5ca3ade8e9b0ecea5c0a9402c3a23
chrulski-intel <christopher.m.chrulski@intel.com>
2022-06-28 12:23:23 -0700

    [DTrans - OpaquePointers] Skeleton for opaque pointer version of MemManageTrans pass (#5766)
    This creates a skeleton for the pass that will replace the MemManageTrans pass
    with a new pass that supports opaque pointers and works with the new DTrans
    infrastructure.

---
+++ llvm/Intel_OptionalComponents/DTrans/include/Intel_DTrans/Transforms/MemManageTransOP.h
@@ -0,0 +1,42 @@
+//===----------- MemManageTransOP.h - DTransMemManageTransOPPass ----------===//
+//
+// Copyright (C) 2022-2022 Intel Corporation. All rights reserved.
+//
+// The information and source code contained herein is the exclusive property
+// of Intel Corporation and may not be disclosed, examined or reproduced in
+// whole or in part without explicit written authorization from the company.
+//
+//===----------------------------------------------------------------------===//
+//
+// This file declares the Initial Memory Management Transformation pass for
+// opaque pointers.
+//
+//===----------------------------------------------------------------------===//
+
+#ifndef INTEL_DTRANS_TRANSFORMS_MEMMANAGETRANSOP_H
+#define INTEL_DTRANS_TRANSFORMS_MEMMANAGETRANSOP_H
+
+#include "llvm/Analysis/TargetLibraryInfo.h"
+#include "llvm/IR/Module.h"
+#include "llvm/IR/PassManager.h"
+
+namespace llvm {
+
+class WholeProgramInfo;
+
+namespace dtransOP {
+
+/// Pass to perform Initial Memory Management Transformation.
+class MemManageTransOPPass : public PassInfoMixin<MemManageTransOPPass> {
+public:
+  using MemTLITy = std::function<const TargetLibraryInfo &(const Function &)>;
+
+  PreservedAnalyses run(Module &M, ModuleAnalysisManager &AM);
+
+  bool runImpl(Module &M, WholeProgramInfo &WPInfo, MemTLITy GetTLI);
+};
+} // namespace dtransOP
+
+} // namespace llvm
+
+#endif // INTEL_DTRANS_TRANSFORMS_MEMMANAGETRANSOP_H
