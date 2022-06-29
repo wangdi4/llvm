f38eeccf40b75962a9b7f82d4543e771e3e702d9
xiangzh1 <xiang1.zhang@intel.com>
2022-06-29 14:26:31 +0800

    [AMX] Add description for some AMX intrinsics (#5530)

--- clang/lib/Headers/Intel_amxlncintrin.h
+++ clang/lib/Headers/Intel_amxlncintrin.h
@@ -36,12 +36,137 @@
   __builtin_ia32_t2rpntlvwz1(tdst, base, stride)
 #define _tile_2rpntlvwz1t1(tdst, base, stride)                                 \
   __builtin_ia32_t2rpntlvwz1t1(tdst, base, stride)
-#define _tile_tdpbf16ps(tsrc1_dst, tsrc2, tsrc3)                               \
-  __builtin_ia32_ttdpbf16ps(tsrc1_dst, tsrc2, tsrc3)
-#define _tile_tdpfp16ps(tsrc1_dst, tsrc2, tsrc3)                               \
-  __builtin_ia32_ttdpfp16ps(tsrc1_dst, tsrc2, tsrc3)
-#define _tile_transposed(dst, tsrc)                                            \
-  __builtin_ia32_ttransposed(dst, tsrc)
+
+/// Compute transpose and dot product of BF16 pairs, accumulating to single
+/// precision. This function has 3 tile operands, one source/dest accumulator
+/// operand \a srcdst and two source operands, \a a and \a b. \a a is
+/// transposed and matrix multiplied with \a b. The transpose operation is
+/// done in BF16 pair granularity, transforming columns of BF16 pairs into rows.
+/// The tile registers specified must be distinct from one another, no repeats.
+/// Any attempt to execute an AMX instruction inside a TSX transaction will
+/// result in a transaction abort.
+///
+/// \headerfile <immintrin.h>
+///
+/// \code
+/// void __tile_ttdpbf16ps(__tile srcdst, __tile a, __tile b);
+/// \endcode
+///
+/// This intrinsic corresponds to the <c> TTDPBF16PS </c> instruction.
+///
+/// \param srcdst
+/// 	The destination tile. Max size is 1024 Bytes.
+/// \param a
+/// 	The 1st source tile. Max size is 1024 Bytes.
+/// \param b
+/// 	The 2nd source tile. Max size is 1024 Bytes.
+///
+/// \code{.operation}
+/// elements_dest:= srcdst.colsb/4
+///
+/// FOR m := 0 TO (srcdst.rows-1)
+/// 	tmp[511:0] := 0
+/// 	FOR k := 0 TO (a.rows-1)
+/// 		FOR n := 0 TO (elements_dest-1)
+/// 			tmp.fp32[2*n+0] += FP32(a.row[k].bfloat16[2*m+0]) * FP32(b.row[k].bfloat16[2*n+0])
+/// 			tmp.fp32[2*n+1] += FP32(a.row[k].bfloat16[2*m+1]) * FP32(b.row[k].bfloat16[2*n+1])
+/// 		ENDFOR
+/// 	ENDFOR
+/// 	
+/// 	FOR n := 0 TO (elements_dest-1)
+/// 		tmp2.f32[n] := tmp.fp32[2*n+0] + tmp.fp32[2*n+1]
+/// 		tmp2.f32[n] += srcdst.row[m].fp32[n]
+/// 	ENDFOR
+/// 	write_row_and_zero(srcdst, m, tmp2, srcdst.colsb)
+///
+/// ENDFOR
+///
+/// zero_upper_rows(srcdst, srcdst.rows)
+/// zero_tileconfig_start()
+/// \endcode
+#define _tile_tdpbf16ps(srcdst, a, b)                               \
+  __builtin_ia32_ttdpbf16ps(srcdst, a, b)
+
+/// Compute transpose and dot product of FP16 pairs, accumulating to single
+/// precision. This function has 3 tile operands, one source/dest accumulator
+/// operand srcdst and two source operands, a and b. a is transposed
+/// and matrix multiplied with b. The transpose operation is done in FP16
+/// pair granularity, transforming columns of FP16 pairs into rows. The tile
+/// registers specified must be distinct from one another, no repeats. Any
+/// attempt to execute an AMX instruction inside a TSX transaction will result
+/// in a transaction abort.
+///
+/// \headerfile <immintrin.h>
+///
+/// \code
+/// void __tile_ttdpfp16ps(__tile srcdst, __tile a, __tile b);
+/// \endcode
+///
+/// This intrinsic corresponds to the <c> TTDPFP16PS </c> instruction.
+///
+/// \param srcdst
+/// 	The destination tile. Max size is 1024 Bytes.
+/// \param a
+/// 	The 1st source tile. Max size is 1024 Bytes.
+/// \param b
+/// 	The 2nd source tile. Max size is 1024 Bytes.
+///
+/// \code{.operation}
+/// elements_dest:= srcdst.colsb/4
+///
+/// FOR m := 0 TO (srcdst.rows-1)
+/// 	tmp[511:0] := 0
+/// 	FOR k := 0 TO (a.rows-1)
+/// 		FOR n := 0 TO (elements_dest-1)
+/// 			tmp.fp32[2*n+0] += FP32(a.row[k].fp16[2*m+0]) * FP32(b.row[k].fp16[2*n+0])
+/// 			tmp.fp32[2*n+1] += FP32(a.row[k].fp16[2*m+1]) * FP32(b.row[k].fp16[2*n+1])
+/// 		ENDFOR
+/// 	ENDFOR
+/// 	
+/// 	FOR n := 0 TO (elements_dest-1)
+/// 		tmp2.f32[n] := tmp.fp32[2*n+0] + tmp.fp32[2*n+1]
+/// 		tmp2.f32[n] += srcdst.row[m].fp32[n]
+/// 	ENDFOR
+/// 	write_row_and_zero(srcdst, m, tmp2, srcdst.colsb)
+/// 	
+/// ENDFOR
+///
+/// zero_upper_rows(srcdst, srcdst.rows)
+/// zero_tileconfig_start()
+/// \endcode
+#define _tile_tdpfp16ps(srcdst, a, b)                                    \
+  __builtin_ia32_ttdpfp16ps(srcdst, a, b)
+
+/// Transpose 32-bit elements from \a src and write the result to \a dst.
+///
+/// \headerfile <immintrin.h>
+///
+/// \code
+/// void __tile_transposed(__tile dst, __tile src);
+/// \endcode
+///
+/// This intrinsic corresponds to the <c> TTRANSPOSED </c> instruction.
+///
+/// \param dst
+/// 	The destination tile. Max size is 1024 Bytes.
+/// \param src
+/// 	The 1st source tile. Max size is 1024 Bytes.
+///
+/// \code{.operation}
+///
+/// FOR i := 0 TO (dst.rows-1)
+/// 	tmp[511:0] := 0
+/// 	FOR j := 0 TO (dst.colsb/4-1)
+/// 		tmp.dword[j] := src.row[j].dword[i]
+/// 	ENDFOR
+/// 	dst.row[i] := tmp
+/// ENDFOR
+///
+/// zero_upper_rows(dst, dst.rows)
+/// zero_tileconfig_start()
+/// \endcode
+#define _tile_transposed(dst, src)                                          \
+  __builtin_ia32_ttransposed(dst, src)
 
 // AMX_LNC AVX512
 #define _tile_tile16move(tdst, tsrc1, tsrc2, tsrc3, tsrc4, tsrc5, tsrc6, tsrc7,\
@@ -49,9 +174,70 @@
   __builtin_ia32_tile16move(tdst, tsrc1, tsrc2, tsrc3, tsrc4, tsrc5, tsrc6,    \
   tsrc7, tsrc8, tsrc9, tsrc10, tsrc11, tsrc12, tsrc13, tsrc14, tsrc15, tsrc16)
 
-#define _tile_tilemovrowi(tsrc1, tsrc2)  __builtin_ia32_tilemovei(tsrc1, tsrc2)
-#define _tile_tilemovrowe(tsrc1, tsrc2)  __builtin_ia32_tilemovee(tsrc1, tsrc2)
-#define _tile_tilemovrowx(tsrc1, tsrc2)  __builtin_ia32_tilemovex(tsrc1, tsrc2)
+/// Move one row of a tile data to a v16f32 data.
+/// The row of the tile is selected by an imm8.
+///
+/// \headerfile <immintrin.h>
+///
+/// \code
+/// __m512 __tile_tilemovrowi(__tile a, unsigned b);
+/// \endcode
+///
+/// This intrinsic corresponds to the <c> TILEMOVROW </c> instruction.
+///
+/// \param a
+/// 	The 1st source tile. Max size is 1024 Bytes.
+/// \param b
+/// 	The 2nd source imm8. Size is 4 Bytes.
+/// \returns
+/// 	The destination v16f32 data. Size is 64 Bytes.
+///
+/// \code{.operation}
+/// VL := 512
+/// VL_bytes := VL>>3
+/// row_index := b&0x3f
+/// row_chunk := (b>>6) * VL_bytes
+/// FOR i := 0 TO (VL_bytes-1)
+/// 	IF (row_chunk + i >= a.colsb)
+/// 		dst.byte[i] := 0
+/// 	ELSE
+/// 		dst.byte[i] := a.row[row_index].byte[row_chunk+i]
+/// ENDFOR
+/// \endcode
+#define _tile_tilemovrowi(a, b)  __builtin_ia32_tilemovei(a, b)
+
+/// Move one row of a tile data to a v16f32 data.
+/// The row of the tile is selected by a 32b GPR.
+///
+/// \headerfile <immintrin.h>
+///
+/// \code
+/// __m512 __tile_tilemovrowe(__tile a, unsigned b);
+/// \endcode
+///
+/// This intrinsic corresponds to the <c> TILEMOVROW </c> instruction.
+///
+/// \param a
+/// 	The 1st source tile. Max size is 1024 Bytes.
+/// \param b
+/// 	The 2nd source r32. Size is 4 Bytes.
+/// \returns
+/// 	The destination v16f32 data. Size is 64 Bytes.
+///
+/// \code{.operation}
+/// VL := 512
+/// VL_bytes := VL>>3
+/// row_index := b&0xffff
+/// row_chunk := ((b>>16)&0xffff) * VL_bytes
+/// FOR i := 0 TO (VL_bytes-1)
+/// 	IF (row_chunk + i >= a.colsb)
+/// 		dst.byte[i] := 0
+/// 	ELSE
+/// 		dst.byte[i] := a.row[row_index].byte[row_chunk+i]
+/// ENDFOR
+/// \endcode
+#define _tile_tilemovrowe(a, b)  __builtin_ia32_tilemovee(a, b)
+#define _tile_tilemovrowx(a, b)  __builtin_ia32_tilemovex(a, b)
 // BF16EVEX
 #define _tile_dpbf16pse(tsrc1_dst, tsrc2, tsrc3)                               \
   __builtin_ia32_tdpbf16pse(tsrc1_dst, tsrc2, tsrc3)
