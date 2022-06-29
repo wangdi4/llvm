4ffd06110a92f4431b71926d5e30659752b5e51e
iclsrc <iclsrc@intel.com>
2022-06-29 02:43:20 -0700

    Resolve of merge (conflict) 3c126d5fe468 [Alignment] Replace commonAlignment with std::min 'main' -> 'xmain-web' (#5836)
    * Resolve of merge (conflict) 3c126d5fe468 [Alignment] Replace commonAlignment with std::min 'main' -> 'xmain-web'
    
      CONFLICT (content): Merge conflict in llvm/lib/Transforms/Vectorize/SLPVectorizer.cpp
    
      commit 3c126d5fe468e703ee3eacfcfc3641879b4fb281
      Author: Guillaume Chatelet <gchatelet@google.com>
      Date:   Wed Jun 22 15:02:48 2022 +0000
    
          [Alignment] Replace commonAlignment with std::min
    
          `commonAlignment` is a shortcut to pick the smallest of two `Align`
          objects. As-is it doesn't bring much value compared to `std::min`.
    
          Differential Revision: https://reviews.llvm.org/D128345
    
    * Resolve conflict
    
    Co-authored-by: iclsrc <ia.compiler.tools.git@intel.com>
    Co-authored-by: Haohai Wen <haohai.wen@intel.com>

--- llvm/lib/Transforms/Vectorize/SLPVectorizer.cpp
+++ llvm/lib/Transforms/Vectorize/SLPVectorizer.cpp
@@ -5092,7 +5092,6 @@ static LoadsState canVectorizeLoads(
           auto *GEP = dyn_cast<GetElementPtrInst>(P);
           return (IsSorted && !GEP && doesNotNeedToBeScheduled(P)) ||
                  (GEP && GEP->getNumOperands() == 2);
-<<<<<<< HEAD
         });
     if (!CompatibilitySLPMode)
       CandidateForGatherLoad =
@@ -5110,23 +5109,11 @@ static LoadsState canVectorizeLoads(
     Align CommonAlignment = cast<LoadInst>(VL0)->getAlign();
     for (Value *V : VL)
       CommonAlignment =
-            commonAlignment(CommonAlignment, cast<LoadInst>(V)->getAlign());
+            std::min(CommonAlignment, cast<LoadInst>(V)->getAlign());
     auto *VecTy = FixedVectorType::get(ScalarTy, VL.size());
     if (TTI.isLegalMaskedGather(VecTy, CommonAlignment) &&
         !TTI.forceScalarizeMaskedGather(VecTy, CommonAlignment))
       return LoadsState::ScatterVectorize;
-=======
-        })) {
-      Align CommonAlignment = cast<LoadInst>(VL0)->getAlign();
-      for (Value *V : VL)
-        CommonAlignment =
-            std::min(CommonAlignment, cast<LoadInst>(V)->getAlign());
-      auto *VecTy = FixedVectorType::get(ScalarTy, VL.size());
-      if (TTI.isLegalMaskedGather(VecTy, CommonAlignment) &&
-          !TTI.forceScalarizeMaskedGather(VecTy, CommonAlignment))
-        return LoadsState::ScatterVectorize;
-    }
->>>>>>> 3c126d5fe468e703ee3eacfcfc3641879b4fb281
   }
 #endif // INTEL_CUSTOMIZATION
 
