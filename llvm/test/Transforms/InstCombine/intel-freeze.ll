wangdi4/llvm

commit 481f348174e709111b9c585688036226016605f1

37930-2: Enable fix for 37930, for both advanced and non-advanced opt cases. (#5778)
Check for actual vector width >= 128.

llvm/lib/Transforms/InstCombine/InstructionCombining.cpp
@@ -220,19 +220,25 @@ static cl::opt<unsigned> ShouldLowerDbgDeclare("instcombine-lower-dbg-declare",
                                                cl::Hidden, cl::init(true));
 
 #if INTEL_CUSTOMIZATION
-// Returns true if the code is targeted to "advanced" vector architectures:
-// prefer-vector-width is on the function (used for OpenCL targets), OR
-// enable-intel-advanced-opts is true and AVX2 or AVX512 is enabled on the
-// target.
-static bool TargetIsAVX(TargetTransformInfo &TTI, Function *F) {
-  auto HasAVX512 =
-      TargetTransformInfo::AdvancedOptLevel::AO_TargetHasIntelAVX512;
-  auto HasAVX2 = TargetTransformInfo::AdvancedOptLevel::AO_TargetHasIntelAVX2;
-  bool HasVecWidth = false;
-  if (F)
-    HasVecWidth = F->getFnAttribute("prefer-vector-width").isValid();
-  return TTI.isAdvancedOptEnabled(HasAVX2) ||
-         TTI.isAdvancedOptEnabled(HasAVX512) || HasVecWidth;
+// Returns true if the code is targeted to Intel AVX2 vector architectures.
+// Any of the following can be true:
+// TTI has AdvancedOptLevel::AO_TargetHasIntelAVX2
+// Function has prefer-vector-width attribute, value >= 128
+// Function has target-features attribute, containing "avx2".
+static bool TargetIsAVX2(TargetTransformInfo &TTI, Function *F) {
+  auto HasAVX2TTI =
+      TargetTransformInfo::AdvancedOptLevel::AO_TargetHasIntelAVX2;
+  unsigned VecWidth = 0;
+  bool HasAVX2Feature = false;
+  assert(F && "Null function");
+  Attribute VecWidthAttr = F->getFnAttribute("prefer-vector-width");
+  VecWidth = VecWidthAttr.isValid() ? VecWidthAttr.getValueAsInt() : 0;
+  Attribute TFAttr = F->getFnAttribute("target-features");
+  StringRef TFStr = TFAttr.isValid() ? TFAttr.getValueAsString() : "";
+  HasAVX2Feature = TFStr.contains("avx2");
+
+  return TTI.isAdvancedOptEnabled(HasAVX2TTI) || VecWidth >= 128 ||
+         HasAVX2Feature;
 }
 #endif // INTEL_CUSTOMIZATION
 
@@ -4163,7 +4169,7 @@ InstCombinerImpl::pushFreezeToPreventPoisonFromPropagating(FreezeInst &OrigFI) {
       // %r2 = add %fr.1
       // ... cmp %fr.1
       auto &TTI = getTargetTransformInfo();
-      if (TargetIsAVX(TTI, Phi->getFunction()))
+      if (TargetIsAVX2(TTI, Phi->getFunction()))
         if (!Phi->hasOneUse())
           for (auto *UserI : Phi->users())
             if (isa<BinaryOperator>(UserI))
@@ -4221,7 +4227,7 @@ bool InstCombinerImpl::freezeOtherUses(FreezeInst &FI) {
   // TODO: Investigate removal of the freezes at their source, 2fa8fc3d.
   auto &TTI = getTargetTransformInfo();
   auto *F = FI.getFunction();
-  if (!F->isFortran() && TargetIsAVX(TTI, F))
+  if (!F->isFortran() && TargetIsAVX2(TTI, F))
     MoveBefore = &FI;
 #endif // INTEL_CUSTOMIZATION
 

llvm/test/Transforms/InstCombine/intel-no-spread-freeze.ll
@@ -1,4 +1,5 @@
 ; RUN: opt -enable-intel-advanced-opts -passes="instcombine" -S %s | FileCheck %s
+; RUN: opt -passes="instcombine" -S %s | FileCheck %s
 
 ; InstCombine was "spreading" the freeze in storemerge.fr, to the other uses
 ; of storemerge92, especially the mul...200 instruction.

commit 61f869fd976553c9d276b4ab9ac62838e55bc537

Commit 481f34817 broke SYCL testing. prefer-vector-width attribute must be parsed as a string, not an int. (#5942)

llvm/lib/Transforms/InstCombine/InstructionCombining.cpp
@@ -231,8 +231,16 @@ static bool TargetIsAVX2(TargetTransformInfo &TTI, Function *F) {
   unsigned VecWidth = 0;
   bool HasAVX2Feature = false;
   assert(F && "Null function");
+  // Get the vector width attr if it exists. This is a string, must be
+  // parsed to an integer.
   Attribute VecWidthAttr = F->getFnAttribute("prefer-vector-width");
-  VecWidth = VecWidthAttr.isValid() ? VecWidthAttr.getValueAsInt() : 0;
+  if (VecWidthAttr.isValid()) {
+    StringRef WidthStr = VecWidthAttr.getValueAsString();
+    // This returns true if there is an error, false if OK.
+    if (WidthStr.getAsInteger(0, VecWidth))
+      VecWidth = 0;
+  }
+  // Check if target-features contains "avx2".
   Attribute TFAttr = F->getFnAttribute("target-features");
   StringRef TFStr = TFAttr.isValid() ? TFAttr.getValueAsString() : "";
   HasAVX2Feature = TFStr.contains("avx2");

llvm/test/Transforms/InstCombine/intel-freeze.ll
@@ -64,5 +64,21 @@ end:
   ret void
 }
 
-attributes #0 = { "target-cpu"="skylake-avx512" "target-features"="+avx512f,+avx512vl,+avx512dq"}
+; Uses "prefer-vector-width" attribute.
+define void @freeze_dominated_uses_test3(i32 %v) #1 {
+; CHECK-LABEL: @freeze_dominated_uses_test3(
+; CHECK-NEXT:    [[V_FR:%.*]] = freeze i32 [[V:%.*]]
+; CHECK-NEXT:    call void @use_i32(i32 [[V_FR]])
+; CHECK-NEXT:    call void @use_i32(i32 [[V_FR]])
+; CHECK-NEXT:    ret void
+;
+  %v.fr = freeze i32 %v
+  call void @use_i32(i32 %v)
+  call void @use_i32(i32 %v.fr)
+  ret void
+}
+
+
+attributes #0 = { "target-cpu"="skylake-avx512" "target-features"="+avx2,+avx512f,+avx512vl,+avx512dq"}
+attributes #1 = { "prefer-vector-width"="256" }
 ; end INTEL_FEATURE_SW_ADVANCED