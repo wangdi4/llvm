b5ae1fae3837d63fc1124a4aa2798bb23141c02e
premanandrao <premanand.m.rao@intel.com>
2022-07-04 13:20:16 +0530

    cmplrllvm-38759: Fix incorrect merge after pulldown from intel/llvm (#5949)

--- clang/lib/Basic/Targets/SPIR.h
+++ clang/lib/Basic/Targets/SPIR.h
@@ -283,7 +283,7 @@ public:
   ArrayRef<Builtin::Info> getTargetBuiltins() const override;
   void getTargetDefines(const LangOptions &Opts,
                         MacroBuilder &Builder) const override;
-  virtual size_t getMaxBitIntWidth() const override { return 2048; }
+  virtual size_t getMaxBitIntWidth() const override { return 4096; }
 };
 
 // x86-64 SPIR64 Intelfpga Windows target
@@ -320,14 +320,6 @@ public:
       : WindowsX86_64_SPIR64INTELFpgaTargetInfo(Triple, Opts) {}
 };
 #endif // INTEL_CUSTOMIZATION
-
-// spir64_fpga target
-class LLVM_LIBRARY_VISIBILITY SPIR64FPGATargetInfo : public SPIR64TargetInfo {
-public:
-  SPIR64FPGATargetInfo(const llvm::Triple &Triple, const TargetOptions &Opts)
-      : SPIR64TargetInfo(Triple, Opts) {}
-  virtual size_t getMaxBitIntWidth() const override { return 4096; }
-};
 
 // x86-32 SPIR Windows target
 class LLVM_LIBRARY_VISIBILITY WindowsX86_32SPIRTargetInfo
