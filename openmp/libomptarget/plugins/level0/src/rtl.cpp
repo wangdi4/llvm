wangdi4/llvm

commit 49ab8148f3ce6ed5b169e0e53929e41218d5150f

[OpenMP][Offload] Use kernel property extension if available (#6050)
* [OpenMP][Offload] Use kernel property extension if available

Use ze_kernel_preferred_group_size_properties_t in L0 plugin to get
correct information that matches OCL property
CL_KERNEL_PREFERRED_WORK_GROUP_SIZE_MULTIPLE.

JIRA: CMPLRLIBS-33992

* Fix Windows build

The change needs to be disabled on Windows for now.

openmp/libomptarget/plugins/level0/src/rtl.cpp
@@ -5662,18 +5662,31 @@ int32_t LevelZeroProgramTy::buildKernels() {
     auto &KernelProperties = DeviceInfo->KernelProperties[DeviceId][Kernels[I]];
     KernelProperties.Name = Name;
     ze_kernel_properties_t KP{ZE_STRUCTURE_TYPE_KERNEL_PROPERTIES, nullptr};
+#ifndef _WIN32
+    // TODO: enable on Windows when this becomes buildable
+    ze_kernel_preferred_group_size_properties_t KPrefGRPSize
+        {ZE_STRUCTURE_TYPE_KERNEL_PREFERRED_GROUP_SIZE_PROPERTIES, nullptr};
+    if (DeviceInfo->DriverAPIVersion >= ZE_API_VERSION_1_2)
+      KP.pNext = &KPrefGRPSize;
+#endif
     CALL_ZE_RET_FAIL(zeKernelGetProperties, Kernels[I], &KP);
     if (DeviceInfo->Option.ForcedKernelWidth > 0) {
       KernelProperties.Width = DeviceInfo->Option.ForcedKernelWidth;
     } else {
-      KernelProperties.Width = KP.maxSubgroupSize;
       KernelProperties.SIMDWidth = KP.maxSubgroupSize;
-      // Temporary workaround before ze_kernel_preferred_group_size_properties_t
-      // becomes available with L0 1.2.
       // Here we try to match OpenCL kernel property
-      // CL_KERNEL_PREFERRED_WORK_GROUP_SIZE_MULTIPLE.
-      if (DeviceInfo->DeviceArchs[DeviceId] != DeviceArch_Gen9)
-        KernelProperties.Width *= 2;
+      // CL_KERNEL_PREFERRED_WORK_GROUP_SIZE_MULTIPLE for "Width".
+#ifndef _WIN32
+      if (KP.pNext) {
+        KernelProperties.Width = KPrefGRPSize.preferredMultiple;
+      } else
+#endif
+      {
+        // Use heuristic if driver does not support the extension
+        KernelProperties.Width = KP.maxSubgroupSize;
+        if (DeviceInfo->DeviceArchs[DeviceId] != DeviceArch_Gen9)
+          KernelProperties.Width *= 2;
+      }
     }
     DP("Kernel %" PRIu32 ": Entry = " DPxMOD ", Name = %s, "
        "NumArgs = %" PRIu32 ", Handle = " DPxMOD "\n", I,

commit 56524cd22145a64365c333ae41a9af336ab4ca34

[OpenMP][Offload] Fix performance regression on PVC (#6110)
This change addresses performance regression introduced by #6050.
The current kernel launch parameter computation results in better
performance when kernel width was obtained from heuristics.

JIRA: CMPLRLIBS-33997

openmp/libomptarget/plugins/level0/src/rtl.cpp
@@ -5674,18 +5674,17 @@ int32_t LevelZeroProgramTy::buildKernels() {
       KernelProperties.Width = DeviceInfo->Option.ForcedKernelWidth;
     } else {
       KernelProperties.SIMDWidth = KP.maxSubgroupSize;
+      KernelProperties.Width = KP.maxSubgroupSize;
       // Here we try to match OpenCL kernel property
       // CL_KERNEL_PREFERRED_WORK_GROUP_SIZE_MULTIPLE for "Width".
 #ifndef _WIN32
-      if (KP.pNext) {
+      if (KP.pNext)
         KernelProperties.Width = KPrefGRPSize.preferredMultiple;
-      } else
 #endif
-      {
-        // Use heuristic if driver does not support the extension
-        KernelProperties.Width = KP.maxSubgroupSize;
-        if (DeviceInfo->DeviceArchs[DeviceId] != DeviceArch_Gen9)
-          KernelProperties.Width *= 2;
+      if (DeviceInfo->isDiscreteDevice(DeviceId)) {
+        // Adjust kernel width to address performance issue (CMPLRLIBS-33997).
+        KernelProperties.Width =
+            (std::max)(KernelProperties.Width, 2 * KernelProperties.SIMDWidth);
       }
     }
     DP("Kernel %" PRIu32 ": Entry = " DPxMOD ", Name = %s, "

openmp/libomptarget/plugins/opencl/src/rtl.cpp
@@ -3563,6 +3563,15 @@ int32_t OpenCLProgramTy::buildKernels() {
       // assign default value to avoid any issues when using this variable.
       KernelProperty.SIMDWidth = KernelProperty.Width / 2;
     }
+
+#if INTEL_CUSTOMIZATION
+    if (DeviceInfo->isDiscreteDevice(DeviceId)) {
+      // Adjust kernel width to match level_zero plugin.
+      KernelProperty.Width =
+          (std::max)(KernelProperty.Width, 2 * KernelProperty.SIMDWidth);
+    }
+#endif // INTEL_CUSTOMIZATION
+
     assert(KernelProperty.SIMDWidth <= KernelProperty.Width &&
            "Invalid preferred team size multiple.");
     CALL_CL_RET_FAIL(clGetKernelWorkGroupInfo, Kernel, Device,