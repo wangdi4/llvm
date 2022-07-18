c62e52bb0f02f277f89ccc56f93d4ffd75b0a410
mdtoguchi <michael.d.toguchi@intel.com>
2022-07-18 15:29:33 -0700

    Revert "[Driver] add deprecation message for -m32 usage (#5774)" (#6191)
    This reverts commit fc885f02f9aaf2071caf6ed51e19a1d2b1c705b2.
    
    It was decided that use of -m32 should not emit the deprecation message
    at this time.  Deprecation continues to exist in documentation.

--- clang/test/Driver/intel-deprecated.cpp
+++
@@ -1,4 +0,0 @@
-/// Test for any deprecated options
-// RUN: %clangxx -m32 %s -### 2>&1 | FileCheck %s -DOPTION=-m32
-// RUN: %clang_cl -Qm32 %s -### 2>&1 | FileCheck %s -DOPTION=-Qm32
-// CHECK: option '[[OPTION]]' is deprecated and will be removed in a future release
