7f9602af360a039039507a90f5916d4826c21b27
iclsrc <iclsrc@intel.com>
2022-07-20 10:11:46 -0700

    Resolve of merge (conflict) 9d1ea1774c51 [x86] use zero-extending load of a byte outside of loops too 'main' -> 'xmain-web' (#6254)
      CONFLICT (content): Merge conflict in llvm/test/CodeGen/X86/store-narrow.ll
      CONFLICT (content): Merge conflict in llvm/test/CodeGen/X86/movmsk-cmp.ll
      CONFLICT (content): Merge conflict in llvm/lib/Target/X86/X86FixupBWInsts.cpp
    
      commit 9d1ea1774c51c44ddf0b5065bf600919988d7015
      Author: Sanjay Patel <spatel@rotateright.com>
      Date:   Tue Jul 19 16:11:14 2022 -0400
    
          [x86] use zero-extending load of a byte outside of loops too
    
          This implements the main suggested change from issue #56498.
          Using the shorter (non-extending) instruction with only
          -Oz ("minsize") rather than -Os ("optsize") is left as a
          possible follow-up.
    
          As noted in the bug report, the zero-extending load may have
          shorter latency/better throughput across a wide range of x86
          micro-arches, and it avoids a potential false dependency.
          The cost is an extra instruction byte.
    
          This could cause perf ups and downs from secondary effects,
          but I don't think it is possible to account for those in
          advance, and that will likely also depend on exact micro-arch.
          This does bring LLVM x86 codegen more in line with existing
          gcc codegen, so if problems are exposed they are more likely
          to occur for both compilers.
    
          Differential Revision: https://reviews.llvm.org/D129775
    
    * Update test for opaque pointers
    
    Co-authored-by: iclsrc <ia.compiler.tools.git@intel.com>
    Co-authored-by: Daniel Woodworth <daniel.woodworth@intel.com>

--- llvm/lib/Target/X86/X86FixupBWInsts.cpp
+++ llvm/lib/Target/X86/X86FixupBWInsts.cpp
@@ -500,8 +500,10 @@ MachineInstr *FixupBWInstPass::tryReplaceInstr(MachineInstr *MI,
   switch (MI->getOpcode()) {
 
   case X86::MOV8rm:
-<<<<<<< HEAD
 #if INTEL_CUSTOMIZATION
+    // The community has changed this to the below code, but it's going to be
+    // reverted so I'm keeping this code to resolve that upcoming conflict.
+#if 0
     // Only replace 8 bit loads with the zero extending versions if
     // in a loop and not optimizing for size. This takes
     // an extra byte to encode, and provides limited performance upside.
@@ -511,16 +513,15 @@ MachineInstr *FixupBWInstPass::tryReplaceInstr(MachineInstr *MI,
           !OptForSize)
         return tryReplaceLoad(X86::MOVZX32rm8, MI);
     }
-=======
+#endif
+#endif
     // Replace 8-bit loads with the zero-extending version if not optimizing
     // for size. The extending op is cheaper across a wide range of uarch and
     // it avoids a potentially expensive partial register stall. It takes an
     // extra byte to encode, however, so don't do this when optimizing for size.
     if (!OptForSize)
       return tryReplaceLoad(X86::MOVZX32rm8, MI);
->>>>>>> 9d1ea1774c51c44ddf0b5065bf600919988d7015
     break;
-#endif
 
   case X86::MOV16rm:
     // Always try to replace 16 bit load with 32 bit zero extending.
