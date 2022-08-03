5695d2022645821a14c0dbabc476c386163d3c85
Yilong Guo <yilong.guo@intel.com>
2022-08-04 07:33:58 +0800

    [OpenCL] Add source/*.td to BuiltinList.inc and CustomMappings.gen generation (#6488)

--- opencl-intel/backend/libraries/ocl_builtins/utils/TableGen/VectorizerTableGen.cpp
+++ opencl-intel/backend/libraries/ocl_builtins/utils/TableGen/VectorizerTableGen.cpp
@@ -1,12 +1,10 @@
-// INTEL CONFIDENTIAL
-//
-// Copyright 2012-2018 Intel Corporation.
+// Copyright (C) 2012-2022 Intel Corporation
 //
 // This software and the related documents are Intel copyrighted materials, and
 // your use of them is governed by the express license under which they were
-// provided to you (License). Unless the License provides otherwise, you may not
-// use, modify, copy, publish, distribute, disclose or transmit this software or
-// the related documents without Intel's prior written permission.
+// provided to you ("License"). Unless the License provides otherwise, you may
+// not use, modify, copy, publish, distribute, disclose or transmit this
+// software or the related documents without Intel's prior written permission.
 //
 // This software and the related documents are provided as is, with no express
 // or implied warranties, other than those that are expressly stated in the
@@ -30,9 +28,8 @@ namespace llvm{
 
 const char* VectorizerTableGen::INVALID_ENTRY = "INVALID_ENTRY";
 
-VectorizerTableGen::VectorizerTableGen(RecordKeeper& rk):
-  m_recordKeeper(rk), m_bidb(rk){
-}
+VectorizerTableGen::VectorizerTableGen(RecordKeeper &rk)
+    : m_recordKeeper(rk), m_bidb(rk, /*CollectImplDefs*/ false) {}
 
 const size_t ROW_SIZE = 6;
 
