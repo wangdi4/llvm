6e1c08f6b3901bcd149a8c287c6463675ca979b0
dbudanov-cmplr <107080225+dbudanov-cmplr@users.noreply.github.com>
2022-08-05 04:36:06 -0700

    Improved opaque pointers element type detection for dopevector constant propagation (#6530)
    * Improved opaque pointers emelement type detection for dopevector constant propagation
    
    * Resolve review comments
    
    * Resolve review comments #2

--- llvm/include/llvm/Analysis/Intel_OPAnalysisUtils.h
+++ llvm/include/llvm/Analysis/Intel_OPAnalysisUtils.h
@@ -1,6 +1,6 @@
 //===-------------------- Intel_OPAnalysisUtils.h -------------------------===//
 //
-// Copyright (C) 2021-2021 Intel Corporation. All rights reserved.
+// Copyright (C) 2021-2022 Intel Corporation. All rights reserved.
 //
 // The information and source code contained herein is the exclusive
 // property of Intel Corporation and may not be disclosed, examined
@@ -17,14 +17,30 @@
 #define LLVM_ANALYSIS_INTEL_OPANALYSISUTILS_H
 
 #include "llvm/IR/Instructions.h"
+#include "llvm/ADT/DenseMap.h"
+#include "llvm/IR/Module.h"
+#include "llvm/ADT/APInt.h"
 
 namespace llvm {
-
 //
 // If the type of 'V' is a pointer type, infer and return its pointer element
 // type, or return 'nullptr'.
 //
 extern Type *inferPtrElementType(Value &V);
+
+
+// The class below is a helper which analyses opaque pointer
+// element types and maps Value(opaque pointer)->Type(element type).
+class OpaquePointerTypeMapper {
+private:
+  using TypeAggregator = Optional<const Type *>;
+  using MapType = DenseMap<const Value *, TypeAggregator>;
+  MapType PtrETypeMap;
+
+public:
+  OpaquePointerTypeMapper(Module &M);
+  const Type *getPointerElementType(const Value *) const;
+};
 
 } // namespace llvm
 
