1480be336d6487a4bc975cb7ee2ccaec25021ec6
iclsrc <iclsrc@intel.com>
2022-08-16 00:11:26 -0700

    Resolve of merge (conflict) 1081bb08cc50 [OpenMP][libomptarget] Fix run region async condition 'main' -> 'xmain-web' (#6682)
      CONFLICT (content): Merge conflict in openmp/libomptarget/src/device.cpp
    
      commit 1081bb08cc509af820849d7c9ef62d8e84bfcde2
      Author: Kevin Sala Penads <ksalapenades@anl.gov>
      Date:   Mon Aug 15 13:07:27 2022 -0400
    
          [OpenMP][libomptarget] Fix run region async condition
    
          This patch fixes a condition in the openmp/libomptarget/src/device.cpp file. The code was checking if the run_region plugin API function was implemented, but it should actually check the run_region_async function instead.
    
          Reviewed By: tianshilei1992
    
          Differential Revision: https://reviews.llvm.org/D131782
    
    Co-authored-by: iclsrc <ia.compiler.tools.git@intel.com>
    Co-authored-by: Maosu Zhao <maosu.zhao@intel.com>

--- openmp/libomptarget/src/device.cpp
+++ openmp/libomptarget/src/device.cpp
@@ -692,11 +692,10 @@ int32_t DeviceTy::dataExchange(void *SrcPtr, DeviceTy &DstDev, void *DstPtr,
 int32_t DeviceTy::runRegion(void *TgtEntryPtr, void **TgtVarsPtr,
                             ptrdiff_t *TgtOffsets, int32_t TgtVarsSize,
                             AsyncInfoTy &AsyncInfo) {
-<<<<<<< HEAD
 #if INTEL_CUSTOMIZATION
   OMPT_TRACE(targetSubmitBegin(RTLDeviceID, 1));
   int32_t Ret;
-  if (!RTL->run_region || !RTL->synchronize)
+  if (!RTL->run_region_async || !RTL->synchronize)
     Ret = RTL->run_region(RTLDeviceID, TgtEntryPtr, TgtVarsPtr, TgtOffsets,
                           TgtVarsSize);
   else
@@ -705,10 +704,7 @@ int32_t DeviceTy::runRegion(void *TgtEntryPtr, void **TgtVarsPtr,
   OMPT_TRACE(targetSubmitEnd(RTLDeviceID, 1));
   return Ret;
 #else // INTEL_CUSTOMIZATION
-  if (!RTL->run_region || !RTL->synchronize)
-=======
   if (!RTL->run_region_async || !RTL->synchronize)
->>>>>>> 1081bb08cc509af820849d7c9ef62d8e84bfcde2
     return RTL->run_region(RTLDeviceID, TgtEntryPtr, TgtVarsPtr, TgtOffsets,
                            TgtVarsSize);
   return RTL->run_region_async(RTLDeviceID, TgtEntryPtr, TgtVarsPtr, TgtOffsets,
