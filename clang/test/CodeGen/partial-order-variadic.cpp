b4dd07488309543304f230a2180f9efb2ec28d59
iclsrc <iclsrc@intel.com>
2022-08-25 20:48:02 -0700

    Resolve of merge (conflict) 088ba8efeb49 [Clang] follow-up D128745, use ClangABICompat15 instead of.. 'main' -> 'xmain-web' (#6857)
      CONFLICT (content): Merge conflict in clang/test/CodeGen/partial-order-variadic.cpp
    
      commit 088ba8efeb4921deb49534714ddefb14a91afe46
      Author: Yuanfang Chen <yuanfang.chen@sony.com>
      Date:   Tue Aug 23 10:25:02 2022 -0700
    
          [Clang] follow-up D128745, use ClangABICompat15 instead of ClangABICompat14
    
          Since the patch missed release 15.x and will be included in release 16.x. Also, simplify related tests.
    
          Reviewed By: aaron.ballman
    
          Differential Revision: https://reviews.llvm.org/D132414
    
    Co-authored-by: iclsrc <ia.compiler.tools.git@intel.com>
    Co-authored-by: Maosu Zhao <maosu.zhao@intel.com>

--- clang/test/CodeGen/partial-order-variadic.cpp
+++ clang/test/CodeGen/partial-order-variadic.cpp
@@ -1,35 +1,5 @@
-<<<<<<< HEAD
-// RUN: %clang_cc1 -opaque-pointers -triple x86_64-unknown-unknown -fclang-abi-compat=14 -DCLANG_ABI_COMPAT=14 %s -emit-llvm -disable-llvm-passes -o - | FileCheck %s --check-prefix=CHECK-14
-// RUN: %clang_cc1 -opaque-pointers -triple x86_64-unknown-unknown %s -emit-llvm -disable-llvm-passes -o - | FileCheck %s
-
-#if defined(CLANG_ABI_COMPAT) && CLANG_ABI_COMPAT <= 14
-
-// CHECK-14: %"struct.temp_func_order_example3::S" = type { i8 }
-
-// CHECK-14: define dso_local void @_ZN24temp_func_order_example31hEi(i32 noundef %i)
-// CHECK-14-NEXT:  entry:
-// CHECK-14-NEXT:    %i.addr = alloca i32, align 4
-// CHECK-14-NEXT:    %r = alloca ptr, align 8
-// CHECK-14-NEXT:    %a = alloca %"struct.temp_func_order_example3::S", align 1
-// CHECK-14-NEXT:    store i32 %i, ptr %i.addr, align 4
-// CHECK-14-NEXT:    %call = call noundef nonnull align 4 dereferenceable(4) ptr @_ZN24temp_func_order_example31gIiJEEERiPT_DpT0_(ptr noundef %i.addr)
-// CHECK-14-NEXT:    store ptr %call, ptr %r, align 8
-// CHECK-14-NEXT:    ret void
-
-namespace temp_func_order_example3 {
-  template <typename T, typename... U> int &g(T *, U...);
-  template <typename T> void g(T);
-
-  template <typename T, typename... Ts> struct S;
-  template <typename T> struct S<T> {};
-
-  void h(int i) {
-    int &r = g(&i);
-    S<int> a;
-  }
-=======
-// RUN: %clang_cc1 -triple x86_64-unknown-unknown -fclang-abi-compat=15 -DCLANG_ABI_COMPAT=15 %s -emit-llvm -disable-llvm-passes -o - | FileCheck %s
-// RUN: %clang_cc1 -triple x86_64-unknown-unknown %s -emit-llvm -disable-llvm-passes -o - | FileCheck %s --check-prefixes=CHECK,AFTER-15
+// RUN: %clang_cc1 -opaque-pointers -triple x86_64-unknown-unknown -fclang-abi-compat=15 -DCLANG_ABI_COMPAT=15 %s -emit-llvm -disable-llvm-passes -o - | FileCheck %s
+// RUN: %clang_cc1 -opaque-pointers -triple x86_64-unknown-unknown %s -emit-llvm -disable-llvm-passes -o - | FileCheck %s --check-prefixes=CHECK,AFTER-15
 
 // CHECK: %struct.S = type { i8 }
 // CHECK: @_Z2ggiRi
@@ -41,7 +11,6 @@ template <typename T> struct S<T> {};
 void gg(int i, int &r) {
   r = g(&i);
   S<int> a;
->>>>>>> 088ba8efeb4921deb49534714ddefb14a91afe46
 }
 
 // CHECK: @_Z1hIJiEEvDpPT_
