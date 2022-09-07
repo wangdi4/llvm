bb09d46b12d7563ac6d8bd1199fd598ed4fcfa93
Hansang Bae <hansang.bae@intel.com>
2022-09-07 08:22:58 -0500

    [OpenMP][Offload] Make test compatible with Intel custom libomp (#7057)
    JIRA: CMPLRLIBS-34081

--- openmp/libomptarget/test/api/omp_host_pinned_memory_alloc.c
+++ openmp/libomptarget/test/api/omp_host_pinned_memory_alloc.c
@@ -6,7 +6,11 @@
 int main() {
   const int N = 64;
 
+#if INTEL_CUSTOMIZATION
+  int *hst_ptr = omp_alloc(N * sizeof(int), omp_target_host_mem_alloc);
+#else // INTEL_CUSTOMIZATION
   int *hst_ptr = omp_alloc(N * sizeof(int), llvm_omp_target_host_mem_alloc);
+#endif // INTEL_CUSTOMIZATION
 
   for (int i = 0; i < N; ++i)
     hst_ptr[i] = 2;
@@ -19,7 +23,11 @@ int main() {
   for (int i = 0; i < N; ++i)
     sum += hst_ptr[i];
 
+#if INTEL_CUSTOMIZATION
+  omp_free(hst_ptr, omp_target_shared_mem_alloc);
+#else // INTEL_CUSTOMIZATION
   omp_free(hst_ptr, llvm_omp_target_shared_mem_alloc);
+#endif // INTEL_CUSTOMIZATION
   // CHECK: PASS
   if (sum == N)
     printf("PASS\n");
