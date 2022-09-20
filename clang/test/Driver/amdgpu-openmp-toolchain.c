47fc4b9417b18b105e82f27d5809741532116791
iclsrc <iclsrc@intel.com>
2022-09-19 20:46:04 -0700

    Resolve of merge (conflict) 8f7b5d9ada37 Merge from 'main' to 'sycl-web' (93 commits) 'sycl-web' -> 'xmain-web' (#7305)
    * Resolve of merge (conflict) 8f7b5d9ada37 Merge from 'main' to 'sycl-web' (93 commits) 'sycl-web' -> 'xmain-web'
    
      CONFLICT (content): Merge conflict in clang/test/Driver/amdgpu-openmp-toolchain.c
    
      commit 8f7b5d9ada3773ec7368c2e21ca69c4fc95af69c
      Merge: c2651eb9254a b647f13226b4
      Author: Haonan Yang <haonan.yang@intel.com>
      Date:   Tue Sep 20 10:37:28 2022 +0800
    
          Merge from 'main' to 'sycl-web' (93 commits)
    
            CONFLICT (content): Merge conflict in clang/test/Driver/openmp-offload.c
            CONFLICT (content): Merge conflict in clang/test/Driver/amdgpu-openmp-toolchain.c
    
    * Conflict resolution
    
    Co-authored-by: iclsrc <ia.compiler.tools.git@intel.com>
    Co-authored-by: Haonan Yang <haonan.yang@intel.com>

--- clang/test/Driver/amdgpu-openmp-toolchain.c
+++ clang/test/Driver/amdgpu-openmp-toolchain.c
@@ -24,24 +24,6 @@
 // CHECK-PHASES: 0: input, "{{.*}}amdgpu-openmp-toolchain.c", c, (host-openmp)
 // CHECK-PHASES: 1: preprocessor, {0}, cpp-output, (host-openmp)
 // CHECK-PHASES: 2: compiler, {1}, ir, (host-openmp)
-<<<<<<< HEAD
-// CHECK-PHASES: 3: backend, {2}, assembler, (host-openmp)
-// CHECK-PHASES: 4: assembler, {3}, object, (host-openmp)
-// CHECK-PHASES: 5: input, "{{.*}}amdgpu-openmp-toolchain.c", c, (device-openmp)
-// CHECK-PHASES: 6: preprocessor, {5}, cpp-output, (device-openmp)
-// CHECK-PHASES: 7: compiler, {6}, ir, (device-openmp)
-// CHECK-PHASES: 8: offload, "host-openmp (x86_64-unknown-linux-gnu)" {2}, "device-openmp (amdgcn-amd-amdhsa)" {7}, ir
-// CHECK-PHASES: 9: backend, {8}, assembler, (device-openmp)
-// CHECK-PHASES: 10: assembler, {9}, object, (device-openmp)
-// INTEL_CUSTOMIZATION
-// CHECK-PHASES: 11: linker, {10}, ir, (device-openmp)
-// CHECK-PHASES: 12: offload, "device-openmp (amdgcn-amd-amdhsa)" {11}, ir
-// end INTEL_CUSTOMIZATION
-// CHECK-PHASES: 13: clang-offload-wrapper, {12}, ir, (host-openmp)
-// CHECK-PHASES: 14: backend, {13}, assembler, (host-openmp)
-// CHECK-PHASES: 15: assembler, {14}, object, (host-openmp)
-// CHECK-PHASES: 16: linker, {4, 15}, image, (host-openmp)
-=======
 // CHECK-PHASES: 3: input, "[[INPUT]]", c, (device-openmp)
 // CHECK-PHASES: 4: preprocessor, {3}, cpp-output, (device-openmp)
 // CHECK-PHASES: 5: compiler, {4}, ir, (device-openmp)
@@ -54,7 +36,6 @@
 // CHECK-PHASES: 12: backend, {11}, assembler, (host-openmp)
 // CHECK-PHASES: 13: assembler, {12}, object, (host-openmp)
 // CHECK-PHASES: 14: clang-linker-wrapper, {13}, image, (host-openmp)
->>>>>>> 8f7b5d9ada3773ec7368c2e21ca69c4fc95af69c
 
 // handling of --libomptarget-amdgpu-bc-path
 // RUN:   %clang -### --target=x86_64-unknown-linux-gnu -fopenmp -fno-openmp-new-driver -fopenmp-targets=amdgcn-amd-amdhsa -Xopenmp-target=amdgcn-amd-amdhsa -march=gfx803 --libomptarget-amdgpu-bc-path=%S/Inputs/hip_dev_lib/libomptarget-amdgpu-gfx803.bc %s 2>&1 | FileCheck %s --check-prefix=CHECK-LIBOMPTARGET
