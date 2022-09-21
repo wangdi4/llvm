48ac7bee108ef5902c605abecc5d183fb7d1c34b
Wei Xiao <wei3.xiao@intel.com>
2022-09-21 10:17:46 +0800

    [X86] Do not coalesce in the case of a 512-bit subregister copy (#7223)

--- llvm/lib/Target/X86/X86RegisterInfo.cpp
+++ llvm/lib/Target/X86/X86RegisterInfo.cpp
@@ -1178,3 +1178,23 @@ bool X86RegisterInfo::getRegAllocationHints(Register VirtReg,
 
   return true;
 }
+
+#if INTEL_CUSTOMIZATION
+/// SrcRC and DstRC will be morphed into NewRC if this returns true
+bool X86RegisterInfo::shouldCoalesce(
+    MachineInstr *MI, const TargetRegisterClass *SrcRC, unsigned SubReg,
+    const TargetRegisterClass *DstRC, unsigned DstSubReg,
+    const TargetRegisterClass *NewRC, LiveIntervals &LIS) const {
+  if (MI->getMF()->getTarget().Options.IntelAdvancedOptim &&
+      MI->getMF()->getTarget().getOptLevel() >= CodeGenOpt::Aggressive &&
+      MI->getMF()->getSubtarget().has4KDSB() &&
+      MI->isCopy() && (SubReg || DstSubReg) &&
+      (DstRC->MC->RegSizeInBits != SrcRC->MC->RegSizeInBits) &&
+      (NewRC->MC->RegSizeInBits == 512))
+    // Do not coalesce in the case of a 512-bit subregister copy
+    // Otherwise, RA may spill ZMM instead of YMM/XMM registers
+    // which is more costly, e.g., SPR bandwidth is 3x256bit or 2x512bit
+    return false;
+  return true;
+}
+#endif // INTEL_CUSTOMIZATION
