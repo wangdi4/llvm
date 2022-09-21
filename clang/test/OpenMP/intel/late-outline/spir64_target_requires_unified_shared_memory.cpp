d7ae426a47c813c1b81255b2cec3bc95ba60bd09
jyu2-git <jennifer.yu@intel.com>
2022-09-21 13:48:05 -0700

    [Opaque Pointer] Fix assert for pointer point to is not element type. (#7334)
    * [Opaque Pointer] Fix assert for pointer point to is not element type.
    
    This is for share memory with declared target var where var is pointer to
    struct.
    
    assert(llvm::cast<llvm::PointerType>(Pointer->getType())
    ->isOpaqueOrPointeeTypeMatches(ElementType))
    
    Where BP is @Nodes_base_decl_tgt_ref_ptr = weak global %struct.NODE** @Nodes_base
    and Ty is %struct.NODE*.
    
    To fix add extra load for this.
    
    This is to fix cmplrllvm-39871

--- clang/test/OpenMP/intel/late-outline/spir64_target_requires_unified_shared_memory.cpp
+++ clang/test/OpenMP/intel/late-outline/spir64_target_requires_unified_shared_memory.cpp
@@ -1,17 +1,20 @@
 // INTEL_COLLAB
 // RUN: %clang_cc1 -opaque-pointers -verify -triple x86_64-unknown-linux-gnu -fopenmp \
+// RUN: -disable-llvm-passes \
 // RUN:  -fintel-compatibility -fopenmp-late-outline \
 // RUN:  -fopenmp-targets=spir64 -emit-llvm -o - %s \
 // RUN:  | FileCheck %s  --check-prefix HOST
 //
 // RUN: %clang_cc1 -opaque-pointers -verify -triple x86_64-unknown-linux-gnu -fopenmp \
+// RUN: -disable-llvm-passes \
 // RUN:  -fintel-compatibility -fopenmp-late-outline \
 // RUN:  -fopenmp-targets=spir64 -emit-llvm-bc %s -o %t-host.bc
 //
 // RUN: %clang_cc1 -opaque-pointers -verify -triple spir64 -fopenmp \
+// RUN: -disable-llvm-passes \
 // RUN:  -fintel-compatibility -fopenmp-late-outline \
 // RUN:  -fopenmp-targets=spir64 -fopenmp-is-device \
-// RUN:  -fopenmp-host-ir-file-path %t-host.bc %s -emit-llvm -o - %s \
+// RUN:  -fopenmp-host-ir-file-path %t-host.bc %s -emit-llvm -o - \
 // RUN:  | FileCheck %s  --check-prefix TARG
 //
 // expected-no-diagnostics
@@ -35,12 +38,52 @@ void foo() {
 }
 //HOST: [[ARR:@arr]] = target_declare global ptr null, align 8
 //HOST: [[ATAG:@arr_decl_tgt_ref_ptr]] = weak target_declare global ptr [[ARR]]
+//HOST: [[Nodes_Base:@.+]] = target_declare global ptr null
+//HOST: [[Nodes_TGT_REF_PTR:@.+]] = weak target_declare global ptr [[Nodes_Base]]
 //HOST: [[ARRAY:@array]] = target_declare global [256 x float] zeroinitializer, align 16
 //HOST: [[ARRTAG:@array_decl_tgt_ref_ptr]] = weak target_declare global ptr [[ARRAY]]
-//HOST: !{i32 1, !"array_decl_tgt_ref_ptr", i32 0, i32 1, ptr [[ARRTAG]]}
-//HOST: !{i32 1, !"arr_decl_tgt_ref_ptr", i32 0, i32 0, ptr [[ATAG]]}
-//TARG: [[ARRTAG:@array_decl_tgt_ref_ptr]] = weak target_declare addrspace(1) global ptr addrspace(4) null
-//TARG: [[ATAG:@arr_decl_tgt_ref_ptr]] = weak target_declare addrspace(1) global ptr addrspace(4) null
-//TARG: !{i32 1, !"array_decl_tgt_ref_ptr", i32 0, i32 1, ptr addrspace(1) [[ARRTAG]]}
-//TARG: !{i32 1, !"arr_decl_tgt_ref_ptr", i32 0, i32 0, ptr addrspace(1) [[ATAG]]}
+//TARG-DAG: [[ARRTAG:@array_decl_tgt_ref_ptr]] = weak target_declare addrspace(1) global ptr addrspace(4) null
+//TARG-DAG: [[ATAG:@arr_decl_tgt_ref_ptr]] = weak target_declare addrspace(1) global ptr addrspace(4) null
+//TARG-DAG: [[Nodes_TGT_REF_PTR:@.+]] = weak target_declare addrspace(1) global ptr addrspace(4)
+
+void * malloc(unsigned long);
+struct NODE
+{
+  int a;
+  int b;
+};
+struct NODE *Nodes_base;
+#pragma omp declare target to(Nodes_base)
+void bar()
+{
+   Nodes_base = (NODE *)malloc(8);
+   Nodes_base[0].a = 1;
+   Nodes_base[0].b = 2;
+   #pragma omp target map(tofrom:Nodes_base[0:2])
+     {
+       Nodes_base[0].a = 3;
+       Nodes_base[0].b = 4;
+     }
+}
+int main() {
+//  bar();
+}
+//HOST: @_Z3barv()
+//HOST: [[LOAD1:%.+]] = load ptr, ptr [[Nodes_Base]]
+//HOST: [[LOAD2:%.+]] = load ptr, ptr [[Nodes_TGT_REF_PTR]]
+//HOST: [[LOAD3:%.+]] = load ptr, ptr [[LOAD2]]
+
+//TARG: @_Z3barv()
+//TARG: [[CALL:%.+]] = call spir_func {{.*}} @_Z6mallocm
+//TARG-NEXT: [[LOAD1:%.+]] = load {{.*}} [[Nodes_TGT_REF_PTR]]
+//TARG-NEXT: store ptr addrspace(4) [[CALL]], {{.*}} [[LOAD1]]
+//TARG-NEXT: [[LOAD2:%1]] = load {{.*}} [[Nodes_TGT_REF_PTR]]
+//TARG-NEXT: [[LOAD3:%.+]] = load ptr addrspace(4), ptr addrspace(4) [[LOAD2]]
+
+//HOST: !{i32 1, !"array_decl_tgt_ref_ptr", i32 0, i32 {{.*}}, ptr [[ARRTAG]]}
+//HOST: !{i32 1, !"Nodes_base_decl_tgt_ref_ptr", i32 0, i32 {{.*}} ptr [[Nodes_TGT_REF_PTR]]
+//HOST: !{i32 1, !"arr_decl_tgt_ref_ptr", i32 0, i32 {{.*}}, ptr [[ATAG]]}
+//TARG: !{i32 1, !"array_decl_tgt_ref_ptr", i32 0, i32 {{.*}}, ptr addrspace(1) [[ARRTAG]]}
+//TARG: !{i32 1, !"Nodes_base_decl_tgt_ref_ptr", i32 0, i32 1, ptr addrspace(1) [[Nodes_TGT_REF_PTR]]
+//TARG: !{i32 1, !"arr_decl_tgt_ref_ptr", i32 0, i32 {{.*}}, ptr addrspace(1) [[ATAG]]}
 //end INTEL_COLLAB
