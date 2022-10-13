9d43acd78df802f1fc6184b729b8c41d7ea38e92
Maosu Zhao <maosu.zhao@intel.com>
2022-10-13 17:48:56 +0800

    [OpenCL] Use newly build clang to build soft_math builtins (#7449)

--- opencl-intel/backend/libraries/ocl_builtins/soft_math/trunctfhf2.c
+++ opencl-intel/backend/libraries/ocl_builtins/soft_math/trunctfhf2.c
@@ -1,3 +1,17 @@
+// INTEL CONFIDENTIAL
+//
+// Copyright 2022 Intel Corporation.
+//
+// This software and the related documents are Intel copyrighted materials, and
+// your use of them is governed by the express license under which they were
+// provided to you (License). Unless the License provides otherwise, you may not
+// use, modify, copy, publish, distribute, disclose or transmit this software or
+// the related documents without Intel's prior written permission.
+//
+// This software and the related documents are provided as is, with no express
+// or implied warranties, other than those that are expressly stated in the
+// License.
+//
 // This file is modified from trunctfhf2.c in LLVM compiler-rt project.
 //
 //===-- lib/trunctfhf2.c - quad -> half conversion ----------------*- C -*-===//
@@ -14,7 +28,10 @@
 
 #define SRC_QUAD
 #define DST_HALF
+#ifdef __INTEL_LLVM_COMPILER
+#define COMPILER_RT_HAS_FLOAT16
+#endif
 #include "fp_trunc_impl.inc"
 
 __attribute__((weak))
-uint16_t __trunctfhf2(long double a) { return __truncXfYf2__(a); }
+dst_t __trunctfhf2(long double a) { return __truncXfYf2__(a); }
