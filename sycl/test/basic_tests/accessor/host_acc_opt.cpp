106bfb209330c9fdf62129994bfac3c65a7d0305
iclsrc <iclsrc@intel.com>
2022-10-13 19:22:44 -0700

    Resolve of merge (conflict) 4b0e9be7e992 [SYCL] Optimize operator[] method of host accessor (#6853).. 'xmain' -> 'xmain-cand' (2 commits) (#7894)
    * Resolve of merge (conflict) 4b0e9be7e992 [SYCL] Optimize operator[] method of host accessor (#6853).. 'xmain' -> 'xmain-cand' (2 commits)
    
      CONFLICT (add/add): Merge conflict in sycl/test/basic_tests/accessor/host_acc_opt.cpp
    
      commit 4b0e9be7e992b0e4cc985e277e4e6d30d0eda728
      Author: Romanov Vlad <vlad.romanov@intel.com>
      Date:   Fri Oct 14 03:20:07 2022 +0200
    
          [SYCL] Optimize operator[] method of host accessor (#6853) (#7778)
    
          * [SYCL] Optimize operator[] method of host accessor (#6853)
    
          Compiler cannot optimize out calls to getPtr(), getOffset() and
          getMemoryRange(), that were used in accessor::operator[], because they
          may have side effects(e.g. printf). The patch adds a member in the
          accessor which points to the actual data stored in the accessor_impl.
          This pointer is initialized once during construction and used instead of
          functions mentioned above.
    
          * [SYCL] Fix post commit fails (#6875)
    
          1. Updated checks to be more be more robust and work when clang discards
          names of values in the LLVM IR.
          2. Added a new symbol to the windows sybmols test.
    
          * [SYCL][NFC] Add a missing symbol (#6885)
    
          While the copy assignment operator of AccessorImplHost is not used, it
          was auto generated by the compiler until recent changes and being
          checked in the ABI tests.
          The safest way to fix this is to just manually define it.
    
          Also removed 2 redundant lines in host_acc_opt test.
    
          * [SYCL] Relax checks in host_acc_opt LIT test (#7027)
    
          It turns out to be non-robust to expect compiler to vectorize the code.
          The code can be optmized in different ways. So relax the checks to
          verify that there are no calls to accessor helper methods (getOffset and
          getMemoryRange) in the loop. Absence of these calls should be enough to
          optimize the code.
    
    * Conflict resolution
    
    Co-authored-by: iclsrc <ia.compiler.tools.git@intel.com>
    Co-authored-by: Haonan Yang <haonan.yang@intel.com>

--- sycl/test/basic_tests/accessor/host_acc_opt.cpp
+++ sycl/test/basic_tests/accessor/host_acc_opt.cpp
@@ -1,39 +1,18 @@
-<<<<<<< HEAD
-// RUN: %clangxx -O2 -std=c++17 -I %sycl_include/sycl -I %sycl_include -S -emit-llvm %s -o - | FileCheck %s
-
-// The test verifies that the accessor::operator[] implementation is
-// good enough for compiler to optimize away calls to getOffset and
-// getMemoryRange and vectorize the loop.
-=======
 // RUN: %clangxx -O2 -isystem %sycl_include/sycl -isystem %sycl_include -S -emit-llvm %s -o - | FileCheck %s
 
 // The test verifies that the accessor::operator[] implementation is
 // good enough for compiler to optimize away calls to getOffset and
 // getMemoryRange.
->>>>>>> 4b0e9be7e992b0e4cc985e277e4e6d30d0eda728
 
 #include <sycl/sycl.hpp>
 
 // CHECK: define {{.*}}foo{{.*}} {
 // CHECK-NOT: call
 // CHECK-NOT: invoke
-<<<<<<< HEAD
-// CHECK: load <4 x i32>
-// CHECK-NOT: call
-// CHECK-NOT: invoke
-// CHECK: store <4 x i32>
-// CHECK-NOT: call
-// CHECK-NOT: invoke
-=======
->>>>>>> 4b0e9be7e992b0e4cc985e277e4e6d30d0eda728
 // CHECK: }
 void foo(sycl::accessor<int, 1, sycl::access::mode::read_write,
                         sycl::target::host_buffer> &Acc,
          int *Src) {
   for (size_t I = 0; I < 64; ++I)
-<<<<<<< HEAD
-    Acc[I] = Src[I];
-=======
     Acc[I] += Src[I];
->>>>>>> 4b0e9be7e992b0e4cc985e277e4e6d30d0eda728
 }
