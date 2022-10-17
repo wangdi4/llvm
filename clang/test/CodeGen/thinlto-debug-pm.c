77bb73ac84310d1dccab97259286ef2495a8cecb
iclsrc <iclsrc@intel.com>
2022-10-17 08:51:58 -0700

    Resolve of merge (conflict) a3a88bc563c2  Align tests with the community versions after switching t.. 'sycl' -> 'xmain-web' (19 commits) (#7948)
    * Resolve of merge (conflict) a3a88bc563c2  Align tests with the community versions after switching t.. 'sycl' -> 'xmain-web' (19 commits)
    
      CONFLICT (content): Merge conflict in llvm/test/Transforms/PhaseOrdering/X86/pr52289.ll
      CONFLICT (content): Merge conflict in llvm/test/Transforms/Inline/exponential-deferred-inlining.ll
      CONFLICT (content): Merge conflict in clang/test/CodeGen/thinlto-debug-pm.c
    
      commit a3a88bc563c27632182840ceb85cf409e936137e
      Author: Viktoria Maximova <viktoria.maksimova@intel.com>
      Date:   Mon Oct 17 11:58:21 2022 +0200
    
           Align tests with the community versions after switching to NewPM (#7048)
    
          This patch is a debt cleanup. The modifications to the tests were done
          when to support old pm pipelenes. Now as long as we switched to New PM
          by default, they are no longer needed.
    
    * Conflict resolution
    
    Co-authored-by: iclsrc <ia.compiler.tools.git@intel.com>
    Co-authored-by: Mariya Podchishchaeva <mariya.podchishchaeva@intel.com>

--- clang/test/CodeGen/thinlto-debug-pm.c
+++ clang/test/CodeGen/thinlto-debug-pm.c
@@ -4,26 +4,18 @@
 // RUN: %clang_cc1 -O2 -o %t.o -flto=thin -triple x86_64-unknown-linux-gnu -emit-llvm-bc %s
 // RUN: llvm-lto -thinlto -o %t %t.o
 
-<<<<<<< HEAD
 // INTEL_CUSTOMIZATION
 // LoopVectorizer is not enabled by default.
 // RUN: %clang_cc1 -fno-legacy-pass-manager -triple x86_64-unknown-linux-gnu -emit-obj -O2 -mllvm -enable-lv -o %t2.o -x ir %t.o -fthinlto-index=%t.thinlto.bc -fdebug-pass-manager 2>&1 | FileCheck %s --check-prefix=O2
 // END INTEL_CUSTOMIZATION
-=======
-// RUN: %clang_cc1 -triple x86_64-unknown-linux-gnu -emit-obj -O2 -o %t2.o -x ir %t.o -fthinlto-index=%t.thinlto.bc -fdebug-pass-manager 2>&1 | FileCheck %s --check-prefix=O2
->>>>>>> a3a88bc563c27632182840ceb85cf409e936137e
 // O2: Running pass: LoopVectorizePass
 
 // RUN: %clang_cc1 -O0 -o %t.o -flto=thin -triple x86_64-unknown-linux-gnu -emit-llvm-bc %s
 // RUN: llvm-lto -thinlto -o %t %t.o
 
-<<<<<<< HEAD
 // INTEL_CUSTOMIZATION
 // RUN: %clang_cc1 -fno-legacy-pass-manager -triple x86_64-unknown-linux-gnu -emit-obj -O0 -mllvm -enable-lv -o %t2.o -x ir %t.o -fthinlto-index=%t.thinlto.bc -fdebug-pass-manager 2>&1 | FileCheck %s --check-prefix=O0
 // END INTEL_CUSTOMIZATION
-=======
-// RUN: %clang_cc1 -triple x86_64-unknown-linux-gnu -emit-obj -O0 -o %t2.o -x ir %t.o -fthinlto-index=%t.thinlto.bc -fdebug-pass-manager 2>&1 | FileCheck %s --check-prefix=O0
->>>>>>> a3a88bc563c27632182840ceb85cf409e936137e
 // O0-NOT: Running pass: LoopVectorizePass
 
 void foo(void) {
