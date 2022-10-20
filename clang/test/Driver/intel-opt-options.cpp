803a3ff70d72404d2a1ee910efbfcfe6c02c7e54
Harini Chilamantula <harini.chilamantula@intel.com>
2022-10-19 17:02:54 -0700

    [Driver] Added warning to use ICPX for compiling C++ code (#7963)
    * [Driver] Added warning to use ICPX for compiling C++ code
    
    * Modified the diagnostic's to emit when performing a link step
    
    * added a test using -c to verify the diag

--- clang/test/Driver/intel-opt-options.cpp
+++ clang/test/Driver/intel-opt-options.cpp
@@ -216,3 +216,9 @@
 // RUN: not %clangxx --intel --- -### -c %s 2>&1 | FileCheck -check-prefix SUPPORT-CHECK1 %s
 // RUN: not %clangxx --intel --dpcpp --- -### -c %s 2>&1 | FileCheck -check-prefix SUPPORT-CHECK1 %s
 // SUPPORT-CHECK1: icpx: error: unsupported option '---'
+
+// warn to use icpx when compiling .cpp files
+// RUN: %clang --intel -### %s 2>&1 | FileCheck -check-prefix=WARN-TO-USE-ICPX %s
+// RUN: %clang --intel -c -### %s 2>&1 | FileCheck -check-prefix=WARN-TO-USE-ICPX-OFF %s
+// WARN-TO-USE-ICPX: warning: Use of 'icx' with a C++ source input file 'intel-opt-options.cpp' will not link with required C++ library. Use 'icpx' for C++ source compilation and link [-Wincompatible-compiler]
+// WARN-TO-USE-ICPX-OFF-NOT: warning: Use of 'icx' with a C++ source input file 'intel-opt-options.cpp' will not link with required C++ library. Use 'icpx' for C++ source compilation and link [-Wincompatible-compiler]
