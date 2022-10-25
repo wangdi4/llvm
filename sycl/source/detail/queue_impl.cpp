0df8b38963b3db13f3b758dfc7ff5f0476aee0e7
iclsrc <iclsrc@intel.com>
2022-10-25 10:47:00 -0700

    Resolve of merge (conflict) 3e51d989eab2 [SYCL] Preserve in-order queue dependency chain between us.. 'xmain' -> 'xmain-cand' (#8196)
    * Resolve of merge (conflict) 3e51d989eab2 [SYCL] Preserve in-order queue dependency chain between us.. 'xmain' -> 'xmain-cand'
    
      CONFLICT (content): Merge conflict in sycl/source/detail/queue_impl.cpp
    
      commit 3e51d989eab2e6cda551948a99bc7d2821b7abe7
      Author: Artur Gainullin <artur.gainullin@intel.com>
      Date:   Fri Oct 14 10:09:00 2022 -0700
    
          [SYCL] Preserve in-order queue dependency chain between usm & host tasks (#6840) (#7839)
    
          Currently last event is not updated for usm commands, so dependency
          chain is incorrect if usm commands are interleaved with host tasks. Also
          we need to wait if the last event is associated with host task before
          submitting usm command.
    
          E2E test: https://github.com/intel/llvm-test-suite/pull/1283
    
    * Conflict resolution
    
    Co-authored-by: iclsrc <ia.compiler.tools.git@intel.com>
    Co-authored-by: Mariya Podchishchaeva <mariya.podchishchaeva@intel.com>

--- sycl/source/detail/queue_impl.cpp
+++ sycl/source/detail/queue_impl.cpp
@@ -71,11 +71,7 @@ event queue_impl::memset(const std::shared_ptr<detail::queue_impl> &Self,
   {
     // We need to submit command and update the last event under same lock if we
     // have in-order queue.
-<<<<<<< HEAD
     auto ScopeLock = isInOrder() ? std::unique_lock<std::mutex>(MLastEventMtx)
-=======
-    auto ScopeLock = isInOrder() ? std::unique_lock(MLastEventMtx)
->>>>>>> 3e51d989eab2e6cda551948a99bc7d2821b7abe7
                                  : std::unique_lock<std::mutex>();
     // If the last submitted command in the in-order queue is host_task then
     // wait for it before submitting usm command.
@@ -117,11 +113,7 @@ event queue_impl::memcpy(const std::shared_ptr<detail::queue_impl> &Self,
   {
     // We need to submit command and update the last event under same lock if we
     // have in-order queue.
-<<<<<<< HEAD
     auto ScopeLock = isInOrder() ? std::unique_lock<std::mutex>(MLastEventMtx)
-=======
-    auto ScopeLock = isInOrder() ? std::unique_lock(MLastEventMtx)
->>>>>>> 3e51d989eab2e6cda551948a99bc7d2821b7abe7
                                  : std::unique_lock<std::mutex>();
     // If the last submitted command in the in-order queue is host_task then
     // wait for it before submitting usm command.
@@ -164,11 +156,7 @@ event queue_impl::mem_advise(const std::shared_ptr<detail::queue_impl> &Self,
   {
     // We need to submit command and update the last event under same lock if we
     // have in-order queue.
-<<<<<<< HEAD
     auto ScopeLock = isInOrder() ? std::unique_lock<std::mutex>(MLastEventMtx)
-=======
-    auto ScopeLock = isInOrder() ? std::unique_lock(MLastEventMtx)
->>>>>>> 3e51d989eab2e6cda551948a99bc7d2821b7abe7
                                  : std::unique_lock<std::mutex>();
     // If the last submitted command in the in-order queue is host_task then
     // wait for it before submitting usm command.
