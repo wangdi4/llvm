0d6c23d5fe9d9c99ccf18a39388b879cd7293f93
Haonan Yang <haonan.yang@intel.com>
2022-10-27 12:47:26 +0800

    [OpenCL] Remove deprecated initializeInstrumentation (#8240)
    Instrumentation legacy pass was removed on https://reviews.llvm.org/D136615

--- opencl-intel/backend/optimizer/Optimizer.cpp
+++ opencl-intel/backend/optimizer/Optimizer.cpp
@@ -924,7 +924,6 @@ void OptimizerOCLLegacy::initializePasses() {
   initializeAnalysis(Registry);
   initializeTransformUtils(Registry);
   initializeInstCombine(Registry);
-  initializeInstrumentation(Registry);
   initializeTarget(Registry);
   // For codegen passes, only passes that do IR to IR transformation are
   // supported.
