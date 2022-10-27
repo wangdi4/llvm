1c881a53540e40119730948b3f179ea65060c1d5
Pankaj Chawla <pankaj.chawla@intel.com>
2022-10-27 09:42:06 -0700

    Bugfix to override division type in replaceIVByCanonExpr() (#8213)
    Only vectorizer is setting the division type as signed in the CanonExpr so
    the bug cannot be exposed without vectorizer changes.
    
    Since ScalarEvolution only has support for unsigned division, HIR framework
    never sets division type as signed. I don't think any loopopt transformations
    set it as signed either.

--- llvm/lib/Analysis/Intel_LoopAnalysis/Utils/CanonExprUtils.cpp
+++ llvm/lib/Analysis/Intel_LoopAnalysis/Utils/CanonExprUtils.cpp
@@ -653,6 +653,11 @@ bool CanonExprUtils::replaceIVByCanonExpr(CanonExpr *CE1, unsigned Level,
 
   // Set denominator from CE1 to CE2
   Term->divide(CE1->getDenominator());
+  // If we reach here, either the CEs are mergeable in which case the division
+  // type should be the same or the denominator of CE2 was 1. In both cases we
+  // should be able to override the division type as it is set so add() below
+  // doesn't fail.
+  Term->setDivisionType(CE1->isSignedDiv());
 
   // CE1 = C1*C2 * B1*B2 + C3*i2 + ...
   if (!add(CE1, Term.get(), /*RelaxedMode*/ true)) {
