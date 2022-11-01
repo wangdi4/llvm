96700b51786f030cc70f346a86ec953f3b8c1a4d
iclsrc <iclsrc@intel.com>
2022-11-01 15:12:45 -0700

    Resolve of merge (conflict) 6aa672f14197 [IR] Take operand bundles into account for call argument r.. 'main' -> 'xmain-web' (#8358)
    * Resolve of merge (conflict) 6aa672f14197 [IR] Take operand bundles into account for call argument r.. 'main' -> 'xmain-web'
    
      CONFLICT (content): Merge conflict in llvm/lib/IR/Instructions.cpp
    
      commit 6aa672f14197e84520671de4ce3fcc724bbfe8d9
      Author: Nikita Popov <npopov@redhat.com>
      Date:   Thu Oct 27 12:45:08 2022 +0200
    
          [IR] Take operand bundles into account for call argument readonly/writeonly
    
          We currently only take operand bundle effects into account when
          querying the function-level memory attributes. However, I believe
          that we also need to do the same for parameter attributes. For
          example, a call with deopt bundle to a function with readnone
          parameter attribute cannot treat that parameter as readnone,
          because the deopt bundle may read it.
    
          Differential Revision: https://reviews.llvm.org/D136834
    
    * Conflict resolution
    
    Co-authored-by: iclsrc <ia.compiler.tools.git@intel.com>
    Co-authored-by: Preethi Kotari <preethi.kotari@intel.com>

--- llvm/lib/IR/Instructions.cpp
+++ llvm/lib/IR/Instructions.cpp
@@ -368,27 +368,6 @@ bool CallBase::paramHasAttr(unsigned ArgNo, Attribute::AttrKind Kind) const {
 
   if (Attrs.hasParamAttr(ArgNo, Kind))
     return true;
-<<<<<<< HEAD
-#if INTEL_CUSTOMIZATION
-  if (const Function *F = getCalledFunction()) {
-    if (F->getAttributes().hasParamAttr(ArgNo, Kind))
-      return true;
-    if (CallBaseLookupCallbackAttrs)
-      // If we are dealing with a callback call site check if callback function
-      // argument has given attribute if broker function forwards it to the
-      // callback and this attribute can be legaly propagated to the caller.
-      if (Kind == Attribute::ByVal || Kind == Attribute::ImmArg ||
-          Kind == Attribute::NoAlias || Kind == Attribute::NoCapture ||
-          Kind == Attribute::NoFree || Kind == Attribute::NonNull ||
-          Kind == Attribute::ReadNone || Kind == Attribute::ReadOnly ||
-          Kind == Attribute::StructRet || Kind == Attribute::WriteOnly)
-        if (Argument *CallbackArg =
-                AbstractCallSite::getCallbackArg(*this, ArgNo))
-          return CallbackArg->hasAttribute(Kind);
-  }
-#endif // INTEL_CUSTOMIZATION
-  return false;
-=======
 
   const Function *F = getCalledFunction();
   if (!F)
@@ -397,18 +376,31 @@ bool CallBase::paramHasAttr(unsigned ArgNo, Attribute::AttrKind Kind) const {
   if (!F->getAttributes().hasParamAttr(ArgNo, Kind))
     return false;
 
-  // Take into account mod/ref by operand bundles.
+#if INTEL_CUSTOMIZATION
+  if (CallBaseLookupCallbackAttrs) {
+    // If we are dealing with a callback call site check if callback function
+    // argument has given attribute if broker function forwards it to the
+    // callback and this attribute can be legaly propagated to the caller.
+    if (Kind == Attribute::ByVal || Kind == Attribute::ImmArg ||
+        Kind == Attribute::NoAlias || Kind == Attribute::NoCapture ||
+        Kind == Attribute::NoFree || Kind == Attribute::NonNull ||
+        Kind == Attribute::ReadNone || Kind == Attribute::ReadOnly ||
+        Kind == Attribute::StructRet || Kind == Attribute::WriteOnly)
+      if (Argument *CallbackArg =
+              AbstractCallSite::getCallbackArg(*this, ArgNo))
+        return CallbackArg->hasAttribute(Kind);
+  } 
+#endif // INTEL_CUSTOMIZATION
   switch (Kind) {
-  case Attribute::ReadNone:
-    return !hasReadingOperandBundles() && !hasClobberingOperandBundles();
-  case Attribute::ReadOnly:
-    return !hasClobberingOperandBundles();
-  case Attribute::WriteOnly:
-    return !hasReadingOperandBundles();
-  default:
-    return true;
+    case Attribute::ReadNone:
+      return !hasReadingOperandBundles() && !hasClobberingOperandBundles();
+    case Attribute::ReadOnly:
+      return !hasClobberingOperandBundles();
+    case Attribute::WriteOnly:
+      return !hasReadingOperandBundles();
+    default:
+      return true;
   }
->>>>>>> 6aa672f14197e84520671de4ce3fcc724bbfe8d9
 }
 
 bool CallBase::hasFnAttrOnCalledFunction(Attribute::AttrKind Kind) const {
