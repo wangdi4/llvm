88140d3765d0de8e25f83879c2e643921c5e9268
Chunyang Dai <chunyang.dai@intel.com>
2022-11-05 10:28:56 +0800

    [OpenCL] Remove legacy pass manager support in optimizer (#8257)
    * [OpenCL] Remove legacy pass manager support in optimizer
    
    We removed the pipeline of LLVM legacy pass manager and corresponding test cases.
    
    * [OpenCL] format code, add copyright for cmake file
    
    * [OpenCL] revert change of header file include.
    
    * [OpenCL]Update test case to use LTO pass manager.
    
    * [OpenCL]Update test case to use LTO pass manager.
    
    Update it in correct test cases.
    
    * [OpenCL] remove unused header files

--- opencl-intel/backend/tests/opencl/SATest/CheckVF/force_vf.tst
+++ opencl-intel/backend/tests/opencl/SATest/CheckVF/force_vf.tst
@@ -1,3 +1,3 @@
-; RUN: SATest -BUILD -tsize=4 -pass-manager-type=lto-legacy -cpuarch=skx -build-log -config=%s.cfg | FileCheck %s
+; RUN: SATest -BUILD -tsize=4 -pass-manager-type=lto -cpuarch=skx -build-log -config=%s.cfg | FileCheck %s
 
 ; CHECK: Kernel "test" was successfully vectorized (4)
--- opencl-intel/tests/framework_test_type/clBuildSpirvFriendlyIRProgramWithBinaryTest.cpp
+++ opencl-intel/tests/framework_test_type/clBuildSpirvFriendlyIRProgramWithBinaryTest.cpp
@@ -27,8 +27,8 @@ bool clBuildSpirvFriendlyIRProgramWithBinaryTest() {
     return true;
 
   // Use the LTO pipeline for nonspirv CPU input.
-  if (!SETENV("CL_CONFIG_LTO_PM", "legacy")) {
-    printf("Error: failed to set env CL_CONFIG_LTO_PM=legacy\n");
+  if (!SETENV("CL_CONFIG_LTO_PM", "new")) {
+    printf("Error: failed to set env CL_CONFIG_LTO_PM=new\n");
     return false;
   }
 
