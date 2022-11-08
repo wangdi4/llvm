e4c000a85c975be3fd274dc3e68e99970bf687ff
Michael Toguchi <michael.d.toguchi@intel.com>
2022-11-08 10:02:44 -0700

    [Driver] Inform usage of DPCPPCFG is deprecated (#8434)
    As we have moved away from using dpcpp in favor if icpx -fsycl, any usage
    of DPCPPCFG should also have some informative information.  Emit a diagnostic
    that DPCPPCFG is deprecated, with the replacement ICPXCFG should be used
    instead.
    
    Addresses:  CMPLRLLVM-40507

--- clang/lib/Driver/Driver.cpp
+++ clang/lib/Driver/Driver.cpp
@@ -1755,6 +1755,12 @@ bool Driver::loadDefaultConfigFiles(ArrayRef<StringRef> CfgFileSearchDirs) {
                       .Case("icx", "ICXCFG")
                       .Case("icx-cl", "ICXCLCFG")
                       .Default("");
+    // Use of 'DPCPPCFG' is deprecated.
+    if (IsDPCPPMode()) {
+      if (Optional<std::string> EnvVarValue =
+              llvm::sys::Process::GetEnv("DPCPPCFG"))
+        Diag(diag::warn_drv_deprecated_env_var) << "DPCPPCFG" << EnvVar;
+    }
     if (Optional<std::string> EnvVarValue =
             llvm::sys::Process::GetEnv(EnvVar)) {
       if (!readConfigFile(*EnvVarValue)) {
