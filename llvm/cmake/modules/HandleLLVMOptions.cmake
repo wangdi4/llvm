374df8777921458437c910d51014c1a3a4bd7645
iclsrc <iclsrc@intel.com>
2022-11-09 16:22:42 -0800

    Resolve of merge (conflict) 8a084f68c8a1 Revert "Move googletest to the third-party directory" 'main' -> 'xmain-web' (#8522)
    * Resolve of merge (conflict) 8a084f68c8a1 Revert "Move googletest to the third-party directory" 'main' -> 'xmain-web'
    
      CONFLICT (content): Merge conflict in llvm/cmake/modules/HandleLLVMOptions.cmake
    
      commit 8a084f68c8a1cac99d9fa0fbcf490b53ab3b6e0c
      Author: Tom Stellard <tstellar@redhat.com>
      Date:   Wed Nov 9 11:32:26 2022 -0800
    
          Revert "Move googletest to the third-party directory"
    
          This reverts commit 59052468c3e38cab15582cefbb5133fd4c2ffce5.
    
          It looks like this patch breaks the build when compiler-rt is passed to
          LLVM_ENABLE_PROJECTS instead of LLVM_ENABLE_RUNTIMES.
    
    * Conflict resolution
    
    Co-authored-by: iclsrc <ia.compiler.tools.git@intel.com>
    Co-authored-by: Preethi Kotari <preethi.kotari@intel.com>

--- llvm/cmake/modules/HandleLLVMOptions.cmake
+++ llvm/cmake/modules/HandleLLVMOptions.cmake
@@ -1397,7 +1397,6 @@ if(LLVM_USE_RELATIVE_PATHS_IN_FILES)
   append_if(SUPPORTS_FFILE_PREFIX_MAP "-ffile-prefix-map=${source_root}/=${LLVM_SOURCE_PREFIX}" CMAKE_C_FLAGS CMAKE_CXX_FLAGS)
   add_flag_if_supported("-no-canonical-prefixes" NO_CANONICAL_PREFIXES)
 endif()
-<<<<<<< HEAD
 
 if(INTEL_CUSTOMIZATION)
   option(INTEL_SDL_BUILD
@@ -1645,7 +1644,3 @@ if(INTEL_CUSTOMIZATION)
     message(STATUS "INTEL: setting SDL options - end.")
   endif()
 endif(INTEL_CUSTOMIZATION)
-set(LLVM_THIRD_PARTY_DIR  ${CMAKE_CURRENT_SOURCE_DIR}/../third-party CACHE STRING
-    "Directory containing third party software used by LLVM (e.g. googletest)")
-=======
->>>>>>> 8a084f68c8a1cac99d9fa0fbcf490b53ab3b6e0c
