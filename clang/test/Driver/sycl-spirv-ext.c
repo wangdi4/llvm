0cf06b2a0733b89d6c7c7c2f34f3738351230d47
iclsrc <iclsrc@intel.com>
2022-12-02 04:41:06 -0800

    Resolve of merge (conflict) 334d0e9708eb [SYCL] Enable SPV_INTEL_masked_gather_scatter extension (#.. 'sycl' -> 'xmain-web' (#8973)
    * Resolve of merge (conflict) 334d0e9708eb [SYCL] Enable SPV_INTEL_masked_gather_scatter extension (#.. 'sycl' -> 'xmain-web'
    
      CONFLICT (content): Merge conflict in clang/test/Driver/sycl-spirv-ext.c
      CONFLICT (content): Merge conflict in clang/lib/Driver/ToolChains/Clang.cpp
    
      commit 334d0e9708ebb7c7582f8f2f0883ac20bb7b7580
      Author: Dmitry Sidorov <dmitry.sidorov@intel.com>
      Date:   Wed Nov 30 00:32:48 2022 +0100
    
          [SYCL] Enable SPV_INTEL_masked_gather_scatter extension (#7530)
    
    * Conflict resolution
    
    Co-authored-by: iclsrc <ia.compiler.tools.git@intel.com>
    Co-authored-by: Mariya Podchishchaeva <mariya.podchishchaeva@intel.com>

--- clang/test/Driver/sycl-spirv-ext.c
+++ clang/test/Driver/sycl-spirv-ext.c
@@ -52,13 +52,9 @@
 // CHECK-DEFAULT-SAME:,+SPV_INTEL_bfloat16_conversion
 // CHECK-DEFAULT-SAME:,+SPV_INTEL_joint_matrix
 // CHECK-DEFAULT-SAME:,+SPV_INTEL_hw_thread_queries
-<<<<<<< HEAD
 // CHECK-DEFAULT-SAME:,+SPV_INTEL_memory_access_aliasing
-// CHECK-DEFAULT-SAME:,+SPV_KHR_uniform_group_instructions"
-=======
 // CHECK-DEFAULT-SAME:,+SPV_KHR_uniform_group_instructions
 // CHECK-DEFAULT-SAME:,+SPV_INTEL_masked_gather_scatter"
->>>>>>> 334d0e9708ebb7c7582f8f2f0883ac20bb7b7580
 // CHECK-FPGA-HW: llvm-spirv{{.*}}"-spirv-ext=-all
 // CHECK-FPGA-HW-SAME:,+SPV_EXT_shader_atomic_float_add
 // CHECK-FPGA-HW-SAME:,+SPV_EXT_shader_atomic_float_min_max
