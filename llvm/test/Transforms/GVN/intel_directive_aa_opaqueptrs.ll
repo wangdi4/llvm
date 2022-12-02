wangdi4/llvm

commit aafeb9be9818dbd7bd33c31e940676592603b512

41504: OpenMP end directives should be treated by AA, the same as the begin directive. Handle OpenMP directives in callCapturesBefore, as it may override BasicAA. (#8562)
Support barrier/ordered in AA.

llvm/lib/Analysis/AliasAnalysis.cpp
@@ -57,6 +57,9 @@
 #include "llvm/Analysis/TargetLibraryInfo.h"
 #include "llvm/Analysis/TypeBasedAliasAnalysis.h"
 #include "llvm/Analysis/ValueTracking.h"
+#if INTEL_COLLAB
+#include "llvm/Analysis/VPO/Utils/VPOAnalysisUtils.h"
+#endif // INTEL_COLLAB
 #include "llvm/IR/Argument.h"
 #include "llvm/IR/Attributes.h"
 #include "llvm/IR/BasicBlock.h"
@@ -279,7 +282,6 @@ ModRefInfo AAResults::getModRefInfo(const CallBase *Call,
                                     const MemoryLocation &Loc,
                                     AAQueryInfo &AAQI) {
   ModRefInfo Result = ModRefInfo::ModRef;
-
   for (const auto &AA : AAs) {
     Result &= AA->getModRefInfo(Call, Loc, AAQI);
 
@@ -837,6 +839,44 @@ ModRefInfo AAResults::getModRefInfo(const Instruction *I,
   }
 }
 
+#if INTEL_COLLAB
+// Tests the operand bundles on an OpenMP intrinsic, against the given Loc.
+// Returns ModRef if there may/must be an alias, Ref otherwise.
+// Does not analyze the call parameters.
+ModRefInfo AAResults::getDirectiveModRefInfo(const CallBase *Call,
+                                             const MemoryLocation &Loc,
+                                             AAQueryInfo &AAQI) {
+  // The modref for an end directive is the modref for its begin directive.
+  // This prevents/allows motion out of both ends of the region.
+  if (vpo::VPOAnalysisUtils::isEndDirective(const_cast<CallBase *>(Call)))
+    Call = cast<const CallBase>(Call->getOperand(0));
+
+  switch (vpo::VPOAnalysisUtils::getDirectiveID(Call)) {
+  case DIR_OMP_BARRIER:
+  case DIR_OMP_ORDERED:
+  case DIR_OMP_FLUSH:
+    return ModRefInfo::ModRef;
+  }
+
+  for (unsigned I = 0; I < Call->getNumOperandBundles(); ++I) {
+    OperandBundleUse BU = Call->getOperandBundleAt(I);
+    // Check for aliasing on all the bundle inputs.
+    // Nontyped MAP clauses may have multiple values in the inputs.
+    for (const Use &U : BU.Inputs) {
+      Value *V = U;
+      if (V->getType()->isPointerTy()) {
+        AliasResult AR =
+            AAQI.AAR.alias(MemoryLocation::getBeforeOrAfter(V), Loc, AAQI);
+        if (AR != AliasResult::NoAlias)
+          return ModRefInfo::ModRef;
+      }
+    }
+  }
+  // Even if all are "NoAlias" (or no operand bundles), return "Ref".
+  // We want to avoid store motion across calls with operand bundles.
+  return ModRefInfo::Ref;
+}
+#endif // INTEL_COLLAB
 /// Return information about whether a particular call site modifies
 /// or reads the specified memory location \p MemLoc before instruction \p I
 /// in a BasicBlock.
@@ -858,6 +898,13 @@ ModRefInfo AAResults::callCapturesBefore(const Instruction *I,
   const auto *Call = dyn_cast<CallBase>(I);
   if (!Call || Call == Object)
     return ModRefInfo::ModRef;
+#if INTEL_COLLAB
+  // "callCapturesBefore" means "Does this call really modref, based on
+  // previous captures". It overrides other AA results, so we need to
+  // analyze OpenMP directives here, as well as in BasicAA.
+  if (vpo::VPOAnalysisUtils::isOpenMPDirective(const_cast<CallBase *>(Call)))
+    return getDirectiveModRefInfo(Call, MemLoc, AAQI);
+#endif // INTEL_COLLAB
 
   if (PointerMayBeCapturedBefore(Object, /* ReturnCaptures */ true,
                                  /* StoreCaptures */ true, I, DT,

llvm/test/Transforms/GVN/intel_directive_aa_opaqueptrs.ll
@@ -1,0 +1,127 @@
+; RUN: opt -passes="gvn" -S %s | FileCheck %s
+
+; Test load/store motion around VPO directives.
+; In the function "guard", the GUARD directive references x.red, and should
+; block motion through the top and bottom of the region.
+
+; The function "simd_private" has a SIMD PRIVATE clause and should also
+; block motion in the same way.
+
+; In the function "par_shared", we should not have undeclared live-in live-out
+; from the region, such as %f or %tmp.
+; The SHARED operands should prevent propagation of values that they alias.
+
+; CHECK-LABEL: guard
+; CHECK-LABEL: DIR.VPO.GUARD.MEM.MOTION.3:
+; CHECK: load float, ptr %x.red
+; CHECK-LABEL: DIR.OMP.END.SIMD.6:
+; CHECK: load float, ptr %x.red
+
+; CHECK-LABEL: simd_private
+; CHECK-LABEL: DIR.OMP.END.SIMD.6:
+; CHECK: load float, ptr %x.red
+
+; CHECK-LABEL: par_shared
+; CHECK-LABEL: DIR.OMP.PAR.1:
+; CHECK: load float, ptr %x
+; CHECK-LABEL: DIR.OMP.END.PAR.6:
+; CHECK: load float, ptr %x
+; CHECK: load float, ptr %y
+
+target datalayout = "e-m:e-p270:32:32-p271:32:32-p272:64:64-i64:64-f80:128-n8:16:32:64-S128"
+target triple = "x86_64-unknown-linux-gnu"
+
+define dso_local float @guard() local_unnamed_addr #0 {
+DIR.OMP.SIMD.131:
+  %x.red = alloca float, align 4
+  store float 6.0, ptr %x.red, align 4
+  br label %DIR.OMP.SIMD.1
+
+DIR.OMP.SIMD.1:                                   ; preds = %DIR.OMP.SIMD.131
+  %i = call token @llvm.directive.region.entry() [ "DIR.OMP.SIMD"()]
+  br label %DIR.VPO.GUARD.MEM.MOTION.3
+
+DIR.VPO.GUARD.MEM.MOTION.3:                       ; preds = %DIR.OMP.SIMD.1
+  %guard.start = call token @llvm.directive.region.entry() [ "DIR.VPO.GUARD.MEM.MOTION"(),
+    "QUAL.OMP.LIVEIN"(ptr %x.red) ]
+  %load1 = load float, ptr %x.red, align 4
+  br label %DIR.OMP.SCAN.5
+
+DIR.OMP.SCAN.5:                                   ; preds = %DIR.VPO.GUARD.MEM.MOTION.3
+  br label %DIR.OMP.END.SCAN.7
+
+DIR.OMP.END.SCAN.7:                               ; preds = %DIR.OMP.SCAN.5
+  store float 5.0, ptr %x.red, align 4
+  br label %DIR.VPO.END.GUARD.MEM.MOTION.9
+
+DIR.VPO.END.GUARD.MEM.MOTION.9:                   ; preds = %DIR.OMP.END.SCAN.7
+  call void @llvm.directive.region.exit(token %guard.start) [ "DIR.VPO.END.GUARD.MEM.MOTION"() ]
+  br label %DIR.OMP.END.SIMD.6
+
+DIR.OMP.END.SIMD.6:                               ; preds = %DIR.VPO.END.GUARD.MEM.MOTION.9
+  call void @llvm.directive.region.exit(token %i) [ "DIR.OMP.END.SIMD"() ]
+  %load2 = load float, ptr %x.red, align 4
+  %rez = fadd float %load1, %load2
+  ret float %rez
+}
+
+define dso_local float @simd_private() local_unnamed_addr #0 {
+DIR.OMP.SIMD.131:
+  %x.red = alloca float, align 4
+  store float 6.0, ptr %x.red, align 4
+  br label %DIR.OMP.SIMD.1
+
+DIR.OMP.SIMD.1:                                   ; preds = %DIR.OMP.SIMD.131
+  %i = call token @llvm.directive.region.entry() [ "DIR.OMP.SIMD"(),
+    "QUAL.OMP.PRIVATE:TYPED"(ptr %x.red, i32 0, i32 1)]
+  store float 5.0, ptr %x.red, align 4
+  br label %DIR.OMP.END.SIMD.6
+
+DIR.OMP.END.SIMD.6:
+  call void @llvm.directive.region.exit(token %i) [ "DIR.OMP.END.SIMD"() ]
+  %load1 = load float, ptr %x.red, align 4
+  ret float %load1
+}
+
+define dso_local float @par_shared(float %f) local_unnamed_addr #0 {
+DIR.OMP.PAR.131:
+  %x = alloca float, align 4
+  %y = alloca float, align 4
+  store float %f, ptr %x, align 4
+  br label %DIR.OMP.PAR.1
+
+DIR.OMP.PAR.1:                                   ; preds = %DIR.OMP.SIMD.131
+  %i = call token @llvm.directive.region.entry() [ "DIR.OMP.PARALLEL"(),
+    "QUAL.OMP.SHARED:TYPED"(ptr %x, i32 0, i32 1),
+    "QUAL.OMP.SHARED:TYPED"(ptr %y, i32 0, i32 1)]
+  %tmp = load float, ptr %x, align 4
+  store float %tmp, ptr %y, align 4
+  br label %DIR.OMP.END.PAR.6
+
+DIR.OMP.END.PAR.6:
+  call void @llvm.directive.region.exit(token %i) [ "DIR.OMP.END.PARALLEL"() ]
+  %load1 = load float, ptr %x, align 4
+  %load2 = load float, ptr %y, align 4
+  %rez = fadd float %load1, %load2
+  ret float %rez
+}
+
+; Function Attrs: nounwind
+declare token @llvm.directive.region.entry() #1
+
+; Function Attrs: nounwind
+declare void @llvm.directive.region.exit(token) #1
+
+; Function Attrs: argmemonly nocallback nofree nosync nounwind willreturn
+declare void @llvm.lifetime.start.p0(i64 immarg, ptr nocapture) #2
+
+; Function Attrs: argmemonly nocallback nofree nosync nounwind willreturn
+declare void @llvm.lifetime.end.p0(i64 immarg, ptr nocapture) #2
+
+attributes #0 = { "unsafe-fp-math"="true" }
+attributes #1 = { nounwind }
+attributes #2 = { argmemonly nocallback nofree nosync nounwind willreturn }
+
+!llvm.ident = !{!0}
+
+!0 = !{!"Intel(R) oneAPI DPC++/C++ Compiler 2023.1.0 (2023.x.0.YYYYMMDD)"}

llvm/include/llvm/Analysis/AliasAnalysis.h
@@ -782,6 +782,11 @@ public:
   ModRefInfo callCapturesBefore(const Instruction *I,
                                 const MemoryLocation &MemLoc, DominatorTree *DT,
                                 AAQueryInfo &AAQIP);
+#if INTEL_COLLAB
+  ModRefInfo getDirectiveModRefInfo(const CallBase *Call,
+                                    const MemoryLocation &Loc,
+                                    AAQueryInfo &AAQI);
+#endif // INTEL_COLLAB
   MemoryEffects getMemoryEffects(const CallBase *Call, AAQueryInfo &AAQI);
 
 private:

llvm/include/llvm/Analysis/BasicAliasAnalysis.h
@@ -125,15 +125,10 @@ public:
   AliasResult alias(const MemoryLocation &LocA, const MemoryLocation &LocB,
                     AAQueryInfo &AAQI);
 
-#if INTEL_CUSTOMIZATION
+#if INTEL_COLLAB
   AliasResult loopCarriedAlias(const MemoryLocation &LocA,
                                const MemoryLocation &LocB, AAQueryInfo &AAQI);
-
-  // Compute modref info for a call's operand bundles.
-  ModRefInfo getDirectiveModRefInfo(const CallBase *Call,
-                                    const MemoryLocation &Loc,
-                                    AAQueryInfo &AAQI);
-#endif // INTEL_CUSTOMIZATION
+#endif // INTEL_COLLAB
 
   ModRefInfo getModRefInfo(const CallBase *Call, const MemoryLocation &Loc,
                            AAQueryInfo &AAQI);

llvm/lib/Analysis/BasicAliasAnalysis.cpp
@@ -44,7 +44,9 @@
 #include "llvm/Analysis/MemoryLocation.h"
 #include "llvm/Analysis/PhiValues.h"
 #include "llvm/Analysis/TargetLibraryInfo.h"
-#include "llvm/Analysis/VPO/Utils/VPOAnalysisUtils.h" // INTEL
+#if INTEL_COLLAB
+#include "llvm/Analysis/VPO/Utils/VPOAnalysisUtils.h"
+#endif // INTEL_COLLAB
 #include "llvm/Analysis/ValueTracking.h"
 #include "llvm/IR/Argument.h"
 #include "llvm/IR/Attributes.h"
@@ -1049,31 +1051,6 @@ AliasResult BasicAAResult::alias(const MemoryLocation &LocA,
   return aliasCheck(LocA.Ptr, LocA.Size, LocB.Ptr, LocB.Size, AAQI);
 }
 
-#if INTEL_CUSTOMIZATION
-// Tests the operand bundles on a call, against the given Loc.
-// Returns ModRef if there may/must be an alias, Ref otherwise.
-// Does not analyze the call parameters.
-ModRefInfo BasicAAResult::getDirectiveModRefInfo(const CallBase *Call,
-                                                 const MemoryLocation &Loc,
-                                                 AAQueryInfo &AAQI) {
-  for (unsigned I = 0; I < Call->getNumOperandBundles(); ++I) {
-    OperandBundleUse BU = Call->getOperandBundleAt(I);
-    for (const Use &U : BU.Inputs) {
-      Value *V = U;
-      if (V->getType()->isPointerTy()) {
-        AliasResult AR = AAQI.AAR.alias(
-            MemoryLocation::getBeforeOrAfter(V), Loc, AAQI);
-        if (AR != AliasResult::NoAlias)
-          return ModRefInfo::ModRef;
-      }
-    }
-  }
-  // Even if all are "NoAlias" (or no operand bundles), return "Ref".
-  // We want to avoid store motion across calls with operand bundles.
-  return ModRefInfo::Ref;
-}
-#endif // INTEL_CUSTOMIZATION
-
 /// Checks to see if the specified callsite can clobber the specified memory
 /// object.
 ///
@@ -1087,15 +1064,13 @@ ModRefInfo BasicAAResult::getModRefInfo(const CallBase *Call,
          "AliasAnalysis query involving multiple functions!");
 
   const Value *Object = getUnderlyingObject(Loc.Ptr);
-
-#if INTEL_CUSTOMIZATION
-  if (vpo::VPOAnalysisUtils::getDirectiveID(Call) == DIR_OMP_SIMD ||
-       vpo::VPOAnalysisUtils::getDirectiveID(Call) == DIR_OMP_END_SIMD)
-    // With SIMD directives, we do not examine the parameters, just
-    // the bundle operands.
-    return getDirectiveModRefInfo(Call, Loc, AAQI);
-#endif // INTEL_CUSTOMIZATION
-
+#if INTEL_COLLAB
+  // Scan the bundle operands of OMP directives. If a bundle operand aliases
+  // the Loc, the OMP directive may  modref the Loc. This prevents
+  // motion in/out of OMP regions.
+  if (vpo::VPOAnalysisUtils::isOpenMPDirective(const_cast<CallBase *>(Call)))
+    return AAQI.AAR.getDirectiveModRefInfo(Call, Loc, AAQI);
+#endif // INTEL_COLLAB
   // Calls marked 'tail' cannot read or write allocas from the current frame
   // because the current frame might be destroyed by the time they run. However,
   // a tail call may use an alloca with byval. Calling with byval copies the

llvm/lib/Analysis/CaptureTracking.cpp
@@ -391,8 +391,10 @@ UseCaptureKind llvm::DetermineUseCaptureKind(
 #endif // INTEL_CUSTOMIZATION
 #if INTEL_COLLAB
     if (auto *II = dyn_cast<IntrinsicInst>(I)) {
-      // The region directive itself does not capture its arguments, and
-      // its use is not important here.
+      // Calls with op bundles capture all the operands by default. Capture
+      // is a strong effect, which prevents optimization across all
+      // unknown stores and calls that occur after the directive. We declare
+      // "nocapture" here, and then block illegal motion in BasicAA.
       if (II->getIntrinsicID() == Intrinsic::directive_region_entry)
         return UseCaptureKind::NO_CAPTURE;
     }

commit 6f2b557c3b8027f6eab1af97708f9dad28e40a0b

42132: Allow reduction and live-in operands to live out of a SIMD region exit. Fixes performance regression from aafeb9b. (#8851)

llvm/lib/Analysis/AliasAnalysis.cpp
@@ -790,12 +790,27 @@ ModRefInfo AAResults::getModRefInfo(const Instruction *I,
 }
 
 #if INTEL_COLLAB
+static bool isSimpleReductionClause(StringRef ClauseStr) {
+  // Allowing no-mod on reduction operands, allows the private-copy reduction
+  // result to live outside the region. For basic-type reductions in SIMD
+  // regions, the vectorizer can detect this, and the copy removal can improve
+  // performance. For multi-instruction reductions such as UDRs and scans, the
+  // private reduction variable must stay inside the region.
+  int ClauseID = vpo::VPOAnalysisUtils::getClauseID(ClauseStr);
+  return vpo::VPOAnalysisUtils::isReductionClause(ClauseID) &&
+         ClauseID != QUAL_OMP_REDUCTION_UDR &&
+         ClauseID != QUAL_OMP_INREDUCTION_UDR && !ClauseStr.contains("SCAN");
+}
+
 // Tests the operand bundles on an OpenMP intrinsic, against the given Loc.
 // Returns ModRef if there may/must be an alias, Ref otherwise.
 // Does not analyze the call parameters.
 ModRefInfo AAResults::getDirectiveModRefInfo(const CallBase *Call,
                                              const MemoryLocation &Loc,
                                              AAQueryInfo &AAQI) {
+  bool isEndSimd =
+      vpo::VPOAnalysisUtils::getDirectiveID(Call) == DIR_OMP_END_SIMD;
+
   // The modref for an end directive is the modref for its begin directive.
   // This prevents/allows motion out of both ends of the region.
   if (vpo::VPOAnalysisUtils::isEndDirective(const_cast<CallBase *>(Call)))
@@ -808,8 +823,26 @@ ModRefInfo AAResults::getDirectiveModRefInfo(const CallBase *Call,
     return ModRefInfo::ModRef;
   }
 
+  // Thread-locals should never live in-out of a potential parallel region.
+  if (auto *GV = dyn_cast<GlobalValue>(Loc.Ptr))
+    if ((GV->isThreadLocal() || GV->isThreadPrivate()) &&
+        vpo::VPOAnalysisUtils::getDirectiveID(Call) != DIR_OMP_SIMD)
+      return ModRefInfo::ModRef;
+
   for (unsigned I = 0; I < Call->getNumOperandBundles(); ++I) {
     OperandBundleUse BU = Call->getOperandBundleAt(I);
+    // Some operands should be blocked from hoisting above the region entry,
+    // but can still be partially registerized as a live-out, if the
+    // region is SIMD.
+    if (isEndSimd) {
+      StringRef ClauseName = BU.getTagName();
+      if (isSimpleReductionClause(ClauseName))
+        continue;
+      else if (vpo::VPOAnalysisUtils::getClauseID(ClauseName) ==
+               QUAL_OMP_LIVEIN)
+        continue;
+    }
+
     // Check for aliasing on all the bundle inputs.
     // Nontyped MAP clauses may have multiple values in the inputs.
     for (const Use &U : BU.Inputs) {
@@ -823,7 +856,8 @@ ModRefInfo AAResults::getDirectiveModRefInfo(const CallBase *Call,
     }
   }
   // Even if all are "NoAlias" (or no operand bundles), return "Ref".
-  // We want to avoid store motion across calls with operand bundles.
+  // We want to ensure that objects are stored back to memory at the end
+  // of the region.
   return ModRefInfo::Ref;
 }
 #endif // INTEL_COLLAB

llvm/test/Transforms/GVN/intel_directive_aa_opaqueptrs.ll
@@ -11,6 +11,17 @@
 ; from the region, such as %f or %tmp.
 ; The SHARED operands should prevent propagation of values that they alias.
 
+; The function "reduce" has a SIMD loop computing a reduction.
+; The stored reduction result can be reused live-out of the loop, instead of
+; forcing a reload.
+
+; The function "f90_livein" has a global which is declared "LIVE_IN" to the
+; SIMD loop. This global can be partiall registerized in the loop and the last
+; value can be reused at the region exit.
+
+; The function "thread_local" tests thread_local and thread_private globals.
+; Neither should be moved across the region.
+
 ; CHECK-LABEL: guard
 ; CHECK-LABEL: DIR.VPO.GUARD.MEM.MOTION.3:
 ; CHECK: load float, ptr %x.red
@@ -27,6 +38,21 @@
 ; CHECK-LABEL: DIR.OMP.END.PAR.6:
 ; CHECK: load float, ptr %x
 ; CHECK: load float, ptr %y
+
+; CHECK-LABEL: reduce
+; CHECK-LABEL: DIR.OMP.END.SIMD.2:
+; CHECK-NOT: load
+; CHECK: ret float %conv4
+
+; CHECK-LABEL: f90_livein
+; CHECK-LABEL: DIR.OMP.END.SIMD.46:
+; CHECK-NEXT: [[RET:%.*]] = phi float
+; CHECK-NEXT: ret float [[RET]]
+
+; CHECK-LABEL: thread_local
+; CHECK-LABEL: DIR.OMP.END.PARALLEL:
+; CHECK: load float, ptr @tp
+; CHECK: load float, ptr @tl
 
 target datalayout = "e-m:e-p270:32:32-p271:32:32-p272:64:64-i64:64-f80:128-n8:16:32:64-S128"
 target triple = "x86_64-unknown-linux-gnu"
@@ -106,6 +132,126 @@ DIR.OMP.END.PAR.6:
   ret float %rez
 }
 
+; Function Attrs: mustprogress nounwind uwtable
+define dso_local noundef float @reduce(ptr nocapture noundef readonly %a) local_unnamed_addr #0 {
+omp.inner.for.body.lr.ph:
+  %total.red = alloca float, align 4
+  %i.linear.iv = alloca i32, align 4
+  store float 0.000000e+00, ptr %total.red, align 4
+  br label %DIR.OMP.SIMD.1
+
+DIR.OMP.SIMD.1:                                   ; preds = %omp.inner.for.body.lr.ph
+  %0 = call token @llvm.directive.region.entry() [ "DIR.OMP.SIMD"(),
+    "QUAL.OMP.REDUCTION.ADD:TYPED"(ptr %total.red, float 0.000000e+00, i32 1),
+    "QUAL.OMP.NORMALIZED.IV:TYPED"(ptr null, i32 0),
+    "QUAL.OMP.NORMALIZED.UB:TYPED"(ptr null, i32 0),
+    "QUAL.OMP.LINEAR:IV.TYPED"(ptr %i.linear.iv, i32 0, i32 1, i32 1) ]
+
+  br label %DIR.OMP.SIMD.118
+
+DIR.OMP.SIMD.118:                                 ; preds = %DIR.OMP.SIMD.1
+  %total.red.promoted = load float, ptr %total.red, align 4
+  br label %omp.inner.for.body
+
+omp.inner.for.body:                               ; preds = %DIR.OMP.SIMD.118, %omp.inner.for.body
+  %indvars.iv = phi i64 [ 0, %DIR.OMP.SIMD.118 ], [ %indvars.iv.next, %omp.inner.for.body ]
+  %1 = phi float [ %total.red.promoted, %DIR.OMP.SIMD.118 ], [ %add1, %omp.inner.for.body ]
+  call void @llvm.lifetime.start.p0(i64 4, ptr nonnull %i.linear.iv) #2
+  %2 = trunc i64 %indvars.iv to i32
+  store i32 %2, ptr %i.linear.iv, align 4
+  %arrayidx = getelementptr inbounds float, ptr %a, i64 %indvars.iv
+  %3 = load float, ptr %arrayidx, align 4
+  %add1 = fadd fast float %1, %3
+  call void @llvm.lifetime.end.p0(i64 4, ptr nonnull %i.linear.iv) #2
+  %indvars.iv.next = add nuw nsw i64 %indvars.iv, 1
+  %exitcond = icmp ne i64 %indvars.iv.next, 100
+  br i1 %exitcond, label %omp.inner.for.body, label %DIR.OMP.END.SIMD.410
+
+DIR.OMP.END.SIMD.410:                             ; preds = %omp.inner.for.body
+  %add1.lcssa = phi float [ %add1, %omp.inner.for.body ]
+  store float %add1.lcssa, ptr %total.red, align 4
+  br label %DIR.OMP.END.SIMD.2
+
+DIR.OMP.END.SIMD.2:                               ; preds = %DIR.OMP.END.SIMD.410
+  call void @llvm.directive.region.exit(token %0) [ "DIR.OMP.END.SIMD"() ]
+
+  br label %DIR.OMP.END.SIMD.3
+
+DIR.OMP.END.SIMD.3:                               ; preds = %DIR.OMP.END.SIMD.2
+  %4 = load float, ptr %total.red, align 4
+  %conv4 = fadd fast float %4, 1.050000e+01
+  ret float %conv4
+}
+
+@test_mp_sum_ = common global float 0.000000e+00, align 8
+
+; Function Attrs: nounwind uwtable
+define float @f90_livein(ptr noalias nocapture dereferenceable(4) %A, ptr noalias nocapture dereferenceable(4) %N) local_unnamed_addr #0 {
+DIR.OMP.SIMD.1:
+  %"test_mp_reduce_$I.linear.iv" = alloca i32, align 4
+  %N_fetch.1 = load i32, ptr %N, align 1
+  %rel.1.not7 = icmp slt i32 %N_fetch.1, 1
+  br i1 %rel.1.not7, label %DIR.OMP.END.SIMD.46, label %omp.pdo.body5.lr.ph
+
+omp.pdo.body5:                                    ; preds = %omp.pdo.body5.lr.ph, %omp.pdo.body5
+  %indvars.iv = phi i64 [ 0, %omp.pdo.body5.lr.ph ], [ %indvars.iv.next, %omp.pdo.body5 ]
+  %test_mp_sum__fetch.710 = phi float [ %test_mp_sum_.promoted, %omp.pdo.body5.lr.ph ], [ %add.3, %omp.pdo.body5 ]
+  %indvars.iv.next = add nuw nsw i64 %indvars.iv, 1
+  %"A[]" = call ptr @llvm.intel.subscript.p0.i64.i64.p0.i64(i8 0, i64 1, i64 4, ptr nonnull elementtype(float) %A, i64 %indvars.iv.next)
+  %"A[]_fetch.9" = load float, ptr %"A[]", align 1
+  %add.3 = fadd reassoc ninf nsz arcp contract afn float %"A[]_fetch.9", %test_mp_sum__fetch.710
+  %exitcond = icmp ne i64 %indvars.iv.next, %wide.trip.count
+  br i1 %exitcond, label %omp.pdo.body5, label %omp.pdo.cond4.DIR.OMP.END.SIMD.4.loopexit_crit_edge
+
+omp.pdo.body5.lr.ph:                              ; preds = %DIR.OMP.SIMD.1
+  %0 = call token @llvm.directive.region.entry() [ "DIR.OMP.SIMD"(),
+    "QUAL.OMP.LINEAR:IV.TYPED"(ptr %"test_mp_reduce_$I.linear.iv", i32 0, i32 1),
+    "QUAL.OMP.NORMALIZED.IV:TYPED"(ptr null, i32 0),
+    "QUAL.OMP.NORMALIZED.UB:TYPED"(ptr null, i32 0),
+    "QUAL.OMP.LIVEIN:TYPED"(ptr %A, i32 0),
+    "QUAL.OMP.LIVEIN:TYPED"(ptr @test_mp_sum_, i32 0),
+    "QUAL.OMP.LIVEIN:TYPED"(ptr %N, i32 0) ]
+
+  %test_mp_sum_.promoted = load float, ptr @test_mp_sum_, align 8
+  %1 = add i32 %N_fetch.1, -1
+  %wide.trip.count = sext i32 %N_fetch.1 to i64
+  br label %omp.pdo.body5
+
+omp.pdo.cond4.DIR.OMP.END.SIMD.4.loopexit_crit_edge: ; preds = %omp.pdo.body5
+  %2 = zext i32 %1 to i64
+  %add.4.le = add nuw nsw i32 %1, 2
+  store i32 %add.4.le, ptr %"test_mp_reduce_$I.linear.iv", align 4
+  store float %add.3, ptr @test_mp_sum_, align 8
+  br label %DIR.OMP.END.SIMD.2
+
+DIR.OMP.END.SIMD.2:                               ; preds = %omp.pdo.cond4.DIR.OMP.END.SIMD.4.loopexit_crit_edge
+  call void @llvm.directive.region.exit(token %0) [ "DIR.OMP.END.SIMD"() ]
+
+  br label %DIR.OMP.END.SIMD.46
+
+DIR.OMP.END.SIMD.46:                              ; preds = %DIR.OMP.END.SIMD.2, %DIR.OMP.SIMD.1
+  %test_mp_sum__fetch.12 = load float, ptr @test_mp_sum_, align 8
+  ret float %test_mp_sum__fetch.12
+}
+
+@tp = thread_private global float 0.0, align 4
+@tl = thread_local global float 0.0, align 4
+
+define dso_local float @thread_local() local_unnamed_addr #0 {
+entry:
+  %i = call token @llvm.directive.region.entry() [ "DIR.OMP.PARALLEL"() ]
+  store float 5.0, ptr @tp, align 4
+  store float 5.0, ptr @tl, align 4
+  br label %DIR.OMP.END.PARALLEL
+
+DIR.OMP.END.PARALLEL:
+  call void @llvm.directive.region.exit(token %i) [ "DIR.OMP.END.PARALLEL"() ]
+  %load1 = load float, ptr @tp, align 4
+  %load2 = load float, ptr @tl, align 4
+  %sum = fadd float %load1, %load2
+  ret float %sum
+}
+
 ; Function Attrs: nounwind
 declare token @llvm.directive.region.entry() #1
 
@@ -117,6 +263,8 @@ declare void @llvm.lifetime.start.p0(i64 immarg, ptr nocapture) #2
 
 ; Function Attrs: argmemonly nocallback nofree nosync nounwind willreturn
 declare void @llvm.lifetime.end.p0(i64 immarg, ptr nocapture) #2
+
+declare ptr @llvm.intel.subscript.p0.i64.i64.p0.i64(i8, i64, i64, ptr, i64) #2
 
 attributes #0 = { "unsafe-fp-math"="true" }
 attributes #1 = { nounwind }