a1b63dfc425581278a458000baecd0ad3349a192
iclsrc <iclsrc@intel.com>
2022-12-02 13:48:12 -0800

    Resolve of merge (conflict) 26fa17ed2914 Implement CWG2631 'main' -> 'xmain-web' (#8989)
    * Resolve of merge (conflict) 26fa17ed2914 Implement CWG2631 'main' -> 'xmain-web'
    
      CONFLICT (content): Merge conflict in clang/test/SemaCXX/source_location.cpp
    
      commit 26fa17ed2914bd80c066d36b325fd3104e45554c
      Author: Corentin Jabot <corentinjabot@gmail.com>
      Date:   Sun Oct 23 17:32:58 2022 +0200
    
          Implement CWG2631
    
          Implement https://cplusplus.github.io/CWG/issues/2631.html.
    
          Immediate calls in default arguments and defaults members
          are not evaluated.
    
          Instead, we evaluate them when constructing a
          `CXXDefaultArgExpr`/`BuildCXXDefaultInitExpr`.
    
          The immediate calls are executed by doing a
          transform on the initializing expression.
    
          Note that lambdas are not considering subexpressions so
          we do not need to transform them.
    
          As a result of this patch, unused default member
          initializers are not considered odr-used, and
          errors about members binding to local variables
          in an outer scope only surface at the point
          where a constructor is defined.
    
          Reviewed By: aaron.ballman, #clang-language-wg
    
          Differential Revision: https://reviews.llvm.org/D136554
    
    * Resolve conflict
    
    Co-authored-by: iclsrc <ia.compiler.tools.git@intel.com>
    Co-authored-by: Ernesto Su <ernesto.su@intel.com>

--- clang/test/SemaCXX/source_location.cpp
+++ clang/test/SemaCXX/source_location.cpp
@@ -1,9 +1,5 @@
-<<<<<<< HEAD
 // RUN: %clang_cc1 -std=c++1z -fcxx-exceptions -fexceptions -fintel-compatibility -fintel-compatibility-enable=DisplayFullFilePath -verify %s
-=======
-// RUN: %clang_cc1 -std=c++1z -fcxx-exceptions -fexceptions -verify %s
-// RUN: %clang_cc1 -std=c++2a -fcxx-exceptions -DUSE_CONSTEVAL -fexceptions -verify %s
->>>>>>> 26fa17ed2914bd80c066d36b325fd3104e45554c
+// RUN: %clang_cc1 -std=c++2a -fcxx-exceptions -fexceptions -fintel-compatibility -fintel-compatibility-enable=DisplayFullFilePath -verify %s
 // expected-no-diagnostics
 
 #define assert(...) ((__VA_ARGS__) ? ((void)0) : throw 42)
