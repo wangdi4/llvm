973f47e286b9c6c8ee06d8c66dfbea54cdd35c38
chrulski-intel <christopher.m.chrulski@intel.com>
2022-12-13 17:42:32 -0700

    [Coverity] Fix for possible buffer overflow in PGO runtime library (#9222)
    * [Coverity] Fix for possible buffer overflow in PGO runtime library
    
    Fix for: https://coverityent.devtools.intel.com/prod6/reports.htm#v12582/p10020/fileInstanceId=383287721&defectInstanceId=166784558&mergedDefectId=2019581
    
    The call to "GetComputerNameExW" needs to pass a size value that
    is the max number of elements to be written.
      https://learn.microsoft.com/en-us/windows/win32/api/sysinfoapi/nf-sysinfoapi-getcomputernameexw
    
    The call in the PGO runtime library is instead passing the raw number
    of bytes into the routine, because it is using WCHAR elements which are
    each 2 bytes.
    
    [CMPLRLLVM-42638]
    
    * clang-format

--- compiler-rt/lib/profile/InstrProfilingUtil.c
+++ compiler-rt/lib/profile/InstrProfilingUtil.c
@@ -6,6 +6,24 @@
 |*
 \*===----------------------------------------------------------------------===*/
 
+// INTEL_CUSTOMIZATION
+//
+// INTEL CONFIDENTIAL
+//
+// Modifications, Copyright (C) 2022 Intel Corporation
+//
+// This software and the related documents are Intel copyrighted materials, and
+// your use of them is governed by the express license under which they were
+// provided to you ("License"). Unless the License provides otherwise, you may not
+// use, modify, copy, publish, distribute, disclose or transmit this software or
+// the related documents without Intel's prior written permission.
+//
+// This software and the related documents are provided as is, with no express
+// or implied warranties, other than those that are expressly stated in the
+// License.
+//
+// end INTEL_CUSTOMIZATION
+
 #ifdef _WIN32
 #include <direct.h>
 #include <process.h>
@@ -112,7 +130,7 @@ void *lprofPtrFetchAdd(void **Mem, long ByteIncr) {
 #ifdef _WIN32
 COMPILER_RT_VISIBILITY int lprofGetHostName(char *Name, int Len) {
   WCHAR Buffer[COMPILER_RT_MAX_HOSTLEN];
-  DWORD BufferSize = sizeof(Buffer);
+  DWORD BufferSize = _countof(Buffer); // INTEL
   BOOL Result =
       GetComputerNameExW(ComputerNameDnsFullyQualified, Buffer, &BufferSize);
   if (!Result)
