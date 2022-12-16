04fe2256c0fda4b9961b0db1381230864c223512
Dmitry Budanov <dmitry.budanov@intel.com>
2022-12-16 13:10:43 -0800

    Add deleted copy constructor and copy assignment operator for CFLSteensAAResult class (#9332)
    * Add deleted copy constructor and copy assignment operator for CFLSteensAAResult class
    
    * Added Intel customization mark for deleted copy ctor and copy assignment operator and Intel copyright header

--- llvm/include/llvm/Analysis/CFLSteensAliasAnalysis.h
+++ llvm/include/llvm/Analysis/CFLSteensAliasAnalysis.h
@@ -1,4 +1,21 @@
 //==- CFLSteensAliasAnalysis.h - Unification-based Alias Analysis -*- C++-*-==//
+// INTEL_CUSTOMIZATION
+//
+// INTEL CONFIDENTIAL
+//
+// Modifications, Copyright (C) 2022 Intel Corporation
+//
+// This software and the related documents are Intel copyrighted materials, and
+// your use of them is governed by the express license under which they were
+// provided to you ("License"). Unless the License provides otherwise, you may
+// not use, modify, copy, publish, distribute, disclose or transmit this
+// software or the related documents without Intel's prior written permission.
+//
+// This software and the related documents are provided as is, with no express
+// or implied warranties, other than those that are expressly stated in the
+// License.
+//
+// end INTEL_CUSTOMIZATION
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
@@ -41,6 +58,8 @@ public:
   explicit CFLSteensAAResult(
       std::function<const TargetLibraryInfo &(Function &)> GetTLI);
   CFLSteensAAResult(CFLSteensAAResult &&Arg);
+  CFLSteensAAResult(const CFLSteensAAResult &) = delete;            // INTEL
+  CFLSteensAAResult &operator=(const CFLSteensAAResult &) = delete; // INTEL
   ~CFLSteensAAResult();
 
   /// Handle invalidation events from the new pass manager.
