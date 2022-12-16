6f4902f59a1052cd2d842a12fc579f26f0d701f6
iclsrc <iclsrc@intel.com>
2022-12-16 07:44:28 -0800

    Resolve of merge (conflict) 49c86eab627b Revert "Implement CWG2631" 'main' -> 'xmain-web' (#9327)
    * Resolve of merge (conflict) 49c86eab627b Revert "Implement CWG2631" 'main' -> 'xmain-web'
    
      CONFLICT (content): Merge conflict in clang/test/SemaCXX/source_location.cpp
    
      commit 49c86eab627b0c4089c8d25dac48728c656448a9
      Author: Corentin Jabot <corentinjabot@gmail.com>
      Date:   Thu Dec 8 19:57:02 2022 +0100
    
          Revert "Implement CWG2631"
    
          This patch causes another link error,
          reverting until I can investigate.
    
          This reverts commit a96a6ed83230265f3eab09a94f0e9525d05f8a74.
    
    * Resolution
    
    Co-authored-by: iclsrc <ia.compiler.tools.git@intel.com>
    Co-authored-by: KornevNikita <nikita.kornev@intel.com>

--- clang/test/SemaCXX/source_location.cpp
+++ clang/test/SemaCXX/source_location.cpp
@@ -1,9 +1,4 @@
-<<<<<<< HEAD
 // RUN: %clang_cc1 -std=c++1z -fcxx-exceptions -fexceptions -fintel-compatibility -fintel-compatibility-enable=DisplayFullFilePath -verify %s
-// RUN: %clang_cc1 -std=c++2a -fcxx-exceptions -DUSE_CONSTEVAL -fexceptions -fintel-compatibility -fintel-compatibility-enable=DisplayFullFilePath -verify %s
-=======
-// RUN: %clang_cc1 -std=c++1z -fcxx-exceptions -fexceptions -verify %s
->>>>>>> 49c86eab627b0c4089c8d25dac48728c656448a9
 // expected-no-diagnostics
 
 #define assert(...) ((__VA_ARGS__) ? ((void)0) : throw 42)
