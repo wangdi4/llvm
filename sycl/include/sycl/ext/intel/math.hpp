d5c3b3274795414c4872bb9413fbefb8779920dd
jinge90 <ge.jin@intel.com>
2022-12-22 20:53:11 +0800

    Add sin, cos, log, exp in sycl intel math header (#9097)
    * Add sin, cos, log, exp in sycl intel math header
    
    Signed-off-by: jinge90 <ge.jin@intel.com>
    
    * fix 2 careless typo
    
    Signed-off-by: jinge90 <ge.jin@intel.com>
    
    ---------
    
    Signed-off-by: jinge90 <ge.jin@intel.com>

--- sycl/include/sycl/ext/intel/math.hpp
+++ sycl/include/sycl/ext/intel/math.hpp
@@ -51,10 +51,34 @@ float __imf_cdfnormf(float);
 double __imf_cdfnorm(double);
 float __imf_cdfnorminvf(float);
 double __imf_cdfnorminv(double);
-float __imf_normf(int, const float*);
-double __imf_norm(int, const double*);
-float __imf_rnormf(int, const float*);
-double __imf_rnorm(int, const double*);
+float __imf_normf(int, const float *);
+double __imf_norm(int, const double *);
+float __imf_rnormf(int, const float *);
+double __imf_rnorm(int, const double *);
+float __imf_cosf(float);
+double __imf_cos(double);
+_iml_half_internal __imf_cosf16(_iml_half_internal);
+float __imf_exp10f(float);
+double __imf_exp10(double);
+_iml_half_internal __imf_exp10f16(_iml_half_internal);
+float __imf_exp2f(float);
+double __imf_exp2(double);
+_iml_half_internal __imf_exp2f16(_iml_half_internal);
+float __imf_expf(float);
+double __imf_exp(double);
+_iml_half_internal __imf_expf16(_iml_half_internal);
+float __imf_log10f(float);
+double __imf_log10(double);
+_iml_half_internal __imf_log10f16(_iml_half_internal);
+float __imf_log2f(float);
+double __imf_log2(double);
+_iml_half_internal __imf_log2f16(_iml_half_internal);
+float __imf_logf(float);
+double __imf_log(double);
+_iml_half_internal __imf_logf16(_iml_half_internal);
+float __imf_sinf(float);
+double __imf_sin(double);
+_iml_half_internal __imf_sinf16(_iml_half_internal);
 /* end INTEL_CUSTOMIZATION */
 float __imf_ceilf(float);
 double __imf_ceil(double);
@@ -123,12 +147,12 @@ std::enable_if_t<std::is_same_v<Tp, double>, double> erfinv(Tp x) {
 
 template <typename Tp>
 std::enable_if_t<std::is_same_v<Tp, float>, float> erfcinv(Tp x) {
-  return __imf_erfinvf(x);
+  return __imf_erfcinvf(x);
 }
 
 template <typename Tp>
 std::enable_if_t<std::is_same_v<Tp, double>, double> erfcinv(Tp x) {
-  return __imf_erfinv(x);
+  return __imf_erfcinv(x);
 }
 
 template <typename Tp>
@@ -152,23 +176,193 @@ std::enable_if_t<std::is_same_v<Tp, double>, double> cdfnorminv(Tp x) {
 }
 
 template <typename Tp>
-std::enable_if_t<std::is_same_v<Tp, float>, float> norm(int dim, const Tp* p) {
+std::enable_if_t<std::is_same_v<Tp, float>, float> norm(int dim, const Tp *p) {
   return __imf_normf(dim, p);
 }
 
 template <typename Tp>
-std::enable_if_t<std::is_same_v<Tp, double>, double> norm(int dim, const Tp* p) {
+std::enable_if_t<std::is_same_v<Tp, double>, double> norm(int dim,
+                                                          const Tp *p) {
   return __imf_norm(dim, p);
 }
 
 template <typename Tp>
-std::enable_if_t<std::is_same_v<Tp, float>, float> rnorm(int dim, const Tp* p) {
+std::enable_if_t<std::is_same_v<Tp, float>, float> rnorm(int dim, const Tp *p) {
   return __imf_rnormf(dim, p);
 }
 
 template <typename Tp>
-std::enable_if_t<std::is_same_v<Tp, double>, double> rnorm(int dim, const Tp* p) {
+std::enable_if_t<std::is_same_v<Tp, double>, double> rnorm(int dim,
+                                                           const Tp *p) {
   return __imf_rnorm(dim, p);
+}
+
+template <typename Tp>
+std::enable_if_t<std::is_same_v<Tp, float>, float> cos(Tp x) {
+  return __imf_cosf(x);
+}
+
+template <typename Tp>
+std::enable_if_t<std::is_same_v<Tp, double>, double> cos(Tp x) {
+  return __imf_cos(x);
+}
+
+template <typename Tp>
+std::enable_if_t<std::is_same_v<Tp, sycl::half>, sycl::half> cos(Tp x) {
+  _iml_half_internal xi = __builtin_bit_cast(_iml_half_internal, x);
+  return __builtin_bit_cast(sycl::half, __imf_cosf16(xi));
+}
+
+template <typename Tp>
+std::enable_if_t<std::is_same_v<Tp, sycl::half2>, sycl::half2> cos(Tp x) {
+  return sycl::half2{cos(x.s0()), cos(x.s1())};
+}
+
+template <typename Tp>
+std::enable_if_t<std::is_same_v<Tp, float>, float> exp10(Tp x) {
+  return __imf_exp10f(x);
+}
+
+template <typename Tp>
+std::enable_if_t<std::is_same_v<Tp, double>, double> exp10(Tp x) {
+  return __imf_exp10(x);
+}
+
+template <typename Tp>
+std::enable_if_t<std::is_same_v<Tp, sycl::half>, sycl::half> exp10(Tp x) {
+  _iml_half_internal xi = __builtin_bit_cast(_iml_half_internal, x);
+  return __builtin_bit_cast(sycl::half, __imf_exp10f16(xi));
+}
+
+template <typename Tp>
+std::enable_if_t<std::is_same_v<Tp, sycl::half2>, sycl::half2> exp10(Tp x) {
+  return sycl::half2{exp10(x.s0()), exp10(x.s1())};
+}
+
+template <typename Tp>
+std::enable_if_t<std::is_same_v<Tp, float>, float> exp2(Tp x) {
+  return __imf_exp2f(x);
+}
+
+template <typename Tp>
+std::enable_if_t<std::is_same_v<Tp, double>, double> exp2(Tp x) {
+  return __imf_exp2(x);
+}
+
+template <typename Tp>
+std::enable_if_t<std::is_same_v<Tp, sycl::half>, sycl::half> exp2(Tp x) {
+  _iml_half_internal xi = __builtin_bit_cast(_iml_half_internal, x);
+  return __builtin_bit_cast(sycl::half, __imf_exp2f16(xi));
+}
+
+template <typename Tp>
+std::enable_if_t<std::is_same_v<Tp, sycl::half2>, sycl::half2> exp2(Tp x) {
+  return sycl::half2{exp2(x.s0()), exp2(x.s1())};
+}
+
+template <typename Tp>
+std::enable_if_t<std::is_same_v<Tp, float>, float> exp(Tp x) {
+  return __imf_expf(x);
+}
+
+template <typename Tp>
+std::enable_if_t<std::is_same_v<Tp, double>, double> exp(Tp x) {
+  return __imf_exp(x);
+}
+
+template <typename Tp>
+std::enable_if_t<std::is_same_v<Tp, sycl::half>, sycl::half> exp(Tp x) {
+  _iml_half_internal xi = __builtin_bit_cast(_iml_half_internal, x);
+  return __builtin_bit_cast(sycl::half, __imf_expf16(xi));
+}
+
+template <typename Tp>
+std::enable_if_t<std::is_same_v<Tp, sycl::half2>, sycl::half2> exp(Tp x) {
+  return sycl::half2{exp(x.s0()), exp(x.s1())};
+}
+
+template <typename Tp>
+std::enable_if_t<std::is_same_v<Tp, float>, float> log10(Tp x) {
+  return __imf_log10f(x);
+}
+
+template <typename Tp>
+std::enable_if_t<std::is_same_v<Tp, double>, double> log10(Tp x) {
+  return __imf_log10(x);
+}
+
+template <typename Tp>
+std::enable_if_t<std::is_same_v<Tp, sycl::half>, sycl::half> log10(Tp x) {
+  _iml_half_internal xi = __builtin_bit_cast(_iml_half_internal, x);
+  return __builtin_bit_cast(sycl::half, __imf_log10f16(xi));
+}
+
+template <typename Tp>
+std::enable_if_t<std::is_same_v<Tp, sycl::half2>, sycl::half2> log10(Tp x) {
+  return sycl::half2{log10(x.s0()), log10(x.s1())};
+}
+
+template <typename Tp>
+std::enable_if_t<std::is_same_v<Tp, float>, float> log2(Tp x) {
+  return __imf_log2f(x);
+}
+
+template <typename Tp>
+std::enable_if_t<std::is_same_v<Tp, double>, double> log2(Tp x) {
+  return __imf_log2(x);
+}
+
+template <typename Tp>
+std::enable_if_t<std::is_same_v<Tp, sycl::half>, sycl::half> log2(Tp x) {
+  _iml_half_internal xi = __builtin_bit_cast(_iml_half_internal, x);
+  return __builtin_bit_cast(sycl::half, __imf_log2f16(xi));
+}
+
+template <typename Tp>
+std::enable_if_t<std::is_same_v<Tp, sycl::half2>, sycl::half2> log2(Tp x) {
+  return sycl::half2{log2(x.s0()), log2(x.s1())};
+}
+
+template <typename Tp>
+std::enable_if_t<std::is_same_v<Tp, float>, float> log(Tp x) {
+  return __imf_logf(x);
+}
+
+template <typename Tp>
+std::enable_if_t<std::is_same_v<Tp, double>, double> log(Tp x) {
+  return __imf_log(x);
+}
+
+template <typename Tp>
+std::enable_if_t<std::is_same_v<Tp, sycl::half>, sycl::half> log(Tp x) {
+  _iml_half_internal xi = __builtin_bit_cast(_iml_half_internal, x);
+  return __builtin_bit_cast(sycl::half, __imf_logf16(xi));
+}
+
+template <typename Tp>
+std::enable_if_t<std::is_same_v<Tp, sycl::half2>, sycl::half2> log(Tp x) {
+  return sycl::half2{log(x.s0()), log(x.s1())};
+}
+
+template <typename Tp>
+std::enable_if_t<std::is_same_v<Tp, float>, float> sin(Tp x) {
+  return __imf_sinf(x);
+}
+
+template <typename Tp>
+std::enable_if_t<std::is_same_v<Tp, double>, double> sin(Tp x) {
+  return __imf_sin(x);
+}
+
+template <typename Tp>
+std::enable_if_t<std::is_same_v<Tp, sycl::half>, sycl::half> sin(Tp x) {
+  _iml_half_internal xi = __builtin_bit_cast(_iml_half_internal, x);
+  return __builtin_bit_cast(sycl::half, __imf_sinf16(xi));
+}
+
+template <typename Tp>
+std::enable_if_t<std::is_same_v<Tp, sycl::half2>, sycl::half2> sin(Tp x) {
+  return sycl::half2{sin(x.s0()), sin(x.s1())};
 }
 
 /* end INTEL_CUSTOMIZATION */
