105d296c5b675b8c79d5d87ea8c085f0cdb9b875
Feng Zou <feng.zou@intel.com>
2022-12-22 15:29:21 +0800

    Fix failure of building OpenCL builtins as commit 4f7e5d220 (#9446)
    Fix handling typed pointers in SROA pass.

--- llvm/lib/Transforms/Scalar/SROA.cpp
+++ llvm/lib/Transforms/Scalar/SROA.cpp
@@ -1650,8 +1650,18 @@ static void rewriteMemOpOfSelect(SelectInst &SI, T &I,
       ++NumLoadsSpeculated;
     auto &CondMemOp = cast<T>(*I.clone());
     CondMemOp.insertBefore(NewMemOpBB->getTerminator());
-    CondMemOp.setOperand(I.getPointerOperandIndex(),
-                         SI.getOperand(1 + SuccIdx));
+#if INTEL_CUSTOMIZATION
+    // Fix for typed pointers
+    auto *PtrType = CondMemOp.getPointerOperandType();
+    auto *Ptr = SI.getOperand(1 + SuccIdx);
+    if (PtrType != Ptr->getType()) {
+      auto *BC = new BitCastInst(Ptr, PtrType);
+      BC->insertBefore(&CondMemOp);
+      Ptr = BC;
+    }
+    CondMemOp.setOperand(I.getPointerOperandIndex(), Ptr);
+#endif // INTEL_CUSTOMIZATION
+
     if (isa<LoadInst>(I)) {
       CondMemOp.setName(I.getName() + (IsThen ? ".then" : ".else") + ".val");
       PN->addIncoming(&CondMemOp, NewMemOpBB);
