d63d7dd025afd00523416a6adad592d8b9fffbef
iclsrc <iclsrc@intel.com>
2023-01-05 16:05:14 -0800

    Resolve of merge (conflict) 37b8f09a4b61 Revert "[SimplifyCFG] `FoldBranchToCommonDest()`: deal wit.. 'main' -> 'xmain-web' (#9732)
    * Resolve of merge (conflict) 37b8f09a4b61 Revert "[SimplifyCFG] `FoldBranchToCommonDest()`: deal wit.. 'main' -> 'xmain-web'
    
      CONFLICT (content): Merge conflict in llvm/test/Transforms/SimplifyCFG/switch-on-const-select.ll
      CONFLICT (content): Merge conflict in llvm/test/Transforms/SimplifyCFG/fold-branch-to-common-dest.ll
      CONFLICT (content): Merge conflict in llvm/test/Transforms/SimplifyCFG/fold-branch-to-common-dest-phis.ll
      CONFLICT (content): Merge conflict in llvm/test/Transforms/SimplifyCFG/X86/switch_to_lookup_table.ll
      CONFLICT (content): Merge conflict in llvm/test/Transforms/SimplifyCFG/X86/SpeculativeExec.ll
      CONFLICT (content): Merge conflict in llvm/test/Transforms/SimplifyCFG/2008-07-13-InfLoopMiscompile.ll
      CONFLICT (content): Merge conflict in llvm/test/Transforms/LICM/hoist-phi.ll
      CONFLICT (content): Merge conflict in llvm/test/Transforms/InstCombine/unused-nonnull.ll
      CONFLICT (content): Merge conflict in llvm/test/CodeGen/AArch64/tailmerging_in_mbp.ll
      CONFLICT (content): Merge conflict in llvm/lib/Transforms/Utils/SimplifyCFG.cpp
    
      commit 37b8f09a4b61bf9bf9d0b9017d790c8b82be2e17
      Author: Alexander Kornienko <alexfh@google.com>
      Date:   Fri Dec 16 16:43:58 2022 +0100
    
          Revert "[SimplifyCFG] `FoldBranchToCommonDest()`: deal with mismatched IV's in PHI's in common successor block"
    
          This reverts commit 1bd0b82e508d049efdb07f4f8a342f35818df341, since it leads to
          miscompiles. See https://reviews.llvm.org/D139275#3993229 and
          https://reviews.llvm.org/D139275#4001580.
    
    * Resolve conflict in SimplifyCFG.cpp so it should build.
    
    ---------
    
    Co-authored-by: iclsrc <ia.compiler.tools.git@intel.com>
    Co-authored-by: Ernesto Su <ernesto.su@intel.com>

--- llvm/lib/Transforms/Utils/SimplifyCFG.cpp
+++ llvm/lib/Transforms/Utils/SimplifyCFG.cpp
@@ -4865,7 +4865,6 @@ static bool performBranchToCommonDestFolding(BranchInst *BI, BranchInst *PBI,
   // If we need to invert the condition in the pred block to match, do so now.
   if (InvertPredCond) {
     Value *NewCond = PBI->getCondition();
-<<<<<<< HEAD
 
 #if INTEL_CUSTOMIZATION
     // If this instruction is a compare with a single use, it is also legal to
@@ -4878,7 +4877,6 @@ static bool performBranchToCommonDestFolding(BranchInst *BI, BranchInst *PBI,
     NewCond =
         Builder.CreateNot(NewCond, PBI->getCondition()->getName() + ".not");
 #endif
-=======
     if (NewCond->hasOneUse() && isa<CmpInst>(NewCond)) {
       CmpInst *CI = cast<CmpInst>(NewCond);
       CI->setPredicate(CI->getInversePredicate());
@@ -4886,7 +4884,6 @@ static bool performBranchToCommonDestFolding(BranchInst *BI, BranchInst *PBI,
       NewCond =
           Builder.CreateNot(NewCond, PBI->getCondition()->getName() + ".not");
     }
->>>>>>> 37b8f09a4b61bf9bf9d0b9017d790c8b82be2e17
 
     PBI->setCondition(NewCond);
     PBI->swapSuccessors();
@@ -5053,7 +5050,6 @@ bool llvm::FoldBranchToCommonDest(BranchInst *BI, DomTreeUpdater *DTU,
   if (MayHaveOpenMP && TooManyInsts(pred_size(BB)))
     return false;
 
-<<<<<<< HEAD
   // This is a special check for blender code.
   Value* Operands[2] = { nullptr, nullptr };
   if (isa<SelectInst>(Cond)) {
@@ -5086,8 +5082,6 @@ bool llvm::FoldBranchToCommonDest(BranchInst *BI, DomTreeUpdater *DTU,
   if (is_contained(successors(BB), BB))
     return false;
 
-=======
->>>>>>> 37b8f09a4b61bf9bf9d0b9017d790c8b82be2e17
   // With which predecessors will we want to deal with?
   SmallVector<BasicBlock *, 8> Preds;
   for (BasicBlock *PredBlock : predecessors(BB)) {
