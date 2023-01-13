llvm/llvm-project

commit e0c3142af075e2ef89395dbed5939071345eb622

Headers: tweak inclusion condition for stdatomic.h
MSVC requires that C++23 be available (_HAS_CXX23) else the entire
content is elided. Conditionalise the inclusion properly so that C/C++
code using stdatomic.h for memory_order_* constants are able to do
so without changing the C++ standard. This repairs builds of Swift and
libdispatch after ba49d39b20cc5358da28af2ac82bd336028780bc.

Differential Revision: https://reviews.llvm.org/D139266
Reviewed By: aaron.ballman, Mordante, fsb4000

clang/lib/Headers/stdatomic.h
@@ -15,10 +15,12 @@
  *
  * Exclude the MSVC path as well as the MSVC header as of the 14.31.30818
  * explicitly disallows `stdatomic.h` in the C mode via an `#error`.  Fallback
- * to the clang resource header until that is fully supported.
+ * to the clang resource header until that is fully supported.  The
+ * `stdatomic.h` header requires C++ 23 or newer.
  */
 #if __STDC_HOSTED__ &&                                                         \
-    __has_include_next(<stdatomic.h>) && !(defined(_MSC_VER) && !defined(__cplusplus))
+    __has_include_next(<stdatomic.h>) &&                                       \
+    !(defined(_MSC_VER) && defined(__cplusplus) && __cplusplus < 202002L)
 # include_next <stdatomic.h>
 #else
 

wangdi4/llvm

commit c0b637e4cb7ebb4fe7bcfc9a2dba726b3f9788eb

Resolve of merge (conflict) 6c3a2902f1f8 Headers: further refine the `stdatomic.h` MSVC inclusion 'main' -> 'xmain-web' (#9922)
* Resolve of merge (conflict) 6c3a2902f1f8 Headers: further refine the `stdatomic.h` MSVC inclusion 'main' -> 'xmain-web'

  CONFLICT (content): Merge conflict in clang/lib/Headers/stdatomic.h

  commit 6c3a2902f1f8d102aa5b3cd37def4f1420ac0c80
  Author: Saleem Abdulrasool <compnerd@compnerd.org>
  Date:   Wed Dec 21 16:45:28 2022 +0000

      Headers: further refine the `stdatomic.h` MSVC inclusion

      The `stdatomic.h` header cannot be included under MSVC when in C++ mode
      as the toolsets prior to 17.5.0 Preview 2 do not support the C11
      atomics.  However, some toolsets prior to the aforementioned version
      support the atomics under the C++ mode.  In these versions the MSVC
      toolset protects against the contamination by entirely eliding the
      declarations, resulting in build failures in some cases.  Unfortunately,
      e0c3142af075e2ef89395dbed5939071345eb622 accidentally regressed the C
      case when fixing the C++ support.

      Unfortunately, we cannot test this reliably as we need to inject a
      header after the resource path as a system header path, which we do not
      have a reliable means of doing.

      Fixes: #59640

* Resolution

---------

Co-authored-by: iclsrc <ia.compiler.tools.git@intel.com>
Co-authored-by: KornevNikita <nikita.kornev@intel.com>

clang/lib/Headers/stdatomic.h
@@ -47,12 +47,8 @@
 /* INTEL_CUSTOMIZATION */
 #if !__USE_CLANG_CATOMICS && __STDC_HOSTED__ &&                                \
     __has_include_next(<stdatomic.h>) &&                                       \
-<<<<<<< HEAD
-    !(defined(_MSC_VER) && defined(__cplusplus) && __cplusplus < 202002L)
-/* end INTEL_CUSTOMIZATION */
-=======
     (!defined(_MSC_VER) || (defined(__cplusplus) && __cplusplus >= 202002L))
->>>>>>> 6c3a2902f1f8d102aa5b3cd37def4f1420ac0c80
+/* end INTEL_CUSTOMIZATION */
 # include_next <stdatomic.h>
 #else
 