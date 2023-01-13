1e5e5a5113e612638a7bae910ce8b21268351570
iclsrc <iclsrc@intel.com>
2023-01-13 11:59:06 -0800

    Resolve of merge (conflict) 719d98dfa841 [clang] Use a StringRef instead of a raw char pointer to s.. 'main' -> 'xmain-web' (#9935)
    * Resolve of merge (conflict) 719d98dfa841 [clang] Use a StringRef instead of a raw char pointer to s.. 'main' -> 'xmain-web'
    
      CONFLICT (content): Merge conflict in clang/lib/AST/ExprConstant.cpp
    
      commit 719d98dfa841c522d8d452f0685e503538415a53
      Author: serge-sans-paille <sguelton@mozilla.com>
      Date:   Mon Dec 12 17:02:15 2022 +0100
    
          [clang] Use a StringRef instead of a raw char pointer to store builtin and call information
    
          This avoids recomputing string length that is already known at compile
          time.
    
          It has a slight impact on preprocessing / compile time, see
    
          https://llvm-compile-time-tracker.com/compare.php?from=3f36d2d579d8b0e8824d9dd99bfa79f456858f88&to=e49640c507ddc6615b5e503144301c8e41f8f434&stat=instructions:u
    
          Differential Revision: https://reviews.llvm.org/D139881
    
    * Resolve conflict
    
    ---------
    
    Co-authored-by: iclsrc <ia.compiler.tools.git@intel.com>
    Co-authored-by: Ernesto Su <ernesto.su@intel.com>

--- clang/lib/AST/ExprConstant.cpp
+++ clang/lib/AST/ExprConstant.cpp
@@ -12285,15 +12285,9 @@ bool IntExprEvaluator::VisitBuiltinCallExpr(const CallExpr *E,
     if (Info.getLangOpts().CPlusPlus11 && // INTEL
         !Info.getLangOpts().IntelCompat) // INTEL
       Info.CCEDiag(E, diag::note_constexpr_invalid_function)
-<<<<<<< HEAD
-        << /*isConstexpr*/0 << /*isConstructor*/0
-        << (std::string("'") + Info.Ctx.BuiltinInfo.getName(BuiltinOp) + "'");
-    else if (!Info.getLangOpts().CPlusPlus11) // INTEL
-=======
           << /*isConstexpr*/ 0 << /*isConstructor*/ 0
           << ("'" + Info.Ctx.BuiltinInfo.getName(BuiltinOp) + "'").str();
-    else
->>>>>>> 719d98dfa841c522d8d452f0685e503538415a53
+    else if (!Info.getLangOpts().CPlusPlus11) // INTEL
       Info.CCEDiag(E, diag::note_invalid_subexpr_in_const_expr);
     [[fallthrough]];
   case Builtin::BI__builtin_strlen:
