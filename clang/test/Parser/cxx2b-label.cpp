af2c5a125d74bd97c07122ed0aaf601ed5bcb333
Tom Honermann <tom.honermann@intel.com>
2023-01-13 10:06:01 -0500

    Correct a merge conflict resolution that introduced a crash with labels at the end of compound statements. (#9855)
    Commit 62b2643222e91c5cdfa7bb50a3aa9bd4661977f1 resolved a merge conflict that
    was introduced when merging upstream Clang following the additions made upstream
    in commit 3285f9a2392fd6bc79241b1e97b124079553e48d to add support for default,
    case, and label statements with an optional substatement at the end of a
    compound statement. This change corrects an incorrect conflict resolution in
    that merge commit that resulted in a crash due to a missing call to create an
    implicit null statement node when a substatement is not present.
    
    CMPLRLLVM-42873

--- clang/test/Parser/cxx2b-label.cpp
+++ clang/test/Parser/cxx2b-label.cpp
@@ -1,12 +1,15 @@
 // RUN: %clang_cc1 -fsyntax-only -verify=expected,cxx2b -std=c++2b -Wpre-c++2b-compat %s
 // RUN: %clang_cc1 -fsyntax-only -verify=expected,cxx20 -std=c++20 %s
+// INTEL RUN: %clang_cc1 -fsyntax-only -verify=expected,intel-cxx2b -std=c++2b -Wpre-c++2b-compat -fintel-compatibility %s
+// INTEL RUN: %clang_cc1 -fsyntax-only -verify=expected,intel-cxx20 -std=c++20 -fintel-compatibility %s
 
 void test_label_in_func() {
 label1:
     int x;
 label2:
     x = 1;
-label3: label4: label5:
+label3: label4: label5: // intel-cxx2b-warning {{expected statement}} \
+                           intel-cxx20-warning {{expected statement}}
 } // cxx20-warning {{label at end of compound statement is a C++2b extension}} \
      cxx2b-warning {{label at end of compound statement is incompatible with C++ standards before C++2b}}
 
@@ -16,14 +19,16 @@ int test_label_in_switch(int v) {
         return 1;
     case 2:
         return 2;
-    case 3: case 4: case 5:
+    case 3: case 4: case 5: // intel-cxx2b-warning {{label at end of switch compound statement: expected statement}} \
+                               intel-cxx20-warning {{label at end of switch compound statement: expected statement}}
     } // cxx20-warning {{label at end of compound statement is a C++2b extension}} \
          cxx2b-warning {{label at end of compound statement is incompatible with C++ standards before C++2b}}
 
     switch (v) {
     case 6:
         return 6;
-    default:
+    default: // intel-cxx2b-warning {{label at end of switch compound statement: expected statement}} \
+                intel-cxx20-warning {{label at end of switch compound statement: expected statement}}
     } // cxx20-warning {{label at end of compound statement is a C++2b extension}} \
          cxx2b-warning {{label at end of compound statement is incompatible with C++ standards before C++2b}}
 
