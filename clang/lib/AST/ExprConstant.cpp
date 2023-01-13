80ae3ff4bb3badbda22f5bb9d8b473e5696904e7
iclsrc <iclsrc@intel.com>
2023-01-13 14:16:19 -0800

    Resolve of merge (conflict) b7065a31b5e7 Revert "[clang] Use a StringRef instead of a raw char poin.. 'main' -> 'xmain-web' (1 commits) (#9936)
    * Resolve of merge (conflict) b7065a31b5e7 Revert "[clang] Use a StringRef instead of a raw char poin.. 'main' -> 'xmain-web' (1 commits)
    
      CONFLICT (content): Merge conflict in clang/lib/AST/ExprConstant.cpp
    
      commit b7065a31b5e7e0bbdae1cbb6cd06fcfe05b7758f
      Author: serge-sans-paille <sguelton@mozilla.com>
      Date:   Fri Dec 23 11:36:56 2022 +0100
    
          Revert "[clang] Use a StringRef instead of a raw char pointer to store builtin and call information"
    
          Failing builds: https://lab.llvm.org/buildbot#builders/9/builds/19030
          This is GCC specific and has been reported upstream: https://gcc.gnu.org/bugzilla/show_bug.cgi?id=108158
    
          This reverts commit 719d98dfa841c522d8d452f0685e503538415a53.
    
    * Resolve conflict
    
    ---------
    
    Co-authored-by: iclsrc <ia.compiler.tools.git@intel.com>
    Co-authored-by: Ernesto Su <ernesto.su@intel.com>

--- clang/lib/AST/ExprConstant.cpp
+++ clang/lib/AST/ExprConstant.cpp
@@ -12285,15 +12285,9 @@ bool IntExprEvaluator::VisitBuiltinCallExpr(const CallExpr *E,
     if (Info.getLangOpts().CPlusPlus11 && // INTEL
         !Info.getLangOpts().IntelCompat) // INTEL
       Info.CCEDiag(E, diag::note_constexpr_invalid_function)
-<<<<<<< HEAD
-          << /*isConstexpr*/ 0 << /*isConstructor*/ 0
-          << ("'" + Info.Ctx.BuiltinInfo.getName(BuiltinOp) + "'").str();
-    else if (!Info.getLangOpts().CPlusPlus11) // INTEL
-=======
         << /*isConstexpr*/0 << /*isConstructor*/0
         << (std::string("'") + Info.Ctx.BuiltinInfo.getName(BuiltinOp) + "'");
-    else
->>>>>>> b7065a31b5e7e0bbdae1cbb6cd06fcfe05b7758f
+    else if (!Info.getLangOpts().CPlusPlus11) // INTEL
       Info.CCEDiag(E, diag::note_invalid_subexpr_in_const_expr);
     [[fallthrough]];
   case Builtin::BI__builtin_strlen:
