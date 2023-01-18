5be8c976512ae3a048bee69c0f6c66747dac8490
iclsrc <iclsrc@intel.com>
2023-01-18 08:22:00 -0800

    Resolve of merge (conflict) 85d049a089d4 Implement support for option 'fexcess-precision'. 'main' -> 'xmain-web' (#10045)
    * Resolve of merge (conflict) 85d049a089d4 Implement support for option 'fexcess-precision'. 'main' -> 'xmain-web'
    
      CONFLICT (add/add): Merge conflict in clang/test/Driver/fexcess-precision.c
      CONFLICT (add/add): Merge conflict in clang/test/CodeGen/X86/fexcess-precision.c
      CONFLICT (content): Merge conflict in clang/lib/Driver/ToolChains/Clang.cpp
    
      commit 85d049a089d4a6a4a67145429ea5d8e155651138
      Author: Zahira Ammarguellat <zahira.ammarguellat@intel.com>
      Date:   Thu Oct 13 14:40:21 2022 -0400
    
          Implement support for option 'fexcess-precision'.
    
          Differential revision: https://reviews.llvm.org/D136176
    
    * resolution
    
    ---------
    
    Co-authored-by: iclsrc <ia.compiler.tools.git@intel.com>
    Co-authored-by: KornevNikita <nikita.kornev@intel.com>

--- clang/test/Driver/fexcess-precision.c
+++ clang/test/Driver/fexcess-precision.c
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 // RUN: %clang -### -target i386 -fexcess-precision=fast -c %s 2>&1  \
 // RUN:   | FileCheck --check-prefix=CHECK-FAST %s
 // RUN: %clang -### -target i386 -fexcess-precision=standard -c %s 2>&1  \
@@ -33,39 +32,3 @@
 // CHECK: "-cc1"
 // CHECK-NOT: "-ffloat16-excess-precision=fast"
 // CHECK-ERR-16: unsupported argument '16' to option '-fexcess-precision='
-=======
-// RUN: %clang -### -target i386 -fexcess-precision=fast -c %s 2>&1  \
-// RUN:   | FileCheck --check-prefix=CHECK-FAST %s
-// RUN: %clang -### -target i386 -fexcess-precision=standard -c %s 2>&1  \
-// RUN:   | FileCheck --check-prefix=CHECK-STD %s
-// RUN: %clang -### -target i386 -fexcess-precision=16 -c %s 2>&1  \
-// RUN:   | FileCheck --check-prefix=CHECK-NONE %s
-// RUN: %clang -### -target i386 -fexcess-precision=none -c %s 2>&1  \
-// RUN:   | FileCheck --check-prefix=CHECK-ERR-NONE %s
-
-// RUN: %clang -### -target x86_64 -fexcess-precision=fast -c %s 2>&1 \
-// RUN:   | FileCheck --check-prefix=CHECK-FAST %s
-// RUN: %clang -### -target x86_64 -fexcess-precision=standard -c %s 2>&1 \
-// RUN:   | FileCheck --check-prefix=CHECK-STD %s
-// RUN: %clang -### -target x86_64 -fexcess-precision=16 -c %s 2>&1 \
-// RUN:   | FileCheck --check-prefix=CHECK-NONE %s
-// RUN: %clang -### -target x86_64 -fexcess-precision=none -c %s 2>&1 \
-// RUN:   | FileCheck --check-prefixes=CHECK-ERR-NONE %s
-
-// RUN: %clang -### -target aarch64 -fexcess-precision=fast -c %s 2>&1 \
-// RUN:   | FileCheck --check-prefix=CHECK %s
-// RUN: %clang -### -target aarch64 -fexcess-precision=standard -c %s 2>&1 \
-// RUN:   | FileCheck --check-prefix=CHECK %s
-// RUN: %clang -### -target aarch64 -fexcess-precision=16 -c %s 2>&1 \
-// RUN:   | FileCheck --check-prefix=CHECK-ERR-16 %s
-// RUN: %clang -### -target aarch64 -fexcess-precision=none -c %s 2>&1 \
-// RUN:   | FileCheck --check-prefix=CHECK-ERR-NONE %s
-
-// CHECK-FAST: "-ffloat16-excess-precision=fast"
-// CHECK-STD: "-ffloat16-excess-precision=standard"
-// CHECK-NONE: "-ffloat16-excess-precision=none"
-// CHECK-ERR-NONE: unsupported argument 'none' to option '-fexcess-precision='
-// CHECK: "-cc1"
-// CHECK-NOT: "-ffloat16-excess-precision=fast"
-// CHECK-ERR-16: unsupported argument '16' to option '-fexcess-precision='
->>>>>>> 85d049a089d4a6a4a67145429ea5d8e155651138
