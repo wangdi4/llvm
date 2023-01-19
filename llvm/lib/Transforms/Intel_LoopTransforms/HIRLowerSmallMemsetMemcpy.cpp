f44e54b86723fd8a72df2b205e4052c810d8316f
yoonseoc-sc <yoonseo.choi@intel.com>
2023-01-18 19:59:43 -0800

    [NFC][CMP-42964] Klocwork - uninitialized defs - resubmit (#10051)

--- llvm/lib/Transforms/Intel_LoopTransforms/HIRLowerSmallMemsetMemcpy.cpp
+++ llvm/lib/Transforms/Intel_LoopTransforms/HIRLowerSmallMemsetMemcpy.cpp
@@ -188,7 +188,7 @@ static bool isOutOfRangeAccess(int64_t TC, int64_t MemOpInnermostDimSize,
 
 bool MemsetMemcpyVisitor::doAnalysis(const HLInst *Inst,
                                      MemsetMemcpyCandidate &MMC) const {
-  Intrinsic::ID IntrinID;
+  Intrinsic::ID IntrinID = 0;
   if (!Inst->isIntrinCall(IntrinID)) {
     return false;
   }
