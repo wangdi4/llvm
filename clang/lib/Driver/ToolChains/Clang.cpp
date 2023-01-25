d6ce1c1d7a91ef2c779e16ed2974eb99104fe5d6
Maksim Sabianin <maksim.sabianin@intel.com>
2023-01-25 16:08:52 +0100

    [OpenMP] Add -mlong-double-64 to Clang Job for OpenMP (#9887)
    C++ STL implementation might use long double which
    translates into x86_fp80 float type which is not
    supported by SPIRV spec.

--- clang/lib/Driver/ToolChains/Clang.cpp
+++ clang/lib/Driver/ToolChains/Clang.cpp
@@ -5719,6 +5719,8 @@ void Clang::ConstructJob(Compilation &C, const JobAction &JA,
         CmdArgs.push_back("-mllvm");
         CmdArgs.push_back("-vpo-paropt-enable-64bit-opencl-atomics=true");
       }
+
+      CmdArgs.push_back("-mlong-double-64");
     }
 #endif // INTEL_CUSTOMIZATION
   }
