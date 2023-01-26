14c399824e759fc1b18dbfc21b08815caa687c46
abuyukku <betul.buyukkurt@intel.com>
2023-01-26 06:27:44 -0800

    [ACD] Remove INTEL_CUSTOMIZATION macros from */Transforms/Utils/Intel_X86EmitMultiVersionResolver.* (#9278) (#8409)

--- llvm/include/llvm/Transforms/Utils/Intel_X86EmitMultiVersionResolver.h
+++ llvm/include/llvm/Transforms/Utils/Intel_X86EmitMultiVersionResolver.h
@@ -1,37 +1,17 @@
-//===-- X86EmitMultiVersionResolver -----------------------------*- C++ -*-===//
-// INTEL_CUSTOMIZATION
+//===--------  Intel_X86EmitMultiVersionResolver.h ------------------------===//
 //
-// INTEL CONFIDENTIAL
+// Copyright (C) Intel Corporation. All rights reserved.
 //
-// Modifications, Copyright (C) 2022 Intel Corporation
-//
-// This software and the related documents are Intel copyrighted materials, and
-// your use of them is governed by the express license under which they were
-// provided to you ("License"). Unless the License provides otherwise, you may not
-// use, modify, copy, publish, distribute, disclose or transmit this software or
-// the related documents without Intel's prior written permission.
-//
-// This software and the related documents are provided as is, with no express
-// or implied warranties, other than those that are expressly stated in the
-// License.
-//
-// end INTEL_CUSTOMIZATION
-//
-// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
-// See https://llvm.org/LICENSE.txt for license information.
-// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
-//
-//===----------------------------------------------------------------------===//
-//
-// This file implements utilities to generate code used for CPU dispatch code.
-// INTEL: Upstreaming attempt is at https://reviews.llvm.org/D108424.
+// The information and source code contained herein is the exclusive property
+// of Intel Corporation and may not be disclosed, examined or reproduced in
+// whole or in part without explicit written authorization from the company.
 //
 //===----------------------------------------------------------------------===//
 
 #ifndef LLVM_TRANSFORMS_UTILS_X86EMITMULTIVERSIONRESOLVER_H
 #define LLVM_TRANSFORMS_UTILS_X86EMITMULTIVERSIONRESOLVER_H
 
-#include "llvm/ADT/APSInt.h" // INTEL
+#include "llvm/ADT/APSInt.h"
 #include "llvm/ADT/ArrayRef.h"
 #include "llvm/ADT/StringRef.h"
 
@@ -60,24 +40,20 @@ struct MultiVersionResolverOption {
 // Emits the body of a multiversion function's resolver. Assumes that the
 // options are already sorted in the proper order, with the 'default' option
 // last (if it exists).
-#if INTEL_CUSTOMIZATION
 void emitMultiVersionResolver(Function *Resolver,
                               ArrayRef<MultiVersionResolverOption> Options,
                               bool UseIFunc, bool UseLibIRC);
 Value *formResolverCondition(IRBuilderBase &Builder,
                              const MultiVersionResolverOption &RO,
                              bool UseLibIRC);
-#endif // INTEL_CUSTOMIZATION
 namespace X86 {
 void emitCPUInit(IRBuilderBase &Builder, bool UseIFunc);
 Value *emitCpuIs(IRBuilderBase &Builder, StringRef CPUStr);
 Value *emitCpuSupports(IRBuilderBase &Builder, uint64_t FeaturesMask);
 Value *emitCpuSupports(IRBuilderBase &Builder, ArrayRef<StringRef> FeatureStrs);
-#if INTEL_CUSTOMIZATION
 void emitCpuFeaturesInit(IRBuilderBase &Builder, bool UseIFunc);
 Value *mayIUseCpuFeatureHelper(IRBuilderBase &Builder,
                                ArrayRef<llvm::APSInt> Pages);
-#endif // INTEL_CUSTOMIZATION
 } // namespace X86
 } // namespace llvm
 
