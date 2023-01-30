c0c26b0857d9e74aa2d248cb536655e9f76330e5
iclsrc <iclsrc@intel.com>
2023-01-30 12:13:06 -0800

    Resolve of merge (conflict) 2ebda47619d7 [Clang][OpenMP] Bail out early if `Scope` is nullptr in ca.. 'main' -> 'xmain-web' (#10323)
    * Resolve of merge (conflict) 2ebda47619d7 [Clang][OpenMP] Bail out early if `Scope` is nullptr in ca.. 'main' -> 'xmain-web'
    
      CONFLICT (add/add): Merge conflict in clang/test/OpenMP/bug59944.c
    
      commit 2ebda47619d7e8b72d460a955de4f398f637489f
      Author: Shilei Tian <i@tianshilei.me>
      Date:   Fri Jan 20 14:40:16 2023 -0500
    
          [Clang][OpenMP] Bail out early if `Scope` is nullptr in case of any crash
    
          When there is any compile error, clang still tries to compile as many code as
          possible, therefore `Scope` can be `nullptr` here. However, we didn't check it
          beforehand, causing compiler crash.
    
          Fix #59944.
    
          Reviewed By: ABataev
    
          Differential Revision: https://reviews.llvm.org/D142233
    
    * conflict resolution
    
    ---------
    
    Co-authored-by: iclsrc <ia.compiler.tools.git@intel.com>
    Co-authored-by: Soumi Manna <soumi.manna@intel.com>

--- clang/test/OpenMP/bug59944.c
+++ clang/test/OpenMP/bug59944.c
@@ -1,21 +1,11 @@
-<<<<<<< HEAD
-// RUN: %clang_cc1 -verify -fopenmp -fopenmp-version=52 -x c -triple x86_64-apple-darwin10 %s
-=======
 // RUN: %clang_cc1 -fopenmp -fopenmp-version=52 -x c -triple x86_64-apple-darwin10 %s -o - 2>&1 | FileCheck %s --check-prefix=CHECK
->>>>>>> 2ebda47619d7e8b72d460a955de4f398f637489f
 
 extern int omp_get_initial_device();
 extern void *omp_get_mapped_ptr(void *, int);
 
 void t() {
-<<<<<<< HEAD
-  omp_get_mapped_ptr(&x, omp_get_initial_device()); //expected-error {{use of undeclared identifier 'x'}}
-}
-
-=======
   omp_get_mapped_ptr(&x, omp_get_initial_device());
 }
 
 // CHECK: error: use of undeclared identifier 'x'
 // CHECK-NOT: crash
->>>>>>> 2ebda47619d7e8b72d460a955de4f398f637489f
