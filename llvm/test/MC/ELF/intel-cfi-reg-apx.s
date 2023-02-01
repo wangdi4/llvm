39687d7b7932318456e82a8931f10af3c7b0984b
KanRobert <shengchen.kan@intel.com>
2023-02-01 16:51:29 +0800

    Support more register in X86 backend (#10308)
    // INTEL_FEATURE_ISA_APX_F
    Add new GPR R16-R31 introduced in APX.
    // end INTEL_FEATURE_ISA_APX_F

---
+++ llvm/test/MC/ELF/intel-cfi-reg-apx.s
@@ -0,0 +1,45 @@
+// INTEL_FEATURE_ISA_APX_F
+// REQUIRES: intel_feature_isa_apx_f
+// RUN: llvm-mc -triple x86_64-pc-linux-gnu %s -o - | FileCheck %s
+
+f:
+	.cfi_startproc
+        .cfi_offset 126, -120
+        .cfi_offset 127, -112
+        .cfi_offset 128, -104
+        .cfi_offset 129, -96
+        .cfi_offset 130, -88
+        .cfi_offset 131, -80
+        .cfi_offset 132, -72
+        .cfi_offset 133, -64
+        .cfi_offset 134, -56
+        .cfi_offset 135, -48
+        .cfi_offset 136, -40
+        .cfi_offset 137, -32
+        .cfi_offset 138, -24
+        .cfi_offset 139, -16
+        .cfi_offset 140, -8
+        .cfi_offset 141, 0
+	.cfi_endproc
+
+// CHECK: f:
+// CHECK-NEXT:         .cfi_startproc
+// CHECK-NEXT:         .cfi_offset %r16, -120
+// CHECK-NEXT:         .cfi_offset %r17, -112
+// CHECK-NEXT:         .cfi_offset %r18, -104
+// CHECK-NEXT:         .cfi_offset %r19, -96
+// CHECK-NEXT:         .cfi_offset %r20, -88
+// CHECK-NEXT:         .cfi_offset %r21, -80
+// CHECK-NEXT:         .cfi_offset %r22, -72
+// CHECK-NEXT:         .cfi_offset %r23, -64
+// CHECK-NEXT:         .cfi_offset %r24, -56
+// CHECK-NEXT:         .cfi_offset %r25, -48
+// CHECK-NEXT:         .cfi_offset %r26, -40
+// CHECK-NEXT:         .cfi_offset %r27, -32
+// CHECK-NEXT:         .cfi_offset %r28, -24
+// CHECK-NEXT:         .cfi_offset %r29, -16
+// CHECK-NEXT:         .cfi_offset %r30, -8
+// CHECK-NEXT:         .cfi_offset %r31, 0
+// CHECK-NEXT:         .cfi_endproc
+
+// end INTEL_FEATURE_ISA_APX_F
