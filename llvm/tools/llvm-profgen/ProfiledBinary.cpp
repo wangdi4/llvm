9f4ac0fd54bf448cb1e35cfa6d7577af149fac47
chrulski-intel <christopher.m.chrulski@intel.com>
2023-02-07 14:53:35 -0700

    [NFC] [Coverity] Fix potential uninitialized use and nullptr dereference in llvm-profgen (#10575)
    Fixes llvm-profgen utility issues flagged by Coverity.
    
    [CMPLRLLVM-43645]

--- llvm/tools/llvm-profgen/ProfiledBinary.cpp
+++ llvm/tools/llvm-profgen/ProfiledBinary.cpp
@@ -1,4 +1,21 @@
 //===-- ProfiledBinary.cpp - Binary decoder ---------------------*- C++ -*-===//
+// INTEL_CUSTOMIZATION
+//
+// INTEL CONFIDENTIAL
+//
+// Modifications, Copyright (C) 2023 Intel Corporation
+//
+// This software and the related documents are Intel copyrighted materials, and
+// your use of them is governed by the express license under which they were
+// provided to you ("License"). Unless the License provides otherwise, you may
+// not use, modify, copy, publish, distribute, disclose or transmit this
+// software or the related documents without Intel's prior written permission.
+//
+// This software and the related documents are provided as is, with no express
+// or implied warranties, other than those that are expressly stated in the
+// License.
+//
+// end INTEL_CUSTOMIZATION
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
@@ -425,8 +442,8 @@ void ProfiledBinary::decodePseudoProbe(const ELFObjectFileBase *Obj) {
 void ProfiledBinary::decodePseudoProbe() {
   OwningBinary<Binary> OBinary = unwrapOrError(createBinary(Path), Path);
   Binary &ExeBinary = *OBinary.getBinary();
-  auto *Obj = dyn_cast<ELFObjectFileBase>(&ExeBinary);
-  decodePseudoProbe(Obj);
+  if (auto *Obj = dyn_cast<ELFObjectFileBase>(&ExeBinary)) // INTEL
+    decodePseudoProbe(Obj);                                // INTEL
 }
 
 void ProfiledBinary::setIsFuncEntry(uint64_t Address, StringRef RangeSymName) {
