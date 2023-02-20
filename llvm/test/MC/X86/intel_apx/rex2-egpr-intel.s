67b8e2ae9b5a327fe896baf80f9712dbd01bc27e
KanRobert <shengchen.kan@intel.com>
2023-02-20 18:46:00 +0800

    Support new registers for more instruction formats (#10943)
    // INTEL_FEATURE_ISA_APX_F
    Support R16-R31 for all instruction formats with REX
    // end INTEL_FEATURE_ISA_APX_F

--- llvm/test/MC/X86/intel_apx/rex2-egpr-intel.s
+++
@@ -1,6 +0,0 @@
-# REQUIRES: intel_feature_isa_apx_f
-# RUN: llvm-mc -triple x86_64 -show-encoding -x86-asm-syntax=intel -output-asm-variant=1 %s | FileCheck %s
-
-# CHECK: inc	r16d
-# CHECK: encoding: [0xd5,0x10,0xff,0xc0]
-         inc	r16d
