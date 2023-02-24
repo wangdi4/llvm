f2e578979ddda8772bb1af3758678d3a80819a59
Chunyang Dai <chunyang.dai@intel.com>
2023-02-24 19:50:07 +0800

    [OpenCL][NFC] Remove unused backend test cases. (#10950)

--- opencl-intel/backend/tests/MIC/satest/workloads/satest_wrap.sh
+++
@@ -1,9 +0,0 @@
-echo Command: SATest -BUILD -cpuarch knc -config $1
-export TMP=`mktemp -d`
-cp *.h ${TMP}/
-cd ${TMP} && SATest -BUILD -cpuarch knc -config $1
-mv SATest.s $2/`basename $1`.s
-mv SATest.ll $2/`basename $1`.ll
-rm -f *
-cd -
-rmdir ${TMP}
