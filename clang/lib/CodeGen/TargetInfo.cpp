2351cceed854cb9880e6da03e509f206e8debb4c
iclsrc <iclsrc@intel.com>
2023-03-05 14:04:18 -0800

    Resolve of merge (conflict) 8b55761761cd [OpenCL] Disable vector to scalar types coercion for OpenC.. 'sycl' -> 'xmain-web' (#11272)
    * Resolve of merge (conflict) 8b55761761cd [OpenCL] Disable vector to scalar types coercion for OpenC.. 'sycl' -> 'xmain-web'
    
      CONFLICT (content): Merge conflict in clang/include/clang/Basic/LangOptions.def
      CONFLICT (content): Merge conflict in clang/include/clang/Driver/Options.td
      CONFLICT (content): Merge conflict in clang/lib/CodeGen/TargetInfo.cpp
    
      commit 8b55761761cd0dd87efd3eb1e9d6f8dd2a878ca6
      Author: Chunyang Dai <chunyang.dai@intel.com>
      Date:   Tue Feb 21 07:12:59 2023 +0800
    
          [OpenCL] Disable vector to scalar types coercion for OpenCL (#8160)
    
          For x86 target, vector types (both result and arguments) can be coerced
          to scalars of the same size, e.g:
    
                define zeroext i1 @_Z18convert_ulong4_rteDv4_t(<4 x i16> %x)
                ; becomes
                define zeroext i1 @_Z18convert_ulong4_rteDv4_t(i64 %x.coerced)
    
          Such behavior is completely valid for x86, but the backend vectorizer
          cannot work with scalars instead of vectors.
    
          With this patch, argument and result types will be leaved unchanged in
          the CodeGen.
    
          New option fopencl-force-vector-abi is also added to force-disables
          vector to scalar coercion when provided.
    
          ---------
    
          Co-authored-by: Wenju He <wenju.he@intel.com>
          Co-authored-by: Alexey Bader <alexey.bader@intel.com>
    
    * Conflict resolution
    
    ---------
    
    Co-authored-by: iclsrc <ia.compiler.tools.git@intel.com>
    Co-authored-by: Manna, Soumi <soumi.manna@intel.com>

--- clang/lib/CodeGen/TargetInfo.cpp
+++ clang/lib/CodeGen/TargetInfo.cpp
@@ -121,10 +121,6 @@ Address ABIInfo::EmitMSVAArg(CodeGenFunction &CGF, Address VAListAddr,
   return Address::invalid();
 }
 
-<<<<<<< HEAD
-#if INTEL_CUSTOMIZATION
-=======
->>>>>>> 8b55761761cd0dd87efd3eb1e9d6f8dd2a878ca6
 static ABIArgInfo classifyOpenCL(QualType Ty, ASTContext &Context) {
   if (Ty->isVoidType())
     return ABIArgInfo::getIgnore();
@@ -148,17 +144,10 @@ static bool doOpenCLClassification(CGFunctionInfo &FI, ASTContext &Context) {
   if (!Context.getLangOpts().OpenCLForceVectorABI)
     return false;
 
-<<<<<<< HEAD
-  // Use OpenCL classify to prevent coercing
-  // Vector ABI must be enforced by enabling the corresponding option
-  // Otherwise, vector types will be coerced to a matching integer
-  // type to conform with ABI, e.g.: <8 x i8> will be coerced to i64
-=======
   // Use OpenCL classify to prevent coercing.
   // Vector ABI must be enforced by enabling the corresponding option.
   // Otherwise, vector types will be coerced to a matching integer
   // type to conform with ABI, e.g.: <8 x i8> will be coerced to i64.
->>>>>>> 8b55761761cd0dd87efd3eb1e9d6f8dd2a878ca6
   FI.getReturnInfo() = classifyOpenCL(FI.getReturnType(), Context);
 
   for (auto &Arg : FI.arguments())
@@ -167,7 +156,7 @@ static bool doOpenCLClassification(CGFunctionInfo &FI, ASTContext &Context) {
   return true;
 }
 
-<<<<<<< HEAD
+#if INTEL_CUSTOMIZATION
 // When an integer type appears in argument of an SVML function, it might be:
 //   a) An AVX512 mask (since the __mmask types are defined as aliases to
 //      integer types in headers), we need to convert it to <n x i1> type and
@@ -252,8 +241,7 @@ static ABIArgInfo classifySVMLStructure(const Type *Base, uint64_t NumElts,
   return IsReturn ? ABIArgInfo::getDirect() : ABIArgInfo::getExpand();
 }
 #endif // INTEL_CUSTOMIZATION
-=======
->>>>>>> 8b55761761cd0dd87efd3eb1e9d6f8dd2a878ca6
+
 static llvm::Type *getVAListElementType(CodeGenFunction &CGF) {
   return CGF.ConvertTypeForMem(
       CGF.getContext().getBuiltinVaListType()->getPointeeType());
@@ -2166,17 +2154,9 @@ ABIArgInfo X86_32ABIInfo::classifyArgumentType(QualType Ty,
 }
 
 void X86_32ABIInfo::computeInfo(CGFunctionInfo &FI) const {
-<<<<<<< HEAD
-#if INTEL_CUSTOMIZATION
   ASTContext &Context = getContext();
   if (doOpenCLClassification(FI, Context))
     return;
-#endif // INTEL_CUSTOMIZATION
-=======
-  ASTContext &Context = getContext();
-  if (doOpenCLClassification(FI, Context))
-    return;
->>>>>>> 8b55761761cd0dd87efd3eb1e9d6f8dd2a878ca6
 
   CCState State(FI);
   if (IsMCUABI)
@@ -4203,17 +4183,9 @@ X86_64ABIInfo::classifyRegCallStructType(QualType Ty, unsigned &NeededInt,
 }
 
 void X86_64ABIInfo::computeInfo(CGFunctionInfo &FI) const {
-<<<<<<< HEAD
-#if INTEL_CUSTOMIZATION
   ASTContext &Context = getContext();
   if (doOpenCLClassification(FI, Context))
     return;
-#endif // INTEL_CUSTOMIZATION
-=======
-  ASTContext &Context = getContext();
-  if (doOpenCLClassification(FI, Context))
-    return;
->>>>>>> 8b55761761cd0dd87efd3eb1e9d6f8dd2a878ca6
 
   const unsigned CallingConv = FI.getCallingConvention();
   // It is possible to force Win64 calling convention on any x86_64 target by
@@ -4715,17 +4687,9 @@ ABIArgInfo WinX86_64ABIInfo::classify(QualType Ty, unsigned &FreeSSERegs,
 }
 
 void WinX86_64ABIInfo::computeInfo(CGFunctionInfo &FI) const {
-<<<<<<< HEAD
-#if INTEL_CUSTOMIZATION
   ASTContext &Context = getContext();
   if (doOpenCLClassification(FI, Context))
     return;
-#endif // INTEL_CUSTOMIZATION
-=======
-  ASTContext &Context = getContext();
-  if (doOpenCLClassification(FI, Context))
-    return;
->>>>>>> 8b55761761cd0dd87efd3eb1e9d6f8dd2a878ca6
 
   const unsigned CC = FI.getCallingConvention();
   bool IsVectorCall = CC == llvm::CallingConv::X86_VectorCall;
