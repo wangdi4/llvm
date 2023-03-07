8e3f65e1a594c8e845d1c2f93cfa96f3501d1a44
Michael Toguchi <michael.d.toguchi@intel.com>
2023-03-07 14:35:30 -0700

    [Driver] Address issues with 32-bit library locations for unified dir layout (#11291) (#11312)
    The expected location for the Intel libraries is base/lib and base/lib32
    Make adjustments for performance libraries and Intel specific compiler libs
    
    Addresses: CMPLRLLVM-45449
    Cherry-pick of: https://github.com/intel-restricted/applications.compilers.llvm-project/commit/d2b0fca1cf68f1dcfc0585a6c5be475b62851dc1

--- clang/lib/Driver/ToolChains/Gnu.cpp
+++ clang/lib/Driver/ToolChains/Gnu.cpp
@@ -3,13 +3,13 @@
 //
 // INTEL CONFIDENTIAL
 //
-// Modifications, Copyright (C) 2021-2022 Intel Corporation
+// Modifications, Copyright (C) 2021-2023 Intel Corporation
 //
 // This software and the related documents are Intel copyrighted materials, and
 // your use of them is governed by the express license under which they were
-// provided to you ("License"). Unless the License provides otherwise, you may not
-// use, modify, copy, publish, distribute, disclose or transmit this software or
-// the related documents without Intel's prior written permission.
+// provided to you ("License"). Unless the License provides otherwise, you may
+// not use, modify, copy, publish, distribute, disclose or transmit this
+// software or the related documents without Intel's prior written permission.
 //
 // This software and the related documents are provided as is, with no express
 // or implied warranties, other than those that are expressly stated in the
@@ -387,7 +387,7 @@ static void addIntelLibPaths(ArgStringList &CmdArgs,
   llvm::sys::path::append(BaseDir, "..");
 #if INTEL_DEPLOY_UNIFIED_LAYOUT
   // The driver is located in <installdir>/bin/compiler
-  llvm::sys::path::append(BaseDir, "..", "opt", "compiler");
+  llvm::sys::path::append(BaseDir, "..");
   llvm::sys::path::append(LibDir, BaseDir);
   if (TC.getEffectiveTriple().getArch() == llvm::Triple::x86_64)
     // lib location is in <installdir>/lib.
