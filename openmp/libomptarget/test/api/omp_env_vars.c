1860635186a71b6c84cfc3733c5f8c2e1f113549
Hansang Bae <hansang.bae@intel.com>
2023-03-13 09:20:43 -0500

    [OpenMP][Offload] Disable unsupported LIT test (#11465)
    JIRA: CMPLRLIBS-34380
    
    The disabled test requires next-gen plugin which we do not support.

--- openmp/libomptarget/test/api/omp_env_vars.c
+++ openmp/libomptarget/test/api/omp_env_vars.c
@@ -2,6 +2,11 @@
 // RUN: env OMP_NUM_TEAMS=1 OMP_TEAMS_THREAD_LIMIT=1 LIBOMPTARGET_INFO=16 \
 // RUN:   %libomptarget-run-generic 2>&1 | %fcheck-generic
 
+// INTEL_CUSTOMIZATION
+// This test requires next-gen plugin which We do not use
+// UNSUPPORTED: x86_64-pc-linux-gnu
+// end INTEL_CUSTOMIZATION
+
 #define N 256
 
 int main() {
