1729b9961679a4274e87936e700f6e0b262c0c28
iclsrc <iclsrc@intel.com>
2023-03-14 00:31:34 -0700

    Resolve of merge (conflict) adbdf273efd5 [CodeGenPrepare] Stop llvm.vscale() -> getelementptr(null,.. 'main' -> 'xmain-web' (#11493)
    * Resolve of merge (conflict) adbdf273efd5 [CodeGenPrepare] Stop llvm.vscale() -> getelementptr(null,.. 'main' -> 'xmain-web'
    
      CONFLICT (content): Merge conflict in llvm/lib/CodeGen/CodeGenPrepare.cpp
    
      commit adbdf273efd584e58e7ef57d391b4e1885e7837f
      Author: Paul Walker <paul.walker@arm.com>
      Date:   Wed Mar 8 15:19:43 2023 +0000
    
          [CodeGenPrepare] Stop llvm.vscale() -> getelementptr(null, 1) transformation.
    
          I've pulled this change from D145404 to land in isolation because
          I'm concerned the code might be more important than the test
          coverage might suggest (NOTE: the code has no test coverage).
    
    * resolve
    
    ---------
    
    Co-authored-by: iclsrc <ia.compiler.tools.git@intel.com>
    Co-authored-by: Freddy Ye <freddy.ye@intel.com>

--- llvm/lib/CodeGen/CodeGenPrepare.cpp
+++ llvm/lib/CodeGen/CodeGenPrepare.cpp
@@ -2459,25 +2459,6 @@ bool CodeGenPrepare::optimizeCallInst(CallInst *CI, ModifyDT &ModifiedDT) {
     case Intrinsic::dbg_assign:
     case Intrinsic::dbg_value:
       return fixupDbgValue(II);
-<<<<<<< HEAD
-    case Intrinsic::vscale: {
-      // If datalayout has no special restrictions on vector data layout,
-      // replace `llvm.vscale` by an equivalent constant expression
-      // to benefit from cheap constant propagation.
-      Type *ScalableVectorTy =
-          VectorType::get(Type::getInt8Ty(II->getContext()), 1, true);
-      if (DL->getTypeAllocSize(ScalableVectorTy).getKnownMinValue() == 8) {
-        auto *Null = Constant::getNullValue(ScalableVectorTy->getPointerTo());
-        auto *One = ConstantInt::getSigned(II->getType(), 1);
-        auto *CGep =
-            ConstantExpr::getGetElementPtr(ScalableVectorTy, Null, One);
-        replaceAllUsesWith(II, ConstantExpr::getPtrToInt(CGep, II->getType()),
-                           FreshBBs, IsHugeFunc);
-        II->eraseFromParent();
-        return true;
-      }
-      break;
-    }
 #if INTEL_CUSTOMIZATION
     case Intrinsic::masked_gather: {
       bool Changed = false;
@@ -2492,12 +2473,6 @@ bool CodeGenPrepare::optimizeCallInst(CallInst *CI, ModifyDT &ModifiedDT) {
       return Changed;
     }
 #endif // INTEL_CUSTOMIZATION
-=======
-    case Intrinsic::masked_gather:
-      return optimizeGatherScatterInst(II, II->getArgOperand(0));
-    case Intrinsic::masked_scatter:
-      return optimizeGatherScatterInst(II, II->getArgOperand(1));
->>>>>>> adbdf273efd584e58e7ef57d391b4e1885e7837f
     }
 
     SmallVector<Value *, 2> PtrOps;
