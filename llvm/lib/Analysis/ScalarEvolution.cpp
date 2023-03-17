845b026fdb463a85bb3114a025ae27b47ddaee91
iclsrc <iclsrc@intel.com>
2023-03-16 19:25:32 -0700

    Resolve of merge (conflict) 484c622760e2 [SCEV] Do not strengthen nuw/nsw flags during get[Zero,Sig.. 'main' -> 'xmain-web' (#11617)
    * Resolve of merge (conflict) 484c622760e2 [SCEV] Do not strengthen nuw/nsw flags during get[Zero,Sig.. 'main' -> 'xmain-web'
    
      CONFLICT (content): Merge conflict in llvm/lib/Analysis/ScalarEvolution.cpp
    
      commit 484c622760e2a46aa1b52c7a791ee59543364225
      Author: Florian Hahn <flo@fhahn.com>
      Date:   Wed Mar 15 20:57:32 2023 +0000
    
          [SCEV] Do not strengthen nuw/nsw flags during get[Zero,Sign]ExtendedExpr.
    
          Modifying AddRecs when constructing other expressions can lead to
          surprising changes. It also seems like it is not really beneficial i
          most cases.
    
          At the moment, there's a single regression, but we still might be able
          to improve the flags at AddRec construction.
    
          Might help with the issue discussed in D143409.
    
          Reviewed By: mkazantsev
    
          Differential Revision: https://reviews.llvm.org/D144051
    
    * resolve conflict
    
    ---------
    
    Co-authored-by: iclsrc <ia.compiler.tools.git@intel.com>
    Co-authored-by: Freddy Ye <freddy.ye@intel.com>

--- llvm/lib/Analysis/ScalarEvolution.cpp
+++ llvm/lib/Analysis/ScalarEvolution.cpp
@@ -1663,14 +1663,6 @@ const SCEV *ScalarEvolution::getZeroExtendExprImpl(const SCEV *Op, Type *Ty,
       unsigned BitWidth = getTypeSizeInBits(AR->getType());
       const Loop *L = AR->getLoop();
 
-<<<<<<< HEAD
-      if (!AR->hasNoUnsignedWrap() || !AR->hasNoSignedWrap()) { // INTEL
-        auto NewFlags = proveNoWrapViaConstantRanges(AR);
-        setNoWrapFlags(const_cast<SCEVAddRecExpr *>(AR), NewFlags);
-      }
-
-=======
->>>>>>> 484c622760e2a46aa1b52c7a791ee59543364225
       // If we have special knowledge that this addrec won't overflow,
       // we don't need to do any further analysis.
       if (AR->hasNoUnsignedWrap()) {
@@ -2050,14 +2042,6 @@ const SCEV *ScalarEvolution::getSignExtendExprImpl(const SCEV *Op, Type *Ty,
       unsigned BitWidth = getTypeSizeInBits(AR->getType());
       const Loop *L = AR->getLoop();
 
-<<<<<<< HEAD
-      if (!AR->hasNoSignedWrap() || !AR->hasNoUnsignedWrap()) { // INTEL
-        auto NewFlags = proveNoWrapViaConstantRanges(AR);
-        setNoWrapFlags(const_cast<SCEVAddRecExpr *>(AR), NewFlags);
-      }
-
-=======
->>>>>>> 484c622760e2a46aa1b52c7a791ee59543364225
       // If we have special knowledge that this addrec won't overflow,
       // we don't need to do any further analysis.
       if (AR->hasNoSignedWrap()) {
