b83872e1c5317a45295f92a2fb1ec05d739e92b7
Andy Kaylor <andrew.kaylor@intel.com>
2023-03-29 13:00:41 -0700

    Fix Coverity issue 2020339 (#11894)
    This code should be upstreamed, but it is being put in xmain first to
    meet Coverity goals.

--- llvm/include/llvm/ADT/SCCIterator.h
+++ llvm/include/llvm/ADT/SCCIterator.h
@@ -298,7 +298,7 @@ class scc_member_iterator {
       return false;
 
     // Make the smaller rank tree a direct child or the root of high rank tree.
-    if (G1->Rank < G1->Rank)
+    if (G1->Rank < G2->Rank) // INTEL
       G1->Group = G2;
     else {
       G2->Group = G1;
