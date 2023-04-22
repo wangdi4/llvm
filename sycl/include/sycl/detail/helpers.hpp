1a47c9098c19d3836dec05889ef64a908057487d
Artur Gainullin <artur.gainullin@intel.com>
2023-04-21 23:15:52 -0700

    Don't use INTEL_CUSTOMIZATION guard in SYCL RT (#12610)
    This macro is not defined for SYCL RT.

--- sycl/include/sycl/detail/helpers.hpp
+++ sycl/include/sycl/detail/helpers.hpp
@@ -242,10 +242,8 @@ getSPIRVMemorySemanticsMask(const access::fence_space AccessSpace,
 // To ensure loop unrolling is done when processing dimensions.
 template <size_t... Inds, class F>
 void loop_impl(std::integer_sequence<size_t, Inds...>, F &&f) {
-#if INTEL_CUSTOMIZATION
   // Partial temporary revert because of the CMPLRLLVM-47016.
-  (f(Inds), ...);
-#endif // INTEL_CUSTOMIZATION
+  (f(Inds), ...); // INTEL
 }
 
 template <size_t count, class F> void loop(F &&f) {
