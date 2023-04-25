36e3e64121e3a306c8cb96e4e9c9321a11805238
iclsrc <iclsrc@intel.com>
2023-04-25 06:05:02 -0700

    Resolve of merge (conflict) 8d75f1caef35 [SYCL] Make backend a property of a platform (#9153) 'sycl' -> 'xmain-web' (1 commits) (#12670)
    * Resolve of merge (conflict) 8d75f1caef35 [SYCL] Make backend a property of a platform (#9153) 'sycl' -> 'xmain-web' (1 commits)
    
      CONFLICT (content): Merge conflict in sycl/unittests/pi/BackendString.hpp
    
      commit 8d75f1caef354914961bea0d7cac3015918dc84e
      Author: smaslov-intel <sergey.v.maslov@intel.com>
      Date:   Tue Apr 25 01:02:05 2023 -0700
    
          [SYCL] Make backend a property of a platform (#9153)
    
          Signed-off-by: Sergey V Maslov <sergey.v.maslov@intel.com>
    
    * resolve
    
    ---------
    
    Co-authored-by: iclsrc <ia.compiler.tools.git@intel.com>
    Co-authored-by: Freddy Ye <freddy.ye@intel.com>

--- sycl/unittests/pi/BackendString.hpp
+++ sycl/unittests/pi/BackendString.hpp
@@ -7,24 +7,6 @@
 #include <detail/plugin.hpp>
 
 namespace pi {
-<<<<<<< HEAD
-inline const char *GetBackendString(sycl::backend backend) {
-  switch (backend) {
-#define PI_BACKEND_STR(backend_name)                                           \
-  case sycl::backend::backend_name:                                            \
-    return #backend_name
-    PI_BACKEND_STR(hip);
-    PI_BACKEND_STR(cuda);
-    PI_BACKEND_STR(host);
-    PI_BACKEND_STR(opencl);
-    PI_BACKEND_STR(level_zero);
-#if INTEL_CUSTOMIZATION
-    PI_BACKEND_STR(ext_intel_esimd_emulator);
-#endif // INTEL_CUSTOMIZATION
-#undef PI_BACKEND_STR
-  default:
-    return "Unknown Plugin";
-=======
 inline const char *GetBackendString(const sycl::detail::plugin &Plugin) {
   std::stringstream Str;
   for (sycl::backend Backend :
@@ -34,7 +16,6 @@ inline const char *GetBackendString(const sycl::detail::plugin &Plugin) {
     if (Plugin.hasBackend(Backend)) {
       Str << Backend;
     }
->>>>>>> 8d75f1caef354914961bea0d7cac3015918dc84e
   }
   return Str.str().c_str();
 }
