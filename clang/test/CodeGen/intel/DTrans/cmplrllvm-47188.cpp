28948b3fb8251852200ccf9e3ab80d1b5b20e54d
Erich Keane <erich.keane@intel.com>
2023-05-02 05:55:11 -0700

    [DTRANS] Implement complex vector type metadata emission (#12852)
    * [DTRANS] Implement complex vector type metadata emission
    
    As reported in CMPLRLLVM-47188, we never ran across a complex type
    before, but when they are represented as a vector of 2 elements, we need
    to emit them properly.  This patch does so.

---
+++ clang/test/CodeGen/intel/DTrans/cmplrllvm-47188.cpp
@@ -0,0 +1,23 @@
+// REQUIRES: intel_feature_sw_dtrans
+// RUN: %clang_cc1 -disable-llvm-passes -O2 -triple x86_64-linux-gnu -emit-dtrans-info -fintel-compatibility -emit-llvm -opaque-pointers %s -o - | FileCheck %s
+
+struct Vector {
+  virtual void add(__complex__ float);
+};
+
+void foo() {
+  Vector *u;
+  u->add(-1);
+}
+
+// CHECK: define dso_local void @_Z3foov
+// CHECK: alloca ptr, align 8, !intel_dtrans_type ![[VECTOR_PTR:[0-9]+]]
+// CHECK: call void %{{.*}} !intel_dtrans_type ![[FUNC_MD:[0-9]+]]
+
+// CHECK: ![[VECTOR_PTR]] = !{%struct._ZTS6Vector.Vector zeroinitializer, i32 1}
+
+// CHECK: ![[FUNC_MD]] = !{!"F", i1 false, i32 2, ![[VOID:[0-9]+]], ![[VECTOR_PTR]], ![[FLOAT_VECTOR:[0-9]+]]}
+// CHECK: ![[VOID]] = !{!"void", i32 0}
+// CHECK: ![[FLOAT_VECTOR]] = !{!"V", i32 2, ![[FLOAT:[0-9]+]]}
+// CHECK: ![[FLOAT]] = !{float {{.*}}, i32 0}
+
