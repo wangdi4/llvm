4df622441ec6dade18f302531b754f4feba91edb
iclsrc <iclsrc@intel.com>
2023-05-05 11:39:01 -0700

    Resolve of merge (conflict) 21ff6aa5d6cc Pull after merge ... 'xmain' -> 'xmain-web' (#13008)
    * Resolve of merge (conflict) 21ff6aa5d6cc Pull after merge ... 'xmain' -> 'xmain-web'
    
      CONFLICT (content): Merge conflict in clang/test/Driver/sycl-offload.cpp
    
      commit 21ff6aa5d6ccaca43e3d1011b3a0bded847ceac8
      Merge: 585ec5b4b40f d6eada4ad1f2
      Author: Artur Gainullin <artur.gainullin@intel.com>
      Date:   Fri May 5 10:08:06 2023 -0700
    
          Pull after merge ...
    
    * Resolved sycl-offload.cpp using reverse patching with fuzz level 0
    
    ---------
    
    Co-authored-by: iclsrc <ia.compiler.tools.git@intel.com>
    Co-authored-by: Ji, Jinsong <jinsong.ji@intel.com>

--- clang/test/Driver/sycl-offload.cpp
+++ clang/test/Driver/sycl-offload.cpp
@@ -170,7 +170,3 @@
 // RUN:          -target x86_64-unknown-linux-gnu -### 2>&1 \
 // RUN:  | FileCheck -check-prefix IGNORE_INPUT %s
 // IGNORE_INPUT: input unused
-<<<<<<< HEAD
-=======
-
->>>>>>> 21ff6aa5d6ccaca43e3d1011b3a0bded847ceac8
