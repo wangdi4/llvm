dbf1ff226dea2cf4c7d71370428c84115893a674
Erich Keane <erich.keane@intel.com>
2023-05-09 15:46:18 -0700

    [DTRANS] Implement array types decayed in function types (#12850) (#12930)
    As reported in CMPLRLLVM-47160, a type that contains only an array of pointers to an opaque type is represented as JUST that array, which is then decayed to just the pointer to opaque type.  This follows previously observed VLA rules, so this patch just enables those in this case.
    
    JIRA: CMPLRLLVM-47160
    Xmain PR: #12850

---
+++ clang/test/CodeGen/intel/DTrans/cmplrllvm-47160.cpp
@@ -0,0 +1,28 @@
+// REQUIRES: intel_feature_sw_dtrans
+// RUN: %clang_cc1 -disable-llvm-passes -O2 -triple x86_64-linux-gnu -emit-dtrans-info -fintel-compatibility -emit-llvm -opaque-pointers %s -o - | FileCheck %s
+
+struct poly_int {
+  poly_int();
+  union U *coeffs[1];
+};
+
+// CHECK: define dso_local "intel_dtrans_func_index"="1" ptr @{{.*}}(ptr "intel_dtrans_func_index"="2" %{{.*}}){{.*}} !intel.dtrans.func.type ![[FUNC_MD:[0-9]+]]
+poly_int to_poly_offset (poly_int J)
+{
+  poly_int I;
+  return I;
+}
+
+// CHECK: declare !intel.dtrans.func.type ![[POLY_CTOR:[0-9]+]] void {{.*}}(ptr {{.*}} "intel_dtrans_func_index"="1")
+
+// CHECK: !intel.dtrans.types = !{![[POLY_INT_TY:[0-9]+]], ![[UNION_TY:[0-9]+]]}
+
+// CHECK: ![[POLY_INT_TY]] = !{!"S", %struct._ZTS8poly_int.poly_int zeroinitializer, i32 1, ![[ARR:[0-9]+]]}
+// CHECK: ![[ARR]] = !{!"A", i32 1, ![[UNION_PTR:[0-9]+]]}
+// CHECK: ![[UNION_PTR]] = !{%union._ZTS1U.U zeroinitializer, i32 1}
+// CHECK: ![[UNION_TY]] = !{!"S", %union._ZTS1U.U zeroinitializer, i32 -1}
+
+// CHECK: ![[FUNC_MD]] = distinct !{![[UNION_PTR]], ![[UNION_PTR]]}
+
+// CHECK: ![[POLY_CTOR]] = distinct !{![[POLY_PTR:[0-9]+]]}
+// CHECK: ![[POLY_PTR]] = !{%struct._ZTS8poly_int.poly_int zeroinitializer, i32 1}
