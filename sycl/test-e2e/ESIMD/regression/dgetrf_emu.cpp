30148ba3e8063df03a943f1dc6904e84a77ecd2e
yubingex007-a11y <bing1.yu@intel.com>
2023-05-12 21:39:06 +0800

    [SYCL][ESIMD] Disable inline for dgetrf.cpp for esimd_emulator otherwise host compile will hang (#13138)

--- sycl/test-e2e/ESIMD/regression/dgetrf_emu.cpp
+++ sycl/test-e2e/ESIMD/regression/dgetrf_emu.cpp
@@ -1,4 +1,4 @@
-//==-------------- dgetrf.cpp  - DPC++ ESIMD on-device test ----------------==//
+//==-------------- dgetrf_emu.cpp  - DPC++ ESIMD on-device test ------------==//
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
@@ -6,11 +6,9 @@
 //
 //===----------------------------------------------------------------------===//
 
-// REQUIRES: gpu
-// UNSUPPORTED: gpu-intel-gen9 && windows
-// UNSUPPORTED: cuda || hip || esimd_emulator
-// RUN: %clangxx -fsycl %s -I%S/.. -o %t.out
-// RUN: %GPU_RUN_PLACEHOLDER %t.out 3 2 1
+// REQUIRES: esimd_emulator
+// RUN: %{build} -I%S/.. -fno-inline-functions -o %t.out
+// RUN: %{run} %t.out 3 2 1
 //
 // This test checks the correctness of ESIMD program for batched LU
 // decomposition without pivoting. The program contains multiple branches
