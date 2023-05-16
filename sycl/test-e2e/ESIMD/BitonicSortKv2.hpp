296618d3d0e8a83f323ae64094d4c604233ec4c7
iclsrc <iclsrc@intel.com>
2023-05-16 09:21:27 -0700

    Resolve of merge (conflict) d80a69bfa5cd [SYCL][ESIMD][E2E] Fix BitonicSortKv2 tests on emulator (#.. 'xmain' -> 'xmain-web' (1 commits) (#13245)
    * Resolve of merge (conflict) d80a69bfa5cd [SYCL][ESIMD][E2E] Fix BitonicSortKv2 tests on emulator (#.. 'xmain' -> 'xmain-web' (1 commits)
    
      CONFLICT (add/add): Merge conflict in sycl/test-e2e/ESIMD/BitonicSortKv2.hpp
      CONFLICT (add/add): Merge conflict in sycl/test-e2e/ESIMD/BitonicSortKv2_emu.cpp
    
      commit d80a69bfa5cdb332175b852c934ca19cf89bc7a3
      Author: Nick Sarnie <sarnex@users.noreply.github.com>
      Date:   Tue May 16 08:52:34 2023 -0400
    
          [SYCL][ESIMD][E2E] Fix BitonicSortKv2 tests on emulator (#13216)
    
          * [SYCL][ESIMD][E2E] Fix another esimd emu test
    
          Cherry pick of https://github.com/intel/llvm/pull/9459
    
          Signed-off-by: Sarnie, Nick <nick.sarnie@intel.com>
    
          * clang format
    
          Signed-off-by: Sarnie, Nick <nick.sarnie@intel.com>
    
          ---------
    
          Signed-off-by: Sarnie, Nick <nick.sarnie@intel.com>
    
    ---------
    
    Co-authored-by: iclsrc <ia.compiler.tools.git@intel.com>
    Co-authored-by: Gainullin, Artur <artur.gainullin@intel.com>

--- sycl/test-e2e/ESIMD/BitonicSortKv2.hpp
+++ sycl/test-e2e/ESIMD/BitonicSortKv2.hpp
@@ -1,19 +1,10 @@
-<<<<<<< HEAD
 //==------------- BitonicSortKv2.hpp  - DPC++ ESIMD on-device test -----==//
-=======
-//==---------------- BitonicSortKv2.hpp  - DPC++ ESIMD on-device test ------==//
->>>>>>> d80a69bfa5cdb332175b852c934ca19cf89bc7a3
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
 // SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 //
-<<<<<<< HEAD
 //==--------------------------------------------------------------------==//
-=======
-//===----------------------------------------------------------------------===//
-
->>>>>>> d80a69bfa5cdb332175b852c934ca19cf89bc7a3
 #include "esimd_test_utils.hpp"
 
 #include <algorithm>
