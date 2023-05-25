73bb83915def4e97c4ce560702c9c7b55d101037
Erich Keane <erich.keane@intel.com>
2023-05-25 06:29:42 -0700

    [DTRANS] Ensure we correctly handle non-powers-of-two with flattened types (#13405)
    Some types can be flattened to integers at times, which doesn't really
    matter to DTrans, however it is necessary that we properly generate the
    types so that the metadata is accurate later.
    
    This fixes https://jira.devtools.intel.com/browse/CMPLRLLVM-47969

---
+++ clang/test/CodeGen/intel/DTrans/FlattenedTypeNonPowerOfTwo.cpp
@@ -0,0 +1,31 @@
+// REQUIRES: intel_feature_sw_dtrans
+// RUN: %clang_cc1 -disable-llvm-passes -O2 -triple x86_64-linux-gnu -mconstructor-aliases -emit-dtrans-info -fintel-compatibility -emit-llvm -opaque-pointers %s -o - | FileCheck %s --check-prefixes=CHECK
+struct ObjectSizeOpts {
+  char EvalMode = 0;
+  bool RoundToAlign = false;
+  bool NullIsUnknownSize = false;
+};
+
+struct ObjectSizeOffsetVisitor {
+  ObjectSizeOffsetVisitor(ObjectSizeOpts Options = {});
+};
+
+ObjectSizeOffsetVisitor::ObjectSizeOffsetVisitor( ObjectSizeOpts Options)
+      { }
+
+// CHECK: define dso_local void @_ZN23ObjectSizeOffsetVisitorC2E14ObjectSizeOpts(ptr {{.*}}"intel_dtrans_func_index"="1" %{{.*}}, i24 %{{.*}}) {{.*}}!intel.dtrans.func.type ![[CTOR:[0-9]+]]
+
+void foo() {
+  ObjectSizeOffsetVisitor Visitor{};
+  // CHECK: call void @_ZN23ObjectSizeOffsetVisitorC1E14ObjectSizeOpts(ptr {{.*}}%{{.*}}, i24 %{{.*}}){{.*}} !intel_dtrans_type ![[FPTR_TYPE:[0-9]+]]
+}
+
+// CHECK: !intel.dtrans.types = !{![[OFFSET_VISITOR:[0-9]+]], ![[OPTS:[0-9]+]]}
+// CHECK: ![[OFFSET_VISITOR]] = !{!"S", %struct._ZTS23ObjectSizeOffsetVisitor.ObjectSizeOffsetVisitor zeroinitializer, i32 1, ![[CHAR:[0-9]+]]}
+// CHECK: ![[CHAR]] = !{i8 0, i32 0}
+// CHECK: ![[OPTS]] = !{!"S", %struct._ZTS14ObjectSizeOpts.ObjectSizeOpts zeroinitializer, i32 3, !2, !2, !2} 
+// CHECK: ![[CTOR]] = distinct !{![[VISITOR_PTR:[0-9]+]]}
+// CHECK: ![[VISITOR_PTR]] = !{%struct._ZTS23ObjectSizeOffsetVisitor.ObjectSizeOffsetVisitor zeroinitializer, i32 1}
+// CHECK: ![[FPTR_TYPE]] = !{!"F", i1 false, i32 2, ![[VOID:[0-9]+]], ![[VISITOR_PTR]], ![[I24:[0-9]+]]}
+// CHECK: ![[VOID]] = !{!"void", i32 0}
+// CHECK: ![[I24]] = !{i24 0, i32 0}
