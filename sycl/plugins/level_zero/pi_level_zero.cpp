f119ada9e157bbc06c846e7862ba914d26ffd2f6
iclsrc <iclsrc@intel.com>
2023-05-30 10:50:22 -0700

    Resolve of merge (conflict) f2eba7dfa19e Merge from 'sycl' to 'sycl-web' (3 commits) 'sycl-web' -> 'xmain-web' (#13559)
    * Resolve of merge (conflict) f2eba7dfa19e Merge from 'sycl' to 'sycl-web' (3 commits) 'sycl-web' -> 'xmain-web'
    
      CONFLICT (content): Merge conflict in sycl/plugins/level_zero/pi_level_zero.cpp
    
      commit f2eba7dfa19ef005355018d4454e21fc1883953f
      Merge: 56b6067cb3f5 3c35b1b59093
      Author: iclsrc <ia.compiler.tools.git@intel.com>
      Date:   Fri May 26 10:35:09 2023 -0700
    
          Merge from 'sycl' to 'sycl-web' (3 commits)
    
    * Resolve conflict
    
    * [SYCL][UR] Update UR loader (#9637)
    
    Signed-off-by: Jaime Arteaga <jaime.a.arteaga.molina@intel.com>
    
    ---------
    
    Signed-off-by: Jaime Arteaga <jaime.a.arteaga.molina@intel.com>
    Co-authored-by: iclsrc <ia.compiler.tools.git@intel.com>
    Co-authored-by: Gainullin, Artur <artur.gainullin@intel.com>
    Co-authored-by: Jaime Arteaga <jaime.a.arteaga.molina@intel.com>

--- sycl/plugins/level_zero/pi_level_zero.cpp
+++ sycl/plugins/level_zero/pi_level_zero.cpp
@@ -102,52 +102,8 @@ pi_result
 piextDeviceSelectBinary(pi_device Device, // TODO: does this need to be context?
                         pi_device_binary *Binaries, pi_uint32 NumBinaries,
                         pi_uint32 *SelectedBinaryInd) {
-<<<<<<< HEAD
-
-  PI_ASSERT(Device, PI_ERROR_INVALID_DEVICE);
-  PI_ASSERT(SelectedBinaryInd, PI_ERROR_INVALID_VALUE);
-  PI_ASSERT(NumBinaries == 0 || Binaries, PI_ERROR_INVALID_VALUE);
-
-  // TODO: this is a bare-bones implementation for choosing a device image
-  // that would be compatible with the targeted device. An AOT-compiled
-  // image is preferred over SPIR-V for known devices (i.e. Intel devices)
-  // The implementation makes no effort to differentiate between multiple images
-  // for the given device, and simply picks the first one compatible.
-  //
-  // Real implementation will use the same mechanism OpenCL ICD dispatcher
-  // uses. Something like:
-  //   PI_VALIDATE_HANDLE_RETURN_HANDLE(ctx, PI_ERROR_INVALID_CONTEXT);
-  //     return context->dispatch->piextDeviceSelectIR(
-  //       ctx, images, num_images, selected_image);
-  // where context->dispatch is set to the dispatch table provided by PI
-  // plugin for platform/device the ctx was created for.
-
-  // Look for GEN binary, which we known can only be handled by Level-Zero now.
-  const char *BinaryTarget = __SYCL_PI_DEVICE_BINARY_TARGET_SPIRV64_GEN;
-
-  // Find the appropriate device image, fallback to spirv if not found
-  constexpr pi_uint32 InvalidInd = (std::numeric_limits<pi_uint32>::max)();
-  pi_uint32 Spirv = InvalidInd;
-
-  for (pi_uint32 i = 0; i < NumBinaries; ++i) {
-    if (strcmp(Binaries[i]->DeviceTargetSpec, BinaryTarget) == 0) {
-      *SelectedBinaryInd = i;
-      return PI_SUCCESS;
-    }
-    if (strcmp(Binaries[i]->DeviceTargetSpec,
-               __SYCL_PI_DEVICE_BINARY_TARGET_SPIRV64) == 0)
-      Spirv = i;
-  }
-  // Points to a spirv image, if such indeed was found
-  if ((*SelectedBinaryInd = Spirv) != InvalidInd)
-    return PI_SUCCESS;
-
-  // No image can be loaded for the given device
-  return PI_ERROR_INVALID_BINARY;
-=======
   return pi2ur::piextDeviceSelectBinary(Device, Binaries, NumBinaries,
                                         SelectedBinaryInd);
->>>>>>> f2eba7dfa19ef005355018d4454e21fc1883953f
 }
 
 pi_result piextDeviceGetNativeHandle(pi_device Device,
