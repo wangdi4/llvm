bb05d536201b345bd23b007469f3815b207e94cc
fineg74 <61437305+fineg74@users.noreply.github.com>
2023-06-02 09:49:22 -0700

    [ESIMD][E2E] Temporarily disable -ffast-math option for 7 LIT tests (#9660) (#13659)
    This PR is a work around for tests failing when compiled with icpx and
    succeeding when compiled with clang++. The root cause of that behavior
    is fast-math option that is enabled by default when using icpx and
    disabled by default when using clang++. As a work around the affected
    tests will be compiled with no-fast-math option.

--- sycl/test-e2e/ESIMD/regression/dgetrf.cpp
+++ sycl/test-e2e/ESIMD/regression/dgetrf.cpp
@@ -5,13 +5,15 @@
 // SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 //
 //===----------------------------------------------------------------------===//
-
+// TODO: remove fno-fast-math option once the issue is investigated and the test
+// is fixed.
 // UNSUPPORTED: esimd_emulator
+// DEFINE: %{mathflags} = %if cl_options %{/clang:-fno-fast-math%} %else %{-fno-fast-math%}
 // INTEL_CUSTOMIZATION
 // this is a temporary fix for CMPLRTST-21037
 // we pass -fno-inline-functions to sure it won't hang in host compile
 // we don't do the same thing in intel/llvm since it will lead to runfail
-// RUN: %{build} -I%S/.. -O2 -fno-inline-functions -o %t.out
+// RUN: %{build} %{mathflags} -I%S/.. -O2 -fno-inline-functions -o %t.out
 // end INTEL_CUSTOMIZATION
 // RUN: %{run} %t.out 3 2 1
 //
