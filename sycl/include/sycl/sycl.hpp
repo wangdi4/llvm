1eb3869a9613fe593ae3ea3830ae21600faba7b8
Brox Chen <brox.chen@intel.com>
2023-06-05 19:28:48 -0700

    add annotated_ptr/arg header to sycl.hpp (#13473)
    The annotated_ptr/arg header is removed previously due to release-related reasons. This PR revert the change of #12231

--- sycl/include/sycl/sycl.hpp
+++ sycl/include/sycl/sycl.hpp
@@ -65,6 +65,13 @@
 #include <sycl/ext/intel/experimental/pipe_properties.hpp>
 #include <sycl/ext/intel/experimental/pipes.hpp>
 #include <sycl/ext/intel/usm_pointers.hpp>
+/* INTEL_CUSTOMIZATION */
+#ifndef DISABLE_SYCL_EXT_ANNOTATED_INTERFACES
+#include <sycl/ext/oneapi/annotated_arg/annotated_arg.hpp>
+#include <sycl/ext/oneapi/annotated_arg/annotated_ptr.hpp>
+#include <sycl/ext/oneapi/annotated_arg/properties.hpp>
+#endif
+/* end INTEL_CUSTOMIZATION */
 #include <sycl/ext/oneapi/device_global/device_global.hpp>
 #include <sycl/ext/oneapi/device_global/properties.hpp>
 #include <sycl/ext/oneapi/experimental/ballot_group.hpp>
--- sycl/test/extensions/annotated_arg/annotated_arg_for_ptr.cpp
+++ sycl/test/extensions/annotated_arg/annotated_arg_for_ptr.cpp
@@ -3,9 +3,6 @@
 
 #include "sycl/sycl.hpp"
 #include <sycl/ext/intel/fpga_extensions.hpp>
-#include <sycl/ext/oneapi/annotated_arg/annotated_arg.hpp>
-#include <sycl/ext/oneapi/annotated_arg/annotated_ptr.hpp>
-#include <sycl/ext/oneapi/annotated_arg/properties.hpp>
 
 #include <iostream>
 
