5a0a12d41387b180d00ecc7519b2244c8469a045
iclsrc <iclsrc@intel.com>
2023-06-21 14:30:13 -0700

    Resolve of merge (conflict) 8b85b6400b82 [SYCL][ESIMD][Driver] Pass -fsycl-esimd-force-stateless-me.. 'sycl' -> 'xmain-web' (#14047)
    CONFLICT (content): Merge conflict in
    clang/lib/Frontend/InitPreprocessor.cpp
    
      commit 8b85b6400b8271cf3bcf8e91fe1cb81a2665e23a
      Author: Nick Sarnie <sarnex@users.noreply.github.com>
      Date:   Thu Jun 15 11:35:17 2023 -0400
    
    [SYCL][ESIMD][Driver] Pass -fsycl-esimd-force-stateless-mem to host
    (#9825)
    
          Right now, the `-fsycl-esimd-force-stateless-mem` flag sets the
    `__ESIMD_FORCE_STATELESS_MEM` macro for the device compiler only. We
          have some APIs that have different arguments with
          `__ESIMD_FORCE_STATELESS_MEM` vs without it, so getting the host
          compiler to not error when calling one of those functions can be
          frustrating for the user.
    
          Make `-fsycl-esimd-force-stateless-mem` set
          `__ESIMD_FORCE_STATELESS_MEM` for the host compiler too.
    
          I also found we had some esimd_emulator tests that were testing
    stateless mode, but the emulator never uses the device code and always
    uses host code, so it wasn't even testing stateless mode. We discussed
          this internally and decided to disable the tests on the emulator.
    
          ---------
    
          Signed-off-by: Sarnie, Nick <nick.sarnie@intel.com>
    
    ---------
    
    Co-authored-by: iclsrc <ia.compiler.tools.git@intel.com>
    Co-authored-by: Ji, Jinsong <jinsong.ji@intel.com>

--- clang/lib/Frontend/InitPreprocessor.cpp
+++ clang/lib/Frontend/InitPreprocessor.cpp
@@ -1380,17 +1380,15 @@ static void InitializePredefinedMacros(const TargetInfo &TI,
   if (LangOpts.SYCLUnnamedLambda)
     Builder.defineMacro("__SYCL_UNNAMED_LAMBDA__");
 
-<<<<<<< HEAD
 #if INTEL_CUSTOMIZATION
   // Define a macro indicating that the HLS cpp source file is being compiled.
   if (LangOpts.HLS) {
     Builder.defineMacro("HLS_EXTERNAL", "__attribute__((hls_device))");
   }
 #endif // INTEL_CUSTOMIZATION
-=======
+
   if (LangOpts.SYCLESIMDForceStatelessMem)
     Builder.defineMacro("__ESIMD_FORCE_STATELESS_MEM");
->>>>>>> 8b85b6400b8271cf3bcf8e91fe1cb81a2665e23a
 
   // OpenCL definitions.
   if (LangOpts.OpenCL) {
