6c64be9316819cee178ed180a82af4c3ed117dcf
iclsrc <iclsrc@intel.com>
2023-07-01 12:02:58 -0700

    Resolve of merge (conflict) d8024f656578 [SYCL][E2E] Enable ESIMD/hardware_dispatch.cpp on Linux af.. 'sycl' -> 'xmain-web' (#14238)
    CONFLICT (content): Merge conflict in
    sycl/test-e2e/ESIMD/hardware_dispatch.cpp
    
      commit d8024f65657858ee9073af9d3c603e66697fa66f
      Author: Dmitry Vodopyanov <dmitry.vodopyanov@intel.com>
      Date:   Thu Jun 29 16:49:24 2023 +0200
    
    [SYCL][E2E] Enable ESIMD/hardware_dispatch.cpp on Linux after the GPU
    driver update (#10132)
    
          Fixes post-commit:
          https://github.com/intel/llvm/pull/10087#issuecomment-1612735241
    
    ---------
    
    Co-authored-by: iclsrc <ia.compiler.tools.git@intel.com>
    Co-authored-by: Jinsong Ji <jinsong.ji@intel.com>

--- sycl/test-e2e/ESIMD/hardware_dispatch.cpp
+++ sycl/test-e2e/ESIMD/hardware_dispatch.cpp
@@ -7,20 +7,11 @@
 //===----------------------------------------------------------------------===//
 // UNSUPPORTED: esimd_emulator
 // RUN: %clangxx -fsycl -fsycl-targets=intel_gpu_bdw %s -o %t.out
-<<<<<<< HEAD
-// RUN: %{run} %t.out
-
-// TODO: Enable the test when GPU RT is updated.
-// XFAIL: windows
-
-// This is basic test for hardware dispatch functionality with ESIMD.
-=======
 // RUN: %t.out
 // TODO: remove XFAIL when the fix in GPU RT for Windows is updated on CI
 // machines
 // XFAIL: windows
 // This is basic test to test hardware dispatch functionality with ESIMD.
->>>>>>> d8024f65657858ee9073af9d3c603e66697fa66f
 
 #include <iostream>
 #include <sycl/ext/intel/esimd.hpp>
