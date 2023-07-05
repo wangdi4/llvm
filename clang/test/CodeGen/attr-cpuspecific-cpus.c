39e2b625cf1d66a468830656273ea2f9d46af14b
Jinsong Ji <jinsong.ji@intel.com>
2023-07-05 11:47:01 -0700

    Comment removed cpu target

--- clang/test/CodeGen/attr-cpuspecific-cpus.c
+++ clang/test/CodeGen/attr-cpuspecific-cpus.c
@@ -32,11 +32,11 @@ ATTR(cpu_specific(haswell)) void CPU(void){}
 ATTR(cpu_specific(core_4th_gen_avx_tsx)) void CPU(void){}
 ATTR(cpu_specific(broadwell)) void CPU(void){}
 ATTR(cpu_specific(core_5th_gen_avx_tsx)) void CPU(void){}
-ATTR(cpu_specific(knl)) void CPU(void){}
+// ATTR(cpu_specific(knl)) void CPU(void){} ;INTEL
 ATTR(cpu_specific(skylake)) void CPU(void){}
 ATTR(cpu_specific(skylake_avx512)) void CPU(void){}
 ATTR(cpu_specific(cannonlake)) void CPU(void){}
-ATTR(cpu_specific(knm)) void CPU(void){}
+// ATTR(cpu_specific(knm)) void CPU(void){} ;INTEL
 
 // ALIAS CPUs
 ATTR(cpu_specific(pentium_iii_no_xmm_regs)) void CPU0(void){}
@@ -44,4 +44,4 @@ ATTR(cpu_specific(core_2nd_gen_avx)) void CPU1(void){}
 ATTR(cpu_specific(core_3rd_gen_avx)) void CPU2(void){}
 ATTR(cpu_specific(core_4th_gen_avx)) void CPU3(void){}
 ATTR(cpu_specific(core_5th_gen_avx)) void CPU4(void){}
-ATTR(cpu_specific(mic_avx512)) void CPU5(void){}
+// ATTR(cpu_specific(mic_avx512)) void CPU5(void){} ;INTEL
