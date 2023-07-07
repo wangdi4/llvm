6c523b188a81ea8b9f187dc1958d34a28b1030e4
iclsrc <iclsrc@intel.com>
2023-07-06 22:03:20 -0700

    Resolve of merge (conflict) f3d0613d852a [CaptureTracking] Don't consider comparison of inbounds GE.. 'main' -> 'xmain-web' (#14309)
    CONFLICT (content): Merge conflict in
    llvm/lib/Analysis/CaptureTracking.cpp
    
      commit f3d0613d852a90563a1e8704930a6e79368f106a
      Author: Nikita Popov <npopov@redhat.com>
      Date:   Thu Jun 29 10:04:46 2023 +0200
    
    [CaptureTracking] Don't consider comparison of inbounds GEP with nonnull
    non-capturing
    
          This is required to bring CaptureTracking in line with the new
    semantics from D154051, as gep inbounds p, 0 is now always non-poison.
    
          There are many ways in which the inbounds special case could be
          preserved: If the index is known non-zero, or there is an inbounds
          chain down to an identified object, etc. However, I have opted to
          drop the special case entirely, as it appears to be low value:
    In cases where we can determine such things (e.g. the affected test
    cases) we would end up removing the compare via isGEPKnownNonNull()
          logic anyway.
    
          Differential Revision: https://reviews.llvm.org/D154054
    
    ---------
    
    Co-authored-by: iclsrc <ia.compiler.tools.git@intel.com>
    Co-authored-by: sys_ce_bb <sys_ce_bb@intel.com>
    Co-authored-by: Wenwan Xing <wenwan.xing@intel.com>

--- llvm/lib/Analysis/CaptureTracking.cpp
+++ llvm/lib/Analysis/CaptureTracking.cpp
@@ -81,27 +81,6 @@ CaptureTracker::~CaptureTracker() = default;
 bool CaptureTracker::shouldExplore(const Use *U) { return true; }
 
 bool CaptureTracker::isDereferenceableOrNull(Value *O, const DataLayout &DL) {
-<<<<<<< HEAD
-  // An inbounds GEP can either be a valid pointer (pointing into
-  // or to the end of an allocation), or be null in the default
-  // address space. So for an inbounds GEP there is no way to let
-  // the pointer escape using clever GEP hacking because doing so
-  // would make the pointer point outside of the allocated object
-  // and thus make the GEP result a poison value. Similarly, other
-  // dereferenceable pointers cannot be manipulated without producing
-  // poison.
-  if (auto *GEP = dyn_cast<GetElementPtrInst>(O))
-    if (GEP->isInBounds())
-      return true;
-#if INTEL_CUSTOMIZATION
-  // The same works for llvm.intel.subscript.
-  // FIXME: GEPs that are not inbounds and are subjects to be
-  // converted into llvm.intel.subscript should be conservative and
-  // refrain from returning NotCaptured.
-  if (isa<SubscriptInst>(O))
-    return true;
-#endif // INTEL_CUSTOMIZATION
-=======
   // We want comparisons to null pointers to not be considered capturing,
   // but need to guard against cases like gep(p, -ptrtoint(p2)) == null,
   // which are equivalent to p == p2 and would capture the pointer.
@@ -112,7 +91,16 @@ bool CaptureTracker::isDereferenceableOrNull(Value *O, const DataLayout &DL) {
   //
   // It is not sufficient to check for inbounds GEP here, because GEP with
   // zero offset is always inbounds.
->>>>>>> f3d0613d852a90563a1e8704930a6e79368f106a
+
+#if INTEL_CUSTOMIZATION
+  // The same works for llvm.intel.subscript.
+  // FIXME: GEPs that are not inbounds and are subjects to be
+  // converted into llvm.intel.subscript should be conservative and
+  // refrain from returning NotCaptured.
+  if (isa<SubscriptInst>(O))
+    return true;
+#endif // INTEL_CUSTOMIZATION
+
   bool CanBeNull, CanBeFreed;
   return O->getPointerDereferenceableBytes(DL, CanBeNull, CanBeFreed);
 }
