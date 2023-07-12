09fa2034d2f9e6b0d0669d1c14de96f64fd76b59
HaohaiWen <haohai.wen@intel.com>
2023-07-12 09:10:37 +0800

    [SPGO][Driver] Support -gsplit-dwarf for -fprofile-sample-generate (#14372)
    JIRA: CMPLRLLVM-47600
    -fprofile-sample-generate implies -gline-tables-only but it will not
    emit PDB file on Windows. This patch makes driver parse debug fission
    kind when -fprofile-sample-generate is specified but gN_Group flags is
    not.

--- clang/test/Driver/clang_f_opts.c
+++ clang/test/Driver/clang_f_opts.c
@@ -55,34 +55,6 @@
 // RUN: %clang -### -S -fprofile-sample-accurate %s 2>&1 | FileCheck -check-prefix=CHECK-PROFILE-SAMPLE-ACCURATE %s
 // CHECK-PROFILE-SAMPLE-ACCURATE: "-fprofile-sample-accurate"
 
-// INTEL_CUSTOMIZATION
-// RUN: %clang -### -target x86_64-unknown-linux -fprofile-sample-generate %s 2>&1 | FileCheck --check-prefix=CHECK-PROFILE-SAMPLE-GENERATE %s
-// CHECK-PROFILE-SAMPLE-GENERATE: "-fdebug-info-for-profiling" "-debug-info-kind=line-tables-only" "-dwarf-version=4" "-debugger-tuning=gdb" "-funique-internal-linkage-names"
-// CHECK-PROFILE-SAMPLE-GENERATE: ld"
-
-// RUN: %clang_cl -### -target x86_64-unknown-windows-msvc /fprofile-sample-generate %s 2>&1 | FileCheck --check-prefix=CHECK-PROFILE-SAMPLE-GENERATE-CL %s
-// CHECK-PROFILE-SAMPLE-GENERATE-CL: "-fdebug-info-for-profiling" "-debug-info-kind=line-tables-only" "-dwarf-version=4" "-funique-internal-linkage-names"
-// CHECK-PROFILE-SAMPLE-GENERATE-CL: lld-link" {{.*}} "-profile-sample-generate"
-
-// RUN: %clang_cl -### -target x86_64-unknown-windows-msvc -S /fprofile-sample-generate /Ob1 %s 2>&1 | FileCheck --check-prefix=CHECK-PROFILE-SAMPLE-GENERATE-ERROR1 %s
-// CHECK-PROFILE-SAMPLE-GENERATE-ERROR1: error: option '/Ob1' not supported for SPGO, use 'Ob2/Ob3' instead
-
-// RUN: %clang -### -target x86_64-unknown-linux -S -fprofile-sample-generate -fno-debug-info-for-profiling %s 2>&1 | FileCheck --check-prefix=CHECK-PROFILE-SAMPLE-GENERATE-ERROR2 %s
-// CHECK-PROFILE-SAMPLE-GENERATE-ERROR2: error: option '-fno-debug-info-for-profiling' not supported for SPGO
-
-// RUN: %clang -### -target x86_64-unknown-linux -S -fprofile-sample-generate -fno-unique-internal-linkage-names %s 2>&1 | FileCheck --check-prefix=CHECK-PROFILE-SAMPLE-GENERATE-ERROR3 %s
-// CHECK-PROFILE-SAMPLE-GENERATE-ERROR3: error: option '-fno-unique-internal-linkage-names' not supported for SPGO
-
-// RUN: %clang_cl -### -target x86_64-unknown-windows-msvc -S /fprofile-sample-generate -fuse-ld=link %s 2>&1 | FileCheck --check-prefix=CHECK-PROFILE-SAMPLE-GENERATE-ERROR4 %s
-// CHECK-PROFILE-SAMPLE-GENERATE-ERROR4: error: SPGO requires lld linker and will use it by default on Windows. Do not specify -fuse-ld= or use -fuse-ld=lld instead
-
-// RUN: %clang -### -target x86_64-unknown-linux -S -fprofile-sample-use=%S/Inputs/file.prof %s 2>&1 | FileCheck -check-prefix=CHECK-PROFILE-SAMPLE-USE %s
-// CHECK-PROFILE-SAMPLE-USE: "-fdebug-info-for-profiling" "-debugger-tuning=gdb" "-funique-internal-linkage-names" {{.*}} "-fprofile-sample-use={{.*}}/file.prof"
-
-// RUN: %clang_cl -### -target x86_64-unknown-windows-msvc -S -fprofile-sample-use=%S/Inputs/file.prof %s 2>&1 | FileCheck -check-prefix=CHECK-PROFILE-SAMPLE-USE-CL %s
-// CHECK-PROFILE-SAMPLE-USE-CL: "-fdebug-info-for-profiling" "-funique-internal-linkage-names" {{.*}} "-fprofile-sample-use={{.*}}/file.prof"
-// end INTEL_CUSTOMIZATION
-
 //
 // RUN: %clang -### -x cuda -nocudainc -nocudalib \
 // RUN:    -c -fprofile-sample-use=%S/Inputs/file.prof %s 2>&1 \
