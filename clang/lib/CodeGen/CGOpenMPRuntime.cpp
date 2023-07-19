0e2c8cdb6bb6e00e21bd486c5f82dccaa364bb62
Mike Rice <michael.p.rice@intel.com>
2023-07-19 11:55:54 -0700

    [OpenMP][NFC] Fix uninitialized variable reported by sanitizer (#14530)
    Though VarSize is definitely uninitialized and passed to
    registerTargetGlobalVariable, due to conditions on the other parameters
    it doesn't appear to be used there. This seems to be why it is hard
    to reproduce. Added zero-initializiation for the variable.
    
    This piece of code has been heavily modified from the upstream
    making hard to follow. We should consider trying to clean this up in
    a separate change.
    
    Fixes: CMPLRLLVM-49490

--- clang/lib/CodeGen/CGOpenMPRuntime.cpp
+++ clang/lib/CodeGen/CGOpenMPRuntime.cpp
@@ -11048,7 +11048,7 @@ void CGOpenMPRuntime::registerTargetGlobalVariable(const VarDecl *VD,
   }
 
 #if INTEL_COLLAB
-  int64_t VarSize;
+  int64_t VarSize = 0;
   StringRef VarName;
   std::string ItaniumMangledName;
   if ((*Res == OMPDeclareTargetDeclAttr::MT_To ||
