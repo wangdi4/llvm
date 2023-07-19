3d2faf395d4e95c12a97e9efccca3ef3c298c536
Romanov Vlad <vlad.romanov@intel.com>
2023-07-19 03:39:04 +0200

    Add MLPGO feature (#14079)
    Intro:
    Some optimizations use branch probability information(e.g. inliner).
    Without PGO
    information LLVM doesn't know which basic block is more likely to be
    taken, so
    it uses some heuristics, which often results in 50/50 for branch
    instruction with
    two successors.
    
    The feature tries to improve prediction by employing machine learning.
    So, the
    probability is calculated by providing the machine learning engine with
    a feature
    set describing specific branch instruction and its distinctive features.
    
    Please, see
    [llvm/docs/Intel/MLPGO/MLPGO.rst](https://github.com/intel-restricted/applications.compilers.llvm-project/compare/reviews/private/vromanov/MLPGO2XMAIN?expand=1#diff-27eb56580294f2293d1b791d5c606b60c13d3aee5f0a25bd7c6e0930d885a774)
    for more information.
    
    
    Groups of changes:
    1. ./clang changes are mostly about enabling PGO mode when specific env
    vars are set.
    2. ./llvm/lib/MLPGO_Intel, ./llvm/include/llvm/MLPGO_Intel contain files
    for the new library which provides interface for MLEngine.
    3. ./llvm/include/llvm/Transforms/Instrumentation/MLPGO_Intel,
    ./llvm/lib/Transforms/Instrumentation/MLPGO_Intel contain files that
    collect and dump features. They also have a new pass which updates
    branch probabilities based on inference.
    4. ./llvm/lib/Passes changes are mostly about injection to the pass
    manager.
    
    
    TODO:
    
    - [ ] Add more LIT tests
    - [x] Cleanup the code
    - [ ] Add `-fprofile-ml-use` option and hook up to the optimizer instead
    of using env var
    
    Co-authored with Zeng, Ruiying, Sergey Maslov, Dubov, Evgeni and others
    
    ---------
    
    Signed-off-by: Sergey Maslov <sergey.v.maslov@intel.com>
    Signed-off-by: Maslov <sergey.v.maslov@intel.com>
    Co-authored-by: Xiang1 Zhang <xiang1.zhang@intel.com>
    Co-authored-by: Ruiying Zeng <ruiying.zeng@intel.com>
    Co-authored-by: Gilad Wallach <gilad.wallach@intel.com>
    Co-authored-by: smaslov-intel <sergey.v.maslov@intel.com>
    Co-authored-by: hungryzzz <32137313+hungryzzz@users.noreply.github.com>

--- llvm/include/llvm/Support/PGOOptions.h
+++ llvm/include/llvm/Support/PGOOptions.h
@@ -1,4 +1,21 @@
 //===------ PGOOptions.h -- PGO option tunables ----------------*- C++ -*--===//
+// INTEL_CUSTOMIZATION
+//
+// INTEL CONFIDENTIAL
+//
+// Modifications, Copyright (C) 2023 Intel Corporation
+//
+// This software and the related documents are Intel copyrighted materials, and
+// your use of them is governed by the express license under which they were
+// provided to you ("License"). Unless the License provides otherwise, you may
+// not use, modify, copy, publish, distribute, disclose or transmit this
+// software or the related documents without Intel's prior written permission.
+//
+// This software and the related documents are provided as is, with no express
+// or implied warranties, other than those that are expressly stated in the
+// License.
+//
+// end INTEL_CUSTOMIZATION
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
@@ -45,6 +62,7 @@ struct PGOOptions {
   bool DebugInfoForProfiling;
   bool PseudoProbeForProfiling;
   IntrusiveRefCntPtr<vfs::FileSystem> FS;
+  bool IsCGPGO = false; // INTEL
 };
 } // namespace llvm
 
