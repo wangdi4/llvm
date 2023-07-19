190c38788d3f0d1169f06f483ae1d6587e1f3f13
iclsrc <iclsrc@intel.com>
2023-07-19 15:07:58 -0700

    Resolve of merge (conflict) 0f1298ea3008 Merge from 'sycl' to 'sycl-web' (5 commits) 'sycl-web' -> 'xmain-web' (#14586)
    CONFLICT (content): Merge conflict in lld/ELF/SymbolTable.h
    
      commit 0f1298ea3008f5b37435aecf82cbdfdd328b11a3
      Merge: 1fd30a2eb48c d3b5b5978376
      Author: Wenwan Xing <wenwan.xing@intel.com>
      Date:   Tue Jul 18 14:24:24 2023 +0800
    
          Merge from 'sycl' to 'sycl-web' (5 commits)
    
            CONFLICT (content): Merge conflict in lld/ELF/Arch/ARM.cpp
    CONFLICT (content): Merge conflict in lld/ELF/SyntheticSections.h
    CONFLICT (modify/delete): lld/test/ELF/arm-cmse-diagnostics.s deleted in
    d3b5b597837605ea00a93a70a8273c70ff9cce1a and modified in HEAD. Version
    HEAD of lld/test/ELF/arm-cmse-diagnostics.s left in tree.
    CONFLICT (modify/delete): lld/test/ELF/arm-cmse-implib.s deleted in
    d3b5b597837605ea00a93a70a8273c70ff9cce1a and modified in HEAD. Version
    HEAD of lld/test/ELF/arm-cmse-implib.s left in tree.
    CONFLICT (modify/delete): lld/test/ELF/arm-cmse-keep-sections.s deleted
    in d3b5b597837605ea00a93a70a8273c70ff9cce1a and modified in HEAD.
    Version HEAD of lld/test/ELF/arm-cmse-keep-sections.s left in tree.
    CONFLICT (modify/delete): lld/test/ELF/arm-cmse-veneers.s deleted in
    d3b5b597837605ea00a93a70a8273c70ff9cce1a and modified in HEAD. Version
    HEAD of lld/test/ELF/arm-cmse-veneers.s left in tree.
    
    ---------
    
    Co-authored-by: iclsrc <ia.compiler.tools.git@intel.com>
    Co-authored-by: sys_ce_bb <sys_ce_bb@intel.com>
    Co-authored-by: Tyler Lanphear <tyler.lanphear@intel.com>

--- lld/ELF/SymbolTable.h
+++ lld/ELF/SymbolTable.h
@@ -77,26 +77,11 @@ public:
   // is used to uniquify them.
   llvm::DenseMap<llvm::CachedHashStringRef, const InputFile *> comdatGroups;
 
-<<<<<<< HEAD
 #if INTEL_CUSTOMIZATION
   // Maps the section name referred to ".gnu.linkonce." to a file
   llvm::DenseMap<llvm::CachedHashStringRef, const InputFile *> gnuLinkOnceGroups;
 #endif // INTEL_CUSTOMIZATION
 
-  // The Map of __acle_se_<sym>, <sym> pairs found in the input objects.
-  // Key is the <sym> name.
-  llvm::SmallMapVector<StringRef, ArmCmseEntryFunction, 1> cmseSymMap;
-
-  // Map of symbols defined in the Arm CMSE import library. The linker must
-  // preserve the addresses in the output objects.
-  llvm::StringMap<Defined *> cmseImportLib;
-
-  // True if <sym> from the input Arm CMSE import library is written to the
-  // output Arm CMSE import library.
-  llvm::StringMap<bool> inCMSEOutImpLib;
-
-=======
->>>>>>> 0f1298ea3008f5b37435aecf82cbdfdd328b11a3
 private:
   SmallVector<Symbol *, 0> findByVersion(SymbolVersion ver);
   SmallVector<Symbol *, 0> findAllByVersion(SymbolVersion ver,
