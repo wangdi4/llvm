2184e4eb0d74bf0c817eefcb9eb74debc15e99ff
Tyler Lanphear <tyler.lanphear@intel.com>
2023-07-21 17:09:10 -0700

    Add header (necessary for createPrintModulePass()) (#14669)

--- llvm/tools/sycl-post-link/sycl-post-link.cpp
+++ llvm/tools/sycl-post-link/sycl-post-link.cpp
@@ -54,6 +54,7 @@
 #include "llvm/GenXIntrinsics/GenXSPIRVWriterAdaptor.h"
 #include "llvm/IR/Dominators.h"
 #include "llvm/IR/IRBuilder.h"
+#include "llvm/IR/IRPrintingPasses.h"
 #include "llvm/IR/LLVMContext.h"
 #include "llvm/IR/LegacyPassManager.h"
 #include "llvm/IR/Module.h"
