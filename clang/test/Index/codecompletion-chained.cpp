d19e2f2a83925e66e377375b91b6ddd87f6d80c8
iclsrc <iclsrc@intel.com>
2023-07-25 02:59:43 -0700

    Resolve of merge (conflict) e0ac46e69d7a Revert "Remove rdar links; NFC" 'main' -> 'xmain-web' (#14722)
    CONFLICT (content): Merge conflict in
    clang/test/Index/codecompletion-chained.cpp
    
      commit e0ac46e69d7adbe327148550ffafe746cbc0ec78
      Author: Mehdi Amini <joker.eph@gmail.com>
      Date:   Mon Jul 17 12:47:39 2023 -0700
    
          Revert "Remove rdar links; NFC"
    
          This reverts commit d618f1c3b12effd0c2bdb7d02108d3551f389d3d.
    This commit wasn't reviewed ahead of time and significant concerns were
    raised immediately after it landed. According to our developer policy
          this warrants immediate revert of the commit.
    
          https://llvm.org/docs/DeveloperPolicy.html#patch-reversion-policy
    
          Differential Revision: https://reviews.llvm.org/D155509
    
    ---------
    
    Co-authored-by: iclsrc <ia.compiler.tools.git@intel.com>
    Co-authored-by: Wenwan Xing <wenwan.xing@intel.com>

--- clang/test/Index/codecompletion-chained.cpp
+++ clang/test/Index/codecompletion-chained.cpp
@@ -28,13 +28,6 @@ Cake::
 #endif
 
 // RUN: c-index-test -write-pch %t1.h.pch %s
-<<<<<<< HEAD
 // INTEL RUN: c-index-test -write-pch %t2.h.pch %s -include-pch %t1.h.pch
-// INTEL RUN: c-index-test -code-completion-at=%s:22:1 %s -include-pch %t2.h.pch | FileCheck -check-prefix=CHECK-TU %s
-// INTEL RUN: c-index-test -code-completion-at=%s:22:7 %s -include-pch %t2.h.pch | FileCheck -check-prefix=CHECK-NAMESPACE %s
-
-=======
-// RUN: c-index-test -write-pch %t2.h.pch %s -include %t1.h
-// RUN: c-index-test -code-completion-at=%s:25:1 %s -include %t2.h | FileCheck -check-prefix=CHECK-TU %s
-// RUN: c-index-test -code-completion-at=%s:25:7 %s -include %t2.h | FileCheck -check-prefix=CHECK-NAMESPACE %s
->>>>>>> e0ac46e69d7adbe327148550ffafe746cbc0ec78
+// INTEL RUN: c-index-test -code-completion-at=%s:25:1 %s -include-pch %t2.h.pch | FileCheck -check-prefix=CHECK-TU %s
+// INTEL RUN: c-index-test -code-completion-at=%s:25:7 %s -include-pch %t2.h.pch | FileCheck -check-prefix=CHECK-NAMESPACE %s
