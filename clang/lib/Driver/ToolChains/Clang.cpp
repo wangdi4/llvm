c6f46c1aed8309c29a02b0556151407a6cff7663
iclsrc <iclsrc@intel.com>
2023-08-03 17:02:10 -0700

    Resolve of merge (conflict) 37bedf8d22b4 [SYCL][Driver] Add hidden option to specify spirv-to-ir-wr.. 'sycl' -> 'xmain-web' (#15037)
    CONFLICT (content): Merge conflict in
    clang/include/clang/Driver/Options.td
    
      commit 37bedf8d22b452bb82e574f49ab4874de90d334b
      Author: Nick Sarnie <sarnex@users.noreply.github.com>
      Date:   Fri Jul 28 15:00:58 2023 -0400
    
    [SYCL][Driver] Add hidden option to specify spirv-to-ir-wrapper flags
    (#10611)
    
    This is a hidden option and is not intended for use by end users. It has
          value for local testing and validation of the generated SPIR-V.
    
          This addresses a missed usecase from
          https://github.com/intel/llvm/pull/9569
    
          Signed-off-by: Sarnie, Nick <nick.sarnie@intel.com>
    
    ---------
    
    Co-authored-by: iclsrc <ia.compiler.tools.git@intel.com>
    Co-authored-by: sys_ce_bb <sys_ce_bb@intel.com>
    Co-authored-by: Jinsong Ji <jinsong.ji@intel.com>

--- clang/lib/Driver/ToolChains/Clang.cpp
+++ clang/lib/Driver/ToolChains/Clang.cpp
@@ -11618,7 +11618,10 @@ void SpirvToIrWrapper::ConstructJob(Compilation &C, const JobAction &JA,
       static_cast<const toolchains::SYCLToolChain &>(getToolChain());
 
   // Handle -Xspirv-to-ir-wrapper
-  TC.TranslateTargetOpt(TCArgs, CmdArgs, options::OPT_Xspirv_to_ir_wrapper,
+#if INTEL_CUSTOMIZATION
+  TC.TranslateTargetOpt(JA.getOffloadingDeviceKind(),
+                        TCArgs, CmdArgs, options::OPT_Xspirv_to_ir_wrapper,
+#endif // INTEL_CUSTOMIZATION
                         options::OPT_Xspirv_to_ir_wrapper_EQ,
                         JA.getOffloadingArch());
 
