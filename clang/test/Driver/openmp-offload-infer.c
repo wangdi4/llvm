7660263d337e27118e0529357100820b68f129f7
iclsrc <iclsrc@intel.com>
2023-08-04 10:13:45 -0700

    Resolve of merge (conflict) 7d9415f20a6c Merge from 'main' to 'sycl-web' (41 commits) 'sycl-web' -> 'xmain-web' (#15062)
    CONFLICT (content): Merge conflict in
    clang/test/Driver/openmp-offload-infer.c
    CONFLICT (content): Merge conflict in
    clang/test/Driver/openmp-offload-jit.c
    CONFLICT (content): Merge conflict in
    clang/test/Driver/x86-malign-branch.c
    
      commit 7d9415f20a6c4fdd9e50ebabd245444e069e0a85
      Merge: 49ff051f1345 662c9ef18094
      Author: Jinsong Ji <jinsong.ji@intel.com>
      Date:   Mon Jul 31 15:32:32 2023 -0700
    
          Merge from 'main' to 'sycl-web' (41 commits)
    
    CONFLICT (content): Merge conflict in
    clang/test/Driver/mlong-double-128.c
    
    ---------
    
    Co-authored-by: iclsrc <ia.compiler.tools.git@intel.com>
    Co-authored-by: Jinsong Ji <jinsong.ji@intel.com>

--- clang/test/Driver/openmp-offload-infer.c
+++ clang/test/Driver/openmp-offload-infer.c
@@ -2,12 +2,8 @@
 // REQUIRES: nvptx-registered-target
 // REQUIRES: amdgpu-registered-target
 
-<<<<<<< HEAD
 // INTEL_CUSTOMIZATION
-// RUN:   not %clang -### --target=x86_64-unknown-linux-gnu -fopenmp=libomp -fopenmp-new-driver \
-=======
-// RUN:   %clang -### --target=x86_64-unknown-linux-gnu -fopenmp=libomp \
->>>>>>> 7d9415f20a6c4fdd9e50ebabd245444e069e0a85
+// RUN:   %clang -### --target=x86_64-unknown-linux-gnu -fopenmp=libomp -fopenmp-new-driver \
 // RUN:          --offload-arch=sm_52 --offload-arch=gfx803 \
 // RUN:          --libomptarget-amdgpu-bc-path=%S/Inputs/hip_dev_lib/libomptarget-amdgpu-gfx803.bc \
 // RUN:          --libomptarget-nvptx-bc-path=%S/Inputs/libomptarget/libomptarget-nvptx-test.bc %s 2>&1 \
@@ -49,12 +45,8 @@
 // CHECK-ARCH-BINDINGS: "x86_64-unknown-linux-gnu" - "clang", inputs: ["[[HOST_BC]]", "[[BINARY]]"], output: "[[HOST_OBJ:.*]]"
 // CHECK-ARCH-BINDINGS: "x86_64-unknown-linux-gnu" - "Offload::Linker", inputs: ["[[HOST_OBJ]]"], output: "a.out"
 
-<<<<<<< HEAD
 // INTEL_CUSTOMIZATION
-// RUN:   not %clang -### --target=x86_64-unknown-linux-gnu -ccc-print-bindings -fopenmp=libomp -fopenmp-new-driver \
-=======
-// RUN:   %clang -### --target=x86_64-unknown-linux-gnu -ccc-print-bindings -fopenmp=libomp \
->>>>>>> 7d9415f20a6c4fdd9e50ebabd245444e069e0a85
+// RUN:   %clang -### --target=x86_64-unknown-linux-gnu -ccc-print-bindings -fopenmp=libomp -fopenmp-new-driver \
 // RUN:     --offload-arch=sm_70 --offload-arch=gfx908 --offload-arch=native \
 // RUN:   %clang -### --target=x86_64-unknown-linux-gnu -ccc-print-bindings -fopenmp \
 // RUN:     --offload-arch=sm_70 --offload-arch=gfx908 --offload-arch=skylake \
