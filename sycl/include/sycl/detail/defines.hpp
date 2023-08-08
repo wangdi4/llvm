0edb77b3e29bd131c4b2d1b80b43942a2c7c20af
Wenwan Xing <wenwan.xing@intel.com>
2023-08-08 11:52:46 +0800

    Fix wrong resolution of merge (#15163)
    Guilty commit :
    https://github.com/intel-restricted/applications.compilers.llvm-project/pull/15156

--- sycl/include/sycl/detail/defines.hpp
+++ sycl/include/sycl/detail/defines.hpp
@@ -10,7 +10,7 @@
 
 #include <sycl/detail/defines_elementary.hpp> // for __SYCL_ID_QUERIES_FIT_...
 
-#if __SYCL_ID_QUERIES_FIT_IN_INT__ && __has_builtin(__builtin_assume)
+#if __SYCL_ID_QUERIES_FIT_IN_INT__ && __has_builtin(__builtin_assume) &&       \
     !__SYCL_EXPLICIT_SIMD__
 #include <climits>
 #define __SYCL_ASSUME_INT(x) __builtin_assume((x) <= INT_MAX)
