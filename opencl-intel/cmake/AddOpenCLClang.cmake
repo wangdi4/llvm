1280bcdaca03595dfadd00da6171e960cb06a118
Wenwan Xing <wenwan.xing@intel.com>
2023-08-16 19:40:23 +0800

    Update opencl-clang tag (#15432)
    To get commit https://github.com/intel/opencl-clang/pull/477

--- opencl-intel/cmake/AddOpenCLClang.cmake
+++ opencl-intel/cmake/AddOpenCLClang.cmake
@@ -22,7 +22,7 @@ if(INTEL_CUSTOMIZATION)
 endif(INTEL_CUSTOMIZATION)
 
 # Download opencl-clang
-set(OPENCL_CLANG_TAG 0729d2ec88690045a693e796f726ff3b79d40db2)
+set(OPENCL_CLANG_TAG ca8c5802603a3c0f51e43e3d0294b78d87ca1f94)
 # Set target name
 set(OPENCL_CLANG_LIBRARY_NAME "common_clang" CACHE STRING "")
 # Override PCH extension map in opencl-clang/options_compile.cpp
