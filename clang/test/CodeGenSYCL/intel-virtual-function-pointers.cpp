3a6360635f5ab16b42429942be04f761a696651c
Jinsong Ji <jinsong.ji@intel.com>
2023-08-30 12:00:38 -0700

    Update intel-virtual-function-pointers

--- clang/test/CodeGenSYCL/intel-virtual-function-pointers.cpp
+++ clang/test/CodeGenSYCL/intel-virtual-function-pointers.cpp
@@ -96,5 +96,5 @@ int main() {
 // CHECK: define dso_local spir_func void {{.+}}test_2{{.+}}
 // CHECK: [[L10:%.*]] = phi ptr [ %memptr.virtualfn, %memptr.virtual ], [ %memptr.nonvirtualfn, %memptr.nonvirtual ]
 // CHECK: [[L11:%.*]] = addrspacecast ptr [[L10]] to ptr addrspace(4)
-// CHECK: call void @__intel_indirect_call_1({{.+}}[[L11]], ptr addrspace(4) %this.adjusted)
+// CHECK: call void @__intel_indirect_call_1({{.+}}[[L11]], ptr addrspace(4) [[GEP:%.*]])
 // CHECK: ret void
