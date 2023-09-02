66ed01bb85081d11248447f310aac1c430318a9a
iclsrc <iclsrc@intel.com>
2023-09-01 20:00:50 -0700

    Resolve of merge (conflict) b1b1f364d633 [Driver] Don't default to DWARF 2 on Solaris 'main' -> 'xmain-web' (#16010)
    CONFLICT (content): Merge conflict in clang/test/CodeGen/dwarf-version.c
    
      commit b1b1f364d63382637fa5f64871ee06bf74bd0405
      Author: Rainer Orth <ro@gcc.gnu.org>
      Date:   Fri Sep 1 20:02:37 2023 +0200
    
          [Driver] Don't default to DWARF 2 on Solaris
    
    `clang` currently defaults to DWARF 2 on Solaris. This dates back to
    LLVM
          3.8.0.  I suspect this is related to `gcc/config/sol2.cc`
          (`solaris_override_options`) doing the same unless
    `HAVE_LD_EH_FRAME_CIEV3`. The latter is 1 on both Solaris 11.3 and 11.4,
          so the workaround has become irrelevant these days.
    
    This patch removes the Solaris override, adjusting affected testcases
          accordingly.
    
    Tested on `amd64-pc-solaris2.11` (`Release` and `Debug` builds) and
          `sparcv9-sun-solaris2.11` (`Release` build).
    
          Differential Revision: https://reviews.llvm.org/D159352
    
    ---------
    
    Co-authored-by: iclsrc <ia.compiler.tools.git@intel.com>
    Co-authored-by: sys_ce_bb <sys_ce_bb@intel.com>
    Co-authored-by: Tyler Lanphear <tyler.lanphear@intel.com>

--- clang/test/CodeGen/dwarf-version.c
+++ clang/test/CodeGen/dwarf-version.c
@@ -2,17 +2,12 @@
 // RUN: %clang -target x86_64-linux-gnu -gdwarf-3 -S -emit-llvm -o - %s | FileCheck %s --check-prefix=VER3
 // RUN: %clang -target x86_64-linux-gnu -gdwarf-4 -S -emit-llvm -o - %s | FileCheck %s --check-prefix=VER4
 // RUN: %clang -target x86_64-linux-gnu -gdwarf-5 -S -emit-llvm -o - %s | FileCheck %s --check-prefix=VER5
-<<<<<<< HEAD
 // INTEL_CUSTOMIZATION
 // RUN: %clang -target x86_64-linux-gnu -g -S -emit-llvm -o - %s | FileCheck %s --check-prefix=VER4
 // RUN: %clang -target x86_64-linux-gnu -gdwarf -S -emit-llvm -o - %s | FileCheck %s --check-prefix=VER4
+// RUN: %clang --target=i386-pc-solaris -g -S -emit-llvm -o - %s | FileCheck %s --check-prefix=VER4
+// RUN: %clang --target=i386-pc-solaris -gdwarf -S -emit-llvm -o - %s | FileCheck %s --check-prefix=VER4
 // end INTEL_CUSTOMIZATION
-=======
-// RUN: %clang -target x86_64-linux-gnu -g -S -emit-llvm -o - %s | FileCheck %s --check-prefix=VER5
-// RUN: %clang -target x86_64-linux-gnu -gdwarf -S -emit-llvm -o - %s | FileCheck %s --check-prefix=VER5
-// RUN: %clang --target=i386-pc-solaris -g -S -emit-llvm -o - %s | FileCheck %s --check-prefix=VER5
-// RUN: %clang --target=i386-pc-solaris -gdwarf -S -emit-llvm -o - %s | FileCheck %s --check-prefix=VER5
->>>>>>> b1b1f364d63382637fa5f64871ee06bf74bd0405
 
 // The -isysroot is used as a hack to avoid LIT messing with the SDKROOT
 // environment variable which indirecty overrides the version in the target
