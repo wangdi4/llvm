ba76f022a9c8386a61c7e6b925dcbe303a6272d7
iclsrc <iclsrc@intel.com>
2023-09-07 22:45:00 -0700

    Resolve of merge (build) 26ccc43d9a32 Merge from 'main' to 'sycl-web' (8 commits) 'sycl-web' -> 'xmain-web' (#16132)
    commit 26ccc43d9a32e34df098d5de45026c88a6be957d
      Merge: f619bc950b17 a07d4c0365ed
      Author: Maronas, Marcos <marcos.maronas@intel.com>
      Date:   Thu Sep 7 03:30:11 2023 -0700
    
          Merge from 'main' to 'sycl-web' (8 commits)
    
            CONFLICT (content): Merge conflict in lld/ELF/Options.td
    
    ---------
    
    Co-authored-by: iclsrc <ia.compiler.tools.git@intel.com>
    Co-authored-by: Tyler Lanphear <tyler.lanphear@intel.com>

--- lld/ELF/Driver.cpp
+++ lld/ELF/Driver.cpp
@@ -1369,8 +1369,9 @@ static void readConfigs(opt::InputArgList &args) {
   config->nostdlib = args.hasArg(OPT_nostdlib);
   config->oFormatBinary = isOutputFormatBinary(args);
   config->omagic = args.hasFlag(OPT_omagic, OPT_no_omagic, false);
-  config->opaquePointers = args.hasFlag(
-      OPT_plugin_opt_opaque_pointers, OPT_plugin_opt_no_opaque_pointers, true);
+#if INTEL_CUSTOMIZATION
+  config->opaquePointers = true;
+#endif // INTEL_CUSTOMIZATION
   config->optRemarksFilename = args.getLastArgValue(OPT_opt_remarks_filename);
   config->optStatsFilename = args.getLastArgValue(OPT_plugin_opt_stats_file);
 
