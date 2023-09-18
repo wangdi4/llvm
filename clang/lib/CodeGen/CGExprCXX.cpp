d744ed826318affce98eed135854e08f24f97561
jyu2-git <jennifer.yu@intel.com>
2023-09-18 10:50:53 -0700

    This is to merge communit https://reviews.llvm.org/D157452 (#16304)
    Author: Alex Voicu <alexandru.voicu@amd.com>
    Date:   Mon Aug 28 20:44:06 2023 +0100
    
    [Clang][CodeGen] `typeid` needs special care when `type_info` is not in
    the default AS
    
    After https://reviews.llvm.org/D153092, for targets that use a
    non-default AS for globals, an "interesting" situation arises around
    typeid and its paired type, type_info:
    
    - on the AST level, the type_info interface is defined with default /
    generic addresses, be it for function arguments, or for this;
    - in IR, type_info values are globals, and thus pointers to type_info
    values are pointers to global
    
    This leads to a mismatch between the function signature / formal type of
    the argument, and its actual type. Currently we try to handle such
    mismatches via `bitcast`, but that is wrong in this case, since an
    `ascast` is required. This patch ensures that iff the pointer to
    `type_info` points to a non-default AS, an ascast is inserted so as to
    match the `typeid` interface / return value type.
    
        Reviewed by: yaxunl
    
        Differential Revision: https://reviews.llvm.org/D157452

--- clang/lib/CodeGen/CGExprCXX.cpp
+++ clang/lib/CodeGen/CGExprCXX.cpp
@@ -2215,21 +2215,20 @@ static llvm::Value *EmitTypeidFromVTable(CodeGenFunction &CGF, const Expr *E,
 }
 
 llvm::Value *CodeGenFunction::EmitCXXTypeidExpr(const CXXTypeidExpr *E) {
-#ifdef INTEL_SYCL_OPAQUEPOINTER_READY
   llvm::Type *PtrTy = llvm::PointerType::getUnqual(getLLVMContext());
-#else // INTEL_SYCL_OPAQUEPOINTER_READY
-  llvm::Type *StdTypeInfoPtrTy =
-    ConvertType(E->getType())->getPointerTo();
-#endif // INTEL_SYCL_OPAQUEPOINTER_READY
+  LangAS GlobAS = CGM.GetGlobalVarAddressSpace(nullptr);
+
+  auto MaybeASCast = [=](auto &&TypeInfo) {
+    if (GlobAS == LangAS::Default)
+      return TypeInfo;
+    return getTargetHooks().performAddrSpaceCast(CGM,TypeInfo, GlobAS,
+                                                 LangAS::Default, PtrTy);
+  };
 
   if (E->isTypeOperand()) {
     llvm::Constant *TypeInfo =
         CGM.GetAddrOfRTTIDescriptor(E->getTypeOperand(getContext()));
-#ifdef INTEL_SYCL_OPAQUEPOINTER_READY
-    return TypeInfo;
-#else // INTEL_SYCL_OPAQUEPOINTER_READY
-    return Builder.CreateBitCast(TypeInfo, StdTypeInfoPtrTy);
-#endif // INTEL_SYCL_OPAQUEPOINTER_READY
+    return MaybeASCast(TypeInfo);
   }
 
   // C++ [expr.typeid]p2:
@@ -2239,20 +2238,10 @@ llvm::Value *CodeGenFunction::EmitCXXTypeidExpr(const CXXTypeidExpr *E) {
   //   type) to which the glvalue refers.
   // If the operand is already most derived object, no need to look up vtable.
   if (E->isPotentiallyEvaluated() && !E->isMostDerived(getContext()))
-#ifdef INTEL_SYCL_OPAQUEPOINTER_READY
     return EmitTypeidFromVTable(*this, E->getExprOperand(), PtrTy);
-#else // INTEL_SYCL_OPAQUEPOINTER_READY
-    return EmitTypeidFromVTable(*this, E->getExprOperand(),
-                                StdTypeInfoPtrTy);
-#endif // INTEL_SYCL_OPAQUEPOINTER_READY
 
   QualType OperandTy = E->getExprOperand()->getType();
-#ifdef INTEL_SYCL_OPAQUEPOINTER_READY
-  return CGM.GetAddrOfRTTIDescriptor(OperandTy);
-#else // INTEL_SYCL_OPAQUEPOINTER_READY
-  return Builder.CreateBitCast(CGM.GetAddrOfRTTIDescriptor(OperandTy),
-                               StdTypeInfoPtrTy);
-#endif // INTEL_SYCL_OPAQUEPOINTER_READY
+  return MaybeASCast(CGM.GetAddrOfRTTIDescriptor(OperandTy));
 }
 
 static llvm::Value *EmitDynamicCastToNull(CodeGenFunction &CGF,
