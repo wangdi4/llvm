b37fde13cf852823daa3eb36773abede5720ec5f
iclsrc <iclsrc@intel.com>
2023-09-18 15:44:32 -0700

    Resolve of merge (conflict) 83e6d2edfcf9 Revert "[ARM] Always lower direct calls as direct when the.. 'main' -> 'xmain-web' (#16341)
    CONFLICT (content): Merge conflict in
    llvm/lib/CodeGen/TargetPassConfig.cpp
    
      commit 83e6d2edfcf9e073167529377eaa250795640d35
      Author: Jon Roelofs <jonathan_roelofs@apple.com>
      Date:   Mon Sep 18 09:42:58 2023 -0700
    
    Revert "[ARM] Always lower direct calls as direct when the outliner is
    enabled (#66434)"
    
          This reverts commit 003bcad9a8b21e15e3786a52b1dafa844075ab84.
    
          ARM folks say it regresses some of their benchmarks:
    https://github.com/llvm/llvm-project/pull/66434#issuecomment-1722424162
    
    ---------
    
    Co-authored-by: iclsrc <ia.compiler.tools.git@intel.com>
    Co-authored-by: Tyler Lanphear <tyler.lanphear@intel.com>

--- llvm/lib/CodeGen/TargetPassConfig.cpp
+++ llvm/lib/CodeGen/TargetPassConfig.cpp
@@ -1305,17 +1305,11 @@ void TargetPassConfig::addMachinePasses() {
   addPass(&LiveDebugValuesID);
   addPass(&MachineSanitizerBinaryMetadataID);
 
-<<<<<<< HEAD
 #if INTEL_CUSTOMIZATION
   if (IntelOptReportEmitter == OptReportOptions::MIR)
     addPass(&MachineOptReportEmitterID);
 #endif  // INTEL_CUSTOMIZATION
 
-  if (EnableMachineOutliner == RunOutliner::NeverOutline)
-    TM->Options.EnableMachineOutliner = false;
-
-=======
->>>>>>> 83e6d2edfcf9e073167529377eaa250795640d35
   if (TM->Options.EnableMachineOutliner &&
       getOptLevel() != CodeGenOptLevel::None &&
       EnableMachineOutliner != RunOutliner::NeverOutline) {
