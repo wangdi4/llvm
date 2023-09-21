fdabde5fb3ed2284deef98feaec4425c03190612
Olga Chupina <olga.chupina@intel.com>
2023-09-21 15:25:20 -0700

    [CMPLRLLVM-51793] Correction for sorting algorithm. (#16398)
    During HIR Opt Var Predicate we sort candidates. Sorting routine should
    return if node levels or if-nesting levels are different.
    
    CMPLRLLVM-51793

--- llvm/lib/Transforms/Intel_LoopTransforms/HIROptVarPredicate.cpp
+++ llvm/lib/Transforms/Intel_LoopTransforms/HIROptVarPredicate.cpp
@@ -1371,13 +1371,15 @@ bool HIROptVarPredicate::processLoop(HLLoop *Loop, bool SetRegionModified,
                                                       EqualCandidates C2) {
     HLIf *If1 = C1.front();
     HLIf *If2 = C2.front();
-    if (If1->getNodeLevel() > If2->getNodeLevel())
-      return true;
+    unsigned NodeLevel1 = If1->getNodeLevel();
+    unsigned NodeLevel2 = If2->getNodeLevel();
+    if (NodeLevel1 != NodeLevel2)
+      return (NodeLevel1 > NodeLevel2);
 
     unsigned If1NestingLevel = getIfNestingLevel(If1);
     unsigned If2NestingLevel = getIfNestingLevel(If2);
-    if (If1NestingLevel < If2NestingLevel)
-      return true;
+    if (If1NestingLevel != If2NestingLevel)
+      return (If1NestingLevel < If2NestingLevel);
 
     return If1->getTopSortNum() < If2->getTopSortNum();
   };
