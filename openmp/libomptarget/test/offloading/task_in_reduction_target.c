dad5aac6bb5c05cbdafa723382ac861ef4832425
Hansang Bae <hansang.bae@intel.com>
2023-09-22 15:26:42 -0500

    [OMP][Offload] Disable new test that requires host runtime change (#16477)
    JIRA: CMPLRLIBS-34637

--- openmp/libomptarget/test/offloading/task_in_reduction_target.c
+++ openmp/libomptarget/test/offloading/task_in_reduction_target.c
@@ -1,5 +1,9 @@
 // RUN: %libomptarget-compile-generic && \
 // RUN: %libomptarget-run-generic
+// INTEL_CUSTOMIZATION
+// UNSUPPORTED: x86_64-pc-linux-gnu
+// TODO: Enable this test after required host runtime change lands on xmain
+// end INTEL_CUSTOMIZATION
 
 #include <omp.h>
 #include <stdio.h>
