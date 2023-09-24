wangdi4/llvm

commit 60920615e06b93f23d3ceb2733d4d94b5e458ad8

[OpenCL] Fix isfinite/isinf/isnormal v1widen mask type (#16450)
CMPLRLLVM-51807. Before this PR, the builtins' mask element type was
i16 for half, i32 for float and i64 for double. The type doesn't match
with vectorizer's characteristic type, i.e. i32. This results in
mismatch between builtin declaration and vectorizer-generated call:

define <8 x i32> @_Z16isfinite_v1widenDv8_dDv8_l(<8 x double> noundef
%x, <8 x i64> noundef %mask)

call <8 x i32> @_Z16isfinite_v1widenDv8_dDv8_l(<8 x double> %13, <8 x
i32> %maskextvector_func)

opencl-intel/backend/libraries/ocl_builtins/GENERIC/code_relational.td
@@ -51,7 +51,7 @@ defvar isnormal_v1widen_code = [{
 // Please refer to masked_math.td for naming convention.
 
 defvar masked_impl_code_I32_G_M = [{
-    $Arg0VecType m$Arg0VarName = select(($Arg0VecType)($DEF0), $Arg0VarName, $Arg1VarName);
+    $Arg0VecType m$Arg0VarName = select(($Arg0VecType)($DEF0), $Arg0VarName, ($Arg0igentype)0 < convert_$Arg0igentype($Arg1VarName));
     return $Func(m$Arg0VarName);
   }];
 

opencl-intel/backend/libraries/ocl_builtins/GENERIC/opencl-v1.2.td
@@ -719,9 +719,9 @@ def isnormal_mask        : OclBuiltin<"isnormal",        (outs rf2igentype:$ret)
 let Types = ExpandTypesByVFAndFlatten<[v1f16, v1f32, v1f64], [4, 8, 16, 32, 64]>.Tout,
     Attrs = [CONSTFUNC, OVERLOADABLE],
     NeedForwardDecl = 1 in {
-def isfinite_v1widen_mask        : OclBuiltin<"isfinite_v1widen",        (outs i32gentype:$ret), (ins gentype:$x, rf2igentype:$mask), [], [], 0, 1>;
-def isinf_v1widen_mask           : OclBuiltin<"isinf_v1widen",           (outs i32gentype:$ret), (ins gentype:$x, rf2igentype:$mask), [], [], 0, 1>;
-def isnormal_v1widen_mask        : OclBuiltin<"isnormal_v1widen",        (outs i32gentype:$ret), (ins gentype:$x, rf2igentype:$mask), [], [], 0, 1>;
+def isfinite_v1widen_mask        : OclBuiltin<"isfinite_v1widen",        (outs i32gentype:$ret), (ins gentype:$x, i32gentype:$mask), [], [], 0, 1>;
+def isinf_v1widen_mask           : OclBuiltin<"isinf_v1widen",           (outs i32gentype:$ret), (ins gentype:$x, i32gentype:$mask), [], [], 0, 1>;
+def isnormal_v1widen_mask        : OclBuiltin<"isnormal_v1widen",        (outs i32gentype:$ret), (ins gentype:$x, i32gentype:$mask), [], [], 0, 1>;
 }
 
 // VectInfo for OclVecClone

opencl-intel/backend/tests/libraries/all_symbols/amx_64bit.ll
@@ -1157,15 +1157,15 @@
 ; CHECK-NEXT: _Z13get_image_dim26ocl_image2d_array_depth_rw
 ; CHECK-NEXT: _Z13get_image_dim26ocl_image2d_array_depth_wo
 ; CHECK-NEXT: _Z13isinf_v1widenDv32_Dh
-; CHECK-NEXT: _Z13isinf_v1widenDv32_DhDv32_s
+; CHECK-NEXT: _Z13isinf_v1widenDv32_DhDv32_i
 ; CHECK-NEXT: _Z13isinf_v1widenDv32_d
-; CHECK-NEXT: _Z13isinf_v1widenDv32_dDv32_l
+; CHECK-NEXT: _Z13isinf_v1widenDv32_dDv32_i
 ; CHECK-NEXT: _Z13isinf_v1widenDv32_f
 ; CHECK-NEXT: _Z13isinf_v1widenDv32_fDv32_i
 ; CHECK-NEXT: _Z13isinf_v1widenDv64_Dh
-; CHECK-NEXT: _Z13isinf_v1widenDv64_DhDv64_s
+; CHECK-NEXT: _Z13isinf_v1widenDv64_DhDv64_i
 ; CHECK-NEXT: _Z13isinf_v1widenDv64_d
-; CHECK-NEXT: _Z13isinf_v1widenDv64_dDv64_l
+; CHECK-NEXT: _Z13isinf_v1widenDv64_dDv64_i
 ; CHECK-NEXT: _Z13isinf_v1widenDv64_f
 ; CHECK-NEXT: _Z13isinf_v1widenDv64_fDv64_i
 ; CHECK-NEXT: _Z13native_acospiDv16_d
@@ -1832,27 +1832,27 @@
 ; CHECK-NEXT: _Z16get_image_height26ocl_image2d_array_depth_rw
 ; CHECK-NEXT: _Z16get_image_height26ocl_image2d_array_depth_wo
 ; CHECK-NEXT: _Z16isfinite_v1widenDv32_Dh
-; CHECK-NEXT: _Z16isfinite_v1widenDv32_DhDv32_s
+; CHECK-NEXT: _Z16isfinite_v1widenDv32_DhDv32_i
 ; CHECK-NEXT: _Z16isfinite_v1widenDv32_d
-; CHECK-NEXT: _Z16isfinite_v1widenDv32_dDv32_l
+; CHECK-NEXT: _Z16isfinite_v1widenDv32_dDv32_i
 ; CHECK-NEXT: _Z16isfinite_v1widenDv32_f
 ; CHECK-NEXT: _Z16isfinite_v1widenDv32_fDv32_i
 ; CHECK-NEXT: _Z16isfinite_v1widenDv64_Dh
-; CHECK-NEXT: _Z16isfinite_v1widenDv64_DhDv64_s
+; CHECK-NEXT: _Z16isfinite_v1widenDv64_DhDv64_i
 ; CHECK-NEXT: _Z16isfinite_v1widenDv64_d
-; CHECK-NEXT: _Z16isfinite_v1widenDv64_dDv64_l
+; CHECK-NEXT: _Z16isfinite_v1widenDv64_dDv64_i
 ; CHECK-NEXT: _Z16isfinite_v1widenDv64_f
 ; CHECK-NEXT: _Z16isfinite_v1widenDv64_fDv64_i
 ; CHECK-NEXT: _Z16isnormal_v1widenDv32_Dh
-; CHECK-NEXT: _Z16isnormal_v1widenDv32_DhDv32_s
+; CHECK-NEXT: _Z16isnormal_v1widenDv32_DhDv32_i
 ; CHECK-NEXT: _Z16isnormal_v1widenDv32_d
-; CHECK-NEXT: _Z16isnormal_v1widenDv32_dDv32_l
+; CHECK-NEXT: _Z16isnormal_v1widenDv32_dDv32_i
 ; CHECK-NEXT: _Z16isnormal_v1widenDv32_f
 ; CHECK-NEXT: _Z16isnormal_v1widenDv32_fDv32_i
 ; CHECK-NEXT: _Z16isnormal_v1widenDv64_Dh
-; CHECK-NEXT: _Z16isnormal_v1widenDv64_DhDv64_s
+; CHECK-NEXT: _Z16isnormal_v1widenDv64_DhDv64_i
 ; CHECK-NEXT: _Z16isnormal_v1widenDv64_d
-; CHECK-NEXT: _Z16isnormal_v1widenDv64_dDv64_l
+; CHECK-NEXT: _Z16isnormal_v1widenDv64_dDv64_i
 ; CHECK-NEXT: _Z16isnormal_v1widenDv64_f
 ; CHECK-NEXT: _Z16isnormal_v1widenDv64_fDv64_i
 ; CHECK-NEXT: _Z16mask_read_imagefi14ocl_image2d_ro11ocl_samplerDv2_f

opencl-intel/backend/tests/libraries/all_symbols/avx2_64bit.ll
@@ -1128,15 +1128,15 @@
 ; CHECK-NEXT: _Z13get_image_dim26ocl_image2d_array_depth_rw
 ; CHECK-NEXT: _Z13get_image_dim26ocl_image2d_array_depth_wo
 ; CHECK-NEXT: _Z13isinf_v1widenDv32_Dh
-; CHECK-NEXT: _Z13isinf_v1widenDv32_DhDv32_s
+; CHECK-NEXT: _Z13isinf_v1widenDv32_DhDv32_i
 ; CHECK-NEXT: _Z13isinf_v1widenDv32_d
-; CHECK-NEXT: _Z13isinf_v1widenDv32_dDv32_l
+; CHECK-NEXT: _Z13isinf_v1widenDv32_dDv32_i
 ; CHECK-NEXT: _Z13isinf_v1widenDv32_f
 ; CHECK-NEXT: _Z13isinf_v1widenDv32_fDv32_i
 ; CHECK-NEXT: _Z13isinf_v1widenDv64_Dh
-; CHECK-NEXT: _Z13isinf_v1widenDv64_DhDv64_s
+; CHECK-NEXT: _Z13isinf_v1widenDv64_DhDv64_i
 ; CHECK-NEXT: _Z13isinf_v1widenDv64_d
-; CHECK-NEXT: _Z13isinf_v1widenDv64_dDv64_l
+; CHECK-NEXT: _Z13isinf_v1widenDv64_dDv64_i
 ; CHECK-NEXT: _Z13isinf_v1widenDv64_f
 ; CHECK-NEXT: _Z13isinf_v1widenDv64_fDv64_i
 ; CHECK-NEXT: _Z13native_acospiDv16_d
@@ -1796,27 +1796,27 @@
 ; CHECK-NEXT: _Z16get_image_height26ocl_image2d_array_depth_rw
 ; CHECK-NEXT: _Z16get_image_height26ocl_image2d_array_depth_wo
 ; CHECK-NEXT: _Z16isfinite_v1widenDv32_Dh
-; CHECK-NEXT: _Z16isfinite_v1widenDv32_DhDv32_s
+; CHECK-NEXT: _Z16isfinite_v1widenDv32_DhDv32_i
 ; CHECK-NEXT: _Z16isfinite_v1widenDv32_d
-; CHECK-NEXT: _Z16isfinite_v1widenDv32_dDv32_l
+; CHECK-NEXT: _Z16isfinite_v1widenDv32_dDv32_i
 ; CHECK-NEXT: _Z16isfinite_v1widenDv32_f
 ; CHECK-NEXT: _Z16isfinite_v1widenDv32_fDv32_i
 ; CHECK-NEXT: _Z16isfinite_v1widenDv64_Dh
-; CHECK-NEXT: _Z16isfinite_v1widenDv64_DhDv64_s
+; CHECK-NEXT: _Z16isfinite_v1widenDv64_DhDv64_i
 ; CHECK-NEXT: _Z16isfinite_v1widenDv64_d
-; CHECK-NEXT: _Z16isfinite_v1widenDv64_dDv64_l
+; CHECK-NEXT: _Z16isfinite_v1widenDv64_dDv64_i
 ; CHECK-NEXT: _Z16isfinite_v1widenDv64_f
 ; CHECK-NEXT: _Z16isfinite_v1widenDv64_fDv64_i
 ; CHECK-NEXT: _Z16isnormal_v1widenDv32_Dh
-; CHECK-NEXT: _Z16isnormal_v1widenDv32_DhDv32_s
+; CHECK-NEXT: _Z16isnormal_v1widenDv32_DhDv32_i
 ; CHECK-NEXT: _Z16isnormal_v1widenDv32_d
-; CHECK-NEXT: _Z16isnormal_v1widenDv32_dDv32_l
+; CHECK-NEXT: _Z16isnormal_v1widenDv32_dDv32_i
 ; CHECK-NEXT: _Z16isnormal_v1widenDv32_f
 ; CHECK-NEXT: _Z16isnormal_v1widenDv32_fDv32_i
 ; CHECK-NEXT: _Z16isnormal_v1widenDv64_Dh
-; CHECK-NEXT: _Z16isnormal_v1widenDv64_DhDv64_s
+; CHECK-NEXT: _Z16isnormal_v1widenDv64_DhDv64_i
 ; CHECK-NEXT: _Z16isnormal_v1widenDv64_d
-; CHECK-NEXT: _Z16isnormal_v1widenDv64_dDv64_l
+; CHECK-NEXT: _Z16isnormal_v1widenDv64_dDv64_i
 ; CHECK-NEXT: _Z16isnormal_v1widenDv64_f
 ; CHECK-NEXT: _Z16isnormal_v1widenDv64_fDv64_i
 ; CHECK-NEXT: _Z16mask_read_imagefi14ocl_image2d_ro11ocl_samplerDv2_f

opencl-intel/backend/tests/libraries/all_symbols/avx512_64bit.ll
@@ -1141,15 +1141,15 @@
 ; CHECK-NEXT: _Z13get_image_dim26ocl_image2d_array_depth_rw
 ; CHECK-NEXT: _Z13get_image_dim26ocl_image2d_array_depth_wo
 ; CHECK-NEXT: _Z13isinf_v1widenDv32_Dh
-; CHECK-NEXT: _Z13isinf_v1widenDv32_DhDv32_s
+; CHECK-NEXT: _Z13isinf_v1widenDv32_DhDv32_i
 ; CHECK-NEXT: _Z13isinf_v1widenDv32_d
-; CHECK-NEXT: _Z13isinf_v1widenDv32_dDv32_l
+; CHECK-NEXT: _Z13isinf_v1widenDv32_dDv32_i
 ; CHECK-NEXT: _Z13isinf_v1widenDv32_f
 ; CHECK-NEXT: _Z13isinf_v1widenDv32_fDv32_i
 ; CHECK-NEXT: _Z13isinf_v1widenDv64_Dh
-; CHECK-NEXT: _Z13isinf_v1widenDv64_DhDv64_s
+; CHECK-NEXT: _Z13isinf_v1widenDv64_DhDv64_i
 ; CHECK-NEXT: _Z13isinf_v1widenDv64_d
-; CHECK-NEXT: _Z13isinf_v1widenDv64_dDv64_l
+; CHECK-NEXT: _Z13isinf_v1widenDv64_dDv64_i
 ; CHECK-NEXT: _Z13isinf_v1widenDv64_f
 ; CHECK-NEXT: _Z13isinf_v1widenDv64_fDv64_i
 ; CHECK-NEXT: _Z13native_acospiDv16_d
@@ -1816,27 +1816,27 @@
 ; CHECK-NEXT: _Z16get_image_height26ocl_image2d_array_depth_rw
 ; CHECK-NEXT: _Z16get_image_height26ocl_image2d_array_depth_wo
 ; CHECK-NEXT: _Z16isfinite_v1widenDv32_Dh
-; CHECK-NEXT: _Z16isfinite_v1widenDv32_DhDv32_s
+; CHECK-NEXT: _Z16isfinite_v1widenDv32_DhDv32_i
 ; CHECK-NEXT: _Z16isfinite_v1widenDv32_d
-; CHECK-NEXT: _Z16isfinite_v1widenDv32_dDv32_l
+; CHECK-NEXT: _Z16isfinite_v1widenDv32_dDv32_i
 ; CHECK-NEXT: _Z16isfinite_v1widenDv32_f
 ; CHECK-NEXT: _Z16isfinite_v1widenDv32_fDv32_i
 ; CHECK-NEXT: _Z16isfinite_v1widenDv64_Dh
-; CHECK-NEXT: _Z16isfinite_v1widenDv64_DhDv64_s
+; CHECK-NEXT: _Z16isfinite_v1widenDv64_DhDv64_i
 ; CHECK-NEXT: _Z16isfinite_v1widenDv64_d
-; CHECK-NEXT: _Z16isfinite_v1widenDv64_dDv64_l
+; CHECK-NEXT: _Z16isfinite_v1widenDv64_dDv64_i
 ; CHECK-NEXT: _Z16isfinite_v1widenDv64_f
 ; CHECK-NEXT: _Z16isfinite_v1widenDv64_fDv64_i
 ; CHECK-NEXT: _Z16isnormal_v1widenDv32_Dh
-; CHECK-NEXT: _Z16isnormal_v1widenDv32_DhDv32_s
+; CHECK-NEXT: _Z16isnormal_v1widenDv32_DhDv32_i
 ; CHECK-NEXT: _Z16isnormal_v1widenDv32_d
-; CHECK-NEXT: _Z16isnormal_v1widenDv32_dDv32_l
+; CHECK-NEXT: _Z16isnormal_v1widenDv32_dDv32_i
 ; CHECK-NEXT: _Z16isnormal_v1widenDv32_f
 ; CHECK-NEXT: _Z16isnormal_v1widenDv32_fDv32_i
 ; CHECK-NEXT: _Z16isnormal_v1widenDv64_Dh
-; CHECK-NEXT: _Z16isnormal_v1widenDv64_DhDv64_s
+; CHECK-NEXT: _Z16isnormal_v1widenDv64_DhDv64_i
 ; CHECK-NEXT: _Z16isnormal_v1widenDv64_d
-; CHECK-NEXT: _Z16isnormal_v1widenDv64_dDv64_l
+; CHECK-NEXT: _Z16isnormal_v1widenDv64_dDv64_i
 ; CHECK-NEXT: _Z16isnormal_v1widenDv64_f
 ; CHECK-NEXT: _Z16isnormal_v1widenDv64_fDv64_i
 ; CHECK-NEXT: _Z16mask_read_imagefi14ocl_image2d_ro11ocl_samplerDv2_f

opencl-intel/backend/tests/libraries/all_symbols/shared.ll
@@ -1222,21 +1222,21 @@
 ; CHECK-NEXT: _Z13fract_ret2ptrDv8_dPS_S1_S_
 ; CHECK-NEXT: _Z13fract_ret2ptrDv8_fPS_S1_S_
 ; CHECK-NEXT: _Z13isinf_v1widenDv16_Dh
-; CHECK-NEXT: _Z13isinf_v1widenDv16_DhDv16_s
+; CHECK-NEXT: _Z13isinf_v1widenDv16_DhDv16_i
 ; CHECK-NEXT: _Z13isinf_v1widenDv16_d
-; CHECK-NEXT: _Z13isinf_v1widenDv16_dDv16_l
+; CHECK-NEXT: _Z13isinf_v1widenDv16_dDv16_i
 ; CHECK-NEXT: _Z13isinf_v1widenDv16_f
 ; CHECK-NEXT: _Z13isinf_v1widenDv16_fDv16_i
 ; CHECK-NEXT: _Z13isinf_v1widenDv4_Dh
-; CHECK-NEXT: _Z13isinf_v1widenDv4_DhDv4_s
+; CHECK-NEXT: _Z13isinf_v1widenDv4_DhDv4_i
 ; CHECK-NEXT: _Z13isinf_v1widenDv4_d
-; CHECK-NEXT: _Z13isinf_v1widenDv4_dDv4_l
+; CHECK-NEXT: _Z13isinf_v1widenDv4_dDv4_i
 ; CHECK-NEXT: _Z13isinf_v1widenDv4_f
 ; CHECK-NEXT: _Z13isinf_v1widenDv4_fDv4_i
 ; CHECK-NEXT: _Z13isinf_v1widenDv8_Dh
-; CHECK-NEXT: _Z13isinf_v1widenDv8_DhDv8_s
+; CHECK-NEXT: _Z13isinf_v1widenDv8_DhDv8_i
 ; CHECK-NEXT: _Z13isinf_v1widenDv8_d
-; CHECK-NEXT: _Z13isinf_v1widenDv8_dDv8_l
+; CHECK-NEXT: _Z13isinf_v1widenDv8_dDv8_i
 ; CHECK-NEXT: _Z13isinf_v1widenDv8_f
 ; CHECK-NEXT: _Z13isinf_v1widenDv8_fDv8_i
 ; CHECK-NEXT: _Z13native_acospiDv16_dS_
@@ -2727,39 +2727,39 @@
 ; CHECK-NEXT: _Z16convert_ushort64Dv64_t
 ; CHECK-NEXT: _Z16get_sub_group_idv
 ; CHECK-NEXT: _Z16isfinite_v1widenDv16_Dh
-; CHECK-NEXT: _Z16isfinite_v1widenDv16_DhDv16_s
+; CHECK-NEXT: _Z16isfinite_v1widenDv16_DhDv16_i
 ; CHECK-NEXT: _Z16isfinite_v1widenDv16_d
-; CHECK-NEXT: _Z16isfinite_v1widenDv16_dDv16_l
+; CHECK-NEXT: _Z16isfinite_v1widenDv16_dDv16_i
 ; CHECK-NEXT: _Z16isfinite_v1widenDv16_f
 ; CHECK-NEXT: _Z16isfinite_v1widenDv16_fDv16_i
 ; CHECK-NEXT: _Z16isfinite_v1widenDv4_Dh
-; CHECK-NEXT: _Z16isfinite_v1widenDv4_DhDv4_s
+; CHECK-NEXT: _Z16isfinite_v1widenDv4_DhDv4_i
 ; CHECK-NEXT: _Z16isfinite_v1widenDv4_d
-; CHECK-NEXT: _Z16isfinite_v1widenDv4_dDv4_l
+; CHECK-NEXT: _Z16isfinite_v1widenDv4_dDv4_i
 ; CHECK-NEXT: _Z16isfinite_v1widenDv4_f
 ; CHECK-NEXT: _Z16isfinite_v1widenDv4_fDv4_i
 ; CHECK-NEXT: _Z16isfinite_v1widenDv8_Dh
-; CHECK-NEXT: _Z16isfinite_v1widenDv8_DhDv8_s
+; CHECK-NEXT: _Z16isfinite_v1widenDv8_DhDv8_i
 ; CHECK-NEXT: _Z16isfinite_v1widenDv8_d
-; CHECK-NEXT: _Z16isfinite_v1widenDv8_dDv8_l
+; CHECK-NEXT: _Z16isfinite_v1widenDv8_dDv8_i
 ; CHECK-NEXT: _Z16isfinite_v1widenDv8_f
 ; CHECK-NEXT: _Z16isfinite_v1widenDv8_fDv8_i
 ; CHECK-NEXT: _Z16isnormal_v1widenDv16_Dh
-; CHECK-NEXT: _Z16isnormal_v1widenDv16_DhDv16_s
+; CHECK-NEXT: _Z16isnormal_v1widenDv16_DhDv16_i
 ; CHECK-NEXT: _Z16isnormal_v1widenDv16_d
-; CHECK-NEXT: _Z16isnormal_v1widenDv16_dDv16_l
+; CHECK-NEXT: _Z16isnormal_v1widenDv16_dDv16_i
 ; CHECK-NEXT: _Z16isnormal_v1widenDv16_f
 ; CHECK-NEXT: _Z16isnormal_v1widenDv16_fDv16_i
 ; CHECK-NEXT: _Z16isnormal_v1widenDv4_Dh
-; CHECK-NEXT: _Z16isnormal_v1widenDv4_DhDv4_s
+; CHECK-NEXT: _Z16isnormal_v1widenDv4_DhDv4_i
 ; CHECK-NEXT: _Z16isnormal_v1widenDv4_d
-; CHECK-NEXT: _Z16isnormal_v1widenDv4_dDv4_l
+; CHECK-NEXT: _Z16isnormal_v1widenDv4_dDv4_i
 ; CHECK-NEXT: _Z16isnormal_v1widenDv4_f
 ; CHECK-NEXT: _Z16isnormal_v1widenDv4_fDv4_i
 ; CHECK-NEXT: _Z16isnormal_v1widenDv8_Dh
-; CHECK-NEXT: _Z16isnormal_v1widenDv8_DhDv8_s
+; CHECK-NEXT: _Z16isnormal_v1widenDv8_DhDv8_i
 ; CHECK-NEXT: _Z16isnormal_v1widenDv8_d
-; CHECK-NEXT: _Z16isnormal_v1widenDv8_dDv8_l
+; CHECK-NEXT: _Z16isnormal_v1widenDv8_dDv8_i
 ; CHECK-NEXT: _Z16isnormal_v1widenDv8_f
 ; CHECK-NEXT: _Z16isnormal_v1widenDv8_fDv8_i
 ; CHECK-NEXT: _Z16soa_fast_length1Dv16_f

opencl-intel/backend/tests/libraries/vectinfo/VectInfo_64bit.ll
@@ -12872,21 +12872,21 @@
 ; CHECK-NEXT:{"_Z5ilogbf","","_ZGVbN4v__Z5ilogbf(_Z5ilogbDv4_f)"},
 ; CHECK-NEXT:{"_Z5ilogbf","","_ZGVbN64v__Z5ilogbf(_Z5ilogbDv64_f)"},
 ; CHECK-NEXT:{"_Z5ilogbf","","_ZGVbN8v__Z5ilogbf(_Z5ilogbDv8_f)"},
-; CHECK-NEXT:{"_Z5isinfDh","","_ZGVbM16v__Z5isinfDh(_Z13isinf_v1widenDv16_DhDv16_s)"},
-; CHECK-NEXT:{"_Z5isinfDh","","_ZGVbM32v__Z5isinfDh(_Z13isinf_v1widenDv32_DhDv32_s)"},
-; CHECK-NEXT:{"_Z5isinfDh","","_ZGVbM4v__Z5isinfDh(_Z13isinf_v1widenDv4_DhDv4_s)"},
-; CHECK-NEXT:{"_Z5isinfDh","","_ZGVbM64v__Z5isinfDh(_Z13isinf_v1widenDv64_DhDv64_s)"},
-; CHECK-NEXT:{"_Z5isinfDh","","_ZGVbM8v__Z5isinfDh(_Z13isinf_v1widenDv8_DhDv8_s)"},
+; CHECK-NEXT:{"_Z5isinfDh","","_ZGVbM16v__Z5isinfDh(_Z13isinf_v1widenDv16_DhDv16_i)"},
+; CHECK-NEXT:{"_Z5isinfDh","","_ZGVbM32v__Z5isinfDh(_Z13isinf_v1widenDv32_DhDv32_i)"},
+; CHECK-NEXT:{"_Z5isinfDh","","_ZGVbM4v__Z5isinfDh(_Z13isinf_v1widenDv4_DhDv4_i)"},
+; CHECK-NEXT:{"_Z5isinfDh","","_ZGVbM64v__Z5isinfDh(_Z13isinf_v1widenDv64_DhDv64_i)"},
+; CHECK-NEXT:{"_Z5isinfDh","","_ZGVbM8v__Z5isinfDh(_Z13isinf_v1widenDv8_DhDv8_i)"},
 ; CHECK-NEXT:{"_Z5isinfDh","","_ZGVbN16v__Z5isinfDh(_Z13isinf_v1widenDv16_Dh)"},
 ; CHECK-NEXT:{"_Z5isinfDh","","_ZGVbN32v__Z5isinfDh(_Z13isinf_v1widenDv32_Dh)"},
 ; CHECK-NEXT:{"_Z5isinfDh","","_ZGVbN4v__Z5isinfDh(_Z13isinf_v1widenDv4_Dh)"},
 ; CHECK-NEXT:{"_Z5isinfDh","","_ZGVbN64v__Z5isinfDh(_Z13isinf_v1widenDv64_Dh)"},
 ; CHECK-NEXT:{"_Z5isinfDh","","_ZGVbN8v__Z5isinfDh(_Z13isinf_v1widenDv8_Dh)"},
-; CHECK-NEXT:{"_Z5isinfd","","_ZGVbM16v__Z5isinfd(_Z13isinf_v1widenDv16_dDv16_l)"},
-; CHECK-NEXT:{"_Z5isinfd","","_ZGVbM32v__Z5isinfd(_Z13isinf_v1widenDv32_dDv32_l)"},
-; CHECK-NEXT:{"_Z5isinfd","","_ZGVbM4v__Z5isinfd(_Z13isinf_v1widenDv4_dDv4_l)"},
-; CHECK-NEXT:{"_Z5isinfd","","_ZGVbM64v__Z5isinfd(_Z13isinf_v1widenDv64_dDv64_l)"},
-; CHECK-NEXT:{"_Z5isinfd","","_ZGVbM8v__Z5isinfd(_Z13isinf_v1widenDv8_dDv8_l)"},
+; CHECK-NEXT:{"_Z5isinfd","","_ZGVbM16v__Z5isinfd(_Z13isinf_v1widenDv16_dDv16_i)"},
+; CHECK-NEXT:{"_Z5isinfd","","_ZGVbM32v__Z5isinfd(_Z13isinf_v1widenDv32_dDv32_i)"},
+; CHECK-NEXT:{"_Z5isinfd","","_ZGVbM4v__Z5isinfd(_Z13isinf_v1widenDv4_dDv4_i)"},
+; CHECK-NEXT:{"_Z5isinfd","","_ZGVbM64v__Z5isinfd(_Z13isinf_v1widenDv64_dDv64_i)"},
+; CHECK-NEXT:{"_Z5isinfd","","_ZGVbM8v__Z5isinfd(_Z13isinf_v1widenDv8_dDv8_i)"},
 ; CHECK-NEXT:{"_Z5isinfd","","_ZGVbN16v__Z5isinfd(_Z13isinf_v1widenDv16_d)"},
 ; CHECK-NEXT:{"_Z5isinfd","","_ZGVbN32v__Z5isinfd(_Z13isinf_v1widenDv32_d)"},
 ; CHECK-NEXT:{"_Z5isinfd","","_ZGVbN4v__Z5isinfd(_Z13isinf_v1widenDv4_d)"},
@@ -14193,21 +14193,21 @@
 ; CHECK-NEXT:{"_Z8half_tanf","","_ZGVbN16v__Z8half_tanf(_Z8half_tanDv16_f)"},
 ; CHECK-NEXT:{"_Z8half_tanf","","_ZGVbN4v__Z8half_tanf(_Z8half_tanDv4_f)"},
 ; CHECK-NEXT:{"_Z8half_tanf","","_ZGVbN8v__Z8half_tanf(_Z8half_tanDv8_f)"},
-; CHECK-NEXT:{"_Z8isfiniteDh","","_ZGVbM16v__Z8isfiniteDh(_Z16isfinite_v1widenDv16_DhDv16_s)"},
-; CHECK-NEXT:{"_Z8isfiniteDh","","_ZGVbM32v__Z8isfiniteDh(_Z16isfinite_v1widenDv32_DhDv32_s)"},
-; CHECK-NEXT:{"_Z8isfiniteDh","","_ZGVbM4v__Z8isfiniteDh(_Z16isfinite_v1widenDv4_DhDv4_s)"},
-; CHECK-NEXT:{"_Z8isfiniteDh","","_ZGVbM64v__Z8isfiniteDh(_Z16isfinite_v1widenDv64_DhDv64_s)"},
-; CHECK-NEXT:{"_Z8isfiniteDh","","_ZGVbM8v__Z8isfiniteDh(_Z16isfinite_v1widenDv8_DhDv8_s)"},
+; CHECK-NEXT:{"_Z8isfiniteDh","","_ZGVbM16v__Z8isfiniteDh(_Z16isfinite_v1widenDv16_DhDv16_i)"},
+; CHECK-NEXT:{"_Z8isfiniteDh","","_ZGVbM32v__Z8isfiniteDh(_Z16isfinite_v1widenDv32_DhDv32_i)"},
+; CHECK-NEXT:{"_Z8isfiniteDh","","_ZGVbM4v__Z8isfiniteDh(_Z16isfinite_v1widenDv4_DhDv4_i)"},
+; CHECK-NEXT:{"_Z8isfiniteDh","","_ZGVbM64v__Z8isfiniteDh(_Z16isfinite_v1widenDv64_DhDv64_i)"},
+; CHECK-NEXT:{"_Z8isfiniteDh","","_ZGVbM8v__Z8isfiniteDh(_Z16isfinite_v1widenDv8_DhDv8_i)"},
 ; CHECK-NEXT:{"_Z8isfiniteDh","","_ZGVbN16v__Z8isfiniteDh(_Z16isfinite_v1widenDv16_Dh)"},
 ; CHECK-NEXT:{"_Z8isfiniteDh","","_ZGVbN32v__Z8isfiniteDh(_Z16isfinite_v1widenDv32_Dh)"},
 ; CHECK-NEXT:{"_Z8isfiniteDh","","_ZGVbN4v__Z8isfiniteDh(_Z16isfinite_v1widenDv4_Dh)"},
 ; CHECK-NEXT:{"_Z8isfiniteDh","","_ZGVbN64v__Z8isfiniteDh(_Z16isfinite_v1widenDv64_Dh)"},
 ; CHECK-NEXT:{"_Z8isfiniteDh","","_ZGVbN8v__Z8isfiniteDh(_Z16isfinite_v1widenDv8_Dh)"},
-; CHECK-NEXT:{"_Z8isfinited","","_ZGVbM16v__Z8isfinited(_Z16isfinite_v1widenDv16_dDv16_l)"},
-; CHECK-NEXT:{"_Z8isfinited","","_ZGVbM32v__Z8isfinited(_Z16isfinite_v1widenDv32_dDv32_l)"},
-; CHECK-NEXT:{"_Z8isfinited","","_ZGVbM4v__Z8isfinited(_Z16isfinite_v1widenDv4_dDv4_l)"},
-; CHECK-NEXT:{"_Z8isfinited","","_ZGVbM64v__Z8isfinited(_Z16isfinite_v1widenDv64_dDv64_l)"},
-; CHECK-NEXT:{"_Z8isfinited","","_ZGVbM8v__Z8isfinited(_Z16isfinite_v1widenDv8_dDv8_l)"},
+; CHECK-NEXT:{"_Z8isfinited","","_ZGVbM16v__Z8isfinited(_Z16isfinite_v1widenDv16_dDv16_i)"},
+; CHECK-NEXT:{"_Z8isfinited","","_ZGVbM32v__Z8isfinited(_Z16isfinite_v1widenDv32_dDv32_i)"},
+; CHECK-NEXT:{"_Z8isfinited","","_ZGVbM4v__Z8isfinited(_Z16isfinite_v1widenDv4_dDv4_i)"},
+; CHECK-NEXT:{"_Z8isfinited","","_ZGVbM64v__Z8isfinited(_Z16isfinite_v1widenDv64_dDv64_i)"},
+; CHECK-NEXT:{"_Z8isfinited","","_ZGVbM8v__Z8isfinited(_Z16isfinite_v1widenDv8_dDv8_i)"},
 ; CHECK-NEXT:{"_Z8isfinited","","_ZGVbN16v__Z8isfinited(_Z16isfinite_v1widenDv16_d)"},
 ; CHECK-NEXT:{"_Z8isfinited","","_ZGVbN32v__Z8isfinited(_Z16isfinite_v1widenDv32_d)"},
 ; CHECK-NEXT:{"_Z8isfinited","","_ZGVbN4v__Z8isfinited(_Z16isfinite_v1widenDv4_d)"},
@@ -14223,21 +14223,21 @@
 ; CHECK-NEXT:{"_Z8isfinitef","","_ZGVbN4v__Z8isfinitef(_Z16isfinite_v1widenDv4_f)"},
 ; CHECK-NEXT:{"_Z8isfinitef","","_ZGVbN64v__Z8isfinitef(_Z16isfinite_v1widenDv64_f)"},
 ; CHECK-NEXT:{"_Z8isfinitef","","_ZGVbN8v__Z8isfinitef(_Z16isfinite_v1widenDv8_f)"},
-; CHECK-NEXT:{"_Z8isnormalDh","","_ZGVbM16v__Z8isnormalDh(_Z16isnormal_v1widenDv16_DhDv16_s)"},
-; CHECK-NEXT:{"_Z8isnormalDh","","_ZGVbM32v__Z8isnormalDh(_Z16isnormal_v1widenDv32_DhDv32_s)"},
-; CHECK-NEXT:{"_Z8isnormalDh","","_ZGVbM4v__Z8isnormalDh(_Z16isnormal_v1widenDv4_DhDv4_s)"},
-; CHECK-NEXT:{"_Z8isnormalDh","","_ZGVbM64v__Z8isnormalDh(_Z16isnormal_v1widenDv64_DhDv64_s)"},
-; CHECK-NEXT:{"_Z8isnormalDh","","_ZGVbM8v__Z8isnormalDh(_Z16isnormal_v1widenDv8_DhDv8_s)"},
+; CHECK-NEXT:{"_Z8isnormalDh","","_ZGVbM16v__Z8isnormalDh(_Z16isnormal_v1widenDv16_DhDv16_i)"},
+; CHECK-NEXT:{"_Z8isnormalDh","","_ZGVbM32v__Z8isnormalDh(_Z16isnormal_v1widenDv32_DhDv32_i)"},
+; CHECK-NEXT:{"_Z8isnormalDh","","_ZGVbM4v__Z8isnormalDh(_Z16isnormal_v1widenDv4_DhDv4_i)"},
+; CHECK-NEXT:{"_Z8isnormalDh","","_ZGVbM64v__Z8isnormalDh(_Z16isnormal_v1widenDv64_DhDv64_i)"},
+; CHECK-NEXT:{"_Z8isnormalDh","","_ZGVbM8v__Z8isnormalDh(_Z16isnormal_v1widenDv8_DhDv8_i)"},
 ; CHECK-NEXT:{"_Z8isnormalDh","","_ZGVbN16v__Z8isnormalDh(_Z16isnormal_v1widenDv16_Dh)"},
 ; CHECK-NEXT:{"_Z8isnormalDh","","_ZGVbN32v__Z8isnormalDh(_Z16isnormal_v1widenDv32_Dh)"},
 ; CHECK-NEXT:{"_Z8isnormalDh","","_ZGVbN4v__Z8isnormalDh(_Z16isnormal_v1widenDv4_Dh)"},
 ; CHECK-NEXT:{"_Z8isnormalDh","","_ZGVbN64v__Z8isnormalDh(_Z16isnormal_v1widenDv64_Dh)"},
 ; CHECK-NEXT:{"_Z8isnormalDh","","_ZGVbN8v__Z8isnormalDh(_Z16isnormal_v1widenDv8_Dh)"},
-; CHECK-NEXT:{"_Z8isnormald","","_ZGVbM16v__Z8isnormald(_Z16isnormal_v1widenDv16_dDv16_l)"},
-; CHECK-NEXT:{"_Z8isnormald","","_ZGVbM32v__Z8isnormald(_Z16isnormal_v1widenDv32_dDv32_l)"},
-; CHECK-NEXT:{"_Z8isnormald","","_ZGVbM4v__Z8isnormald(_Z16isnormal_v1widenDv4_dDv4_l)"},
-; CHECK-NEXT:{"_Z8isnormald","","_ZGVbM64v__Z8isnormald(_Z16isnormal_v1widenDv64_dDv64_l)"},
-; CHECK-NEXT:{"_Z8isnormald","","_ZGVbM8v__Z8isnormald(_Z16isnormal_v1widenDv8_dDv8_l)"},
+; CHECK-NEXT:{"_Z8isnormald","","_ZGVbM16v__Z8isnormald(_Z16isnormal_v1widenDv16_dDv16_i)"},
+; CHECK-NEXT:{"_Z8isnormald","","_ZGVbM32v__Z8isnormald(_Z16isnormal_v1widenDv32_dDv32_i)"},
+; CHECK-NEXT:{"_Z8isnormald","","_ZGVbM4v__Z8isnormald(_Z16isnormal_v1widenDv4_dDv4_i)"},
+; CHECK-NEXT:{"_Z8isnormald","","_ZGVbM64v__Z8isnormald(_Z16isnormal_v1widenDv64_dDv64_i)"},
+; CHECK-NEXT:{"_Z8isnormald","","_ZGVbM8v__Z8isnormald(_Z16isnormal_v1widenDv8_dDv8_i)"},
 ; CHECK-NEXT:{"_Z8isnormald","","_ZGVbN16v__Z8isnormald(_Z16isnormal_v1widenDv16_d)"},
 ; CHECK-NEXT:{"_Z8isnormald","","_ZGVbN32v__Z8isnormald(_Z16isnormal_v1widenDv32_d)"},
 ; CHECK-NEXT:{"_Z8isnormald","","_ZGVbN4v__Z8isnormald(_Z16isnormal_v1widenDv4_d)"},

commit 4bdcf854daedb6c2f0890c8a5ab98bb83fddddd4

[OpenCL] Fix isfinite/isinf/isnormal mask code (#16496)
This fixes a regression in #16450. CMPLRLLVM-51807

opencl-intel/backend/libraries/ocl_builtins/GENERIC/code_relational.td
@@ -51,7 +51,7 @@ defvar isnormal_v1widen_code = [{
 // Please refer to masked_math.td for naming convention.
 
 defvar masked_impl_code_I32_G_M = [{
-    $Arg0VecType m$Arg0VarName = select(($Arg0VecType)($DEF0), $Arg0VarName, ($Arg0igentype)0 < convert_$Arg0igentype($Arg1VarName));
+    $Arg0VecType m$Arg0VarName = select(($Arg0VecType)($DEF0), $Arg0VarName, ($Arg0igentype)0 != convert_$Arg0igentype($Arg1VarName));
     return $Func(m$Arg0VarName);
   }];
 

opencl-intel/backend/tests/opencl/SATest/builtin/isfinite.cl
@@ -1,0 +1,8 @@
+kernel void test(global float *a) {
+  size_t gid = get_global_id(0);
+  a[gid] = get_sub_group_size();
+  if (gid < 4)
+    a[gid] += isfinite(as_float(0x7F800000));
+  else
+    a[gid] += isfinite(as_float(0x00800000));
+}

opencl-intel/backend/tests/opencl/SATest/builtin/isfinite.ref.xml
@@ -1,0 +1,17 @@
+<?xml version="1.0" encoding="UTF-8" ?>
+<ICSCData>
+  <BufferContainerList size="1" version="0.1">
+    <BufferContainer size="1">
+      <Buffer length="8" type="f32" type_size_in_bytes="4" neat="0">
+        <Vector id="0">8.0</Vector>
+        <Vector id="1">8.0</Vector>
+        <Vector id="2">8.0</Vector>
+        <Vector id="3">8.0</Vector>
+        <Vector id="4">9.0</Vector>
+        <Vector id="5">9.0</Vector>
+        <Vector id="6">9.0</Vector>
+        <Vector id="7">9.0</Vector>
+      </Buffer>
+    </BufferContainer>
+  </BufferContainerList>
+</ICSCData>

opencl-intel/backend/tests/opencl/SATest/builtin/isfinite.tst
@@ -1,0 +1,3 @@
+; RUN: SATest --VAL --config=%s.cfg -tsize=8 -cpuarch="skx" 2>&1 | FileCheck %s
+
+; CHECK: Test Passed.

opencl-intel/backend/tests/opencl/SATest/builtin/isfinite.tst.cfg
@@ -1,0 +1,18 @@
+
+<?xml version="1.0" ?>
+<RunConfiguration>
+    <UseVTune>0</UseVTune>
+    <UseVectorizer>1</UseVectorizer>
+    <ProgramFile compilation_flags="">isfinite.cl</ProgramFile>
+    <ProgramFileType>CL</ProgramFileType>
+    <KernelConfiguration Name="test">
+      <WorkDimention>1</WorkDimention>
+      <LocalWorkSize>8</LocalWorkSize>
+      <GlobalWorkSize>8</GlobalWorkSize>
+      <GlobalWorkOffset>0</GlobalWorkOffset>
+      <InputDataFile>isfinite.ref.xml</InputDataFile>
+      <InputDataFileType>xml</InputDataFileType>
+      <ReferenceDataFile>isfinite.ref.xml</ReferenceDataFile>
+      <ReferenceDataFileType>xml</ReferenceDataFileType>
+    </KernelConfiguration>
+</RunConfiguration>