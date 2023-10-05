b2b1b53d344b1d378b5c2fcd9b68aaf1362eaac6
iclsrc <iclsrc@intel.com>
2023-10-05 07:48:32 -0700

    Resolve of merge (conflict) 53db700e7d25 Merge from 'sycl' to 'sycl-web' (1 commits) 'sycl-web' -> 'xmain-web' (1 commits) (#16697)
    CONFLICT (content): Merge conflict in
    sycl/source/detail/program_manager/program_manager.cpp
    
      commit 53db700e7d2585d78d3360cdbfecd4d9a6725a5b
      Merge: c41bcf3d7481 502ada4a2f70
      Author: iclsrc <ia.compiler.tools.git@intel.com>
      Date:   Thu Oct 5 04:43:19 2023 -0700
    
          Merge from 'sycl' to 'sycl-web' (1 commits)
    
    ---------
    
    Co-authored-by: iclsrc <ia.compiler.tools.git@intel.com>
    Co-authored-by: Maronas, Marcos <marcos.maronas@intel.com>

--- sycl/source/detail/program_manager/program_manager.cpp
+++ sycl/source/detail/program_manager/program_manager.cpp
@@ -427,7 +427,6 @@ appendCompileOptionsForGRFSizeProperties(std::string &CompileOpts,
     // This option works for both LO AND OCL backends.
     CompileOpts += IsEsimdImage ? "-doubleGRF" : "-ze-opt-large-register-file";
   }
-<<<<<<< HEAD
 #if INTEL_CUSTOMIZATION
 #if INTEL_FEATURE_ESIMD_EMBARGO
   if (Is512GRF) {
@@ -438,11 +437,7 @@ appendCompileOptionsForGRFSizeProperties(std::string &CompileOpts,
   }
 #endif // INTEL_FEATURE_ESIMD_EMBARGO
 #endif // INTEL_CUSTOMIZATION
-  // TODO: Support Auto GRF for ESIMD once vc supports it.
-  if (IsAutoGRF && !IsEsimdImage) {
-=======
   if (IsAutoGRF) {
->>>>>>> 53db700e7d2585d78d3360cdbfecd4d9a6725a5b
     if (!CompileOpts.empty())
       CompileOpts += " ";
     // This option works for both LO AND OCL backends.
