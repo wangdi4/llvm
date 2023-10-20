cb9747f05083ab02b5604f0dc44844dff0ee89f1
iclsrc <iclsrc@intel.com>
2023-10-20 13:02:52 -0700

    Resolve of merge (conflict) 9698767dfee6 [SYCL][ESIMD] Reenable fast-math compilation option for dg.. 'sycl' -> 'xmain-web' (#17037)
    CONFLICT (content): Merge conflict in
    sycl/test-e2e/ESIMD/regression/dgetrf.cpp
    CONFLICT (modify/delete): sycl/test-e2e/ESIMD/regression/dgetrf_emu.cpp
    deleted in 9698767dfee6acde192d5e06d6f47b7abf97cea2 and modified in
    HEAD. Version HEAD of sycl/test-e2e/ESIMD/regression/dgetrf_emu.cpp left
    in tree.
    
      commit 9698767dfee6acde192d5e06d6f47b7abf97cea2
      Author: fineg74 <61437305+fineg74@users.noreply.github.com>
      Date:   Fri Oct 20 10:11:54 2023 -0700
    
    [SYCL][ESIMD] Reenable fast-math compilation option for dgetrf test
    (#11264)
    
          Remove emulator only test
    
    ---------
    
    Co-authored-by: iclsrc <ia.compiler.tools.git@intel.com>
    Co-authored-by: Fine, Gregory <gregory.fine@intel.com>

--- sycl/test-e2e/ESIMD/regression/dgetrf.cpp
+++ sycl/test-e2e/ESIMD/regression/dgetrf.cpp
@@ -5,21 +5,13 @@
 // SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 //
 //===----------------------------------------------------------------------===//
-<<<<<<< HEAD
-// TODO: remove fno-fast-math option once the issue is investigated and the test
-// is fixed.
-// UNSUPPORTED: esimd_emulator
-// DEFINE: %{mathflags} = %if cl_options %{/clang:-fno-fast-math%} %else %{-fno-fast-math%}
 // INTEL_CUSTOMIZATION
 // this is a temporary fix for CMPLRTST-21037
 // we pass -fno-inline-functions to sure it won't hang in host compile
 // we don't do the same thing in intel/llvm since it will lead to runfail
 // DEFINE: %{noinlineflags} = %if cl_options %{/clang:-fno-inline-functions%} %else %{-fno-inline-functions%}
-// RUN: %{build} %{mathflags} %{noinlineflags} -I%S/.. -O2 -o %t.out
+// RUN: %{build} %{noinlineflags} -I%S/.. -O2 -o %t.out
 // end INTEL_CUSTOMIZATION
-=======
-// RUN: %{build} -I%S/.. -o %t.out
->>>>>>> 9698767dfee6acde192d5e06d6f47b7abf97cea2
 // RUN: %{run} %t.out 3 2 1
 //
 // This test checks the correctness of ESIMD program for batched LU
