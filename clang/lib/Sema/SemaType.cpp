393ee4e388dc87977e84332624a896645263972f
iclsrc <iclsrc@intel.com>
2023-10-26 15:38:21 -0700

    Resolve of merge (conflict) 56b58d0a2f0c Merge from 'main' to 'sycl-web' (93 commits) 'sycl-web' -> 'xmain-web' (#17161)
    CONFLICT (content): Merge conflict in clang/lib/Sema/SemaType.cpp
    
      commit 56b58d0a2f0c79b33db24172fb5ab4772392d6a4
      Merge: 32c1c0663326 09e8ef975d99
      Author: Fine, Gregory <gregory.fine@intel.com>
      Date:   Thu Oct 26 13:29:42 2023 -0700
    
          Merge from 'main' to 'sycl-web' (93 commits)
    
    CONFLICT (content): Merge conflict in clang/lib/Sema/SemaType.cpp
    
    ---------
    
    Co-authored-by: iclsrc <ia.compiler.tools.git@intel.com>
    Co-authored-by: Fine, Gregory <gregory.fine@intel.com>

--- clang/lib/Sema/SemaType.cpp
+++ clang/lib/Sema/SemaType.cpp
@@ -2786,18 +2786,9 @@ QualType Sema::BuildArrayType(QualType T, ArrayType::ArraySizeModifier ASM,
   }
 
   // Delay diagnostic to SemaSYCL so only Kernel functions are diagnosed.
-<<<<<<< HEAD
 #if INTEL_COLLAB
   if (!getLangOpts().OpenMPLateOutline)
 #endif // INTEL_COLLAB
-  if (T->isVariableArrayType() && !Context.getTargetInfo().isVLASupported() &&
-      !getLangOpts().SYCLIsDevice) {
-    // CUDA device code and some other targets don't support VLAs.
-    bool IsCUDADevice = (getLangOpts().CUDA && getLangOpts().CUDAIsDevice);
-    targetDiag(Loc,
-               IsCUDADevice ? diag::err_cuda_vla : diag::err_vla_unsupported)
-        << (IsCUDADevice ? CurrentCUDATarget() : 0);
-=======
   if (T->isVariableArrayType() && !getLangOpts().SYCLIsDevice) {
     if (!Context.getTargetInfo().isVLASupported()) {
       // CUDA device code and some other targets don't support VLAs.
@@ -2810,7 +2801,6 @@ QualType Sema::BuildArrayType(QualType T, ArrayType::ArraySizeModifier ASM,
       // checking that the VLA is not being used within a coroutine.
       FSI->setHasVLA(Loc);
     }
->>>>>>> 56b58d0a2f0c79b33db24172fb5ab4772392d6a4
   }
 
   // If this is not C99, diagnose array size modifiers on non-VLAs.
