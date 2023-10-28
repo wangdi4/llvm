6fea0657c8cf0c1d11f1882435afec7dc352b3f0
fineg74 <61437305+fineg74@users.noreply.github.com>
2023-10-27 17:38:25 -0700

    Fix test failure (#17158)
    JIRA: https://jira.devtools.intel.com/browse/CMPLRLLVM-52992

--- sycl/test/abi/preview_lib_marker.cpp
+++ sycl/test/abi/preview_lib_marker.cpp
@@ -25,4 +25,4 @@ int main() {
 }
 
 // CHECK-NO-PREVIEW-WINDOWS: unresolved external symbol "void __cdecl sycl::_V1::detail::PreviewMajorReleaseMarker(void)"
-// CHECK-NO-PREVIEW-LINUX: undefined reference to `sycl::_V1::detail::PreviewMajorReleaseMarker()'
+// CHECK-NO-PREVIEW-LINUX: undefined symbol: sycl::_V1::detail::PreviewMajorReleaseMarker()
