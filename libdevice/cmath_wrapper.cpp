0e61e38e3d6ed6b33d84f079d05b44d6636216c1
jinge90 <ge.jin@intel.com>
2023-10-29 21:51:26 +0800

    Support isnan, isinf, isnormal, signbitn in OMP libdevice (#16849)
    This PR aims to support a requirement from A21:
    https://jira.devtools.intel.com/browse/CMPLRLIBS-33270
    Customer requires isnan, isnormal, isinf, signbit in to be offloadable.
    These math functions depend on some internal symbol on Linux, we support
    them via openmp libdevice.
    This PR can't support those functions on Windows since MSVC math
    implementation depends on some MSVC internal functions, we will support
    later in future.
    Thanks very much.
    
    
    
    Signed-off-by: jinge90 <ge.jin@intel.com>
    
    Signed-off-by: jinge90 <ge.jin@intel.com>

--- libdevice/cmath_wrapper.cpp
+++ libdevice/cmath_wrapper.cpp
@@ -167,6 +167,31 @@ float nearbyintf(float x) { return __nv_nearbyintf(x); }
 
 #if INTEL_COLLAB
 #if OMP_LIBDEVICE
+#if !defined(_WIN32)
+DEVICE_EXTERN_C_INLINE
+int __devicelib___isnanf(float x);
+
+DEVICE_EXTERN_C_INLINE
+int __isnanf(float x) { return __devicelib___isnanf(x); }
+
+DEVICE_EXTERN_C_INLINE
+int __devicelib___isinff(float x);
+
+DEVICE_EXTERN_C_INLINE
+int __isinff(float x) { return __devicelib___isinff(x); }
+
+DEVICE_EXTERN_C_INLINE
+int __devicelib___isnormalf(float x);
+
+DEVICE_EXTERN_C_INLINE
+int __isnormalf(float x) { return __devicelib___isnormalf(x); }
+
+DEVICE_EXTERN_C_INLINE
+int __devicelib___signbitf(float x);
+
+DEVICE_EXTERN_C_INLINE
+int __signbitf(float x) { return __devicelib___signbitf(x); }
+#endif
 #pragma omp end declare target
 #endif  // OMP_LIBDEVICE
 #endif  // INTEL_COLLAB
