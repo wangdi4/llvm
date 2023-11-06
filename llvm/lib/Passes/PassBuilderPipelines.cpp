c6d3c2e5159490aef19a0491ce989ae1805d50af
iclsrc <iclsrc@intel.com>
2023-11-05 23:20:13 -0800

    Resolve of merge (conflict) 87f5e22987f5 [MemProf] Tolerate missing leaf debug frames (#71233) 'main' -> 'xmain-web' (#17385)
    CONFLICT (content): Merge conflict in
    llvm/lib/Passes/PassBuilderPipelines.cpp
    
      commit 87f5e22987f509b7bd5b67eb2a19697508077f25
      Author: Teresa Johnson <tejohnson@google.com>
      Date:   Fri Nov 3 21:01:07 2023 -0700
    
          [MemProf] Tolerate missing leaf debug frames (#71233)
    
    Loosen up the matching so that a missing leaf debug frame in the profile
    does not prevent matching an allocation context if we can match further
    up the inlined call context. This relies on the pre-inliner, which was
    already the default when performing normal PGO feedback along with the
    MemProf feedback, but to ensure matching is not affected by the presence
          of PGO, enable the pre-inliner for MemProf feedback as well.
    
    ---------
    
    Co-authored-by: iclsrc <ia.compiler.tools.git@intel.com>
    Co-authored-by: Wang, Xin10 <xin10.wang@intel.com>

--- llvm/lib/Passes/PassBuilderPipelines.cpp
+++ llvm/lib/Passes/PassBuilderPipelines.cpp
@@ -1388,6 +1388,13 @@ void PassBuilder::addPreInlinerPasses(ModulePassManager &MPM,
   // performance testing.
   // FIXME: this comment is cargo culted from the old pass manager, revisit).
   IP.HintThreshold = Level.isOptimizingForSize() ? PreInlineThreshold : 325;
+#if INTEL_CUSTOMIZATION
+  IP.PrepareForLTO = PrepareForLTO;
+  // Parse - inline-forceinline option and transforms corresponding attributes
+  MPM.addPass(InlineForceInlinePass());
+  // Parse -[no]inline-list option and set corresponding attributes.
+  MPM.addPass(InlineListsPass());
+#endif //INTEL_CUSTOMIZATION
   ModuleInlinerWrapperPass MIWP(
       IP, /* MandatoryFirst */ true,
       InlineContext{LTOPhase, InlinePass::EarlyInliner});
@@ -1398,7 +1405,11 @@ void PassBuilder::addPreInlinerPasses(ModulePassManager &MPM,
   FPM.addPass(EarlyCSEPass()); // Catch trivial redundancies.
   FPM.addPass(SimplifyCFGPass(SimplifyCFGOptions().convertSwitchRangeToICmp(
       true)));                    // Merge & remove basic blocks.
-  FPM.addPass(InstCombinePass()); // Combine silly sequences.
+#if INTEL_CUSTOMIZATION
+  // Combine silly sequences. Set PreserveAddrCompute to true in LTO phase 1
+  // if IP ArrayTranspose is enabled.
+  addInstCombinePass(FPM, !DTransEnabled, true /* EnableCanonicalizeSwap */);
+#endif // INTEL_CUSTOMIZATION
   invokePeepholeEPCallbacks(FPM, Level);
 
   CGPipeline.addPass(createCGSCCToFunctionPassAdaptor(
@@ -1420,56 +1431,6 @@ void PassBuilder::addPGOInstrPasses(ModulePassManager &MPM,
                                     std::string ProfileRemappingFile,
                                     IntrusiveRefCntPtr<vfs::FileSystem> FS) {
   assert(Level != OptimizationLevel::O0 && "Not expecting O0 here!");
-<<<<<<< HEAD
-  if (!IsCS && !DisablePreInliner) {
-    InlineParams IP;
-
-    IP.DefaultThreshold = PreInlineThreshold;
-
-    // FIXME: The hint threshold has the same value used by the regular inliner
-    // when not optimzing for size. This should probably be lowered after
-    // performance testing.
-    // FIXME: this comment is cargo culted from the old pass manager, revisit).
-    IP.HintThreshold = Level.isOptimizingForSize() ? PreInlineThreshold : 325;
-    IP.PrepareForLTO = PrepareForLTO; // INTEL
-
-#if INTEL_CUSTOMIZATION
-    // Parse - inline-forceinline option and transforms corresponding attributes
-    MPM.addPass(InlineForceInlinePass());
-    // Parse -[no]inline-list option and set corresponding attributes.
-    MPM.addPass(InlineListsPass());
-#endif //INTEL_CUSTOMIZATION
-
-    ModuleInlinerWrapperPass MIWP(
-        IP, /* MandatoryFirst */ true,
-        InlineContext{LTOPhase, InlinePass::EarlyInliner});
-    CGSCCPassManager &CGPipeline = MIWP.getPM();
-
-    FunctionPassManager FPM;
-    FPM.addPass(CleanupFakeLoadsPass()); // INTEL
-    FPM.addPass(SROAPass(SROAOptions::ModifyCFG));
-    FPM.addPass(EarlyCSEPass()); // Catch trivial redundancies.
-    FPM.addPass(SimplifyCFGPass(SimplifyCFGOptions().convertSwitchRangeToICmp(
-        true)));                    // Merge & remove basic blocks.
-#if INTEL_CUSTOMIZATION
-    // Combine silly sequences. Set PreserveAddrCompute to true in LTO phase 1
-    // if IP ArrayTranspose is enabled.
-    addInstCombinePass(FPM, !DTransEnabled, true /* EnableCanonicalizeSwap */);
-#endif // INTEL_CUSTOMIZATION
-    invokePeepholeEPCallbacks(FPM, Level);
-
-    CGPipeline.addPass(createCGSCCToFunctionPassAdaptor(
-        std::move(FPM), PTO.EagerlyInvalidateAnalyses));
-
-    MPM.addPass(std::move(MIWP));
-
-    // Delete anything that is now dead to make sure that we don't instrument
-    // dead code. Instrumentation can end up keeping dead code around and
-    // dramatically increase code size.
-    MPM.addPass(GlobalDCEPass());
-  }
-=======
->>>>>>> 87f5e22987f509b7bd5b67eb2a19697508077f25
 
 #if INTEL_CUSTOMIZATION
   if (PGOAction == PGOOptions::MLUse) {
@@ -1986,23 +1947,18 @@ PassBuilder::buildModuleSimplificationPipeline(OptimizationLevel Level,
   MPM.addPass(createModuleToFunctionPassAdaptor(std::move(GlobalCleanupPM),
                                                 PTO.EagerlyInvalidateAnalyses));
 
-<<<<<<< HEAD
-#if INTEL_CUSTOMIZATION
-#if INTEL_FEATURE_SW_DTRANS
-  if (PrepareForLTO && DTransEnabled)
-    MPM.addPass(dtransOP::DTransForceInlineOPPass());
-#endif // INTEL_FEATURE_SW_DTRANS
-#endif // INTEL_CUSTOMIZATION
-
-#if INTEL_CUSTOMIZATION
-=======
   // Invoke the pre-inliner passes for instrumentation PGO or MemProf.
   if (PGOOpt && Phase != ThinOrFullLTOPhase::ThinLTOPostLink &&
       (PGOOpt->Action == PGOOptions::IRInstr ||
        PGOOpt->Action == PGOOptions::IRUse || !PGOOpt->MemoryProfile.empty()))
     addPreInlinerPasses(MPM, Level, Phase);
 
->>>>>>> 87f5e22987f509b7bd5b67eb2a19697508077f25
+#if INTEL_CUSTOMIZATION
+#if INTEL_FEATURE_SW_DTRANS
+  if (PrepareForLTO && DTransEnabled)
+    MPM.addPass(dtransOP::DTransForceInlineOPPass());
+#endif // INTEL_FEATURE_SW_DTRANS
+
   // Add all the requested passes for instrumentation PGO, if requested.
   if (PGOOpt && !PGOOpt->IsCGPGO &&
       Phase != ThinOrFullLTOPhase::ThinLTOPostLink &&
@@ -3264,7 +3220,7 @@ PassBuilder::buildPerModuleDefaultPipeline(OptimizationLevel Level,
       addPGOInstrPasses(MPM, Level, PGOOpt->Action,
                         /* IsCS */ false, PGOOpt->AtomicCounterUpdate,
                         PGOOpt->ProfileFile, PGOOpt->ProfileRemappingFile,
-                        LTOPhase, PGOOpt->FS);
+                        PGOOpt->FS);
       MPM.addPass(PGOIndirectCallPromotion(false, false));
     }
     if (PGOOpt && PGOOpt->IsCGPGO &&
@@ -4146,7 +4102,7 @@ PassBuilder::buildLTODefaultPipeline(OptimizationLevel Level,
     addPGOInstrPasses(MPM, Level, PGOOpt->Action, // INTEL
                       /* IsCS */ false, PGOOpt->AtomicCounterUpdate,
                       PGOOpt->ProfileFile, PGOOpt->ProfileRemappingFile,
-                      ThinOrFullLTOPhase::FullLTOPostLink, PGOOpt->FS);
+                      PGOOpt->FS);
     MPM.addPass(PGOIndirectCallPromotion(false, false));
   }
 #endif // !INTEL_PRODUCT_RELEASE
