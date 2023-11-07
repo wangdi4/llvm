852cfba658bcbe9ba38841517ff58947bec089fa
Haonan Yang <haonan.yang@intel.com>
2023-11-07 16:30:24 +0800

    [OpenCL] Assertion fails if host CPU is generic. (#17379)
    sys::getHostCPUName() returns "generic" if
    getIntelProcessorTypeAndSubtype returns empty string.
    CMPLRLLVM-53034.

--- opencl-intel/utils/cl_sys_utils/cl_cpu_detect.cpp
+++ opencl-intel/utils/cl_sys_utils/cl_cpu_detect.cpp
@@ -213,6 +213,11 @@ CPUDetect *CPUDetect::GetInstance() {
 
 CPUDetect::CPUDetect(void) : m_is64BitOS(sizeof(void *) == 8) {
   m_HostCPUString = llvm::sys::getHostCPUName().str();
+#ifdef INTEL_PRODUCT_RELEASE
+  if (m_HostCPUString == "generic")
+    reportWarning("Unknown host CPU.");
+#endif // INTEL_PRODUCT_RELEASE
+  assert(m_HostCPUString != "generic" && "Unknown host CPU.");
   llvm::sys::getHostCPUFeatures(m_HostCPUFeatures);
   GetHostCPUBrandInfo();
   // If CL_CONFIG_CPU_TARGET_ARCH is not used, m_CPUFeatures and
