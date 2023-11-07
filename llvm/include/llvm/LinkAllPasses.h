4913fc3c0de3538b38f15ae22423a6b172414dec
iclsrc <iclsrc@intel.com>
2023-11-06 19:08:42 -0800

    Resolve of merge (conflict) 2400c54c37d5 [Vectorize] Remove Transforms/Vectorize.h (#71294) 'main' -> 'xmain-web' (#17410)
    CONFLICT (content): Merge conflict in llvm/include/llvm/LinkAllPasses.h
    CONFLICT (modify/delete): llvm/include/llvm/Transforms/Vectorize.h
    deleted in 2400c54c37d5afdfec016b9a71f161ac10a49b31 and modified in
    HEAD. Version HEAD of llvm/include/llvm/Transforms/Vectorize.h left in
    tree.
    
      commit 2400c54c37d5afdfec016b9a71f161ac10a49b31
      Author: Tom Stellard <tstellar@redhat.com>
      Date:   Mon Nov 6 14:04:22 2023 -0800
    
          [Vectorize] Remove Transforms/Vectorize.h (#71294)
    
          The only thing in this file is a declaration for
    createLoadStoreVectorizerPass(), and this function is already declared
          in LoadStoreVectorizer.h.
    
    ---------
    
    Co-authored-by: iclsrc <ia.compiler.tools.git@intel.com>
    Co-authored-by: Wang, Xin10 <xin10.wang@intel.com>

--- llvm/include/llvm/LinkAllPasses.h
+++ llvm/include/llvm/LinkAllPasses.h
@@ -70,10 +70,9 @@
 #include "llvm/Transforms/Utils.h"
 #include "llvm/Transforms/Utils/SymbolRewriter.h"
 #include "llvm/Transforms/Utils/UnifyFunctionExitNodes.h"
-<<<<<<< HEAD
-#include "llvm/Transforms/Vectorize.h"
 
 #if INTEL_CUSTOMIZATION
+#include "llvm/Transforms/Vectorize.h"
 #include "llvm/Analysis/Intel_ArrayUseAnalysis.h"
 #include "llvm/Analysis/Intel_LoopAnalysis/Passes.h"
 #include "llvm/Analysis/Intel_OptReport/OptReportOptionsPass.h"
@@ -93,9 +92,7 @@
 #include "llvm/Transforms/VPO/VPOPasses.h"
 #endif // INTEL_COLLAB
 
-=======
 #include "llvm/Transforms/Vectorize/LoadStoreVectorizer.h"
->>>>>>> 2400c54c37d5afdfec016b9a71f161ac10a49b31
 #include <cstdlib>
 
 namespace {
