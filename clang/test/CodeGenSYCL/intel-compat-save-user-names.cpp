fc20c7c907c9fa8aea20f45638efcb3e46694076
premanandrao <premanand.m.rao@intel.com>
2023-11-13 13:37:09 -0500

    cmplrllvm-36590: use alloca to home byval arguments for debug (#16082)
    To enable better debugging, save the byval arguments also using alloca.

---
+++ clang/test/CodeGenSYCL/intel-compat-save-user-names.cpp
@@ -0,0 +1,85 @@
+// RUN:  %clang_cc1 -fintel-compatibility -fsycl-is-device -internal-isystem %S/Inputs -triple spir64-unknown-unknown -disable-llvm-passes -emit-llvm %s -o - | FileCheck %s
+
+// Test to verify that user specified names are retained in SPIR
+// kernel argument names.
+
+// This test is cloned from the test save-user-names.cpp, and uses the -fintel-compatibility flag in addition.
+// This is to minimize the effect of INTEL_CUSTOMIZATION changes on community tests.
+
+#include "sycl.hpp"
+
+struct NestedSimple {
+  int NestedSimpleField;
+};
+
+struct NestedComplex {
+  int NestedComplexField;
+  sycl::accessor<char, 1, sycl::access::mode::read> NestedAccField;
+};
+
+struct KernelFunctor {
+  int IntField;
+  sycl::accessor<char, 1, sycl::access::mode::read> AccField1;
+  sycl::accessor<char, 1, sycl::access::mode::read> AccField2;
+  NestedSimple NestedSimpleObj;
+  NestedComplex NestedComplexObj;
+  void operator()() const {}
+};
+
+int main() {
+  sycl::queue q;
+
+  q.submit([&](sycl::handler &cgh) {
+    KernelFunctor FunctorObj;
+    cgh.single_task<class Kernel1>(FunctorObj);
+  });
+
+  q.submit([&](sycl::handler &cgh) {
+    int Data;
+    NestedSimple NestedSimpleObj;
+    NestedComplex NestedComplexObj;
+    sycl::accessor<char, 1, sycl::access::mode::read> CapturedAcc1, CapturedAcc2;
+    cgh.single_task<class Kernel2>([=]() {
+      Data;
+      CapturedAcc1;
+      CapturedAcc2;
+      NestedSimpleObj;
+      NestedComplexObj;
+    });
+  });
+
+  return 0;
+}
+
+// Check kernel parameters generated when kernel is defined as Functor
+
+// NOTE: Accessor fields have 4 corresponding openCL kernel arguments. When
+// the compiler generates the openCL kernel arguments, they are generated
+// with the same name (i.e the user-specified name for accessor). Since LLVM
+// IR cannot have same names, a number is appended in IR.
+//
+// CHECK: define {{.*}}spir_kernel void @{{.*}}Kernel1
+// CHECK-SAME: %_arg_IntField
+// INTEL_CUSTOMIZATION
+// CHECK-SAME: %_arg_AccField1{{.*}}%_arg_AccField11{{.*}}%_arg_AccField12{{.*}}%_arg_AccField14
+// CHECK-SAME: %_arg_AccField2{{.*}}%_arg_AccField26{{.*}}%_arg_AccField27{{.*}}%_arg_AccField29
+// end INTEL_CUSTOMIZATION
+// CHECK-SAME: %_arg_NestedSimpleObj
+// CHECK-SAME: %_arg_NestedComplexField
+// INTEL_CUSTOMIZATION
+// CHECK-SAME: %_arg_NestedAccField{{.*}}%_arg_NestedAccField11{{.*}}%_arg_NestedAccField12{{.*}}%_arg_NestedAccField14
+// end INTEL_CUSTOMIZATION
+
+// Check kernel parameters generated when kernel is defined as Lambda
+//
+// CHECK: define {{.*}}spir_kernel void @{{.*}}Kernel2
+// CHECK-SAME: %_arg_Data
+// INTEL_CUSTOMIZATION
+// CHECK-SAME: %_arg_CapturedAcc1{{.*}}%_arg_CapturedAcc11{{.*}}%_arg_CapturedAcc12{{.*}}%_arg_CapturedAcc14
+// CHECK-SAME: %_arg_CapturedAcc2{{.*}}%_arg_CapturedAcc26{{.*}}%_arg_CapturedAcc27{{.*}}%_arg_CapturedAcc29
+// end INTEL_CUSTOMIZATION
+// CHECK-SAME: %_arg_NestedSimpleObj
+// CHECK-SAME: %_arg_NestedComplexField
+// INTEL_CUSTOMIZATION
+// CHECK-SAME: %_arg_NestedAccField{{.*}}%_arg_NestedAccField11{{.*}}%_arg_NestedAccField12{{.*}}%_arg_NestedAccField14
+// end INTEL_CUSTOMIZATION
