30dc141de52d24fad275d75bb9948c3cb8cedcd4
Nick Sarnie <sarnex@users.noreply.github.com>
2023-11-27 22:01:09 +0000

    [SYCL][ESIMD] Support sycl::address_space_cast (#17814)
    Cherry pick of
    https://github.com/intel/llvm/commit/1060b20df94a32d3f23f85a639c763048a9aa909
    
    ---------
    
    Signed-off-by: Sarnie, Nick <nick.sarnie@intel.com>

--- llvm/lib/SYCLLowerIR/ESIMD/ESIMDVerifier.cpp
+++ llvm/lib/SYCLLowerIR/ESIMD/ESIMDVerifier.cpp
@@ -48,11 +48,13 @@ namespace id = itanium_demangle;
 #define DEBUG_TYPE "esimd-verifier"
 
 // A list of SYCL functions (regexps) allowed for use in ESIMD context.
+// clang-format off
 static const char *LegalSYCLFunctions[] = {
     "^sycl::_V1::accessor<.+>::accessor",
     "^sycl::_V1::accessor<.+>::~accessor",
     "^sycl::_V1::accessor<.+>::getQualifiedPtr",
     "^sycl::_V1::accessor<.+>::__init_esimd",
+    "^sycl::_V1::address_space_cast",
     "^sycl::_V1::local_accessor<.+>::local_accessor",
     "^sycl::_V1::local_accessor<.+>::__init_esimd",
     "^sycl::_V1::local_accessor<.+>::get_pointer",
@@ -102,6 +104,7 @@ static const char *LegalSYCLFunctionsInStatelessMode[] = {
     "^sycl::_V1::accessor<.+>::getMemoryRange",
     "^sycl::_V1::accessor<.+>::getOffset",
     "^sycl::_V1::accessor<.+>::operator\\[\\]"};
+// clang-format on
 
 namespace {
 
